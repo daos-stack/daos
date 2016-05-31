cf88090fa7ba2d19889d23e6dc68a38f15d0db27
Johann Lombardi <johann.lombardi@intel.com>
2016-05-31 13:08:08 +0200

    DAOS-96 license: move source files to Apache 2.0
    Change license in source files to Apache 2.0.
    
    Change-Id: I34595d8ea3277881c6ff9329439f570900502c13
    Signed-off-by: Johann Lombardi <johann.lombardi@intel.com>
    Reviewed-on: http://review.whamcloud.com/20517
    Tested-by: Jenkins

--- src/server/tls.c
+++ src/server/tls.c
@@ -1,22 +1,24 @@
 /**
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-2.1.html
+ * (C) Copyright 2016 Intel Corporation.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
  * The Government's rights to use, modify, reproduce, release, perform, display,
- * or disclose this software are subject to the terms of the LGPL License as
+ * or disclose this software are subject to the terms of the Apache License as
  * provided in Contract No. B609815.
  * Any reproduction of computer software, computer software documentation, or
  * portions thereof marked with this legend must also reproduce the markings.
- *
- * (C) Copyright 2016 Intel Corporation.
  */
 /**
  * This file is part of the DAOS server. It implements thread-local storage
--- src/vos/tests/vos_test.sh
+++ src/vos/tests/vos_test.sh
@@ -1,27 +1,4 @@
 ##!/bin/bash
-#
-##All rights reserved. This program and the accompanying materials
-#are made available under the terms of the GNU Lesser General Public License
-#(LGPL) version 2.1 which accompanies this distribution, and is available at
-#http://www.gnu.org/licenses/lgpl-2.1.html
-#
-#This library is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#Lesser General Public License for more details.
-#GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
-#The Government's rights to use, modify, reproduce, release, perform, display,
-#or disclose this software are subject to the terms of the LGPL License as
-#provided in Contract No. B609815.
-#any reproduction of computer software, computer software documentation, or
-#portions thereof marked with this legend must also reproduce the markings.
-#
-#-------------------------------------------------------------------------
-#(C) Copyright 2016 Intel Corporation.
-#Test script for launching vos tests
-#vos/tests/vos_test.sh
-#Author: Vishwanath Venkatesan <vishwanath.venkatesan@intel.com>
-#-------------------------------------------------------------------------
 
 #Source Directory
 DAOS_DIR=${DAOS_DIR:-$(cd $(dirname $0)/../../..; echo $PWD)}
