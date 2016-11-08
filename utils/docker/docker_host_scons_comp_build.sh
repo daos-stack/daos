81550dbdcfab688aa3829366da77bbaf64eda5c7
John.E.Malmberg <john.e.malmberg@intel.com>
2016-11-08 18:35:10 +0000

    DCO-6107 Docker host lookup comp_build.sh
    Have the Docker host script lookup the actual comp_build.sh
    script used by the component.
    
    Change-Id: I7d24033545e8ab2ef212096d0a44df218792bdb0
    Signed-off-by: John.E.Malmberg <john.e.malmberg@intel.com>
    Reviewed-on: http://review.whamcloud.com/23657
    Tested-by: Jenkins
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- utils/docker/docker_host_scons_comp_build.sh
+++ utils/docker/docker_host_scons_comp_build.sh
@@ -7,7 +7,11 @@ docker_post_script=`find . -name docker_host_postrun.sh`
 
 source ${docker_pre_script}
 
-comp_build_script=`find . -name comp_build.sh`
+# Look up the original comp_build.sh script expected to be used.
+comp_build1=`grep comp_build.sh -r --include="build_${TARGET}.sh"`
+comp_build2=${comp_build1#*:}
+IFS=' ' read -a comp_build3 <<<"${comp_build2}"
+comp_build_script=${comp_build3[0]}
 
 # Actually run the file
 ${comp_build_script} $*
