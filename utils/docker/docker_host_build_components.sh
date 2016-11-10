14cec43d86c9298e41aee105bbe7b29773cd8eb5
John.E.Malmberg <john.e.malmberg@intel.com>
2016-11-10 22:30:48 +0000

    DCO-6107 Review job changes for Docker
    For review jobs, docker_host_build_components.sh needs
     * To not specify a REQUIRES target
     * To not the prebuild script to delete the working dir.
     * To not run the post build script that records the
       git commit hashes used.
    
    Save off stderr from the container run in case it is needed.
    
    Change-Id: I04d6512d34b250ebbc8387fd3b11a1cf0a56c111
    Signed-off-by: John.E.Malmberg <john.e.malmberg@intel.com>
    Reviewed-on: http://review.whamcloud.com/23712
    Tested-by: Jenkins
    Reviewed-by: Martin Mennig <martinx.mennig@intel.com>

--- utils/docker/docker_host_build_components.sh
+++ utils/docker/docker_host_build_components.sh
@@ -5,7 +5,20 @@ set -uex
 docker_pre_script=`find . -name docker_host_prerun.sh`
 docker_post_script=`find . -name docker_host_postrun.sh`
 
-source ${docker_pre_script}
+# Review jobs need to run the pre script once before this script
+# and not re-run it.
+if [[ ! -v DOCKER_IMAGE ]];then
+  source ${docker_pre_script}
+else
+  docker_setup_file="docker_setup.sh"
+fi
+
+default_requires=
+
+real_job_name=${JOB_NAME%*/}
+if [ "${real_job_name}" == *${JOB_SUFFIX} ];then
+  default_requires="REQUIRES=${TARGET}"
+fi
 
 printf "option=\"TARGET_PREFIX=${DIST_TARGET} SRC_PREFIX=../\"\n" >> \
   ${docker_setup_file}
@@ -14,9 +27,12 @@ docker run --rm -u $USER -v ${PWD}:/work \
            -v ${WORK_TARGET}:${DIST_MOUNT} \
            -a stderr -a stdout -i coral/${DOCKER_IMAGE} \
            /work/scons_local/utils/docker/docker_build_components.sh \
-	   REQUIRES=${TARGET} | tee docker_build.log
+	   ${default_requires} 2>&1 | tee docker_build.log
 
-source ${docker_post_script}
+# Review jobs do not have artifacts to process
+if [ "${real_job_name}" == *${JOB_SUFFIX} ];then
+  source ${docker_post_script}
+fi
 
 set +u
 target_post_build=`find . -name ${TARGET}_post_build.sh`
