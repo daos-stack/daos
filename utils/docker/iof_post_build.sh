8245532269b102d642db3224251e49da5169eb00
John.E.Malmberg <john.e.malmberg@intel.com>
2017-03-03 19:22:39 +0000

    DCO-6783 IOF tests now work on SLES/Ubuntu
    Change-Id: Idbc679d9a2bfaf3e9769fad058b1f5430bf50e47
    Signed-off-by: John.E.Malmberg <john.e.malmberg@intel.com>
    Reviewed-on: https://review.whamcloud.com/25761
    Tested-by: Jenkins
    Reviewed-by: Martin Mennig <martinx.mennig@intel.com>

--- utils/docker/iof_post_build.sh
+++ utils/docker/iof_post_build.sh
@@ -1,23 +1,13 @@
 
 test_results="docker_build.log"
 
-case ${distro} in
-  sles12*)
-    echo "No cmocka package for SLES, unit tests not run!"
-    ;;
-  ubuntu*)
-    echo "No cmocka package for Ubuntu, unit tests not run!"
-    ;;
-  *)
-    ls ${test_results}
-    set +e
-    grep "This run had test failures" ${test_results}
-    rc=$?
-    set -e
-    if [ ${rc} == 0 ]; then
-      # Fail this build.
-      exit 1
-    fi
-    ;;
-esac
+ls ${test_results}
+set +e
+grep "This run had test failures" ${test_results}
+rc=$?
+set -e
+if [ ${rc} == 0 ]; then
+  # Fail this build.
+  exit 1
+fi
 
