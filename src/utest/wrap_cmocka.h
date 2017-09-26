35e22539a70cb9bc9bb32571f0b006b5a2350581
Jeff Olivier <jeffrey.v.olivier@intel.com>
2017-09-26 14:54:42 +0000

    CART-89 scons: Use check_component rather than Configure
    Let scons_local check the prerequisites.  Updates to latest
    scons_local which provides the check_component feature
    
    Change-Id: Iddcf7c13a23f2a69f6d5b814ccc24ca6c38e265a
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>
    Reviewed-on: https://review.whamcloud.com/29210
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Yulu Jia <yulu.jia@intel.com>
    Reviewed-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/utest/wrap_cmocka.h
+++ src/utest/wrap_cmocka.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Intel Corporation
+/* Copyright (C) 2017 Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -36,46 +36,20 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /** Header file to include items needs by cmocka libraries */
-#ifndef __UTEST_CMOCKA_H__
-# define __UTEST_CMOCKA_H__
+#ifndef __WRAP_CMOCKA_H__
+# define __WRAP_CMOCKA_H__
 
-#include "wrap_cmocka.h"
+# include <stdarg.h>
+# include <stddef.h>
+# include <setjmp.h>
+
 # ifdef __cplusplus
 extern "C" {
 # endif /* __cplusplus */
 
-# include <pmix.h>
-
-int __wrap_PMIx_Init(pmix_proc_t *proc, pmix_info_t info[], size_t ninfo);
-int __wrap_PMIx_Get(const pmix_proc_t *proc, const char key[],
-		    const pmix_info_t info[], size_t ninfo, pmix_value_t **val);
-int __wrap_PMIx_Publish(const pmix_info_t info[], size_t ninfo);
-int __wrap_PMIx_Lookup(pmix_pdata_t data[], size_t ndata,
-		       const pmix_info_t info[], size_t ninfo);
-int __wrap_PMIx_Fence(const pmix_proc_t procs[], size_t nprocs,
-		      const pmix_info_t info[], size_t ninfo);
-int __wrap_PMIx_Unpublish(char **keys, const pmix_info_t info[], size_t ninfo);
-void __wrap_PMIx_Register_event_handler(pmix_status_t codes[], size_t ncodes,
-					pmix_info_t info[], size_t ninfo,
-					pmix_notification_fn_t evhdlr,
-					pmix_evhdlr_reg_cbfunc_t cbfunc,
-					void *cbdata);
-
-#define expect_pmix_get(type, value)                        \
-	do {                                                \
-		will_return(__wrap_PMIx_Get, type);         \
-		will_return(__wrap_PMIx_Get, value);        \
-		will_return(__wrap_PMIx_Get, PMIX_SUCCESS); \
-	} while (0)
-
-#define expect_pmix_lookup(type, value)                        \
-	do {                                                   \
-		will_return(__wrap_PMIx_Lookup, type);         \
-		will_return(__wrap_PMIx_Lookup, value);        \
-		will_return(__wrap_PMIx_Lookup, PMIX_SUCCESS); \
-	} while (0)
+# include <cmocka.h>
 
 # ifdef __cplusplus
 }
-# endif /* __cplusplus */
-#endif /* __UTEST_CMOCKA_H__ */
+#endif /* __cplusplus */
+#endif /* __WRAP_CMOCKA_H__ */
