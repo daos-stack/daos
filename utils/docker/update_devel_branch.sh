ed8c1246d6b2680961bd3d87807e28a134257ff3
John.E.Malmberg <john.e.malmberg@intel.com>
2017-11-09 17:38:41 +0000

    CORCI-159 Fix/Simplify update_devel_branch.sh
    The sanity checks that update_devel_branch was doing to try to validate
    the commit hashes in the build.config were not working, producing both
    false positives and negatives.
    
    The fix is to assume that the build.config is correct and then make sure
    that the component branches are properly set.
    
    Change-Id: I65bc28fe160b60f7f28eee8e8b055decf0ad7338
    Signed-off-by: John.E.Malmberg <john.e.malmberg@intel.com>
    Reviewed-on: https://review.whamcloud.com/30017
    Tested-by: Jenkins
    Reviewed-by: Ashley Pittman <ashley.m.pittman@intel.com>
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- utils/docker/update_devel_branch.sh
+++ utils/docker/update_devel_branch.sh
@@ -3,51 +3,12 @@
 # This script reads in the build.config file and will validate and optionally
 # update the git HPDD git repository <component>_<devel> branch for each
 # component to match the commit hashes in build.config file.
-
-# <component>-master are built with the master branch of all components.
-#
-# <name>-<name>_<devel> is the master branch of <name> that is built with
-# the component selected by the commit has from the build.config file.
-# The <name>-<name>_<devel> jobs correspond to the "update_scratch" jobs
-#
-# In general the commit hash will be looked for first in the
-# <component>-<name>_<devel> job, and if it is not found there it will
-# be looked for in the <component>-<component>_<devel> job.  If there is no
-# <component>-<component>_<devel> job then <component>-master is used.
-#
-# <component>-<name>_<devel> is the <name>_<devel> branch of <component>
-# built with the <name>_<devel> branch for the components.  This procedure
-# is to make sure that branch is properly set for future builds of those
-# components when the build.config is changed.
-# The <component>-<name>_<devel> jobs are similar to a specific build of
-# an update_scratch job that is referenced in a build.config file.
-#
-# <component>-<component>_<devel> is the same as <name>-<name>_<devel>.
-#
-# A successful build for each <component> job with the commit hashes
-# is needed to be found for the branch to be updated.
-#
-# For example, iof-iof_devel is a build of the master branch of iof using
-# the components specified by its build.config.
-#
-# Those components normally will come from cart-iof_devel and fuse-iof-devel
-# which are built from the iof_devel branch of the cart and nvml repositories.
-#
-# In the case that the build.config has been recently updated to specify a
-# newer build of cart, then the cart-cart_devel job would be used as a source.
-#
-# In the case that the build.config has been recently updated to specify a
-# newer build of fuse, then since fuse does not have any components,
-# fuse-master would be used for the components.
 #
 # This procedure should then be used after a build.config update is validated
 # to update the branches.
 #
 # The repositories for the components will be updated in their own
 # subdirectory at the appropriate branch, which will usually be master.
-#
-# This script uses the fetch_dependent_artifacts.sh script to obtain and
-# validate the git commits in the file.
 #
 # ${1} Needs to be "update" for the git branch to be updated in gerrit
 # and the user will need push permission to actually do an update.
@@ -62,8 +23,8 @@
 #                 Default is JOB_NAME portion before the first "-" if JOB_NAME
 #                 exists.  If JOB_NAME does not exist, TARGET needs to exist.
 #
-# BRANCH_SUFFIX   Specifies the suffix to used for the branch and component
-#                 Jenkins jobs.  Default is "devel"
+# BRANCH_SUFFIX   Specifies the suffix to used for the branch.
+#                 Default is "devel"
 #
 # SCONS_LOCAL     Specifies where the scons_local directory to use is.
 #                 Default is to the use scons_local from the parent directory
@@ -123,45 +84,6 @@ popd
 
 . ${component_script}
 
-# Still need some special logic to look up the Jenkins jobs.
-# cppr has to inherit cart from iof, so iof_devel must include cart_devel.
-# daos can use a different cart than iof, so uses daos_devel and cart_devel.
-
-cart_depend_jobs=""
-cart_d_depend_jobs=""
-for rq in ${cart_requires_full}; do
-  cart_depend_jobs="${cart_depend_jobs} ${rq}-release"
-  cart_depend_jobs="${cart_depend_jobs} ${rq}-master"
-  cart_depend_jobs="${cart_depend_jobs} ${rq}-cart_${bsx}"
-  cart_d_depend_jobs="${cart_d_depend_jobs} ${rq}-daos_${bsx}"
-done
-
-iof_depend_jobs=""
-for rq in ${iof_requires}; do
-  iof_depend_jobs="${iof_depend_jobs} ${rq}-release"
-  iof_depend_jobs="${iof_depend_jobs} ${rq}-master"
-  iof_depend_jobs="${iof_depend_jobs} ${rq}-iof_${bsx}"
-done
-iof_depend_jobs="cart-cart_${bsx} ${iof_depend_jobs} ${cart_depend_jobs}"
-
-cppr_depend_jobs=""
-for rq in ${cppr_requires}; do
-  cppr_depend_jobs="${cppr_depend_jobs} ${rq}-cppr_${bsx}"
-done
-cppr_depend_jobs="iof-iof_${bsx} ${cppr_depend_jobs} ${iof_depend_jobs}"
-
-daos_depend_jobs=""
-for rq in ${daos_requires}; do
-  daos_depend_jobs="${daos_depend_jobs} ${rq}-release"
-  daos_depend_jobs="${daos_depend_jobs} ${rq}-master"
-  daos_depend_jobs="${daos_depend_jobs} ${rq}-daos_${bsx}"
-done
-daos_depend_jobs="cart-cart_${bsx} ${daos_depend_jobs} ${cart_d_depend_jobs}"
-
-wanted_depend_jobs_name="${TARGET}_depend_jobs"
-
-: ${DEPEND_JOBS:="${!wanted_depend_jobs_name}"}
-
 # Keep public_repo_xxx arrays in same order and count.
 public_repo_name=(\
   'argobots' \
@@ -202,21 +124,6 @@ for i in "${!comp_repo_name[@]}"; do
   fi
 done
 
-case ${TARGET} in
-  daos)
-    for depend in ${cart_requires_full}; do
-      declare ${depend}_branch=daos_${bsx}
-    done
-    declare nvml_branch=daos_${bsx}
-    ;;
-  *)
-    wanted_rqs_name="${TARGET}_requires_full"
-    for depend in ${!wanted_rqs_name}; do
-      declare ${depend}_branch=${TARGET}_${bsx}
-    done
-    ;;
-esac
-
 pushd ${TARGET}
   git config advise.detachedHead false
   git clean -dfx
@@ -227,81 +134,97 @@ pushd ${TARGET}
   git clean -df
 popd
 
-. ${my_path_abs}/utils/docker/fetch_dependent_artifacts.sh
-
-
 print_err() { printf "%s\n" "$*" 1>&2; }
 
-if [ -e commit_not_found ]; then
-  print_err "A build for one or more required commit hashes was not found."
+def_build_config=`find . -name 'build.config' -print -quit`
+: ${BUILD_CONFIG:="${def_build_config}"}
+
+set + u
+if [ -z "${BUILD_CONFIG}" ]; then
+  print_err "A build.config file was not found."
   exit 1
 fi
+
+# Read and parse the git commit hashes from build.config
+gr1='grep -v depends='
+gr2='grep -v component='
+depend_hashes=`grep -i "\s*=\s*" ${BUILD_CONFIG} | ${gr1} | ${gr2}`
+mapfile -t depend_lines <<< "${depend_hashes}"
+depend_names=""
+for line in "${depend_lines[@]}"; do
+  if [[ "${line}" != \#* ]]; then
+    depend_name=${line%=*}
+    depend_name=${depend_name% *}
+    depend_name=${depend_name,,}
+    depend_hash=${line#*=}
+    depend_hash=${depend_hash#* }
+    depend_hash=${depend_hash,,}
+    declare ${depend_name}_git_hash=${depend_hash}
+    depend_names="${depend_names} ${depend_name}"
+  fi
+done
+
+
+# TODO, Need to update the docker/SConstruct_info to extract branch info.
+# Not currently needed.
+my_branch=""
 
 # Loop through the repos in use.
 for i in "${!std_repo_name[@]}"; do
   repo="${std_repo_name[i]}"
-  if [ -e "${DEPEND_INFO}/${repo}_git_commit" ]; then
-    my_branch=""
-    if [ -e "${DEPEND_INFO}/${repo}_git_branch" ]; then
-      my_branch=`cat ${DEPEND_INFO}/${repo}_git_branch`
+  if [ ! -d ${repo} ]; then
+    git clone ${repo_base}/${std_repo_dir[i]} ${std_repo_name[i]}
+  fi
+  set +u
+  my_wanted_commit="${repo}_git_hash"
+  my_commit="${!my_wanted_commit}"
+  if [ -z "${my_commit}" ]; then
+    continue
+  fi
+
+  branch_name="${TARGET}_${bsx}"
+
+  pushd ${repo}
+    git config advise.detachedHead false
+    git clean -dfx
+    git reset --hard
+    git checkout master
+    if [ -n "${my_branch}" ]; then
+      git branch -D ${my_branch} || true
+      git fetch origin ${my_branch}
+      git checkout -b ${my_branch} origin/${my_branch}
+    else
+      git fetch origin master
+      git reset --hard FETCH_HEAD
+      git clean -df
     fi
-    if [ ! -d ${repo} ]; then
-      git clone ${repo_base}/${std_repo_dir[i]} ${std_repo_name[i]}
-      if [ -n "${my_branch}" ]; then
-        pushd ${repo}
-          git checkout ${my_branch}
-        popd
+    git checkout -f "${my_commit}"
+    git clean -dfx
+    set +e
+    branch_commit=`git rev-parse origin/${branch_name}`
+    rc=${?}
+    set -e
+    if [ ${rc} -eq 0 ]; then
+      if [ "${my_commit}" != "${branch_commit}" ]; then
+        git branch -d ${branch_name} || true
+        git branch -f ${branch_name} ${my_commit}
+        if [ -n "${update}" ]; then
+          git push -f -u origin ${branch_name}
+        else
+          echo "would git push -u origin ${branch_name} to ${repo}"
+          git branch -d ${branch_name} || true
+        fi
+      fi
+    else
+      git branch -f ${branch_name} ${my_commit}
+      if [ -n "${update}" ]; then
+        git push -f -u origin ${branch_name}
+      else
+        echo "would git push -u origin ${branch_name}"
+        git branch -d ${branch_name} || true
       fi
     fi
-    my_commit="`cat ${DEPEND_INFO}/${repo}_git_commit`"
-    set +u
-    my_wanted_branch="${repo}_branch"
-    branch_name="${!my_wanted_branch}"
-    if [ -n "${branch_name}" ]; then
-      set -u
-      pushd ${repo}
-        git config advise.detachedHead false
-        git clean -dfx
-        git reset --hard
-        git checkout master
-        if [ -n "${my_branch}" ]; then
-          git branch -D ${my_branch} || true
-          git fetch origin ${my_branch}
-          git checkout -b ${my_branch} origin/${my_branch}
-        else
-          git fetch origin master
-          git reset --hard FETCH_HEAD
-          git clean -df
-        fi
-        git checkout -f "${my_commit}"
-        git clean -dfx
-        set +e
-        branch_commit=`git rev-parse origin/${branch_name}`
-        rc=${?}
-        set -e
-        if [ ${rc} -eq 0 ]; then
-          if [ "${my_commit}" != "${branch_commit}" ]; then
-            git branch -d ${branch_name} || true
-            git branch -f ${branch_name} ${my_commit}
-            if [ -n "${update}" ]; then
-              git push -f -u origin ${branch_name}
-            else
-              echo "would git push -u origin ${branch_name} to ${repo}"
-              git branch -d ${branch_name} || true
-            fi
-          fi
-        else
-          git branch -f ${branch_name} ${my_commit}
-          if [ -n "${update}" ]; then
-            git push -f -u origin ${branch_name}
-          else
-            echo "would git push -u origin ${branch_name}"
-            git branch -d ${branch_name} || true
-          fi
-        fi
-        set -e
-      popd
-    fi
-    set -u
-  fi
+    set -e
+  popd
+  set -u
 done
