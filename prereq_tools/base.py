7f511b29376e452f615ee67b44e71e217aa00c1b
Jeff Olivier <jeffrey.v.olivier@intel.com>
2017-11-10 11:39:36 -0700

    CORCI-95 prereq: Merge headers_only implementation with needed_libs
    If headers_only is specified, that means the same thing as an empty
    list of libraries.
    
    Change-Id: I36faa4f4dfa79af86c2afcbf1c5e0430b978c96a
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>
    Reviewed-on: https://review.whamcloud.com/30042
    Tested-by: Jenkins
    Reviewed-by: John Malmberg <john.e.malmberg@intel.com>

--- prereq_tools/base.py
+++ prereq_tools/base.py
@@ -831,7 +831,8 @@ class PreReqComponent(object):
 
         Keyword arguments:
             headers_only -- if set to True, skip library configuration
-            [comp]_libs --  Override the default libs for a package
+            [comp]_libs --  Override the default libs for a package.  Ignored
+                            if headers_only is set
         """
         changes = False
         headers_only = kw.get('headers_only', False)
@@ -839,12 +840,15 @@ class PreReqComponent(object):
             if comp not in self.__defined:
                 raise MissingDefinition(comp)
             comp_def = self.__defined[comp]
-            needed_libs = kw.get('%s_libs' % comp, comp_def.libs)
+            if headers_only:
+                needed_libs = None
+            else:
+                needed_libs = kw.get('%s_libs' % comp, comp_def.libs)
             if comp in self.__required:
                 if GetOption('help'):
                     continue
                 # checkout and build done previously
-                comp_def.set_environment(env, headers_only, needed_libs)
+                comp_def.set_environment(env, needed_libs)
                 if GetOption('clean'):
                     continue
                 if self.__required[comp]:
@@ -852,7 +856,7 @@ class PreReqComponent(object):
                 continue
             self.__required[comp] = False
             comp_def.configure()
-            if comp_def.build(env, headers_only, needed_libs):
+            if comp_def.build(env, needed_libs):
                 self.__required[comp] = False
                 changes = True
 
@@ -1246,7 +1250,7 @@ class _Component(object):
             except:
                 pass
 
-    def set_environment(self, env, headers_only, needed_libs):
+    def set_environment(self, env, needed_libs):
         """Modify the specified construction environment to build with
            the external component"""
         for path in self.include_path:
@@ -1263,7 +1267,7 @@ class _Component(object):
         for define in self.defines:
             env.AppendUnique(CPPDEFINES=[define])
 
-        if headers_only:
+        if needed_libs is None:
             return
 
         for path in self.lib_path:
@@ -1286,14 +1290,14 @@ class _Component(object):
             else:
                 raise MissingTargets(self.name, self.package)
 
-    def check_user_options(self, env, headers_only, needed_libs):
+    def check_user_options(self, env, needed_libs):
         """check help and clean options"""
         if GetOption('help'):
             if self.requires:
                 self.prereqs.require(env, *self.requires)
             return True
 
-        self.set_environment(env, headers_only, needed_libs)
+        self.set_environment(env, needed_libs)
         if GetOption('clean'):
             return True
 
@@ -1335,22 +1339,21 @@ class _Component(object):
             with open(self.crc_file, 'w') as crcfile:
                 crcfile.write(new_crc)
 
-    def build(self, env, headers_only, needed_libs):
+    def build(self, env, needed_libs):
         """Build the component, if necessary
 
         :param env: Scons Environment for building.
         :type env: environment
-        :param headers_only: If set to True, skip library configuration.
-        :type headers_only: bool
+        :param needed_libs: Only configure libs in list
         :return: True if the component needed building.
         """
         # Ensure requirements are met
         changes = False
         envcopy = self.prereqs.system_env.Clone()
 
-        if self.check_user_options(env, headers_only, needed_libs):
+        if self.check_user_options(env, needed_libs):
             return True
-        self.set_environment(envcopy, False, needed_libs)
+        self.set_environment(envcopy, self.libs)
         if self.prebuilt_path:
             self.check_installed_package(envcopy)
             return False
@@ -1365,8 +1368,8 @@ class _Component(object):
 
             if self.requires:
                 changes = self.prereqs.require(envcopy, *self.requires,
-                                               headers_only=True)
-                self.set_environment(envcopy, False, needed_libs)
+                                               needed_libs=None)
+                self.set_environment(envcopy, self.libs)
 
             if self.has_missing_system_deps(self.prereqs.system_env):
                 raise MissingSystemLibs(self.name)
