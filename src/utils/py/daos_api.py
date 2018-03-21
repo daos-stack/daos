30c54141ae5e469ff9537fe435f0299455e73250
Scott Kirvan <scott.kirvan@intel.com>
2018-03-21 18:45:44 +0000

    DAOS-717 testing: add a wrapper class for server mgmt
    Change-Id: I0f55a6f095f8e19211f02eeefd2bc606b235846c
    Signed-off-by: Scott Kirvan <scott.kirvan@intel.com>
    Reviewed-on: https://review.whamcloud.com/31709
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/utils/py/daos_api.py
+++ src/utils/py/daos_api.py
@@ -26,9 +26,8 @@ import traceback
 
 class RankList(ctypes.Structure):
     """ For those DAOS calls that take a rank list """
-    _fields_ = [("num", ctypes.c_uint),
-                ("num_out", ctypes.c_uint),
-                ("rl_ranks", ctypes.POINTER(ctypes.c_uint))]
+    _fields_ = [("rl_ranks", ctypes.POINTER(ctypes.c_uint)),
+                ("rl_nr", ctypes.c_uint)]
 
 class DaosPool(object):
     """ A python object representing a DAOS pool."""
@@ -40,21 +39,40 @@ class DaosPool(object):
 
     def create(self, mode, uid, gid, size, group):
         """ send a pool creation request to the daos server group """
-        mode = ctypes.c_uint(mode)
-        uid = ctypes.c_uint(uid)
-        gid = ctypes.c_uint(gid)
-        size = ctypes.c_longlong(size)
-        group = ctypes.create_string_buffer(group)
-        uuid = ctypes.create_string_buffer(17)
-        rl_ranks = ctypes.POINTER(ctypes.c_uint)(ctypes.c_uint(0))
+        c_mode = ctypes.c_uint(mode)
+        c_uid = ctypes.c_uint(uid)
+        c_gid = ctypes.c_uint(gid)
+        c_size = ctypes.c_longlong(size)
+        c_group = ctypes.create_string_buffer(group)
+        c_uuid = (ctypes.c_int*3)()
+        rank = ctypes.c_uint(1)
+        rl_ranks = ctypes.POINTER(ctypes.c_uint)(rank)
         whatever = ctypes.create_string_buffer(b"rubbish")
 
-        svc = RankList(1, 0, rl_ranks)
+        svc = RankList(rl_ranks, 1)
 
         func = self.context.get_function('create-pool')
-        func(mode, uid, gid, group, None, whatever, size,
-             ctypes.byref(svc), uuid, None)
-        self.uuid = uuid
+        func(c_mode, c_uid, c_gid, c_group, None, whatever, c_size,
+             ctypes.byref(svc), c_uuid, None)
+        self.uuid = c_uuid
+
+class DaosServer(object):
+    """Represents a DAOS Server"""
+
+    def __init__(self, context, group, rank):
+        """ setup the python pool object, not the real pool. """
+        self.context = context
+        self.group_name = group
+        self.rank = rank
+
+    def kill(self, force):
+        """ send a pool creation request to the daos server group """
+        c_group = ctypes.create_string_buffer(self.group_name)
+        c_force = ctypes.c_int(force)
+        c_rank = ctypes.c_uint(self.rank)
+
+        func = self.context.get_function('kill-server')
+        func(c_group, c_rank, c_force, None)
 
 class DaosContext(object):
     """Provides environment and other info for a DAOS client."""
@@ -68,10 +86,10 @@ class DaosContext(object):
         # of LAZY symbol loading and the load order used below.
         ctypes.CDLL(path+"libdaos_common.so", mode=0x00101)
         self.libdaos = ctypes.CDLL(path+"libdaos.so.0.0.2", mode=0x00101)
-        ctypes.CDLL(path+"libdaos_tier.so", mode=0x00101)
         self.libdaos.daos_init()
         self.ftable = {
              'create-pool':self.libdaos.daos_pool_create,
+             'kill-server':self.libdaos.daos_mgmt_svc_rip,
         }
 
     def __del__(self):
@@ -83,15 +101,21 @@ class DaosContext(object):
         return self.ftable[function]
 
 if __name__ == '__main__':
-    # test the API functions
+    # test the API functions, this is just unit testing
+    # for this file.
 
     try:
-        CONTEXT = DaosContext('/home/skirvan/daos_py/install/lib/')
+        CONTEXT = DaosContext('path to library goes here')
         print("initialized!!!\n")
 
         POOL = DaosPool(CONTEXT)
-        POOL.create(511, 0, 0, 1024*1024*1024, b'daos_server')
-        print ("created\n")
+        POOL.create(448, 11374638, 11374638, 1024*1024*1024, b'daos_server')
+        print ("created!\n")
+
+        SERVICE = DaosServer(CONTEXT, b'daos_server', 0)
+        SERVICE.kill(1)
+        print ("server killed!\n")
+
     except Exception as EXCEP:
         print ("Something horrible happened\n")
         print (traceback.format_exc())
