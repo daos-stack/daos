9e6fe3513d54b5dc777324231677fcc862404d4c
Christopher A Holguin <christopher.a.holguin@intel.com>
2018-04-30 20:07:11 +0000

    DAOS-747: vos_tests fix bug in daos_post_build.sh
    daos_post_build.sh shouldn't check for the skipped
    message if DAOS_RUN_UNIT_TESTS is set to 0
    
    Change-Id: I813b7ae2ab23979797d247e8b6172698e6406131
    Signed-off-by: Christopher A Holguin <christopher.a.holguin@intel.com>
    Reviewed-on: https://review.whamcloud.com/32211
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>
    Tested-by: Jenkins

--- utils/docker/daos_post_build.sh
+++ utils/docker/daos_post_build.sh
@@ -9,5 +9,5 @@ if [ "$DAOS_RUN_UNIT_TESTS" -eq "1" ]
 then
         grep "SUCCESS! NO TEST FAILURES" ${test_results}
 else
-        grep "Daos unit tests skipped." ${test_results}
+        echo "Daos unit tests skipped."
 fi
