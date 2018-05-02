5d9610c866f64d06e7c10fbde4007e0e4bf57e57
Christopher Holguin <christopher.a.holguin@intel.com>
2018-05-02 20:08:19 +0000

    DAOS-747: unit_test check for /mnt/daos
    make the unit test script more robust such
    that it checks for existance of /mnt/daos
    before running the tests
    
    improve the help text in utils/run_test.sh
    
    Change-Id: Id9d9bdb41518a3c19aef6f270e27ec61acd030ad
    Signed-off-by: Christopher Holguin <christopher.a.holguin@intel.com>
    Reviewed-on: https://review.whamcloud.com/32256
    Reviewed-by: John Malmberg <john.e.malmberg@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- utils/run_test.sh
+++ utils/run_test.sh
@@ -15,4 +15,13 @@
 #
 #  Note: $DIST_TARGET/daos is where all daos artifacts are
 #    located in the docker container when a build completes
-time flock /mnt/daos/jenkins.lock ${DIST_TARGET}/daos/bin/vos_tests -A 500
+#  Note: if further tests are added, be sure to modify
+#    scons_local/utils/docker/daos_post_build.sh
+#    to scan for the updated success or failure text
+
+#check for existence of /mnt/daos first:
+if [ -d "/mnt/daos" ]; then
+    time flock /mnt/daos/jenkins.lock ${DIST_TARGET}/daos/bin/vos_tests -A 500
+else
+    echo "/mnt/daos isn't present for unit tests"
+fi
