007a3f4fb0d0a8be81e69b37f6e191b41d9a3ce1
Stephen Willson <stephen.d.willson@intel.com>
2018-05-23 16:19:44 -0700

    DAOS-795 update avocado test to check pool query information
    Modify InfoTests to use DAOS Python API rather than daosctl.
    
    Change-Id: I98c48647699da755d015e82511ac25b45c9f9b61
    Signed-off-by: Stephen Willson <stephen.d.willson@intel.com>
    Reviewed-on: https://review.whamcloud.com/32534
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Scott Kirvan <scott.kirvan@intel.com>

--- src/tests/ftest/pool/InfoTests.py
+++ src/tests/ftest/pool/InfoTests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-'''
+"""
   (C) Copyright 2018 Intel Corporation.
 
   Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,36 +20,54 @@
   provided in Contract No. B609815.
   Any reproduction of computer software, computer software documentation, or
   portions thereof marked with this legend must also reproduce the markings.
-'''
-
+"""
 import os
 import time
 import traceback
 import sys
+import json
+import logging
+
+sys.path.append('./util')
+import ServerUtils
+import CheckForPool
+import WriteHostFile
+
+sys.path.append('../../utils/py')
+from daos_api import *
 
 from avocado       import Test
 from avocado       import main
 from avocado.utils import process
 
-sys.path.append('./util')
-import ServerUtils
-import CheckForPool
+progress_log = logging.getLogger("progress")
+
 
 class InfoTests(Test):
     """
     Tests DAOS pool query.
-
-    Note tags not set at present because the test isn't in a working state
-
-    :avocado: tags=rubbish
     """
-
-    # super wasteful since its doing this for every variation
     def setUp(self):
-           pass
+        with open('../../../.build_vars.json') as f:
+            data = json.load(f)
+        self.basepath = os.path.normpath(data['PREFIX'] + "/../")
+        self.tmp = data['PREFIX'] + '/tmp'
+        self.server_group = self.params.get("server_group", '/server/',
+                                            'daos_server')
+
+        context = DaosContext(data['PREFIX'] + '/lib/')
+        print("initialized!!!\n")
+
+        self.pool = DaosPool(context)
+        hostlist = self.params.get("test_machines1", '/run/hosts/')
+        hostfile = WriteHostFile.WriteHostFile(hostlist, self.tmp)
+        ServerUtils.runServer(hostfile, self.server_group, self.basepath)
 
     def tearDown(self):
-           pass
+        # shut 'er down
+        self.pool.destroy(1)
+        ServerUtils.stopServer()
+        os.remove(self.hostfile)
 
     def test_simple_query(self):
         """
@@ -57,52 +75,60 @@ class InfoTests(Test):
 
         Note tags not set at present because the test isn't in a working state
 
-        :avocado: tags=rubbish
+        :avocado: tags=pool,poolquery,infotest
         """
         # there is a presumption that this test lives in a specific spot
         # in the repo
-        basepath = os.path.normpath(os.getcwd() + "../../../../")
-        server_group = self.params.get("server_group",'/server/','daos_server')
-        hostfile = basepath + self.params.get("hostfile",
-                                              '/run/testparams/one_host/')
-
-        ServerUtils.runServer(hostfile, server_group, basepath)
-
-        # not sure I need to do this but ... give it time to start
-        time.sleep(1)
-
         setid = self.params.get("setname",
                                 '/run/testparams/setnames/validsetname/')
 
-        mode = self.params.get("mode",'/run/testparams/modes/*',0731)
-        size = self.params.get("size",'/run/testparams/sizes/*',0)
-        connectperm = self.params.get("perms",'/run/testparams/connectperms/*',
-                                      '')
+        # create pool
+        mode = self.params.get("mode", '/run/testparams/modes/*', 0731)
+        uid = os.geteuid()
+        gid = os.getegid()
+        size = self.params.get("size", '/run/testparams/sizes/*', 0)
+        tgt_list = None
+        group = self.server_group
 
-        try:
-               # use the uid/gid of the user running the test, these should
-               # be perfectly valid
-               uid = os.geteuid()
-               gid = os.getegid()
+        self.pool.create(mode, uid, gid, size, group, tgt_list)
+        progress_log.info("created pool")
 
-               # TODO make these params in the yaml
-               daosctl = basepath + '/install/bin/daosctl'
+        # connect to the pool
+        flags = self.params.get("perms", '/run/testparams/connectperms/*', '')
+        connect_flags = 1 << flags
+        self.pool.connect(connect_flags)
+        progress_log.info("connected to pool")
 
-               create_connect_query_cmd = (
-                      '{0} test-connect-pool '
-                      '-m {1} -u {2} -g {3} -s {4} -z {5} {6}'.
-                   format(daosctl, mode, uid, gid, setid, 1, size, connectperm))
+        # query the pool
+        pool_info = self.pool.pool_query()
+        progress_log.info("queried pool info")
 
-               process.system(create_connect_query_cmd)
+        # validate returned pool info, uuid
+        uuid_str = ""
+        for x in pool_info.pi_uuid:
+            uuid_str = uuid_str + ("{0} ".format(x))
+        progress_log.info("pool uuid: {0}".format(uuid_str))
 
-        except Exception as e:
-               print e
-               print traceback.format_exc()
-               self.fail("Expecting to pass but test has failed.\n")
+        # number of targets
+        progress_log.info("number of targets in pool: {0}"
+                          .format(pool_info.pi_ntargets))
+        if pool_info.pi_ntargets != len(self.hostlist):
+            self.fail("found number of targets in pool did not match "
+                      "expected number, 1. num targets: {0}"
+                      .format(pool_info.pi_ntargets))
 
-        # no matter what happens shutdown the server
-        finally:
-               ServerUtils.stopServer()
+        # number of disabled targets
+        progress_log.info("number of disabled targets in pool: {0}"
+                          .format(pool_info.pi_ndisabled))
+        if pool_info.pi_ndisabled > 0:
+            self.fail("found disabled targets, none expected to be disabled")
+
+        # mode
+        progress_log.info("pool mode: {0}".format(pool_info.pi_mode))
+        if pool_info.pi_mode != mode:
+            self.fail("found different mode than expected. expected {0}, "
+                      "found {1}.".format(mode, pool_info.pi_mode))
+
 
 if __name__ == "__main__":
     main()
