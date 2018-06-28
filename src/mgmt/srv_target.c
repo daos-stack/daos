72276b4aec0de2fa02dabf69be5b2882e19257bd
Brian J. Murrell <brian.murrell@intel.com>
2018-06-28 08:11:00 -0400

    DAOS-1332 Display errno when fallocate() fails
    When fallocate() fails in tgt_vos_create() it's useful to know why.
    
    Display the string representation of the errno value in the error
    message returned when fallocate() fails.
    
    Also, get rc from daos_errno2der(errno) rather than using the the
    return value from fallocate() which will always be -1 in this case.
    
    Change-Id: I85bdccc1af391f96bf10985ce5a529ab39ae9204
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Reviewed-on: https://review.hpdd.intel.com/33140
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Tested-by: Jenkins
    Reviewed-by: Alexander Oganezov <alexander.a.oganezov@intel.com>
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/mgmt/srv_target.c
+++ src/mgmt/srv_target.c
@@ -336,10 +336,10 @@ tgt_vos_create(uuid_t uuid, daos_size_t tgt_size)
 		 */
 		rc = fallocate(fd, 0, 0, size);
 		if (rc) {
-			D_ERROR(DF_UUID": failed to allocate vos file %s with "
-				"size: "DF_U64", rc: %d.\n",
-				DP_UUID(uuid), path, size, rc);
 			rc = daos_errno2der(errno);
+			D_ERROR(DF_UUID": failed to allocate vos file %s with "
+				"size: "DF_U64", rc: %d, %s.\n",
+				DP_UUID(uuid), path, size, rc, strerror(errno));
 			break;
 		}
 
