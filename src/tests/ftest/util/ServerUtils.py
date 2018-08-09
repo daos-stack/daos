40a679e5c3ec93e1e7acbc9ee410f8b2f47b14ae
Brian J. Murrell <brian.murrell@intel.com>
2018-08-09 14:48:09 -0400

    Bump RPC up to DEBUG
    Change-Id: I110f824841086a4647c54a22c8cbfdc3e24c7bdd
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/tests/ftest/util/ServerUtils.py
+++ src/tests/ftest/util/ServerUtils.py
@@ -72,7 +72,7 @@ def runServer(hostfile, setname, basepath):
         initial_cmd = "/bin/sh"
         server_cmd = orterun_bin + " --np {0} ".format(server_count)
         server_cmd += "--hostfile {0} --enable-recovery ".format(hostfile)
-        server_cmd += "-x D_LOG_MASK=DEBUG,RPC=ERR,MEM=ERR -x D_LOG_FILE="
+        server_cmd += "-x D_LOG_MASK=DEBUG,MEM=ERR -x D_LOG_FILE="
         server_cmd += basepath + "/install/tmp/daos.log "
         # uncomment if you prefer separate logs per server (and comment above)
         #server_cmd += "/tmp/daos.log "
