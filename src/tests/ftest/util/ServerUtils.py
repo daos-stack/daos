6dd48ebbe02f1f4339e2f01fc30d046c56d780a7
Brian J. Murrell <brian.murrell@intel.com>
2018-08-14 14:41:13 -0400

    Install openpa and libfabric RPMs from Jenkins job
    Change-Id: I5820210f625e1384e80e8b13397918edc58ad49e
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/tests/ftest/util/ServerUtils.py
+++ src/tests/ftest/util/ServerUtils.py
@@ -57,15 +57,12 @@ def runServer(hostfile, setname, basepath):
         ld_lib_path = os.path.join(build_vars["PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["PREFIX"], "lib/daos_srv") + os.pathsep + \
             os.path.join(build_vars["OMPI_PREFIX"], "lib") + os.pathsep + \
-            os.path.join(build_vars["MERCURY_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["HWLOC_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["ARGOBOTS_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["SPDK_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["PMDK_PREFIX"], "lib") + os.pathsep + \
-            os.path.join(build_vars["OPENPA_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["CART_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["ISAL_PREFIX"], "lib") + os.pathsep + \
-            os.path.join(build_vars["OFI_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["FUSE_PREFIX"], "lib") + os.pathsep + \
             os.path.join(build_vars["PMIX_PREFIX"], "lib")
 
