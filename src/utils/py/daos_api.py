7807898751b38afd186d3bda29c8a42ed765355c
Saurabh Tandan <saurabh.tandan@intel.com>
2018-08-28 11:52:38 -0700

    DAOS-1281 tests: moving conversion.py and corecting daos_api.py
    As part of this ticket patch, the location of
    conversion.py has been changed.
    Moved from src/tests/ftest/utils/conversion.py
    to src/utils.py/conversion.py
    
    Also made changes to daos_api.py to accomodate
    the above change.
    
    Change-Id: Ie961316780b26d28fad7b043b3c3b6b35d3bdd67
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>
    Reviewed-on: https://review.hpdd.intel.com/33098
    Reviewed-by: Stephen Willson <stephen.d.willson@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Tested-by: Jenkins
    Reviewed-by: Jean-Baptiste Riaux <riaux.jb@intel.com>
    Reviewed-by: Samir Raval <samir.raval@intel.com>
    Reviewed-by: Scott Kirvan <scott.kirvan@intel.com>

--- src/utils/py/daos_api.py
+++ src/utils/py/daos_api.py
@@ -30,33 +30,7 @@ import json
 import os
 
 from daos_cref import *
-
-# this needs to be removed when conversions module
-# is moved to a better spot
-def c_uuid(p_uuid, c_uuid):
-    """ utility function to create a UUID in C format from a python UUID """
-    hexstr = p_uuid.hex
-    for i in range(0, 31, 2):
-        c_uuid[i/2] = int(hexstr[i:i+2], 16)
-
-# ditto, remove when conversions is moved
-def c_uuid_to_str(uuid):
-    """ utility function to convert a C uuid into a standard string format """
-    uuid_str = '{:02X}{:02X}{:02X}{:02X}-{:02X}{:02X}-{:02X}{:02X}-{:02X}'\
-               '{:02X}-{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}'.format(
-                   uuid[0], uuid[1], uuid[2], uuid[3], uuid[4], uuid[5],
-                   uuid[6], uuid[7], uuid[8], uuid[9], uuid[10], uuid[11],
-                   uuid[12], uuid[13], uuid[14], uuid[15])
-    return uuid_str
-
-# ditto, remove when conversions is moved
-def str_to_c_uuid(uuidstr):
-    """ utility function to convert string format uuid to a C uuid """
-    uuidstr2 = '{' + uuidstr + '}'
-    puuid = uuid.UUID(uuidstr2)
-    cuuid = (ctypes.c_ubyte * 16)()
-    c_uuid(puuid, cuuid)
-    return cuuid
+from conversion import *
 
 class DaosPool(object):
     """ A python object representing a DAOS pool."""
