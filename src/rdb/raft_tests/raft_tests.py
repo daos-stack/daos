6e6df9a3bf44446ee5189ed8867a8cc345160858
Brian J. Murrell <brian.murrell@intel.com>
2018-08-28 12:28:50 -0400

    DAOS-1257 Build raft tests at build time
    Update to the raft commit that builds the library and
    test tool at the same time so that at test time it
    doesn't need to be built.
    
    Change-Id: I232f06e6227163d236a19c03fd6372b5d945d9c7
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/rdb/raft_tests/raft_tests.py
+++ src/rdb/raft_tests/raft_tests.py
@@ -42,15 +42,11 @@ def number_of_failures():
     """
     failures = 0
     successes = 0
-    if not os.path.isfile(os.path.join(DIR, "tests_main")):
-        try:
-            res = subprocess.check_output(['make', '-C', DIR, 'tests'])
-        except Exception as e:
-            print("Building Raft Tests failed due to\n{}".format(e))
-            return TEST_NOT_RUN
-    else:
-        os.chdir(DIR)
-        res = subprocess.check_output("./tests_main; gcov raft_server.c", shell=True)
+    try:
+        res = subprocess.check_output(['make', '-C', DIR, 'tests'])
+    except Exception as e:
+        print("Building/running Raft Tests failed due to\n{}".format(e))
+        return TEST_NOT_RUN
 
     for line in res.split('\n'):
         if line.startswith("not ok"):
