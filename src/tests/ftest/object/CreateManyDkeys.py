964d82a9235964f01ffbbaf8fa321ac3e2cee20f
Scott Kirvan <scott.kirvan@intel.com>
2018-11-06 21:17:43 +0000

    DAOS-1701 test: add container destroy to existing test
    Signed-off-by: Scott Kirvan <scott.kirvan@intel.com>
    
    I added container destroy to this existing test to create 1,000,000
    values in an object.  The test now fails (the destroy doesn't work)
    so I put in the skip decorator for now.  See DAOS-1721.
    
    Change-Id: I95ab33a0985a9b55ab239d40671f139787aff312
    Reviewed-on: https://review.hpdd.intel.com/33497
    Reviewed-by: Scott Kirvan <scott.kirvan@intel.com>
    Tested-by: Jenkins

--- src/tests/ftest/object/CreateManyDkeys.py
+++ src/tests/ftest/object/CreateManyDkeys.py
@@ -27,6 +27,7 @@ import sys
 import json
 import ctypes
 import traceback
+import avocado
 from avocado       import Test
 
 sys.path.append('./util')
@@ -38,9 +39,12 @@ import WriteHostFile
 
 from daos_api import DaosContext, DaosPool, DaosContainer, IORequest
 
-class CreateMillionDkeys(Test):
+class CreateManyDkeys(Test):
     """
-    Tests To Create the Millions Dkeys in same Object.
+    Test Class Description:
+        Tests that create large numbers of keys in objects/containers and then
+        destroy the containers and verify the space has been reclaimed.
+
     """
     def setUp(self):
         with open('../../../.build_vars.json') as json_f:
@@ -65,10 +69,6 @@ class CreateMillionDkeys(Test):
                          self.params.get("setname", '/run/pool/createset/*'),
                          None)
         self.pool.connect(1 << 1)
-        poh = self.pool.handle
-        self.container = DaosContainer(self.context)
-        self.container.create(poh)
-        self.container.open()
 
     def tearDown(self):
         if self.hostfile is not None:
@@ -77,47 +77,89 @@ class CreateMillionDkeys(Test):
         ServerUtils.stopServer()
         ServerUtils.killServer(self.hostlist)
 
-    def test_million_dkeys(self):
+    def write_a_bunch_of_values(self, how_many):
         """
-        Test millions dkeys in same object
-        :avocado: tags=dkeys,regression,vm,large
+        Write data to an object, each with a dkey and akey.  The how_many
+        parameter determines how many key:value pairs are written.
         """
+
+        self.container = DaosContainer(self.context)
+        self.container.create(self.pool.handle)
+        self.container.open()
+
         ioreq = IORequest(self.context, self.container, None)
         epoch = self.container.get_new_epoch()
         c_epoch = ctypes.c_uint64(epoch)
+
+        print("Started Writing the Dataset-----------\n")
+        inc = 50000
+        last_key = inc
+        for key in range(how_many):
+            c_dkey = ctypes.create_string_buffer("dkey {0}".format(key))
+            c_akey = ctypes.create_string_buffer("akey {0}".format(key))
+            c_value = ctypes.create_string_buffer(
+                "some data that gets stored with the key {0}".format(key))
+            c_size = ctypes.c_size_t(ctypes.sizeof(c_value))
+            ioreq.single_insert(c_dkey,
+                                c_akey,
+                                c_value,
+                                c_size,
+                                c_epoch)
+
+            if key > last_key:
+                print("written: {}".format(key))
+                sys.stdout.flush()
+                last_key = key + inc
+
+        self.container.commit_epoch(c_epoch)
+
+        print("Started Verification of the Dataset-----------\n")
+        last_key = inc
+        for key in range(how_many):
+            c_dkey = ctypes.create_string_buffer("dkey {0}".format(key))
+            c_akey = ctypes.create_string_buffer("akey {0}".format(key))
+            the_data = "some data that gets stored with the key {0}".format(key)
+            val = ioreq.single_fetch(c_dkey,
+                                     c_akey,
+                                     len(the_data)+1,
+                                     c_epoch)
+
+            if the_data != (repr(val.value)[1:-1]):
+                self.fail("ERROR: Data mismatch for dkey = {0}, akey={1}, "
+                          "Expected Value={2} and Received Value={3}\n"
+                          .format("dkey {0}".format(key),
+                                  "akey {0}".format(key),
+                                  the_data,
+                                  repr(val.value)[1:-1]))
+
+            if key > last_key:
+                print("veried: {}".format(key))
+                sys.stdout.flush()
+                last_key = key + inc
+
+        print("starting destroy")
+        self.container.close()
+        self.container.destroy()
+        print("destroy complete")
+
+    @avocado.fail_on(ValueError)
+    @avocado.skip("Skipping until DAOS-1721 is fixed.")
+    def test_many_dkeys(self):
+        """
+        Test ID: DAOS-1701
+        Test Description: Test many of dkeys in same object.
+        Use Cases: 1. large key counts
+                   2. space reclaimation after destroy
+        :avocado: tags=object,vm,many_dkeys
+
+        """
+
         no_of_dkeys = self.params.get("number_of_dkeys", '/run/dkeys/')
-        try:
-            print("Started Writing the Dataset-----------\n")
-            for key in range(no_of_dkeys):
-                c_dkey = ctypes.create_string_buffer("dkey {0}".format(key))
-                c_akey = ctypes.create_string_buffer("akey {0}".format(key))
-                c_value = ctypes.create_string_buffer("data {0}".format(key))
-                c_size = ctypes.c_size_t(len("data {0}".format(key))+1)
-                ioreq.single_insert(c_dkey,
-                                    c_akey,
-                                    c_value,
-                                    c_size,
-                                    epoch)
 
-            self.container.commit_epoch(c_epoch)
-            print("Started Verification of the Dataset-----------\n")
-            for key in range(no_of_dkeys):
-                c_dkey = ctypes.create_string_buffer("dkey {0}".format(key))
-                c_akey = ctypes.create_string_buffer("akey {0}".format(key))
-                val = ioreq.single_fetch(c_dkey,
-                                         c_akey,
-                                         len("data {0}".format(key))+1,
-                                         c_epoch)
-                original_data = "data {0}".format(key)
-                if original_data != (repr(val.value)[1:-1]):
-                    self.fail("ERROR: Data mismatch for dkey = {0}, akey={1}, "
-                              "Expected Value={2} and Received Value={3}\n"
-                              .format("dkey {0}".format(key),
-                                      "akey {0}".format(key),
-                                      original_data,
-                                      repr(val.value)[1:-1]))
+        # write a lot of individual data items, verify them, then destroy
+        self.write_a_bunch_of_values(no_of_dkeys)
 
-        except ValueError as e:
-            print (e)
-            print (traceback.format_exc())
-            self.fail("Test was expected to pass but it failed.\n")
+
+        # do it again, which should verify the first container
+        # was truely destroyed because a second round won't fit otherwise
+        self.write_a_bunch_of_values(no_of_dkeys)
