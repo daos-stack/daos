f50faac91841344f2ac3a35d05cbe8ebd6e5f3d4
David Quigley <david.quigley@intel.com>
2018-12-10 20:40:23 -0500

    DAOS-654 pool: Modify pool connect to use security credential
    Modify pool connect RPC to pass the opaque security credential
    instead of uid and gid of the calling process.
    
    Modifies the daos client library call for pool connect to request
    the credential from the agent and packs it into the connect RPC.
    
    Modifies the daos_io_server pool connect handler to pass the credential
    and the pool attributes new can_pool_connect api call in the
    security module.
    
    Implements upcall from daos_io_server to daos_server to validate and
    unpack the credential for use in can_pool_connect.
    
    Change-Id: If512ac64d4787747c88df82ff4ff63bafae18edf
    Signed-off-by: David Quigley <david.quigley@intel.com>

--- src/pool/cli.c
+++ src/pool/cli.c
@@ -32,6 +32,7 @@
 #include <daos/event.h>
 #include <daos/placement.h>
 #include <daos/pool.h>
+#include <daos/security.h>
 #include <daos_types.h>
 #include "cli_internal.h"
 #include "rpc.h"
@@ -576,22 +577,27 @@ dc_pool_connect(tse_task_t *task)
 		D_ERROR("failed to create rpc: %d\n", rc);
 		D_GOTO(out_pool, rc);
 	}
-
 	/** for con_argss */
 	crt_req_addref(rpc);
 
 	/** fill in request buffer */
 	pci = crt_req_get(rpc);
+
+	/** request credentials */
+	rc = dc_sec_request_creds(&pci->pci_cred);
+	if (rc != 0) {
+		D_ERROR("failed to obtain security credential: %d\n", rc);
+		D_GOTO(out_req, rc);
+	}
+
 	uuid_copy(pci->pci_op.pi_uuid, args->uuid);
 	uuid_copy(pci->pci_op.pi_hdl, pool->dp_pool_hdl);
-	pci->pci_uid = geteuid();
-	pci->pci_gid = getegid();
 	pci->pci_capas = args->flags;
 
 	rc = map_bulk_create(daos_task2ctx(task), &pci->pci_map_bulk, &map_buf,
 			     pool_buf_nr(pool->dp_map_sz));
 	if (rc != 0)
-		D_GOTO(out_req, rc);
+		D_GOTO(out_cred, rc);
 
 	/** Prepare "con_args" for pool_connect_cp(). */
 	con_args.pca_info = args->info;
@@ -613,6 +619,8 @@ dc_pool_connect(tse_task_t *task)
 
 out_bulk:
 	map_bulk_destroy(pci->pci_map_bulk, map_buf);
+out_cred:
+	daos_iov_free(&pci->pci_cred);
 out_req:
 	crt_req_decref(rpc);
 	crt_req_decref(rpc); /* free req */
