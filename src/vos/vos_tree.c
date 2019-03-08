da534368eb3193744f2d68e7fc6f225830dab20d
Ryon Jensen <ryon.jensen@intel.com>
2019-03-08 08:24:16 -0700

    DAOS-1659 vos: Checksums stored in VOS for Array Types (#156)
       - evt_desc, evt_root updated to store checksum information
       - changed daos_csum_buf_t to account for multiple checksums
       - added tests specific to checksums stored for extents
    
    Signed-off-by: Ryon Jensen <ryon.jensen@intel.com>

--- src/vos/vos_tree.c
+++ src/vos/vos_tree.c
@@ -151,7 +151,7 @@ ktr_rec_load(struct btr_instance *tins, struct btr_record *rec,
 	csum->cs_len  = krec->kr_cs_size;
 	csum->cs_type = krec->kr_cs_type;
 	if (csum->cs_csum == NULL)
-		csum->cs_csum = vos_krec2csum(krec);
+		csum->cs_csum = (uint8_t *) vos_krec2csum(krec);
 	else if (csum->cs_buf_len > csum->cs_len)
 		memcpy(csum->cs_csum, vos_krec2csum(krec), csum->cs_len);
 
@@ -589,7 +589,7 @@ svt_rec_load(struct btr_instance *tins, struct btr_record *rec,
 			memcpy(csum->cs_csum,
 			       vos_irec2csum(irec), csum->cs_len);
 		else
-			csum->cs_csum = vos_irec2csum(irec);
+			csum->cs_csum = (uint8_t *) vos_irec2csum(irec);
 	}
 
 	rbund->rb_rsize	= irec->ir_size;
