daos-stack/daos

commit 33f2f079d01ff0da3046dda2c3ef3f3ce89463b6

CART-89 build: change preprocessor (#99)
* CART-89 build: change preprocessor

change the preprocessor used for preprocessing to the same one used for
compiling. Previously gcc is used for preprocessing no matter what
compiler is supplied uisng
        scons COMPILER=

Note: this is just for the auto-generated header files to help developers, not
for the preprocessor used to build the code.

Signed-off-by: Yulu Jia <yulu.jia@intel.com>

src/cart/SConscript
@@ -97,7 +97,7 @@ def scons():
     prereqs.require(denv, 'mercury', 'pmix')
     denv.AppendUnique(RPATH=['$PREFIX/lib'])
 
-    pp_env = Environment(TOOLS=['default', 'extra'])
+    pp_env = env.Clone(tools=['extra'])
     pp_env.AppendUnique(CPPPATH=['src/include', 'src/cart'])
     prereqs.require(pp_env, 'mercury', 'pmix', headers_only=True)
 

commit 949df6a3d81365538e4eaeea848ae73aec07f531

CART-89 fix: Fix build issue (#109)
* CART-89 fix: Fix build hang issue

- Temporary fix to build hang issue caused by pre-processor being
now passed -g3 option introduced with patch:
33f2f079d01ff0da3046dda2c3ef3f3ce89463b6

- Update scons local

- icc11 defines atomic_compare_exchange_weak with 3 args, while memory
orders are specified via atomic_compare_exchange_weak_explicit. Updated gurt macro to use _explicit version of atomic operations.

Signed-off-by: Alexander Oganezov <alexander.a.oganezov@intel.com>

src/cart/SConscript
@@ -97,7 +97,7 @@ def scons():
     prereqs.require(denv, 'mercury', 'pmix')
     denv.AppendUnique(RPATH=['$PREFIX/lib'])
 
-    pp_env = env.Clone(tools=['extra'])
+    pp_env = Environment(TOOLS=['default', 'extra'])
     pp_env.AppendUnique(CPPPATH=['src/include', 'src/cart'])
     prereqs.require(pp_env, 'mercury', 'pmix', headers_only=True)
 

src/include/gurt/atomic.h
@@ -52,7 +52,7 @@
 
 /* stdatomic interface for compare_and_exchange doesn't quite align */
 #define atomic_compare_exchange(ptr, oldvalue, newvalue) \
-	atomic_compare_exchange_weak(ptr, &oldvalue, newvalue, \
+	atomic_compare_exchange_weak_explicit(ptr, &oldvalue, newvalue, \
 				memory_order_relaxed, memory_order_relaxed)
 #define atomic_store_release(ptr, value) \
 	atomic_store_explicit(ptr, value, memory_order_release)