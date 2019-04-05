a32c1f28541c46822cba4f0c663b1765f39ff5b5
Saurabh Tandan <saurabh.tandan@intel.com>
2019-04-05 21:54:51 +0000

    DAOS-2231 tests: Adding LLNL and Mpi4py test suites to avocado tests
    Automation of LLNL and Mpi4py test suite to be run under the framework
    of avocado.
    Minor comment added to romio.yaml
    
    Files modified:
    romio.yaml
    MpioUtils.py
    Files added:
    io/llnl_mpi4py.py
    io/llnl_mpi4py.yaml
    
    Change-Id: Icb48f1ae9ac7c35a382450949954b8a70f6a0d0e
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/util/MpioUtils.py
+++ src/tests/ftest/util/MpioUtils.py
@@ -15,12 +15,14 @@
     limitations under the License.
 
     GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
-    The Government's rights to use, modify, reproduce, release, perform, display,
-    or disclose this software are subject to the terms of the Apache License as
-    provided in Contract No. B609815.
+    The Government's rights to use, modify, reproduce, release, perform,
+    display, or disclose this software are subject to the terms of the Apache
+    License as provided in Contract No. B609815.
     Any reproduction of computer software, computer software documentation, or
     portions thereof marked with this legend must also reproduce the markings.
     '''
+from __future__ import print_function
+import os
 import subprocess
 import paramiko
 import socket
@@ -40,36 +42,42 @@ class MpioUtils():
 
         try:
             # checking mpich install
-            checkmpich = subprocess.check_output(["ssh", hostlist[0], "which mpichversion"])
+            checkmpich = subprocess.check_output(["ssh", hostlist[0],
+                                                  "which mpichversion"])
 
-            # Obtaning the location where mpich is installed by removing "mpichversion"
-            self.mpichinstall = checkmpich[:-13]
-
+            # Obtaning the location where mpich is installed by
+            # removing "mpichversion"
+            self.mpichinstall = checkmpich[:-17]
+            print (self.mpichinstall)
             return True
 
-        except subprocess.CalledProcessError as e:
-            print ("Mpich not installed \n {}".format(e))
+        except subprocess.CalledProcessError as error:
+            print ("Mpich not installed \n {}".format(error))
             return False
 
     def run_romio(self, basepath, hostlist, romio_test_repo):
         """
             Running ROMIO testsuite under mpich
             Function Arguments:
-                basepath --path where all daos and it's dependencies can be fetched from
-                hostlist --list of client hosts
+                basepath        --path where all daos and it's dependencies can
+                                  be fetched from
+                hostlist        --list of client hosts
                 romio_test_repo --built romio test directory
         """
 
         # environment variables only to be set on client node
         env_variables = ["export PATH={}/bin:$PATH".format(self.mpichinstall),
-                         "export LD_LIBRARY_PATH={}/lib:$LD_LIBRARY_PATH".format(self.mpichinstall),
-                         "export CRT_ATTACH_INFO_PATH={}/install/tmp/".format(basepath),
+                         "export LD_LIBRARY_PATH={}/lib:$LD_LIBRARY_PATH"\
+                         .format(self.mpichinstall),
+                         "export CRT_ATTACH_INFO_PATH={}/install/tmp/"\
+                         .format(basepath),
                          "export MPI_LIB=''", "export DAOS_SINGLETON_CLI=1"]
 
         # setting attributes
         cd_cmd = 'cd ' + romio_test_repo
         test_cmd = './runtests -fname=daos:test1 -subset -daos'
         run_cmd = cd_cmd + ' && ' + test_cmd
+        print ("Romio test run command: {}".format(run_cmd))
 
         try:
             # establish conection and run romio test
@@ -79,13 +87,75 @@ class MpioUtils():
             ssh.load_system_host_keys()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(hostlist[0])
-            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(env_variables[0] + \
-                                   " && " + env_variables[1] + " && " + env_variables[2] + \
-                                   " && " + env_variables[3] + " && " + env_variables[4] + \
-                                   " && " + run_cmd)
-            print ssh_stdout.read()
-            print ssh_stderr.read()
+            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(env_variables[0] \
+                                                + " && " + env_variables[1] + \
+                                                " && " + env_variables[2] + \
+                                                " && " + env_variables[3] + \
+                                                " && " + env_variables[4] + \
+                                                " && " + run_cmd)
+            print (ssh_stdout.read())
+            print (ssh_stderr.read())
 
-        except (IOError, OSError, paramiko.SSHException, socket.error) as e:
-            raise MpioFailed("<ROMIO Test FAILED> \nException occurred: {}".format(str(e)))
+        except (IOError, OSError, paramiko.SSHException, socket.error) as error:
+            raise MpioFailed("<ROMIO Test FAILED> \nException occurred: {}"\
+                             .format(str(error)))
 
+    def run_llnl_mpi4py(self, basepath, hostfile, pool_uuid, test_repo,
+                        test_name):
+        """
+            Running LLNL and MPI4PY testsuites
+            Function Arguments:
+                basepath          --path where all daos and it's dependencies
+                                    can be fetched from
+                hostfile          --client hostfile
+                pool_uuid         --Pool UUID
+                test_repo         --test repo location
+                display_output    --print output.
+        """
+
+        # environment variables only to be set on client node
+        env_variables = ["export PATH={}/bin:$PATH".format(self.mpichinstall),
+                         "export LD_LIBRARY_PATH={}/lib:$LD_LIBRARY_PATH"
+                         .format(self.mpichinstall),
+                         "export CRT_ATTACH_INFO_PATH={}/install/tmp/"
+                         .format(basepath),
+                         "export MPI_LIB=''", "export DAOS_SINGLETON_CLI=1",
+                         "export MPIO_USER_PATH=daos:",
+                         "export DAOS_POOL={}".format(pool_uuid),
+                         "export DAOS_SVCL={}".format(0)]
+
+        # setting attributes
+        cd_cmd = 'cd ' + test_repo
+        # running 8 client processes
+        if test_name == "llnl" and os.path.isfile(test_repo + "/testmpio_daos"):
+            test_cmd = "mpirun -np 8 --hostfile {} ./testmpio_daos 1"\
+                           .format(hostfile)
+        elif test_name == "mpi4py" and os.path.isfile(test_repo +
+                                                      "/test_io_daos.py"):
+            test_cmd = "mpiexec -n 8 python test_io_daos.py"
+        else:
+            raise MpioFailed("Wrong test name or test repo location specified")
+
+        run_cmd = cd_cmd + ";" + env_variables[0] + ";" + env_variables[1] \
+                  + ";" + env_variables[2] + ";" + env_variables[3] + ";" \
+                  + env_variables[4] + ";" + env_variables[5] + ";" \
+                  + env_variables[6] + ";" + env_variables[7] + ";" + test_cmd
+
+        print ("run command: {}".format(run_cmd))
+
+        try:
+            process = subprocess.Popen(run_cmd, stdout=subprocess.PIPE,
+                                       stderr=subprocess.STDOUT, shell=True)
+            while True:
+                output = process.stdout.readline()
+                if output == '' and process.poll() is not None:
+                    break
+                if output:
+                    print (output.strip())
+            if process.poll() != 0:
+                raise MpioFailed("{} Run process".format(test_name)
+                                 + " Failed with non zero exit"
+                                 + " code:{}".format(process.poll()))
+        except (ValueError, OSError) as error:
+            raise MpioFailed("<Test FAILED> \nException occurred: {}"\
+                             .format(str(error)))
