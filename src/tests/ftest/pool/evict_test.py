4e4e91456489e269289161cea6781ac2c33ce471
ravalsam <ravalsam@gmail.com>
2019-04-11 12:58:18 -0700

    Update evict_test.py

--- src/tests/ftest/pool/evict_test.py
+++ src/tests/ftest/pool/evict_test.py
@@ -57,7 +57,7 @@ class EvictTest(Test):
 
         self.daosctl = self.basepath + '/install/bin/daosctl'
 
-        server_group = self.params.get("name", '/serveri_config/',
+        server_group = self.params.get("name", '/server_config/',
                                        'daos_server')
 
         self.agent_sessions = AgentUtils.run_agent(self.basepath, self.hostlist)
