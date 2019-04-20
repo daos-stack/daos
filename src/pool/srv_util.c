cf1e820dc1b5b4324d315f4a2fc081f839a792a9
Wang Di <di.wang@intel.com>
2019-04-20 03:51:05 +0000

    DAOS-2315 rebuild: few fixes in rebuild and dtx
    A few fixes in rebuild and dtx to make dtx pass
    rebuild test.
    
    1. Reset fseq to 1 instead of 0 after add back, since
    1 is the initial value for fseq.
    
    2. Do not add back failed target/rank until validation
    finish, otherwise validate might fetch data from
    the original target.
    
    3. Also Update the placement map when updating the pool map.
    
    4. Urgent ult (for dtx_resync) pool should not be PRIV, since
    dtx_resync can not be private ULT.
    
    5. enable rebuild tests to 8
    
    Signed-off-by: Wang Di <di.wang@intel.com>

--- src/pool/srv_util.c
+++ src/pool/srv_util.c
@@ -318,15 +318,22 @@ ds_pool_map_tgts_update(struct pool_map *map, struct pool_target_id_list *tgts,
 		} else if (opc == POOL_ADD &&
 			 target->ta_comp.co_status != PO_COMP_ST_UP &&
 			 target->ta_comp.co_status != PO_COMP_ST_UPIN) {
+			/**
+			 * XXX this is only temporarily used for recovering
+			 * the DOWNOUT target back to UP after rebuild test,
+			 * so we do not update co_ver for now, otherwise the
+			 * object layout might be changed, so the ring shuffle
+			 * is based on target version. Once this is used
+			 * for reintegrate new target, co_ver should be
+			 * updated.
+			 */
 			D_DEBUG(DF_DSMS, "change target %u/%u to UP %p\n",
 				target->ta_comp.co_rank,
 				target->ta_comp.co_index, map);
 			target->ta_comp.co_status = PO_COMP_ST_UP;
-			target->ta_comp.co_ver = version;
-			target->ta_comp.co_fseq = 0;
+			target->ta_comp.co_fseq = 1;
 			nchanges++;
 			dom->do_comp.co_status = PO_COMP_ST_UP;
-			dom->do_comp.co_ver = version;
 		} else if (opc == POOL_EXCLUDE_OUT &&
 			 target->ta_comp.co_status == PO_COMP_ST_DOWN) {
 			D_DEBUG(DF_DSMS, "change target %u/%u to DOWNOUT %p\n",
