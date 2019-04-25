94a9ebb5ab5a423e8d178880120ddc11d9f9ca23
Padmanabhan <ravindran.padmanabhan@intel.com>
2019-04-25 10:27:19 -0400

    DAOS-2308: Convert btree test to CMOCKA model to produce XML output.
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/common/tests/btree.c
+++ src/common/tests/btree.c
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -83,7 +83,7 @@ struct ik_rec {
 
 #define IK_TREE_CLASS	100
 #define POOL_NAME "/mnt/daos/btree-test"
-#define POOL_SIZE ((1024 * 1024	 * 1024ULL))
+#define POOL_SIZE ((1024 * 1024 * 1024ULL))
 
 /** customized functions for btree */
 static int
