a64749cef66b479dcd0c1112ea6ed4d7748d62b3
ravalsam <samir.raval@intel.com>
2019-05-15 17:38:10 +0000

    DAOS-2483: Add support for container aggregation in Python test API.
    Signed-off-by: ravalsam <samir.raval@intel.com>

--- src/utils/py/daos_api.py
+++ src/utils/py/daos_api.py
@@ -1814,6 +1814,38 @@ class DaosContainer(object):
 
         return results
 
+    def aggregate(self, coh, epoch, cb_func=None):
+        """
+        Container Epoch Aggregation
+        Args:
+            coh - Container handler
+            epoch - Epoch to be aggregated to.
+            cb_func[Optional]:  To run API in Asynchronous mode.
+        return:
+            None
+        raise:
+            DaosApiError raised incase of API return code is nonzero
+        """
+        self.coh = coh
+        func = self.context.get_function('cont-aggregate')
+        epoch = ctypes.c_uint64(epoch)
+
+        if cb_func is None:
+            retcode = func(coh, ctypes.byref(epoch), None)
+            if retcode != 0:
+                raise DaosApiError("cont aggregate returned non-zero.RC: {0}"
+                                   .format(retcode))
+        else:
+            event = DaosEvent()
+            params = [coh, ctypes.byref(epoch), event]
+            thread = threading.Thread(target=AsyncWorker1,
+                                      args=(func,
+                                            params,
+                                            self.context,
+                                            cb_func,
+                                            self))
+            thread.start()
+
 class DaosSnapshot(object):
     """ A python object that can represent a DAOS snapshot. We do not save the
     coh in the snapshot since it is different each time the container is opened.
@@ -1959,6 +1991,7 @@ class DaosContext(object):
             'generate-oid'   : self.libtest.dts_oid_gen,
             'get-cont-attr'  : self.libdaos.daos_cont_get_attr,
             'get-pool-attr'  : self.libdaos.daos_pool_get_attr,
+            'cont-aggregate' : self.libdaos.daos_cont_aggregate,
             'get-layout'     : self.libdaos.daos_obj_layout_get,
             'init-event'     : self.libdaos.daos_event_init,
             'kill-server'    : self.libdaos.daos_mgmt_svc_rip,
