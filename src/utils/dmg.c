f6503f35457fe63f63434ebf1756ec53b6b0e68c
Samir Raval <ravalsam@gmail.com>
2019-05-21 12:42:41 -0700

    Update dmg.c

--- src/utils/dmg.c
+++ src/utils/dmg.c
@@ -857,7 +857,7 @@ create options:\n\
 		supports K (KB), M (MB), G (GB), T (TB) and P (PB) suffixes\n\
   --nvme=BYTES	target NVMe size in bytes (%s)\n\
   --svcn=N	number of pool service replicas (\"%u\")\n\
-  --rank=N	pool service server rank \n\
+  --rank=N	storage server rank \n\
   --target=N	pool targets on server like 0:1:2:3:4 (whole group)\n\
   --uid=UID	pool UID (geteuid())\n", default_group, default_mode,
 	       default_scm_size, default_nvme_size, default_svc_nreplicas);
