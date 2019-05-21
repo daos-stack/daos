daos-stack/daos

commit 02f8b5b1799d00357d89681ce41d9f71a52293a2

DAOS-1587 control: Add and parse configuration file for DAOS clients (#433)
DAOS Agent and DMG tool now read configuration data
from daos.yml.  Items such as hostlist, runtime_dir
and log_file are now parsed and used where applicable.
Not all items in the daos.yml are used at this time
and will be addressed at a later date.

Signed-off-by: Joel Rosenzweig <joel.b.rosenzweig@intel.com>

src/control/client/config.go
@@ -1,0 +1,147 @@
+//
+// (C) Copyright 2019 Intel Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+// The Government's rights to use, modify, reproduce, release, perform, display,
+// or disclose this software are subject to the terms of the Apache License as
+// provided in Contract No. 8F-30005.
+// Any reproduction of computer software, computer software documentation, or
+// portions thereof marked with this legend must also reproduce the markings.
+//
+
+package client
+
+import (
+	"github.com/daos-stack/daos/src/control/common"
+	"github.com/daos-stack/daos/src/control/log"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+const (
+	daosAgentDrpcSockEnv = "DAOS_AGENT_DRPC_DIR"
+	defaultRuntimeDir    = "/var/run/daos_agent"
+	defaultLogFile       = "/tmp/daos_agent.log"
+	defaultConfigPath    = "etc/daos.yml"
+	defaultSystemName    = "daos_server"
+	defaultPort          = 10000
+)
+
+// External interface provides methods to support various os operations.
+type External interface {
+	Getenv(string) string
+	RunCommand(string) error
+}
+
+type ext struct{}
+
+// runCommand executes command in subshell (to allow redirection) and returns
+// error result.
+func (e *ext) RunCommand(cmd string) error {
+	return exec.Command("sh", "-c", cmd).Run()
+}
+
+// getEnv wraps around os.GetEnv and implements External.getEnv().
+func (e *ext) Getenv(key string) string {
+	return os.Getenv(key)
+}
+
+// Configuration contains all known configuration variables available to the client
+type Configuration struct {
+	SystemName    string   `yaml:"name"`
+	AccessPoints  []string `yaml:"access_points"`
+	Port          int      `yaml:"port"`
+	HostList      []string `yaml:"hostlist"`
+	RuntimeDir    string   `yaml:"runtime_dir"`
+	HostFile      string   `yaml:"host_file"`
+	Cert          string   `yaml:"cert"`
+	Key           string   `yaml:"key"`
+	LogFile       string   `yaml:"log_file"`
+	LogFileFormat string   `yaml:"log_file_format"`
+	Path          string
+	Ext           External
+}
+
+// newDefaultConfiguration creates a new instance of configuration struct
+// populated with defaults.
+func newDefaultConfiguration(ext External) Configuration {
+	return Configuration{
+		SystemName:   defaultSystemName,
+		AccessPoints: []string{"localhost"},
+		Port:         defaultPort,
+		HostList:     []string{"localhost:10001"},
+		RuntimeDir:   defaultRuntimeDir,
+		LogFile:      defaultLogFile,
+		Path:         defaultConfigPath,
+		Ext:          ext,
+	}
+}
+
+// ProcessConfigFile loads a configuration file from the path given,
+// or from the default location if none is provided.  It returns a populated
+// Configuration struct based upon the default values and any config file overrides.
+func ProcessConfigFile(ConfigPath string) (Configuration, error) {
+	config := NewConfiguration()
+	if ConfigPath != "" {
+		log.Debugf("Overriding default config path with: %s", ConfigPath)
+		config.Path = ConfigPath
+	}
+
+	if !filepath.IsAbs(config.Path) {
+		newPath, err := common.GetAbsInstallPath(config.Path)
+		if err != nil {
+			return config, err
+		}
+		config.Path = newPath
+	}
+
+	err := config.LoadConfig()
+	if err != nil {
+		log.Debugf("\nUnable to read the configuration file from: '%s'\nUsing default configuration.", config.Path)
+		return config, nil
+	}
+	log.Debugf("DAOS Client config read from %s", config.Path)
+
+	return config, nil
+}
+
+// LoadConfig reads the configuration file specified by Configuration.Path
+// and parses it.  Parsed values override any default values.
+func (c *Configuration) LoadConfig() error {
+	bytes, err := ioutil.ReadFile(c.Path)
+	if err != nil {
+		return err
+	}
+
+	if err = c.parse(bytes); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// decodes YAML representation of Configuration struct
+func (c *Configuration) parse(data []byte) error {
+	return yaml.Unmarshal(data, c)
+}
+
+// NewConfiguration creates a new instance of the Configuration struct
+// populated with defaults and default external interface.
+func NewConfiguration() Configuration {
+	return newDefaultConfiguration(&ext{})
+}

src/control/dmg/main.go
@@ -25,21 +25,18 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-
 	"github.com/daos-stack/daos/src/control/client"
 	"github.com/daos-stack/daos/src/control/log"
-
 	flags "github.com/jessevdk/go-flags"
 	"github.com/pkg/errors"
+	"os"
+	"strings"
 )
 
 type cliOptions struct {
-	Hostlist string `short:"l" long:"hostlist" default:"localhost:10001" description:"comma separated list of addresses <ipv4addr/hostname:port>"`
+	Hostlist string `short:"l" long:"hostlist" description:"comma separated list of addresses <ipv4addr/hostname:port>"`
 	// TODO: implement host file parsing
-	Hostfile string `short:"f" long:"hostfile" description:"path of hostfile specifying list of addresses <ipv4addr/hostname:port>, if specified takes preference over HostList"`
-	// TODO: implement client side configuration file parsing
+	Hostfile   string  `short:"f" long:"hostfile" description:"path of hostfile specifying list of addresses <ipv4addr/hostname:port>, if specified takes preference over HostList"`
 	ConfigPath string  `short:"o" long:"config-path" description:"Client config file path"`
 	Storage    StorCmd `command:"storage" alias:"st" description:"Perform tasks related to storage attached to remote servers"`
 	Service    SvcCmd  `command:"service" alias:"sv" description:"Perform distributed tasks related to DAOS system"`
@@ -48,19 +45,18 @@ type cliOptions struct {
 }
 
 var (
-	opts  = new(cliOptions)
-	conns = client.NewConnect()
+	opts   = new(cliOptions)
+	conns  = client.NewConnect()
+	config = client.NewConfiguration()
 )
 
 func connectHosts() error {
+
 	if opts.Hostfile != "" {
 		return errors.New("hostfile option not implemented")
 	}
-	hosts := strings.Split(opts.Hostlist, ",")
-	if len(hosts) < 1 {
-		return errors.New("no hosts to connect to")
-	}
-	fmt.Println(sprintConns(conns.ConnectClients(hosts)))
+
+	fmt.Println(sprintConns(conns.ConnectClients(config.HostList)))
 	return nil
 }
 
@@ -68,6 +64,19 @@ func main() {
 	if dmgMain() != nil {
 		os.Exit(1)
 	}
+}
+
+// applyCmdLineOverrides will overwrite Configuration values with any non empty
+// data provided, usually from the commandline.
+func applyCmdLineOverrides(c *client.Configuration, opts *cliOptions) error {
+
+	if len(opts.Hostlist) > 0 {
+		hosts := strings.Split(opts.Hostlist, ",")
+		log.Debugf("Overriding hostlist from config file with %s", hosts)
+		c.HostList = hosts
+	}
+
+	return nil
 }
 
 func dmgMain() error {
@@ -83,12 +92,20 @@ func dmgMain() error {
 		return err
 	}
 
-	// TODO: implement configuration file parsing
-	if opts.ConfigPath != "" {
-		err = errors.New("config-path option not implemented")
-		log.Errorf(err.Error())
+	// Load the configuration file using the supplied path or the default path if none provided.
+	config, err = client.ProcessConfigFile(opts.ConfigPath)
+	if err != nil {
+		log.Errorf("An unrecoverable error occurred while processing the configuration file: %s", err)
 		return err
 	}
+
+	// Override configuration with any commandline values given
+	err = applyCmdLineOverrides(&config, opts)
+	if err != nil {
+		log.Errorf("Failed to apply command line overrides %s", err)
+		return err
+	}
+
 	err = connectHosts()
 	if err != nil {
 		log.Errorf("unable to connect to hosts: %v", err)

src/control/agent/main.go
@@ -24,26 +24,50 @@
 package main
 
 import (
-	"flag"
+	"github.com/daos-stack/daos/src/control/client"
+	"github.com/daos-stack/daos/src/control/common"
+	"github.com/daos-stack/daos/src/control/drpc"
+	"github.com/daos-stack/daos/src/control/log"
+	"github.com/jessevdk/go-flags"
 	"os"
 	"os/signal"
 	"path/filepath"
 	"syscall"
-
-	"github.com/daos-stack/daos/src/control/common"
-	"github.com/daos-stack/daos/src/control/drpc"
-	"github.com/daos-stack/daos/src/control/log"
 )
 
-var (
-	runtimeDir = flag.String("runtime_dir", "/var/run/daos_agent", "The path to runtime socket directory for daos_agent")
-	logFile    = flag.String("logfile", "/tmp/daos_agent.log", "Path for the daos agent log file")
+const (
+	agentSockName        = "agent.sock"
+	daosAgentDrpcSockEnv = "DAOS_AGENT_DRPC_DIR"
 )
+
+type cliOptions struct {
+	ConfigPath string `short:"o" long:"config-path" description:"Path to agent configuration file"`
+	RuntimeDir string `short:"s" long:"runtime_dir" description:"Path to agent communications socket"`
+	LogFile    string `short:"l" long:"logfile" description:"Full path and filename for daos agent log file"`
+}
+
+var opts = new(cliOptions)
 
 func main() {
 	if agentMain() != nil {
 		os.Exit(1)
 	}
+}
+
+// applyCmdLineOverrides will overwrite Configuration values with any non empty
+// data provided, usually from the commandline.
+func applyCmdLineOverrides(c *client.Configuration, opts *cliOptions) {
+
+	if opts.RuntimeDir != "" {
+		log.Debugf("Overriding socket path from config file with %s", opts.RuntimeDir)
+		c.RuntimeDir = opts.RuntimeDir
+	}
+
+	if opts.LogFile != "" {
+		log.Debugf("Overriding LogFile path from config file with %s", opts.LogFile)
+		c.LogFile = opts.LogFile
+	}
+
 }
 
 func agentMain() error {
@@ -52,13 +76,40 @@ func agentMain() error {
 
 	log.Debugf("Starting daos_agent:")
 
-	flag.Parse()
+	p := flags.NewParser(opts, flags.Default)
 
-	f, err := common.AppendFile(*logFile)
+	_, err := p.Parse()
+	if err != nil {
+		return err
+	}
+
+	// Load the configuration file using the supplied path or the
+	// default path if none provided.
+	config, err := client.ProcessConfigFile(opts.ConfigPath)
+	if err != nil {
+		log.Errorf("An unrecoverable error occurred while processing the configuration file: %s", err)
+		return err
+	}
+
+	// Override configuration with any commandline values given
+	applyCmdLineOverrides(&config, opts)
+
+	env := config.Ext.Getenv(daosAgentDrpcSockEnv)
+	if env != config.RuntimeDir {
+		log.Debugf("Environment variable '%s' has value '%s' which does not "+
+			"match '%s'", daosAgentDrpcSockEnv, env, config.RuntimeDir)
+	}
+
+	sockPath := filepath.Join(config.RuntimeDir, agentSockName)
+	log.Debugf("Full socket path is now: %s", sockPath)
+
+	f, err := common.AppendFile(config.LogFile)
 	if err != nil {
 		log.Errorf("Failure creating log file: %s", err)
 		return err
 	}
+	log.Debugf("Using logfile: %s", config.LogFile)
+
 	defer f.Close()
 	log.SetOutput(f)
 
@@ -67,7 +118,6 @@ func agentMain() error {
 	finish := make(chan bool, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
-	sockPath := filepath.Join(*runtimeDir, "agent.sock")
 	drpcServer, err := drpc.NewDomainSocketServer(sockPath)
 	if err != nil {
 		log.Errorf("Unable to create socket server: %v", err)

utils/config/daos.yml
@@ -1,8 +1,8 @@
-# DAOS agent configuration file.
+# DAOS client configuration file.
 #
 # Location of this configuration file is determined by first checking for the
-# path specified through the -f option of the daos_agent command line.
-# Otherwise, /etc/daos.conf is used.
+# path specified through the -o option of the daos_agent and DMG command line.
+# Otherwise, etc/daos.yml is used.
 #
 # Section describing the client configuration
 #
@@ -10,33 +10,36 @@
 # DAOS installations from the same node in the future.
 #
 # Specify the associated DAOS systems.
-# Name must match name specifed in the daos_server.conf file on the server.
+# Name must match name specifed in the daos_server.yml file on the server.
 #
-systems:
--
-  name: daos
-#
-#  # Access points
-#  # To operate, DAOS will need a quorum of access point nodes to be available.
-#  # Hosts can be specified with or without port, default port below.
-#  #
-#  # access_pts: ['hostname1:10001','hostname2:10001','hostname3:10001']
-#  # access_pts: [hostname1,hostname2,hostname3]
-#
-#  # Force default port number to connect to on daos_server, this will also
-#  # be used when connecting to access points if no port is specified.
-#  #
-#  # default: 10000
-#  # port: 10001
-#
-#  # Path to CA certificate
-#  # Not required if the DAOS server is configured in an insecured way
-#  #
-#  # ca_cert: ./.daos/ca.crt
-#
-#  # Path to client certificate and key file.
-#  # Discarded if no CA certificate is passed.
-#  #
-#  # default: ./.daos/daos.{crt,key}
-#  # cert: ./.daos/daos.crt
-#  # key: ./.daos/daos.key
+# default: daos_server
+#name: daos
+
+# Management server access points
+# default: hostname of this node at port 10001 for local testing
+#access_points: ['hostname1:10001','hostname2:10001','hostname3:10001']
+#access_points: [hostname1,hostname2,hostname3]
+
+# Force different port number to connect to access points.
+# default: 10000
+#port: 10001
+
+# Hostlist
+# comma separated list of addresses <ipv4addr/hostname:port>
+# default: ['localhost:10001']
+#hostlist: ['localhost:10001']
+
+
+# Path to client certificate and key file.
+# Discarded if no CA certificate is passed.
+# default: ./.daos/daos.{crt,key}
+#cert: ./.daos/daos.crt
+#key: ./.daos/daos.key
+
+# Use the given directory for creating unix domain sockets
+# default: /var/run/daos_agent
+#runtime_dir: /var/run/daos_agent
+
+# Full path and name of the DAOS agent logfile.
+# default: /tmp/daos_agent.log
+#log_file: /tmp/daos_agent.log

commit 66b7b0c68fa62bbeca71cec05a326378a4a70682

DAOS-2525 control: fix daos_shell subcommands not connecting
Fix regression introduced in
(02f8b5b1799d00357d89681ce41d9f71a52293a2) where daos_shell
subcommands don't connect to the hosts specified on the
commandline hostlist.

Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

Conflicts:
	src/control/dmg/main.go
	src/control/dmg/storage.go

src/control/client/config.go
@@ -24,13 +24,15 @@
 package client
 
 import (
-	"github.com/daos-stack/daos/src/control/common"
-	"github.com/daos-stack/daos/src/control/log"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/daos-stack/daos/src/control/common"
+	"github.com/daos-stack/daos/src/control/log"
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -105,15 +107,17 @@ func ProcessConfigFile(ConfigPath string) (Configuration, error) {
 	if !filepath.IsAbs(config.Path) {
 		newPath, err := common.GetAbsInstallPath(config.Path)
 		if err != nil {
-			return config, err
+			return config, errors.WithMessage(
+				err, "resolving install path")
 		}
+
 		config.Path = newPath
 	}
 
 	err := config.LoadConfig()
 	if err != nil {
-		log.Debugf("\nUnable to read the configuration file from: '%s'\nUsing default configuration.", config.Path)
-		return config, nil
+		return config, errors.WithMessagef(
+			err, "parsing config file %s", config.Path)
 	}
 	log.Debugf("DAOS Client config read from %s", config.Path)
 

src/control/dmg/main.go
@@ -34,9 +34,9 @@ import (
 )
 
 type cliOptions struct {
-	Hostlist string `short:"l" long:"hostlist" description:"comma separated list of addresses <ipv4addr/hostname:port>"`
+	HostList string `short:"l" long:"host-list" description:"comma separated list of addresses <ipv4addr/hostname:port>"`
 	// TODO: implement host file parsing
-	Hostfile   string  `short:"f" long:"hostfile" description:"path of hostfile specifying list of addresses <ipv4addr/hostname:port>, if specified takes preference over HostList"`
+	HostFile   string  `short:"f" long:"host-file" description:"path of hostfile specifying list of addresses <ipv4addr/hostname:port>, if specified takes preference over HostList"`
 	ConfigPath string  `short:"o" long:"config-path" description:"Client config file path"`
 	Storage    StorCmd `command:"storage" alias:"st" description:"Perform tasks related to storage attached to remote servers"`
 	Service    SvcCmd  `command:"service" alias:"sv" description:"Perform distributed tasks related to DAOS system"`
@@ -50,13 +50,29 @@ var (
 	config = client.NewConfiguration()
 )
 
-func connectHosts() error {
+func appSetup() error {
+	// Load the configuration file using the supplied path or the default
+	// path if none provided.
+	config, err := client.ProcessConfigFile(opts.ConfigPath)
+	if err != nil {
+		return errors.WithMessage(err, "processing config file")
+	}
 
-	if opts.Hostfile != "" {
+	if opts.HostList != "" {
+		config.HostList = strings.Split(opts.HostList, ",")
+	}
+
+	if opts.HostFile != "" {
 		return errors.New("hostfile option not implemented")
 	}
 
-	fmt.Println(sprintConns(conns.ConnectClients(config.HostList)))
+	ok, out := hasConns(conns.ConnectClients(config.HostList))
+	if !ok {
+		return errors.New(out) // no active connections
+	}
+
+	fmt.Println(out)
+
 	return nil
 }
 
@@ -64,19 +80,6 @@ func main() {
 	if dmgMain() != nil {
 		os.Exit(1)
 	}
-}
-
-// applyCmdLineOverrides will overwrite Configuration values with any non empty
-// data provided, usually from the commandline.
-func applyCmdLineOverrides(c *client.Configuration, opts *cliOptions) error {
-
-	if len(opts.Hostlist) > 0 {
-		hosts := strings.Split(opts.Hostlist, ",")
-		log.Debugf("Overriding hostlist from config file with %s", hosts)
-		c.HostList = hosts
-	}
-
-	return nil
 }
 
 func dmgMain() error {
@@ -92,24 +95,8 @@ func dmgMain() error {
 		return err
 	}
 
-	// Load the configuration file using the supplied path or the default path if none provided.
-	config, err = client.ProcessConfigFile(opts.ConfigPath)
-	if err != nil {
-		log.Errorf("An unrecoverable error occurred while processing the configuration file: %s", err)
-		return err
-	}
-
-	// Override configuration with any commandline values given
-	err = applyCmdLineOverrides(&config, opts)
-	if err != nil {
-		log.Errorf("Failed to apply command line overrides %s", err)
-		return err
-	}
-
-	err = connectHosts()
-	if err != nil {
-		log.Errorf("unable to connect to hosts: %v", err)
-		return err
+	if err := appSetup(); err != nil {
+		fmt.Println(err.Error()) // notify of app setup errors
 	}
 
 	// If no subcommand is specified, interactive shell is started

src/control/dmg/service.go
@@ -26,8 +26,6 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/pkg/errors"
 )
 
 // SvcCmd is the struct representing the top-level service subcommand.
@@ -51,8 +49,8 @@ func killRankSvc(uuid string, rank uint32) {
 
 // Execute is run when KillRankSvcCmd activates
 func (k *KillRankSvcCmd) Execute(args []string) error {
-	if err := connectHosts(); err != nil {
-		return errors.WithMessage(err, "unable to connect to hosts")
+	if err := appSetup(); err != nil {
+		return err
 	}
 
 	killRankSvc(k.PoolUUID, k.Rank)

src/control/dmg/storage.go
@@ -26,8 +26,6 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/pkg/errors"
 )
 
 // StorCmd is the struct representing the top-level storage subcommand.
@@ -52,8 +50,8 @@ func scanStor() {
 
 // Execute is run when ScanStorCmd activates
 func (s *ScanStorCmd) Execute(args []string) error {
-	if err := connectHosts(); err != nil {
-		return errors.Wrap(err, "unable to connect to hosts")
+	if err := appSetup(); err != nil {
+		return err
 	}
 
 	scanStor()
@@ -85,8 +83,8 @@ func formatStor() {
 
 // Execute is run when FormatStorCmd activates
 func (s *FormatStorCmd) Execute(args []string) error {
-	if err := connectHosts(); err != nil {
-		return errors.Wrap(err, "unable to connect to hosts")
+	if err := appSetup(); err != nil {
+		return err
 	}
 
 	formatStor()

src/control/dmg/utils.go
@@ -32,16 +32,16 @@ import (
 	pb "github.com/daos-stack/daos/src/control/common/proto/mgmt"
 )
 
-func hasConns(results client.ResultMap) (out string) {
-	out = sprintConns(results)
+func hasConns(results client.ResultMap) (bool, string) {
+	out := sprintConns(results)
 	for _, res := range results {
 		if res.Err == nil {
-			return
+			return true, out
 		}
 	}
 
 	// notify if there have been no successful connections
-	return fmt.Sprintf("%sNo active connections!", out)
+	return false, fmt.Sprintf("%sNo active connections!", out)
 }
 
 func sprintConns(results client.ResultMap) (out string) {

src/control/dmg/utils_test.go
@@ -82,7 +82,8 @@ func TestHasConnection(t *testing.T) {
 	}
 
 	for _, tt := range shelltests {
-		AssertEqual(t, hasConns(tt.results), tt.out, "bad output")
+		_, out := hasConns(tt.results)
+		AssertEqual(t, out, tt.out, "bad output")
 	}
 }
 