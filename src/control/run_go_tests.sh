c516ed5d11fef8deca1110bb925d94f87a7c56b6
David Quigley <david.quigley@intel.com>
2019-05-22 07:48:18 -0600

    Modify run_go_tests to detect and setup its environment
    Signed-off-by: David Quigley <david.quigley@intel.com>

--- src/control/run_go_tests.sh
+++ src/control/run_go_tests.sh
@@ -1,6 +1,61 @@
 #!/bin/bash
 ## Run linters across control plane code and execute Go tests
-set -e
+set -x
+
+function find_build_source()
+{
+	BASE=$PWD
+	
+	while true
+	do
+		path=`realpath ${BASE}`
+		if [ "${path}" == "/" ]; then
+			break
+		fi
+		test -e "${path}/.build_vars.sh" && echo "${path}/.build_vars.sh" && return
+		BASE=`dirname "${path}"`
+	done
+	echo ""
+}
+
+function check_environment()
+{
+	if [ -z "$LD_LIBRARY_PATH" ]; then
+		echo "false" && return
+	fi
+
+	if [ -z "$CGO_LDFLAGS" ]; then
+		echo "false" && return
+	fi
+
+	if [ -z "$CGO_CFLAGS" ]; then
+		echo "false" && return
+	fi
+
+	echo "true" && return
+} 
+
+function setup_environment()
+{
+	build_source=$(find_build_source)
+
+	if [ "${build_source}" == "" ]; then
+		echo "Unable to find .build_source.sh" && exit 1
+	fi
+
+	source ${build_source}
+
+	LD_LIBRARY_PATH="${SL_PREFIX}/lib:${SL_SPDK_PREFIX}/lib:${LD_LIBRARY_PATH}"
+	export LD_LIBRARY_PATH
+	export CGO_LDFLAGS="-L${SL_SPDK_PREFIX}/lib -L${SL_PREFIX}/lib"
+	export CGO_CFLAGS="-I${SL_SPDK_PREFIX}/include"
+}
+
+check=$(check_environment)
+
+if [ "$check" == "false" ]; then
+	setup_environment
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
