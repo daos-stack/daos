98f9fae3395e3b737029a1dca54391b768fafd85
David Quigley <david.quigley@intel.com>
2019-05-21 21:34:35 -0600

    DAOS-2372 Security: Add Admin Certificate loading in dmg for gRPC and provide --insecure flag
    The gRPC channel between the admin client and daos_server is protected with a
    TLS channel. This adds the certificate loading and transport credential setup
    to ensure that the channel is protected. It also adds an --insecure flag to the
    dmg client to tell it to bypass trying to establish the channel and just use an
    unencrypted http connection.
    
    Signed-off-by: David Quigley <david.quigley@intel.com>

--- src/control/client/mocks.go
+++ src/control/client/mocks.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/daos-stack/daos/src/control/common"
 	pb "github.com/daos-stack/daos/src/control/common/proto/mgmt"
+	"github.com/daos-stack/daos/src/control/security"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
@@ -274,7 +275,7 @@ type mockControl struct {
 	svcClient  pb.MgmtSvcClient
 }
 
-func (m *mockControl) connect(addr string) error {
+func (m *mockControl) connect(addr string, cfg *security.TransportConfig) error {
 	if m.connectRet == nil {
 		m.address = addr
 	}
@@ -322,7 +323,7 @@ type mockControllerFactory struct {
 	connectRet error
 }
 
-func (m *mockControllerFactory) create(address string) (Control, error) {
+func (m *mockControllerFactory) create(address string, cfg *security.TransportConfig) (Control, error) {
 	// returns controller with mock properties specified in constructor
 	cClient := newMockMgmtCtlClient(
 		m.features, m.ctrlrs, m.ctrlrResults,
@@ -334,7 +335,7 @@ func (m *mockControllerFactory) create(address string) (Control, error) {
 	controller := newMockControl(
 		address, m.state, m.connectRet, cClient, sClient)
 
-	err := controller.connect(address)
+	err := controller.connect(address, cfg)
 
 	return controller, err
 }
