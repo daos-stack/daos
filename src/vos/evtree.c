0e7766fa632343173bed5a99760cd09dfc393460
Jeff Olivier <jeffrey.v.olivier@intel.com>
2019-06-04 11:30:47 -0600

    DAOS-2637 vos: Update metadata tool for dynamic tree order
    Removes single entry handling and replaces it with generalized
    dynamic tree ordering based on what vos tool reports.
    Remove order_idx and remove dynamic ordering from object tree
    based on comments on a previous patch
    
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/vos/evtree.c
+++ src/vos/evtree.c
@@ -2845,11 +2845,13 @@ int evt_overhead_get(int alloc_overhead, int tree_order,
 		return -DER_INVAL;
 	}
 
+	ovhd->to_dyn_count = 0;
 	ovhd->to_record_msize = alloc_overhead + sizeof(struct evt_desc);
-	ovhd->to_single_size = alloc_overhead + sizeof(struct evt_desc) +
-		sizeof(struct evt_node_entry);
-	ovhd->to_node_size = alloc_overhead + sizeof(struct evt_node) +
+	ovhd->to_node_rec_msize = sizeof(struct evt_node_entry);
+	ovhd->to_node_overhead.no_size = alloc_overhead +
+		sizeof(struct evt_node) +
 		(tree_order * sizeof(struct evt_node_entry));
-	ovhd->to_order = tree_order;
+	ovhd->to_node_overhead.no_order = tree_order;
+
 	return 0;
 }
