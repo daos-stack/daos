0c75b0f9b6958232e2999c29ab4a8a1ba65fc58d
wangdi <di.wang@intel.com>
2019-06-07 09:06:15 -0700

    DAOS-2379 iosrv: cleanup IO stack for EC (#443)
    Cleanup the IO stack to EC
    
    1. Move some distribute transaction stuff to
    dtx to make IO processing clearer.
    2. Some code cleanup for IO RPC processing.
    3. Add target update/punch handler to separate
    leader/nonleader I/O request handler.
    4. Move part of obj req forward to DTX.
    
    Signed-off-by: Wang Di <di.wang@intel.com>

--- src/include/daos/object.h
+++ src/include/daos/object.h
@@ -69,6 +69,15 @@ struct daos_obj_layout {
 	struct daos_obj_shard	*ol_shards[0];
 };
 
+#define TGTS_IGNORE		((d_rank_t)-1)
+/** to identify each obj shard's target */
+struct daos_shard_tgt {
+	uint32_t		st_rank;	/* rank of the shard */
+	uint32_t		st_shard;	/* shard index */
+	uint32_t		st_tgt_idx;	/* target xstream index */
+	uint32_t		st_tgt_id;	/* target id */
+};
+
 static inline bool
 daos_obj_id_equal(daos_obj_id_t oid1, daos_obj_id_t oid2)
 {
