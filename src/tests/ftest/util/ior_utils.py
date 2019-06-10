66def299234d6d5e1468be4ebc4a2ccd669b87c4
Saurabh Tandan <saurabh.tandan@intel.com>
2019-06-10 21:09:15 +0000

    Modified ior_utils.py
    Change-Id: If3bb0e2b25c80eeb1135efb0cbad560596134eab
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/util/ior_utils.py
+++ src/tests/ftest/util/ior_utils.py
@@ -157,7 +157,7 @@ def run_ior_mpiio(basepath, mpichinstall, pool_uuid, svcl, np, hostfile,
             env_variables[4] + ";" + env_variables[5] + ";" +
 #            env_variables[6] + ";" +
             mpichinstall + "/bin/mpirun -np {0} --hostfile {1} "
-            "/home/standan/mpiio/ior/build/src/ior -a MPIIO {2} -i {3} "
+            "ior -a MPIIO {2} -i {3} "
             "-t {4} -b {5} -o daos:{6}".format(np, hostfile, ior_flags,
                                                     iteration,
                                                     transfer_size,
