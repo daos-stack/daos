f8a2e4186413a6cf7760859e435cdc4c3cad3168
Phil Henderson <phillip.henderson@intel.com>
2019-06-20 16:31:00 -0400

    Fixed pylint disable/enable typo
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/ior_utils.py
+++ src/tests/ftest/util/ior_utils.py
@@ -33,7 +33,7 @@ class IorFailed(Exception):
     """Raise if Ior failed."""
 
 
-# pylint: disable too-few-public-methods
+# pylint: disable=too-few-public-methods
 class IorParam(object):
     """Defines a object representing a single IOR command line parameter."""
 
@@ -73,7 +73,7 @@ class IorParam(object):
 
         """
         self.value = test.params.get(name, path, self.default)
-# pylint: enable too-few-public-methods
+# pylint: enable=too-few-public-methods
 
 
 class IorCommand(object):
