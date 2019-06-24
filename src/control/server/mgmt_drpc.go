5ee54206990c35bf91cdedd025968f7817bc69c1
Tom Nabarro <tom.nabarro@intel.com>
2019-06-24 10:43:37 +0100

    DAOS-2673 control: implement pool destroy
    Add capability to destroy DAOS pool from the control plane management
    tool, this involves creating gRPC client and server functionality to
    accept relevant parameters from user, validate and pass in protobuf
    messages over gRPC.
    
      - Pool identified by UUID
      - Force flag indicates remove even with existing connections
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/mgmt_drpc.go
+++ src/control/server/mgmt_drpc.go
@@ -44,6 +44,7 @@ const (
 	join          = C.DRPC_METHOD_MGMT_JOIN
 	getAttachInfo = C.DRPC_METHOD_MGMT_GET_ATTACH_INFO
 	createPool    = C.DRPC_METHOD_MGMT_CREATE_POOL
+	destroyPool   = C.DRPC_METHOD_MGMT_DESTROY_POOL
 
 	srvModuleID = C.DRPC_MODULE_SRV
 	notifyReady = C.DRPC_METHOD_SRV_NOTIFY_READY
