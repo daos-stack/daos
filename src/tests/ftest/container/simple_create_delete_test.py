c9f9a69f1dcb9230010185530a325a20fb406281
Amanda Justiniano <amanda.justiniano-pagn@intel.com>
2019-06-26 03:09:21 +0000

    Changes to how test creates and connects to pool.
    Signed-off-by: Amanda Justiniano <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/container/simple_create_delete_test.py
+++ src/tests/ftest/container/simple_create_delete_test.py
@@ -29,8 +29,9 @@ from avocado import main
 from apricot import TestWithoutServers
 
 
-import agent_utils
-import server_utils
+import agent_utils import run_agent, stop_agent
+import server_utils import run_server, stop_server
+import general_utils import get_pool, get_container
 import write_host_file
 from daos_api import DaosPool, DaosContainer, DaosApiError
 from conversion import c_uuid_to_str
@@ -51,48 +52,32 @@ class SimpleCreateDeleteTest(TestWithoutServers):
         Test basic container create/destroy/open/close/query.  Nothing fancy
         just making sure they work at a rudimentary level
 
-        :avocado: tags=container,containercreate,containerdestroy,basecont
+        :avocado: tags=pr,container,containercreate,containerdestroy,basecont
         """
-
-        pool = None
-        hostlist = None
-
         try:
-            hostlist = self.params.get("test_machines", '/run/hosts/*')
-            hostfile = write_host_file.write_host_file(hostlist,
-                                                       self.workdir)
+            self.hostlist_servers = self.params.get("test_machines", '/run/hosts/*')
+            self.hostfile_servers = write_host_file.write_host_file(
+                                    self.hostlist_servers, self.workdir)
 
-            self.agent_sessions = agent_utils.run_agent(self.basepath, hostlist)
-            server_utils.run_server(hostfile, self.server_group, self.basepath)
+            self.agent_sessions = run_agent(self.basepath, self.hostlist_servers)
+            run_server(self.hostfile_servers, self.server_group, self.basepath)
 
             # give it time to start
             time.sleep(2)
 
-            # parameters used in pool create
-            createmode = self.params.get("mode", '/run/conttests/createmode/')
-            createuid = self.params.get("uid", '/run/conttests/createuid/')
-            creategid = self.params.get("gid", '/run/conttests/creategid/')
-            createsetid = self.params.get("setname",
-                                          '/run/conttests/createset/')
-            createsize = self.params.get("size", '/run/conttests/createsize/')
+            # Parameters used in pool create
+            pool_mode = self.params.get("mode", '/run/pool/createmode/')
+            pool_name = self.params.get("setname", '/run/pool/createset/')
+            pool_size = self.params.get("size", '/run/pool/createsize/')
 
-            # initialize a python pool object then create the underlying
-            # daos storage
-            pool = DaosPool(self.context)
-            pool.create(createmode, createuid, creategid,
-                        createsize, createsetid, None)
+            # Create pool and connect
+            self.pool = get_pool(
+            self.context, pool_mode, pool_size, pool_name, 1, self.d_log)
 
-            # need a connection to create container
-            pool.connect(1 << 1)
+            # Create a container and open
+            self.container = get_container(self.context, self.pool, self.d_log)
 
-            # create a container
-            container = DaosContainer(self.context)
-            container.create(pool.handle)
-
-            # now open it
-            container.open()
-
-            # do a query and compare the UUID returned from create with
+            # Query and compare the UUID returned from create with
             # that returned by query
             container.query()
 
@@ -113,13 +98,13 @@ class SimpleCreateDeleteTest(TestWithoutServers):
         except Exception as excep:
             self.fail("Daos code segfaulted most likely, error: %s" % excep)
         finally:
-            # cleanup the pool
-            if pool is not None:
+            self.log.info("Clean up pool")
+            if self.pool is not None:
                 pool.disconnect()
                 pool.destroy(1)
             if self.agent_sessions:
-                agent_utils.stop_agent(self.agent_sessions)
-            server_utils.stop_server(hosts=hostlist)
+                stop_agent(self.agent_sessions)
+            stop_server(hosts=self.hostlist_servers)
 
 if __name__ == "__main__":
     main()
