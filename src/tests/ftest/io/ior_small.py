fcb936bc1860f2bb9fa42fb17fb699d64db714a6
Brian J. Murrell <brian.murrell@intel.com>
2019-07-18 15:38:17 -0400

    Additions to Sauburah's patch
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/tests/ftest/io/ior_small.py
+++ src/tests/ftest/io/ior_small.py
@@ -41,7 +41,7 @@ class IorSmallDaos(IorSingleServer):
                   offset instead of sequential.
                   All above three cases to be run with different combination of
                   transfer sizes and object classes.
-        :avocado: tags=all,daosio,small,iorsmall,iorsmalldaos
+        :avocado: tags=all,daosio,small,iorsmall,iorsmalldaos,pr,hw
         """
         # override ior flags and object class
         self.ior_flags = self.params.get("F", '/run/ior/iorflags/*/')
@@ -66,7 +66,7 @@ class IorSmallMpiio(EightServers):
                   offset instead of sequential.
                   All above three cases to be run with different combination of
                   transfer sizes and object classes.
-        :avocado: tags=all,daosio,small,iorsmall,iorsmallmpiio
+        :avocado: tags=all,daosio,small,iorsmall,iorsmallmpiio,pr,hw
         """
         # override ior flags
         ior_flags = self.params.get("F", '/run/ior/iorflags/*/')
