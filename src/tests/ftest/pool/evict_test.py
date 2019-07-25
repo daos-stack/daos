944b49b17c3c627c0c461de6beb705d86efca4b3
Maureen Jean <maureen.jean@intel.com>
2019-07-25 18:02:40 -0400

    Including test_utils,general_utils and command utils
    Test-tag:all,pool,pr,full_regression,small,poolevict
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/pool/evict_test.py
+++ src/tests/ftest/pool/evict_test.py
@@ -260,7 +260,7 @@ class EvictTests(TestWithServers):
         """
         Test evicting a pool using an invalid server group name.
 
-        :avocado: tags=all,pool,pr,full_regression,small,poolevict,
+        :avocado: tags=all,pool,pr,full_regression,small,poolevict
         :avocado: tags=poolevict_bad_server_name
         """
         test_param = self.params.get("server_name", '/run/badparams/*')
@@ -270,7 +270,7 @@ class EvictTests(TestWithServers):
         """
         Test evicting a pool using an invalid uuid.
 
-        :avocado: tags=all,pool,pr,full_regression,small,poolevict,
+        :avocado: tags=all,pool,pr,full_regression,small,poolevict
         :avocado: tags=poolevict_bad_uuid
         """
         test_param = self.params.get("uuid", '/run/badparams/*')
