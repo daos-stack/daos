74f538821e00bc0826b06c81cb70587141f8cd5d
Fan Yong <fan.yong@intel.com>
2019-07-26 09:23:36 +0800

    DAOS-2928 object: fix dtx related log message
    Under some cases, the server may ask the client to retry
    the RPC because of some non-committed DTX. That is normal,
    not error. Fix related log message to avoid confusing.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/vos/vos_iterator.c
+++ src/vos/vos_iterator.c
@@ -137,9 +137,12 @@ nested_prepare(vos_iter_type_t type, struct vos_iter_dict *dict,
 		    rc == -DER_NONEXIST)
 			D_DEBUG(DB_TRACE, "%s iterator does not exist\n",
 				dict->id_name);
+		else if (rc == -DER_INPROGRESS)
+			D_DEBUG(DB_TRACE, "Cannot fetch nested tree from "
+				"because of conflict modification: %d\n", rc);
 		else
 			D_ERROR("Problem fetching nested tree from iterator:"
-				" %d", rc);
+				" %d\n", rc);
 		return rc;
 	}
 
@@ -556,8 +559,13 @@ probe:
 			daos_anchor_set_eof(anchor);
 			rc = 0;
 		} else {
-			D_ERROR("failed to probe iterator (type=%d anchor=%p): "
-				"%d\n", type, probe_anchor, rc);
+			if (rc == -DER_INPROGRESS)
+				D_DEBUG(DB_TRACE, "Cannot probe because of "
+					"some conflict modification: %d\n", rc);
+			else
+				D_ERROR("failed to probe iterator (type=%d "
+					"anchor=%p): %d\n",
+					type, probe_anchor, rc);
 		}
 		D_GOTO(out, rc);
 	}
@@ -565,8 +573,13 @@ probe:
 	while (1) {
 		rc = vos_iter_fetch(ih, &iter_ent, anchor);
 		if (rc != 0) {
-			D_ERROR("failed to fetch iterator (type=%d): %d\n",
-				type, rc);
+			if (rc == -DER_INPROGRESS)
+				D_DEBUG(DB_TRACE, "Cannot fetch iterator "
+					"(type=%d) because of conflict "
+					"modification: %d\n", type, rc);
+			else
+				D_ERROR("failed to fetch iterator (type=%d): "
+					"%d\n", type, rc);
 			break;
 		}
 
