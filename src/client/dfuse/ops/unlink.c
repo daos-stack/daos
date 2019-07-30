a7ca905a16b329ef5c72cd4373b9d90537dddf2e
Ashley Pittman <ashley.m.pittman@intel.com>
2019-07-30 08:39:14 +0100

    style
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/client/dfuse/ops/unlink.c
+++ src/client/dfuse/ops/unlink.c
@@ -33,9 +33,8 @@ dfuse_cb_unlink(fuse_req_t req, struct dfuse_inode_entry *parent,
 
 	rc = dfs_remove(parent->ie_dfs->dfs_ns, parent->ie_obj, name, false);
 
-	if (rc == 0) {
+	if (rc == 0)
 		DFUSE_REPLY_ZERO(req);
-	} else {
+	else
 		DFUSE_REPLY_ERR_RAW(fs_handle, req, -rc);
-	}
 }
