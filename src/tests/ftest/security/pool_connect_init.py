a23f38dcb24173b0cfccb91bbe0289747e7723a6
Ding Ho <ding-hwa.ho@intel.com>
2019-08-19 22:18:23 +0000

    DAOS-2930 DAOS security: initial tests pool creation with defaul ACL and connect New     file: security/pool_connect_init.py New     file: security/pool_connect_init.yaml
    Signed-off-by: Ding Ho <ding-hwa.ho@intel.com>

--- src/tests/ftest/security/pool_connect_init.py
+++ src/tests/ftest/security/pool_connect_init.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 '''
-  (C) Copyright 2017-2019 Intel Corporation.
+  (C) Copyright 2019 Intel Corporation.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -41,38 +41,32 @@ class PoolSecurityTest(TestWithServers):
             pool3: owner and owner-group with both root-id.
         """
         super(PoolSecurityTest, self).setUp()
+        root_id = 0
+        user_uid = os.geteuid()
+        user_gid = os.getegid()
+        self.pool1 = self.poolCreate(user_uid, root_id)
+        self.pool2 = self.poolCreate(root_id, user_gid)
+        self.pool3 = self.poolCreate(root_id, root_id)
+
+    def poolCreate(self, user_uid, user_gid):
         # get parameters from yaml file
         mode = self.params.get("mode", '/run/poolparams/createmode/', 511)
         setid = self.params.get("setname", '/run/poolparams/createset/',
                                 "daos_server")
         size = self.params.get("size", '/run/poolparams/createsize/',
                                107374182)
-        root_id = 0
-        user_uid = os.geteuid()
-        user_gid = os.getegid()
+        self.log.info("===Create DAOS pool with:")
+        self.log.info("===uid:  %s", user_uid)
+        self.log.info("===gid:  %s", user_gid)
         try:
-            self.log.info("===Create DAOS pool1 with:")
-            self.log.info("===uid:  %s", user_uid)
-            self.log.info("===gid:  %s", root_id)
-            self.pool1 = DaosPool(self.context)
-            self.pool1.create(mode, user_uid, root_id, size, setid)
-            self.log.info("===>Pool1 create succeed.")
-            self.log.info("===Create DAOS pool2 with:")
-            self.log.info("===uid:  %s", root_id)
-            self.log.info("===gid:  %s", user_gid)
-            self.pool2 = DaosPool(self.context)
-            self.pool2.create(mode, root_id, user_gid, size, setid)
-            self.log.info("===>Pool2 create succeed.")
-            self.log.info("===Create DAOS pool3 with:")
-            self.log.info("===uid:  %s", root_id)
-            self.log.info("===gid:  %s", root_id)
-            self.pool3 = DaosPool(self.context)
-            self.pool3.create(mode, root_id, root_id, size, setid)
-            self.log.info("===>Pool3 create succeed.")
+            self.pool = DaosPool(self.context)
+            self.pool.create(mode, user_uid, user_gid, size, setid)
+            self.log.info("===>Pool create succeed.")
         except DaosApiError as exc:
             self.log.info(exc)
             self.log.info(traceback.format_exc())
             self.fail("##(0)Pool create failed in setUp.\n")
+        return self.pool
 
     def tearDown(self):
         try:
@@ -94,7 +88,7 @@ class PoolSecurityTest(TestWithServers):
     def test_poolconnect(self):
         """
         Test basic pool security in pool creation and connect.
-        DAOS-2930: DAOS security: initial tests pool creation with defaul
+        DAOS-2930: DAOS security: initial tests pool creation with default
                    ACL and connect.
         Tests DAOS pools creation, and connect with:
         (1)pool1: match of owner uid.  (succeed)
@@ -108,9 +102,14 @@ class PoolSecurityTest(TestWithServers):
         user_gid = os.getegid()
         der_no_permission = "RC: -1001"
         try:
-            self.log.info("===Connecting to the DAOS pool1")
+            self.log.info(
+                "===Connecting to the DAOS pool1, match of owner uid.")
             self.log.info("===user_uid:  %s", user_uid)
             self.log.info("===user_gid:  %s", user_gid)
+            # connect to the pool with permission
+            #   DAOS_PC_RO = int(1 << 0)
+            #   DAOS_PC_RW = int(1 << 1)
+            #   DAOS_PC_EX = int(1 << 2)
             self.pool1.connect(1 << 1)
             self.log.info("===>(1.1)Connect to the DAOS pool1 succeed.")
         except DaosApiError as exc:
@@ -119,7 +118,8 @@ class PoolSecurityTest(TestWithServers):
             self.fail(
                 "##(1.1)Connecting to the DAOS pool1 failed.")
         try:
-            self.log.info("===Connecting to the DAOS pool2")
+            self.log.info(
+                "===Connecting to the DAOS pool2, match of owner gid.")
             self.log.info("===user_uid:  %s", user_uid)
             self.log.info("===user_gid:  %s", user_gid)
             self.pool2.connect(1 << 1)
@@ -130,7 +130,8 @@ class PoolSecurityTest(TestWithServers):
             self.fail(
                 "##(1.2)Connecting to the DAOS pool2 failed.")
         try:
-            self.log.info("===Connecting to the DAOS pool3")
+            self.log.info(
+                "===Connecting to the DAOS pool3, mismatch of uid and gid.")
             self.log.info("===user_uid:  %s", user_uid)
             self.log.info("===user_gid:  %s", user_gid)
             self.pool3.connect(1 << 1)
