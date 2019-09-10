ed528bdf41471ce04436a78f49ac08c861d9d82c
Hua Kuang <hua.kuang@intel.com>
2019-09-10 22:09:56 +0000

    DAOS-2759 Avocado functional tests do not run in DAOS env installed from RPMs
                1.  Renamed install/lib that was holding libraries to lib64
                2.  Copied src/tests/ftest to $PREFIX/lib/daos/TESTING/ftest
                3.  copied .build_vars.* to install/lib to allow existing test code run
                4.  updated some avocado test setup code to adapt the new file locations
    
    Change-Id: Idd82a4b104123117b44d65229ada372df62da7d8
    Signed-off-by: Hua Kuang <hua.kuang@intel.com>

--- src/tests/ftest/util/server_utils.py
+++ src/tests/ftest/util/server_utils.py
@@ -209,9 +209,9 @@ def run_server(hostfile, setname, basepath, uri_path=None, env_dict=None,
 
         # Run server in insecure mode until Certificate tests are in place
         server_cmd.extend([daos_srv_bin, "--debug", "--config",
-                          '{}/{}'.format(build_vars["PREFIX"], AVOCADO_FILE),
-                          "start", "-i", "-a",
-                          os.path.join(build_vars["PREFIX"], "tmp")])
+                           '{}/{}'.format(build_vars["PREFIX"], AVOCADO_FILE),
+                           "start", "-i", "-a",
+                           os.path.join(build_vars["PREFIX"], "tmp")])
 
         print("Start CMD>>>>{0}".format(' '.join(server_cmd)))
 
