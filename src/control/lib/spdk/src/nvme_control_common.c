c0d93ba80a7f3da8f55c36d458c3dbdc49e97c18
Amanda Justiniano-Pagan <amanda.justiniano-pagn@intel.com>
2019-09-11 18:54:18 +0000

    Fixed feedback from daosbuild1.
    Signed-off-by: Amanda Justiniano-Pagan <amanda.justiniano-pagn@intel.com>

--- src/control/lib/spdk/src/nvme_control_common.c
+++ src/control/lib/spdk/src/nvme_control_common.c
@@ -20,7 +20,7 @@
 // Any reproduction of computer software, computer software documentation, or
 // portions thereof marked with this legend must also reproduce the markings.
 //
-+
+
 #include "spdk/stdinc.h"
 #include "spdk/nvme.h"
 #include "spdk/env.h"
