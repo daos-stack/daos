8ff38afecb22d2cc4af2734a7879ebe4ee7f515b
Amanda Justiniano-Pagan <amanda.justiniano-pagn@intel.com>
2019-09-11 19:24:31 +0000

    Use * for comments. Fixed long comments.
    Signed-off-by: Amanda Justiniano-Pagan <amanda.justiniano-pagn@intel.com>

--- src/control/lib/spdk/include/nvme_control_common.h
+++ src/control/lib/spdk/include/nvme_control_common.h
@@ -1,24 +1,24 @@
 /**
-// (C) Copyright 2018-2019 Intel Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
-// The Government's rights to use, modify, reproduce, release, perform, display,
-// or disclose this software are subject to the terms of the Apache License as
-// provided in Contract No. 8F-30005.
-// Any reproduction of computer software, computer software documentation, or
-// portions thereof marked with this legend must also reproduce the markings.
+* (C) Copyright 2018-2019 Intel Corporation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+* The Government's rights to use, modify, reproduce, release, perform, display,
+* or disclose this software are subject to the terms of the Apache License as
+* provided in Contract No. 8F-30005.
+* Any reproduction of computer software, computer software documentation, or
+* portions thereof marked with this legend must also reproduce the markings.
 */
 
 #ifndef NVMECONTROL_STATIC_H
@@ -83,7 +83,8 @@ attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 	const struct spdk_nvme_ctrlr_opts *opts);
 
 /**
- * Initialize the ret_t struct by allocating memory and setting attributes to NULL
+ * Initialize the ret_t struct by allocating memory and setting attributes
+ * to NULL
  *
  * \return struct ret_t
  **/
--- src/control/lib/spdk/src/nvme_control_common.c
+++ src/control/lib/spdk/src/nvme_control_common.c
@@ -1,24 +1,24 @@
 /**
-// (C) Copyright 2018-2019 Intel Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
-// The Government's rights to use, modify, reproduce, release, perform, display,
-// or disclose this software are subject to the terms of the Apache License as
-// provided in Contract No. 8F-30005.
-// Any reproduction of computer software, computer software documentation, or
-// portions thereof marked with this legend must also reproduce the markings.
+* (C) Copyright 2018-2019 Intel Corporation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+* The Government's rights to use, modify, reproduce, release, perform, display,
+* or disclose this software are subject to the terms of the Apache License as
+* provided in Contract No. 8F-30005.
+* Any reproduction of computer software, computer software documentation, or
+* portions thereof marked with this legend must also reproduce the markings.
 */
 
 #include "spdk/stdinc.h"
@@ -96,10 +96,10 @@ attach_cb(void *cb_ctx, const struct spdk_nvme_transport_id *trid,
 	g_controllers = entry;
 
 	/*
-	 * Each controller has one or more namespaces.  An NVMe namespace is basically
-	 *  equivalent to a SCSI LUN.  The controller's IDENTIFY data tells us how
-	 *  many namespaces exist on the controller.  For Intel(R) P3X00 controllers,
-	 *  it will just be one namespace.
+	 * Each controller has one or more namespaces.  An NVMe namespace is
+	 *  basically equivalent to a SCSI LUN.  The controller's IDENTIFY data
+	 *  tells us how many namespaces exist on the controller.
+	 *  For Intel(R) P3X00 controllers, it will just be one namespace.
 	 *
 	 * Note that in NVMe, namespace IDs start at 1, not 0.
 	 */
