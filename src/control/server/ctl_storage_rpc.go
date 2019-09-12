adcf5535a000a72677ef5a18b5c5704b43909de9
Tom Nabarro <tom.nabarro@intel.com>
2019-09-12 13:40:32 +0100

    DAOS-3245 control: improve test coverage for storage prepare (#1075)
    Improve unit test coverage of storage prepare gRPC flow.
    Separately fetch scm state and perform init implicitly before prep operations,
    log messages separately for prep scm/nvme, fix copyright notice dates
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/ctl_storage_rpc.go
+++ src/control/server/ctl_storage_rpc.go
@@ -86,8 +86,13 @@ func (c *StorageControlService) doScmPrepare(req *pb.PrepareScmReq) (resp *pb.Pr
 	resp = &pb.PrepareScmResp{}
 	msg := "Storage Prepare SCM"
 
-	needsReboot, pmemDevs, err := c.PrepareScm(PrepareScmRequest{Reset: req.GetReset_()},
-		types.ScmStateUnknown)
+	scmState, err := c.GetScmState()
+	if err != nil {
+		resp.State = newState(c.log, pb.ResponseStatus_CTRL_ERR_SCM, err.Error(), "", msg)
+		return
+	}
+
+	needsReboot, pmemDevs, err := c.PrepareScm(PrepareScmRequest{Reset: req.GetReset_()}, scmState)
 	if err != nil {
 		resp.State = newState(c.log, pb.ResponseStatus_CTRL_ERR_SCM, err.Error(), "", msg)
 		return
