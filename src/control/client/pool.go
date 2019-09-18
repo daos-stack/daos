e65cc145a4b9357209f7dad12533e47acaa4bd7e
Tom Nabarro <tom.nabarro@intel.com>
2019-09-18 11:22:12 +0100

    another code review related update
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/client/pool.go
+++ src/control/client/pool.go
@@ -34,14 +34,12 @@ import (
 //
 // Currently expect only one connection to be available and return that.
 func chooseServiceLeader(cs []Control) (Control, error) {
-	switch len(cs) {
-	case 0:
-		return nil, errors.Errorf("unexpected number of connections, "+
-			"want 1, have %d", len(cs))
-	default:
-		// just return the first connection, the service leader
-		return cs[0], nil
+	if len(cs) == 0 {
+		return nil, errors.New("no active connections")
 	}
+
+	// just return the first connection, expected to be the service leader
+	return cs[0], nil
 }
 
 // PoolCreateReq struct contains request
