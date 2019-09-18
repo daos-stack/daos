e9d88b0a81fecee11ed3a9925cf22cd10aef4f45
Phil Henderson <phillip.henderson@intel.com>
2019-09-18 10:54:35 -0400

    DAOS-3250 tests: Enhancing apricot tearDown() method. (#1066)
    * DAOS-3172 tests: Enhancing apricot tearDown() method.
    
    Adding self.contianer and self.pool destruction into the tearDown()
    method to aleviate the need to repeat the code in multiple test cases.
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/mdtest_test_base.py
+++ src/tests/ftest/util/mdtest_test_base.py
@@ -58,15 +58,6 @@ class MdtestBase(TestWithServers):
         self.processes = self.params.get("np", '/run/mdtest/client_processes/*')
         self.manager = self.params.get("manager", '/run/mdtest/*', "MPICH")
 
-    def tearDown(self):
-        """Tear down each test case."""
-        try:
-            if self.pool is not None:
-                self.pool.destroy(1)
-        finally:
-            # Stop the servers and agents
-            super(MdtestBase, self).tearDown()
-
     def execute_mdtest(self):
         """Create a pool and execute Mdtest."""
         # Get the pool params
