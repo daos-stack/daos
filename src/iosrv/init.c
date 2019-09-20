33b8252de62d0d4426d6901e51540ef43b3f860d
Joel Rosenzweig <joel.b.rosenzweig@intel.com>
2019-09-20 13:51:12 +0000

    Attempt to fix style "ERROR" on help text.
    Signed-off-by: Joel Rosenzweig <joel.b.rosenzweig@intel.com>

--- src/iosrv/init.c
+++ src/iosrv/init.c
@@ -646,7 +646,7 @@ Options:\n\
       Number of helper XS -per vos target (default 1)\n\
   --firstcore=firstcore, -f firstcore\n\
       index of first core for service thread (default 0)\n\
-  --pinned_numa_node=numanode, -p numanode\n\
+  --pinned_numa_node = numanode, -p numanode\n\
       Bind to cores within the specified NUMA node (default 0)\n\
   --group=group, -g group\n\
       Server group name (default \"%s\")\n\
