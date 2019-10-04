22065fed88ff0e2bf98e8060810a7e2b035c5d1b
Tom Nabarro <tom.nabarro@intel.com>
2019-10-04 22:37:57 +0100

    fix checkpatch warning
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/init/setup_spdk.sh
+++ src/control/server/init/setup_spdk.sh
@@ -17,9 +17,9 @@ else
 	PCI_WHITELIST="$_PCI_WHITELIST" NRHUGE="$_NRHUGE" \
 	TARGET_USER="$_TARGET_USER" "$scriptpath"
 
-	set -e
+	eet -e
 
-	chown -R $_TARGET_USER /dev/hugepages /dev/uio* \
+	chown -R "$_TARGET_USER" /dev/hugepages /dev/uio* \
 		/sys/class/uio/uio*/device/config \
 		/sys/class/uio/uio*/device/resource*
 
