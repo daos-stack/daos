1b30c8fc600154c17d09f0f85ba3661b1997a46b
Joel Rosenzweig <joel.b.rosenzweig@intel.com>
2019-10-04 20:25:19 +0000

    Fixed bad merge in mocks.go
    Signed-off-by: Joel Rosenzweig <joel.b.rosenzweig@intel.com>

--- src/control/client/mocks.go
+++ src/control/client/mocks.go
@@ -89,16 +89,6 @@ func (m *mgmtCtlListFeaturesClient) Recv() (*ctlpb.Feature, error) {
 	return m.features[0], nil
 }
 
-type mgmtCtlRequestDeviceScanStreamerClient struct {
-	grpc.ClientStream
-}
-
-func (m *mgmtCtlRequestDeviceScanStreamerClient) Recv() (*ctlpb.DeviceScanReply, error) {
-	return &ctlpb.DeviceScanReply{}, nil
-}
-
-
-
 type mgmtCtlStorageFormatClient struct {
 	grpc.ClientStream
 	ctrlrResults  NvmeControllerResults
@@ -188,15 +178,6 @@ func (m *mockMgmtCtlClient) ListFeatures(ctx context.Context, req *ctlpb.EmptyRe
 	return &mgmtCtlListFeaturesClient{features: m.features}, nil
 }
 
-func (m* mockMgmtCtlClient) RequestDeviceScanStreamer(ctx context.Context, in *ctlpb.DeviceScanRequest, o ...grpc.CallOption) (ctlpb.MgmtCtl_RequestDeviceScanStreamerClient, error) {
-	// undefined: mgmtCtlRequestDeviceScanStreamerClient, but it is defined in src/control/common/proto/ctl/control.pb.go
-	return &mgmtCtlRequestDeviceScanStreamerClient{}, nil
-}
-
-func (m* mockMgmtCtlClient) RequestProviderList(ctx context.Context, in *ctlpb.ProviderListRequest, o ...grpc.CallOption) (*ctlpb.ProviderListReply, error) {
-	return &ctlpb.ProviderListReply{}, nil
-}
-
 func (m *mockMgmtCtlClient) StoragePrepare(ctx context.Context, req *ctlpb.StoragePrepareReq, o ...grpc.CallOption) (*ctlpb.StoragePrepareResp, error) {
 	// return successful prepare results, state member messages
 	// initialise with zero values indicating mgmt.CTRL_SUCCESS
@@ -240,6 +221,22 @@ func (m *mockMgmtCtlClient) StorageBurnIn(ctx context.Context, req *ctlpb.Storag
 
 func (m *mockMgmtCtlClient) FetchFioConfigPaths(ctx context.Context, req *ctlpb.EmptyReq, o ...grpc.CallOption) (ctlpb.MgmtCtl_FetchFioConfigPathsClient, error) {
 	return &mgmtCtlFetchFioConfigPathsClient{}, nil
+}
+
+type mgmtCtlRequestDeviceScanStreamerClient struct {
+	grpc.ClientStream
+}
+
+func (m *mgmtCtlRequestDeviceScanStreamerClient) Recv() (*ctlpb.DeviceScanReply, error) {
+	return &ctlpb.DeviceScanReply{}, nil
+}
+
+func (m* mockMgmtCtlClient) RequestDeviceScanStreamer(ctx context.Context, in *ctlpb.DeviceScanRequest, o ...grpc.CallOption) (ctlpb.MgmtCtl_RequestDeviceScanStreamerClient, error) {
+	return &mgmtCtlRequestDeviceScanStreamerClient{}, nil
+}
+
+func (m* mockMgmtCtlClient) RequestProviderList(ctx context.Context, in *ctlpb.ProviderListRequest, o ...grpc.CallOption) (*ctlpb.ProviderListReply, error) {
+	return &ctlpb.ProviderListReply{}, nil
 }
 
 func newMockMgmtCtlClient(
@@ -297,6 +294,17 @@ func (m *mockMgmtSvcClient) SmdListDevs(
 	return &mgmtpb.SmdDevResp{}, nil
 }
 
+func (m *mockMgmtSvcClient) SmdListPools(
+	ctx context.Context,
+	req *mgmtpb.SmdPoolReq,
+	o ...grpc.CallOption,
+) (*mgmtpb.SmdPoolResp, error) {
+
+	// return successful SMD pool list
+	// initialise with zero values indicating mgmt.CTRL_SUCCESS
+	return &mgmtpb.SmdPoolResp{}, nil
+}
+
 func (m *mockMgmtSvcClient) Join(ctx context.Context, req *mgmtpb.JoinReq, o ...grpc.CallOption) (*mgmtpb.JoinResp, error) {
 
 	return &mgmtpb.JoinResp{}, nil
@@ -308,6 +316,14 @@ func (c *mockMgmtSvcClient) GetAttachInfo(ctx context.Context, in *mgmtpb.GetAtt
 
 func (m *mockMgmtSvcClient) KillRank(ctx context.Context, req *mgmtpb.DaosRank, o ...grpc.CallOption) (*mgmtpb.DaosResp, error) {
 	return &mgmtpb.DaosResp{}, nil
+}
+
+func (m *mockMgmtSvcClient) SystemMemberQuery(ctx context.Context, req *mgmtpb.SystemMemberQueryReq, o ...grpc.CallOption) (*mgmtpb.SystemMemberQueryResp, error) {
+	return &mgmtpb.SystemMemberQueryResp{}, nil
+}
+
+func (m *mockMgmtSvcClient) SystemStop(ctx context.Context, req *mgmtpb.SystemStopReq, o ...grpc.CallOption) (*mgmtpb.SystemStopResp, error) {
+	return &mgmtpb.SystemStopResp{}, nil
 }
 
 func newMockMgmtSvcClient() mgmtpb.MgmtSvcClient {
