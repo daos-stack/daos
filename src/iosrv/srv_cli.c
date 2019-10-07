657585b7dd9f362bf69f5ef4e3f7131f94f06bf3
Jeff Olivier <jeffrey.v.olivier@intel.com>
2019-10-07 04:53:22 -0600

    DAOS-2753 vos: Update punch model for keys (#1056)
    The incarnation log (ilog) is introduced.  It's used to log
    two things
    1. All punches
    2. The first update after a punch (or the set of updates that
       may be first once committed).
    
    The fork on punch model for keys is replaced with logging.  This
    simplifies the btree used for keys as a key is created and the
    incarnation log is scanned to determine existence at a given
    point in time.
    
    Iteration is modified in VOS to return future punches for
    visible keys to support rebuild.
    
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/iosrv/srv_cli.c
+++ src/iosrv/srv_cli.c
@@ -286,8 +286,7 @@ dsc_obj_fetch(daos_handle_t oh, daos_epoch_t epoch, daos_key_t *dkey,
 int
 dsc_obj_list_obj(daos_handle_t oh, daos_epoch_t epoch, daos_key_t *dkey,
 		 daos_key_t *akey, daos_size_t *size, uint32_t *nr,
-		 daos_key_desc_t *kds, daos_epoch_range_t *eprs,
-		 d_sg_list_t *sgl, daos_anchor_t *anchor,
+		 daos_key_desc_t *kds, d_sg_list_t *sgl, daos_anchor_t *anchor,
 		 daos_anchor_t *dkey_anchor, daos_anchor_t *akey_anchor)
 {
 	tse_task_t	*task;
@@ -300,7 +299,7 @@ dsc_obj_list_obj(daos_handle_t oh, daos_epoch_t epoch, daos_key_t *dkey,
 		return rc;
 
 	rc = dc_obj_list_obj_task_create(oh, th, dkey, akey, size, nr, kds,
-					 eprs, sgl, anchor, dkey_anchor,
+					 sgl, anchor, dkey_anchor,
 					 akey_anchor, true, NULL,
 					 dsc_scheduler(), &task);
 	if (rc)
