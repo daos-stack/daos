c43752a7898fa68f9c52c7545e4468e534b2f096
Fan Yong <fan.yong@intel.com>
2019-10-11 22:02:49 +0800

    DAOS-2429 dtx: DTX performance optimization - B4
    Mainly include the following:
    
    1. Increase the CPU yield frequency during DTX aggregation and
       batched commit to allow more normal IO schedule.
    
    2. Use client-side HLC in the DTX ID as the modification epoch.
    
    3. Replace HLC based race detection between DTX resync and normal
       modification handling logic with server-side generation.
    
    4. Fine-grained contents for PMDK undo log to reduce the overhead
       caused by PMDK related logic.
    
    5. Store the indexed committed DTX table in DRAM rather than SCM.
    
       When a DTX is committed, it is added into the in-DRAM committed
       DTX table that is organized as a Btree. If related DAOS server
       restarts because of some reason, for supporting DAOS server to
       rejoin system (if not evicted), then we need to re-establish
       the indexed committed DTX table in DRAM. So we will store the
       committed DTX entries in SCM without index (only append) when
       it is committed.
    
       A dedicated ULT will scan the non-indexed committed DTX entries
       in SCM and re-generate the indexed committed DTX table in DRAM
       when open the container.
    
    6. Some DTX logic can be optimized if the DTX contains single
       participator (such as modifying single replicated object).
       For example: the DTX can be committed immediately after
       local modification done.
    
    6.1. It is unnecessary to allocate related DTX records for the
         target (object/dkey/akey/SV/EV rec) to be modified.
    
    6.2. It is unnecessary to insert the DTX into active DTX tree,
         instead, it can be directly inserted into the committed
         DTX table when commit.
    
    6.3. Punch logic can be simplified as without DTX case.
    
    6.4. CoS logic can be bypassed.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/include/daos_srv/vos_types.h
+++ src/include/daos_srv/vos_types.h
@@ -226,8 +226,8 @@ typedef struct {
 	union {
 		/** Returned earliest update epoch for a key */
 		daos_epoch_t			ie_earliest;
-		/** Return the DTX handled time for DTX iteration. */
-		uint64_t			ie_dtx_time;
+		/** record size */
+		daos_size_t			ie_rsize;
 	};
 	union {
 		/** Returned entry for container UUID iterator */
@@ -251,8 +251,6 @@ typedef struct {
 			uint32_t		ie_dtx_intent;
 		};
 		struct {
-			/** record size */
-			daos_size_t		ie_rsize;
 			/** record extent */
 			daos_recx_t		ie_recx;
 			/* original in-tree extent */
