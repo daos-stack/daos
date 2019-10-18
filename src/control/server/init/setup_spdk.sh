575263c3884167722d1be8786c148733472fe7b4
Samir Raval <samir.raval@intel.com>
2019-10-18 19:04:17 +0000

    Revert "DAOS-3415 control: spdk setup no chmod as root (#1192)"
    This reverts commit 911ad23405b93fd010ce66f0d71d2cadc85c17b8.
    
    Signed-off-by: Samir Raval <samir.raval@intel.com>

--- src/control/server/init/setup_spdk.sh
+++ src/control/server/init/setup_spdk.sh
@@ -17,15 +17,9 @@ else
 	PCI_WHITELIST="$_PCI_WHITELIST" NRHUGE="$_NRHUGE" \
 	TARGET_USER="$_TARGET_USER" "$scriptpath"
 
-	# build arglist manually to filter missing directories/files
-	# so we don't error on non-existent entities
-	for glob in /dev/hugepages '/dev/uio*'		\
-		'/sys/class/uio/uio*/device/config'	\
-		'/sys/class/uio/uio*/device/resource*'; do
-
-		if list=$(ls "$glob"); then
-			echo "$list" | xargs -r chown -R "$_TARGET_USER"
-		fi
-	done
+	chmod 777 /dev/hugepages
+	chmod 666 /dev/uio*
+	chmod 666 /sys/class/uio/uio*/device/config
+	chmod 666 /sys/class/uio/uio*/device/resource*
 fi
 
