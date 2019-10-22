7f1f986d278751ccfdf571b85652c20878c2d960
Joel Rosenzweig <joel.b.rosenzweig@intel.com>
2019-10-22 16:53:35 +0000

    Removed old code that I left in by mistake.
    Signed-off-by: Joel Rosenzweig <joel.b.rosenzweig@intel.com>

--- src/control/lib/netdetect/netdetect_test.go
+++ src/control/lib/netdetect/netdetect_test.go
@@ -34,62 +34,6 @@ import (
 // hwloc will use this topology for queries instead of the local system
 // running the test.
 func TestParseTopology(t *testing.T) {
-	/*
-	   	tests := []struct {
-	   		netDevsList []string
-	   		topology    string
-	   		expected    []string
-	   	}{
-	   		// boro-84 has two NUMA nodes, with eth0, eth1, ib0 on NUMA 0,
-	   		// and no ib1 in the topology
-	   		// The format for the expected string is "device:cpuset:nodeset:numa"
-	   		{[]string{""}, "testdata/boro-84.xml", []string{}},
-	   		{[]string{"eth0"}, "testdata/boro-84.xml", []string{"eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"eth1"}, "testdata/boro-84.xml", []string{"eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"ib0"}, "testdata/boro-84.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"eth0", "eth1"}, "testdata/boro-84.xml", []string{"eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"eth0", "eth1", "ib0"}, "testdata/boro-84.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"ib0", "eth0", "ib1", "eth1"}, "testdata/boro-84.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"eth0", "eth1", "ib0"}, "testdata/boro-84.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"eth0", "eth1", "ib0"}, "testdata/boro-84.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"eth0", "eth1", "ib0"}, "testdata/boro-84.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		// wolf-133 has two NUMA nodes, with eth0, eth1, ib0 on NUMA 0,
-	   		// and ib1 on NUMA 1.  Notice that the cpuset and nodeset for ib1
-	   		// reflect that they are on a different node
-	   		{[]string{"eth0"}, "testdata/wolf-133.xml", []string{"eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"eth1"}, "testdata/wolf-133.xml", []string{"eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"ib0"}, "testdata/wolf-133.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"ib1"}, "testdata/wolf-133.xml", []string{"ib1:0xffffff00,0x0000ffff,0xff000000:0x00000002:1"}},
-	   		{[]string{"eth0", "eth1"}, "testdata/wolf-133.xml", []string{"eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"ib0", "eth0", "eth1"}, "testdata/wolf-133.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0"}},
-	   		{[]string{"ib0", "eth0", "ib1", "eth1"}, "testdata/wolf-133.xml", []string{"ib0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth0:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "eth1:0x000000ff,0xffff0000,0x00ffffff:0x00000001:0", "ib1:0xffffff00,0x0000ffff,0xff000000:0x00000002:1"}},
-	   	}
-
-	   	for _, tt := range tests {
-	   		_, err := os.Stat(tt.topology)
-	   		AssertEqual(t, err, nil, "unable to load xmlTopology")
-	   		os.Setenv("HWLOC_XMLFILE", tt.topology)
-	   		defer os.Unsetenv("HWLOC_XMLFILE")
-	   		netAdapterAffinity, err := GetAffinityForNetworkDevices(tt.netDevsList)
-	   		if err != nil {
-	   			t.Fatal(err)
-	   		}
-	   		AssertEqual(t, len(netAdapterAffinity), len(tt.expected), "number of devices expected vs found does not match")
-	   		for j, i := range netAdapterAffinity {
-	   			AssertEqual(t, i.String(), tt.expected[j],
-	   				"unexpected mismatch with device and topology")
-	   		}
-	   	}
-
-	   /////////////////////////
-	*/
-	// boro-84 has two NUMA nodes, with eth0, eth1, ib0 on NUMA 0,
-	// and no ib1 in the topology
-	// The format for the expected string is "device:cpuset:nodeset:numa"
-	// wolf-133 has two NUMA nodes, with eth0, eth1, ib0 on NUMA 0,
-	// and ib1 on NUMA 1.  Notice that the cpuset and nodeset for ib1
-	// reflect that they are on a different node
-
 	for name, tc := range map[string]struct {
 		netDevsList []string
 		topology    string
