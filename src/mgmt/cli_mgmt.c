b33127e7c3a62888b2243f53788188629c222ed7
Niu Yawei <yawei.niu@intel.com>
2019-10-28 21:03:29 +0800

    DAOS-3106 pool: NVMe faulty reaction ops (#1250)
    Implemented NVMe faulty reaction operation, added nvme_recovery test
    suite.
    
    Limitations:
    - Due to lack of the infrastructure to get pool map offline, the
      offline NVMe faulty reaction isn't supported. It'll be done once
      the pool cache & pool map IV cleanup done.
    
    - The nvme_recovery test can only run manually, because NVMe reint
      isn't supported yet, the failed VOS target can't be reintegrated
      back and DAOS server needs be restarted after each run.
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I04aa76ab240fb51f003feae24627b64d4c68f66b

--- src/mgmt/cli_mgmt.c
+++ src/mgmt/cli_mgmt.c
@@ -452,10 +452,12 @@ detach_group(bool server, bool pmixless, crt_group_t *group)
 {
 	int rc = 0;
 
-	if (!pmixless)
-		rc = crt_group_detach(group);
-	else if (!server)
-		rc = crt_group_view_destroy(group);
+	if (!server) {
+		if (!pmixless)
+			rc = crt_group_detach(group);
+		else
+			rc = crt_group_view_destroy(group);
+	}
 	D_ASSERTF(rc == 0, "%d\n", rc);
 }
 
