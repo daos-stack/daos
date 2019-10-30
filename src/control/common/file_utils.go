daos-stack/daos

commit 539f2d0cea40ef09400e3d7d1d6c160f29050bb7

DAOS-3270 control: Remove global logger (#1286)
Final push to remove all use of the global logger.

Also changes common.ShowBufferOnFailure() to run directly
rather than returning a closure, as the previous behavior was
only needed for compatibility with the removed ShowLogOnFailure().

Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

src/control/common/file_utils.go
@@ -35,7 +35,7 @@ import (
 	"github.com/pkg/errors"
 	yaml "gopkg.in/yaml.v2"
 
-	log "github.com/daos-stack/daos/src/control/logging"
+	"github.com/daos-stack/daos/src/control/logging"
 )
 
 const (
@@ -232,7 +232,7 @@ func SyncDir(path string) (err error) {
 }
 
 // Run executes command in os and builds useful error message.
-func Run(cmd string) error {
+func Run(log logging.Logger, cmd string) error {
 	log.Debugf("exec '%s'\n", cmd)
 
 	// executing as subshell enables pipes in cmd string

src/control/server/external.go
@@ -38,7 +38,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/daos-stack/daos/src/control/common"
-	log "github.com/daos-stack/daos/src/control/logging"
+	"github.com/daos-stack/daos/src/control/logging"
 )
 
 const (
@@ -84,6 +84,7 @@ func errPermsAnnotate(err error) (e error) {
 }
 
 type ext struct {
+	log     logging.Logger
 	history []string
 }
 
@@ -96,7 +97,7 @@ func (e *ext) getHistory() []string {
 func (e *ext) runCommand(cmd string) error {
 	e.history = append(e.history, fmt.Sprintf(msgCmd, cmd))
 
-	return common.Run(cmd)
+	return common.Run(e.log, cmd)
 }
 
 // writeToFile wraps around common.WriteString and writes input string to given
@@ -124,10 +125,10 @@ func (e *ext) createEmpty(path string, size int64) error {
 	defer file.Close()
 
 	if err := syscall.Fallocate(int(file.Fd()), 0, 0, size); err != nil {
-		e, ok := err.(syscall.Errno)
-		if ok && (e == syscall.ENOSYS || e == syscall.EOPNOTSUPP) {
-			log.Debugf(
-				"Warning: Fallocate not supported, attempting Truncate: ", e)
+		errno, ok := err.(syscall.Errno)
+		if ok && (errno == syscall.ENOSYS || errno == syscall.EOPNOTSUPP) {
+			e.log.Debugf(
+				"Warning: Fallocate not supported, attempting Truncate: %s", errno)
 
 			if err := file.Truncate(size); err != nil {
 				return err
@@ -144,7 +145,7 @@ func (e *ext) mount(
 
 	op := fmt.Sprintf(msgMount, dev, mount, mntType, fmt.Sprint(flags), opts)
 
-	log.Debugf(op)
+	e.log.Debug(op)
 	e.history = append(e.history, op)
 
 	if flags == 0 {
@@ -160,7 +161,7 @@ func (e *ext) mount(
 
 // isMountPoint checks if path is likely to be a mount point.straiowhotenoul
 func (e *ext) isMountPoint(path string) (bool, error) {
-	log.Debugf(msgIsMountPoint, path)
+	e.log.Debugf(msgIsMountPoint, path)
 	e.history = append(e.history, fmt.Sprintf(msgIsMountPoint, path))
 
 	pStat, err := os.Stat(path)
@@ -184,7 +185,7 @@ func (e *ext) isMountPoint(path string) (bool, error) {
 // NOTE: requires elevated privileges, lazy unmount, mntpoint may not be
 //       available immediately after
 func (e *ext) unmount(path string) error {
-	log.Debugf(msgUnmount, path)
+	e.log.Debugf(msgUnmount, path)
 	e.history = append(e.history, fmt.Sprintf(msgUnmount, path))
 
 	// ignore NOENT errors, treat as success
@@ -204,7 +205,7 @@ func (e *ext) unmount(path string) error {
 
 // NOTE: may require elevated privileges
 func (e *ext) mkdir(path string) error {
-	log.Debugf(msgMkdir, path)
+	e.log.Debugf(msgMkdir, path)
 	e.history = append(e.history, fmt.Sprintf(msgMkdir, path))
 
 	if err := os.MkdirAll(path, 0777); err != nil {
@@ -215,7 +216,7 @@ func (e *ext) mkdir(path string) error {
 
 // NOTE: may require elevated privileges
 func (e *ext) remove(path string) error {
-	log.Debugf(msgRemove, path)
+	e.log.Debugf(msgRemove, path)
 	e.history = append(e.history, fmt.Sprintf(msgRemove, path))
 
 	// ignore NOENT errors, treat as success
@@ -226,7 +227,7 @@ func (e *ext) remove(path string) error {
 }
 
 func (e *ext) exists(path string) (bool, error) {
-	log.Debugf(msgExists, path)
+	e.log.Debugf(msgExists, path)
 	e.history = append(e.history, fmt.Sprintf(msgExists, path))
 
 	if _, err := os.Stat(path); err == nil {
@@ -258,7 +259,7 @@ func (e *ext) listGroups(usr *user.User) ([]string, error) {
 func (e *ext) chownR(root string, uid int, gid int) error {
 	op := fmt.Sprintf(msgChownR, root, uid, gid)
 
-	log.Debugf(op)
+	e.log.Debug(op)
 	e.history = append(e.history, op)
 
 	return filepath.Walk(root, func(name string, info os.FileInfo, err error) error {

src/control/client/config_test.go
@@ -61,7 +61,7 @@ func getTestFile(t *testing.T) *os.File {
 
 func TestLoadConfigDefaultsNoFile(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer common.ShowBufferOnFailure(t, buf)()
+	defer common.ShowBufferOnFailure(t, buf)
 
 	defaultConfig := getDefaultConfig(t)
 
@@ -76,7 +76,7 @@ func TestLoadConfigDefaultsNoFile(t *testing.T) {
 
 func TestLoadConfigFromDefaultFile(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer common.ShowBufferOnFailure(t, buf)()
+	defer common.ShowBufferOnFailure(t, buf)
 
 	defaultConfig := getDefaultConfig(t)
 	if err := os.MkdirAll(path.Dir(defaultConfig.Path), 0755); err != nil {
@@ -106,7 +106,7 @@ func TestLoadConfigFromDefaultFile(t *testing.T) {
 
 func TestLoadConfigFromFile(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer common.ShowBufferOnFailure(t, buf)()
+	defer common.ShowBufferOnFailure(t, buf)
 
 	testFile := getTestFile(t)
 	defer os.Remove(testFile.Name())
@@ -128,7 +128,7 @@ func TestLoadConfigFromFile(t *testing.T) {
 
 func TestLoadConfigFailures(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer common.ShowBufferOnFailure(t, buf)()
+	defer common.ShowBufferOnFailure(t, buf)
 
 	testFile := getTestFile(t)
 	defer os.Remove(testFile.Name())

src/control/client/connect_test.go
@@ -89,7 +89,7 @@ func checkResults(t *testing.T, addrs Addresses, results ResultMap, e error) {
 
 func TestConnectClients(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	eMsg := "socket connection is not active (%s)"
 
@@ -137,7 +137,7 @@ func TestConnectClients(t *testing.T) {
 
 func TestDuplicateConns(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	cc := defaultMockConnect(log)
 	results := cc.ConnectClients(append(MockServers, MockServers...))
@@ -147,7 +147,7 @@ func TestDuplicateConns(t *testing.T) {
 
 func TestGetClearConns(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	cc := defaultClientSetup(log)
 
@@ -169,7 +169,7 @@ func TestGetClearConns(t *testing.T) {
 
 func TestListFeatures(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	cc := defaultClientSetup(log)
 
@@ -182,7 +182,7 @@ func TestListFeatures(t *testing.T) {
 
 func TestStorageScan(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	cc := defaultClientSetup(log)
 
@@ -200,7 +200,7 @@ func TestStorageScan(t *testing.T) {
 
 func TestStorageFormat(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	for name, tt := range map[string]struct {
 		formatRet error
@@ -246,7 +246,7 @@ func TestStorageFormat(t *testing.T) {
 
 func TestStorageUpdate(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	tests := []struct {
 		updateRet error
@@ -293,7 +293,7 @@ func TestStorageUpdate(t *testing.T) {
 
 func TestKillRank(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	tests := []struct {
 		killRet error
@@ -316,7 +316,7 @@ func TestKillRank(t *testing.T) {
 
 func TestPoolGetACL(t *testing.T) {
 	log, buf := logging.NewTestLogger(t.Name())
-	defer ShowBufferOnFailure(t, buf)()
+	defer ShowBufferOnFailure(t, buf)
 
 	for name, tt := range map[string]struct {
 		addr             Addresses

src/control/cmd/agent/main.go
@@ -156,14 +156,18 @@ func agentMain(log *logging.LeveledLogger, opts *cliOptions) error {
 	finish := make(chan bool, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
-	drpcServer, err := drpc.NewDomainSocketServer(sockPath)
+	drpcServer, err := drpc.NewDomainSocketServer(log, sockPath)
 	if err != nil {
 		log.Errorf("Unable to create socket server: %v", err)
 		return err
 	}
 
-	drpcServer.RegisterRPCModule(NewSecurityModule(config.TransportConfig))
-	drpcServer.RegisterRPCModule(&mgmtModule{config.AccessPoints[0], config.TransportConfig})
+	drpcServer.RegisterRPCModule(NewSecurityModule(log, config.TransportConfig))
+	drpcServer.RegisterRPCModule(&mgmtModule{
+		log:  log,
+		ap:   config.AccessPoints[0],
+		tcfg: config.TransportConfig,
+	})
 
 	err = drpcServer.Start()
 	if err != nil {

src/control/cmd/agent/mgmt_rpc.go
@@ -35,7 +35,7 @@ import (
 
 	mgmtpb "github.com/daos-stack/daos/src/control/common/proto/mgmt"
 	"github.com/daos-stack/daos/src/control/drpc"
-	log "github.com/daos-stack/daos/src/control/logging"
+	"github.com/daos-stack/daos/src/control/logging"
 	"github.com/daos-stack/daos/src/control/security"
 )
 
@@ -48,6 +48,7 @@ const (
 // Management Service proxy, handling dRPCs sent by libdaos by forwarding them
 // to MS.
 type mgmtModule struct {
+	log logging.Logger
 	// The access point
 	ap   string
 	tcfg *security.TransportConfig
@@ -74,7 +75,7 @@ func (mod *mgmtModule) handleGetAttachInfo(reqb []byte) ([]byte, error) {
 		return nil, errors.Wrap(err, "unmarshal GetAttachInfo request")
 	}
 
-	log.Debugf("GetAttachInfo %s %v", mod.ap, *req)
+	mod.log.Debugf("GetAttachInfo %s %v", mod.ap, *req)
 
 	dialOpt, err := security.DialOptionForTransportConfig(mod.tcfg)
 	if err != nil {

src/control/cmd/agent/security_rpc.go
@@ -31,6 +31,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/daos-stack/daos/src/control/drpc"
+	"github.com/daos-stack/daos/src/control/logging"
 	"github.com/daos-stack/daos/src/control/security"
 	"github.com/daos-stack/daos/src/control/security/auth"
 )
@@ -92,13 +93,15 @@ func (e *external) LookupGroupID(gid uint32) (*user.Group, error) {
 
 // SecurityModule is the security drpc module struct
 type SecurityModule struct {
+	log    logging.Logger
 	ext    auth.UserExt
 	config *security.TransportConfig
 }
 
 //NewSecurityModule creates a new module with the given initialized TransportConfig
-func NewSecurityModule(tc *security.TransportConfig) *SecurityModule {
+func NewSecurityModule(log logging.Logger, tc *security.TransportConfig) *SecurityModule {
 	mod := SecurityModule{
+		log:    log,
 		config: tc,
 	}
 	mod.InitModule(nil)
@@ -111,7 +114,7 @@ func (m *SecurityModule) HandleCall(client *drpc.Client, method int32, body []by
 		return nil, errors.Errorf("Attempt to call unregistered function")
 	}
 
-	info, err := security.DomainInfoFromUnixConn(client.Conn)
+	info, err := security.DomainInfoFromUnixConn(m.log, client.Conn)
 	if err != nil {
 		return nil, errors.WithMessage(err, "Unable to get credentials for client socket")
 	}

src/control/cmd/daos_server/start_test.go
@@ -125,8 +125,6 @@ func cmpEnv(t *testing.T, wantConfig, gotConfig *ioserver.Config) {
 }
 
 func TestStartOptions(t *testing.T) {
-	defer common.ShowLogOnFailure(t)()
-
 	insecureTransport := server.NewConfiguration().TransportConfig
 	insecureTransport.AllowInsecure = true
 
@@ -276,10 +274,8 @@ func TestStartOptions(t *testing.T) {
 		},
 	} {
 		t.Run(desc, func(t *testing.T) {
-			var logBuf bytes.Buffer
-			log := logging.NewCombinedLogger(t.Name(), &logBuf).
-				WithLogLevel(logging.LogLevelDebug)
-			defer showBufOnFailure(t, logBuf)
+			log, buf := logging.NewTestLogger(t.Name())
+			defer common.ShowBufferOnFailure(t, buf)
 
 			var gotConfig *server.Configuration
 			var opts mainOpts

commit c790bc00a0a6318f02a5d2b6e0648a37cbb97f00

DAOS-3594 control: Remove log calls from server.ext
Fixes a nil pointer dereference crash caused by the ext struct
having a nil logger instance.

This regression was introduced by 539f2d0, which did away with
the global logger. It added a log member to the ext struct, but
did not plumb in a way to set that member via the Configuration
constructor. Unfortunately, this was not detected by unit tests
because there is no coverage of the ext struct.

Also removes unused methods on the ext struct, another step along
the way toward removing it entirely.

Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

src/control/common/file_utils.go
@@ -34,8 +34,6 @@ import (
 
 	"github.com/pkg/errors"
 	yaml "gopkg.in/yaml.v2"
-
-	"github.com/daos-stack/daos/src/control/logging"
 )
 
 const (
@@ -232,9 +230,7 @@ func SyncDir(path string) (err error) {
 }
 
 // Run executes command in os and builds useful error message.
-func Run(log logging.Logger, cmd string) error {
-	log.Debugf("exec '%s'\n", cmd)
-
+func Run(cmd string) error {
 	// executing as subshell enables pipes in cmd string
 	out, err := exec.Command("sh", "-c", cmd).CombinedOutput()
 	if err != nil {

src/control/server/external.go
@@ -23,22 +23,15 @@
 
 package server
 
-//#include <unistd.h>
-//#include <errno.h>
-import "C"
-
 import (
 	"fmt"
 	"os"
 	"os/user"
 	"path/filepath"
-	"strings"
-	"syscall"
 
 	"github.com/pkg/errors"
 
 	"github.com/daos-stack/daos/src/control/common"
-	"github.com/daos-stack/daos/src/control/logging"
 )
 
 const (
@@ -54,15 +47,6 @@ const (
 
 // External interface provides methods to support various os operations.
 type External interface {
-	runCommand(string) error
-	writeToFile(string, string) error
-	createEmpty(string, int64) error
-	mount(string, string, string, uintptr, string) error
-	isMountPoint(string) (bool, error)
-	unmount(string) error
-	mkdir(string) error
-	remove(string) error
-	exists(string) (bool, error)
 	getAbsInstallPath(string) (string, error)
 	lookupUser(string) (*user.User, error)
 	lookupGroup(string) (*user.Group, error)
@@ -84,160 +68,11 @@ func errPermsAnnotate(err error) (e error) {
 }
 
 type ext struct {
-	log     logging.Logger
 	history []string
 }
 
 func (e *ext) getHistory() []string {
 	return e.history
-}
-
-// runCommand executes command in subshell (to allow redirection) and returns
-// error result.
-func (e *ext) runCommand(cmd string) error {
-	e.history = append(e.history, fmt.Sprintf(msgCmd, cmd))
-
-	return common.Run(e.log, cmd)
-}
-
-// writeToFile wraps around common.WriteString and writes input string to given
-// file path.
-func (e *ext) writeToFile(contents string, path string) error {
-	return common.WriteString(path, contents)
-}
-
-// createEmpty creates a file (if it doesn't exist) of specified size in bytes
-// at the given path.
-// If Fallocate not supported by kernel or backing fs, fall back to Truncate.
-func (e *ext) createEmpty(path string, size int64) error {
-	if !filepath.IsAbs(path) {
-		return errors.Errorf("please specify absolute path (%s)", path)
-	}
-
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
-		return err
-	}
-
-	file, err := common.TruncFile(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	if err := syscall.Fallocate(int(file.Fd()), 0, 0, size); err != nil {
-		errno, ok := err.(syscall.Errno)
-		if ok && (errno == syscall.ENOSYS || errno == syscall.EOPNOTSUPP) {
-			e.log.Debugf(
-				"Warning: Fallocate not supported, attempting Truncate: %s", errno)
-
-			if err := file.Truncate(size); err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-// NOTE: requires elevated privileges
-func (e *ext) mount(
-	dev string, mount string, mntType string, flags uintptr, opts string) error {
-
-	op := fmt.Sprintf(msgMount, dev, mount, mntType, fmt.Sprint(flags), opts)
-
-	e.log.Debug(op)
-	e.history = append(e.history, op)
-
-	if flags == 0 {
-		flags = uintptr(syscall.MS_NOATIME | syscall.MS_SILENT)
-		flags |= syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_NOSUID
-	}
-
-	if err := syscall.Mount(dev, mount, mntType, flags, opts); err != nil {
-		return errPermsAnnotate(os.NewSyscallError("mount", err))
-	}
-	return nil
-}
-
-// isMountPoint checks if path is likely to be a mount point.straiowhotenoul
-func (e *ext) isMountPoint(path string) (bool, error) {
-	e.log.Debugf(msgIsMountPoint, path)
-	e.history = append(e.history, fmt.Sprintf(msgIsMountPoint, path))
-
-	pStat, err := os.Stat(path)
-	if err != nil {
-		return false, err
-	}
-
-	rStat, err := os.Stat(filepath.Dir(strings.TrimSuffix(path, "/")))
-	if err != nil {
-		return false, err
-	}
-
-	if pStat.Sys().(*syscall.Stat_t).Dev == rStat.Sys().(*syscall.Stat_t).Dev {
-		return false, nil
-	}
-
-	// if root dir has different parent device than path then probably a mountpoint
-	return true, nil
-}
-
-// NOTE: requires elevated privileges, lazy unmount, mntpoint may not be
-//       available immediately after
-func (e *ext) unmount(path string) error {
-	e.log.Debugf(msgUnmount, path)
-	e.history = append(e.history, fmt.Sprintf(msgUnmount, path))
-
-	// ignore NOENT errors, treat as success
-	if err := syscall.Unmount(
-		path, syscall.MNT_DETACH); err != nil && !os.IsNotExist(err) {
-
-		// when mntpoint exists but is unmounted, get EINVAL
-		e, ok := err.(syscall.Errno)
-		if ok && e == syscall.EINVAL {
-			return nil
-		}
-
-		return errPermsAnnotate(os.NewSyscallError("umount", err))
-	}
-	return nil
-}
-
-// NOTE: may require elevated privileges
-func (e *ext) mkdir(path string) error {
-	e.log.Debugf(msgMkdir, path)
-	e.history = append(e.history, fmt.Sprintf(msgMkdir, path))
-
-	if err := os.MkdirAll(path, 0777); err != nil {
-		return errPermsAnnotate(errors.WithMessage(err, "mkdir"))
-	}
-	return nil
-}
-
-// NOTE: may require elevated privileges
-func (e *ext) remove(path string) error {
-	e.log.Debugf(msgRemove, path)
-	e.history = append(e.history, fmt.Sprintf(msgRemove, path))
-
-	// ignore NOENT errors, treat as success
-	if err := os.RemoveAll(path); err != nil && !os.IsNotExist(err) {
-		return errPermsAnnotate(errors.WithMessage(err, "remove"))
-	}
-	return nil
-}
-
-func (e *ext) exists(path string) (bool, error) {
-	e.log.Debugf(msgExists, path)
-	e.history = append(e.history, fmt.Sprintf(msgExists, path))
-
-	if _, err := os.Stat(path); err == nil {
-		return true, nil
-	} else if !os.IsNotExist(err) {
-		return false, errPermsAnnotate(
-			errors.WithMessage(err, "os stat"))
-	}
-
-	return false, nil
 }
 
 func (e *ext) getAbsInstallPath(path string) (string, error) {
@@ -259,7 +94,6 @@ func (e *ext) listGroups(usr *user.User) ([]string, error) {
 func (e *ext) chownR(root string, uid int, gid int) error {
 	op := fmt.Sprintf(msgChownR, root, uid, gid)
 
-	e.log.Debug(op)
 	e.history = append(e.history, op)
 
 	return filepath.Walk(root, func(name string, info os.FileInfo, err error) error {