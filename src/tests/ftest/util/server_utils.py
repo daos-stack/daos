a654131765f3570c738f415fcd4576231a19b7e9
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2019-10-31 22:47:54 -0400

    Change attach_info_path to a subdirectory and change permissions of folder and file within the subdirectory.
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/util/server_utils.py
+++ src/tests/ftest/util/server_utils.py
@@ -436,9 +436,20 @@ class ServerManager(ExecutableCommand):
                 cmd_touch_log = "touch {}".format(lfile)
                 pcmd(self._hosts, cmd_touch_log, False)
 
-            # Change ownership of attach info file
-            chmod_attach = "chmod 777 -R {}".format(self.attach.value)
-            pcmd(self._hosts, chmod_attach, False)
+            # Create sub directory on top of attach_info_path directory
+            chmod_attach = [
+                "mkdir {}/attach_dir".format(self.attach.value),
+                "chmod 777 {}/attach_dir".format(self.attach.value),
+            ]
+            pcmd(self._hosts, "; ".join(chmod_attach), False)
+
+            # Set server -a attach_info_path value to the sub directory
+            self.runner.job.action_command.attach.value = \
+                "{}/attach_dir".format(self.attach.value)
+
+            # Set server environment info_attach_path
+            os.environ["CRT_ATTACH_INFO_PATH"] = \
+                self.runner.job.action_command.attach.value
 
         try:
             self.run()
@@ -468,7 +479,7 @@ class ServerManager(ExecutableCommand):
 
             # Change ownership of attach info file
             chmod_cmds = "sudo chmod 777 {}/daos_server.attach_info_tmp".format(
-                self.attach.value)
+                self.runner.job.action_command.attach.value)
             pcmd(self._hosts, chmod_cmds, False)
 
         return True
