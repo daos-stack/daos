9ee1d77bd733d5e00c1f79e0e09941658df0ca07
Samir Raval <samir.raval@intel.com>
2019-11-07 21:37:25 +0000

    Updating the code based on review.
    Signed-off-by: Samir Raval <samir.raval@intel.com>

--- src/tests/ftest/util/daos_io_conf.py
+++ src/tests/ftest/util/daos_io_conf.py
@@ -22,9 +22,11 @@
   portions thereof marked with this legend must also reproduce the markings.
 """
 import os
+from avocado.utils import process
 
 from apricot import TestWithServers
-from command_utils import ExecutableCommand
+from command_utils import ExecutableCommand, CommandFailure, FormattedParameter
+from command_utils import BasicParameter
 from test_utils import TestPool
 
 class IoConfGen(ExecutableCommand):
@@ -42,9 +44,46 @@ class IoConfGen(ExecutableCommand):
             path (str, optional): path to location of command binary file.
                 Defaults to ""
         """
-        super(IoConfGen, self).__init__("", path)
+        super(IoConfGen, self).__init__("/run/run_gen_io_conf/*",
+                                        "daos_gen_io_conf")
         self.verbose = True
         self.env = env
+        self.ranks = FormattedParameter("-g {}")
+        self.targets = FormattedParameter("-t {}")
+        self.obj_num = FormattedParameter("-o {}")
+        self.akeys = FormattedParameter("-a {}")
+        self.dkeys = FormattedParameter("-d {}")
+        self.re_size = FormattedParameter("-s {}")
+        self.obj_class = FormattedParameter("-O {}")
+        self.filename = BasicParameter(None, "testfile")
+
+    def run_conf(self):
+        """Run the daos_run_io_conf command as a foreground process.
+
+        Raises:
+            CommandFailure: if there is an error running the command
+
+        """
+        command = " ".join([os.path.join(self._path, "daos_run_io_conf"),
+                            self.filename.value])
+        kwargs = {
+            "cmd": command,
+            "timeout": self.timeout,
+            "verbose": self.verbose,
+            "allow_output_check": "combined",
+            "shell": True,
+            "env": self.env,
+            "sudo": self.sudo,
+        }
+        try:
+            # Block until the command is complete or times out
+            return process.run(**kwargs)
+
+        except process.CmdError as error:
+            # Command failed or possibly timed out
+            msg = "Error occurred running '{}': {}".format(command, error)
+            self.log.error(msg)
+            raise CommandFailure(msg)
 
 class IoConfTestBase(TestWithServers):
     """Base rebuild test class.
@@ -61,69 +100,13 @@ class IoConfTestBase(TestWithServers):
         self.pool = TestPool(self.context, self.log)
         self.pool.get_params(self)
 
-    def run_gen_io_conf(self, filename="testfile"):
-        """
-        Generate the test data set file based on parameter from yaml file
-
-        Args:
-            filename (string): Filename (with/without path) for
-            create the data set.
-        """
-        rank_size = self.params.get("ranks",
-                                    "/run/daos_io_conf_run/no_of_ranks/*/")
-        target_size = self.params.get("targets",
-                                      "/run/daos_io_conf_run/tgts/*/")
-        obj_num = self.params.get("obj_num",
-                                   "/run/daos_io_conf_run/no_of_objects/*/")
-        dkey_num = self.params.get("dkeys",
-                                   "/run/daos_io_conf_run/no_of_dkeys/*/")
-        akey_num = self.params.get("akeys",
-                                   "/run/daos_io_conf_run/no_of_akeys/*/")
-        rec_size = self.params.get("re_size",
-                                   "/run/daos_io_conf_run/record_size/*/")
-        obj_class = self.params.get("obj_class",
-                                   "/run/daos_io_conf_run/object_class/*/")
-
-        #Get the daos_gen_io_conf bin path.
-        daos_gen_io_conf_bin = (os.path.join(self.prefix,
-                                             "bin/daos_gen_io_conf"))
-        cmd = [daos_gen_io_conf_bin,
-               rank_size,
-               target_size,
-               obj_num,
-               dkey_num,
-               akey_num,
-               rec_size,
-               obj_class,
-               filename]
-
-        #Run daos_gen_io_conf
-        #daos_gen_io_conf -g <rank_size> -t <tgt_size>-o [obj_num]
-        #-d [dkey_num] -a [akey_num] -s [rec_size]-O obj_class <file_name>
-        io_conf_gen = IoConfGen(" ".join(cmd))
-        io_conf_gen.run()
-
-    def run_io_conf(self, filename="testfile"):
-        """ Run daos_run_io_conf <file_name> """
-        #Get the daos_run_io_conf bin path.
-        daos_run_io_conf_bin = (os.path.join(self.prefix,
-                                             "bin/daos_run_io_conf"))
-        cmd = [daos_run_io_conf_bin,
-               filename]
-
-        #Run daos_run_io_conf
-        io_conf = IoConfGen(" ".join(cmd),
-                                env={"POOL_SCM_SIZE":"{}"
-                                     .format(self.pool.scm_size)})
-        io_conf.run()
-
     def execute_io_conf_run_test(self):
         """
         Execute the rebuild test steps.
         """
-        #Setup the pool
         self.setup_test_pool()
-        #create test file using daos_gen_io_conf
-        self.run_gen_io_conf()
+        io_conf = IoConfGen()
+        io_conf.get_params(self)
+        io_conf.run()
         #Run test file using daos_run_io_conf
-        self.run_io_conf()
+        io_conf.run_conf()
