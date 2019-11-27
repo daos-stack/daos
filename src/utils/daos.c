c0933938ef74c37d8eda8db9bd087de1684d9d44
Bruno Faccini <bruno.faccini@intel.com>
2019-11-27 09:51:12 +0100

    DAOS-3823 utils: display version in daos cmd help
    Add version display to daos command usage/help stuff.
    
    Change-Id: I41fc4b288b6884667a281a99d2073c5a72d92eba
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>

--- src/utils/daos.c
+++ src/utils/daos.c
@@ -795,6 +795,8 @@ help_hdlr(struct cmd_args_s *ap)
 
 	stream = (ap->ostream != NULL) ? ap->ostream : stdout;
 
+	fprintf(stream, "daos admin command (v%s)\n, DAOS_VERSION);
+
 	fprintf(stream,
 "usage: daos RESOURCE COMMAND [OPTIONS]\n"
 "resources:\n"
