eb575d2a97161d9ccb9c61ad3722d087306a7643
Ashley Pittman <ashley.m.pittman@intel.com>
2019-12-04 12:14:01 +0000

    Add a basic readahead implementation.

--- src/client/dfuse/ops/read.c
+++ src/client/dfuse/ops/read.c
@@ -29,9 +29,11 @@ dfuse_cb_read(fuse_req_t req, fuse_ino_t ino, size_t len, off_t position,
 	      struct fuse_file_info *fi)
 {
 	struct dfuse_obj_hdl		*oh = (struct dfuse_obj_hdl *)fi->fh;
+	struct dfuse_projection_info	*fs_handle = fuse_req_userdata(req);
 	const struct fuse_ctx		*fc = fuse_req_ctx(req);
 	d_iov_t				iov = {};
 	d_sg_list_t			sgl = {};
+	struct fuse_bufvec		fb = {};
 	daos_size_t			size;
 	void				*buff;
 	int				rc;
@@ -62,9 +64,35 @@ dfuse_cb_read(fuse_req_t req, fuse_ino_t ino, size_t len, off_t position,
 	position += len;
 	len = 1024 * 128;
 
+	/* Note that from this line onwards there is a race condition
+	 * against close, now that this callback has replied the
+	 * kernel is at liberty to close the file, and oh might be
+	 * released by another thread as this readahead code is
+	 * being executed.
+	 */
 	if (len + position - 1 > oh->doh_ie->ie_stat.st_size)
 		return;
 
 	DFUSE_TRA_INFO(oh, "Will try readahead");
 
+	D_ALLOC(buff, len);
+	if (!buff)
+		return;
+
+	fb.count = 1;
+	fb.buf[0].mem = buff;
+	fb.buf[0].size = len;
+
+	sgl.sg_nr = 1;
+	d_iov_set(&iov, (void *)buff, len);
+	sgl.sg_iovs = &iov;
+
+	rc = dfs_read(oh->doh_dfs, oh->doh_obj, &sgl, position, &size, NULL);
+	if (rc != 0 || size != len)
+		return;
+
+	rc = fuse_lowlevel_notify_store(fs_handle->dpi_info->di_session, ino,
+					position, &fb, 0);
+	DFUSE_TRA_INFO(oh, "notfiy_store returned %d", rc);
+	D_FREE(buff);
 }
