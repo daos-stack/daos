7430da98c630f1b8dc97174ac1d75c05607f8386
Saurabh Tandan <saurabh.tandan@intel.com>
2019-12-06 21:10:44 +0000

    Removed one file which got accidentally pushed, added unique test tag for each test case
    Change-Id: I7a1588f9370140040422a8d3f3b3917ac88f0a14
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/io/ior_large.py
+++ src/tests/ftest/io/ior_large.py
@@ -41,7 +41,7 @@ class IorLarge(IorTestBase):
             Different combinations of 1/64/128 Clients and
             1K/4K/32K/128K/512K/1M transfersize.
 
-        :avocado: tags=all,daosio,iorlarge_sequential
+        :avocado: tags=all,daosio,iorlarge_sequential,iorlarge
         """
         ior_flags = self.params.get("F", "/run/ior/iorflags/sequential/")
         self.ior_cmd.flags.update(ior_flags)
@@ -57,7 +57,7 @@ class IorLarge(IorTestBase):
             Different combinations of 1/64/128 Clients and
             1K/4K/32K/128K/512K/1M transfersize.
 
-        :avocado: tags=all,daosio,iorlarge_random
+        :avocado: tags=all,daosio,iorlarge_random,iorlarge
         """
         ior_flags = self.params.get("F", "/run/ior/iorflags/random/")
         self.ior_cmd.flags.update(ior_flags)
@@ -73,7 +73,7 @@ class IorLarge(IorTestBase):
             Different combinations of 1/64/128 Clients and
             1K/4K/32K/128K/512K/1M transfersize.
 
-        :avocado: tags=all,daosio,iorlarge_sequential
+        :avocado: tags=all,daosio,iorlarge_fpp,iorlarge
         """
         ior_flags = self.params.get("F", "/run/ior/iorflags/fpp/")
         self.ior_cmd.flags.update(ior_flags)
