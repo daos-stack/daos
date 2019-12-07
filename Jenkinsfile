daos-stack/daos

commit b8623792820306eeabd9717828847ec4e5fccd63

DAOS-623 build: Move mpich back to default branch (#1535)
Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

Jenkinsfile
@@ -49,7 +49,7 @@ def component_repos = ""
 def daos_repo = "daos@${env.BRANCH_NAME}:${env.BUILD_NUMBER}"
 def el7_daos_repos = el7_component_repos + ' ' + component_repos + ' ' + daos_repo
 def sle12_daos_repos = sle12_component_repos + ' ' + component_repos + ' ' + daos_repo
-def ior_repos = "mpich@PR-20 ior-hpc@PR-48"
+def ior_repos = "mpich@daos-adio_rpm ior-hpc@PR-48"
 
 def rpm_test_pre = '''if git show -s --format=%B | grep "^Skip-test: true"; then
                           exit 0

commit 0bc974d7273a141b0122cd26c8cf87c243aba79e

DAOS-623 build: Fix mpich job name
Commit b862379 introduced an error in the mpich job name. This
commit fixes it. (s/daos-adio_rpm/daos_adio-rpm/)

Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

Jenkinsfile
@@ -49,7 +49,7 @@ def component_repos = ""
 def daos_repo = "daos@${env.BRANCH_NAME}:${env.BUILD_NUMBER}"
 def el7_daos_repos = el7_component_repos + ' ' + component_repos + ' ' + daos_repo
 def sle12_daos_repos = sle12_component_repos + ' ' + component_repos + ' ' + daos_repo
-def ior_repos = "mpich@daos-adio_rpm ior-hpc@PR-48"
+def ior_repos = "mpich@daos_adio-rpm ior-hpc@PR-48"
 
 def rpm_test_pre = '''if git show -s --format=%B | grep "^Skip-test: true"; then
                           exit 0