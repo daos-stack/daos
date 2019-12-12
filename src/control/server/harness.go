daos-stack/daos

commit 8e8aef73df82f7e8b36dae4ffda5a61f49988bd5

DAOS-2881 control: Fix MS bootstrap issue with multiple servers (#1560)
The MS service should be started before moving on to other
instances.

Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

src/control/server/harness.go
@@ -216,10 +216,14 @@ func (h *IOServerHarness) Start(parent context.Context) error {
 				return err
 			}
 		}
-	}
 
-	if err := h.StartManagementService(ctx); err != nil {
-		return errors.Wrap(err, "failed to start management service")
+		// If this instance is a MS replica, start it before
+		// moving on so that other instances can join.
+		if instance.IsMSReplica() {
+			if err := instance.StartManagementService(); err != nil {
+				return errors.Wrap(err, "failed to start management service")
+			}
+		}
 	}
 
 	// now monitor them

commit 0c9f0ac3f4b75d5223337330db3de58a9cf4ba5d

DAOS-2881 control: Correct harness startup sequence
Commit 8e8aef7 introduced a regression in the instance startup
sequence. Non-MS instances shouldn't need to call
StartManagementService(), so a new method has been added to allow
them to load modules and start up normally.

Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

src/control/server/harness.go
@@ -224,6 +224,10 @@ func (h *IOServerHarness) Start(parent context.Context) error {
 				return errors.Wrap(err, "failed to start management service")
 			}
 		}
+
+		if err := instance.LoadModules(); err != nil {
+			return errors.Wrap(err, "failed to load I/O server modules")
+		}
 	}
 
 	// now monitor them

src/control/server/instance.go
@@ -359,7 +359,11 @@ func (srv *IOServerInstance) StartManagementService() error {
 		}
 	}
 
-	// Notify the I/O server that it may set up its server modules now.
+	return nil
+}
+
+// LoadModules initiates the I/O server startup sequence.
+func (srv *IOServerInstance) LoadModules() error {
 	return srv.callSetUp()
 }
 