4611be03bb2bdfbc58de194ee0fb60f2160067a5
Fan Yong <fan.yong@intel.com>
2019-12-23 11:21:03 +0800

    DAOS-2429 dtx: misc fixes for DTX logic
    Include the followings:
    
    1. Incarnation log may return -DER_AGAIN when hit non-committed
       DTX with the same epoch. Under such case, the DTX leader will
       retry with newer epoch (HLC).
    
    2. Synchronously commit the DTX if it creates new object, that
       can reduce potential conflict with subsequent modifications
       against the same object.
    
    3. Fix vos_test for DTX CoS.
    
    4. Some debug message and code adjustment.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/include/daos_srv/dtx_srv.h
+++ src/include/daos_srv/dtx_srv.h
@@ -79,7 +79,9 @@ struct dtx_handle {
 					 /* dti_cos has been committed. */
 					 dth_dti_cos_done:1,
 					 /* XXX: touch ilog entry. */
-					 dth_has_ilog:1;
+					 dth_has_ilog:1,
+					 /* epoch conflict, need to renew. */
+					 dth_renew:1;
 	/* The count the DTXs in the dth_dti_cos array. */
 	uint32_t			 dth_dti_cos_count;
 	/* The array of the DTXs for Commit on Share (conflcit). */
