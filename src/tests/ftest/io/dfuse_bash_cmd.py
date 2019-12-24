89b945ecb2008058e9ddbec7e9601b593e3f68f7
Saurabh Tandan <saurabh.tandan@intel.com>
2019-12-24 20:50:49 +0000

    Getting rid of unwanted ascii characters in the string.
    Test-tag: pr,-hw bashcmd
    
    Change-Id: Ifd070ee07e1db8de318cc69a9772e19ed1a3ee2a
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/io/dfuse_bash_cmd.py
+++ src/tests/ftest/io/dfuse_bash_cmd.py
@@ -168,10 +168,13 @@ class BashCmd(TestWithServers):
             # perform test for multiple containers.
             for count in range(self.cont_count):
                 self.start_dfuse(count)
-                abs_dir_path = str(self.dfuse.mount_dir.value + "/"
-                                   + self.dir_name)
-                abs_file_path1 = str(abs_dir_path + "/" + self.file_name1)
-                abs_file_path2 = str(abs_dir_path + "/" + self.file_name2)
+                abs_dir_path = (
+                    self.dfuse.mount_dir.value + "/" + self.dir_name).\
+                    decode("utf-8").encode("ascii", "ignore")
+                abs_file_path1 = (abs_dir_path + "/" + self.file_name1).\
+                                 decode("utf-8").encode("ascii", "ignore")
+                abs_file_path2 = (abs_dir_path + "/" + self.file_name2).\
+                                 decode("utf-8").encode("ascii", "ignore")
                 # check if the dir exists.
                 dir_exists, _ = general_utils.check_file_exists(
                     self.hostlist_clients[:-1], abs_dir_path, directory=True)
