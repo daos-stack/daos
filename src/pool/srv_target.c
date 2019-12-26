32e917259c177e79885187fdab313e8a63368e54
Peter Fetros <46941515+PetFet@users.noreply.github.com>
2019-12-25 19:31:19 -0800

    DAOS-3876 Container: Added Redundancy Factor Check to Open (#1575)
    Added the necessary code to check redundancy factor requirements
    on container open and ensure the container is usable based on it's
    redundancy factor and level.
    
    This includes:
    - Add array to pool map to track failed domains for lookup.
    - Add redundancy factor 0 for 0 failure toleration.
    - Add function to recursively scan the pool_map for downstream failures.
    - Add scan to pool map update.
    - Add redundancy Factor/Level check to container open.
    
    Signed-off-by: Peter Fetros <peter.fetros@intel.com>

--- src/pool/srv_target.c
+++ src/pool/srv_target.c
@@ -910,6 +910,7 @@ ds_pool_tgt_map_update(struct ds_pool *pool, struct pool_buf *buf,
 {
 	struct pool_map *map = NULL;
 	int		rc = 0;
+	bool		null_maps;
 
 	if (buf != NULL) {
 		rc = pool_map_create(buf, map_version, &map);
@@ -920,10 +921,12 @@ ds_pool_tgt_map_update(struct ds_pool *pool, struct pool_buf *buf,
 		}
 	}
 
+	null_maps = (map == NULL || pool->sp_map == NULL);
+
 	ABT_rwlock_wrlock(pool->sp_lock);
 	if (pool->sp_map_version < map_version ||
-	    (pool->sp_map_version == map_version &&
-	     (map != NULL && pool->sp_map == NULL))) {
+	   ((!null_maps) && (pool->sp_map_version == map_version ||
+	    pool_map_get_version(pool->sp_map) < map_version))) {
 		if (map != NULL) {
 			struct pool_map *tmp = pool->sp_map;
 
@@ -943,43 +946,32 @@ ds_pool_tgt_map_update(struct ds_pool *pool, struct pool_buf *buf,
 				D_GOTO(out, rc);
 			}
 
+			rc = pool_map_update_failed_cnt(map);
+			if (rc != 0) {
+				ABT_rwlock_unlock(pool->sp_lock);
+				D_ERROR(DF_UUID": failed fail-cnt update pl_map"
+					": %d\n", DP_UUID(pool->sp_uuid), rc);
+				D_GOTO(out, rc);
+			}
+
+			/* drop the stale map */
 			pool->sp_map = map;
 			map = tmp;
 		}
 
-		D_DEBUG(DF_DSMS, DF_UUID
-			": changed cached map version: %u -> %u pool %p"
-			" map %p map_ver %u\n", DP_UUID(pool->sp_uuid),
-			pool->sp_map_version, map_version, pool, pool->sp_map,
-			pool_map_get_version(pool->sp_map));
+		if (pool->sp_map_version < map_version ||
+		   (pool->sp_map_version == map_version && !null_maps)) {
+			D_DEBUG(DF_DSMS, DF_UUID
+				": changed cached map version: %u -> %u pool %p"
+				" map %p map_ver %u\n", DP_UUID(pool->sp_uuid),
+				pool->sp_map_version, map_version,
+				pool, pool->sp_map,
+				pool_map_get_version(pool->sp_map));
 
-		pool->sp_map_version = map_version;
-		rc = dss_task_collective(update_child_map, pool, 0);
-		D_ASSERT(rc == 0);
-	} else if (pool->sp_map != NULL &&
-		   pool_map_get_version(pool->sp_map) < map_version &&
-		   map != NULL) {
-		struct pool_map *tmp = pool->sp_map;
-
-		rc = update_pool_group(pool, map);
-		if (rc != 0) {
-			ABT_rwlock_unlock(pool->sp_lock);
-			goto out;
+			pool->sp_map_version = map_version;
+			rc = dss_task_collective(update_child_map, pool, 0);
+			D_ASSERT(rc == 0);
 		}
-
-		rc = pl_map_update(pool->sp_uuid, map,
-				   pool->sp_map != NULL ? false : true,
-				   DEFAULT_PL_TYPE);
-		if (rc != 0) {
-			ABT_rwlock_unlock(pool->sp_lock);
-			D_ERROR(DF_UUID": failed to update pl_map: %d\n",
-				DP_UUID(pool->sp_uuid), rc);
-			D_GOTO(out, rc);
-		}
-
-		/* drop the stale map */
-		pool->sp_map = map;
-		map = tmp;
 	} else {
 		D_WARN("Ignore old map version: cur=%u, input=%u pool %p\n",
 		       pool->sp_map_version, map_version, pool);
