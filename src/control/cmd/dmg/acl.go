0c381b2c70dc66ffb8a582397ce1e1ad19a219ce
Kris Jacque <kristin.jacque@intel.com>
2020-01-09 12:46:58 -0700

    DAOS-3536 dmg: Add verbose mode to pool get-ACL (#1664)
    The verbose option adds comments (starting with '#') that
    expand the fields of each ACL entry into more descriptive
    strings.
    
    Signed-off-by: Kris Jacque <kristin.jacque@intel.com>

--- src/control/cmd/dmg/acl.go
+++ src/control/cmd/dmg/acl.go
@@ -74,7 +74,7 @@ func parseACL(reader io.Reader) (*client.AccessControlList, error) {
 }
 
 // formatACL converts the AccessControlList to a human-readable string.
-func formatACL(acl *client.AccessControlList) string {
+func formatACL(acl *client.AccessControlList, verbose bool) string {
 	var builder strings.Builder
 
 	if acl.HasOwner() {
@@ -92,8 +92,115 @@ func formatACL(acl *client.AccessControlList) string {
 	}
 
 	for _, ace := range acl.Entries {
+		if verbose {
+			fmt.Fprintf(&builder, "# %s\n", getVerboseACE(ace))
+		}
 		fmt.Fprintf(&builder, "%s\n", ace)
 	}
 
 	return builder.String()
+}
+
+// formatACLDefault formats the AccessControlList in non-verbose mode.
+func formatACLDefault(acl *client.AccessControlList) string {
+	return formatACL(acl, false)
+}
+
+func getVerboseACE(shortACE string) string {
+	if shortACE == "" {
+		return ""
+	}
+
+	const (
+		// Field indices
+		ACETypes = iota
+		ACEFlags
+		ACEIdentity
+		ACEPerms
+		ACENumFields // Must be last
+	)
+
+	fields := strings.Split(shortACE, ":")
+	if len(fields) != ACENumFields {
+		return "invalid ACE"
+	}
+
+	var b strings.Builder
+
+	fmt.Fprintf(&b, "%s:", getVerboseType(fields[ACETypes]))
+
+	if fields[ACEFlags] != "" {
+		b.WriteString(getVerboseFlags(fields[ACEFlags]))
+	}
+	b.WriteString(":")
+
+	fmt.Fprintf(&b, "%s:", getVerboseIdentity(fields[ACEIdentity]))
+
+	b.WriteString(getVerbosePermissions(fields[ACEPerms]))
+
+	return b.String()
+}
+
+func getVerboseType(field string) string {
+	types := map[string]string{
+		"A": "Allow",
+	}
+
+	return getVerboseField(field, types)
+}
+
+func getVerboseFlags(field string) string {
+	flags := map[string]string{
+		"G": "Group",
+	}
+
+	return getVerboseField(field, flags)
+}
+
+func getVerboseIdentity(field string) string {
+	specialIDs := map[string]string{
+		"OWNER@":    "Owner",
+		"GROUP@":    "Owner-Group",
+		"EVERYONE@": "Everyone",
+	}
+
+	identity := field
+	if identity == "" {
+		identity = "None"
+	} else if v, ok := specialIDs[identity]; ok {
+		identity = v
+	}
+
+	return identity
+}
+
+func getVerbosePermissions(field string) string {
+	perms := map[string]string{
+		"r": "Read",
+		"w": "Write",
+	}
+
+	return getVerboseField(field, perms)
+}
+
+func getVerboseField(field string, verbose map[string]string) string {
+	if field == "" {
+		return "None"
+	}
+
+	var b strings.Builder
+	for i, c := range field {
+		if i != 0 {
+			b.WriteString("/")
+		}
+		var verboseVal string
+		if v, ok := verbose[string(c)]; ok {
+			verboseVal = v
+		} else {
+			verboseVal = "Unknown"
+		}
+		b.WriteString(verboseVal)
+	}
+
+	return b.String()
 }
