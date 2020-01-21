dee2f4af7709f0581cf2051c7b507310f750ea2e
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2020-01-21 14:59:23 -0500

    Moved from using local variable to using self.server_managers. Modified some test cases wih feedback.
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/server/daos_server_config.py
+++ src/tests/ftest/server/daos_server_config.py
@@ -50,16 +50,18 @@ class DaosServerConfigTest(TestWithServers):
         :avocado: tags=all,tiny,control,pr,server_start,basic
         """
         # Setup the servers
-        server = ServerManager(self.bin, os.path.join(self.ompi_prefix, "bin"))
-        server.get_params(self)
-        server.hosts = (
+        self.server_managers.append(ServerManager(
+            self.bin,
+            os.path.join(self.ompi_prefix, "bin")))
+        self.server_managers[-1].get_params(self)
+        self.server_managers[-1].hosts = (
             self.hostlist_servers, self.workdir, self.hostfile_servers_slots)
 
         # Get the input to verify
         c_val = self.params.get("config_val", "/run/server_config_val/*/")
 
         # Identify the attribute and modify its value to test value
-        yml_params = server.runner.job.yaml_params
+        yml_params = self.server_managers[-1].runner.job.yaml_params
         if c_val[3] == "gen":
             getattr(yml_params, c_val[0]).value = c_val[1]
         elif c_val[3] == "server":
@@ -68,7 +70,7 @@ class DaosServerConfigTest(TestWithServers):
         self.log.info("Starting server changing %s with %s", c_val[0], c_val[1])
         yamlfile = os.path.join(self.tmp, "daos_avocado_test.yaml")
         try:
-            server.start(yamlfile)
+            self.server_managers[-1].start(yamlfile)
             exception = None
         except ServerFailed as err:
             exception = err
@@ -80,9 +82,3 @@ class DaosServerConfigTest(TestWithServers):
         elif c_val[2] == "PASS" and exception is not None:
             self.log.error("Server was expected to start")
             self.fail("{}".format(exception))
-
-        # Stop servers
-        try:
-            server.stop()
-        except ServerFailed as err:
-            self.fail("Error stopping the server: {}".format(err))
