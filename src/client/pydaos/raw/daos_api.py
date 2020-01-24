153d407d8ccb9a33ed606c46dcf3bd2b9209fcb2
Samir Raval <samir.raval@intel.com>
2020-01-24 01:30:09 +0000

    Pylint Fix.
    Signed-off-by: Samir Raval <samir.raval@intel.com>

--- src/client/pydaos/raw/daos_api.py
+++ src/client/pydaos/raw/daos_api.py
@@ -1463,7 +1463,7 @@ class DaosContainer(object):
         # 3. Server Verfiy
         # 4. Chunk Size Allocation.
         if ((self.cont_input_values.type != "Unknown")
-            and (self.cont_input_values.enable_chksum is False)):
+                and (self.cont_input_values.enable_chksum is False)):
             # Only type like posix, hdf5 defined.
             num_prop = 1
         elif ((self.cont_input_values.type == "Unknown")
