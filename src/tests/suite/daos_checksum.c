a273bb00c5aa23c504c95d414fa68cdcb479a6df
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-01-24 23:33:06 -0500

    DAOS-3999: Adding delay for VM testing. Skip-run_test: true Skip-func-hw-test: true
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/suite/daos_checksum.c
+++ src/tests/suite/daos_checksum.c
@@ -305,11 +305,14 @@ test_server_data_corruption(void **state)
 				DAOS_CHECKSUM_SDATA_CORRUPT | DAOS_FAIL_ALWAYS,
 				0, NULL);
 	assert_int_equal(rc, 0);
+	/** Adding delay for functional testing under VM */
+	/** On actual hardware, delay is not needed */
+	sleep(15);
 	/** Perform the update */
 	rc = daos_obj_update(ctx.oh, DAOS_TX_NONE, 0, &ctx.dkey, 1,
 			&ctx.update_iod, &ctx.update_sgl, NULL);
 	assert_int_equal(rc, 0);
-
+	sleep(15);
 	/** Clear the fail injection flag */
 	rc = daos_mgmt_set_params(arg->group, -1, DMG_KEY_FAIL_LOC, 0, 0, NULL);
 	assert_int_equal(rc, 0);
