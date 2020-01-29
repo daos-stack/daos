6c086d680cc1074d7869d29df52adbc57f5e75d7
Ken Cain <kenneth.c.cain@intel.com>
2020-01-29 09:49:16 -0500

    DAOS-3626: md: on startup, cleanup newborn pool SPDK blobs
    With this change, on daos_io_server startup, any newborn pools in
    the process of being created (but not fully created) during a previous
    server run will have any SPDK blobs removed. Before this change,
    only the pool folder in the SCM mountpoint newborns path was cleaned.
    Depending on when the previous server run stopped, the possibility
    exists that SPDK blobs created for this pool were left in NVMe storage.
    
    Newborn pool cleanup also now is performed later in mgmt module setup
    instead of init because SPDK blob cleanup is done as a per-target
    xstream collective operation (xstreams need to be started first).
    
    Signed-off-by: Ken Cain <kenneth.c.cain@intel.com>

--- src/mgmt/srv_internal.h
+++ src/mgmt/srv_internal.h
@@ -128,8 +128,8 @@ int ds_mgmt_dev_state_query(uuid_t uuid, Mgmt__DevStateResp *resp);
 int ds_mgmt_dev_set_faulty(uuid_t uuid, Mgmt__DevStateResp *resp);
 
 /** srv_target.c */
-int ds_mgmt_tgt_init(void);
-void ds_mgmt_tgt_fini(void);
+int ds_mgmt_tgt_setup(void);
+void ds_mgmt_tgt_cleanup(void);
 void ds_mgmt_hdlr_tgt_create(crt_rpc_t *rpc_req);
 void ds_mgmt_hdlr_tgt_destroy(crt_rpc_t *rpc_req);
 int ds_mgmt_tgt_create_aggregator(crt_rpc_t *source, crt_rpc_t *result,
