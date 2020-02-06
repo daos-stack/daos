3e0dbdfe908146f310351e7629298703a7c7cea0
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2020-02-06 10:53:45 -0500

    WIP
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

---
+++ src/tests/ftest/control/super_block_versioning.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+"""
+  (C) Copyright 2020 Intel Corporation.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+  The Government's rights to use, modify, reproduce, release, perform, display,
+  or disclose this software are subject to the terms of the Apache License as
+  provided in Contract No. B609815.
+  Any reproduction of computer software, computer software documentation, or
+  portions thereof marked with this legend must also reproduce the markings.
+"""
+from __future__ import print_function
+
+import os
+import getpass
+
+from dmg_utils import storage_format
+from general_utils import check_file_exists
+from server_utils import ServerManager, ServerFailed
+from apricot import TestWithServers
+
+
+class SuperBlockVersioning(TestWithServers):
+    """Test Class Description:
+    Test to verify that super block data structure is versioned.
+    :avocado: recursive
+    """
+
+    def test_super_block_versioning(self):
+        """
+        JIRA ID: DAOS-2895
+        Test Description: Basic test to verify that superblock file is
+        versioned.
+        :avocado: tags=all,tiny,pr,ds_versioning,basic
+        """
+        # Update hostlist value for dmg command
+        port = self.params.get("port", "/run/server_config/*")
+        h_ports = [
+            "{}:{}".format(host, port) for host in self.hostlist_servers]
+
+        # Run format command under non-root user
+        self.log.info("Performing SCM format")
+        format_res = storage_format(os.path.join(self.prefix, "bin"), h_ports)
+        if format_res is None:
+            self.fail("Failed to format storage")
+
+        # we can use VMs to run this test.
+        # We need to make sure the server starts up with /mnt/daos
+        # Check that the superblock file exists under the scm_mount dir.
+        # If it does exist, we need to make sure that 'version' is specified in
+        #   the file.
+
