2eacdea7b851cf13bb44cf17c3755d536d0c29c7
Yulu Jia <yulu.jia@intel.com>
2020-02-07 14:20:21 -0800

    CART-89 build: apply nameserver patch (#64)
    Add nameserver patch file, apply it to mercury.
    
    Signed-off-by: Yulu Jia <yulu.jia@intel.com>

--- components/__init__.py
+++ components/__init__.py
@@ -148,6 +148,7 @@ def define_mercury(reqs):
                          True)
     reqs.define('mercury',
                 retriever=retriever,
+                patch="$PATCH_PREFIX/c2c262813811c3ede28ee32fdebbffd417a7cb80...b580971d17ad460529a4516e4c79f0aead7b1515.patch",
                 commands=['cmake -DMERCURY_USE_CHECKSUMS=OFF '
                           '-DOPA_LIBRARY=$OPENPA_PREFIX/lib' +
                           check(reqs, 'openpa', '', '64') + '/libopa.a '
