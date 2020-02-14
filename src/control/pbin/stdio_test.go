906817ccdb1818fd3ef426f836df89d6e8283928
Michael MacDonald <mjmac.macdonald@intel.com>
2020-02-14 09:38:35 -0500

    DAOS-4036 control: Always forward privileged requests (#1848)
    Removes the ability to disable request forwarding to the
    privileged helper, as this is not a supportable configuration.
    
    Modifies the daos_server<->daos_admin plumbing to represent
    daos_admin errors as Faults for better error reporting and
    resolution display.
    
    Adds pbin.CheckHelper() to allow early identification of
    installation problems with clear resolution messages.
    
    Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

--- src/control/pbin/stdio_test.go
+++ src/control/pbin/stdio_test.go
@@ -1,5 +1,5 @@
 //
-// (C) Copyright 2019 Intel Corporation.
+// (C) Copyright 2019-2020 Intel Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -59,7 +59,7 @@ func echo() {
 	}
 }
 
-func TestStdioSimpleParentChild(t *testing.T) {
+func TestPbin_StdioSimpleParentChild(t *testing.T) {
 	var errBuf bytes.Buffer
 	defer func() {
 		if t.Failed() {
