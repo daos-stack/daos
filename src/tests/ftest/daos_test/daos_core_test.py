44609e040f4fd4fc06ecd11bedd1047ee0d603e6
Samir Raval <samir.raval@intel.com>
2020-02-15 00:41:04 +0000

    DAOS-3818, Run more daos_test subtests on HW.
    Signed-off-by: Samir Raval <samir.raval@intel.com>

--- src/tests/ftest/daos_test/daos_core_test.py
+++ src/tests/ftest/daos_test/daos_core_test.py
@@ -38,6 +38,6 @@ class DaosCoreTest(DaosCoreBase):
 
         Use Cases: core tests for daos_test
 
-        :avocado: tags=all,pr,medium,daos_test
+        :avocado: tags=all,pr,hw,medium,daos_test
         """
         DaosCoreBase.run_subtest(self)
