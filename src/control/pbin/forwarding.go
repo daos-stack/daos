028cfa463448d51ea5192b5a28ffc443828a67d2
Michael MacDonald <mjmac.macdonald@intel.com>
2020-02-20 14:20:37 -0500

    HACK: Allow forwarding to be disabled
    Apply this patch against master in order to remove a hard requirement
    on forwarding requests to daos_admin. As before,
    DAOS_DISABLE_REQ_FWD=true will disable forwarding to daos_admin.
    
    Please note that this is an unsupported configuration, and future work
    will likely remove all ability for daos_server to operate independently
    of daos_admin.

--- src/control/pbin/forwarding.go
+++ src/control/pbin/forwarding.go
@@ -10,6 +10,7 @@ import (
 	"context"
 	"encoding/json"
 	"os"
+	"strconv"
 
 	"github.com/pkg/errors"
 
@@ -51,6 +52,16 @@ func NewForwarder(log logging.Logger, pbinName string) *Forwarder {
 	fwd := &Forwarder{
 		log:      log,
 		pbinName: pbinName,
+	}
+
+	if val, set := os.LookupEnv(DisableReqFwdEnvVar); set {
+		disabled, err := strconv.ParseBool(val)
+		if err != nil {
+			log.Errorf("%s was set to non-boolean value (%q); not disabling",
+				DisableReqFwdEnvVar, val)
+			return fwd
+		}
+		fwd.Disabled = disabled
 	}
 
 	return fwd
