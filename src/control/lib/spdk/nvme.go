749f3913d2be8a7141b3641e14088466b01c7f5c
Tom Nabarro <tom.nabarro@intel.com>
2020-03-05 19:18:35 +0000

    add library deps
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/lib/spdk/nvme.go
+++ src/control/lib/spdk/nvme.go
@@ -28,8 +28,9 @@ package spdk
 
 /*
 #cgo CFLAGS: -I .
-#cgo LDFLAGS: -lspdk_env_dpdk -lrte_mempool -lrte_mempool_ring -lrte_bus_pci
-#cgo LDFLAGS: -lrte_pci -lrte_ring -lrte_mbuf -lrte_eal -lrte_kvargs -ldl -lnuma
+#cgo LDFLAGS: -lspdk_env_dpdk -lspdk_nvme
+#cgo LDFLAGS: -lrte_mempool -lrte_mempool_ring -lrte_bus_pci -lrte_pci
+#cgo LDFLAGS: -lrte_ring -lrte_mbuf -lrte_eal -lrte_kvargs -ldl -lnuma
 #cgo LDFLAGS: -L . -lnvme_control
 
 #include "stdlib.h"
