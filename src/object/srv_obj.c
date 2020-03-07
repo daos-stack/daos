80b676a88a0b4a514d54f6d3382427e4b213464f
David Quigley <david.quigley@intel.com>
2020-03-07 10:20:09 -0700

    This is the enforcement part of the commit just before this
    Signed-off-by: David Quigley <david.quigley@intel.com>

--- src/object/srv_obj.c
+++ src/object/srv_obj.c
@@ -1045,6 +1045,16 @@ obj_ioc_init(uuid_t pool_uuid, uuid_t coh_uuid, uuid_t cont_uuid, int opc,
 		return rc;
 	}
 
+	// TODO: Needs to be cleaned up maybe pulling all checks into their own
+	// function.
+	if (!obj_is_modification_opc(opc) &&
+	    !ds_sec_cont_can_read_data(coh->sch_sec_capas)) {
+		D_ERROR("cont "DF_UUID" hdl "DF_UUID" sec_capas "DF_U64", "
+			"NO_PERM to read.\n", DP_UUID(cont_uuid),
+			DP_UUID(coh_uuid), coh->sch_sec_capas);
+		D_GOTO(failed, rc = -DER_NO_PERM);
+	}
+
 	if (obj_is_modification_opc(opc) &&
 	    !ds_sec_cont_can_write_data(coh->sch_sec_capas)) {
 		D_ERROR("cont "DF_UUID" hdl "DF_UUID" sec_capas "DF_U64", "
