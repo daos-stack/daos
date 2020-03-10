d0ef48db95d4b27bf939b340a4cf208bff0642aa
Kris Jacque <kristin.jacque@intel.com>
2020-03-10 12:01:58 -0600

    DAOS-4305 sec: Check access capas for cont attr calls
    - Add permission checks based on data access for API calls
      to get, set, and list user-settable attributes.
    - Add daos_test test for attribute access.
    - Add data access checks for snapshot access.
    
    Signed-off-by: Kris Jacque <kristin.jacque@intel.com>

--- src/container/srv_epoch.c
+++ src/container/srv_epoch.c
@@ -136,6 +136,13 @@ ds_cont_epoch_aggregate(struct rdb_tx *tx, struct ds_pool_hdl *pool_hdl,
 		DP_CONT(pool_hdl->sph_pool->sp_uuid, in->cei_op.ci_uuid), rpc,
 		in->cei_epoch);
 
+	/* Verify handle has write access */
+	if (!ds_sec_cont_can_write_data(hdl->ch_sec_capas)) {
+		D_ERROR(DF_CONT": permission denied to aggregate\n",
+			DP_CONT(cont->c_svc->cs_pool_uuid, cont->c_uuid));
+		return -DER_NO_PERM;
+	}
+
 	if (epoch >= DAOS_EPOCH_MAX)
 		return -DER_INVAL;
 	else if (in->cei_epoch == 0)
@@ -283,8 +290,10 @@ ds_cont_snap_create(struct rdb_tx *tx, struct ds_pool_hdl *pool_hdl,
 		DP_CONT(pool_hdl->sph_pool->sp_uuid, in->cei_op.ci_uuid), rpc,
 		in->cei_epoch);
 
-	/* Verify the container handle capabilities. */
+	/* Verify handle has write access */
 	if (!ds_sec_cont_can_write_data(hdl->ch_sec_capas)) {
+		D_ERROR(DF_CONT": permission denied to create snapshot\n",
+			DP_CONT(cont->c_svc->cs_pool_uuid, cont->c_uuid));
 		rc = -DER_NO_PERM;
 		goto out;
 	}
@@ -316,6 +325,14 @@ ds_cont_snap_destroy(struct rdb_tx *tx, struct ds_pool_hdl *pool_hdl,
 	D_DEBUG(DF_DSMS, DF_CONT": processing rpc %p: epoch="DF_U64"\n",
 		DP_CONT(pool_hdl->sph_pool->sp_uuid, in->cei_op.ci_uuid),
 		rpc, in->cei_epoch);
+
+	/* Verify the handle has write access */
+	if (!ds_sec_cont_can_write_data(hdl->ch_sec_capas)) {
+		D_ERROR(DF_CONT": permission denied to delete snapshot\n",
+			DP_CONT(cont->c_svc->cs_pool_uuid, cont->c_uuid));
+		rc = -DER_NO_PERM;
+		goto out;
+	}
 
 	d_iov_set(&key, &in->cei_epoch, sizeof(daos_epoch_t));
 	rc = rdb_tx_delete(tx, &cont->c_snaps, &key);
@@ -357,6 +374,15 @@ ds_cont_snap_list(struct rdb_tx *tx, struct ds_pool_hdl *pool_hdl,
 	D_DEBUG(DF_DSMS, DF_CONT": processing rpc %p: hdl="DF_UUID"\n",
 		DP_CONT(pool_hdl->sph_pool->sp_uuid, in->sli_op.ci_uuid),
 		rpc, DP_UUID(in->sli_op.ci_hdl));
+
+	/* Verify the handle has read access */
+	if (!ds_sec_cont_can_read_data(hdl->ch_sec_capas)) {
+		D_ERROR(DF_CONT": permission denied to list snapshots\n",
+			DP_CONT(cont->c_svc->cs_pool_uuid, cont->c_uuid));
+		rc = -DER_NO_PERM;
+		goto out;
+	}
+
 	/*
 	 * If remote bulk handle does not exist, only aggregate size is sent.
 	 */
