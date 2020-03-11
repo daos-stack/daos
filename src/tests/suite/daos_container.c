d8ee39a8f6f53482e6f33166f94857ff656832d9
Li Wei <wei.g.li@intel.com>
2020-03-11 09:14:43 +0800

    DAOS-365 container: Mind handles in cont_destroy (#1949)
    When destroying a container with existing handles, cont_destroy should
    return -DER_BUSY if force == false or evict all existing handles if
    force == true. To implement this, we add a handle index KVS under each
    container property KVS, in order to quickly identify the set of all
    handles for a container.
    
    Signed-off-by: Li Wei <wei.g.li@intel.com>

--- src/tests/suite/daos_container.c
+++ src/tests/suite/daos_container.c
@@ -1772,6 +1772,43 @@ co_set_owner_access(void **state)
 	test_teardown((void **)&arg);
 }
 
+static void
+co_destroy_force(void **state)
+{
+	test_arg_t	*arg = *state;
+	uuid_t		 uuid;
+	daos_handle_t	 coh;
+	daos_cont_info_t info;
+	int		 rc;
+
+	if (arg->myrank != 0)
+		return;
+
+	uuid_generate(uuid);
+
+	print_message("creating container "DF_UUIDF"\n",
+		      DP_UUID(uuid));
+	rc = daos_cont_create(arg->pool.poh, uuid, NULL, NULL);
+	assert_int_equal(rc, 0);
+
+	print_message("opening container\n");
+	rc = daos_cont_open(arg->pool.poh, uuid, DAOS_COO_RW, &coh,
+			    &info, NULL);
+	assert_int_equal(rc, 0);
+
+	print_message("destroying container (force=false): should err\n");
+	rc = daos_cont_destroy(arg->pool.poh, uuid, 0 /* force */, NULL);
+	assert_int_equal(rc, -DER_BUSY);
+
+	print_message("destroying container (force=true): should succeed\n");
+	rc = daos_cont_destroy(arg->pool.poh, uuid, 1 /* force */, NULL);
+	assert_int_equal(rc, 0);
+
+	print_message("closing container: should succeed\n");
+	rc = daos_cont_close(coh, NULL);
+	assert_int_equal(rc, 0);
+}
+
 static int
 co_setup_sync(void **state)
 {
@@ -1834,6 +1871,8 @@ static const struct CMUnitTest co_tests[] = {
 	  co_set_owner, NULL, test_case_teardown},
 	{ "CONT19: container set-owner access by ACL",
 	  co_set_owner_access, NULL, test_case_teardown},
+	{ "CONT20: container destroy force",
+	  co_destroy_force, NULL, test_case_teardown},
 };
 
 int
