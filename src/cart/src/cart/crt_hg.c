a77ee4605770543faf7f2b349c2a2459b1fe7798
Xuezhao Liu <xuezhao.liu@intel.com>
2020-04-17 09:20:13 +0000

    CART-869 mercury: set progress_mode as NA_NO_BLOCK for PSM2
    In perf-test, we see ioops improvement for NO_BLOCK mode, So set it
    for PSM2 provider.
    
    Signed-off-by: Xuezhao Liu <xuezhao.liu@intel.com>

--- src/cart/src/cart/crt_hg.c
+++ src/cart/src/cart/crt_hg.c
@@ -491,6 +491,15 @@ crt_hg_log(FILE *stream, const char *fmt, ...)
 	return 0;
 }
 
+static bool
+crt_hg_no_block_mode()
+{
+	if (crt_gdata.cg_na_plugin == CRT_NA_OFI_PSM2)
+		return true;
+	else
+		return false;
+}
+
 /* to be called only in crt_init */
 int
 crt_hg_init(crt_phy_addr_t *addr, bool server)
@@ -519,7 +528,10 @@ crt_hg_init(crt_phy_addr_t *addr, bool server)
 	if (rc != 0)
 		D_GOTO(out, rc);
 
-	init_info.na_init_info.progress_mode = 0;
+	if (crt_hg_no_block_mode())
+		init_info.na_init_info.progress_mode = NA_NO_BLOCK;
+	else
+		init_info.na_init_info.progress_mode = 0;
 	init_info.na_init_info.max_contexts = 1;
 	if (crt_gdata.cg_share_na == false)
 		/* one context per NA class */
@@ -680,7 +692,10 @@ crt_hg_ctx_init(struct crt_hg_context *hg_ctx, int idx)
 		if (rc != 0)
 			D_GOTO(out, rc);
 
-		init_info.na_init_info.progress_mode = 0;
+		if (crt_hg_no_block_mode())
+			init_info.na_init_info.progress_mode = NA_NO_BLOCK;
+		else
+			init_info.na_init_info.progress_mode = 0;
 		init_info.na_init_info.max_contexts = 1;
 		na_class = NA_Initialize_opt(info_string, crt_is_service(),
 					     &init_info.na_init_info);
