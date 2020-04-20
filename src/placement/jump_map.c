1919e9289562dbdfd3a2786c927b37fc730cb63c
Peter Fetros <peter.fetros@intel.com>
2020-04-20 18:47:55 +0000

    Style Fixes
    Signed-off-by: Peter Fetros <peter.fetros@intel.com>

--- src/placement/jump_map.c
+++ src/placement/jump_map.c
@@ -33,13 +33,13 @@
 
 
 /*
- * These ops determine whether extra information is calculated during 
+ * These ops determine whether extra information is calculated during
  * placement.
  *
  * PL_PLACE_EXTENDED calculates an extended layout for use when there
  * is a reintegration operation currently ongoing.
  *
- * PL_REINT calculates the post-reintegration layout for use during 
+ * PL_REINT calculates the post-reintegration layout for use during
  * reintegration, it treats the UP status targets as UP_IN.
  *
  * Currently the other OP types calculate a normal layout without extra info.
