fc0e8977296eacd4235689fad749624ddb6c4f3d
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2020-04-22 19:52:40 -0400

    Refactored the test to include using system information to retreave the device info.
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/control/dmg_network_scan.py
+++ src/tests/ftest/control/dmg_network_scan.py
@@ -25,20 +25,80 @@ from __future__ import print_function
 
 import os
 import re
+import socket
 
-from general_utils import run_cmd
 from avocado.utils import process
 from control_test_base import ControlTestBase
 
 
+class NetDevList(object):
+    """ A class that holds a list of NetDev objects."""
+    def __init__(self, netdevs=None):
+        """ Initialize a list of netdevices."""
+        self.netdevs = netdevs
+
+    def __str__(self):
+        for i in self.netdevs:
+            print(i)
+
+    def __eq__(self, other_devs):
+        """Overrides the default implementation to compare devices."""
+        equality = True
+        if isinstance(other_devs, NetDevList):
+            if len(self.netdevs) != len(other_devs.netdevs):
+                equality = False
+            else:
+                for i, _ in enumerate(self.netdevs):
+                    if other_devs.netdevs[i] not in self.netdevs:
+                        equality = False
+        return equality
+
+    def __ne__(self, other_devs):
+        """Overrides the default implementation (unnecessary in Python 3)"""
+        return not self.__eq__(other_devs)
+
+
 class NetDev(object):
     """A class to represent the information of a network device"""
-    def __init__(self, host, fabric_iface, provider, numa_node):
+    def __init__(self, host=None, f_iface=None, providers=None, numa=None):
         """ Initialize the network device data object."""
         self.host = host
-        self.fabric_iface = fabric_iface
-        self.provider = provider
-        self.numa_node = numa_node
+        self.f_iface = f_iface
+        self.providers = providers
+        self.numa = numa
+
+    def __repr__(self):
+        self.__str__()
+
+    def __str__(self):
+        return "{}\n{}\n{}\n{}".format(
+            self.host, self.f_iface, self.providers, self.numa)
+
+    def get_dev_info(self, prefix):
+        """ Get all of this devices' information."""
+        self._get_numa()
+        self._get_providers(prefix)
+
+    def _get_numa(self):
+        """ Get the numa node information for this device."""
+        if self.f_iface:
+            p = "/sys/class/net/{}/device/numa_node".format(self.f_iface)
+            if os.path.exists(p):
+                self.numa = ''.join(open(p, 'r').read()).rstrip()
+
+    def _get_providers(self, prefix):
+        """ Get the provider information for this device."""
+        if self.f_iface:
+            fi_info = os.path.join(
+                prefix, "bin", "fi_info -d {}".format(self.f_iface))
+
+            # Run command
+            out = process.run(fi_info)
+
+            # Parse the list and remove dups
+            dups = re.findall(r"(?:provider):\s+([A-Za-z0-9;_+]+)", out.stdout)
+            self.providers = list(dict.fromkeys(dups))
+
 
 class DmgNetworkScanTest(ControlTestBase):
     """Test Class Description:
@@ -51,50 +111,58 @@ class DmgNetworkScanTest(ControlTestBase):
         super(DmgNetworkScanTest, self).__init__(*args, **kwargs)
         self.setup_start_agents = False
 
+    def get_devs(self):
+        """ Get list of devices."""
+        devs = []
+        for dev in os.listdir("/sys/class/net/"):
+            devs.append(dev)
+            # Check if we need to add infiniband dev to list
+            p = "/sys/class/net/{}/device/infiniband".format(dev)
+            if os.path.exists(p):
+                devs.extend(os.listdir(p))
+        return devs
+
     def get_sys_info(self):
         """ Get expected values of numa nodes with lstopo."""
-        sys_info = {}
-        for d in os.listdir("/sys/class/net/"):
-            sys_info[d] = {}
-            if os.path.exists("/sys/class/net/{}/device/infiniband".format(d)):
-                sys_info[d]["dev"] = os.listdir(
-                    "/sys/class/net/{}/device/infiniband".format(d))
-            if os.path.exists('/sys/class/net/{}/device/numa_node'.format(d)):
-                numa_node = ''.join(
-                    open('/sys/class/net/{}/device/numa_node'.format(d),
-                         'r').read()).rstrip()
-                sys_info[d]["numa"] = numa_node
-        return sys_info
+        host = socket.gethostname().split(".")[0]
+        sys_net_devs = []
+        for dev in self.get_devs():
+            dev_info = NetDev(host, dev)
+            dev_info.get_dev_info(self.prefix)
+            sys_net_devs.append(dev_info)
 
-    def get_dev_provider(self, device):
-        """Get expected values of provider with fi_info.
+        return NetDevList(sys_net_devs)
 
-        Args:
-            device (str): network device name
-        """
-        fi_info_dir = os.path.join(self.prefix, 'bin')
-        fi_info = os.path.join(fi_info_dir, "fi_info -d {}".format(device))
+    def get_dmg_info(self):
+        """ Store the information received from dmg output."""
+        # Parse the dmg info
+        host = None
+        dmg_net_devs = []
+        scan_info = self.get_dmg_output("network_scan")
 
-        # Run the command
-        output = process.run(fi_info, ignore_status=True)
+        # Get devices divided
+        info = [scan_info[dev:(dev + 3)] for dev in range(0, len(scan_info), 3)]
+        for dev in info:
+            if dev[0][0] != "":
+                host = dev[0][0]
+            dev_info = NetDev(host, dev[0][2], [dev[1][2]], dev[2][2])
+            dmg_net_devs.append(dev_info)
 
-        # Command failed or possibly timed out
-        if output.exit_status != 0 and output.exit_status != 61:
-            msg = "Error occurred running '{}': {}".format(
-                fi_info, output.exit_status)
-            self.fail(msg)
+        # Consolidate the dups
+        conso = []
+        diff = []
+        for idx, i in enumerate(dmg_net_devs):
+            for j in dmg_net_devs[idx + 1:]:
+                if i.host == j.host and i.f_iface == j.f_iface and \
+                        i.numa == j.numa and i.providers != j.providers:
+                    providers = list(dict.fromkeys(i.providers + j.providers))
+                    conso.append(NetDev(i.host, i.f_iface, providers, i.numa))
+        for i in conso:
+            for j in dmg_net_devs:
+                if i.f_iface != j.f_iface and j not in diff:
+                    diff.append(j)
 
-        # Clean up string
-        providers = []
-        if output.exit_status == 61:
-            return providers
-        for line in output.stdout.splitlines():
-            if "provider" in line:
-                prov = line.strip().split()[-1]
-                if prov not in providers:
-                    providers.append(prov)
-
-        return providers
+        return NetDevList(conso + diff)
 
     def test_dmg_network_scan_basic(self):
         """
@@ -103,35 +171,9 @@ class DmgNetworkScanTest(ControlTestBase):
         devices on the system.
         :avocado: tags=all,small,pr,hw,dmg,network_scan,basic
         """
-        net_scan_info = self.get_dmg_output("network_scan")
-        for dev_info in net_scan_info:
-            if dev_info[0] != "":
+        # Get info, both these functions will return a NetDevList
+        dmg_info = self.get_dmg_info()
+        sys_info = self.get_sys_info()
 
-
-        # Validate dmg output against 3rd party tools: lstopo, fi_info
-        covered = []
-        for dev in dmg_out:
-            dmg_iface = dev[0].split()[1]
-            dmg_numa = int(dev[1].split()[1])
-            dmg_prov = dev[2].split()[1].split("+")[1]
-            tr = {
-                "ib0": "hfi1_0", "ib1": "hfi1_1",
-                "eth0": "i40iw1", "eth1": "i40iw0"
-            }
-            for sys_numa, sys_ifaces in self.get_numa_info().items():
-                if sys_numa == 0:
-                    sys_ifaces.append("lo")
-                if sys_numa == dmg_numa and dmg_iface in sys_ifaces:
-                    # Verify that the provider is supported for this device
-                    if dmg_prov in self.get_dev_provider(dmg_iface):
-                        covered.append(dev)
-                    if (dmg_iface in tr and
-                            dmg_prov in self.get_dev_provider(tr[dmg_iface])):
-                        covered.append(dev)
-
-        # Sort both lists
-        dmg_out.sort()
-        covered.sort()
-
-        if covered != dmg_out:
-            self.fail("dmg output does not match network devices found.")
+        # Validate the output with what we expect.
+        self.assertEqual(sys_info, dmg_info)
