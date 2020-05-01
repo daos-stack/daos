cc3bd873e81d41455e284b892eae1f3e1bc746ed
Maureen Jean <maureen.jean@intel.com>
2020-04-30 23:21:16 -0400

    DAOS-4693 tests: enable daos_test for no FI builds
    Removed subtests from daos_test that require fault injection
    
    Disabled tests:
    MGMT5
    DTX3,4,16
    VC4-9
    IO19,31
    CSUM01-10
    EC_CSUM01,02
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

---
+++ src/tests/ftest/daos_test/daos_core_test-faults.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+"""
+  (C) Copyright 2018-2019 Intel Corporation.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+  The Government's rights to use, modify, reproduce, release, perform, display,
+  or disclose this software are subject to the terms of the Apache License as
+  provided in Contract No. B609815.
+  Any reproduction of computer software, computer software documentation, or
+  portions thereof marked with this legend must also reproduce the markings.
+"""
+
+from daos_core_base import DaosCoreBase
+
+
+class DaosCoreTest(DaosCoreBase):
+    # pylint: disable=too-many-ancestors
+    """Runs the non-rebuild daos_test tests with fault injectors enabled.
+
+    :avocado: recursive
+    """
+
+    def test_faults(self):
+        """Jira ID: DAOS-4693.
+
+        Test Description: Run daos_test with fault injection enabled
+
+        Use Cases: core tests for daos_test with fault injection
+
+        :avocado: tags=all,pr,hw,ib2,medium,daos_test_faults
+        """
+        DaosCoreBase.run_subtest(self)
