a6b5d84149bdf4f7a0671722e82f33e47e6f1601
saurabhtandan <saurabh.tandan@intel.com>
2020-05-04 08:58:15 -0700

    DAOS-3173 test: Fixing nvme_io.py failing in weekly run (#1076) (#2618)
    Corrected nvme_io.py test to use correct storage and disabled aggregation.
    Originally test was not using nvme and dcpm, hence enabled that in
    nvme_io.yaml file.
    
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>
    Conflicts:
    	src/tests/ftest/util/dmg_utils.py

--- src/tests/ftest/util/test_utils_pool.py
+++ src/tests/ftest/util/test_utils_pool.py
@@ -74,6 +74,8 @@ class TestPool(TestDaosApiBase):
         self.target_list = BasicParameter(None)
         self.scm_size = BasicParameter(None)
         self.nvme_size = BasicParameter(None)
+        self.prop_name = BasicParameter(None)      # name of property to be set
+        self.prop_value = BasicParameter(None)     # value of property
 
         # Set USE_API to use API or USE_DMG to use dmg. If it's not set, API is
         # used.
@@ -262,6 +264,30 @@ class TestPool(TestDaosApiBase):
             self.svc_ranks = None
 
         return status
+
+    @fail_on(CommandFailure)
+    def set_property(self):
+        """Set Property.
+
+        It sets property for a given pool uuid using
+        dmg.
+
+        """
+        if self.pool:
+            self.log.info("Set-prop for Pool: %s", self.uuid)
+
+            if self.control_method.value == self.USE_DMG and self.dmg:
+                # set-prop for given pool using dmg
+                self.dmg.pool_set_prop(self.uuid, self.prop_name,
+                                       self.prop_value)
+
+            elif self.control_method.value == self.USE_DMG:
+                self.log.error("Error: Undefined dmg command")
+
+            else:
+                self.log.error(
+                    "Error: Undefined control_method: %s",
+                    self.control_method.value)
 
     @fail_on(DaosApiError)
     def get_info(self):
