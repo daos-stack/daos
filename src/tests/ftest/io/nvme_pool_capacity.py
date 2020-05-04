7722c812bfcfc17516c0ffcd60c6df04b3cbd993
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-05-04 11:43:07 -0400

    DAOS-2085 test: Fix block size issue for one test. Test-tag-hw-medium: full_regression,hw,medium,ib2,nvme_pool_capacity
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/io/nvme_pool_capacity.py
+++ src/tests/ftest/io/nvme_pool_capacity.py
@@ -204,7 +204,7 @@ class NvmePoolCapacity(TestWithServers):
                 for thrd in range(0, num_jobs):
                     # Based on pools/jobs, split block size
                     if thrd == 0:
-                        tmp = int(float(test[3])) / num_pool
+                        tmp = int(float(test[3]) / num_pool)
                         test[3] = str(tmp)
                     # Add a thread for these IOR arguments
                     threads.append(threading.Thread(target=self.ior_thread,
