199c41e56683b95736921719af5bb24317ede56e
Fan Yong <fan.yong@intel.com>
2020-05-07 13:48:20 +0800

    DAOS-3905 test: just for verify the baseline
    For test only.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/client/pydaos/raw/daos_api.py
+++ src/client/pydaos/raw/daos_api.py
@@ -1134,6 +1134,7 @@ class IORequest(object):
             self.iod.iod_type = 1
             self.iod.iod_size = size
             self.iod.iod_nr = 1
+            self.iod.iod_recxs = None
 
         # now do it
         if dkey is not None:
