ee9fd510e9e246e42d325d81ff8c0f28424c64a2
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2020-05-08 14:12:45 -0400

    Add agent test files, made several fixes to server config test.
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/control/daos_agent_config.py
+++ src/tests/ftest/control/daos_agent_config.py
@@ -25,12 +25,9 @@ from __future__ import print_function
 
 import os
 
-from command_daos_utils import CommonConfig
-from agent_utils import (DaosAgentYamlParameters, DaosAgentCommand,
-                         DaosAgentManager, DaosAgentTransportCredentials,
-                         include_local_host)
 from apricot import TestWithServers
-from server_utils import ServerManager, ServerFailed
+from agent_utils import include_local_host
+from command_utils_base import CommandFailure
 
 
 class DaosAgentConfigTest(TestWithServers):
@@ -44,40 +41,41 @@ class DaosAgentConfigTest(TestWithServers):
         """Initialize a DaosAgentConfigTest object."""
         super(DaosAgentConfigTest, self).__init__(*args, **kwargs)
         self.setup_start_agents = False
+        self.setup_start_servers = False
 
-    def test_daos_server_config_basic(self):
+    def test_daos_agent_config_basic(self):
         """
         JIRA ID: DAOS-1508
+
         Test Description: Test daos_agent start/stops properly.
         on the system.
-        :avocado: tags=all,tiny,control,pr,agent_start,basic
+
+        :avocado: tags=all,small,control,pr,agent_start,basic
         """
         # Setup the agents
-        transport = DaosAgentTransportCredentials()
-
-        # Use the unique agent group name to create a unique yaml file
-        config_file = self.get_config_file(self.server_group, "agent")
+        self.add_agent_manager()
+        self.configure_manager(
+            "agent",
+            self.agent_managers[-1],
+            include_local_host(self.hostlist_clients),
+            self.hostfile_clients_slots)
 
         # Get the input to verify
         c_val = self.params.get("config_val", "/run/agent_config_val/*/")
 
         # Setup the access points with the server hosts
-        common_cfg = CommonConfig(self.server_group, transport)
-        self.add_agent_manager(config_file, common_cfg)
-        self.configure_manager(
-            "agent",
-            self.agent_managers[-1],
-            self.hostlist_clients,
-            self.hostfile_clients_slots,
-            self.hostlist_servers)
+        self.log.info("Starting agent with %s = %s", c_val[0], c_val[1])
 
-        self.log.info("Starting server changing %s with %s", c_val[0], c_val[1])
-        self.start_agent_managers()
+        try:
+            self.start_agent_managers[-1].start()
+            exception = None
+        except CommandFailure as err:
+            exception = err
 
         # Verify
         if c_val[2] == "FAIL" and exception is None:
-            self.log.error("Server was expected to fail")
+            self.log.error("Agent was expected to fail")
             self.fail("{}".format(exception))
         elif c_val[2] == "PASS" and exception is not None:
-            self.log.error("Server was expected to start")
+            self.log.error("Agent was expected to start")
             self.fail("{}".format(exception))
