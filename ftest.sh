4632a668b9d7eb1f9048adaec76bf2f101642453
Phillip Henderson <phillip.henderson@intel.com>
2020-05-12 12:40:49 -0400

    Removing debug verbose flag from ftest.sh
    Skip-python-bandit: false
    Test-tag: pr,-hw unittest
    Test-tag-hw-large: pr,hw,large soak_smoke
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- ftest.sh
+++ ftest.sh
@@ -404,7 +404,7 @@ enabled=0
 EOF\"
 
 # now run it!
-if ! ./launch.py -crispav -ts ${TEST_NODES} ${NVME_ARG} ${TEST_TAG_ARR[*]}; then
+if ! ./launch.py -crispa -ts ${TEST_NODES} ${NVME_ARG} ${TEST_TAG_ARR[*]}; then
     rc=\${PIPESTATUS[0]}
 else
     rc=0
