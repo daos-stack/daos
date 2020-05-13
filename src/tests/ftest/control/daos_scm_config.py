963813cf1b70e3af3dea3212b355a573e0efcb5d
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2020-05-13 13:48:46 -0400

    WIP
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/control/daos_scm_config.py
+++ src/tests/ftest/control/daos_scm_config.py
@@ -23,6 +23,8 @@
 """
 from __future__ import print_function
 
+from general_utils import check_file_exists
+from pydaos.raw import IORequest
 from apricot import TestWithServers
 
 
@@ -33,13 +35,45 @@ class SCMConfigTest(TestWithServers):
     :avocado: recursive
     """
 
-    def test_scm_config_basic(self):
+    def test_scm_in_use_basic(self):
         """
         JIRA ID: DAOS-2972
 
-        Test Description: Test basic SCM configurations of storage devices.
+        Test Description: Verify that an attempt to configure devices that have
+        already been configured and are in use by DAOS is handled.
 
-        :avocado: tags=all,small,full_regression,hw,scm_config,basic
+        :avocado: tags=all,small,pr,hw,scm_in_use,basic
         """
+        # Check that pmem is mounted
+        self.log.info(
+            "==>    Verifying pmem is mounted: <%s>", self.hostlist_servers)
+        if self.server_managers[-1].manager.job.using_dcpm:
+            scm_list = self.server_managers[-1].get_config_value("scm_list")
+            errors = []
+            for pmem in scm_list:
+                check_result = check_file_exists(self.hostlist_servers, pmem)
+                if not check_result[0]:
+                    errors.append("{}: {}".format(pmem, check_result[1]))
+            if errors:
+                self.fail("pmems not found: \n{}".format(errors))
+        else:
+            self.fail("Detected dcpm not specified")
 
+        # Storage prepare should return error
+        self.log.info("==>    Verifying storage prepare is done")
+        self.server_managers[-1].dmg.exit_status_exception = False
 
+        result = self.server_managers[-1].dmg.storage_prepare(scm=True)
+        if result.exit_status == 0:
+            self.fail("Storage prepare expected to fail: {}".format(
+                result.stdout))
+
+    def test_scm_mock_media_error(self):
+        """
+        JIRA ID: DAOS-2972
+
+        Test Description: Using ipmctl tool, inject an error into DIMMs to
+        verify that we handle cases of bad devices.
+
+        :avocado: tags=all,small,full_regression,hw,scm_media_error,basic
+        """
