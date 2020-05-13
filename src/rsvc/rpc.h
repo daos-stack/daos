f6a7b03d171be351d3222bfbd4a899bab6f1838e
Li Wei <wei.g.li@intel.com>
2020-05-13 11:55:37 +0800

    Update copyright dates
    Signed-off-by: Li Wei <wei.g.li@intel.com>

--- src/rsvc/rpc.h
+++ src/rsvc/rpc.h
@@ -1,5 +1,5 @@
-/**
- * (C) Copyright 2016-2019 Intel Corporation.
+/*
+ * (C) Copyright 2016-2020 Intel Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@
  * portions thereof marked with this legend must also reproduce the markings.
  */
 /**
+ * \file
+ *
  * ds_rsvc: RPC Protocol Definitions
  */
 
