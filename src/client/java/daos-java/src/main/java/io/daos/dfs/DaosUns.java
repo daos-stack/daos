1d69bfab5b2c2834c6debb761afa98e81ef27127
jiafu zhang <jiafu.zhang@intel.com>
2020-05-15 09:47:59 +0000

    command line tool for operating UNS wrapper
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- src/client/java/daos-java/src/main/java/io/daos/dfs/DaosUns.java
+++ src/client/java/daos-java/src/main/java/io/daos/dfs/DaosUns.java
@@ -23,12 +23,15 @@
 
 package io.daos.dfs;
 
+import com.google.protobuf.TextFormat;
 import io.daos.dfs.uns.*;
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.nio.ch.DirectBuffer;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
@@ -339,25 +342,33 @@ public class DaosUns {
         }
 
         private void buildProperties(DunsAttribute.Builder attrBuilder) {
-            if (!propMap.isEmpty()) {
-                Properties.Builder builder = Properties.newBuilder();
-                builder.setReserved(propReserved);
-                Entry.Builder eb = Entry.newBuilder();
-                for (Map.Entry<PropType, PropValue> entry : propMap.entrySet()) {
-                    eb.clear();
-                    eb.setType(entry.getKey()).setReserved(entry.getValue().getReserved());
-                    Class<?> valueClass = PropValue.getValueClass(entry.getKey());
-                    if (valueClass == Long.class) {
-                        eb.setVal((Long)entry.getValue().getValue());
-                    } else if (valueClass == String.class) {
-                        eb.setStr((String)entry.getValue().getValue());
-                    } else {
-                        eb.setPval((DaosAcl)entry.getValue().getValue());
-                    }
-                    builder.addEntries(eb.build());
-                }
+            Properties.Builder builder = setProperties(propReserved, propMap);
+            if (builder != null) {
                 attrBuilder.setProperties(builder.build());
             }
+        }
+
+        private static Properties.Builder setProperties(int propReserved, Map<PropType, PropValue> propMap) {
+            if (propMap.isEmpty()) {
+                return null;
+            }
+            Properties.Builder builder = Properties.newBuilder();
+            builder.setReserved(propReserved);
+            Entry.Builder eb = Entry.newBuilder();
+            for (Map.Entry<PropType, PropValue> entry : propMap.entrySet()) {
+                eb.clear();
+                eb.setType(entry.getKey()).setReserved(entry.getValue().getReserved());
+                Class<?> valueClass = PropValue.getValueClass(entry.getKey());
+                if (valueClass == Long.class) {
+                    eb.setVal((Long)entry.getValue().getValue());
+                } else if (valueClass == String.class) {
+                    eb.setStr((String)entry.getValue().getValue());
+                } else {
+                    eb.setPval((DaosAcl)entry.getValue().getValue());
+                }
+                builder.addEntries(eb.build());
+            }
+            return builder;
         }
     }
 
@@ -413,5 +424,355 @@ public class DaosUns {
                 default: throw new IllegalArgumentException("no value class for " + propType);
             }
         }
+    }
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String args[]) throws Exception {
+        if (needUsage(args)) {
+            String usage = getUsage();
+            log.info(usage);
+            return;
+        }
+        if (args.length < 1) {
+            throw new IllegalArgumentException("need one of commands" +
+                    " [create|resolve|destroy|parse|util]\n" + getUsage());
+        }
+        switch (args[0]) {
+            case "create": create(); return;
+            case "resolve": resolve(); return;
+            case "destroy": destroy(); return;
+            case "parse": parse(); return;
+            case "util": util(); return;
+            default: throw new IllegalArgumentException("not supported command, " + args[0]);
+        }
+    }
+
+    private static void util() {
+        String op = System.getProperty("op");
+        if (StringUtils.isBlank(op)) {
+            throw new IllegalArgumentException("need operation type.\n" + getUsage());
+        }
+        switch (op) {
+            case "list-object-types": listObjectTypes(); return;
+            case "list-property-types": listPropertyTypes(); return;
+            case "get-property-value-type": getPropertyValueType(); return;
+            case "sample-properties": sampleProperties(); return;
+            default: throw new IllegalArgumentException("not supported operation, " + op);
+        }
+    }
+
+    private static void listObjectTypes() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("object types:\n");
+        for (DaosObjectType type : DaosObjectType.values()) {
+            sb.append(type).append("\n");
+        }
+        log.info(sb.toString());
+    }
+
+    private static void listPropertyTypes() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("property types:\n");
+        for (PropType type : PropType.values()) {
+            sb.append(type).append("\n");
+        }
+        log.info(sb.toString());
+    }
+
+    private static void getPropertyValueType() {
+        String type = System.getProperty("prop_type");
+        if (StringUtils.isBlank(type)) {
+            throw new IllegalArgumentException("need property type.\n" + getUsage());
+        }
+        log.info("property value type is: " + PropValue.getValueClass(PropType.valueOf(type)));
+    }
+
+    private static void sampleProperties() {
+        log.info("sample 1, Set Integer Value:");
+        printPropIntValue();
+        log.info("sample 2, Set String Value:");
+        printPropStringValue();
+        log.info("sample 3, Set Two ACL Values:");
+        printPropAclValues();
+        log.info("sample 4, Set Integer And ACL Values:");
+        printPropIntAndAclValues();
+    }
+
+    private static String escapeDoubleQuote(String str) {
+        return str.replaceAll("\"", "\\\\\"");
+    }
+
+    private static void printPropIntValue() {
+        Map<PropType, DaosUns.PropValue> propMap = new HashMap<>();
+        propMap.put(PropType.DAOS_PROP_CO_LAYOUT_VER, new DaosUns.PropValue(2L, 0));
+        Properties.Builder builder = DaosUnsBuilder.setProperties(1, propMap);
+        String str = TextFormat.shortDebugString(builder);
+        log.info("-Dproperties=\"" + escapeDoubleQuote(str) + "\"");
+    }
+
+    private static void printPropStringValue() {
+        Map<PropType, DaosUns.PropValue> propMap = new HashMap<>();
+        propMap.put(PropType.DAOS_PROP_CO_LABEL, new DaosUns.PropValue("label", 0));
+        Properties.Builder builder = DaosUnsBuilder.setProperties(0, propMap);
+        String str = TextFormat.shortDebugString(builder);
+        log.info("-Dproperties=\"" + escapeDoubleQuote(str) + "\"");
+    }
+
+    private static void printPropAclValues() {
+        Map<PropType, DaosUns.PropValue> propMap = new HashMap<>();
+        int aclOwner = 0;
+        int aclUser = 1;
+        String user = new com.sun.security.auth.module.UnixSystem().getUsername() + "@";
+        int accessAllow = 1;
+        int permDel = 1 << 3;
+        int permGet = 1 << 6;
+        int permSet = 1 << 7;
+        int perms =  permGet | permDel | permSet;
+        DaosAce ace = DaosAce.newBuilder()
+                .setAccessTypes(accessAllow)
+                .setPrincipal(user)
+                .setPrincipalType(aclUser)
+                .setPrincipalLen(user.length())
+                .setAllowPerms(perms)
+                .build();
+        DaosAce ace2 = DaosAce.newBuilder()
+                .setAccessTypes(accessAllow)
+                .setPrincipalType(aclOwner)
+                .setAllowPerms(perms)
+                .setPrincipalLen(0)
+                .build();
+        DaosAcl.Builder aclBuilder = DaosAcl.newBuilder()
+                .setVer(1);
+        aclBuilder.addAces(ace2).addAces(ace);
+        DaosAcl acl = aclBuilder.build();
+        propMap.put(PropType.DAOS_PROP_CO_ACL, new DaosUns.PropValue(acl, 1));
+        Properties.Builder builder = DaosUnsBuilder.setProperties(0, propMap);
+        String str = TextFormat.shortDebugString(builder);
+        log.info("-Dproperties=\"" + escapeDoubleQuote(str) + "\"");
+    }
+
+    private static void printPropIntAndAclValues() {
+        Map<PropType, DaosUns.PropValue> propMap = new HashMap<>();
+        int aclUser = 1;
+        String user = new com.sun.security.auth.module.UnixSystem().getUsername() + "@";
+        int accessAllow = 1;
+        int permDel = 1 << 3;
+        int permGet = 1 << 6;
+        int permSet = 1 << 7;
+        int perms =  permGet | permDel | permSet;
+        DaosAce ace = DaosAce.newBuilder()
+                .setAccessTypes(accessAllow)
+                .setPrincipal(user)
+                .setPrincipalType(aclUser)
+                .setPrincipalLen(user.length())
+                .setAllowPerms(perms)
+                .build();
+        DaosAcl.Builder aclBuilder = DaosAcl.newBuilder()
+                .setVer(1);
+        aclBuilder.addAces(ace);
+        DaosAcl acl = aclBuilder.build();
+        propMap.put(PropType.DAOS_PROP_CO_LAYOUT_VER, new DaosUns.PropValue(2L, 0));
+        propMap.put(PropType.DAOS_PROP_CO_ACL, new DaosUns.PropValue(acl, 1));
+        Properties.Builder builder = DaosUnsBuilder.setProperties(0, propMap);
+        String str = TextFormat.shortDebugString(builder);
+        log.info("-Dproperties=\"" + escapeDoubleQuote(str) + "\"");
+    }
+
+    private static void parse() {
+        String input = System.getProperty("input");
+        try {
+            DunsAttribute attribute = DaosUns.parseAttribute(input);
+            log.info("parsed attributes from string: " + input);
+            log.info(TextFormat.printer().printToString(attribute));
+        } catch (Exception e) {
+            log.error("failed to parse attribute string.", e);
+        }
+    }
+
+    private static void resolve() {
+        String path = System.getProperty("path");
+        try {
+            DunsAttribute attribute = DaosUns.resolvePath(path);
+            log.info("resolved attributes from UNS path: " + path);
+            log.info(TextFormat.printer().printToString(attribute));
+        } catch (Exception e) {
+            log.error("failed to resolve path. ", e);
+        }
+    }
+
+    private static void destroy() {
+        String path = System.getProperty("path");
+        String poolId = System.getProperty("pool_id");
+        String ranks = System.getProperty("ranks");
+        String serverGrp = System.getProperty("server_group");
+        String poolFlags = System.getProperty("pool_flags");
+
+        if (StringUtils.isBlank(path)) {
+            throw new IllegalArgumentException("need path, -Dpath=");
+        }
+        if (StringUtils.isBlank(poolId)) {
+            throw new IllegalArgumentException("need pool UUID, -Dpool_id");
+        }
+
+        File file = new File(path);
+        DaosUns.DaosUnsBuilder builder = new DaosUns.DaosUnsBuilder();
+        builder.path(file.getAbsolutePath());
+        builder.poolId(poolId);
+
+        if (!StringUtils.isBlank(ranks)) {
+            builder.ranks(ranks);
+        }
+        if (!StringUtils.isBlank(serverGrp)) {
+            builder.serverGroup(serverGrp);
+        }
+        if (!StringUtils.isBlank(poolFlags)) {
+            builder.poolFlags(Integer.valueOf(poolFlags));
+        }
+
+        DaosUns uns = builder.build();
+        try {
+            uns.destroyPath();
+            log.info("UNS path destroyed.");
+        } catch (Exception e) {
+            log.error("failed to destroy UNS path.", e);
+        }
+    }
+
+    private static void create() {
+        String path = System.getProperty("path");
+        String poolId = System.getProperty("pool_id");
+        String contId = System.getProperty("cont_id");
+        String layout = System.getProperty("layout");
+        String objectType = System.getProperty("object_type");
+        String chunkSize = System.getProperty("chunk_size");
+        String onLustre = System.getProperty("on_lustre");
+        String ranks = System.getProperty("ranks");
+        String serverGrp = System.getProperty("server_group");
+        String poolFlags = System.getProperty("pool_flags");
+        String properties = System.getProperty("properties");
+
+        if (StringUtils.isBlank(path)) {
+            throw new IllegalArgumentException("need path, -Dpath=");
+        }
+        if (StringUtils.isBlank(poolId)) {
+            throw new IllegalArgumentException("need pool UUID, -Dpool_id");
+        }
+
+        File file = new File(path);
+        DaosUns.DaosUnsBuilder builder = new DaosUns.DaosUnsBuilder();
+        builder.path(file.getAbsolutePath());
+        builder.poolId(poolId);
+        if (!StringUtils.isBlank(contId)) {
+            builder.containerId(contId);
+        }
+        if (!StringUtils.isBlank(layout)) {
+            builder.layout(Layout.valueOf(layout));
+        }
+        if (!StringUtils.isBlank(objectType)) {
+            builder.objectType(DaosObjectType.valueOf(objectType));
+        }
+        if (!StringUtils.isBlank(chunkSize)) {
+            builder.chunkSize(Long.valueOf(chunkSize));
+        }
+        if (!StringUtils.isBlank(onLustre)) {
+            builder.onLustre(Boolean.valueOf(onLustre));
+        }
+        if (!StringUtils.isBlank(ranks)) {
+            builder.ranks(ranks);
+        }
+        if (!StringUtils.isBlank(serverGrp)) {
+            builder.serverGroup(serverGrp);
+        }
+        if (!StringUtils.isBlank(poolFlags)) {
+            builder.poolFlags(Integer.valueOf(poolFlags));
+        }
+        if (!StringUtils.isBlank(properties)) {
+            Properties.Builder pb = Properties.newBuilder();
+            try {
+                TextFormat.getParser().merge(properties, pb);
+                Properties p = pb.build();
+                builder.propReserved(p.getReserved());
+                if (p.getEntriesCount() > 0) {
+                    for (Entry e : p.getEntriesList()) {
+                        Object o;
+                        switch (e.getValueCase()) {
+                            case STR: o = e.getStr(); break;
+                            case VAL: o = e.getVal(); break;
+                            case PVAL: o = e.getPval(); break;
+                            default: throw new IllegalArgumentException("unknown value case: " + e.getValueCase());
+                        }
+                        builder.putEntry(e.getType(), new PropValue(o, e.getReserved()));
+                    }
+                }
+            } catch (Exception e) {
+                log.error("failed to parse properties, " + properties, e);
+                return;
+            }
+        }
+
+        DaosUns uns = builder.build();
+        try {
+            uns.createPath();
+            log.info("UNS path created.");
+        } catch (Exception e) {
+            log.error("failed to create UNS path.", e);
+        }
+    }
+
+    private static boolean needUsage(String args[]) {
+        for (String arg : args) {
+            if ("--help".equals(arg) || "-h".equals(arg)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String getUsage() {
+        String usage = "===================================================\n" +
+                "Usage: create/resolve/destroy/parse UNS path associated with DAOS.\n" +
+                "see following commands and their parameters:\n" +
+                "command: [create|resolve|destroy|parse|util]\n" +
+                "=>create:\n" +
+                "   -Dpath=, required, OS file path. The file should not exist.\n" +
+                "   -Dpool_id=, required, DAOS pool UUID.\n" +
+                "   -Dcont_id=, optional, DAOS container UUID. New container will be created if not set.\n" +
+                "   -Dlayout=, optional, filesystem layout. Default is POSIX. [POSIX|HDF5].\n" +
+                "   -Dobject_type=, optional, file object type. Default is OC_SX. [enums from DaosObjectType]. " +
+                "use the \"util -Dop=list-object-types\" command to list all object types.\n" +
+                "   -Dchunk_size=, optional, file chunck size. Default is 0 which lets DAOS decides (1MB for now).\n" +
+                "   -Don_lustre=, optional, on lustre file system? Default is false. [true|false]\n" +
+                "   -Dranks=, optional, pool ranks. Default is 0.\n" +
+                "   -Dserver_group=, optional, DAOS server group. Default is daos_server.\n" +
+                "   -Dpool_flags=, optional, pool access flags. Default is 2. [1(readonly)|2(readwrite)|4(execute)].\n"
+                +
+                "   -Dproperties=, optional, properties. No default. " +
+                "[key(enums from PropType)=value(value type can be get from PropValue.getValueClass(type))].\n" +
+                "use the \"util -Dop=list-property-types\" command to list all property types.\n" +
+                "use the \"util -Dop=get-property-value-type\" command to get property value type of giving property " +
+                "type.\n" +
+                "use the \"util -Dop=sample-properties\" command to see examples of setting different types " +
+                "of properties.\n" +
+                "=>resolve:\n" +
+                "   -Dpath=, required, OS file path. The file should exist.\n" +
+                "=>destroy:\n" +
+                "   -Dpath=, required, OS file path. The file should exist.\n" +
+                "   -Dpool_id=, required, DAOS pool UUID.\n" +
+                "   -Dranks=, optional, pool ranks. Default is 0.\n" +
+                "   -Dserver_group=, optional, DAOS server group. Default is daos_server.\n" +
+                "   -Dpool_flags=, optional, pool access flags. Default is 2. [2(readwrite)].\n" +
+                "=>parse:\n" +
+                "   -Dinput=, required, attribute string.\n" +
+                "=>util\n" +
+                "   -Dop=, required, operation types. [list-object-types|list-property-types|" +
+                "get-property-value-type|sample-properties]\n" +
+                "   -Dprop_type=, required when op=get-property-value-type\n" +
+                "==================================================="
+                ;
+        return usage;
     }
 }
