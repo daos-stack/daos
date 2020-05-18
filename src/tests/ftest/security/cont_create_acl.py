5364a75c77b60db39db8e623b6329f6fa1b42d69
David Maldonado Moreno <david.maldonado.moreno@intel.com>
2020-05-18 22:02:34 +0000

    DAOS-3700 test: Test user tools: Create container with ACL
    Extended the container_create in daos_utils.py to include the acl-file
    parameter to enable current and future functional tests.
    Also, implemented the container_get_acl in the same daos_utils.py file
    to give support to the 'daos container get-acl' command.
    
    Implemented a new 'security_test_base.py' utility to implement
    general and common functions shared in pool_security_test_base.py:
    - acl_entry
    - acl_principal
    - add_del_user
    - create_acl_file
    
    Also, included the check_uuid_format function to check the correct
    UUID format.
    Updated the list of parameters in 'acl_entry' in order to accept the
    list of PERMISSIONS which could be different from test to test.
    
    Test-tag: pr,cont_create_acl
    
    Signed-off-by: David Maldonado Moreno <david.maldonado.moreno@intel.com>

--- src/tests/ftest/security/cont_create_acl.py
+++ src/tests/ftest/security/cont_create_acl.py
@@ -17,7 +17,7 @@
   GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
   The Government's rights to use, modify, reproduce, release, perform, display,
   or disclose this software are subject to the terms of the Apache License as
-  provided in Contract No. B609815.
+  provided in Contract No. 8F-30005.
   Any reproduction of computer software, computer software documentation, or
   portions thereof marked with this legend must also reproduce the markings.
 """
