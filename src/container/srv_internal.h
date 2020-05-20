fdf28b6c09de45fbfa445a71a80909da3d4bfb9c
Liang Zhen <liang.zhen@intel.com>
2020-05-20 15:42:24 +0800

    DAOS-4873 server: client stack on server
    DAOS server stack relies on client stack for many reasons:
    - rebuild, reintegration, rebalance
    - object enumeration
    - EC aggregation
    
    This patch moves those functions in src/iosrv/srv_cli.c to the
    components they belonging to:
    - src/object/srv_cli.c
    - src/container/srv_cli.c
    - src/pool/srv_cli.c
    
    It also includes a few cleanups:
    - prefix of client functions used by server (only) are changed
      from "dc_" to "dsc_"
    - dc_cont_local_open/close are moved to src/container/srv_cli.c
      because they are only used by server stack
    - dc_pool_local_open/close are moved to src/pool/srv_cli.c
      because they are only used by server stack
    - Functions exported to other modules should have dc/ds/dsc_ as
      prefix, a few container IV functions are renamed due to this.
    - Task body function of DAOS API are using _task as postfix to
      avoid naming conflicts (only renamed a few, the rest will be
      renamed in a follow-on patch)
    
    TODO:
    - add _task postfix for all task body functions of DAOS API
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>

--- src/container/srv_internal.h
+++ src/container/srv_internal.h
@@ -240,7 +240,12 @@ int ds_cont_iv_fini(void);
 int cont_iv_capability_update(void *ns, uuid_t cont_hdl_uuid, uuid_t cont_uuid,
 			      uint64_t flags, uint64_t sec_capas);
 int cont_iv_capability_invalidate(void *ns, uuid_t cont_hdl_uuid);
+int cont_iv_prop_fetch(struct ds_iv_ns *ns, uuid_t cont_hdl_uuid,
+		       daos_prop_t *cont_prop);
 int cont_iv_prop_update(void *ns, uuid_t iv_key_uuid, uuid_t cont_uuid,
 			daos_prop_t *prop);
+int cont_iv_snapshots_refresh(void *ns, uuid_t cont_uuid);
+int cont_iv_snapshots_update(void *ns, uuid_t cont_uuid,
+			     uint64_t *snapshots, int snap_count);
 
 #endif /* __CONTAINER_SRV_INTERNAL_H__ */
