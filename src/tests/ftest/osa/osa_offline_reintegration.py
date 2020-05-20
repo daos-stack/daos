a0f3375242bf1539f51343a512fcfe753480dabe
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-05-20 14:14:42 -0400

    DAOS-4749 test: Added multiple pools, data test Test-tag-hw-large: pr,hw,large offline_reintegration
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/osa/osa_offline_reintegration.py
+++ src/tests/ftest/osa/osa_offline_reintegration.py
@@ -22,8 +22,12 @@
   portions thereof marked with this legend must also reproduce the markings.
 """
 import time
+import random
+import ctypes
 from apricot import TestWithServers
 from test_utils_pool import TestPool
+from pydaos.raw import (DaosContainer, IORequest,
+                        DaosObj)
 
 
 class OSAOfflineReintegration(TestWithServers):
@@ -34,11 +38,25 @@ class OSAOfflineReintegration(TestWithServers):
 
     :avocado: recursive
     """
-
     def setUp(self):
         """Set up for test case."""
         super(OSAOfflineReintegration, self).setUp()
         self.dmg_command = self.get_dmg_command()
+        self.no_of_dkeys = self.params.get("no_of_dkeys", '/run/dkeys/*')[0]
+        self.no_of_akeys = self.params.get("no_of_akeys", '/run/akeys/*')[0]
+        self.record_length = self.params.get("length", '/run/record/*')
+
+    def get_pool_leader(self):
+        """Get the pool leader
+           Args: None
+           Returns : pool_leader (int)
+        """
+        out = []
+        kwargs = {"pool_uuid": self.pool.uuid}
+        out = self.dmg_command.get_output("pool_query", **kwargs)
+        pool_leader = out[0][3]
+        pool_leader = pool_leader.split('=')
+        return int(pool_leader[1])
 
     def get_pool_version(self):
         """Get the pool version
@@ -52,27 +70,111 @@ class OSAOfflineReintegration(TestWithServers):
         pool_version = pool_version.split('=')
         return int(pool_version[1])
 
+    def write_single_object(self):
+        self.pool.connect(2)
+        csum = self.params.get("enable_checksum", '/run/container/*')
+        container = DaosContainer(self.context)
+        input_param = container.cont_input_values
+        input_param.enable_chksum = csum
+        container.create(poh=self.pool.pool.handle,
+                         con_prop=input_param)
+        container.open()
+        obj = DaosObj(self.context, container)
+        obj.create(objcls=1)
+        obj.open()
+        ioreq = IORequest(self.context,
+                          container,
+                          obj, objtype=4)
+        self.log.info("Writing the Single Dataset")
+        record_index = 0
+        for dkey in range(self.no_of_dkeys):
+            for akey in range(self.no_of_akeys):
+                indata = ("{0}".format(str(akey)[0])
+                          * self.record_length[record_index])
+                d_key_value = "dkey {0}".format(dkey)
+                c_dkey = ctypes.create_string_buffer(d_key_value)
+                a_key_value = "akey {0}".format(akey)
+                c_akey = ctypes.create_string_buffer(a_key_value)
+                c_value = ctypes.create_string_buffer(indata)
+                c_size = ctypes.c_size_t(ctypes.sizeof(c_value))
+                ioreq.single_insert(c_dkey, c_akey, c_value, c_size)
+                record_index = record_index + 1
+                if record_index == len(self.record_length):
+                    record_index = 0
+
+    def run_offline_reintegration_test(self, num_pool, data=False):
+        """Run the offline reintegration without data.
+           Args: num_pool (int) : Total pools to create
+            for testing purpose.
+           Returns : None
+        """
+        # Create a pool
+        pool = {}
+        pool_uuid = []
+        target_list = []
+        exclude_servers = len(self.hostlist_servers) - 1
+        nvme_size = 54000000000
+        scm_size = 6000000000
+
+        # Exclude target : random two targets
+        n = random.randint(1, 7)
+        target_list.append(n)
+        target_list.append(n+1)
+        t_string = "{},{}".format(target_list[0], target_list[1])
+
+        # Exclude rank : two ranks other than rank 0.
+        n = random.randint(1, exclude_servers)
+        rank = n
+
+        for val in range(0, num_pool):
+            pool[val] = TestPool(self.context,
+                                 dmg_command=self.get_dmg_command())
+            pool[val].get_params(self)
+            # Split total SCM and NVME size for creating multiple pools.
+            pool[val].scm_size.value = int(scm_size / num_pool)
+            pool[val].nvme_size.value = int(nvme_size / num_pool)
+            pool[val].create()
+            pool_uuid.append(pool[val].uuid)
+            self.pool = pool[val]
+            if data is True:
+                self.write_single_object()
+
+        # Exclude and reintegrate the pool_uuid, rank and targets
+        for val in range(0, num_pool):
+            self.pool = pool[val]
+            self.pool.display_pool_daos_space("Pool space: Beginning")
+            pver_begin = self.get_pool_version()
+            self.log.info("Pool Version at the beginning %d", pver_begin)
+            output = self.dmg_command.pool_exclude(self.pool.uuid,
+                                                   rank, t_string)
+            self.log.info(output)
+            time.sleep(10)
+            pver_exclude = self.get_pool_version()
+            self.log.info("Pool Version after exclude %d", pver_exclude)
+            if pver_exclude <= pver_begin:
+                self.fail("Pool Version Error:  After exclude")
+            output = self.dmg_command.pool_reintegrate(self.pool.uuid,
+                                                       rank,
+                                                       t_string)
+            self.log.info(output)
+            time.sleep(10)
+            pver_reint = self.get_pool_version()
+            self.log.info("Pool Version after reintegrate %d", pver_reint)
+            if pver_reint <= pver_exclude:
+                self.fail("Pool Version Error: After reintegrate")
+
+        for val in range(0, num_pool):
+            display_string = "Pool{} space at the End".format(val)
+            self.pool = pool[val]
+            self.pool.display_pool_daos_space(display_string)
+            pool[val].destroy()
+
     def test_osa_offline_reintegration(self):
         """Test ID: DAOS-4749
         Test Description: Validate Offline Reintegration
 
         :avocado: tags=all,pr,hw,large,osa,offline_reintegration
         """
-        # Create a pool
-        self.pool = TestPool(self.context, dmg_command=self.dmg_command)
-        self.pool.get_params(self)
-        self.pool.create()
-        self.pool.display_pool_daos_space("Pool space at the Beginning")
-        pversion_begin = self.get_pool_version()
-        self.log.info("Pool Version at the beginning %d", pversion_begin)
-        # Exclude a rank 2
-        output = self.dmg_command.pool_exclude(self.pool.uuid, 2, "2,4")
-        self.log.info(output)
-        time.sleep(5)
-        pversion_exclude = self.get_pool_version()
-        self.log.info("Pool Version at the beginning %d", pversion_exclude)
-        output = self.dmg_command.pool_reintegrate(self.pool.uuid, 2, "2,4")
-        self.log.info(output)
-        time.sleep(5)
-        pversion_reintegrate = self.get_pool_version()
-        self.log.info("Pool Version at the beginning %d", pversion_reintegrate)
+        for x in range(1, 4):
+            self.run_offline_reintegration_test(x)
+        self.run_offline_reintegration_test(1, True)
