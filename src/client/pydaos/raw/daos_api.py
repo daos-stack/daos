261e7425dfb22605a1caddca900e596988ade276
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-05-29 13:44:55 -0400

    DAOS-4436 test: Enable checksum under ior/daos_api
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/client/pydaos/raw/daos_api.py
+++ src/client/pydaos/raw/daos_api.py
@@ -1443,6 +1443,7 @@ class DaosContainer(object):
         # 2. Enable checksum,
         # 3. Server Verfiy
         # 4. Chunk Size Allocation.
+        self.cont_input_values.enable_chksum = True
         if ((self.cont_input_values.type != "Unknown")
                 and (self.cont_input_values.enable_chksum is False)):
             # Only type like posix, hdf5 defined.
