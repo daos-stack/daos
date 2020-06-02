de3c6dd3e4e7bde352af2c33fd235c8aab834b63
Li Wei <wei.g.li@intel.com>
2020-06-02 14:05:11 +0800

    DAOS-3932 container: Fix CONT_CLOSE -DER_EVICTEDs
    A CONT_CLOSE may return -DER_EVICTED if during the CONT_TGT_CLOSE bcast
    a pool server terminates and gets excluded from the pool map. This patch
    adds -DER_EVICTED to daos_rpc_retryable_rc, so that all metadata RPCs
    get the fix.
    
    Signed-off-by: Li Wei <wei.g.li@intel.com>

--- src/include/daos/rpc.h
+++ src/include/daos/rpc.h
@@ -214,7 +214,7 @@ static inline bool
 daos_rpc_retryable_rc(int rc)
 {
 	return daos_crt_network_error(rc) || rc == -DER_TIMEDOUT ||
-	       rc == -DER_GRPVER;
+	       rc == -DER_GRPVER || rc == -DER_EVICTED;
 }
 
 /* Determine if the RPC is from a client. If not, it's from a server rank. */
