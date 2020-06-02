5a51a1f55473f454fc1b752c181db9cd3b725be3
John E Malmberg <john.e.malmberg@intel.com>
2020-06-02 15:55:50 -0500

    CORCI-918 Build: Refactor RPM build to simplify
    Skip-checkpatch: true
    Skip-python-bandit: false
    //Skip-centos7-gcc: true
    Skip-ubuntu-clang: true
    Skip-leap15-icc: true
    //Skip-run_test: true
    Skip-coverity-test: true
    Skip-func-test: true
    Skip-func-hw-test: true
    //Skip-test: true
    //Skip-leap15-rpm: true
    //Skip-scan-centos-rpms: true
    
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>

--- ci/rpm/test_daos.sh
+++ ci/rpm/test_daos.sh
@@ -7,7 +7,7 @@ set -uex
 if git show -s --format=%B | grep "^Skip-test: true"; then
   exit 0
 fi
-nodelist=("${NODELIST//,/ }")
+nodelist=(${NODELIST//,/ })
 src/tests/ftest/config_file_gen.py -n "${nodelist[0]}" \
   -a /tmp/daos_agent.yml -s /tmp/daos_server.yml
 src/tests/ftest/config_file_gen.py -n "${nodelist[0]}" -d /tmp/dmg.yml
@@ -21,4 +21,4 @@ scp -i ci_key /tmp/daos_agent.yml /tmp/dmg.yml /tmp/daos_server.yml \
 ssh "$SSH_KEY_ARGS" jenkins@"${nodelist[0]}" \
   "NODE=${nodelist[0]}                       \
    DAOS_PKG_VERSION=$DAOS_PKG_VERSION        \
-   $(cat "$mydir/rpm_test_daos_test_node.sh")"
+   $(cat "$mydir/test_daos_node.sh")"
