f17dfa9c97b6d2b963f906c2ccfd2fa92ca938d7
jiafu zhang <jiafu.zhang@intel.com>
2020-06-11 08:17:29 +0000

    add apache copyright
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- src/client/java/hadoop-daos/src/test/java/io/daos/fs/hadoop/DaosFileSystemTest.java
+++ src/client/java/hadoop-daos/src/test/java/io/daos/fs/hadoop/DaosFileSystemTest.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,6 @@ package io.daos.fs.hadoop;
 
 import io.daos.dfs.DaosFsClient;
 import io.daos.dfs.DaosUns;
-import io.daos.dfs.DaosUtils;
 import io.daos.dfs.DunsInfo;
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
