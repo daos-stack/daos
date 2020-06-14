8fddf9d2322f457677a3c4618d8e0c62426e5734
jiafu zhang <jiafu.zhang@intel.com>
2020-06-14 04:25:26 +0000

    sync code
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- src/client/java/daos-java/src/main/java/io/daos/obj/DaosObject.java
+++ src/client/java/daos-java/src/main/java/io/daos/obj/DaosObject.java
@@ -26,6 +26,7 @@ package io.daos.obj;
 import io.daos.BufferAllocator;
 import io.daos.Constants;
 import io.daos.DaosIOException;
+import io.daos.obj.attr.DaosObjectAttribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.nio.ch.DirectBuffer;
@@ -34,6 +35,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.List;
 
+/**
+ * //TODO: buffer management
+ */
 public class DaosObject {
 
   private DaosObjClient client;
@@ -87,30 +91,116 @@ public class DaosObject {
     }
   }
 
-  public void punchObject() throws IOException {
+  public void punch() throws IOException {
     checkOpen();
     client.punchObject(objectPtr, 0);
   }
 
-  public void punchObjectDkeys(List<String> dkeys) throws IOException {
-    checkOpen();
-    if (dkeys.isEmpty()) {
-      return;
-    }
+  private ByteBuffer encodeKeys(List<String> keys) throws IOException {
     int bufferLen = 0;
-    for (String key : dkeys) {
+    for (String key : keys) {
       bufferLen += (key.length() + 4);
     }
     if (bufferLen == 0) {
+      return null;
+    }
+    // encode keys to buffer
+    ByteBuffer buffer = BufferAllocator.directBuffer(bufferLen);
+    buffer.order(Constants.DEFAULT_ORDER);
+    int capacity = buffer.capacity();
+    bufferLen = 0;
+    int keyLen;
+    for (String key : keys) {
+      byte[] bytes = key.getBytes(Constants.KEY_CHARSET);
+      keyLen = (4 + bytes.length);
+      if ((bufferLen + keyLen) > capacity) { // in case there is non ascii char
+        capacity *= 2;
+        ByteBuffer newBuffer = BufferAllocator.directBuffer(capacity);
+        newBuffer.order(Constants.DEFAULT_ORDER);
+        buffer.flip();
+        newBuffer.put(buffer);
+        buffer = newBuffer;
+      }
+      buffer.putInt(bytes.length).put(bytes);
+      bufferLen += keyLen;
+    }
+    buffer.flip();
+    return buffer;
+  }
+
+  public void punchDkeys(List<String> dkeys) throws IOException {
+    checkOpen();
+    ByteBuffer buffer = encodeKeys(dkeys);
+    if (buffer == null) {
       log.warn("no dkeys specified when punch object dkeys");
       return;
     }
-    ByteBuffer buffer = BufferAllocator.directBuffer(bufferLen);
-    buffer.order(Constants.DEFAULT_ORDER);
-    for (String key : dkeys) {
-      buffer.putInt(key.length()).put(key.getBytes());
+    client.punchObjectDkeys(objectPtr, 0, dkeys.size(), ((DirectBuffer)buffer).address(), buffer.limit());
+  }
+
+  public void punchAkeys(String dkey, List<String> akeys) throws IOException {
+    checkOpen();
+    if (akeys.isEmpty()) {
+      log.warn("no akeys specified when punch object akeys");
+      return;
     }
-    client.punchObjectDkeys(objectPtr, 0, dkeys.size(), ((DirectBuffer)buffer).address(), bufferLen);
+    int nbrOfAkyes = akeys.size();
+    akeys.add(0, dkey);
+    ByteBuffer buffer = encodeKeys(akeys);
+    akeys.remove(0);
+    client.punchObjectAkeys(objectPtr, 0, nbrOfAkyes, ((DirectBuffer)buffer).address(), buffer.limit());
+  }
+
+  public DaosObjectAttribute queryAttribute() throws IOException {
+    checkOpen();
+    byte[] bytes = client.queryObjectAttribute(objectPtr);
+    return DaosObjectAttribute.parseFrom(bytes);
+  }
+
+  public void fetch(IOValueDesc desc) throws IOException {
+    checkOpen();
+    desc.encode();
+
+    client.fetchObject(objectPtr, 0, desc.getNbrOfEntries(),
+        ((DirectBuffer)desc.getDescBuffer()).address(),
+      ((DirectBuffer)desc.getValueBuffer()).address());
+  }
+
+  public void update(IOValueDesc desc) throws IOException {
+    checkOpen();
+    desc.encode();
+
+    client.updateObject(objectPtr, 0, desc.getNbrOfEntries(), ((DirectBuffer)desc.getDescBuffer()).address(),
+      ((DirectBuffer)desc.getValueBuffer()).address());
+  }
+
+  /**
+   * list object dkeys.
+   *
+   * @param desc
+   * @return
+   * @throws IOException
+   */
+  public List<String> listDkeys(IOKeyDesc desc) throws IOException {
+    checkOpen();
+    desc.encode();
+
+    // TODO: check completion of list
+    client.listObjectDkeys(objectPtr, ((DirectBuffer)desc.getDescBuffer()).address(),
+      ((DirectBuffer)desc.getKeyBuffer()).address(), ((DirectBuffer)desc.getAnchorBuffer()).address(),
+      desc.getNbrOfKeysPerCall());
+    return null;
+  }
+
+  public List<String> listAkeys(IOKeyDesc desc) throws IOException {
+    checkOpen();
+    desc.encode();
+
+    // TODO: check completion of list
+    client.listObjectAkeys(objectPtr, ((DirectBuffer)desc.getDescBuffer()).address(),
+      ((DirectBuffer)desc.getKeyBuffer()).address(), ((DirectBuffer)desc.getAnchorBuffer()).address(),
+      desc.getNbrOfKeysPerCall());
+    return null;
   }
 
   private void checkOpen() throws IOException {
