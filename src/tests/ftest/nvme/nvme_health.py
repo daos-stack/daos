da785ade21de4907a37e552942c8be4b932feee5
Samir Raval <samir.raval@intel.com>
2020-06-16 16:50:48 -0700

    Update nvme_health.py

--- src/tests/ftest/nvme/nvme_health.py
+++ src/tests/ftest/nvme/nvme_health.py
@@ -22,7 +22,6 @@
   portions thereof marked with this legend must also reproduce the markings.
 '''
 import os
-from apricot import TestWithServers
 from nvme_utils import ServerFillUp, get_device_ids
 from test_utils_pool import TestPool
 from dmg_utils import DmgCommand
