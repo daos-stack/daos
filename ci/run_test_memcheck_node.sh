69ebde8b1939bc6d2af0bf2554386e0058e84c03
Marcela Rosales <marcela.a.rosales.jimenez@intel.com>
2020-07-01 18:14:49 +0000

    remove empty.supp, instead use test_memcheck.supp
    Signed-off-by: Marcela Rosales <marcela.a.rosales.jimenez@intel.com>

--- ci/run_test_memcheck_node.sh
+++ ci/run_test_memcheck_node.sh
@@ -24,6 +24,6 @@ sudo ln -s "$SL_PREFIX/include"  /usr/share/spdk/include
 
 # run_test.sh with valgrind memcheck
 cd "$DAOS_BASE"
-IS_CI=true OLD_CI=false RUN_TEST_VALGRIND=memcheck VALGRIND_SUPP=utils/empty.supp utils/run_test.sh
+IS_CI=true OLD_CI=false RUN_TEST_VALGRIND=memcheck utils/run_test.sh
 mkdir -p valgrind_memcheck_results
 mv results-*-memcheck.xml valgrind_memcheck_results
