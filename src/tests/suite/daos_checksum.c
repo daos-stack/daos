faec184c1e75274b52647ac69d0a34e1b18643d6
Ryon Jensen <ryon.jensen@intel.com>
2020-07-01 08:45:48 -0600

    DAOS-5065 csum: Bug Fixes Discovered in CI (#2976)
    Several bug fixes discovered by a CI and IOR run.
    
    - Fix for request of non-existent data.
    - In VOS check if iod_csum has csum infos before returning. This
      protects against punches when csum enabled.
    - Fix case where second half of chunk is requested. Logic was incomplete
      and only looked at rest of chunk to determine if biov extent was
      whole chunk.
    - More tracing to help troubleshoot.
    
    Signed-off-by: Ryon Jensen <ryon.jensen@intel.com>

--- src/tests/suite/daos_checksum.c
+++ src/tests/suite/daos_checksum.c
@@ -854,6 +854,21 @@ overlapping_after_first_chunk(void **state)
 }
 
 static void
+request_second_half_of_chunk(void **state)
+{
+	ARRAY_UPDATE_FETCH_TESTCASE(state, {
+		.chunksize = 1024,
+		.csum_prop_type = dts_csum_prop_type,
+		.server_verify = false,
+		.rec_size = 1,
+		.recx_cfgs = {
+			{.idx = 0, .nr = 1024, .data = "12345678"},
+		},
+		.fetch_recx = {.rx_idx = 512, .rx_nr = 512},
+	});
+}
+
+static void
 extents_with_holes_1(void **state)
 {
 	ARRAY_UPDATE_FETCH_TESTCASE(state, {
@@ -1370,6 +1385,40 @@ many_iovs_with_single_values(void **state)
 }
 
 static void
+request_non_existent_data(void **state)
+{
+	int			rc;
+	struct csum_test_ctx	ctx;
+
+	setup_from_test_args(&ctx, *state);
+	setup_cont_obj(&ctx, dts_csum_prop_type, false, 1024, dts_csum_oc);
+	setup_simple_data(&ctx);
+
+	ctx.update_iod.iod_recxs[0].rx_idx = 1;
+	ctx.update_iod.iod_recxs[0].rx_nr = 1;
+
+	rc = daos_obj_update(ctx.oh, DAOS_TX_NONE, DAOS_COND_DKEY_INSERT,
+			     &ctx.dkey, 1, &ctx.update_iod,
+			     &ctx.update_sgl, NULL);
+	assert_success(rc);
+
+	ctx.fetch_iod.iod_recxs[0].rx_idx = 0;
+	ctx.fetch_iod.iod_recxs[0].rx_nr = 1;
+	ctx.fetch_iod.iod_recxs[1].rx_idx = 1;
+	ctx.fetch_iod.iod_recxs[1].rx_nr = 1;
+	ctx.fetch_iod.iod_recxs[2].rx_idx = 3;
+	ctx.fetch_iod.iod_recxs[2].rx_nr = 1;
+	ctx.fetch_iod.iod_recxs[3].rx_idx = 4;
+	ctx.fetch_iod.iod_recxs[3].rx_nr = 1;
+	ctx.fetch_iod.iod_nr = 4;
+
+	rc = daos_obj_fetch(ctx.oh, DAOS_TX_NONE, 0, &ctx.dkey,
+			    1, &ctx.fetch_iod,
+			    &ctx.fetch_sgl, NULL, NULL);
+	assert_success(rc);
+}
+
+static void
 test_update_fetch_a_key(void **state)
 {
 	key_csum_fetch_update(state,
@@ -1527,6 +1576,8 @@ static const struct CMUnitTest csum_tests[] = {
 		record_size_larger_than_chunksize),
 	CSUM_TEST("DAOS_CSUM03.4: Setup multiple overlapping/unaligned extents",
 		  overlapping_after_first_chunk),
+	CSUM_TEST("DAOS_CSUM03.5: Request the second half of a chunk",
+		  request_second_half_of_chunk),
 	CSUM_TEST("DAOS_CSUM04.1: With holes between extents. All in 1 chunk",
 		  extents_with_holes_1),
 	CSUM_TEST("DAOS_CSUM04.2: With holes at beginning and end. of extent."
@@ -1552,6 +1603,8 @@ static const struct CMUnitTest csum_tests[] = {
 	CSUM_TEST("DAOS_CSUM10: Enumerate A Keys", test_enumerate_a_key),
 	CSUM_TEST("DAOS_CSUM11: Enumerate D Keys", test_enumerate_d_key),
 	CSUM_TEST("DAOS_CSUM12: Many IODs", many_iovs_with_single_values),
+	CSUM_TEST("DAOS_CSUM13: Request non existent data",
+		  request_non_existent_data),
 
 	EC_CSUM_TEST("DAOS_EC_CSUM00: csum disabled", checksum_disabled),
 	EC_CSUM_TEST("DAOS_EC_CSUM01: simple update with server side verify",
