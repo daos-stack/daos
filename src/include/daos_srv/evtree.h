e12f9bea3a370a50911a917dc7ce0e6db56d7d73
Jeff Olivier <jeffrey.v.olivier@intel.com>
2020-07-02 10:59:36 -0600

    DAOS-5238 vos: Add evt_remove_all and VOS_OF_REMOVE flag (#3026)
    * DAOS-5238 vos: Add evt_remove_all and VOS_OF_REMOVE flag
    
    Add an API flag to remove extents within a range and an
    associated evtree API to do it.  This simply finds all
    matching extents and removes them using evt_delete
    
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/include/daos_srv/evtree.h
+++ src/include/daos_srv/evtree.h
@@ -555,6 +555,21 @@ int evt_delete(daos_handle_t toh, const struct evt_rect *rect,
 	       struct evt_entry *ent);
 
 /**
+ * Remove all whole extents in the specified range written prior to the
+ * specified epoch. The range must only cover whole extents.   If any
+ * partial extents are in the range, the function fails.
+ *
+ * \param toh		[IN]	The tree open handle
+ * \param ext		[IN]	The extent range
+ * \param epoch		[IN]	High epoch
+ *
+ * \return	0		Success
+ *		-DER_NOPERM	Partial overlaps found
+ */
+int evt_remove_all(daos_handle_t toh, const struct evt_extent *ext,
+		   daos_epoch_t epoch);
+
+/**
  * Search the tree and return all visible versioned extents which overlap with
  * \a rect to \a ent_array.
  *
