63d052df7aab6f2a040be9ff5ab938160f8e7831
Ashley Pittman <ashley.m.pittman@intel.com>
2020-07-07 16:31:06 +0100

    Add o-direct usage to nlt.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/node_local_test.py
+++ utils/node_local_test.py
@@ -1021,6 +1021,15 @@ def run_il_test(server, conf):
     # Read it from within a container
     ret = il_cmd(dfuse, ['md5sum', os.path.join(dirs[-1], 'bash')])
     assert ret.returncode == 0
+    ret = subprocess.run(['dd',
+                          'if={}'.format(os.path.join(dirs[-1], 'bash')),
+                          'of={}'.format(os.path.join(dirs[-1], 'bash_copy')),
+                          'iflag=direct',
+                          'oflag=direct',
+                          'bs=128k'])
+                          
+    print(ret)
+    assert ret.returncode == 0
     dfuse.stop()
 
 def run_in_fg(server, conf):
