90bde8f8e63bdfb841a4edc443ba886933d840a5
Michael MacDonald <mjmac.macdonald@intel.com>
2020-07-08 17:21:01 -0400

    ioserver updates

--- src/mgmt/srv_drpc.c
+++ src/mgmt/srv_drpc.c
@@ -257,6 +257,61 @@ ds_mgmt_drpc_get_attach_info(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 }
 
 void
+ds_mgmt_drpc_group_update(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
+{
+	Mgmt__GroupUpdateReq	*req = NULL;
+	Mgmt__GroupUpdateResp	resp = MGMT__GROUP_UPDATE_RESP__INIT;
+	struct mgmt_grp_up_in	in = {};
+	uint8_t			*body;
+	size_t			 len;
+	int			 rc, i;
+
+	/* Unpack the inner request from the drpc call body */
+	req = mgmt__group_update_req__unpack(
+		NULL, drpc_req->body.len, drpc_req->body.data);
+
+	if (req == NULL) {
+		drpc_resp->status = DRPC__STATUS__FAILED_UNMARSHAL_PAYLOAD;
+		D_ERROR("Failed to unpack req (group_update)\n");
+		return;
+	}
+
+	D_INFO("Received request to update group map\n");
+
+	D_ALLOC_ARRAY(in.gui_servers, req->n_servers);
+	if (in.gui_servers == NULL) {
+		rc = -DER_NOMEM;
+		goto out;
+	}
+
+	for (i = 0; i < req->n_servers; i++) {
+		in.gui_servers[i].se_rank = req->servers[i]->rank;
+		in.gui_servers[i].se_uri = req->servers[i]->uri;
+	}
+	in.gui_n_servers = req->n_servers;
+	in.gui_map_version = req->map_version;
+
+	rc = ds_mgmt_group_update_handler(&in);
+out:
+	if (in.gui_servers != NULL)
+		D_FREE(in.gui_servers);
+
+	resp.status = rc;
+	len = mgmt__group_update_resp__get_packed_size(&resp);
+	D_ALLOC(body, len);
+	if (body == NULL) {
+		drpc_resp->status = DRPC__STATUS__FAILED_MARSHAL;
+		D_ERROR("Failed to allocate drpc response body\n");
+	} else {
+		mgmt__group_update_resp__pack(&resp, body);
+		drpc_resp->body.len = len;
+		drpc_resp->body.data = body;
+	}
+
+	mgmt__group_update_req__free_unpacked(req, NULL);
+}
+
+void
 ds_mgmt_drpc_join(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 {
 	Mgmt__JoinReq		*req = NULL;
@@ -280,6 +335,7 @@ ds_mgmt_drpc_join(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 	D_INFO("Received request to join\n");
 
 	in.ji_rank = req->rank;
+	in.ji_map_version = req->map_version;
 	in.ji_server.sr_flags = SERVER_IN;
 	in.ji_server.sr_nctxs = req->nctxs;
 	rc = uuid_parse(req->uuid, in.ji_server.sr_uuid);
@@ -522,6 +578,7 @@ ds_mgmt_drpc_pool_destroy(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 	Mgmt__PoolDestroyReq	*req = NULL;
 	Mgmt__PoolDestroyResp	 resp = MGMT__POOL_DESTROY_RESP__INIT;
 	uuid_t			 uuid;
+	d_rank_list_t		*svc_ranks;
 	uint8_t			*body;
 	size_t			 len;
 	int			 rc;
@@ -546,15 +603,22 @@ ds_mgmt_drpc_pool_destroy(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 		D_GOTO(out, rc = -DER_INVAL);
 	}
 
+	svc_ranks = uint32_array_to_rank_list(req->svcreps, req->n_svcreps);
+	if (svc_ranks == NULL)
+		D_GOTO(out, rc = -DER_NOMEM);
+
 	/* Sys and force params are currently ignored in receiver. */
-	rc = ds_mgmt_destroy_pool(uuid, req->sys,
+	rc = ds_mgmt_destroy_pool(uuid, svc_ranks, req->sys,
 				  (req->force == true) ? 1 : 0);
 	if (rc != 0) {
 		D_ERROR("Failed to destroy pool %s: "DF_RC"\n", req->uuid,
 			DP_RC(rc));
-		goto out;
+		goto out_ranks;
 	}
 
+out_ranks:
+	if (svc_ranks)
+		d_rank_list_free(svc_ranks);
 out:
 	resp.status = rc;
 	len = mgmt__pool_destroy_resp__get_packed_size(&resp);
@@ -1496,6 +1560,7 @@ ds_mgmt_drpc_pool_query(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 	Mgmt__StorageUsageStats	nvme = MGMT__STORAGE_USAGE_STATS__INIT;
 	Mgmt__PoolRebuildStatus	rebuild = MGMT__POOL_REBUILD_STATUS__INIT;
 	uuid_t			uuid;
+	d_rank_list_t		*svc_ranks;
 	daos_pool_info_t	pool_info = {0};
 	size_t			len;
 	uint8_t			*body;
@@ -1515,11 +1580,15 @@ ds_mgmt_drpc_pool_query(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 		D_GOTO(out, rc = -DER_INVAL);
 	}
 
+	svc_ranks = uint32_array_to_rank_list(req->svcreps, req->n_svcreps);
+	if (svc_ranks == NULL)
+		D_GOTO(out, rc = -DER_NOMEM);
+
 	pool_info.pi_bits = DPI_ALL;
-	rc = ds_mgmt_pool_query(uuid, &pool_info);
+	rc = ds_mgmt_pool_query(uuid, svc_ranks, &pool_info);
 	if (rc != 0) {
 		D_ERROR("Failed to query the pool, rc=%d\n", rc);
-		D_GOTO(out, rc);
+		D_GOTO(out_ranks, rc);
 	}
 
 	/* Populate the response */
@@ -1542,6 +1611,9 @@ ds_mgmt_drpc_pool_query(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 	pool_rebuild_status_from_info(&rebuild, &pool_info.pi_rebuild_st);
 	resp.rebuild = &rebuild;
 
+out_ranks:
+	if (svc_ranks)
+		d_rank_list_free(svc_ranks);
 out:
 	resp.status = rc;
 
