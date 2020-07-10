c0d87ff75166cb833a5edf8aed24a2c8e8973063
amjustin <amanda.justiniano-pagn@intel.com>
2020-07-10 15:28:13 -0400

    DAOS-3925 tests: dmg storage query tests (#2251)
    Add tests for:
      * storage query list-devices
      * storage query list-pools
      * storage query list-devices --health
      * storage query device-state
      * storage set nvme-faulty
    
    Tests are basic positive tests, concentrating on command
    output verification.
    
    In addition, I am updating the command_utils.get_output() method
    to include a new argument regex_method that will allow the user to
    decouple the executed method from the regex used to parse the
    information. To supplement this, I am also adding a new method
    command_utils.parse_output() to remove the parsing logic from
    get_output() method.
    
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/util/control_test_base.py
+++ src/tests/ftest/util/control_test_base.py
@@ -23,9 +23,31 @@
 """
 
 from avocado import fail_on
+from collections import defaultdict
 from apricot import TestWithServers
 from general_utils import get_host_data
 from command_utils import CommandFailure
+
+
+def cleanup_output(output):
+    """Cleanup output that is of this form:
+        [(host, "", ""), ("", "data1", "data2"), ("", "data1", "data2")]
+
+    Args:
+        output (list): output to be parsed.
+
+    Returns:
+        dict: integrated dictionary containing information for each host.
+
+    """
+    host = None
+    info = defaultdict(list)
+    for item in output:
+        if item[0]:
+            host = item[0]
+            continue
+        info[host].append(item[1:])
+    return info
 
 
 class ControlTestBase(TestWithServers):
@@ -45,9 +67,9 @@ class ControlTestBase(TestWithServers):
         self.dmg = self.get_dmg_command()
 
     @fail_on(CommandFailure)
-    def get_dmg_output(self, method_name, **kwargs):
+    def get_dmg_output(self, method_name, regex_method=None, **kwargs):
         """Run the dmg command."""
-        return self.dmg.get_output(method_name, **kwargs)
+        return self.dmg.get_output(method_name, regex_method, **kwargs)
 
     def get_superblock_info(self, sp_file, sp_value):
         """Get the superblock information for each host.
@@ -67,3 +89,45 @@ class ControlTestBase(TestWithServers):
         error = "Error obtaining superblock info: {}".format(sp_value)
 
         return get_host_data(self.dmg.hostlist, cmd, text, error, 20)
+
+    def get_device_info(self, rank=None, health=None):
+        """Query storage device information.
+
+        Args:
+            rank (int, optional): Limit response to devices on this rank.
+                Defaults to None.
+            health (bool, optional): Include device health in response.
+                Defaults to false.
+
+        Returns:
+            list: device info containing lists with queried device information.
+
+        """
+        info = None
+        kwargs = {"rank": rank, "health": health}
+        if health:
+            info = self.get_dmg_output(
+                "storage_query_list_devices",
+                "storage_query_device_health",
+                **kwargs)
+        else:
+            info = cleanup_output(
+                self.get_dmg_output("storage_query_list_devices", **kwargs))
+        return info
+
+    def get_pool_info(self, uuid=None, rank=None, verbose=False):
+        """Query pool information.
+
+        Args:
+            uuid (str): Device UUID to query. Defaults to None.
+            rank (int, optional): Limit response to devices on this rank.
+                Defaults to None.
+            verbose (bool, optional): create verbose output. Defaults to False.
+
+        Returns:
+            dict: Dictionary containing pool information for each host.
+
+        """
+        kwargs = {"uuid": uuid, "rank": rank, "verbose": verbose}
+        return cleanup_output(
+            self.get_dmg_output("storage_query_list_pools", **kwargs))
