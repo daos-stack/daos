a857ffa71f1f0be122ea1152bcf42a417f48ee9a
Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>
2020-07-15 13:45:59 +0000

    DAOS-5152 test: Enable debug logs for daos_test
    test: Enable D_LOG_MASK=DEBUG, DD_MASK=mgmt,io,md,epc,rebuild
    for daos_test
    
    Skip-run_test: true
    Skip-func-test: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-large: true
    Signed-off-by: Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>

--- src/tests/ftest/util/daos_core_base.py
+++ src/tests/ftest/util/daos_core_base.py
@@ -66,7 +66,7 @@ class DaosCoreBase(TestWithServers):
         dmg_config_file = dmg.yaml.filename
 
         cmd = "{} {} -n {} -x D_LOG_FILE={} \
-            -x D_LOG_MASK=DEBUG -x DD_MASK=mgmt,io,md,epc,rebuild \
+            -x D_LOG_MASK=DEBUG -x DD_MASK=io,md,epc,rebuild \
             {} -s {} -{} {}".format(self.orterun, self.client_mca,
                                     num_clients,
                                     get_log_file(self.client_log),
