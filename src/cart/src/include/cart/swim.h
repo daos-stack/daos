62d5e7d42d0c0e1d8136d57ae9d5db2e9484336a
David Maldonado Moreno <david.maldonado.moreno@intel.com>
2020-07-21 21:55:33 +0000

    Fixing dates of licenses in some source files.
    Signed-off-by: David Maldonado Moreno <david.maldonado.moreno@intel.com>

--- src/cart/src/include/cart/swim.h
+++ src/cart/src/include/cart/swim.h
@@ -1,5 +1,6 @@
 /*
- * (C) Copyright 2016-2020 Intel Corporation.
+ * Copyright (c) 2016 UChicago Argonne, LLC
+ * (C) Copyright 2018-2020 Intel Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/cart/src/include/gurt/dlog.h
+++ src/cart/src/include/gurt/dlog.h
@@ -19,6 +19,33 @@
  * provided in Contract No. 8F-30005.
  * Any reproduction of computer software, computer software documentation, or
  * portions thereof marked with this legend must also reproduce the markings.
+ *
+ * Portions of this file are based on The Self-* Storage System Project
+ * Copyright (c) 2004-2011, Carnegie Mellon University.
+ * All rights reserved.
+ * http://www.pdl.cmu.edu/  (Parallel Data Lab at Carnegie Mellon)
+ *
+ * This software is being provided by the copyright holders under the
+ * following license. By obtaining, using and/or copying this software,
+ * you agree that you have read, understood, and will comply with the
+ * following terms and conditions:
+ *
+ * Permission to reproduce, use, and prepare derivative works of this
+ * software is granted provided the copyright and "No Warranty" statements
+ * are included with all reproductions and derivative works and associated
+ * documentation. This software may also be redistributed without charge
+ * provided that the copyright and "No Warranty" statements are included
+ * in all redistributions.
+ *
+ * NO WARRANTY. THIS SOFTWARE IS FURNISHED ON AN "AS IS" BASIS.
+ * CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER
+ * EXPRESSED OR IMPLIED AS TO THE MATTER INCLUDING, BUT NOT LIMITED
+ * TO: WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY
+ * OF RESULTS OR RESULTS OBTAINED FROM USE OF THIS SOFTWARE. CARNEGIE
+ * MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT
+ * TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+ * COPYRIGHT HOLDERS WILL BEAR NO LIABILITY FOR ANY USE OF THIS SOFTWARE
+ * OR DOCUMENTATION.
  */
 
 /**
