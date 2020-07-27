54511ef8018bcccb130968648100006ce86a51c4
Saurabh Tandan <saurabh.tandan@intel.com>
2020-07-27 23:05:10 +0000

    Modified as per suggestions.
    Test-tag-hw-small: pr,hw,small dfusesparsefile
    
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/io/dfuse_sparse_file.py
+++ src/tests/ftest/io/dfuse_sparse_file.py
@@ -22,13 +22,10 @@
   portions thereof marked with this legend must also reproduce the markings.
 """
 
-#import subprocess
 from getpass import getuser
 import paramiko
 
 from general_utils import get_remote_file_size
-#from ClusterShell.NodeSet import NodeSet
-#from command_utils import CommandFailure
 from ior_test_base import IorTestBase
 
 
@@ -42,37 +39,12 @@ class DfuseSparseFile(IorTestBase):
         """Initialize a DfuseSparseFile object."""
         super(DfuseSparseFile, self).__init__(*args, **kwargs)
         self.space_before = None
-#        self.ret = None
-
-    def tearDown(self):
-        """Tear down each test case."""
-        try:
-            if self.dfuse:
-                self.dfuse.stop()
-        finally:
-            # Stop the servers and agents
-            super(DfuseSparseFile, self).tearDown()
-
-    def get_nvme_free_space(self, display=True):
-        """Display pool free space.
-
-          Args:
-            display (bool): boolean to display output of free space.
-
-          Returns:
-            free_space_nvme (int): Free space available in nvme.
-        """
-        free_space_nvme = self.pool.get_pool_free_space("nvme")
-        if display:
-            self.log.info("Free nvme space: %s", free_space_nvme)
-
-        return free_space_nvme
 
     def test_dfusesparsefile(self):
         """Jira ID: DAOS-3768
 
         Test Description:
-            Purpose of this test is to mount dfuse and verify behaviour
+            Purpose of this test is to mount dfuse and verify behavior
             when reading sparse file.
         Use cases:
             Create Pool
@@ -83,7 +55,7 @@ class DfuseSparseFile(IorTestBase):
             Write to it's first byte.
             Then, write to it's 1024th Byte
             Verify both the bytes got written with desired data.
-            Verify, the bytes between 1std byte and 1024th byte are empty.
+            Verify, the bytes between 1st byte and 1024th byte are empty.
             Now try to read the file from it's last 512 bytes till EOF.
             This should return EOF, otherwise fail the test.
         :avocado: tags=all,hw,daosio,small,full_regression,dfusesparsefile
@@ -94,7 +66,7 @@ class DfuseSparseFile(IorTestBase):
         self._start_dfuse()
 
         # get scm space before write
-        self.space_before = self.get_nvme_free_space()
+        self.space_before = self.pool.get_pool_free_space("nvme")
 
         # create large fle and perform write to it so that if goes out of
         # space.
@@ -117,39 +89,36 @@ class DfuseSparseFile(IorTestBase):
         file_obj = sftp.open(sparse_file, 'r+')
         # set file size to max available nvme size
         sftp.truncate(sparse_file, self.space_before)
+        fsize_after_truncate = get_remote_file_size(self.hostlist_clients[0],
+                                                    sparse_file)
         self.log.info("File size (in bytes) after truncate: %s",
-                      get_remote_file_size(
-                          self.hostlist_clients[0], sparse_file))
+                      fsize_after_truncate)
         # verifying the file size got set to desired value
-        self.assertTrue(
-            get_remote_file_size(
-                self.hostlist_clients[0], sparse_file) == self.space_before)
+        self.assertTrue(fsize_after_truncate == self.space_before)
 
         # write to the first byte of the file with char 'A'
         dd_first_byte = u"echo 'A' | dd conv=notrunc of={} bs=1 count=1".\
                         format(sparse_file)
         self.execute_cmd(dd_first_byte)
+        fsize_write_1stbyte = get_remote_file_size(self.hostlist_clients[0],
+                                                   sparse_file)
         self.log.info("File size (in bytes) after writing first byte: %s",
-                      get_remote_file_size(
-                          self.hostlist_clients[0], sparse_file))
+                      fsize_write_1stbyte)
         # verify file did not got overriten after dd write.
-        self.assertTrue(
-            get_remote_file_size(
-                self.hostlist_clients[0], sparse_file) == self.space_before)
+        self.assertTrue(fsize_write_1stbyte == self.space_before)
 
         # write to the 1024th byte position of the file
         dd_1024_byte = u"echo 'A' | dd conv=notrunc of={} obs=1 seek=1023 \
                        bs=1 count=1".format(sparse_file)
         self.execute_cmd(dd_1024_byte)
-        self.log.info("File size (in bytes) after writing first byte: %s",
-                      get_remote_file_size(
-                          self.hostlist_clients[0], sparse_file))
+        fsize_write_1024thwrite = get_remote_file_size(self.hostlist_clients[0],
+                                                       sparse_file)
+        self.log.info("File size (in bytes) after writing 1024th byte: %s",
+                      fsize_write_1024thwrite)
         # verify file did not got overriten after dd write.
-        self.assertTrue(
-            get_remote_file_size(
-                self.hostlist_clients[0], sparse_file) == self.space_before)
+        self.assertTrue(fsize_write_1024thwrite == self.space_before)
 
-        # Obtainer the value of 1st byte and 1024th bytes in the file and
+        # Obtain the value of 1st byte and 1024th byte in the file and
         # compare their values, they should be same.
         check_first_byte = file_obj.read(1)
         file_obj.seek(1022, 1)
