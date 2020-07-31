445c170c710c1bb8dcf62389cfb03f014cc5ec47
Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>
2020-07-31 03:22:34 +0000

    DAOS-5369 test: Revert "DAOS-5152 Enable debug logs for daos_test(#3046)"
    This reverts commit b05b8852fbbd7ff1e622fa32b1d959ccb0f3c742.
    
    Signed-off-by: Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>

--- src/tests/ftest/util/daos_core_base.py
+++ src/tests/ftest/util/daos_core_base.py
@@ -62,14 +62,13 @@ class DaosCoreBase(TestWithServers):
                                        '/run/daos_tests/num_replicas/*')
         scm_size = self.params.get("scm_size", '/run/pool/*')
         args = self.params.get("args", '/run/daos_tests/Tests/*', "")
+        dmg = self.get_dmg_command()
+        dmg_config_file = dmg.yaml.filename
 
-        cmd = "{} {} -n {} -x D_LOG_FILE={} \
-            -x D_LOG_MASK=DEBUG -x DD_MASK=mgmt,io,md,epc,rebuild \
-            {} -s {} -n {} {}".format(self.orterun, self.client_mca,
-                                      num_clients,
-                                      get_log_file(self.client_log),
-                                      self.daos_test, num_replicas,
-                                      subtest, args)
+        cmd = "{} {} -n {} -x D_LOG_FILE={} {} -s {} -n {} -{} {}".format(
+            self.orterun, self.client_mca, num_clients,
+            get_log_file(self.client_log), self.daos_test,
+            num_replicas, dmg_config_file, subtest, args)
 
         env = {}
         env['CMOCKA_XML_FILE'] = "%g_results.xml"
