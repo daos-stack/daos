2230e6566679be1728cf4fe16da9fe6229bf776c
Ding Ho <ding-hwa.ho@intel.com>
2020-08-05 02:13:41 +0000

    DAOS-3596 test: Tests for daos io server restart and daos server reformat.
    New files: server/daos_server_restart.py
               server/daos_server_restart.yaml
    Test-tag: pr,hw,small
    Signed-off-by: Ding Ho <ding-hwa.ho@intel.com>

---
+++ src/tests/ftest/server/daos_server_restart.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+"""
+  (C) Copyright 2020 Intel Corporation.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+  The Government's rights to use, modify, reproduce, release, perform, display,
+  or disclose this software are subject to the terms of the Apache License as
+  provided in Contract No. B609815.
+  Any reproduction of computer software, computer software documentation, or
+  portions thereof marked with this legend must also reproduce the markings.
+"""
+from __future__ import print_function
+
+from apricot import TestWithServers
+from server_utils import ServerFailed
+from daos_utils import DaosCommand
+
+
+class DaosServerTest(TestWithServers):
+    """Daos server tests.
+
+    Test Class Description:
+        Test to verify that the daos_io_server, daos_server starts/stops
+        and reformat.
+
+    :avocado: recursive
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a DaosServerTest object."""
+        super(DaosServerTest, self).__init__(*args, **kwargs)
+
+    def restart_daos_server(self):
+        """method to perform server stop and start"""
+
+        try:
+            self.server_managers[0].stop()
+        except ServerFailed as error:
+            self.fail(
+                "##Failed stoping server, {}".format(error))
+        try:
+            self.server_managers[0].start()
+        except ServerFailed as error:
+            self.fail(
+                "##Failed restartng server, {}".format(error))
+
+    def restart_daos_io_server(self, force=False):
+        """method to perform io_server stop and start by dmg"""
+
+        try:
+            self.server_managers[0].dmg.system_stop(force)
+        except CommandFailure as error:
+            self.fail(
+                "##Failed stoping daos io-server by dmg, {}".format(error))
+        try:
+            self.server_managers[0].dmg.system_start()
+        except CommandFailure as error:
+            self.fail(
+                "##Failed starting daos io-server by dmg, {}".format(error))
+
+    def get_pool_list(self):
+        """method to get the pool list contents"""
+        pool_list = self.get_dmg_command().get_output("pool_list")
+        self.log.info(
+            "get_pool-list: {}".format(pool_list))
+        return pool_list
+
+    def verify_pool_list(self, expected_pool_list=[]):
+        """method to verify the pool list"""
+        pool_list = self.get_pool_list()
+        self.log.info(
+            "\n===Current pool-list:  {}\n===Expected pool-list: {}\n".format(
+                pool_list, expected_pool_list))
+        if pool_list != expected_pool_list:
+            self.fail(
+                "##Current pool-list mismatch with the expected pool-list.")
+
+    def create_pool_and_container(self):
+        """method to create pool and container"""
+        scm_size = self.params.get("scm_size", "/run/server/*/", 138000000)
+        num_of_pool = self.params.get("num_of_pool", "/run/server/*/", 3)
+        container_per_pool = self.params.get(
+            "container_per_pool", "/run/server/*/", 2)
+        for i in range(num_of_pool):
+            self.dmg = self.get_dmg_command()
+            result = self.dmg.pool_create(scm_size)
+            uuid = result['uuid']
+            svc = result['svc']
+            for j  in range(container_per_pool):
+                daos_cmd = DaosCommand(self.bin)
+                daos_cmd.exit_status_exception = False
+                result = daos_cmd.container_create(pool=uuid, svc=svc)
+                self.log.info("container create status:", result)
+
+    def test_daos_server_reformat(self):
+        """JIRA ID: DAOS-3596.
+
+        Test Description: verify reformating DAOS is same as a fresh install.
+        Steps:
+        (1)Verify daos server pool list after started.
+        (2)Restart server without pool been created, and verify.
+        (3)Create pools, containers and verify the pools.
+        (4)Perform an orderly DAOS shutdown, and Use cmd line tools to
+           re-format both SSDs and NVDIMMs.
+        (5)Verify after DAOS server restarted, it should appear as an empty
+           fresh installation.
+
+        :avocado: tags=all,pr,hw,large,server_test,server_restart
+        """
+
+        self.log.info("(1)Verify daos server pool list after started.")
+        self.verify_pool_list()
+        self.log.info("(2)Restart server without pool created and verify.")
+        self.restart_daos_server()
+        self.verify_pool_list()
+        self.log.info("(3)Create pools, containers.")
+        self.create_pool_and_container()
+        self.log.info("(4)Shutdown, restart and reformat the server")
+        self.restart_daos_server()
+        self.log.info("(5)Verify after server restarted.")
+        self.verify_pool_list()
+
+    def test_daos_io_server_restart(self):
+        """
+        JIRA ID: DAOS-3593.
+
+        Test Description: Test cmd to perform daos io_server restart.
+        Steps:
+        (1)Use the cmd line to perform a controlled shutdown from a
+           quiescent state (i.e. clients disconnected).
+        (2)Use the cmd line to perform a controlled shutdown from a
+           partially quiescent state (i.e. clients attached but no
+           transactions in progress).
+        (3)Force shutdown and restart the daos io-server.
+        (4)Verify pool list after forced shutdown and restart the
+           daos io-server.
+        (5)Use the cmd line to perform a controlled shutdown when the
+           daos cluster is incomplete (i.e. 1 of the 2 servers is down).
+
+        :avocado: tags=all,pr,hw,large,server_test,server_reformat
+        """
+
+        self.log.info(
+            "(1)Shutdown and restart the daos io-server "
+            "from a quiescent state.")
+        self.agent_managers[0].stop()
+        self.verify_pool_list()
+        self.restart_daos_io_server()
+        self.agent_managers[0].start()
+        self.log.info(
+            "(2)Shutdown and restart the daos io-server with pools "
+            "and containers created.")
+        self.create_pool_and_container()
+        pool_list = self.get_pool_list()
+        self.restart_daos_io_server()
+        self.log.info(
+            "(3)Force shutdown and restart the daos io-server.")
+        self.restart_daos_io_server(force=True)
+        self.log.info(
+            "(4)Verify pool list after forced shutdown and restart "
+            "the daos io-server.")
+        self.verify_pool_list(pool_list)
+
+        print(" ")
+        print("self.hostlist_servers= ", self.hostlist_servers)
+        print(" ")
+        hosts = self.hostlist_servers
+        self.hostlist_servers = hosts[-1]
+        print(" ")
+        print("(5)Restart daos io server for the last server on the cluster ")
+        print("   self.hostlist_servers= ", self.hostlist_servers)
+        self.restart_daos_io_server()
+        self.verify_pool_list(pool_list)
+        self.hostlist_servers = hosts
