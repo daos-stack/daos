9c4f0e4366852044755e8228d959e8a7b8487a85
Kenneth Cain <kenneth.c.cain@intel.com>
2020-08-14 13:26:38 -0400

    DAOS-4415 vos: vos_pool_open() API add small pool option
    PR 3087 changed the amount of system reserved storage for a vos pool
    when it is opened, setting aside >= 2GB to avoid fragmentation.
    However, rdb vos pool sizes are default 1GB SCM. Such a large amount
    of reserved memory is not possible. This results in vos_space_init()
    logging a warning "Disable SCM space reserving for tiny pool" and
    not reserving SCM for background activities essential for stability.
    
    With this change, the vos_pool_open() API adds a boolean hint argument
    "small" to allow the caller to effectively request a nominal (not huge)
    amount of reserved memory. All vos_pool_open() calls are changed to
    specify the new argument (currently false for all callers except rdb).
    
    Signed-off-by: Kenneth Cain <kenneth.c.cain@intel.com>

--- src/pool/srv_target.c
+++ src/pool/srv_target.c
@@ -225,7 +225,7 @@ pool_child_add_one(void *varg)
 		return rc;
 	}
 
-	rc = vos_pool_open(path, arg->pla_uuid, &child->spc_hdl);
+	rc = vos_pool_open(path, arg->pla_uuid, false, &child->spc_hdl);
 
 	D_FREE(path);
 
