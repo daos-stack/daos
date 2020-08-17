4052f3082bbee2adbb89227c9ca0ea6371451eb6
David Maldonado Moreno <david.maldonado.moreno@intel.com>
2020-08-17 15:40:24 +0000

    Adding same solution as Ashley in PR#3228
    Signed-off-by: David Maldonado Moreno <david.maldonado.moreno@intel.com>

--- ci/unit/test_main_node.sh
+++ ci/unit/test_main_node.sh
@@ -28,6 +28,5 @@ export CMOCKA_XML_FILE="$DAOS_BASE"/test_results/%g.xml
 cd "$DAOS_BASE"
 IS_CI=true OLD_CI=false utils/run_test.sh
 #./utils/node_local_test.py all #DM
-rm -rf vm_test #DM
 mkdir -p vm_test #DM
 ./utils/node_local_test.py --output-file=vm_test/nlt-errors.json all #DM
