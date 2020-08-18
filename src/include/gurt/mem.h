9603941932294386ca797173e75241ad71dec9d4
Johann Lombardi <johann.lombardi@intel.com>
2020-08-18 10:41:48 +0200

    Move string.h

--- src/include/gurt/mem.h
+++ src/include/gurt/mem.h
@@ -20,6 +20,8 @@
 extern "C" {
 #endif
 
+#include <string.h>
+
 /**
  * \file
  * Optimized mem{cmp,cpy} functions
@@ -560,8 +562,6 @@ d_memcpy(void *dst, const void *src, size_t n)
 }
 
 #else /**!__AVX2__ */
-
-#include <string.h>
 
 #define d_memcpy	memcpy
 #define d_memcmp	memcmp
