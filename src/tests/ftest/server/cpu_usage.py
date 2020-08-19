468ca9cfe6d275fadab9b2a40887a077b39a53e3
Makito Kano <makito.kano@intel.com>
2020-08-19 22:16:39 +0000

    DAOS-4826 test: Check if the CPU usage is stable by verifying the usage diff percentage
    Test-tag-hw-small: pr,hw,small cpu_usage
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/server/cpu_usage.py
+++ src/tests/ftest/server/cpu_usage.py
@@ -38,17 +38,38 @@ class CPUUsage(TestWithServers):
         """
         JIRA ID: DAOS-4826
         Test Description: Test CPU usage of daos_server.
-        :avocado: tags=all,server,small,full_regression,cpu_usage
+        :avocado: tags=all,hw,server,small,full_regression,cpu_usage
         """
         ps_get_cpu = r"ps -C daos_io_server -o %\cpu"
-        # It takes about 5 sec for CPU usage to become stable.
-        time.sleep(10)
-        task = run_task(hosts=self.hostlist_servers, command=ps_get_cpu)
-        # Sample output.
-        # %CPU
-        # 1798
-        for output, _ in task.iter_buffers():
-            usage = str(output).splitlines()[-1]
-            self.log.info("CPU usage = %s", usage)
-            self.assertTrue(
-                int(usage) < 100, "CPU usage is above 100%: {}%".format(usage))
+
+        prev_usage = 1
+        usage = 1
+        time.sleep(5)
+        for _ in range(10):
+            time.sleep(5)
+            task = run_task(hosts=self.hostlist_servers, command=ps_get_cpu)
+            # Sample output.
+            # %CPU
+            # 1798
+            for output, _ in task.iter_buffers():
+                usage = str(output).splitlines()[-1]
+                self.log.info("CPU usage = %s", usage)
+            # Check if daos_io_server has started.
+            if usage == "%CPU":
+                continue
+
+            usage = int(usage)
+            if usage == 0:
+                break
+            diff = usage - prev_usage
+            diff_p = (float(abs(diff)) / prev_usage) * 100
+
+            # Check if the CPU usage is stable; the change was less than 10%.
+            if diff_p <= float(10):
+                break
+            prev_usage = usage
+
+        self.assertTrue(
+            usage != "%CPU", "daos_io_server CPU usage couldn't be obtained!")
+        self.assertTrue(
+            usage < 100, "CPU usage is above 100%: {}%".format(usage))
