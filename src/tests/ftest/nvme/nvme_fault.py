f5d5fc5c809b470f3de1595079081a3cb7c0781b
Samir Raval <samir.raval@intel.com>
2020-08-20 08:02:39 -0700

    Update nvme_fault.py
    Adding Skip because of existing issue to nvme_fault.
    
    Test-tag-hw-medium: nvme_health

--- src/tests/ftest/nvme/nvme_fault.py
+++ src/tests/ftest/nvme/nvme_fault.py
@@ -25,6 +25,7 @@ import os
 from nvme_utils import ServerFillUp
 from dmg_utils import DmgCommand
 from command_utils_base import CommandFailure
+from apricot import skipForTicket
 
 class NvmeFault(ServerFillUp):
     # pylint: disable=too-many-ancestors
@@ -51,6 +52,7 @@ class NvmeFault(ServerFillUp):
         #Set to True to generate the NVMe fault during IO
         self.set_faulty_device = True
 
+    @skipForTicket("DAOS-5497")
     def test_nvme_fault(self):
         """Jira ID: DAOS-4722.
 
