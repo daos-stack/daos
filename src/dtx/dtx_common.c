3e54cabd14aade8adb55e8eb5ac4ca27faed9b56
Jeff Olivier <jeffrey.v.olivier@intel.com>
2020-08-21 11:20:15 -0600

    Use vos_dtx_rsrvd_fini in dtx_end
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/dtx/dtx_common.c
+++ src/dtx/dtx_common.c
@@ -844,8 +844,7 @@ dtx_end(struct dtx_handle *dth, struct ds_cont_child *cont, int result)
 
 	D_FREE(dth->dth_oid_array);
 
-	if (dth->dth_rsrvds != &dth->dth_rsrvd_inline)
-		D_FREE(dth->dth_rsrvds);
+	vos_dtx_rsrvd_fini(dth);
 
 	return result;
 }
