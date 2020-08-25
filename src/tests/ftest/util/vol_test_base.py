24cd0b70010c78e09d6b69386240dc8cc2bab6ed
Maureen Jean <maureen.jean@intel.com>
2020-08-25 10:55:24 -0400

    Incldue DfuseTestBase
    PR-repos: hdf5@PR-14 hdf5_vol_daos@PR-1
    Test-tag-hw-medium: vol
    Skip-checkpatch: true
    Skip-run_test: true
    Skip-func-test: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-small: true
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/util/vol_test_base.py
+++ src/tests/ftest/util/vol_test_base.py
@@ -1,78 +1,40 @@
 #!/usr/bin/python
-'''
-    (C) Copyright 2020 Intel Corporation.
+"""
+(C) Copyright 2020 Intel Corporation.
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-    GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
-    The Government's rights to use, modify, reproduce, release, perform,
-    display, or disclose this software are subject to the terms of the Apache
-    License as provided in Contract No. B609815.
-    Any reproduction of computer software, computer software documentation, or
-    portions thereof marked with this legend must also reproduce the markings.
-    '''
-from __future__ import print_function
+GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+The Government's rights to use, modify, reproduce, release, perform,
+display, or disclose this software are subject to the terms of the Apache
+License as provided in Contract No. B609815.
+Any reproduction of computer software, computer software documentation, or
+portions thereof marked with this legend must also reproduce the markings.
+"""
 
 import os
 
-from apricot import TestWithServers
+from dfuse_test_base import DfuseTestBase
 from command_utils_base import EnvironmentVariables, CommandFailure
 from job_manager_utils import Mpirun, Orterun
-from dfuse_utils import Dfuse
-from ClusterShell.NodeSet import NodeSet
 
 
-class VolTestBase(TestWithServers):
-    """
-
-    Runs HDF5 vol test suites.
+class VolTestBase(DfuseTestBase):
+    # pylint: disable=too-few-public-methods
+    """Runs HDF5 vol test suites.
 
     :avocado: recursive
     """
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a TestWithServers object."""
-        super(VolTestBase, self).__init__(*args, **kwargs)
-        self.dfuse = None
-
-    def tearDown(self):
-        """Tear down each test case."""
-        try:
-            if self.dfuse:
-                self.dfuse.stop()
-        finally:
-            # Stop the servers and agents
-            super(VolTestBase, self).tearDown()
-
-    def _start_dfuse(self):
-        """Create a DfuseCommand object to start dfuse."""
-        self.dfuse = Dfuse(self.hostlist_clients, self.tmp)
-        self.dfuse.get_params(self)
-
-        # update dfuse params
-        self.dfuse.set_dfuse_params(self.pool)
-        self.dfuse.set_dfuse_cont_param(self.container)
-        self.dfuse.set_dfuse_exports(self.server_managers[0], self.client_log)
-
-        try:
-            # start dfuse
-            self.dfuse.run()
-        except CommandFailure as error:
-            self.log.error("Dfuse command %s failed on hosts %s",
-                           str(self.dfuse),
-                           str(NodeSet.fromlist(self.dfuse.hosts)),
-                           exc_info=error)
-            self.fail("Test was expected to pass but it failed.\n")
 
     def run_test(self):
         """Run the HDF5 VOL testsuites.
@@ -97,7 +59,7 @@ class VolTestBase(TestWithServers):
         #  Currently nfs does not have this attribute so it was recommended
         #  to create a dfuse dir and run vol tests from there.
         # create dfuse container
-        self._start_dfuse()
+        self.start_dfuse(self.hostlist_clients, self.pool, self.container)
 
         # for test_param in test_list:
         # testname = test_param[0][1]
