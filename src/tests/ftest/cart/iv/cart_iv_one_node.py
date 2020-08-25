a92736b4fb8fb12b6b691794cbcc829d6e43bc92
Ethan Mallove <ethanx.a.mallove@intel.com>
2020-08-25 02:36:07 +0000

    DAOS-4582 tests: More tracing for JSON decode msg
    More tracing needed for this Avocado exception:
      "No JSON object could be decoded"
    
    Skip-build-leap15-rpm: true
    Skip-build-ubuntu-clang: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Skip-checkpatch: true
    Quick-build: true
    Skip-run_test: true
    Test-tag: all_cart
    Skip-func-test-leap15: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    
    Signed-off-by: Ethan Mallove <ethanx.a.mallove@intel.com>

--- src/tests/ftest/cart/iv/cart_iv_one_node.py
+++ src/tests/ftest/cart/iv/cart_iv_one_node.py
@@ -97,6 +97,7 @@ def _check_key(key_rank, key_idx, received_key_hex):
     print('DEBUG log: line 88, received_key_hex[:8] = ', received_key_hex[:8])
 
     if len(received_key_hex) != 16:
+        print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:100")
         return False
 
     rank = struct.unpack("<I",
@@ -104,6 +105,7 @@ def _check_key(key_rank, key_idx, received_key_hex):
     idx = struct.unpack("<I",
                         codecs.decode(received_key_hex[8:], "hex"))[0]
 
+    print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:108")
     return (rank == key_rank) and (idx == key_idx)
 
 class CartIvOneNodeTest(Test):
@@ -124,6 +126,7 @@ class CartIvOneNodeTest(Test):
 
     def _verify_action(self, action):
         """verify the action"""
+        print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:129")
         if (('operation' not in action) or
                 ('rank' not in action) or
                 ('key' not in action)):
@@ -137,6 +140,7 @@ class CartIvOneNodeTest(Test):
 
     def _verify_fetch_operation(self, action):
         """verify fetch operation"""
+        print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:143")
         if (('return_code' not in action) or
                 ('expected_value' not in action)):
             self.utils.print("Error: fetch operation was malformed")
@@ -145,7 +149,9 @@ class CartIvOneNodeTest(Test):
     def _iv_test_actions(self, cmd, actions):
         #pylint: disable=too-many-locals
         """Go through each action and perform the test"""
+        print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:152")
         for action in actions:
+            print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:154")
             clicmd = cmd
             command = 'tests/iv_client'
 
@@ -157,21 +163,28 @@ class CartIvOneNodeTest(Test):
             key_idx = int(action['key'][1])
 
             if "fetch" in operation:
+                print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:166")
                 self._verify_fetch_operation(action)
                 expected_rc = int(action['return_code'])
 
                 # Create a temporary file for iv_client to write the results to
                 log_fd, log_path = tempfile.mkstemp()
 
+                # try writing to an unwritable spot
+                # log_path = "/"
+
                 command = " {!s} -o '{!s}' -r '{!s}' -k '{!s}:{!s}' -l '{!s}'" \
                     .format(command, operation, rank, key_rank, key_idx,
                             log_path)
                 clicmd += command
+                print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:180")
 
                 self.utils.print("\nClient cmd : %s\n" % clicmd)
                 cli_rtn = subprocess.call(shlex.split(clicmd))
 
+                print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:185")
                 if cli_rtn != 0:
+                    print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:187")
                     raise ValueError('Error code {!s} running command "{!s}"' \
                         .format(cli_rtn, command))
 
@@ -196,6 +209,7 @@ class CartIvOneNodeTest(Test):
 
                 # Parse return code and make sure it matches
                 if expected_rc != test_result["return_code"]:
+                    print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:212")
                     raise ValueError("Fetch returned return code {!s} != " \
                                      "expected value {!s}".format(
                                          test_result["return_code"],
@@ -203,15 +217,18 @@ class CartIvOneNodeTest(Test):
 
                 # Other values will be invalid if return code is failure
                 if expected_rc != 0:
+                    print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:220")
                     continue
 
                 # Check that returned key matches expected one
                 if not _check_key(key_rank, key_idx, test_result["key"]):
+                    print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:225")
                     raise ValueError("Fetch returned unexpected key")
 
                 # Check that returned value matches expected one
                 if not _check_value(action['expected_value'],
                                     test_result["value"]):
+                    print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:231")
                     raise ValueError("Fetch returned unexpected value")
 
             if "update" in operation:
@@ -223,8 +240,10 @@ class CartIvOneNodeTest(Test):
                                 action['value'])
                 clicmd += command
 
+                print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:243")
                 self.utils.print("\nClient cmd : %s\n" % clicmd)
                 cli_rtn = subprocess.call(shlex.split(clicmd))
+                print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:246")
 
                 if cli_rtn != 0:
                     raise ValueError('Error code {!s} running command "{!s}"' \
@@ -235,8 +254,10 @@ class CartIvOneNodeTest(Test):
                     command, operation, rank, key_rank, key_idx)
                 clicmd += command
 
+                print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:257")
                 self.utils.print("\nClient cmd : %s\n" % clicmd)
                 cli_rtn = subprocess.call(shlex.split(clicmd))
+                print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:260")
 
                 if cli_rtn != 0:
                     raise ValueError('Error code {!s} running command "{!s}"' \
@@ -250,6 +271,7 @@ class CartIvOneNodeTest(Test):
         """
 
         srvcmd = self.utils.build_cmd(self, self.env, "test_servers")
+        print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:274")
 
         try:
             srv_rtn = self.utils.launch_cmd_bg(self, srvcmd)
@@ -259,6 +281,7 @@ class CartIvOneNodeTest(Test):
 
         # Verify the server is still running.
         if not self.utils.check_process(srv_rtn):
+            print("TRACE, src/tests/ftest/cart/iv/cart_iv_one_node.py:284")
             procrtn = self.utils.stop_process(srv_rtn, self.utils)
             self.fail("Server did not launch, return code %s" \
                        % procrtn)
