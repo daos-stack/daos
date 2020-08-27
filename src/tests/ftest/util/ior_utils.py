84024f8be1438fdf0b099133dfb8bfdff7134568
Maureen Jean <maureen.jean@intel.com>
2020-08-27 18:52:47 -0400

    Use plugin path and dont use dfuse testfile
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/util/ior_utils.py
+++ src/tests/ftest/util/ior_utils.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 """
-  (C) Copyright 2018-2020 Intel Corporation.
+(C) Copyright 2018-2020 Intel Corporation.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-  GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
-  The Government's rights to use, modify, reproduce, release, perform, display,
-  or disclose this software are subject to the terms of the Apache License as
-  provided in Contract No. B609815.
-  Any reproduction of computer software, computer software documentation, or
-  portions thereof marked with this legend must also reproduce the markings.
+GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+The Government's rights to use, modify, reproduce, release, perform, display,
+or disclose this software are subject to the terms of the Apache License as
+provided in Contract No. B609815.
+Any reproduction of computer software, computer software documentation, or
+portions thereof marked with this legend must also reproduce the markings.
 """
 from __future__ import print_function
 
@@ -299,10 +299,9 @@ class IorCommand(ExecutableCommand):
         """
         logger.info("\n")
         logger.info(message)
-        for message in metrics:
-            logger.info(message)
+        for metric in metrics:
+            logger.info(metric)
         logger.info("\n")
-
 
     def check_ior_subprocess_status(self, sub_process, command,
                                     pattern_timeout=10):
@@ -365,6 +364,7 @@ class IorCommand(ExecutableCommand):
                     "%s subprocess startup detected - %s", command, msg)
 
         return complete
+
 
 class IorMetrics(IntEnum):
     """Index Name and Number of each column in IOR result summary."""
