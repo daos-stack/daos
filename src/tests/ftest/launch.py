617bb5778b184829f3eff5c476bcffd241dd43b2
Ethan Mallove <ethanx.a.mallove@intel.com>
2020-08-27 04:40:20 +0000

    DAOS-4582 tests: Add trace to scp shell script
    Add trace to files in daos_testing dir
    
    Skip-build-leap15-rpm: true
    Skip-build-ubuntu-clang: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Skip-checkpatch: true
    Quick-build: true
    Skip-run_test: true
    Test-tag: all_cart
    Skip-func-test-leap15: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    
    Signed-off-by: Ethan Mallove <ethanx.a.mallove@intel.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -1020,10 +1020,16 @@ def archive_files(destination, host_list, source_files):
     # related to any failed command.
     commands = [
         "set -eu",
+        "set +x",
         "rc=0",
         "copied=()",
         "for file in $(ls {})".format(source_files),
         "do ls -sh $file",
+
+        # DEBUGGING
+        "find $(dirname $file)",
+
+
         "if scp $file {}:{}/${{file##*/}}-$(hostname -s)".format(
             this_host, destination),
         "then copied+=($file)",
