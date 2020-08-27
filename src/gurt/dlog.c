a34673c3ece7eb2546e0b2be7eff95202de1ef11
Hua Kuang <hua.kuang@intel.com>
2020-08-27 16:05:05 +0000

    DAOSSDE-58 SDL: T295: Avoid storing unencrypted confidential data without access control mechanisms
    Tighten the accessw control for some possible confidential data in log file and pool.
    
    Change-Id: I175fca65f887695b5dac6298d8c27c6029d07d7d
    Signed-off-by: Hua Kuang <hua.kuang@intel.com>

--- src/gurt/dlog.c
+++ src/gurt/dlog.c
@@ -674,7 +674,7 @@ d_log_open(char *tag, int maxfac_hint, int default_mask, int stderr_mask,
 			setlinebuf(stderr);
 			mst.logfd = fileno(stderr);
 		} else {
-			mst.logfd = open(mst.logfile, log_flags, 0666);
+			mst.logfd = open(mst.logfile, log_flags, 0644);
 		}
 		if (mst.logfd < 0) {
 			fprintf(stderr, "d_log_open: cannot open %s: %s\n",
