c616283b2adee5e6efa71f7b14cbe611d8149d00
Phillip Henderson <phillip.henderson@intel.com>
2020-08-31 10:25:32 -0400

    Adding disconnecting pool prior to container create for root_container_test.py
    Test-tag-hw-small: pr,hw,small dfusespacecheck rootcontainer dfusesparsefile
    Test-tag-hw-medium: pr,hw,medium,ib2 parallelio
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/container/root_container_test.py
+++ src/tests/ftest/container/root_container_test.py
@@ -78,11 +78,15 @@ class RootContainerTest(DfuseTestBase):
             TestContainer: the newly added container
 
         """
+        # Disconnect from pool otherwise daos container create will fail
+        pool.disconnect()
+
         # Get container params
         container = TestContainer(pool, daos_command=DaosCommand(self.bin))
         container.get_params(self)
         if path is not None:
             container.path.update(path)
+
         # create container
         container.create()
         self.container.append(container)
