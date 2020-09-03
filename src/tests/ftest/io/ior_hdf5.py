f320e26252c788d26d955867dfeb65885ca76570
Maureen Jean <maureen.jean@intel.com>
2020-09-03 13:05:04 -0400

    Include changes for DAOS-3657
    PR-repos: ior-hpc@PR-3
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/io/ior_hdf5.py
+++ src/tests/ftest/io/ior_hdf5.py
@@ -27,70 +27,37 @@ from ior_test_base import IorTestBase
 
 class IorHdf5(IorTestBase):
     # pylint: disable=too-few-public-methods
-    """Test class Description: Runs IOR/HDF5 on 1 server with basic parameters.
+    """Test class Description: Runs IOR/HDF5 on 2 server with basic parameters.
 
     :avocado: recursive
     """
-
-    def ior_hdf5(self, hdf5_plugin_path=None):
-        """Run IOR cmd with HDF5.
-
-        Args:
-            hdf5_plugin_path (str, optional): HDF5 vol connector library path.
-                This will enable dfuse (xattr) working directory which is
-                needed to run vol connector for DAOS. Default is None.
-        """
-        flags = self.params.get("ior_flags", '/run/ior/iorflags/*')
-        transfer_block_size = self.params.get("transfer_block_size",
-                                              '/run/ior/iorflags/*')
-        obj_class = self.params.get("obj_class", '/run/ior/iorflags/*')
-
-        # run tests for different variants
-        self.ior_cmd.flags.update(flags[0])
-        for oclass in obj_class:
-            self.ior_cmd.dfs_oclass.update(oclass)
-            for test in transfer_block_size:
-                # update transfer and block size
-                self.ior_cmd.transfer_size.update(test[0])
-                self.ior_cmd.block_size.update(test[1])
-                # run ior
-                self.run_ior_with_pool(plugin_path=hdf5_plugin_path)
-
     def test_ior_hdf5(self):
-        """Jira ID: DAOS-4909.
+        """Jira ID: DAOS-3657.
 
         Test Description:
             Purpose of this test is to have small ior test to check basic
             functionality for HDF5 api
 
         Use case:
-            Run ior with read, write, CheckWrite, CheckRead in ssf mode.
-            Run ior with read, write, CheckWrite, CheckRead in fpp mode.
-            Run ior with read, write, CheckWrite and access to random
-                offset instead of sequential.
-            All above three cases to be run with single client and
-                multiple client processes in two separate nodes.
+            Run IOR on HDF5 using a single shared file
+            Generate 2 terabytes of data with IOR and read back and verify
 
         :avocado: tags=all,pr,hw,large,daosio,hdf5,iorhdf5
         """
-        self.ior_hdf5()
+        self.run_ior_with_pool()
 
     def test_ior_hdf5_vol(self):
         """Jira ID: DAOS-4909.
 
         Test Description:
-            Purpose of this test is to have small ior test to check basic
+            Purpose of this test is to have ior test to check basic
             functionality for HDF5 api using the vol connector
 
         Use case:
-            Run ior with read, write, CheckWrite, CheckRead in ssf mode.
-            Run ior with read, write, CheckWrite, CheckRead in fpp mode.
-            Run ior with read, write, CheckWrite and access to random
-                offset instead of sequential.
-            All above three cases to be run with single client and
-                multiple client processes in two separate nodes.
+            Run IOR on HDF5 with vol connector using a single shared file
+            Generate 2 terabytes of data with IOR and read back and verify
 
         :avocado: tags=all,pr,hw,large,daosio,hdf5,vol,iorhdf5vol
         """
         hdf5_plugin_path = self.params.get("plugin_path", '/run/hdf5_vol/*')
-        self.ior_hdf5(hdf5_plugin_path)
+        self.run_ior_with_pool(plugin_path=hdf5_plugin_path)
