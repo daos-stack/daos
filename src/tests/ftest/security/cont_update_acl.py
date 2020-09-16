0b14bd6456f1d4819a6dbf510a2dcb2f3c721c90
Justiniano-pagn <amanda.justiniano-pagn@intel.com>
2020-09-16 16:05:20 -0400

    Add test to for command inputs
    Signed-off-by: Justiniano-pagn <amanda.justiniano-pagn@intel.com>

--- src/tests/ftest/security/cont_update_acl.py
+++ src/tests/ftest/security/cont_update_acl.py
@@ -40,43 +40,113 @@ class UpdateContainerACLTest(ContSecurityTestBase):
         self.prepare_pool()
         self.add_container(self.pool)
 
+        # List of ACL entries
+        self.cont_acl = self.get_container_acl_list(
+            self.pool.uuid, self.pool.svc_ranks[0], self.container.uuid)
+
+    def error_handling(self, results, err_msg):
+        """Handle errors when test fails and when command unexpectedly passes.
+
+        Args:
+            results (CmdResult): object containing stdout, stderr and
+                exit status
+
+        Returns:
+            list: list of test errors encountered.
+
+        """
+        test_errs = []
+        if results.exit_status == 0:
+            test_errs.append("overwrite-acl passed unexpectedly: {}".format(
+                results.stdout))
+        elif results.exit_status == 1:
+            # REMOVE BELOW IF Once DAOS-5635 is resolved
+            if results.stdout and err_msg in results.stdout:
+                self.log.info("Found expected error %s", results.stdout)
+            # REMOVE ABOVE IF Once DAOS-5635 is resolved
+            elif results.stderr and err_msg in results.stderr:
+                self.log.info("Found expected error %s", results.stderr)
+            else:
+                self.fail("overwrite-acl seems to have failed with \
+                    unexpected error: {}".format(results))
+        return test_errs
+
+    def acl_file_diff(self, prev_acl, flag=True):
+        """Helper function to compare current content of acl-file.
+
+        If provided  prev_acl file information is different from current acl
+        file information test will fail if flag=True. If flag=False, test will
+        fail in the case that the acl contents are found to have no difference.
+
+        Args:
+            prev_acl (list): list of acl entries within acl-file.
+                Defaults to True.
+            flag (bool): if True, test will fail when acl-file contents are
+                different, else test will fail when acl-file contents are same.
+        """
+        current_acl = self.get_container_acl_list(
+            self.pool.uuid, self.pool.svc_ranks[0], self.container.uuid)
+        if self.compare_acl_lists(prev_acl, current_acl) != flag:
+            self.fail("Previous ACL:\n{} \nPost command ACL:\n{}".format(
+                prev_acl, current_acl))
+
     def test_acl_update_invalid_inputs(self):
         """
         JIRA ID: DAOS-3711
+
         Test Description: Test that container update command performs as
-            expected with invalid inputs in command line and within ACL file
-            provided.
-        :avocado: tags=all,pr,security,container_acl,cont_update_acl
+            expected with valid and invalid inputs in command line for the
+            --entry and --acl-file options.
+
+        :avocado: tags=all,pr,security,container_acl,cont_update_acl_inputs
         """
-        # Get list of invalid ACL principal values
-        invalid_acl_filename = self.params.get("invalid_acl_filename", "/run/*")
+        # Get lists of invalid
+        invalid_entries = self.params.get("invalid_acl_entries", "/run/*")
+        invalid_filename = self.params.get("invalid_acl_filename", "/run/*")
 
-        # Check for failure on invalid inputs.
-        for acl_file in invalid_acl_filename:
-            try:
-                self.daos_cmd.container_update_acl(
-                    self.pool.uuid,
-                    self.pool.svc_ranks[0],
-                    self.container.uuid,
-                    acl_file)
-            except process.CmdError as err:
-                if "No such file or directory" in err.result.stdout:
-                    self.log.info(
-                        "Found expected error %s with invalid acl-file %s",
-                        err.result.stdout,
-                        acl_file)
-                else:
-                    self.fail("update-acl seems to have failed with \
-                        unexpected error: {}".format(err))
+        # Disable raising an exception if the daos command fails
+        self.daos_cmd.exit_status_exception = False
 
-        self.fail("container update-acl command expected to fail.")
+        test_errs = []
+        for entry in invalid_entries:
+
+            # Run update command
+            self.daos_cmd.container_update_acl(
+                self.pool.uuid,
+                self.pool.svc_ranks[0],
+                self.container.uuid,
+                entry=entry)
+            test_errs.extend(self.error_handling(self.daos_cmd.result, "-1003"))
+
+            # Check that the acl file was unchanged
+            self.acl_file_diff(self.cont_acl)
+
+        for acl_file in invalid_filename:
+
+            # Run update command
+            self.daos_cmd.container_update_acl(
+                self.pool.uuid,
+                self.pool.svc_ranks[0],
+                self.container.uuid,
+                acl_file=acl_file)
+            test_errs.extend(self.error_handling(
+                self.daos_cmd.result, "No such file or directory"))
+
+            # Check that the acl file was unchanged
+            self.acl_file_diff(self.cont_acl)
+
+        if test_errs:
+            self.fail("container update-acl command expected to fail: \
+                {}".format("\n".join(test_errs)))
 
     def test_update_invalid_acl_file(self):
         """
         JIRA ID: DAOS-3711
+
         Test Description: Test that container update command performs as
             expected with invalid inputs in command line and within ACL file
             provided.
+
         :avocado: tags=all,pr,security,container_acl,cont_update_acl
         """
         acl_filename = "test_acl_file.txt"
