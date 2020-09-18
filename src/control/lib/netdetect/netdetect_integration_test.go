cc19e1d6cd473d02aaac91bae4723d911e46b463
Joel Rosenzweig <joel.b.rosenzweig@intel.com>
2020-09-18 17:39:59 -0400

    DAOS-5448 Agent: Unexpected default AttachInfo response (#3284)
    A race condition was found in the daos_agent when
    enough clients simultaneously issued GetAttachInfo.
    
    The agent initialized the attachInfo cache multiple
    times due to the race condition.  Some of the cache
    init attempts failed to find devices in the fabric
    scan due to a second related race condition.  Those
    instances that failed to find devices in the fabric
    scan populated a cache with only the default entry
    and supplied the default entry to the caller.
    
    Two race conditions were fixed, one that controls
    the cache initialization, and another that protects
    the hwloc library from concurrent access to
    hwloc_get_proc_cpubind().
    
    Signed-off-by: Joel Rosenzweig <joel.b.rosenzweig@intel.com>

--- src/control/lib/netdetect/netdetect_integration_test.go
+++ src/control/lib/netdetect/netdetect_integration_test.go
@@ -20,7 +20,6 @@
 // Any reproduction of computer software, computer software documentation, or
 // portions thereof marked with this legend must also reproduce the markings.
 //
-
 // +build concurrency
 
 package netdetect
@@ -34,53 +33,72 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pkg/errors"
+
 	"github.com/daos-stack/daos/src/control/common"
 	"github.com/daos-stack/daos/src/control/logging"
 )
 
 const maxConcurrent = 1000
 
-func getSocketID(t *testing.T, pid int32, wg *sync.WaitGroup) {
+func getSocketID(ctx context.Context, t *testing.T, pid int32, wg *sync.WaitGroup) error {
 	defer wg.Done()
-
-	netCtx, err := Init(context.Background())
-	defer CleanUp(netCtx)
-	common.AssertEqual(t, err, nil, fmt.Sprintf("Failed to initialize NetDetectContext: %v", err))
-
-	numaNode, err := GetNUMASocketIDForPid(netCtx, int32(pid))
-	common.AssertEqual(t, err, nil, fmt.Sprintf("GetNUMASocketIDForPid error on NUMA %d / pid %d", numaNode, pid))
+	numaNode, err := GetNUMASocketIDForPid(ctx, int32(pid))
+	if err != nil {
+		return errors.Errorf("GetNUMASocketIDForPid error on NUMA %d / pid %d / error: %v", numaNode, pid, err)
+	}
+	return nil
 }
 
-// TestConcurrentGetNUMASocket launches maxConcurrent go routines
-// that concurrently access the hwloc topology.  This test verifies that there are
-// no race conditions when the topology is initialized on each access
+// TestConcurrentGetNUMASocket launches go routines
+// that concurrently call GetNUMASocketIDForPid() by itself to verify that there are
+// no race conditions
 func TestConcurrentGetNUMASocket(t *testing.T) {
 	_, buf := logging.NewTestLogger(t.Name())
 	defer common.ShowBufferOnFailure(t, buf)
 
+	netCtx, err := Init(context.Background())
+	defer CleanUp(netCtx)
+	common.AssertEqual(t, err, nil, fmt.Sprintf("Failed to initialize NetDetectContext: %v", err))
+
 	var wg sync.WaitGroup
 	pid := syscall.Getpid()
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < maxConcurrent; i++ {
 		wg.Add(1)
-		go func(n int32) {
+		go func(ctx context.Context, n int32) {
 			time.Sleep(time.Duration(rand.Intn(100)) * time.Microsecond)
-			getSocketID(t, n, &wg)
-		}(int32(pid))
+			err := getSocketID(ctx, t, n, &wg)
+			common.AssertEqual(t, err, nil, fmt.Sprintf("getSocketIDWithContext error: %v", err))
+
+		}(netCtx, int32(pid))
 	}
 	wg.Wait()
 }
 
-func getSocketIDWithContext(t *testing.T, pid int32, wg *sync.WaitGroup, ctx context.Context) {
+func getSocketAndScanFabric(ctx context.Context, t *testing.T, pid int32, wg *sync.WaitGroup) error {
 	defer wg.Done()
 	numaNode, err := GetNUMASocketIDForPid(ctx, int32(pid))
-	common.AssertEqual(t, err, nil, fmt.Sprintf("GetNUMASocketIDForPid error on NUMA %d / pid %d", numaNode, pid))
+	if err != nil {
+		return errors.Errorf("GetNUMASocketIDForPid error on NUMA %d / pid %d / error: %v", numaNode, pid, err)
+	}
+
+	provider := ""
+	results, err := ScanFabric(ctx, provider)
+	if err != nil {
+		return err
+	}
+
+	if len(results) == 0 {
+		return errors.Errorf("len was 0")
+	}
+	return err
 }
 
-// TestConcurrentGetNUMASocket launches maxConcurrent go routines
-// that concurrently access the hwloc topology.  This test verifies that there are
-// no race conditions when using a shared topology pointer
-func TestConcurrentGetNUMASocketWithContext(t *testing.T) {
+// TestConcurrentGetNUMASocketAndScanFabric launches go routines
+// that concurrently call GetNUMASocketIDForPid() and ScanFabric() together to verify that there are
+// no race conditions when called in this sequence
+func TestConcurrentGetNUMASocketAndScanFabric(t *testing.T) {
 	_, buf := logging.NewTestLogger(t.Name())
 	defer common.ShowBufferOnFailure(t, buf)
 
@@ -93,10 +111,52 @@ func TestConcurrentGetNUMASocketWithContext(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < maxConcurrent; i++ {
 		wg.Add(1)
-		go func(n int32, ctx context.Context) {
+		go func(ctx context.Context, n int32) {
 			time.Sleep(time.Duration(rand.Intn(100)) * time.Microsecond)
-			getSocketIDWithContext(t, n, &wg, ctx)
-		}(int32(pid), netCtx)
+			err := getSocketAndScanFabric(ctx, t, n, &wg)
+			common.AssertEqual(t, err, nil, fmt.Sprintf("getSocketIDWithContext error: %v", err))
+
+		}(netCtx, int32(pid))
+	}
+	wg.Wait()
+}
+
+func getScanFabric(ctx context.Context, t *testing.T, wg *sync.WaitGroup) error {
+	defer wg.Done()
+	provider := ""
+	results, err := ScanFabric(ctx, provider)
+	if err != nil {
+		return err
+	}
+
+	if len(results) == 0 {
+		return errors.Errorf("len was 0")
+	}
+	return err
+}
+
+// TestConcurrentScanFabric launches go routines
+// that concurrently call ScanFabric() by itself to verify that there are
+// no race conditions
+func TestConcurrentScanFabric(t *testing.T) {
+	_, buf := logging.NewTestLogger(t.Name())
+	defer common.ShowBufferOnFailure(t, buf)
+
+	netCtx, err := Init(context.Background())
+	defer CleanUp(netCtx)
+	common.AssertEqual(t, err, nil, fmt.Sprintf("Failed to initialize NetDetectContext: %v", err))
+
+	var wg sync.WaitGroup
+	rand.Seed(time.Now().UnixNano())
+	r := rand.Intn(1500)
+	for i := 0; i < 2*maxConcurrent+r; i++ {
+		wg.Add(1)
+		go func(ctx context.Context) {
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Microsecond)
+			err := getScanFabric(ctx, t, &wg)
+			common.AssertEqual(t, err, nil, fmt.Sprintf("ScanFabric failure: %v", err))
+
+		}(netCtx)
 	}
 	wg.Wait()
 }
