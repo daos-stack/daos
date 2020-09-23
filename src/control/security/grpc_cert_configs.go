77bbb5ddbc743c2bde9c00ced952c5d84c68cf44
David Quigley <david.quigley@intel.com>
2020-09-23 12:14:59 -0600

    Add comments explaining the code and why we need to do this
    Signed-off-by: David Quigley <david.quigley@intel.com>

--- src/control/security/grpc_cert_configs.go
+++ src/control/security/grpc_cert_configs.go
@@ -32,6 +32,18 @@ import (
 	"github.com/pkg/errors"
 )
 
+// As of Go 1.15 default certificate validation does not allow certificates
+// that only contain a CommonName with no SubjectAlternativeName. DAOS certificate
+// usage is a one certificate per component type model not a certificate per host
+// model. Because of this we do not generate certificates with SubjectAlternativeNames
+// at those SANs must be able to be validated against the hosts hostname. To handle
+// the case where this behavior is not possible to support Go 1.15 and later added
+// the ability to define custom connection validation functions for TLS connections.
+// To support the pre Go 1.15 behavior we define custom validation functions which still
+// validate the certificate chain however they do not validate the SubjectAlternativeName.
+// On the client side we still ensure the CommonName for the server is correct and
+// validate the certificate chain.
+
 func serverTLSConfig(cfg *TransportConfig) *tls.Config {
 	return &tls.Config{
 		ClientAuth:               tls.RequireAndVerifyClientCert,
@@ -59,6 +71,8 @@ func serverTLSConfig(cfg *TransportConfig) *tls.Config {
 	}
 }
 
+const ServerCommonName = "server"
+
 func clientTLSConfig(cfg *TransportConfig) *tls.Config {
 	return &tls.Config{
 		Certificates:             []tls.Certificate{*cfg.tlsKeypair},
@@ -70,7 +84,18 @@ func clientTLSConfig(cfg *TransportConfig) *tls.Config {
 			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 		},
+		// InsecureSkipVerify disables the default verifier and instead
+		// uses our customer verifier which effectively does the same thing.
 		InsecureSkipVerify: true,
+
+		// The custom verification code will take the loaded CA pool and
+		// based on the received peer certificate build the intermediate
+		// certificate chain and verify the received certificate against
+		// that chain. This is the same as the normal verification process
+		// except it will not validate the hostname of the SubjectAlternativeName.
+		// After the certificate is verified it will ensure the CommonName
+		// of the received certificate is "server" to ensure we are
+		// communicating with a DAOS server.
 		VerifyConnection: func(cs tls.ConnectionState) error {
 			opts := x509.VerifyOptions{
 				Roots:         cfg.caPool,
@@ -83,7 +108,7 @@ func clientTLSConfig(cfg *TransportConfig) *tls.Config {
 			if err != nil {
 				return err
 			}
-			if cs.PeerCertificates[0].Subject.CommonName != "server" {
+			if cs.PeerCertificates[0].Subject.CommonName != ServerCommonName {
 				return errors.New("Server certificate does not identify as Server")
 			}
 			return nil
