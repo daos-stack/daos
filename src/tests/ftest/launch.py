4a8614ec1e82fdde1a76cc529e0f4e7bc304b032
Ethan Mallove <ethanx.a.mallove@intel.com>
2020-09-25 18:12:05 +0000

    DAOS-4582 tests: debug ad-hoc scp command
    Skip-build-leap15-rpm: true
    Skip-build-ubuntu-clang: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Skip-checkpatch: true
    Quick-build: true
    Test-tag: cart
    Skip-func-test-leap15: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    
    Signed-off-by: Ethan Mallove <ethanx.a.mallove@intel.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -903,6 +903,8 @@ def archive_logs(avocado_logs_dir, test_yaml, args):
     # Copy any log files written to the DAOS_TEST_LOG_DIR directory
     logs_dir = os.environ.get("DAOS_TEST_LOG_DIR", DEFAULT_DAOS_TEST_LOG_DIR)
 
+    print("\nENV : %s\n" % os.environ)
+
     archive_files(destination, host_list, "{}/*.log*".format(logs_dir))
 
 def archive_config_files(avocado_logs_dir):
@@ -959,6 +961,7 @@ def archive_files(destination, host_list, source_files):
         "set -eu",
         "rc=0",
         "copied=()",
+        "find /var/tmp/ /mnt/share/tmp* ",
         "for file in $(ls {})".format(source_files),
         "do ls -sh $file",
         "/usr/lib/daos/TESTING/ftest/cart/util/cart_logtest.py $file",
