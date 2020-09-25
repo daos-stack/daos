904508061df8d9ad517911750bf92d15b39f28f3
Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>
2020-09-25 19:50:19 +0000

    DAOS-XXXX Fn cov
    Quick-build: true
    Skip-bullseye: false
    Skip-run_test: true
    Skip-checkpatch: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    Skip-func-test-leap15: true
    Skip-func-test: true
    Skip-build-leap15-rpm: true
    RPM-test: false
    Test-tag: cartselftest
    
    Fix
     - ci/functional/test_main_node.sh
    
    Signed-off-by: Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>

--- ci/functional/test_main_node.sh
+++ ci/functional/test_main_node.sh
@@ -19,7 +19,7 @@ DAOS_TEST_SHARED_DIR=$(mktemp -d -p /mnt/share/)
 trap 'rm -rf $DAOS_TEST_SHARED_DIR' EXIT
 
 export DAOS_TEST_SHARED_DIR
-export TEST_RPMS=true
+#export TEST_RPMS=true
 export REMOTE_ACCT=jenkins
 
 if $TEST_RPMS; then
