0deaedbb00cc2b7298073e46042d7e784a77b98c
Fan Yong <fan.yong@intel.com>
2020-09-27 03:03:16 +0800

    DAOS-4910 tests: control invalid update during daos_racer
    Too many invalid updates during long time daos_racer test may
    generate a lot of noisy error message on server as to exhaust
    server side log space. This patch controls the invalid update
    percentage, less than 1%.
    
    The patch also removes some redundant log messages.
    
    Test-tag-hw-large: pr,hw,large daosracer
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/dtx/dtx_rpc.c
+++ src/dtx/dtx_rpc.c
@@ -231,7 +231,9 @@ dtx_req_list_cb(void **args)
 		}
 
 		drr = args[0];
-		D_CDEBUG(dra->dra_result < 0, DLOG_ERR, DB_TRACE,
+		D_CDEBUG(dra->dra_result < 0 &&
+			 dra->dra_result != -DER_NONEXIST,
+			 DLOG_ERR, DB_TRACE,
 			 "DTX req for opc %x ("DF_DTI") %s, count %d: %d.\n",
 			 dra->dra_opc, DP_DTI(drr->drr_dti),
 			 dra->dra_result < 0 ? "failed" : "succeed",
