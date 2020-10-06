0e44e4bf19f4ef27366e195df6db6bb1a5391b24
wangdi <di.wang@intel.com>
2020-10-06 09:05:07 -0700

    DAOS-5717 test: remove a few unnecessary sleep in the test (#3535)
    1. Remove a few unnecessary sleep in the test after we shrink
    SWIM_SUSPECT time.
    
    2. reschedule un-complete the target rebuild, otherwise skipping
    the failure target will cause futher failure.
    
    3. Remove rebuild aggregation, which is used to track the failure
    of ranks to start rebuild. Since the whole process will reply on
    pool map changing, so we do not need this anymore.
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/tests/suite/daos_test_common.c
+++ src/tests/suite/daos_test_common.c
@@ -665,7 +665,7 @@ rebuild_pool_wait(test_arg_t *arg)
 	pinfo.pi_bits = DPI_REBUILD_STATUS;
 	rc = test_pool_get_info(arg, &pinfo);
 	rst = &pinfo.pi_rebuild_st;
-	if (rst->rs_done || rc != 0) {
+	if ((rst->rs_done || rc != 0) && rst->rs_version != 0) {
 		print_message("Rebuild "DF_UUIDF" (ver=%d) is done %d/%d, "
 			      "obj="DF_U64", rec="DF_U64".\n",
 			       DP_UUID(arg->pool.pool_uuid), rst->rs_version,
