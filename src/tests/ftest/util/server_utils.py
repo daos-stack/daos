4536f172c38021f7ead5719806fafc90b4e3321f
Phillip Henderson <phillip.henderson@intel.com>
2020-10-08 10:22:20 -0400

    Updated correct timeout.
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/server_utils.py
+++ src/tests/ftest/util/server_utils.py
@@ -159,7 +159,6 @@ class DaosServerCommand(YamlCommand):
             value = self.yaml.using_dcpm
         return value
 
-
     class NetworkSubCommand(CommandWithSubCommand):
         """Defines an object for the daos_server network sub command."""
 
@@ -466,9 +465,7 @@ class DaosServerManager(SubprocessManager):
             cmd.sub_command_class.sub_command_class.hugepages.value = 4096
 
         self.log.info("Preparing DAOS server storage: %s", str(cmd))
-        # Temporarily increasing timeout to avoid CI errors until DAOS-5764 can
-        # be further investigated.
-        result = pcmd(self._hosts, str(cmd), timeout=40)
+        result = pcmd(self._hosts, str(cmd), timeout=30)
         if len(result) > 1 or 0 not in result:
             dev_type = "nvme"
             if using_dcpm and using_nvme:
@@ -557,7 +554,9 @@ class DaosServerManager(SubprocessManager):
         # Format storage and wait for server to change ownership
         self.log.info(
             "<SERVER> Formatting hosts: <%s>", self.dmg.hostlist)
-        self.dmg.storage_format()
+        # Temporarily increasing timeout to avoid CI errors until DAOS-5764 can
+        # be further investigated.
+        self.dmg.storage_format(timeout=40)
 
         # Wait for all the doas_io_servers to start
         self.detect_io_server_start()
