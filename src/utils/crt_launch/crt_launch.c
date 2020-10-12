19ba05228324b16ceac63f5a9b54a0baf653a843
Alexander Oganezov <alexander.a.oganezov@intel.com>
2020-10-12 10:59:34 -0700

    CART-89 ports: Add port option to crt_launch and checks (#3629)
    - crt_init() checks for supplied OFI_PORT to be outside of
    the range specified by /proc/sys/net/ipv4/ip_local_port_range
    
    - crt_launch utility modified to add optional -p argument
    that allows specification of a start port number from which ports
    are handed out to all servers
    
    - -e <app> option modified to report error if <app> is not found
    
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/utils/crt_launch/crt_launch.c
+++ src/utils/crt_launch/crt_launch.c
@@ -70,6 +70,10 @@
  * be handed out to random cart contexts.
  *
  * echo 31416-31516 > /proc/sys/net/ipv4/ip_local_reserved_ports
+ *
+ * Alternatively a port selected must be outside of the local port
+ * range specified by:
+ * /proc/sys/net/ipv4/ip_local_port_range
  */
 #define START_PORT 31416
 
@@ -87,6 +91,7 @@ struct options_t {
 	int	show_help;
 	char	*app_to_exec;
 	int	app_args_indx;
+	int	start_port;
 };
 
 struct options_t g_opt;
@@ -96,9 +101,10 @@ show_usage(const char *msg)
 {
 	printf("----------------------------------------------\n");
 	printf("%s\n", msg);
-	printf("Usage: crt_launch [-ch] <-e app_to_exec app_args>\n");
+	printf("Usage: crt_launch [-cph] <-e app_to_exec app_args>\n");
 	printf("Options:\n");
 	printf("-c	: Indicate app is a client\n");
+	printf("-p	: Optional argument to set first port to use\n");
 	printf("-h	: Print this help and exit\n");
 	printf("----------------------------------------------\n");
 }
@@ -110,13 +116,16 @@ parse_args(int argc, char **argv)
 	int				rc = 0;
 	struct option			long_options[] = {
 		{"client",	no_argument,		0, 'c'},
+		{"port",	required_argument,	0, 'p'},
 		{"help",	no_argument,		0, 'h'},
 		{"exec",	required_argument,	0, 'e'},
 		{0, 0, 0, 0}
 	};
 
+	g_opt.start_port = START_PORT;
+
 	while (1) {
-		rc = getopt_long(argc, argv, "e:ch", long_options,
+		rc = getopt_long(argc, argv, "e:p:ch", long_options,
 				 &option_index);
 		if (rc == -1)
 			break;
@@ -131,6 +140,9 @@ parse_args(int argc, char **argv)
 			g_opt.app_to_exec = optarg;
 			g_opt.app_args_indx = optind - 1;
 			return 0;
+		case 'p':
+			g_opt.start_port = atoi(optarg);
+			break;
 		default:
 			g_opt.show_help = true;
 			return 1;
@@ -149,13 +161,12 @@ get_self_uri(struct host *h, int rank)
 	char		*p;
 	int		len;
 	int		rc;
-	char		*str_port;
+	char		*str_port = NULL;
 
-	/* Assign ports sequentually to each rank starting from START_PORT */
-	rc = asprintf(&str_port, "%d", START_PORT + rank);
-	if (rc == -1) {
+	/* Assign ports sequentually to each rank */
+	D_ASPRINTF(str_port, "%d", g_opt.start_port + rank);
+	if (str_port == NULL)
 		return -DER_NOMEM;
-	}
 
 	setenv("OFI_PORT", str_port, 1);
 
@@ -210,6 +221,7 @@ get_self_uri(struct host *h, int rank)
 	}
 
 out:
+	D_FREE(str_port);
 	return rc;
 }
 
@@ -281,6 +293,12 @@ int main(int argc, char **argv)
 
 	if (g_opt.app_to_exec == NULL) {
 		show_usage("-e option is required\n");
+		return -1;
+	}
+
+	if (access(g_opt.app_to_exec, F_OK) == -1) {
+		fprintf(stderr, "ERROR: Unable to locate '%s'\n",
+			g_opt.app_to_exec);
 		return -1;
 	}
 
