010e6ea5b87131bead7f95a1d2ce51e2ba59b55b
Michael MacDonald <mjmac.macdonald@intel.com>
2020-10-15 16:28:09 -0400

    disable automatic NVMe prepare

--- src/control/server/server.go
+++ src/control/server/server.go
@@ -176,10 +176,13 @@ func Start(log *logging.LeveledLogger, cfg *config.Server) error {
 		}
 	}
 
-	log.Debugf("automatic NVMe prepare req: %+v", prepReq)
-	if _, err := bdevProvider.Prepare(prepReq); err != nil {
-		log.Errorf("automatic NVMe prepare failed (check configuration?)\n%s", err)
-	}
+	/*
+		log.Debugf("automatic NVMe prepare req: %+v", prepReq)
+		if _, err := bdevProvider.Prepare(prepReq); err != nil {
+			log.Errorf("automatic NVMe prepare failed (check configuration?)\n%s", err)
+		}
+	*/
+	log.Info("automatic NVMe prepare disabled on Endeavour")
 
 	hugePages, err := getHugePageInfo()
 	if err != nil {
