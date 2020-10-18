b3f9174215a81ba320e64b3272dcfff56cd61965
Makito Kano <makito.kano@intel.com>
2020-10-18 21:25:07 +0000

    DAOS-5833 test: Removed pool disconnect in TestContainer.create method
    Skip-unit-test: true
    Skip-func-test: true
    Test-tag-hw-small: daos_object_query
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/util/test_utils_container.py
+++ src/tests/ftest/util/test_utils_container.py
@@ -337,9 +337,6 @@ class TestContainer(TestDaosApiBase):
             self._call_method(self.container.create, kwargs)
 
         elif self.control_method.value == self.USE_DAOS and self.daos:
-            # Disconnect the pool if connected
-            self.pool.disconnect()
-
             # Create a container with the daos command
             kwargs = {
                 "pool": self.pool.uuid,
