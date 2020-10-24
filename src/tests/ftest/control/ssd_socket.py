d6dab4aad4ad47aedac81699ee76c32c91080ad0
Makito Kano <makito.kano@intel.com>
2020-10-24 17:56:09 +0000

    DAOS-3584 test: Verify Socket ID against NUMA socket ID value defined in file system
    Test-tag-hw-small: ssd_socket
    Skip-unit-test: true
    Skip-func-test: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    Signed-off-by: Makito Kano <makito.kano@intel.com>

---
+++ src/tests/ftest/control/ssd_socket.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+"""
+  (C) Copyright 2020 Intel Corporation.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+  The Government's rights to use, modify, reproduce, release, perform, display,
+  or disclose this software are subject to the terms of the Apache License as
+  provided in Contract No. B609815.
+  Any reproduction of computer software, computer software documentation, or
+  portions thereof marked with this legend must also reproduce the markings.
+"""
+from apricot import TestWithServers
+from general_utils import run_command, DaosTestError
+
+
+class SSDSocketTest(TestWithServers):
+    """Test Class Description: Verify NVMe NUMA socket values.
+
+    Call dmg storage scan --verbose to obtain NUMA socket value (Socket ID) of
+    each NVMe disk. Verify against the value in
+    /sys/bus/pci/devices/<PCI address>/numa_node
+
+    :avocado: recursive
+    """
+
+    def test_scan_ssd(self):
+        """
+        JIRA ID: DAOS-3584
+
+        Test Description: Verify NVMe NUMA socket values.
+
+        :avocado: tags=all,small,full_regression,hw,control,ssd_socket
+        """
+        # Call dmg storage scan --verbose and get the PCI addresses.
+        data = self.get_dmg_command().storage_scan(verbose=True)
+        pci_addrs = data[self.hostlist_servers[0]]["nvme"].keys()
+        self.log.info("Testing PCI addresses: {}".format(pci_addrs))
+
+        # For every PCI address, verify its Socket ID against its NUMA socket
+        # ID.
+        errors = []
+        for pci_addr in pci_addrs:
+            cmd_socket_id = data[self.hostlist_servers[0]]["nvme"][pci_addr]\
+                ["socket"]
+            numa_node_path = "/sys/bus/pci/devices/{}/numa_node".format(
+                pci_addr)
+            try:
+                cmd_result = run_command(command="cat {}".format(numa_node_path))
+            except DaosTestError:
+                errors.append("{} not found!".format(numa_node_path))
+                continue
+
+            fs_socket_id = cmd_result.stdout.rstrip()
+            self.assertEqual(
+                cmd_socket_id, fs_socket_id,
+                "Unexpected socket ID! Cmd: {}; FS: {}".format(
+                    cmd_socket_id, fs_socket_id))
+
+        if errors:
+            self.fail("Error found!\n{}".format("\n".join(errors)))
