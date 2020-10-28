5b8e86e192e220acc3a56c10c09b4c9b17d80c9e
Phillip Henderson <phillip.henderson@intel.com>
2020-10-28 18:25:29 -0400

    Fix pylint error.
    Test-tag-hw-medium: pr,hw,medium,ib2 storage_ratio crashior
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/pool/create_test.py
+++ src/tests/ftest/pool/create_test.py
@@ -28,6 +28,7 @@ from server_utils import ServerFailed
 
 
 class PoolCreateTests(PoolTestBase):
+    # pylint: disable=too-many-ancestors
     """Pool create tests.
 
     All of the tests verify pool create performance with 7 servers and 1 client.
