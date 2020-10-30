ee90ef9a0fb4047b51b71c03021ccd0443355243
Maureen Jean <maureen.jean@intel.com>
2020-10-30 06:51:13 -0400

    prepend env to debuild
    faults-enabled:false
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- utils/rpms/packaging/Makefile_packaging.mk
+++ utils/rpms/packaging/Makefile_packaging.mk
@@ -236,8 +236,8 @@ $(subst deb,%,$(DEBS)): $(DEB_BUILD).tar.$(SRC_EXT) \
 	      $(DEB_TOP)/*.dsc $(DEB_TOP)/*.build* $(DEB_TOP)/*.changes \
 	      $(DEB_TOP)/*.debian.tar.*
 	rm -rf $(DEB_TOP)/*-tmp
-	cd $(DEB_BUILD); debuild --no-lintian -b -us -uc
-	cd $(DEB_BUILD); debuild -- clean
+	cd $(DEB_BUILD); "SCONS_ARGS=$(SCONS_ARGS)" debuild --no-lintian -b -us -uc
+	cd $(DEB_BUILD); "SCONS_ARGS=$(SCONS_ARGS)" debuild -- clean
 	git status
 	rm -rf $(DEB_TOP)/$(NAME)-tmp
 	lfile1=$(shell echo $(DEB_TOP)/$(NAME)[0-9]*_$(VERSION)-1_amd64.deb);\
@@ -249,7 +249,7 @@ $(subst deb,%,$(DEBS)): $(DEB_BUILD).tar.$(SRC_EXT) \
 	    sed 's/$(DEB_RVERS)-1/$(DEB_BVERS)/' \
 	    $(DEB_TOP)/$(NAME)-tmp/DEBIAN/symbols \
 	    > $(DEB_BUILD)/debian/$${lname}.symbols; fi
-	cd $(DEB_BUILD);  debuild -us -uc
+	cd $(DEB_BUILD);  "SCONS_ARGS=$(SCONS_ARGS)" debuild -us -uc
 	rm $(DEB_BUILD).tar.$(SRC_EXT)
 	for f in $(DEB_TOP)/*.deb; do \
 	  echo $$f; dpkg -c $$f; done
@@ -260,7 +260,7 @@ $(DEB_TOP)/$(DEB_DSC): $(CALLING_MAKEFILE) $(DEB_BUILD).tar.$(SRC_EXT) \
 	  $(DEB_TOP)/*.dsc $(DEB_TOP)/*.build* $(DEB_TOP)/*.changes \
 	  $(DEB_TOP)/*.debian.tar.*
 	rm -rf $(DEB_TOP)/*-tmp
-	cd $(DEB_BUILD); debuild --no-lintian -b -us -uc  --set-env="SCONS_ARGS=$(SCONS_ARGS)"
+	cd $(DEB_BUILD); "SCONS_ARGS=$(SCONS_ARGS)" debuild --no-lintian -b -us -uc
 
 $(SRPM): $(SPEC) $(SOURCES)
 	rpmbuild -bs $(COMMON_RPM_ARGS) $(RPM_BUILD_OPTIONS) $(SPEC)
