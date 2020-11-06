7d2b1d7cff76b63b8e2bd61423194ba21286cb0b
Maureen Jean <maureen.jean@intel.com>
2020-11-06 15:09:16 -0500

    DAOS-5469 test: Update hdf5 rpms and test
    PR-repos: hdf5@PR-16 hdf5-vol-daos@PR-2
    Test-tag-hw-small: vol
    Test-tag-hw-large: iorsmall
    Skip-checkpatch: true
    Skip-run_test: true
    Skip-func-test: true
    Skip-func-hw-test-medium: true
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/io/daos_vol.py
+++ src/tests/ftest/io/daos_vol.py
@@ -31,17 +31,6 @@ class DaosVol(VolTestBase):
     :avocado: recursive
     """
 
-    def setUp(self):
-        """Set up each test case."""
-        # Cancel h5_test_testhdf5 with MPICH
-        mpi_type = self.params.get("job_manager_mpi_type")
-        testname = self.params.get("testname")
-        if mpi_type == "mpich" and testname == "h5_test_testhdf5":
-            self.cancelForTicket("DAOS-5469")
-        if testname == "h5_partest_t_bigio":
-            self.cancelForTicket("DAOS-5496")
-        super(DaosVol, self).setUp()
-
     def test_daos_vol(self):
         """Jira ID: DAOS-3656.
 
@@ -59,6 +48,6 @@ class DaosVol(VolTestBase):
               h5daos_test_map_parallel
               h5daos_test_oclass
 
-        :avocado: tags=all,hw,small,hdf5,vol,DAOS_5610
+        :avocado: tags=all,pr,hw,small,hdf5,vol,DAOS_5610
         """
         self.run_test()
