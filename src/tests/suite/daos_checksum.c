4cbb16cf8edc9ddf5c7503b4448bf897c8331ea3
Ryon Jensen <ryon.jensen@intel.com>
2020-11-09 11:32:28 -0700

    DAOS-5632 csum: EC tests for arrays (#3479)
    Test that the checksums for recxs are verified
    correctly when EC and checksums is enabled
    
    Signed-off-by: Ryon Jensen <ryon.jensen@intel.com>

--- src/tests/suite/daos_checksum.c
+++ src/tests/suite/daos_checksum.c
@@ -640,6 +640,7 @@ struct partial_unaligned_fetch_testcase_args {
 	size_t			 chunksize;
 	struct recx_config	 recx_cfgs[RECX_CONFIGS_NR];
 	daos_recx_t		 fetch_recxs[FETCH_RECX_NR];
+	daos_oclass_id_t	 oclass;
 };
 
 static void
@@ -719,6 +720,9 @@ array_update_fetch_testcase(char *file, int line, test_arg_t *test_arg,
 	size_t			max_data_size = 0;
 	int			rc;
 	int			i;
+
+	if (args->oclass != OC_UNKNOWN)
+		oc = args->oclass;
 
 	if (args->dkey != NULL)
 		d_iov_set(&ctx.dkey, args->dkey, strlen(args->dkey));
@@ -1845,6 +1849,7 @@ rebuild_3(void **state)
 {
 	rebuild_test(state, 1024, BULK_DATA, DAOS_IOD_ARRAY);
 }
+
 static void
 rebuild_4(void **state)
 {
@@ -2204,6 +2209,49 @@ test_enumerate_object_csum_buf_too_small(void **state)
 	cleanup_cont_obj(&ctx);
 }
 
+static void
+ec_chunk_plus_one(void **state)
+{
+	const uint32_t ec_chunk_plus_one = 1024 * 1024 + 1;
+
+	if (!test_runable(*state, 3))
+		skip();
+
+
+	ARRAY_UPDATE_FETCH_TESTCASE(state, {
+		.oclass = OC_EC_2P1G1,
+		.chunksize = 8,
+		.csum_prop_type = dts_csum_prop_type,
+		.server_verify = false,
+		.rec_size = 1,
+		.recx_cfgs = {
+			{.idx = 0, .nr = ec_chunk_plus_one, .data = "ABCDEF"},
+		},
+		.fetch_recxs = { {.rx_idx = 0, .rx_nr = ec_chunk_plus_one} },
+	});
+}
+
+static void
+ec_two_chunk_plus_one(void **state)
+{
+	const uint32_t ec_chunk_plus_one = 2 * 1024 * 1024 + 1;
+
+	if (!test_runable(*state, 3))
+		skip();
+
+	ARRAY_UPDATE_FETCH_TESTCASE(state, {
+		.oclass = OC_EC_2P1G1,
+		.chunksize = 8,
+		.csum_prop_type = dts_csum_prop_type,
+		.server_verify = false,
+		.rec_size = 1,
+		.recx_cfgs = {
+			{.idx = 0, .nr = ec_chunk_plus_one, .data = "ABCDEF"},
+		},
+		.fetch_recxs = { {.rx_idx = 0, .rx_nr = ec_chunk_plus_one} },
+	});
+}
+
 static int
 setup(void **state)
 {
@@ -2293,6 +2341,10 @@ static const struct CMUnitTest csum_tests[] = {
 	EC_CSUM_TEST("DAOS_EC_CSUM03: Single Value Checksum", single_value),
 	EC_CSUM_TEST("DAOS_EC_CSUM04: DTX with checksum enabled against EC obj",
 		     dtx_with_csum),
+	CSUM_TEST("DAOS_EC_CSUM04: Single extent that is 1 byte larger than "
+		  "EC chunk", ec_chunk_plus_one),
+	CSUM_TEST("DAOS_EC_CSUM04: Single extent that is 1 byte larger than "
+		  "2 EC chunks", ec_two_chunk_plus_one),
 };
 
 static int
