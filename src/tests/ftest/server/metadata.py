ed4dcba6cb633b4c25230e6c947ad6f3a56f2a01
Ding Ho <ding-hwa.ho@intel.com>
2020-11-12 20:46:33 +0000

    Update with missing spaces for test.log.
    Test-tag-hw-small: pr,hw,small
    Signed-off-by: Ding Ho <ding-hwa.ho@intel.com>

--- src/tests/ftest/server/metadata.py
+++ src/tests/ftest/server/metadata.py
@@ -397,12 +397,12 @@ class ObjectMetadata(TestWithServers):
                         "##Expecting der_no_space RC: -1007, not seen")
                 else:
                     self.log.info(traceback.format_exc())
-                    self.log.info("(1.4)No space error shown with additional"
+                    self.log.info("(1.4)No space error shown with additional "
                                   "%d containers created, test passed.", cont)
                 break
         if cont == additional_container - 1:
             self.fail(
-                "##Storage resource did not exhausted after {} containers"
+                "##Storage resource did not exhausted after {} containers "
                 "created".format(len(container_array)))
         self.log.info(
             "(1.5)Additional %d containers created and detected der_no_space.",
