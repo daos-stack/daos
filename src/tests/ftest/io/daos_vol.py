e0c4a0d1fcf2515b164c268e5d414f000c65beab
Maureen Jean <maureen.jean@intel.com>
2020-11-15 19:10:37 -0500

    disable h5_*testhdf5
    Skip-build-ubuntu20-rpm: true
    PR-repos: hdf5@PR-16 hdf5-vol-daos@PR-2 ior-hpc@PR-6 MACSio@PR-4
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/io/daos_vol.py
+++ src/tests/ftest/io/daos_vol.py
@@ -31,6 +31,19 @@ class DaosVol(VolTestBase):
     :avocado: recursive
     """
 
+    def setUp(self):
+        """Set up each test case."""
+        # Cancel h5_test_testhdf5 with MPICH
+        mpi_type = self.params.get("job_manager_mpi_type")
+        testname = self.params.get("testname")
+        if mpi_type == "mpich" and testname == "h5_test_testhdf5":
+            self.cancelForTicket("DAOS-5469")
+        if mpi_type == "mpich" and testname == "h5_partest_testhdf5":
+            self.cancelForTicket("DAOS-5469")
+        if testname == "h5_partest_t_bigio":
+            self.cancelForTicket("DAOS-5496")
+        super(DaosVol, self).setUp()
+
     def test_daos_vol(self):
         """Jira ID: DAOS-3656.
 
