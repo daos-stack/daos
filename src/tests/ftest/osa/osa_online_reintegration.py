78b1a87df49549b37aab5e2655170fd62f7bc18d
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-11-16 00:04:04 -0500

    DAOS-5920 test: test only server restart test
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/osa/osa_online_reintegration.py
+++ src/tests/ftest/osa/osa_online_reintegration.py
@@ -294,7 +294,7 @@ class OSAOnlineReintegration(TestWithServers):
 
         Test Description: Validate Online Reintegration
 
-        :avocado: tags=all,pr,hw,large,osa,online_reintegration,DAOS_5610
+        :avocado: tags=all,pr,hw,large,osa,on_reintegration_ex,DAOS_5610
         """
         # Perform reintegration testing with 1 pool.
         for pool_num in range(1, 2):
