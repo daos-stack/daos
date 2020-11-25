66c437dc36c45b55d1b8e1a7a05b30ff5519e9ec
jiafu zhang <jiafu.zhang@intel.com>
2020-11-25 11:03:46 +0800

    Revert "suggested style changes from daosbuild"
    This reverts commit aef5236a88a17d7eb3e8a61997bddb5c02d094aa.
    
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- src/client/java/daos-java/src/main/native/io_daos_DaosClient.c
+++ src/client/java/daos-java/src/main/native/io_daos_DaosClient.c
@@ -68,10 +68,9 @@ Java_io_daos_DaosClient_daosOpenPool(JNIEnv * env,
 		daos_handle_t poh;
 		int rc;
 
-		int rc;
-
 		rc = daos_pool_connect(pool_uuid, server_group, svcl,
-				       flags,
+			flags,
+			&poh /* returned pool handle */,
 			NULL /* returned pool info */,
 			NULL /* event */);
 
