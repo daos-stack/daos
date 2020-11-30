bab39568594c4f3f96784d54df2402063ea56c6a
Johann Lombardi <johann.lombardi@intel.com>
2020-11-30 16:20:31 +0100

    Re-add pr
    Signed-off-by: Johann Lombardi <johann.lombardi@intel.com>

--- src/tests/ftest/osa/osa_offline_drain.py
+++ src/tests/ftest/osa/osa_offline_drain.py
@@ -115,7 +115,7 @@ class OSAOfflineDrain(OSAUtils):
 
         Test Description: Validate Offline Drain
 
-        :avocado: tags=all,hw,large,osa,osa_drain,offline_drain
+        :avocado: tags=all,pr,hw,large,osa,osa_drain,offline_drain
         """
         for pool_num in range(1, 3):
             self.run_offline_drain_test(pool_num, True)
