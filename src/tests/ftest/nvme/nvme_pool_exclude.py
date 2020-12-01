fff8d3580324dacfbec37df4ffc56559fa10724d
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-11-30 21:01:43 -0500

    DAOS-2086 test: Skip test due to DAOS-6108 Test-tag-hw-large: pr,hw,large nvme_pool_exclude
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/nvme/nvme_pool_exclude.py
+++ src/tests/ftest/nvme/nvme_pool_exclude.py
@@ -28,7 +28,7 @@ import threading
 
 from itertools import product
 from avocado import fail_on
-from apricot import TestWithServers
+from apricot import TestWithServers, skipForTicket
 from test_utils_pool import TestPool
 from ior_utils import IorCommand
 from job_manager_utils import Mpirun
@@ -240,6 +240,7 @@ class NvmePoolExclude(TestWithServers):
             self.pool.display_pool_daos_space(display_string)
             pool[val].destroy()
 
+    @skipForTicket("DAOS-6108")
     def test_nvme_pool_excluded(self):
         """Test ID: DAOS-2086
         Test Description: NVME Pool Exclude
