daos-stack/daos

commit 3c92f9f870382d1a44e6d5cb58fef00bff73c824

DAOS-5731 common: DAOS 1.2 Coverity defects group fixing (#3672)
Fixing various defects flagged by coverity scans

Signed-off-by: Hua Kuang <hua.kuang@intel.com>
Co-authored-by: Ashley Pittman <ashley.m.pittman@intel.com>

src/pool/srv_target.c
@@ -380,7 +380,10 @@ err_iv_ns:
 err_group:
 	crt_group_secondary_destroy(pool->sp_group);
 err_done_cond:
-	ABT_cond_free(&pool->sp_fetch_hdls_done_cond);
+	rc = ABT_cond_free(&pool->sp_fetch_hdls_done_cond);
+	if (rc != 0)
+		D_ERROR(DF_UUID": failed to destroy pool group: %d\n",
+			DP_UUID(pool->sp_uuid), rc);
 err_cond:
 	ABT_cond_free(&pool->sp_fetch_hdls_cond);
 err_mutex:

src/bio/bio_device.c
@@ -332,7 +332,7 @@ replace_dev(struct bio_xs_context *xs_ctxt, struct smd_dev_info *old_info,
 	rc = smd_pool_list(&pool_list, &pool_cnt);
 	if (rc) {
 		D_ERROR("Failed to list pools in SMD. "DF_RC"\n", DP_RC(rc));
-		goto out;
+		goto pool_list_out;
 	}
 
 	rc = create_old_blobs(xs_ctxt, old_info, new_dev, &pool_list,
@@ -373,6 +373,7 @@ replace_dev(struct bio_xs_context *xs_ctxt, struct smd_dev_info *old_info,
 
 out:
 	free_blob_list(xs_ctxt, &blob_list, new_dev);
+pool_list_out:
 	free_pool_list(&pool_list);
 	if (new_dev)
 		new_dev->bb_replacing = false;

src/bio/bio_xstream.c
@@ -827,7 +827,9 @@ bio_bdev_event_cb(enum spdk_bdev_event_type type, struct spdk_bdev *bdev,
 		return;
 	}
 
-	spdk_thread_send_msg(owner_thread(bbs), teardown_bio_bdev, d_bdev);
+	if (bbs != NULL)
+		spdk_thread_send_msg(owner_thread(bbs), teardown_bio_bdev,
+				     d_bdev);
 }
 
 void
@@ -1680,8 +1682,9 @@ scan_bio_bdevs(struct bio_xs_context *ctxt, uint64_t now)
 			continue;
 
 		scan_period = 0;
-		spdk_thread_send_msg(owner_thread(bbs), teardown_bio_bdev,
-				     d_bdev);
+		if (bbs != NULL)
+			spdk_thread_send_msg(owner_thread(bbs),
+					     teardown_bio_bdev, d_bdev);
 	}
 
 	if (scan_period == 0)

src/cart/crt_init.c
@@ -292,9 +292,8 @@ crt_init_opt(crt_group_id_t grpid, uint32_t flags, crt_init_options_t *opt)
 	if (gdata_init_flag == 0) {
 		rc = data_init(server, opt);
 		if (rc != 0) {
-			D_ERROR("data_init failed, rc(%d) - %s.\n",
-				rc, strerror(rc));
-			D_GOTO(out, rc = -rc);
+			D_ERROR("data_init failed, "DF_RC"\n", DP_RC(rc));
+			D_GOTO(out, rc);
 		}
 	}
 	D_ASSERT(gdata_init_flag == 1);

src/cart/crt_iv.c
@@ -2739,7 +2739,7 @@ bulk_update_transfer_done_aux(const struct crt_bulk_cb_info *info)
 					&cb_info->buc_iv_value,
 					cb_info->buc_user_priv);
 		output->rc = rc;
-		rc = crt_reply_send(info->bci_bulk_desc->bd_rpc);
+		crt_reply_send(info->bci_bulk_desc->bd_rpc);
 
 		RPC_PUB_DECREF(info->bci_bulk_desc->bd_rpc);
 		IVNS_DECREF(update_cb_info->uci_ivns_internal);

src/cart/crt_self_test_client.c
@@ -539,12 +539,18 @@ test_rpc_cb(const struct crt_cb_info *cb_info)
 {
 	struct st_cb_args	*cb_args = cb_info->cci_arg;
 	struct timespec		 now;
+	int			 ret;
 
 	D_ASSERT(cb_args != NULL);
 	D_ASSERT(g_data != NULL);
 
 	/* Record latency of this RPC */
-	d_gettime(&now);
+	ret = d_gettime(&now);
+	if (ret != 0) {
+		D_ERROR("d_gettime failed; ret = %d\n", ret);
+		return;
+	}
+
 	g_data->rep_latencies[cb_args->rep_idx].val =
 		d_timediff_ns(&cb_args->sent_time, &now);
 

src/cart/swim/swim.c
@@ -602,6 +602,7 @@ void
 swim_fini(struct swim_context *ctx)
 {
 	struct swim_item *next, *item;
+	int    rc;
 
 	if (ctx == NULL)
 		return;
@@ -638,7 +639,10 @@ swim_fini(struct swim_context *ctx)
 		item = next;
 	}
 
-	SWIM_MUTEX_DESTROY(ctx->sc_mutex);
+	rc = SWIM_MUTEX_DESTROY(ctx->sc_mutex);
+	if (rc != 0)
+		D_DEBUG(DB_TRACE, "Failed to destroy lock %d %s",
+			rc, strerror(rc));
 
 	D_FREE(ctx);
 }

src/client/dfs/dfs.c
@@ -321,8 +321,9 @@ fetch_entry(daos_handle_t oh, daos_handle_t th, const char *name,
 
 	rc = daos_obj_fetch(oh, th, 0, &dkey, 1, &iod, &sgl, NULL, NULL);
 	if (rc) {
-		D_ERROR("Failed to fetch entry %s (%d)\n", name, rc);
-		return rc;
+		D_ERROR("Failed to fetch entry %s " DF_RC "\n", name,
+			DP_RC(rc));
+		return daos_der2errno(rc);
 	}
 
 	if (fetch_sym && S_ISLNK(entry->mode)) {
@@ -344,7 +345,8 @@ fetch_entry(daos_handle_t oh, daos_handle_t th, const char *name,
 		rc = daos_obj_fetch(oh, th, 0, &dkey, 1, &iod, &sgl, NULL,
 				    NULL);
 		if (rc) {
-			D_ERROR("Failed to fetch entry %s (%d)\n", name, rc);
+			D_ERROR("Failed to fetch entry %s " DF_RC "\n", name,
+				DP_RC(rc));
 			D_GOTO(out, rc = daos_der2errno(rc));
 		}
 

commit e9643affdd48f520ff0f0e812fcbcaa903a7efd2

DAOS-6204 pool: Fix pool_alloc_ref error reporting
When pool_alloc_ref encounters certain errors, the ABT_cond_free call
under err_done_cond throws away the error and report its own return
value, which is likely 0, instead. This has led to segfaults in
daos_lru_ref_hold, which sees the 0 return value, believes the entry has
been allocated successfully, and dereferences the NULL entry pointer. It
is a regression introduced by #3672. This patch guesses that #3672
intends to check the return value of the crt_group_secondary_destroy
call instead, and employ a temporary variable to store its return value.

Signed-off-by: Li Wei <wei.g.li@intel.com>

src/pool/srv_target.c
@@ -318,6 +318,7 @@ pool_alloc_ref(void *key, unsigned int ksize, void *varg,
 	struct dss_module_info	       *info = dss_get_module_info();
 	unsigned int			iv_ns_id;
 	int				rc;
+	int				rc_tmp;
 
 	if (arg == NULL) {
 		/* The caller doesn't want to create a ds_pool object. */
@@ -385,12 +386,12 @@ pool_alloc_ref(void *key, unsigned int ksize, void *varg,
 err_iv_ns:
 	ds_iv_ns_destroy(pool->sp_iv_ns);
 err_group:
-	crt_group_secondary_destroy(pool->sp_group);
+	rc_tmp = crt_group_secondary_destroy(pool->sp_group);
+	if (rc_tmp != 0)
+		D_ERROR(DF_UUID": failed to destroy pool group: "DF_RC"\n",
+			DP_UUID(pool->sp_uuid), DP_RC(rc_tmp));
 err_done_cond:
-	rc = ABT_cond_free(&pool->sp_fetch_hdls_done_cond);
-	if (rc != 0)
-		D_ERROR(DF_UUID": failed to destroy pool group: %d\n",
-			DP_UUID(pool->sp_uuid), rc);
+	ABT_cond_free(&pool->sp_fetch_hdls_done_cond);
 err_cond:
 	ABT_cond_free(&pool->sp_fetch_hdls_cond);
 err_mutex: