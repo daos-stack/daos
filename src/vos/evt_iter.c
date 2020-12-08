1490cfc22e6c88d9aa3db49e30655ca549c5a9a1
Fan Yong <fan.yong@intel.com>
2020-12-09 00:43:00 +0800

    DAOS-5554 dtx: async batched commit distributed  transactions
    Originally, for the distributed transaction that cross multiple
    redundancy groups, we will sychronously commit it when all the
    participants 'prepared'. Such sync commit may introduce latency
    for related distributed transaction.
    
    This patch adjusts the commit behavior, similar as the case of
    stand-alone modification, the 'committable' DTX will be cached
    on the leader, and will be batched committed via dedicated ULT
    sometime later. It is expected that some transactions may have
    some participants on the same DAOS target(s), then the batched
    commit can save some DTX commit RPCs.
    
    Under such async batched commit mode, the DTX status on leader
    may be 'committable', but is 'prepared' on non-leader. If some
    read request hits these 'prepared' DTX on non-leader (in VOS),
    then related caller (object) will get -DER_INPROGRESS together
    with related DTX information (ID, leader), and then the caller
    will query with related DTX leader whether it is committable
    or not. If yes, reply the non-leader and then commit such DRX
    to avoid further check by others. Introduce 'DTX_REFRESH' RPC
    for such purpose.
    
    Another main change is for DTX resync to handle the distributed
    transaction that lost some whole redundancy group. Under such
    case, the DTX resync logic cannot make the decision whether
    commit or abort related transaction, because either one maybe
    wrong as to cause or data loss or break transaction semantics.
    Then it will mark the DTX entry as 'corrputed' globally. That
    will prevent subsequent read operations against such DTX via
    returning -DER_DATA_LOSS. On the other hand, the 'corrupted'
    DTX only affects its modified pieces, but not affect others
    that share the same target (object/key). The targets attached
    to the 'corrupted' DTX will not be purged by vos aggregation,
    but may be freshed by new update/punch. We need special tools
    to handle (recover or cleanup) those 'corrupted' DTX entries
    in the future.
    
    The patch also fixes current confused 'rebuild' intent for
    DTX availability check. We use new 'DAOS_INTENT_MIGRATION'
    for all rebuild and migration related logic. The DTX logic
    in VOS needs some special handling for the operations with
    such intent, for example, return 'ALB_UNAVAILABLE' instead
    of '-DER_DATA_LOSS' when hit 'corrupted' DTX entry.
    
    It also contains some code cleanup for 'enum obj_rpc_flags'.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/vos/evt_iter.c
+++ src/vos/evt_iter.c
@@ -209,8 +209,8 @@ evt_iter_intent(struct evt_iterator *iter)
 {
 	if (iter->it_options & EVT_ITER_FOR_PURGE)
 		return DAOS_INTENT_PURGE;
-	if (iter->it_options & EVT_ITER_FOR_REBUILD)
-		return DAOS_INTENT_REBUILD;
+	if (iter->it_options & EVT_ITER_FOR_MIGRATION)
+		return DAOS_INTENT_MIGRATION;
 	return DAOS_INTENT_DEFAULT;
 }
 
