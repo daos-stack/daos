2ab47dac09bf932ce920a3da5744a7d443f6e118
Alexander A Oganezov <alexander.a.oganezov@intel.com>
2020-12-09 20:02:52 +0000

    CART-928 fix: Add newly discovered rank
    Once a URI for a server is discovered, we need to check whether
    the server rank is in membership list. If not then it means we've
    discovered a new rank and need to add it to groups membership list.
    
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/cart/crt_rpc.c
+++ src/cart/crt_rpc.c
@@ -593,6 +593,9 @@ uri_lookup_cb(const struct crt_cb_info *cb_info)
 	struct crt_uri_lookup_in	*ul_in;
 	struct crt_grp_priv		*grp_priv;
 	crt_rpc_t			*lookup_rpc;
+	d_rank_list_t			*membs;
+	bool				found;
+	int				i;
 	int				rc = 0;
 
 	chained_rpc_priv = cb_info->cci_arg;
@@ -661,6 +664,34 @@ uri_lookup_cb(const struct crt_cb_info *cb_info)
 		D_GOTO(out, rc);
 	}
 
+	/* After a URI lookup, check if membership list has this rank.
+	 * If not - we discovered a new rank and need to populate it in membs
+	 * list of the group.
+	 */
+	D_RWLOCK_WRLOCK(&default_grp_priv->gp_rwlock);
+	membs = grp_priv_get_membs(grp_priv);
+	found = false;
+
+	if (membs) {
+		for (i = 0; i < membs->rl_nr; i++) {
+			if (membs->rl_ranks[i] == ul_in->ul_rank) {
+				found = true;
+				break;
+			}
+		}
+	}
+
+	if (!found) {
+		rc = grp_add_to_membs_list(grp_priv, ul_in->ul_rank);
+		if (rc != 0) {
+			D_ERROR("Failed to add %d to group\n", ul_in->ul_rank);
+			D_RWLOCK_UNLOCK(&default_grp_priv->gp_rwlock);
+			D_GOTO(out, rc);
+		}
+	}
+	D_RWLOCK_UNLOCK(&default_grp_priv->gp_rwlock);
+
+	/* issue the original RPC */
 	rc = crt_req_send_internal(chained_rpc_priv);
 
 retry:
