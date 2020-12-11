1b9c006d26f167431120f89e3a6ea5f1c28b38c1
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-12-11 14:34:04 -0500

    DAOS-6061 test: fix the regex under dmg_utils.py
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/util/dmg_utils.py
+++ src/tests/ftest/util/dmg_utils.py
@@ -487,7 +487,7 @@ class DmgCommand(DmgCommandBase):
             r"\s+Free:\s+([0-9.]+\s+[A-Z]+),\smin:([0-9.]+\s+[A-Z]+),"
             r"\s+max:([0-9.]+\s+[A-Z]+),\s+mean:([0-9.]+\s+[A-Z]+))"
             r"|Rebuild\s+(\w+),\s+(?:rc=)?(\d+)(?:\s+\w+)?,"
-            r"\s+(?:status=-)?(\d+)(?:\s+\w+)?",
+            r"\s+(?:status=-)?(\d+)(?:\s+\w+)?)",
             self.result.stdout)
         if match:
             # Mapping of the pool data entries to the match[0] indices
