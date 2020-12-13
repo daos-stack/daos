8baf8fd5516fbabe6ee540f212a9045e33e45198
Padmanabhan <ravindran.padmanabhan@intel.com>
2020-12-13 11:18:43 -0500

    DAOS-6061 test: Revert vos_dtx.c changes
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/vos/vos_dtx.c
+++ src/vos/vos_dtx.c
@@ -917,7 +917,7 @@ vos_dtx_commit_one(struct vos_container *cont, struct dtx_id *dti,
 	*dae_p = dae;
 
 out:
-	D_CDEBUG(rc != 0 && rc != -DER_NONEXIST, DLOG_ERR, DB_TRACE,
+	D_CDEBUG(rc != 0 && rc != -DER_NONEXIST, DLOG_ERR, DB_IO,
 		 "Commit the DTX "DF_DTI": rc = "DF_RC"\n",
 		 DP_DTI(dti), DP_RC(rc));
 	if (rc != 0) {
