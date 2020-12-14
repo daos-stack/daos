8c754728adf7c05a3e59b911c454e668026cd20f
GitHuaKuang <hua.kuang@intel.com>
2020-12-14 10:37:43 -0500

    DAOS-6110 SDL: DAOS v1.2 Coverity defect fix (#3841)
    fixes for issues found by Coverity scan
    
    Signed-off-by: Hua Kuang <hua.kuang@intel.com>
    Co-authored-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/container/oid_iv.c
+++ src/container/oid_iv.c
@@ -255,8 +255,10 @@ oid_iv_ent_init(struct ds_iv_key *iv_key, void *data, struct ds_iv_entry *entry)
 
 	/* create the entry mutex */
 	rc = ABT_mutex_create(&oid_entry->lock);
-	if (rc != ABT_SUCCESS)
+	if (rc != ABT_SUCCESS) {
+		D_FREE(oid_entry);
 		return dss_abterr2der(rc);
+	}
 
 	/** init the entry key */
 	entry->iv_key.class_id = iv_key->class_id;
