956de07cce1e3571d3950cd218429bc76181b18c
David Maldonado Moreno <david.maldonado.moreno@intel.com>
2020-12-17 17:48:00 +0000

    Based on code review made in PR #3984 I removed the DF_RC from the error message of dc_deprecated function.
    Signed-off-by: David Maldonado Moreno <david.maldonado.moreno@intel.com>

--- src/mgmt/cli_mgmt.c
+++ src/mgmt/cli_mgmt.c
@@ -118,7 +118,7 @@ out_task:
 int
 dc_deprecated(tse_task_t *task)
 {
-	D_ERROR("This API is deprecated "DF_RC"\n", DP_RC(-DER_NOSYS));
+	D_ERROR("This API is deprecated\n");
 	tse_task_complete(task, -DER_NOSYS);
 	return -DER_NOSYS;
 }
