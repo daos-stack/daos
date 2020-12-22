29e7ee8c92ad8a2866c024ec265ef6313cb2156c
Ashley Pittman <ashley.m.pittman@intel.com>
2020-12-22 10:13:01 +0000

    Use rsync filters to do the copy.
    Copy both dnt* and daos* in all cases, as I suspect at some point
    we'll want both.
    
    Ensure everything is quoted properly.
    
    Skip-func-hw-test: true
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/unit/test_post_always.sh
+++ ci/unit/test_post_always.sh
@@ -25,32 +25,30 @@ ssh "$SSH_KEY_ARGS" jenkins@"$NODE" \
   "DAOS_BASE=$DAOS_BASE             \
    $(cat "$mydir/test_post_always_node.sh")"
 
-dnt_logs=false
 case $STAGE_NAME in
     *Bullseye*)
-	test_log_dir="covc_test_logs/"
+	test_log_dir="covc_test_logs"
 	;;
     *memcheck*)
-	test_log_dir="unit_test_memcheck_logs/"
+	test_log_dir="unit_test_memcheck_logs"
 	;;
     *Unit*)
-	test_log_dir="unit_test_logs/"
+	test_log_dir="unit_test_logs"
 	;;
     *NLT*)
-	test_log_dir="nlt_logs/"
-	dnt_logs=true
+	test_log_dir="nlt_logs"
 	;;
 esac
 
 rm -rf $test_log_dir
 mkdir $test_log_dir
 
-if $dnt_logs
-then
-    rsync -av -z -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":/tmp/dnt*.log $test_log_dir
-else
-    rsync -av -z -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":/tmp/daos*.log $test_log_dir
-fi
+# Copy any log files.  Use rsync filters here to allow us to specify
+# all files we want to copy, as it's much more flexible than using
+# standard wildcards.
+rsync -v -rpt -z -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":/tmp/ \
+      --filter="include dnt*.log" --filter="include daos*.log" \
+      --filter="exclude *" "$test_log_dir/"
 
 # Note that we are taking advantage of the NFS mount here and if that
 # should ever go away, we need to pull run_test.sh/ from $NODE
