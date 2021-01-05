d830bac2c6226412f97842d620769d27fb409e79
Alexander A Oganezov <alexander.a.oganezov@intel.com>
2021-01-05 21:02:38 +0000

    CART-89 test: Fix issue with test
    - If multiple contexts are specified, test was not waiting for
    individual check_in() operations to complete before starting next
    one. This resulted in RPC handles being incorrectly re-used.
    
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/tests/ftest/cart/test_group_np_cli.c
+++ src/tests/ftest/cart/test_group_np_cli.c
@@ -75,19 +75,15 @@ test_run(void)
 	test_g.t_fault_attr_5000 = d_fault_attr_lookup(5000);
 
 	if (!test_g.t_shut_only) {
-
 		for (i = 0; i < rank_list->rl_nr; i++) {
 			rank = rank_list->rl_ranks[i];
 
 			for (tag = 0; tag < test_g.t_srv_ctx_num; tag++) {
 				DBG_PRINT("Sending rpc to %d:%d\n", rank, tag);
 				check_in(grp, rank, tag);
+				tc_sem_timedwait(&test_g.t_token_to_proceed, 61,
+						 __LINE__);
 			}
-		}
-
-		for (i = 0; i < rank_list->rl_nr; i++) {
-			tc_sem_timedwait(&test_g.t_token_to_proceed, 61,
-					 __LINE__);
 		}
 	}
 
