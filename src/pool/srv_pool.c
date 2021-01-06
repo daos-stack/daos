b9b59f9414f5906115ab2110dab8cc753aeeecc6
David Quigley <david.quigley@intel.com>
2021-01-05 19:23:19 -0700

    Fixed comment and checkpatch issue
    Signed-off-by: David Quigley <david.quigley@intel.com>

--- src/pool/srv_pool.c
+++ src/pool/srv_pool.c
@@ -4690,9 +4690,11 @@ out:
  *
  * \param[in]	pool_uuid	UUID of the pool
  * \param[in]	ranks		Pool service replicas
+ * \param[in]	handles		List of handles to selectively evict
+ * \param[in]	n_handles	Numeber of items in handles
  * \param[in]	destroy		If true the evict request is a destroy request
  * \param[in]	force		If true and destroy is true request all handles
- * 				be forcibly evicted
+ *				be forcibly evicted
  *
  * \return	0		Success
  *		-DER_BUSY	Open pool handles exist and no force requested
