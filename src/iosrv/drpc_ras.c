c36315a3ca5077393d2581c10f44f1f115f2a92c
Tom Nabarro <tom.nabarro@intel.com>
2021-01-13 17:58:42 +0000

    address code review comments, consolidate init/log/send
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/iosrv/drpc_ras.c
+++ src/iosrv/drpc_ras.c
@@ -35,53 +35,41 @@
 #include "srv_internal.h"
 
 static void
-free_ras(Shared__RASEvent *evt)
+free_event(Shared__RASEvent *evt)
 {
-	if (evt->obj_id)
+	if (evt->obj_id != NULL)
 		D_FREE(evt->obj_id);
-	if (evt->pool_uuid)
+	if (evt->pool_uuid != NULL)
 		D_FREE(evt->pool_uuid);
-	if (evt->cont_uuid)
+	if (evt->cont_uuid != NULL)
 		D_FREE(evt->cont_uuid);
-	if (evt->hostname)
+	if (evt->hostname != NULL)
 		D_FREE(evt->hostname);
-	if (evt->timestamp)
+	if (evt->timestamp != NULL)
 		D_FREE(evt->timestamp);
 }
 
 static int
-init_ras(ras_event_t id, char *msg, ras_type_t type, ras_sev_t sev, char *hid,
-	 d_rank_t *rank, char *jid, uuid_t *puuid, uuid_t *cuuid,
-	 daos_obj_id_t *oid, char *cop, Shared__RASEvent *evt)
+init_event(ras_event_t id, char *msg, ras_type_t type, ras_sev_t sev, char *hid,
+	   d_rank_t *rank, char *jid, uuid_t *puuid, uuid_t *cuuid,
+	   daos_obj_id_t *oid, char *cop, Shared__RASEvent *evt)
 {
 	struct timeval	 tv;
 	struct tm	*tm;
 	int		 rc;
 
-	evt->timestamp = NULL;
-	evt->hostname = NULL;
-	evt->msg = NULL;
-	/* max value indicates nil rank */
-	evt->rank = UINT32_MAX;
-	evt->hw_id = NULL;
-	evt->proc_id = NULL;
-	evt->thread_id = NULL;
-	evt->job_id = NULL;
-	evt->pool_uuid = NULL;
-	evt->cont_uuid = NULL;
-	evt->obj_id = NULL;
-	evt->ctl_op = NULL;
-
 	/* Populate mandatory RAS fields. */
 
 	(void)gettimeofday(&tv, 0);
 	tm = localtime(&tv.tv_sec);
-	if (tm)
+	if (tm != NULL)
 		D_ASPRINTF(evt->timestamp,
 			   "%04d/%02d/%02d-%02d:%02d:%02d.%02ld",
 			   tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
 			   tm->tm_hour, tm->tm_min, tm->tm_sec,
 			   (long)tv.tv_usec / 10000);
+	else
+		evt->timestamp = NULL;
 
 	evt->id = (uint32_t)id;
 	evt->type = (uint32_t)type;
@@ -89,93 +77,106 @@ init_ras(ras_event_t id, char *msg, ras_type_t type, ras_sev_t sev, char *hid,
 
 	if (strnlen(dss_hostname, DSS_HOSTNAME_MAX_LEN) == 0) {
 		D_ERROR("missing hostname parameter\n");
-		D_GOTO(out_fail, rc = -DER_UNINIT);
+		D_GOTO(out_ts, rc = -DER_UNINIT);
 	}
 	D_STRNDUP(evt->hostname, dss_hostname, DSS_HOSTNAME_MAX_LEN);
 
-	if (!msg || strnlen(msg, DAOS_RAS_STR_FIELD_SIZE) == 0) {
+	if ((msg == NULL) || strnlen(msg, DAOS_RAS_STR_FIELD_SIZE) == 0) {
 		D_ERROR("missing msg parameter\n");
-		D_GOTO(out_fail, rc = -DER_INVAL);
+		D_GOTO(out_host, rc = -DER_INVAL);
 	}
 	evt->msg = msg;
 
 	/* Populate optional RAS fields. */
 
-	if (hid)
-		evt->hw_id = hid;
-	if (rank)
-		evt->rank = (uint32_t)*rank;
-	if (jid)
-		evt->job_id = jid;
-	if (puuid && !uuid_is_null(*puuid))
+	evt->hw_id = (hid != NULL) ? hid : NULL;
+	/* UINT32_MAX/CRT_NO_RANK indicates nil rank in daos_{,io_}server */
+	evt->rank = (rank != NULL) ? (uint32_t)*rank : CRT_NO_RANK;
+	evt->job_id = (jid != NULL) ? jid : NULL;
+	evt->ctl_op = (cop != NULL) ? cop : NULL;
+
+	if ((puuid != NULL) && !uuid_is_null(*puuid))
 		D_ASPRINTF(evt->pool_uuid, DF_UUIDF, DP_UUID(*puuid));
-	if (cuuid && !uuid_is_null(*cuuid))
+	else
+		evt->pool_uuid = NULL;
+
+	if ((cuuid != NULL) && !uuid_is_null(*cuuid))
 		D_ASPRINTF(evt->cont_uuid, DF_UUIDF, DP_UUID(*cuuid));
-	if (oid)
+	else
+		evt->cont_uuid = NULL;
+
+	if (oid != NULL)
 		D_ASPRINTF(evt->obj_id, DF_OID, DP_OID(*oid));
-	if (cop)
-		evt->ctl_op = cop;
+	else
+		evt->obj_id = NULL;
 
 	return 0;
-out_fail:
-	free_ras(evt);
+
+out_host:
+	if (evt->hostname != NULL)
+		D_FREE(evt->hostname);
+out_ts:
+	if (evt->timestamp != NULL)
+		D_FREE(evt->timestamp);
 
 	return rc;
 }
 
 static void
-log_ras(Shared__RASEvent *evt)
+log_event(Shared__RASEvent *evt)
 {
-	FILE		*stream;
-	char		*buf = NULL;
-	size_t		 len;
+	FILE					*stream;
+	char					*buf = NULL;
+	size_t					 len;
+	Shared__RASEvent__ExtendedInfoCase	 eic = evt->extended_info_case;
 
 	stream = open_memstream(&buf, &len);
-	if (!stream) {
+	if (stream == NULL) {
 		D_ERROR("open_memstream failed: "DF_RC"\n", DP_RC(-DER_NOMEM));
 		return;
 	}
 
 	/* Log mandatory RAS fields. */
-
 	D_FPRINTF(stream, " id: [%s]", ras_event2str(evt->id));
-	if (evt->timestamp)
+	if (evt->timestamp != NULL)
 		D_FPRINTF(stream, " ts: [%s]", evt->timestamp);
-	if (evt->hostname)
+	if (evt->hostname != NULL)
 		D_FPRINTF(stream, " host: [%s]", evt->hostname);
 	D_FPRINTF(stream, " type: [%s] sev: [%s]", ras_type2str(evt->type),
 		  ras_sev2str(evt->severity));
-	if (evt->msg)
+	if (evt->msg != NULL)
 		D_FPRINTF(stream, " msg: [%s]", evt->msg);
 
 	/* Log optional RAS fields. */
-
-	if (evt->hw_id)
+	if (evt->hw_id != NULL)
 		D_FPRINTF(stream, " hwid: [%s]", evt->hw_id);
-	if (evt->rank != UINT32_MAX)
+	if (evt->rank != CRT_NO_RANK)
 		D_FPRINTF(stream, " rank: [%u]", evt->rank);
-	if (evt->job_id)
+	if (evt->job_id != NULL)
 		D_FPRINTF(stream, " jobid: [%s]", evt->job_id);
-	if (evt->pool_uuid)
+	if (evt->pool_uuid != NULL)
 		D_FPRINTF(stream, " puuid: [%s]", evt->pool_uuid);
-	if (evt->cont_uuid)
+	if (evt->cont_uuid != NULL)
 		D_FPRINTF(stream, " cuuid: [%s]", evt->cont_uuid);
-	if (evt->obj_id)
+	if (evt->obj_id != NULL)
 		D_FPRINTF(stream, " oid: [%s]", evt->obj_id);
-	if (evt->ctl_op)
+	if (evt->ctl_op != NULL)
 		D_FPRINTF(stream, " control_op: [%s]", evt->ctl_op);
-	if (evt->extended_info_case == \
-			SHARED__RASEVENT__EXTENDED_INFO_STR_INFO && \
-			evt->str_info)
+
+	/* Log data blob if event info is non-specific */
+	if (eic != SHARED__RASEVENT__EXTENDED_INFO_STR_INFO)
+		goto out;
+	if (evt->str_info != NULL)
 		D_FPRINTF(stream, " data: [%s]", evt->str_info);
 
+out:
 	fclose(stream);
 	D_DEBUG(DB_MGMT, "&&& RAS EVENT%s", buf);
 	D_FREE(buf);
 }
 
 static int
-send_ras(Shared__RASEvent *evt)
+send_event(Shared__RASEvent *evt)
 {
 	Shared__ClusterEventReq	 req = SHARED__CLUSTER_EVENT_REQ__INIT;
 	uint8_t			*reqb;
@@ -189,7 +190,7 @@ send_ras(Shared__RASEvent *evt)
 		return -DER_UNINIT;
 	}
 
-	if (!evt) {
+	if (evt == NULL) {
 		D_ERROR("null RAS event\n");
 		return -DER_INVAL;
 	}
@@ -221,11 +222,39 @@ send_ras(Shared__RASEvent *evt)
 	}
 
 	drpc_response_free(dresp);
+
 out_dreq:
 	/* This also frees reqb via dreq->body.data. */
 	drpc_call_free(dreq);
 out:
-	free_ras(evt);
+	free_event(evt);
+
+	return rc;
+}
+
+static int
+raise_ras(ras_event_t id, char *msg, ras_type_t type, ras_sev_t sev, char *hid,
+          d_rank_t *rank, char *jid, uuid_t *puuid, uuid_t *cuuid,
+          daos_obj_id_t *oid, char *cop, Shared__RASEvent *evt)
+{
+	int	rc;
+
+	rc = init_event(id, msg, type, sev, hid, rank, jid, puuid, cuuid, oid,
+			cop, evt);
+	if (rc != 0) {
+		D_ERROR("failed to init RAS event %s: "DF_RC"\n",
+			ras_event2str(RAS_POOL_REPS_UPDATE), DP_RC(rc));
+		return rc;
+	}
+
+	log_event(evt);
+	rc = send_event(evt);
+	if (rc != 0)
+		D_ERROR("failed to send RAS event %s over dRPC: "DF_RC"\n",
+			ras_event2str(RAS_POOL_REPS_UPDATE), DP_RC(rc));
+
+	free_event(evt);
+
 	return rc;
 }
 
@@ -236,32 +265,17 @@ ds_notify_ras_event(ras_event_t id, char *msg, ras_type_t type, ras_sev_t sev,
 {
 	Shared__RASEvent	evt = SHARED__RASEVENT__INIT;
 	d_rank_t		this_rank = dss_self_rank();
-	int			rc;
 
 	/* use opaque blob oneof case for extended info for passthrough event */
 	evt.extended_info_case = SHARED__RASEVENT__EXTENDED_INFO_STR_INFO;
 	evt.str_info = data;
-	D_DEBUG(DB_MGMT, "&&& RAS EVENT OPT DATA: %s", evt.str_info);
 
 	/* populate rank param if empty */
-	if (!rank)
+	if (rank == NULL)
 		rank = &this_rank;
 
-	rc = init_ras(id, msg, type, sev, hid, rank, jid, puuid, cuuid, oid,
-		      cop, &evt);
-	if (rc != 0) {
-		D_ERROR("failed to init RAS event %s: "DF_RC"\n",
-			ras_event2str(id), DP_RC(rc));
-		return;
-	}
-
-	log_ras(&evt);
-	rc = send_ras(&evt);
-	if (rc != 0)
-		D_ERROR("failed to send RAS event %s over dRPC: "DF_RC"\n",
-			ras_event2str(id), DP_RC(rc));
-
-	free_ras(&evt);
+	raise_ras(id, msg, type, sev, hid, rank, jid, puuid, cuuid, oid, cop,
+		  &evt);
 }
 
 int
@@ -273,11 +287,11 @@ ds_notify_pool_svc_update(uuid_t *puuid, d_rank_list_t *svc)
 	d_rank_t				rank = dss_self_rank();
 	int					rc;
 
-	if (!puuid || uuid_is_null(*puuid)) {
+	if ((puuid == NULL) || uuid_is_null(*puuid)) {
 		D_ERROR("invalid pool\n");
 		return -DER_INVAL;
 	}
-	if (!svc || svc->rl_nr == 0) {
+	if ((svc == NULL) || svc->rl_nr == 0) {
 		D_ERROR("invalid service replicas\n");
 		return -DER_INVAL;
 	}
@@ -291,25 +305,12 @@ ds_notify_pool_svc_update(uuid_t *puuid, d_rank_list_t *svc)
 	evt.extended_info_case = SHARED__RASEVENT__EXTENDED_INFO_POOL_SVC_INFO;
 	evt.pool_svc_info = &info;
 
-	rc = init_ras(RAS_POOL_REPS_UPDATE,
-		      "List of pool service replica ranks has been updated.",
-		      RAS_TYPE_STATE_CHANGE, RAS_SEV_INFO, NULL /* hid */,
-		      &rank /* rank */, NULL /* jid */, puuid, NULL /* cuuid */,
-		      NULL /* oid */, NULL /* cop */, &evt);
-	if (rc != 0) {
-		D_ERROR("failed to init RAS event %s: "DF_RC"\n",
-			ras_event2str(RAS_POOL_REPS_UPDATE), DP_RC(rc));
-		goto out_svcreps;
-	}
+	rc = raise_ras(RAS_POOL_REPS_UPDATE,
+		       "List of pool service replica ranks has been updated.",
+		       RAS_TYPE_STATE_CHANGE, RAS_SEV_INFO, NULL /* hid */,
+		       &rank /* rank */, NULL /* jid */, puuid,
+		       NULL /* cuuid */, NULL /* oid */, NULL /* cop */, &evt);
 
-	log_ras(&evt);
-	rc = send_ras(&evt);
-	if (rc != 0)
-		D_ERROR("failed to send RAS event %s over dRPC: "DF_RC"\n",
-			ras_event2str(RAS_POOL_REPS_UPDATE), DP_RC(rc));
-
-	free_ras(&evt);
-out_svcreps:
 	D_FREE(info.svc_reps);
 
 	return rc;
