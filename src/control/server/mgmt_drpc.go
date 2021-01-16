acb65e1de8f36e3db4cecd070f9cfccb1cc4ba10
Tom Nabarro <tom.nabarro@intel.com>
2021-01-16 17:33:08 +0000

    DAOS-6088 control: raise and process ras event on pool svc ranks update (#4240)
    Raise event in io_server on pool svc replica list update when failed
    ranks are replaced. The event is sent to local control-plane instance
    over dRPC then forwarded to MS leader control-plane instance over gRPC
    to be handled in the MS database. The relevant pool service record
    is then modified in the system database to reflect the updated list of
    service replica ranks. Event propagated through new RAS RPCs.
    
    PR consists of the following changes:
      * Add handler to publish cluster events received over dRPC.
      * Accommodate PR-4168 iosrv RAS requirements.
      * Raise PoolSvcReplicasUpdate event on replacement of failed replicas.
      * Update pool svc replica ranks on receipt of PoolSvcRanksUpdate event.
      * Extend dRPC client unit tests, fix leaks and add to run_test.sh.
      * Implement RAS event wrapper sent using SRV dRPC module.
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/mgmt_drpc.go
+++ src/control/server/mgmt_drpc.go
@@ -1,5 +1,5 @@
 //
-// (C) Copyright 2018-2020 Intel Corporation.
+// (C) Copyright 2018-2021 Intel Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -28,23 +28,30 @@ import (
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 
+	sharedpb "github.com/daos-stack/daos/src/control/common/proto/shared"
 	srvpb "github.com/daos-stack/daos/src/control/common/proto/srv"
 	"github.com/daos-stack/daos/src/control/drpc"
+	"github.com/daos-stack/daos/src/control/events"
 	"github.com/daos-stack/daos/src/control/logging"
 	"github.com/daos-stack/daos/src/control/system"
 )
 
 // mgmtModule represents the daos_server mgmt dRPC module. It sends dRPCs to
-// the daos_io_server iosrv module (src/iosrv).
+// the daos_io_server iosrv module (src/iosrv) but doesn't receive.
 type mgmtModule struct{}
 
+// newMgmtModule creates a new management module and returns its reference.
+func newMgmtModule() *mgmtModule {
+	return &mgmtModule{}
+}
+
 // HandleCall is the handler for calls to the mgmtModule
-func (m *mgmtModule) HandleCall(session *drpc.Session, method drpc.Method, req []byte) ([]byte, error) {
+func (mod *mgmtModule) HandleCall(session *drpc.Session, method drpc.Method, req []byte) ([]byte, error) {
 	return nil, drpc.UnknownMethodFailure()
 }
 
 // ID will return Mgmt module ID
-func (m *mgmtModule) ID() drpc.ModuleID {
+func (mod *mgmtModule) ID() drpc.ModuleID {
 	return drpc.ModuleMgmt
 }
 
@@ -54,6 +61,18 @@ type srvModule struct {
 	log    logging.Logger
 	sysdb  *system.Database
 	iosrvs []*IOServerInstance
+	events *events.PubSub
+}
+
+// newSrvModule creates a new srv module references to the system database,
+// resident IOServerInstances and event publish subscribe reference.
+func newSrvModule(log logging.Logger, sysdb *system.Database, iosrvs []*IOServerInstance, events *events.PubSub) *srvModule {
+	return &srvModule{
+		log:    log,
+		sysdb:  sysdb,
+		iosrvs: iosrvs,
+		events: events,
+	}
 }
 
 // HandleCall is the handler for calls to the srvModule.
@@ -65,11 +84,14 @@ func (mod *srvModule) HandleCall(session *drpc.Session, method drpc.Method, req 
 		return nil, mod.handleBioErr(req)
 	case drpc.MethodGetPoolServiceRanks:
 		return mod.handleGetPoolServiceRanks(req)
+	case drpc.MethodClusterEvent:
+		return mod.handleClusterEvent(req)
 	default:
 		return nil, drpc.UnknownMethodFailure()
 	}
 }
 
+// ID will return SRV module ID
 func (mod *srvModule) ID() drpc.ModuleID {
 	return drpc.ModuleSrv
 }
@@ -142,4 +164,18 @@ func (mod *srvModule) handleBioErr(reqb []byte) error {
 	mod.iosrvs[req.InstanceIdx].BioErrorNotify(req)
 
 	return nil
+}
+
+func (mod *srvModule) handleClusterEvent(reqb []byte) ([]byte, error) {
+	req := new(sharedpb.ClusterEventReq)
+	if err := proto.Unmarshal(reqb, req); err != nil {
+		return nil, drpc.UnmarshalingPayloadFailure()
+	}
+
+	resp, err := mod.events.HandleClusterEvent(req)
+	if err != nil {
+		return nil, errors.Wrapf(err, "handle cluster event %+v", req)
+	}
+
+	return proto.Marshal(resp)
 }
