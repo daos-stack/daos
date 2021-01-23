b7ca276720c7ea874c49e5d4d9f9eede947319c7
Ashley Pittman <ashley.m.pittman@intel.com>
2021-01-23 18:42:11 +0000

    Add bz2 files.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/unit/test_nlt_post.sh
+++ ci/unit/test_nlt_post.sh
@@ -13,8 +13,9 @@ mkdir nlt_logs
 # Copy any log files.  Use rsync filters here to allow us to specify
 # all files we want to copy, as it's much more flexible than using
 # standard wildcards.
-rsync -v -dpt -z -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":/tmp/ \
-      --filter="include dnt*.log" --filter="exclude *" nlt_logs/
+rsync -v -dpt -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":/tmp/ \
+      --filter="include dnt*.log" --filter="include dnt*.log.bz2" \
+      --filter="exclude *" nlt_logs/
 
 rsync -v -dpt -z -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":build/ \
       --filter="include nlt-errors.json" --filter="include dnt*.xml" \
