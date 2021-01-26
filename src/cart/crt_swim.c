1167ee045d6277c6cb967a1bfe58957ea0c30618
SABollinger <71015252+SABollinger@users.noreply.github.com>
2021-01-26 11:49:00 -0500

    CART-933 debug: Add D_EMIT capability. (#4227)
    - Add D_EMIT macro that will always print to logs
    - Some of swim messages are modified from being D_ERRORs to use D_EMIT
    - Minor cleanups
    
    Signed-off-by: Steven Bollinger <stevenx.bollinger@intel.com>

--- src/cart/crt_swim.c
+++ src/cart/crt_swim.c
@@ -90,11 +90,11 @@ crt_swim_fault_init(const char *args)
 			s++; /* skip space */
 		if (!strncasecmp(s, "delay=", 6)) {
 			crt_swim_fail_delay = strtoul(s + 6, &end, 0);
-			D_ERROR("*** CRT_SWIM_FAIL_DELAY=%lu\n",
-				crt_swim_fail_delay);
+			D_EMIT("CRT_SWIM_FAIL_DELAY=%lu\n",
+			       crt_swim_fail_delay);
 		} else if (!strncasecmp(s, "rank=", 5)) {
 			crt_swim_fail_id = strtoul(s + 5, &end, 0);
-			D_ERROR("*** CRT_SWIM_FAIL_ID=%lu\n", crt_swim_fail_id);
+			D_EMIT("CRT_SWIM_FAIL_ID=%lu\n", crt_swim_fail_id);
 		}
 		s = NULL;
 	}
@@ -157,9 +157,9 @@ static void crt_swim_srv_cb(crt_rpc_t *rpc_req)
 	D_ASSERT(crt_is_service());
 
 	D_TRACE_DEBUG(DB_TRACE, rpc_req,
-		"incoming opc %#x with %zu updates %lu <= %lu\n",
-		rpc_req->cr_opc, rpc_swim_input->upds.ca_count,
-		self_id, from_id);
+		      "incoming opc %#x with %zu updates %lu <= %lu\n",
+		      rpc_req->cr_opc, rpc_swim_input->upds.ca_count,
+		      self_id, from_id);
 
 	rpc_priv = container_of(rpc_req, struct crt_rpc_priv, crp_pub);
 
@@ -217,7 +217,7 @@ static void crt_swim_srv_cb(crt_rpc_t *rpc_req)
 
 	if (CRT_SWIM_SHOULD_FAIL(d_fa_swim_drop_rpc, self_id)) {
 		rc = d_fa_swim_drop_rpc->fa_err_code;
-		D_ERROR("*** DROP incoming opc %#x with %zu updates "
+		D_EMIT("DROP incoming opc %#x with %zu updates "
 			"%lu <= %lu error: "DF_RC"\n", rpc_req->cr_opc,
 			rpc_swim_input->upds.ca_count, self_id, from_id,
 			DP_RC(rc));
@@ -268,7 +268,8 @@ static void crt_swim_cli_cb(const struct crt_cb_info *cb_info)
 	int			 rc = 0;
 
 	D_TRACE_DEBUG(DB_TRACE, rpc_req,
-		      "complete opc %#x with %zu updates %lu => %lu "DF_RC"\n",
+		      "complete opc %#x with %zu updates %lu => %lu "
+		      DF_RC"\n",
 		      rpc_req->cr_opc, rpc_swim_input->upds.ca_count,
 		      rpc_swim_input->src, id, DP_RC(cb_info->cci_rc));
 
@@ -328,7 +329,7 @@ static int crt_swim_send_message(struct swim_context *ctx, swim_id_t to,
 
 	if (CRT_SWIM_SHOULD_FAIL(d_fa_swim_drop_rpc, self_id)) {
 		rc = d_fa_swim_drop_rpc->fa_err_code;
-		D_ERROR("*** DROP outgoing opc %#x with %zu updates "
+		D_EMIT("DROP outgoing opc %#x with %zu updates "
 			"%lu => %lu error: "DF_RC"\n", opc, nupds,
 			self_id, to, DP_RC(rc));
 		if (!rc)
@@ -356,8 +357,8 @@ static int crt_swim_send_message(struct swim_context *ctx, swim_id_t to,
 		rc = crt_req_set_timeout(rpc_req, crt_swim_rpc_timeout);
 		if (rc) {
 			D_TRACE_ERROR(rpc_req,
-				      "crt_req_set_timeout() failed "DF_RC"\n",
-				      DP_RC(rc));
+				      "crt_req_set_timeout() failed "
+				      DF_RC"\n", DP_RC(rc));
 			crt_req_decref(rpc_req);
 			D_GOTO(out, rc);
 		}
@@ -369,8 +370,8 @@ static int crt_swim_send_message(struct swim_context *ctx, swim_id_t to,
 	rpc_swim_input->upds.ca_count  = nupds;
 
 	D_TRACE_DEBUG(DB_TRACE, rpc_req,
-		"sending opc %#x with %zu updates %lu => %lu\n",
-		opc, nupds, self_id, to);
+		      "sending opc %#x with %zu updates %lu => %lu\n",
+		      opc, nupds, self_id, to);
 
 	rc = crt_req_send(rpc_req, crt_swim_cli_cb, ctx);
 	if (rc) {
@@ -550,7 +551,7 @@ static void crt_swim_progress_cb(crt_context_t crt_ctx, void *arg)
 	if (crt_swim_fail_hlc && crt_hlc_get() >= crt_swim_fail_hlc) {
 		crt_swim_should_fail = true;
 		crt_swim_fail_hlc = 0;
-		D_ERROR("*** SWIM id=%lu should fail\n", crt_swim_fail_id);
+		D_EMIT("SWIM id=%lu should fail\n", crt_swim_fail_id);
 	}
 
 	rc = swim_progress(ctx, CRT_SWIM_PROGRESS_TIMEOUT);
@@ -625,10 +626,10 @@ int crt_swim_init(int crt_ctx_idx)
 
 		for (i = 0; i < grp_priv->gp_size; i++) {
 			rc = crt_swim_rank_add(grp_priv,
-					grp_membs->rl_ranks[i]);
+					       grp_membs->rl_ranks[i]);
 			if (rc && rc != -DER_ALREADY) {
-				D_ERROR("crt_swim_rank_add() failed "DF_RC"\n",
-					DP_RC(rc));
+				D_ERROR("crt_swim_rank_add() failed "
+					DF_RC"\n", DP_RC(rc));
 				D_GOTO(cleanup, rc);
 			}
 		}
@@ -660,8 +661,8 @@ int crt_swim_init(int crt_ctx_idx)
 	/* Search the attr in inject yml first */
 	d_fa_swim_drop_rpc = d_fault_attr_lookup(CRT_SWIM_FAIL_DROP_RPC);
 	if (d_fa_swim_drop_rpc != NULL) {
-		D_ERROR("*** fa_swim_drop_rpc: id=%u/0x%x, "
-			"interval=%u, max="DF_U64", x=%u, y=%u, args='%s'\n",
+		D_EMIT("fa_swim_drop_rpc: id=%u/0x%x, "
+			"interval=%u, max=" DF_U64 ", x=%u, y=%u, args='%s'\n",
 			d_fa_swim_drop_rpc->fa_id,
 			d_fa_swim_drop_rpc->fa_id,
 			d_fa_swim_drop_rpc->fa_interval,
@@ -725,15 +726,15 @@ int crt_swim_enable(struct crt_grp_priv *grp_priv, int crt_ctx_idx)
 		if (rc == -DER_NONEXIST)
 			rc = 0;
 		if (rc)
-			D_ERROR("crt_unregister_progress_cb() failed "DF_RC"\n",
-				DP_RC(rc));
+			D_ERROR("crt_unregister_progress_cb() failed "
+				DF_RC"\n", DP_RC(rc));
 	}
 	if (old_ctx_idx != crt_ctx_idx) {
 		rc = crt_register_progress_cb(crt_swim_progress_cb,
 					      crt_ctx_idx, NULL);
 		if (rc)
-			D_ERROR("crt_register_progress_cb() failed "DF_RC"\n",
-				DP_RC(rc));
+			D_ERROR("crt_register_progress_cb() failed "
+				DF_RC"\n", DP_RC(rc));
 	}
 
 out:
@@ -769,8 +770,8 @@ int crt_swim_disable(struct crt_grp_priv *grp_priv, int crt_ctx_idx)
 		if (rc == -DER_NONEXIST)
 			rc = 0;
 		if (rc)
-			D_ERROR("crt_unregister_progress_cb() failed "DF_RC"\n",
-				DP_RC(rc));
+			D_ERROR("crt_unregister_progress_cb() failed "
+				DF_RC"\n", DP_RC(rc));
 	}
 
 out:
@@ -854,7 +855,7 @@ int crt_swim_rank_add(struct crt_grp_priv *grp_priv, d_rank_t rank)
 		cst->cst_state.sms_incarnation = 0;
 		cst->cst_state.sms_status = SWIM_MEMBER_INACTIVE;
 		D_CIRCLEQ_INSERT_AFTER(&csm->csm_head, csm->csm_target, cst,
-					cst_link);
+				       cst_link);
 		rank_in_list = true;
 
 		for (n = 1 + rand() % (grp_priv->gp_size + 1); n > 0; n--)
