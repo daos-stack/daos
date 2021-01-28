f2741e87a5bb5aa5c3ddb8db162652a0cecf92ec
Phillip Henderson <phillip.henderson@intel.com>
2021-01-28 09:16:02 -0500

    Fixing daos_server_config.py test errors.
    Test-tag-vm: pr,-hw daily_regression,-hw
    Test-tag-hw-small: pr,hw,small daily_regression,hw,small
    Test-tag-hw-medium: pr,hw,medium,ib2 daily_regression,hw,medium,ib2
    Test-tag-hw-large: pr,hw,large daily_regression,hw,large
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/server/daos_server_config.py
+++ src/tests/ftest/server/daos_server_config.py
@@ -50,7 +50,9 @@ class DaosServerConfigTest(TestWithServers):
             "Error setting the '{}' config file parameter to '{}'".format(
                 c_val[0], c_val[1]))
 
-        self.log.info("Starting server with %s = %s", c_val[0], c_val[1])
+        self.log.info(
+            "Starting server with %s = %s, expected to %s",
+            c_val[0], c_val[1], c_val[2])
 
         try:
             self.server_managers[0].start()
@@ -59,13 +61,23 @@ class DaosServerConfigTest(TestWithServers):
             exception = err
 
         # Verify
+        fail_message = ""
         if c_val[2] == "FAIL" and exception is None:
             self.log.error("Server was expected to fail")
-            self.fail("{}".format(exception))
+            fail_message = (
+                "Server start completed successfully when it was expected to "
+                "fail with {} = {}".format(c_val[0], c_val[1]))
         elif c_val[2] == "PASS" and exception is not None:
             self.log.error("Server was expected to start")
-            self.fail("{}".format(exception))
+            fail_message = (
+                "Server start failed when it was expected to complete "
+                "successfully with {} = {}: {}".format(
+                    c_val[0], c_val[1], exception))
 
         # Updated the expected state for servers that should fail to start
         if c_val[2] == "FAIL":
             self.server_managers[-1].update_expected_states(None, "Stopped")
+
+        if fail_message:
+            self.fail(fail_message)
+        self.log.info("Test passed!")
