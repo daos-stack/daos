d88aad7810e17661a98734f6bce65f6859fbe664
Ashley Pittman <ashley.m.pittman@intel.com>
2021-01-29 16:07:50 +0000

    rm the build after it's done.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/install_packages.sh
+++ utils/install_packages.sh
@@ -15,5 +15,7 @@ apt-get -y install scons python3-distro autoconf bash clang cmake curl \
         liblz4-dev python3-distro \
 	ndctl golang-go python-distro
 
+set +e
 add-apt-repository universe
+apt-get search fuse
 apt-get -y install fuse3 libfuse3-dev
