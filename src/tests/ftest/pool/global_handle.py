e1fdfef818389f392086a5e66b6eaef396928dc4
Phil Henderson <phillip.henderson@intel.com>
2021-02-02 15:59:57 -0500

    DAOS-4867 tests: Using systemctl to start servers. (#4170)
    Replacing the use of orterun to start the daos_server command with
    systemctl.  This also enables the option to start the daos_server once
    per avocado test file which reduces the number of start/stops between
    avocado test variants.
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/pool/global_handle.py
+++ src/tests/ftest/pool/global_handle.py
@@ -4,35 +4,38 @@
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 '''
-from __future__ import print_function
 import traceback
 
 from apricot import TestWithServers
-import check_for_pool
 from pydaos.raw import DaosPool, DaosContainer, DaosApiError
-from test_utils_pool import TestPool
+
 
 class GlobalHandle(TestWithServers):
+    """Test the ability to share pool handles among processes.
 
-    """
-    This class contains tests to verify the ability to share pool
-    handles among processes.
     :avocado: recursive
     """
 
-    def tearDown(self):
-        try:
-            super(GlobalHandle, self).tearDown()
-        finally:
-            # really make sure everything is gone
-            check_for_pool.cleanup_pools(self.hostlist_servers)
-
     def check_handle(self, buf_len, iov_len, buf, uuidstr, rank):
-        """
-        This gets run in a child process and verifyes the global
-        handle can be turned into a local handle in another process.
-        """
+        """Verify that the global handle can be turned into a local handle.
 
+        This gets run in a child process and verifies the global handle can be
+        turned into a local handle in another process.
+
+        Args:
+            buf_len (object): buffer length; 1st return value from
+                DaosPool.local2global()
+            iov_len (object): iov length; 2nd return value from
+                DaosPool.local2global()
+            buf (object): buffer; 3rd return value from DaosPool.local2global()
+            uuidstr (str): pool UUID
+            rank (int): pool svc rank
+
+        Raises:
+            DaosApiError: if there was an error converting the pool handle or
+                using the local pool handle to create a container.
+
+        """
         pool = DaosPool(self.context)
         pool.set_uuid_str(uuidstr)
         pool.set_svc(rank)
@@ -48,40 +51,36 @@ class GlobalHandle(TestWithServers):
         container.create(pool.handle)
 
     def test_global_handle(self):
-        """
-        Test ID: DAO
+        """Test ID: Jira-XXXX.
 
         Test Description: Use a pool handle in another process.
 
-        :avocado: tags=all,pool,daily_regression,tiny,poolglobalhandle
+        :avocado: tags=all,daily_regression
+        :avocado: tags=tiny
+        :avocado: tags=pool,global_handle,pool_global_handle
         """
         # initialize a python pool object then create the underlying
         # daos storage
-        self.pool = TestPool(self.context, self.get_dmg_command())
-        self.pool.get_params(self)
-        self.pool.create()
+        self.add_pool()
 
-        self.pool.connect()
+        # create a container just to make sure handle is good
+        self.add_container(self.pool)
 
         try:
-            # create a container just to make sure handle is good
-            self.container = DaosContainer(self.context)
-            self.container.create(self.pool.pool.handle)
-
             # create a global handle
             iov_len, buf_len, buf = self.pool.pool.local2global()
 
             # this should work in the future but need on-line server addition
-            #arg_list = (buf_len, iov_len, buf, pool.get_uuid_str(), 0)
-            #p = Process(target=check_handle, args=arg_list)
-            #p.start()
-            #p.join()
+            # arg_list = (buf_len, iov_len, buf, pool.get_uuid_str(), 0)
+            # p = Process(target=check_handle, args=arg_list)
+            # p.start()
+            # p.join()
             # for now verifying global handle in the same process which is not
             # the intended use case
             self.check_handle(buf_len, iov_len, buf,
                               self.pool.pool.get_uuid_str(), 0)
 
-        except DaosApiError as excep:
-            print(excep)
-            print(traceback.format_exc())
+        except DaosApiError as error:
+            self.log.error(error)
+            self.log.error(traceback.format_exc())
             self.fail("Expecting to pass but test has failed.\n")
