0291188a8f837ffea0956c82d00ea8d3fe4e4c86
Liu Xuezhao <xuezhao.liu@intel.com>
2021-02-03 21:06:47 +0800

    DAOS-4884 container: cont rf and status handling (#4393)
    * DAOS-4884 container: cont rf and status handling
    
    Add DAOS_PROP_CO_STATUS container property, to represent container
    healthy status, which is determined by cont RF value and pool map.
    
    When (#concurrent_failed_devices > RF), the container will be treated
    as UNCLEAN status that will set DAOS_PROP_CO_STATUS prop as
    DAOS_PROP_CO_UNCLEAN, and then will fail the cont_open. User can
    clear that UNCLEAN status (by set_prop), or passing DAOS_COO_FORCE
    flag to open the container.
    
    container status can be queried and set by -
    daos cont get-prop --pool= --cont=
    daos cont set-prop --properties=status:healthy --pool= --cont=
    
    To support failure history query, 2 changes made to pool map -
    1. add co_out_ver to pool_component, to record the pm version when
       exclude out.
    2. add co_flags (PO_COMPF_DOWN2OUT) to pool_component, to
       differentiate DRAIN->EXCLUDE_OUT and EXCLUDE->EXCLUDE_OUT cases.
    
    Signed-off-by: Xuezhao Liu <xuezhao.liu@intel.com>

--- src/container/srv_layout.h
+++ src/container/srv_layout.h
@@ -67,6 +67,7 @@ extern d_iov_t ds_cont_prop_acl;		/* struct daos_acl */
 extern d_iov_t ds_cont_prop_owner;		/* string */
 extern d_iov_t ds_cont_prop_owner_group;	/* string */
 extern d_iov_t ds_cont_prop_snapshots;		/* snapshot KVS */
+extern d_iov_t ds_cont_prop_co_status;		/* uint64_t */
 extern d_iov_t ds_cont_attr_user;		/* user attribute KVS */
 extern d_iov_t ds_cont_prop_handles;		/* handle index KVS */
 extern d_iov_t ds_cont_prop_roots;		/* container first citizens */
