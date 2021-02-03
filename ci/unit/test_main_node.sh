915e54413209edf0278dffd3e6ebd712018a6dd2
Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>
2021-02-03 05:04:26 +0000

    DAOS-6274 test: Restructure DAOS results and logs
    archive unit test xml
    
    Skip-checkpatch: true
    Skip-build-leap15-rpm: true
    Skip-nlt: true
    Skip-unit-test-memcheck: true
    Skip-func-test-vm: true
    Skip-func-hw-test-large: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-coverity-test: true
    Skip-scan-centos-rpms: true
    Skip-test-centos-rpms: true
    
    Signed-off-by: Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>

--- ci/unit/test_main_node.sh
+++ ci/unit/test_main_node.sh
@@ -47,5 +47,4 @@ sudo mount -t tmpfs -o size=16G tmpfs /mnt/daos
 IS_CI=true OLD_CI=false RUN_TEST_VALGRIND="$WITH_VALGRIND" \
     DAOS_BASE="$DAOS_BASE" utils/run_test.sh
 
-mkdir -p unit_test_logs/xml
-cp test_results/*xml unit_test_logs/xml
+cp test_results/*.xml unit_test_logs/
