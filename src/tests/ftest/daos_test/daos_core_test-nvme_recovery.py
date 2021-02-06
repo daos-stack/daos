ae7cbf201297630587e0cd956a113af573c3f798
Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>
2021-02-06 07:05:07 +0000

    Revert "DAOS-6578 test: Refactor daos test python yaml files (#4367)"
    This reverts commit 43367a7691502a1743ed0c73bb47a71b5f2c69d1.
    
    Conflicts:
    	src/tests/ftest/daos_test/daos_core_test-rebuild.py
    	src/tests/ftest/daos_test/daos_core_test-rebuild.yaml
    	src/tests/ftest/daos_test/daos_core_test.py
    	src/tests/ftest/daos_test/daos_core_test.yaml

--- src/tests/ftest/daos_test/daos_core_test-nvme_recovery.py
+++ src/tests/ftest/daos_test/daos_core_test-nvme_recovery.py
@@ -14,66 +14,15 @@ class DaosCoreTestNvme(DaosCoreBase):
     :avocado: recursive
     """
 
-    def test_daos_nvme_recovery_1(self):
+    def test_nvme(self):
         """Jira ID: DAOS-3846.
 
         Test Description:
-            Run daos_test -N subtest 1
+            Purpose of this test is to run just the daos_test NVMe recovery
+            tests.
 
-        Use cases:
-            daos_test NVMe recovery test
+        :avocado: tags=all,hw,medium,nvme,ib2,full_regression
+        :avocado: tags=unittest,daos_test_nvme_recovery
 
-        :avocado: tags=all,full_regression
-        :avocado: tags=hw,medium,ib2
-        :avocado: tags=nvme
-        :avocado: tags=daos_nvme_recovery_1,unittest
         """
-        self.run_subtest()
-
-    def test_daos_nvme_recovery_2(self):
-        """Jira ID: DAOS-3846.
-
-        Test Description:
-            Run daos_test -N subtest 2
-
-        Use cases:
-            daos_test NVMe recovery test
-
-        :avocado: tags=all,full_regression
-        :avocado: tags=hw,medium,ib2
-        :avocado: tags=nvme
-        :avocado: tags=daos_nvme_recovery_2,unittest
-        """
-        self.run_subtest()
-
-    def test_daos_nvme_recovery_3(self):
-        """Jira ID: DAOS-3846.
-
-        Test Description:
-            Run daos_test -N subtest 3
-
-        Use cases:
-            daos_test NVMe recovery test
-
-        :avocado: tags=all,full_regression
-        :avocado: tags=hw,medium,ib2
-        :avocado: tags=nvme
-        :avocado: tags=daos_nvme_recovery_3,unittest
-        """
-        self.run_subtest()
-
-    def test_daos_nvme_recovery_4(self):
-        """Jira ID: DAOS-3846.
-
-        Test Description:
-            Run daos_test -N subtest 4
-
-        Use cases:
-            daos_test NVMe recovery test
-
-        :avocado: tags=all,full_regression
-        :avocado: tags=hw,medium,ib2
-        :avocado: tags=nvme
-        :avocado: tags=daos_nvme_recovery_4,unittest
-        """
-        self.run_subtest()
+        DaosCoreBase.run_subtest(self)
--- src/tests/ftest/daos_test/daos_core_test_dfs.py
+++ src/tests/ftest/daos_test/daos_core_test_dfs.py
@@ -9,7 +9,8 @@ import os
 from daos_core_base import DaosCoreBase
 
 class DaosCoreTestDfs(DaosCoreBase):
-    """Runs DAOS file system (DFS) tests.
+    """
+    Runs DAOS file system (DFS) tests.
 
     :avocado: recursive
     """
@@ -19,11 +20,11 @@ class DaosCoreTestDfs(DaosCoreBase):
         super(DaosCoreTestDfs, self).__init__(*args, **kwargs)
         self.hostfile_clients_slots = None
 
-    def test_daos_dfs_unit(self):
+    def test_subtest(self):
         """Jira ID: DAOS-5409
 
         Test Description:
-            Run daos_test -u
+            Run DAOS file system tests 'dfs_test'
 
         Use cases:
             Daos File system tests
@@ -31,24 +32,6 @@ class DaosCoreTestDfs(DaosCoreBase):
         :avocado: tags=all,pr,daily_regression
         :avocado: tags=hw,large
         :avocado: tags=dfs_test
-        :avocado: tags=daos_dfs_unit
-        """
-        self.daos_test = os.path.join(self.bin, 'dfs_test')
-        self.run_subtest()
-
-    def test_daos_dfs_parallel(self):
-        """Jira ID: DAOS-5409
-
-        Test Description:
-            Run daos_test -p
-
-        Use cases:
-            Daos File system tests
-
-        :avocado: tags=all,pr,daily_regression
-        :avocado: tags=hw,large
-        :avocado: tags=dfs_test
-        :avocado: tags=daos_dfs_parallel
         """
         self.daos_test = os.path.join(self.bin, 'dfs_test')
         self.run_subtest()
