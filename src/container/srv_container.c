0f1eca93fb681db9316f7b42ccd6b4c0559155b0
wangdi <di.wang@intel.com>
2021-02-09 09:13:04 -0800

    DAOS-6255 reintegration: few fixes for reintegration (#4536)
    1. st_shard in shard_tgt should be the real offset
    of the shard in object shards array.
    
    2. Do not add new remap shards to the extend list for
    remap shards for reintegration layout.
    
    3. Add po_rebuilding for extending/reintegration shards
    in anycases to avoid fetching and leader to be selected
    on that shard..
    
    4 add tests to verify large stripe rebuild/reintegration.
    
    5. retry rebuild with correct rebuild operation.
    
    6. Do not retry rebuild if the current leader will step
    down.
    
    7. Remove unnecessary Assert in cont_agg_eph_leader_ult().
    
    8. minor fixes in OID tests to avoid DER_NOTREACH failure.
    
    9. move ds_iv_ns_stop ahead of other service abort to make
    sure IV is aborted immediately.
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/container/srv_container.c
+++ src/container/srv_container.c
@@ -1231,14 +1231,19 @@ cont_agg_eph_leader_ult(void *arg)
 					min_eph = ec_agg->ea_server_ephs[i].eph;
 			}
 
-			D_ASSERTF(min_eph >= ec_agg->ea_current_eph, DF_U64" < "
-				  DF_U64"\n", min_eph, ec_agg->ea_current_eph);
 			if (min_eph == ec_agg->ea_current_eph)
 				continue;
 
-			D_DEBUG(DB_MD, DF_CONT" sync "DF_U64"\n",
+			/**
+			 * NB: during extending or reintegration, the new
+			 * server might cause the minum epoch is less than
+			 * ea_current_eph.
+			 */
+			D_DEBUG(DB_MD, DF_CONT" minimum "DF_U64" current "
+				DF_U64"\n",
 				DP_CONT(svc->cs_pool_uuid,
-					ec_agg->ea_cont_uuid), min_eph);
+					ec_agg->ea_cont_uuid),
+				min_eph, ec_agg->ea_current_eph);
 			rc = cont_iv_ec_agg_eph_refresh(pool->sp_iv_ns,
 							ec_agg->ea_cont_uuid,
 							min_eph);
