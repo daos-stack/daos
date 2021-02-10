9c003fbafde7c68a3675c3a613c8ffaedef5fc6d
Phillip Henderson <phillip.henderson@intel.com>
2021-02-10 00:40:43 -0500

    Updated bad_exclude.py test
    Quick-build: true
    Skip-unit-tests: true
    Skip-func-hw-test: true
    Test-tag-vm: badexclude
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/pool/bad_exclude.py
+++ src/tests/ftest/pool/bad_exclude.py
@@ -22,74 +22,51 @@ class BadExcludeTest(TestWithServers):
     def test_exclude(self):
         """Pass bad parameters to the dmg pool exclude command.
 
-        :avocado: tags=all,pool,full_regression,tiny,badexclude
+        :avocado: tags=all,full_regression
+        :avocado: tags=pool,bad_exclude_test,test_exclude
         """
-        # Accumulate a list of pass/fail indicators representing what is
-        # expected for each parameter then "and" them to determine the
-        # expected result of the test
-        expected_for_param = []
+        # Get the test parameters for the dmg pool exclude command
+        #   dmg pool exclude command options]
+        #       --pool=         Unique ID of DAOS pool
+        #       --rank=         Rank of the targets to be excluded
+        #       --target-idx=   Comma-separated list of target idx(s) to be
+        #                       excluded from the rank
+        uuid = self.params.get("uuid", "/run/test_params/*")
+        rank = self.params.get("rank", "/run/test_params/*")
+        target = self.params.get("target", "/run/test_params/*")
 
-        tgtlist = self.params.get("ranklist", '/run/testparams/tgtlist/*/')
-        targets = []
-
-        if tgtlist[0] == "NULLPTR":
-            targets = None
-            self.cancel("skipping null pointer test until DAOS-1929 is fixed")
-        else:
-            targets.append(tgtlist[0])
-        expected_for_param.append(tgtlist[1])
-
-        setlist = self.params.get("setname",
-                                  '/run/testparams/connectsetnames/*/')
-        connectset = setlist[0]
-        expected_for_param.append(setlist[1])
-
-        uuidlist = self.params.get("uuid", '/run/testparams/UUID/*/')
-        excludeuuid = uuidlist[0]
-        expected_for_param.append(uuidlist[1])
-
-        # if any parameter is FAIL then the test should FAIL, in this test
-        # virtually everyone should FAIL since we are testing bad parameters
-        expected_result = 'PASS'
-        for result in expected_for_param:
-            if result == 'FAIL':
-                expected_result = 'FAIL'
+        # Determine if this test is expected to pass or fail
+        expected_result = "PASS"
+        for data in (uuid, rank, target):
+            if not data[1]:
+                expected_result = "FAIL"
                 break
 
-        saved_grp = None
-        saved_uuid = None
-        self.add_pool(create=True, connect=True)
+        # Create the pool
+        self.add_pool(create=True, connect=False)
 
-        saved_grp = self.pool.pool.group
-        if connectset == 'NULLPTR':
-            # trash the pool group value
-            self.pool.pool.group = None
-        else:
-            self.pool.pool.set_group(connectset)
+        # Set arguments for dmg pool exclude
+        kwargs = {"pool": uuid[0], "rank": rank[0], "tgt_idx": target[0]}
+        for key in kwargs:
+            if kwargs[key] == "VALID" and key == "pool":
+                kwargs[key] = self.pool.uuid
+            elif kwargs[key] == "NULLPTR":
+                kwargs[key] = None
 
-        # trash the UUID value in various ways
-        if excludeuuid == 'NULLPTR':
-            self.cancel("skipping this test until DAOS-1932 is fixed")
-            ctypes.memmove(saved_uuid, self.pool.pool.uuid, 16)
-            self.pool.pool.uuid = 0
-        if excludeuuid == 'CRAP':
-            self.cancel("skipping this test until DAOS-1932 is fixed")
-            ctypes.memmove(saved_uuid, self.pool.pool.uuid, 16)
-            self.pool.pool.uuid[4] = 244
-
+        # Attempt the dmg pool exclude command with the specified parameters
         try:
-            self.pool.exclude(targets)
+            self.pool.dmg.pool_exclude(**kwargs)
+            actual_result = "PASS"
+        except CommandFailure as error:
+            self.log.info(
+                "dmg.pool_exclude(%s) failed: %s",
+                ", ".join(
+                    ["{}={}".format(key,value) for key, value in kwargs.items()]
+                ), error)
+            actual_result = "FAIL"
 
-            if expected_result == 'FAIL':
-                self.fail("Test was expected to fail but it passed.\n")
-
-        except DaosApiError as excep:
-            print(excep)
-            print(traceback.format_exc())
-            if expected_result == 'PASS':
-                self.fail("Test was expected to pass but it failed.\n")
-        finally:
-            if saved_grp is not None:
-                self.pool.pool.group = saved_grp
-            if saved_uuid is not None:
-                ctypes.memmove(self.pool.pool.uuid, saved_uuid, 16)
+        if actual_result != expected_result:
+            self.fail(
+                "Test was expected to {} but {}ED".format(
+                    expected_result, actual_result))
+        self.log.info("Test PASSED with expected result")
