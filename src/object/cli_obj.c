9128b25e067bfc84d0859b1aba41c0c563fea96e
David M. Moreno <david.mmoreno@gmail.com>
2021-02-12 02:12:36 -0600

    DAOS-5434 object: check for valid pool handle in obj_layout_create  (#4586)
    Tests 'test_invalid_container_handle' and 'test_closed_container_handle' were
    failing not reporting the expected status because in the obj_layout_create()
    function in object/cli_obj.c L308, when trying to get the pool from an
    incorrect container handler, the test was aborting as soon as it noticed
    that pool==NULL.
    In order to fix this the D_ASSERT call was replaced with a D_GOTO call
    instead, passing the expected '-DER_NO_HDL' return code. With this, the
    'open()' function of the DaosObj class can raise the DaosApiError to the
    test itself.
    
    Test-tag: pr,-hw object
    
    Signed-off-by: David Maldonado Moreno <david.maldonado.moreno@intel.com>

--- src/object/cli_obj.c
+++ src/object/cli_obj.c
@@ -306,7 +306,10 @@ obj_layout_create(struct dc_object *obj, bool refresh)
 	int			rc;
 
 	pool = dc_hdl2pool(dc_cont_hdl2pool_hdl(obj->cob_coh));
-	D_ASSERT(pool != NULL);
+	if (pool == NULL) {
+		D_WARN("Cannot find valid pool\n");
+		D_GOTO(out, rc = -DER_NO_HDL);
+	}
 
 	map = pl_map_find(pool->dp_pool, obj->cob_md.omd_id);
 	dc_pool_put(pool);
