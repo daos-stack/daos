46f0000428ef1b79720db8c639ccb0381d83c2c1
rpadma2 <ravindran.padmanabhan@intel.com>
2021-02-18 12:50:48 -0500

    DAOS-5866 test: Merge with master Test-tag-hw-medium: pr,hw,medium,ib2 osa
    Signed-off-by: rpadma2 <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/osa/osa_dmg_negative_test.py
+++ src/tests/ftest/osa/osa_dmg_negative_test.py
@@ -12,7 +12,7 @@ from test_utils_pool import TestPool
 class OSADmgNegativeTest(OSAUtils):
     # pylint: disable=too-many-ancestors
     """
-    Test Class Description: This test runs
+    Test Class Description: This test
     passes invalid parameters to dmg OSA
     related commands and check whether
     appropriate errors are returned.
