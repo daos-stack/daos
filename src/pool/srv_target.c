2a77de334073c176d340de278e24e4c769db25ad
Ashley Pittman <ashley.m.pittman@intel.com>
2021-02-19 10:12:04 +0000

    DAOS-6726 cart: Do not take a second reference on secondary groups. (#4688)
    * cart: Do not take a second initial reference on secondary groups.
    * cleanups
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/pool/srv_target.c
+++ src/pool/srv_target.c
@@ -507,7 +507,7 @@ pool_fetch_hdls_ult(void *data)
 	ABT_mutex_unlock(pool->sp_mutex);
 retry:
 	if (pool->sp_stopping) {
-		D_DEBUG(DB_MD, DF_UUID" skip fetching hdl due to stop\n",
+		D_DEBUG(DB_MD, DF_UUID": skip fetching hdl due to stop\n",
 			DP_UUID(pool->sp_uuid));
 		D_GOTO(out, rc);
 	}
@@ -570,8 +570,8 @@ ds_pool_start_ec_eph_query_ult(struct ds_pool *pool)
 	sched_req_attr_init(&attr, SCHED_REQ_GC, &pool->sp_uuid);
 	pool->sp_ec_ephs_req = sched_req_get(&attr, ec_eph_query_ult);
 	if (pool->sp_ec_ephs_req == NULL) {
-		D_ERROR(DF_UUID"Failed to get req for ec eph query ULT\n",
-		       DP_UUID(pool->sp_uuid));
+		D_ERROR(DF_UUID": Failed to get req for ec eph query ULT\n",
+			DP_UUID(pool->sp_uuid));
 		ABT_thread_join(ec_eph_query_ult);
 		return -DER_NOMEM;
 	}
@@ -585,7 +585,7 @@ ds_pool_tgt_ec_eph_query_abort(struct ds_pool *pool)
 	if (pool->sp_ec_ephs_req == NULL)
 		return;
 
-	D_DEBUG(DB_MD, DF_UUID"Stopping EC query ULT\n",
+	D_DEBUG(DB_MD, DF_UUID": Stopping EC query ULT\n",
 		DP_UUID(pool->sp_uuid));
 
 	sched_req_wait(pool->sp_ec_ephs_req, true);
@@ -979,14 +979,13 @@ pool_tgt_query(struct ds_pool *pool, struct daos_pool_space *ps)
 					&coll_args.ca_exclude_tgts,
 					&coll_args.ca_exclude_tgts_cnt);
 	if (rc) {
-		D_ERROR(DF_UUID "failed to get index : rc "DF_RC"\n",
+		D_ERROR(DF_UUID": failed to get index : rc "DF_RC"\n",
 			DP_UUID(pool->sp_uuid), DP_RC(rc));
 		return rc;
 	}
 
 	rc = dss_thread_collective_reduce(&coll_ops, &coll_args, 0);
-	if (coll_args.ca_exclude_tgts)
-		D_FREE(coll_args.ca_exclude_tgts);
+	D_FREE(coll_args.ca_exclude_tgts);
 	if (rc) {
 		D_ERROR("Pool query on pool "DF_UUID" failed, "DF_RC"\n",
 			DP_UUID(pool->sp_uuid), DP_RC(rc));
@@ -1050,7 +1049,7 @@ ds_pool_tgt_connect(struct ds_pool *pool, struct pool_iv_conn *pic)
 	}
 
 out:
-	if (rc != 0 && hdl != NULL)
+	if (rc != 0)
 		D_FREE(hdl);
 
 	D_DEBUG(DF_DSMS, DF_UUID": connect "DF_RC"\n",
@@ -1257,7 +1256,7 @@ ds_pool_tgt_map_update(struct ds_pool *pool, struct pool_buf *buf,
 				     0, 0, NULL);
 		if (ret) {
 			D_ERROR("dtx_resync_ult failure %d\n", ret);
-			D_FREE_PTR(arg);
+			D_FREE(arg);
 		}
 	} else {
 		D_WARN("Ignore update pool "DF_UUID" %d -> %d\n",
