8c02da0e9aff724bb8dfa88de0be1f29fcb43431
Ashley Pittman <ashley.m.pittman@intel.com>
2021-02-25 20:57:42 +0000

    DAOS-6726 engine: Resolve two leaks in the drpc code.
    The normal code-paths for two entries was not cleaning up resources
    leading to small leaks every time a pool was queried.
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/pool/srv_pool.c
+++ src/pool/srv_pool.c
@@ -2813,6 +2813,7 @@ out:
 	D_DEBUG(DF_DSMS, DF_UUID": replying rpc %p: "DF_RC"\n",
 		DP_UUID(in->pqi_op.pi_uuid), rpc, DP_RC(rc));
 	crt_reply_send(rpc);
+	daos_prop_free(prop);
 }
 
 /* Convert pool_comp_state_t to daos_target_state_t */
