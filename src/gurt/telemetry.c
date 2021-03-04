e2fdfb880abf111f2464186d3cfb4c67002a2bf2
Joel Rosenzweig <joel.b.rosenzweig@intel.com>
2021-03-04 12:35:35 -0500

    DAOS-6885 Telemetry: Added DER_SHMEM_PERMS (#4777)
    Added DER_SHMEM_PERMS which is now used to differentiate
    between a shared memory permissions failure and an out of
    shared memory error.
    
    Added steps to the admin guide troubleshooting section that
    describes what the DER_SHMEM_PERMS error means and how to
    correct it.
    
    Signed-off-by: Joel Rosenzweig <joel.b.rosenzweig@intel.com>

--- src/gurt/telemetry.c
+++ src/gurt/telemetry.c
@@ -265,7 +265,7 @@ d_tm_init(int id, uint64_t mem_size, int flags)
 	d_tm_shmem_root = d_tm_allocate_shared_memory(id, mem_size);
 
 	if (d_tm_shmem_root == NULL) {
-		rc = -DER_NO_SHMEM;
+		rc = -DER_SHMEM_PERMS;
 		goto failure;
 	}
 
@@ -318,12 +318,16 @@ void d_tm_fini(void)
 	if (d_tm_shmem_root == NULL)
 		return;
 
-	shmdt(d_tm_shmem_root);
-	if (!d_tm_retain_shmem) {
+	rc = shmdt(d_tm_shmem_root);
+	if (rc < 0)
+		D_ERROR("Unable to detach from shared memory segment.  "
+			"shmdt failed, %s.\n", strerror(errno));
+
+	if ((rc == 0) && !d_tm_retain_shmem) {
 		rc = shmctl(d_tm_shmid, IPC_RMID, NULL);
 		if (rc < 0)
-			D_ERROR("Unable to remove shared memory segment, %s.\n",
-				strerror(errno));
+			D_ERROR("Unable to remove shared memory segment.  "
+				"shmctl failed, %s.\n", strerror(errno));
 	}
 
 	d_tm_serialization = false;
@@ -2352,16 +2356,24 @@ d_tm_get_key(int srv_idx)
 uint64_t *
 d_tm_allocate_shared_memory(int srv_idx, size_t mem_size)
 {
-	key_t	key;
+	uint64_t	*addr;
+	key_t		key;
 
 	key = d_tm_get_key(srv_idx);
 	d_tm_shmid = shmget(key, mem_size, IPC_CREAT | 0660);
 	if (d_tm_shmid < 0) {
-		D_ERROR("shmget failed, %s\n", strerror(errno));
+		D_ERROR("Unable to allocate shared memory.  shmget failed, "
+			"%s\n", strerror(errno));
 		return NULL;
 	}
 
-	return (uint64_t *)shmat(d_tm_shmid, NULL, 0);
+	addr = shmat(d_tm_shmid, NULL, 0);
+	if (addr == (void *)-1) {
+		D_ERROR("Unable to allocate shared memory.  shmat failed, "
+			"%s\n", strerror(errno));
+		return NULL;
+	}
+	return addr;
 }
 
 /**
@@ -2383,13 +2395,15 @@ d_tm_get_shared_memory(int srv_idx)
 	key = d_tm_get_key(srv_idx);
 	shmid = shmget(key, 0, 0);
 	if (shmid < 0) {
-		D_ERROR("shmget failed, %s\n", strerror(errno));
+		D_ERROR("Unable to access shared memory.  shmget failed, "
+			"%s\n", strerror(errno));
 		return NULL;
 	}
 
 	addr = shmat(shmid, NULL, 0);
 	if (addr == (void *)-1) {
-		D_ERROR("shmat failed, %s\n", strerror(errno));
+		D_ERROR("Unable to access shared memory.  shmat failed, "
+			"%s\n", strerror(errno));
 		return NULL;
 	}
 	return addr;
