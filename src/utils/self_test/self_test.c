760d02c2c69e6e68c1ce976def9c1dfcdf14c50a
Steven Bollinger <stevenx.bollinger@intel.com>
2021-03-11 19:03:07 +0000

    CaRT-955 Selftest: Fix memory leak
    Signed-off-by: Steven Bollinger <stevenx.bollinger@intel.com>

--- src/utils/self_test/self_test.c
+++ src/utils/self_test/self_test.c
@@ -1893,6 +1893,15 @@ int main(int argc, char *argv[])
 
 	/********************* Clean up *********************/
 cleanup:
+	if (ms_endpts != NULL) {
+		free(ms_endpts);
+		ms_endpts = NULL;
+	}
+	if (endpts != NULL) {
+		free(endpts);
+		endpts = NULL;
+	}
+
 	if (all_params != NULL)
 		D_FREE(all_params);
 	d_log_fini();
