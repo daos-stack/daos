88dcee9775906b0817dcca64077b6c928c3d5f38
Joel Rosenzweig <joel.b.rosenzweig@intel.com>
2021-03-15 22:18:50 +0000

    DAOS-7017 Telemetry: Add capability to specify units for metrics
        Using d_tm_add_metric(), the developer can now specify a unit
        string as metadata at initialization time.  The long
        description metadata field has been replaced by this new unit
        string.
    
        CSV output has been updated to show the revised metadata
        fields.
    
        When printing a metric with associated units, the units are
        now shown next to the value.  Units for duration and snapshot
        metrics are fixed at microseconds.
    
        Arbitary units may be specified for counters and gauges.
        See telemetry_common.h for some typical unit definitions
        that may be used.
    
        Durations and timer snapshots do not accept any unit type
        other than D_TM_MICROSECOND.  Timestamps do not accept a
        unit string.
    
    Signed-off-by: Joel Rosenzweig <joel.b.rosenzweig@intel.com>

--- src/include/gurt/telemetry_producer.h
+++ src/include/gurt/telemetry_producer.h
@@ -27,7 +27,7 @@ int d_tm_decrement_gauge(struct d_tm_node_t **metric, uint64_t value,
 int d_tm_init(int id, uint64_t mem_size, int flags);
 int d_tm_init_histogram(struct d_tm_node_t *node, char *path, int num_buckets,
 			int initial_width, int multiplier);
-int d_tm_add_metric(struct d_tm_node_t **node, int metric_type, char *sh_desc,
-		    char *lng_desc, const char *fmt, ...);
+int d_tm_add_metric(struct d_tm_node_t **node, int metric_type, char *desc,
+		    char *units, const char *fmt, ...);
 void d_tm_fini(void);
 #endif /* __TELEMETRY_PRODUCER_H__ */
