776dfa7eb921d39cba86dc7e3208e210ab32ded8
Bruno Faccini <bruno.faccini@intel.com>
2021-03-16 17:20:14 +0100

    DAOS-1452 tests: specific test and output archive/cleanup
    Add a specific functional test to verify ULT stacks dump can
    occur on multple servers.
    
    Add ULT stacks dump files to the list of artifacts to
    archived/cleanup by CI.
    
    Change-Id: I63dcc2a14b85ee2b85b7f08c3b02efe7052973f3
    Test-tag: pr,-hw test_daos_server_dump_basic
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>

---
+++ src/tests/ftest/server/daos_server_dump.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+"""
+  (C) Copyright 2020-2021 Intel Corporation.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+"""
+from __future__ import print_function
+
+from apricot import TestWithServers
+from server_utils import ServerFailed
+from general_utils import pcmd
+
+
+class DaosServerDumpTest(TestWithServers):
+    """Daos server dump tests.
+
+    Test Class Description:
+        Simple test to verify that ULT stacks dumps work
+
+    :avocado: recursive
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a DaosServerDumpTest object."""
+        super(DaosServerDumpTest, self).__init__(*args, **kwargs)
+        self.start_servers_once = False
+        self.setup_start_agents = False
+        self.setup_start_servers = False
+
+    def test_daos_server_dump_basic(self):
+        """JIRA ID: DAOS-1452.
+
+        Test Description: Test engine ULT stacks dump.
+
+        :avocado: tags=all,small,control,daily_regression,server_start,basic
+        :avocado: tags=test_daos_server_dump_basic
+        """
+        # Setup the servers
+        self.add_server_manager()
+        self.configure_manager(
+            "server", self.server_managers[0], self.hostlist_servers,
+            self.hostfile_servers_slots)
+
+        try:
+            self.server_managers[0].start()
+            exception = None
+        except ServerFailed as err:
+            exception = err
+
+        # Verify
+        fail_message = ""
+        if exception is not None:
+            self.log.error("Server was expected to start")
+            self.fail(
+                "Server start failed when it was expected to complete")
+
+        self.kill()
+
+        # XXX may need to check for one file per engine...
+        ret_codes = pcmd(self.hostlist_servers, "ls /tmp/daos_dump\*.txt")
+        # Report any failures
+        if len(ret_codes) > 1 or 0 not in ret_codes:
+            failed = [
+                "{}: rc={}".format(val, key)
+                for key, val in ret_codes.items() if key != 0
+            ]
+        if failed:
+            self.fail(
+                "no ULT stacks dump found on following hosts: {}".format(
+                ", ".join(failed)))
+
+        self.log.info("Test passed!")
