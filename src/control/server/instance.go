ba93c07c692867ac9f265ca62a616cd079473352
Kris Jacque <kristin.jacque@intel.com>
2021-03-24 12:23:12 -0600

    DAOS-6351 control: Log when fault domain changes
    - Add fault domain to superblock, and update when it changes.
    - Log when the engine comes up with a different domain than the
      one written in its superblock.
    
    Signed-off-by: Kris Jacque <kristin.jacque@intel.com>

--- src/control/server/instance.go
+++ src/control/server/instance.go
@@ -217,9 +217,47 @@ func (ei *EngineInstance) determineRank(ctx context.Context, ready *srvpb.Notify
 	return r, resp.LocalJoin, nil
 }
 
+func (ei *EngineInstance) isFaultDomainChanging(sb *Superblock) bool {
+	if ei.hostFaultDomain == nil && sb.HostFaultDomain == "" {
+		return false
+	}
+
+	return !(ei.hostFaultDomain.String() == sb.HostFaultDomain)
+}
+
+func (ei *EngineInstance) updateFaultDomainInSuperblock() error {
+	superblock := ei.getSuperblock()
+	if superblock == nil {
+		return errors.New("nil superblock while updating fault domain")
+	}
+
+	if !ei.isFaultDomainChanging(superblock) {
+		return nil
+	}
+
+	ei.log.Infof("instance %d setting host fault domain to %q (previously %q)",
+		ei.Index(), ei.hostFaultDomain, superblock.HostFaultDomain)
+
+	newDomainStr := ""
+	if ei.hostFaultDomain != nil {
+		newDomainStr = ei.hostFaultDomain.String()
+	}
+	superblock.HostFaultDomain = newDomainStr
+
+	ei.setSuperblock(superblock)
+	if err := ei.WriteSuperblock(); err != nil {
+		return err
+	}
+	return nil
+}
+
 // handleReady determines the instance rank and sends a SetRank dRPC request
 // to the Engine.
 func (ei *EngineInstance) handleReady(ctx context.Context, ready *srvpb.NotifyReadyReq) error {
+	if err := ei.updateFaultDomainInSuperblock(); err != nil {
+		ei.log.Error(err.Error()) // nonfatal
+	}
+
 	r, localJoin, err := ei.determineRank(ctx, ready)
 	if err != nil {
 		return err
