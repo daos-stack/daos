e4337a956c966db92ff9e5591a97617bdc8bfc21
Alexander A Oganezov <alexander.a.oganezov@intel.com>
2021-04-01 15:29:03 +0000

    change warns to info
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -392,7 +392,7 @@ crt_init_opt(crt_group_id_t grpid, uint32_t flags, crt_init_options_t *opt)
 		}
 
 		if (strcmp(addr_env, "ofi+sockets") == 0) {
-			D_WARN("Overriding to use ofi+tcp;ofi_rxm\n");
+			D_INFO("Overriding to use ofi+tcp;ofi_rxm\n");
 			addr_env = "ofi+tcp;ofi_rxm";
 		}
 
@@ -437,7 +437,7 @@ do_init:
 
 			srx_env = getenv("FI_OFI_RXM_USE_SRX");
 			if (srx_env == NULL) {
-				D_WARN("FI_OFI_RXM_USE_SRX not set, set=1\n");
+				D_INFO("FI_OFI_RXM_USE_SRX not set, set=1\n");
 				setenv("FI_OFI_RXM_USE_SRX", "1", true);
 			}
 		}
