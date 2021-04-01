57b320cd04e813facf4c50852a73cbf7bf0caac6
mjean308 <maureen.jean@intel.com>
2021-04-01 13:34:31 -0400

    DAOS-7111 test: Cleanup python2.7 references (#5206)
    Test-tag-vm: pr,-hw daily_regression,-hw
    Test-tag-hw-small: pr,hw,small daily_regression,hw,small
    Test-tag-hw-medium: pr,hw,medium,ib2 daily_regression,hw,medium,ib2
    Test-tag-hw-large: pr,hw,large daily_regression,hw,large soak_smoke
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/client/pydaos/raw/daos_api.py
+++ src/client/pydaos/raw/daos_api.py
@@ -6,6 +6,9 @@
 """
 # pylint: disable=pylint-too-many-lines
 
+# pylint: disable=relative-beyond-top-level
+from .. import pydaos_shim
+# pylint: enable=relative-beyond-top-level
 
 import ctypes
 import threading
@@ -19,12 +22,6 @@ from . import daos_cref
 from . import conversion
 from .. import DaosClient
 
-# pylint: disable=import-error
-if sys.version_info < (3, 0):
-    from .. import pydaos_shim_27 as pydaos_shim
-else:
-    from .. import pydaos_shim_3 as pydaos_shim
-# pylint: enable=import-error
 
 DaosObjClass = enum.Enum(
     "DaosObjClass",
@@ -35,6 +32,7 @@ DaosContPropEnum = enum.Enum(
     "DaosContPropEnum",
     {key: value for key, value in list(pydaos_shim.__dict__.items())
      if key.startswith("DAOS_PROP_")})
+
 
 class DaosPool():
     """A python object representing a DAOS pool."""
@@ -1286,6 +1284,7 @@ class DaosContProperties(ctypes.Structure):
         self.chksum_type = ctypes.c_uint64(100)
         self.chunk_size = ctypes.c_uint64(0)
 
+
 class DaosInputParams():
     # pylint: disable=too-few-public-methods
     """ This is a helper python method
@@ -1310,6 +1309,7 @@ class DaosInputParams():
         create container method.
         """
         return self.co_prop
+
 
 class DaosContainer():
     # pylint: disable=too-many-public-methods
@@ -2138,6 +2138,7 @@ class DaosSnapshot():
         if retcode != 0:
             raise Exception("Failed to destroy the snapshot. RC: {0}"
                             .format(retcode))
+
 
 class DaosContext():
     # pylint: disable=too-few-public-methods
--- utils/rpms/daos.spec
+++ utils/rpms/daos.spec
@@ -8,7 +8,7 @@
 
 Name:          daos
 Version:       1.1.4
-Release:       2%{?relval}%{?dist}
+Release:       3%{?relval}%{?dist}
 Summary:       DAOS Storage Engine
 
 License:       BSD-2-Clause-Patent
@@ -356,7 +356,7 @@ getent passwd daos_agent >/dev/null || useradd -s /sbin/nologin -r -g daos_agent
 %{_libdir}/python3/site-packages/pydaos/__pycache__/*.pyc
 %{_libdir}/python3/site-packages/storage_estimator/__pycache__/*.pyc
 %endif
-%{_libdir}/python3/site-packages/pydaos/pydaos_shim_3.so
+%{_libdir}/python3/site-packages/pydaos/pydaos_shim.so
 %dir %{_libdir}/python3/site-packages/pydaos/raw
 %{_libdir}/python3/site-packages/pydaos/raw/*.py
 %if (0%{?rhel} >= 7)
@@ -401,7 +401,10 @@ getent passwd daos_agent >/dev/null || useradd -s /sbin/nologin -r -g daos_agent
 %{_libdir}/*.a
 
 %changelog
-* Mon Mar 29 2021 Brian J. Murrell <brian.murrell@intel.com> - 1.1.4-2
+* Thu Apr 1 2021 Maureen Jean <maureen.jean@intel.com> 1.1.4-3
+- Change pydaos_shim_3 to pydaos_shim
+
+* Mon Mar 29 2021 Brian J. Murrell <brian.murrell@intel.com> 1.1.4-2 
 - Move libdts.so to the daos-tests subpackage
 
 * Mon Mar 22 2021 Johann Lombardi <johann.lombardi@intel.com> 1.1.4-1
