41ac2a6e7250dadaf40e1f2eaa787a6d28f3dbca
Samir Raval <samir.raval@intel.com>
2021-04-13 14:05:08 -0700

    Update daos_nvme_recovery.c
    Quick-build: true
    Test-tag: daos_core_test_nvme

--- src/tests/suite/daos_nvme_recovery.c
+++ src/tests/suite/daos_nvme_recovery.c
@@ -75,10 +75,10 @@ nvme_fault_reaction(void **state, int mode)
 	}
 
 	/**
-	 * If test need multiple pool with both mode offline and online
-	 * create the another pool which will be offline by default.
-	 * create the another pool which will be offline by default.
-	 */
+	* If test need multiple pool with both mode offline and online
+	* create the another pool which will be offline by default.
+	* create the another pool which will be offline by default.
+	*/
 	if (mode == 2) {
 		char	*env;
 		int	size_gb;
