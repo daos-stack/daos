cc501b68e1cc1c06532599688a927ed43d7323d6
Samir Raval <samir.raval@intel.com>
2021-04-13 14:06:16 -0700

    Update daos_nvme_recovery.c
    Signed-off-by: Samir Raval <samir.raval@intel.com>

--- src/tests/suite/daos_nvme_recovery.c
+++ src/tests/suite/daos_nvme_recovery.c
@@ -77,7 +77,6 @@ nvme_fault_reaction(void **state, int mode)
 	/**
 	* If test need multiple pool with both mode offline and online
 	* create the another pool which will be offline by default.
-	* create the another pool which will be offline by default.
 	*/
 	if (mode == 2) {
 		char	*env;
