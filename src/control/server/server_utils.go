62b80489327e421973a5f73d80bc122a2a34c6e5
Michael MacDonald <mjmac.macdonald@intel.com>
2021-04-16 12:41:26 -0400

    HACK: Disable SCM/NVMe scanning
    Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>
    Signed-off-by: Dalton Bohning <daltonx.bohning@intel.com>

--- src/control/server/server_utils.go
+++ src/control/server/server_utils.go
@@ -284,24 +284,25 @@ func prepBdevStorage(srv *server, iommuEnabled bool) error {
 
 // scanBdevStorage performs discovery and validates existence of configured NVMe SSDs.
 func scanBdevStorage(srv *server) (*storage.BdevScanResponse, error) {
-	defer srv.logDuration(track("time to scan bdev storage"))
+	return &storage.BdevScanResponse{}, nil
+	// defer srv.logDuration(track("time to scan bdev storage"))
 
-	if srv.cfg.DisableHugepages {
-		srv.log.Debugf("skip nvme scan as hugepages have been disabled in config")
-		return &storage.BdevScanResponse{}, nil
-	}
+	// if srv.cfg.DisableHugepages {
+	// 	srv.log.Debugf("skip nvme scan as hugepages have been disabled in config")
+	// 	return &storage.BdevScanResponse{}, nil
+	// }
 
-	nvmeScanResp, err := srv.ctlSvc.NvmeScan(storage.BdevScanRequest{
-		DeviceList:  getBdevCfgsFromSrvCfg(srv.cfg).Bdevs(),
-		BypassCache: true, // init cache on first scan
-	})
-	if err != nil {
-		err = errors.Wrap(err, "NVMe Scan Failed")
-		srv.log.Errorf("%s", err)
-		return nil, err
-	}
+	// nvmeScanResp, err := srv.ctlSvc.NvmeScan(storage.BdevScanRequest{
+	// 	DeviceList:  getBdevCfgsFromSrvCfg(srv.cfg).Bdevs(),
+	// 	BypassCache: true, // init cache on first scan
+	// })
+	// if err != nil {
+	// 	err = errors.Wrap(err, "NVMe Scan Failed")
+	// 	srv.log.Errorf("%s", err)
+	// 	return nil, err
+	// }
 
-	return nvmeScanResp, nil
+	// return nvmeScanResp, nil
 }
 
 func setEngineBdevs(engine *EngineInstance, scanResp *storage.BdevScanResponse, lastEngineIdx, lastBdevCount *int) error {
