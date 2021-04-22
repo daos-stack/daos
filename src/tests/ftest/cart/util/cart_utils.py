17c6d671289c4e1a0842e95711d14d2791e979d2
Ethan Mallove <ethanx.a.mallove@intel.com>
2021-04-22 20:05:50 +0000

    CART-952 tests: Fix undefined var: self
    Skip-build-leap15-rpm: true
    Skip-func-test-leap15: true
    Skip-build-ubuntu-clang: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Parallel-build: true
    Quick-build: true
    Skip-unit-tests: true
    Skip-nlt: true
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-coverity-test: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    
    Signed-off-by: Ethan Mallove <ethanx.a.mallove@intel.com>

--- src/tests/ftest/cart/util/cart_utils.py
+++ src/tests/ftest/cart/util/cart_utils.py
@@ -344,7 +344,7 @@ class CartTest(TestWithoutServers):
 
         return tst_cmd
 
-    def log_check_valgrind_memcheck():
+    def log_check_valgrind_memcheck(self):
         """Check valgrind memcheck log files for errors."""
 
         memcheck_errors = 0
@@ -406,7 +406,7 @@ class CartTest(TestWithoutServers):
                 "Failed, return codes client {} server {}".format(
                     cli_rtn, srv_rtn))
 
-        self.log_check_valgrind_memcheck()
+        self.log_check_valgrind_memcheck(self)
 
         return 0
 
