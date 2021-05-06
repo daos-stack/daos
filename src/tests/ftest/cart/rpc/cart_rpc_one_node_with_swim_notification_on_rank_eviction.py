4dabf195c9a62cb86c8a95e4a47c46d6aa8c0732
Ethan Mallove <ethanx.a.mallove@intel.com>
2021-05-06 02:00:52 +0000

    CART-952 tests: Put "rpc" on 2nd tags= line
    Skip-build-leap15-rpm: true
    Skip-func-test-leap15: true
    Skip-build-ubuntu-clang: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Parallel-build: true
    Quick-build: true
    Skip-unit-tests: true
    Skip-nlt: true
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-coverity-test: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    
    Signed-off-by: Ethan Mallove <ethanx.a.mallove@intel.com>

--- src/tests/ftest/cart/rpc/cart_rpc_one_node_with_swim_notification_on_rank_eviction.py
+++ src/tests/ftest/cart/rpc/cart_rpc_one_node_with_swim_notification_on_rank_eviction.py
@@ -17,8 +17,8 @@ class CartRpcOneNodeSwimNotificationOnRankEvictionTest(CartTest):
     def test_cart_rpc(self):
         """Test CaRT RPC.
 
-        :avocado: tags=all,pr,rpc
-        :avocado: tags=cart,one_node,swim_rank_eviction,memcheck
+        :avocado: tags=all,pr
+        :avocado: tags=cart,rpc,one_node,swim_rank_eviction,memcheck
         """
         srvcmd = self.build_cmd(self.env, "test_servers")
 
