5423299a95cca980b4afa2a2f0ce5b2307163f1b
Ashley Pittman <ashley.m.pittman@intel.com>
2021-05-13 12:22:03 +0100

    DAOS-623 actions: Improve github actions support.
    Add another build test to github-actions.  This doesn't
    modify the existing code, but intends to replace it once
    stable.
    
    Use the github cache to re-use docker images for speed.
    Do a single image create, then re-use this for different
    compilers etc.
    Run NLT in docker, and report server leaks as annotations.
    Remove PSM2 from the Docker builds.
    
    This is enabled to build centos.8 with gcc/clang, and to
    build and test with ubuntu/clang but can be readily
    extended to the rest of the matrix once landed and stable
    without adding additional time or complexity.
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

---
+++ utils/run_in_ga.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+SCONS=scons
+
+if [ ! -e /usr/bin/scons ]
+then
+    SCONS=scons-3
+fi
+
+echo ::group::Rebuild spdk
+rm -rf /opt/daos/prereq/release/spdk
+$SCONS PREFIX=/opt/daos --build-deps=yes --deps-only
+echo ::endgroup::
+
+echo ::group::Build type debug.
+$SCONS --jobs 10 PREFIX=/opt/daos COMPILER=$COMPILER TARGET_TYPE=release BUILD_TYPE=debug
+echo ::endgroup::
+
+cat daos.conf
+
+echo ::group::Install debug
+$SCONS install
+echo ::endgroup::
+
+echo ::group::Setting up daos_admin
+. utils/sl/setup_local.sh
+./utils/setup_daos_admin.sh
+echo ::endgroup::
+
+echo ::group::Container copy test
+# DAOS-7440
+export LD_LIBRARY_PATH=/opt/daos/lib:/opt/daos/lib64:/opt/daos/bin/../lib64/daos_srv/../../prereq/release/spdk/lib/:$LD_LIBRARY_PATH
+./utils/node_local_test.py --no-root --test cont_copy
+echo ::endgroup::
---
+++ utils/run_in_ga_wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+IMAGE=$(docker images -all --filter label=DAOS=true --quiet | tail -n 1)
+
+docker run --name build-post --mount type=tmpfs,destination=/mnt/daos,tmpfs-mode=1777 --env COMPILER $IMAGE ./utils/run_in_ga.sh
