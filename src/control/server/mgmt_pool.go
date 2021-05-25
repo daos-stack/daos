b29454fa97229165b9d144e2bb9fc2a09335aaba
Krzysztof Majzerowicz-Jaszcz <krzysztof.majzerowicz-jaszcz@intel.com>
2021-05-25 10:38:51 +0200

    Pool tiering policy
    Set-prop disabled for pool tiering policy.
    
    Signed-off-by: Krzysztof Majzerowicz-Jaszcz <krzysztof.majzerowicz-jaszcz@intel.com>

--- src/control/server/mgmt_pool.go
+++ src/control/server/mgmt_pool.go
@@ -619,18 +619,18 @@ func resolvePoolPropVal(req *mgmtpb.PoolSetPropReq) (*mgmtpb.PoolSetPropReq, err
 		default:
 			return nil, errors.Errorf("unhandled self_heal type %q", healType)
 		}
-	case "policy":
-		newReq.SetPropertyNumber(drpc.PoolPropertyPolicy)
+	// case "policy":
+	// 	newReq.SetPropertyNumber(drpc.PoolPropertyPolicy)
 
-		recType := strings.TrimSpace(req.GetStrval())
-		switch strings.ToLower(recType) {
-		case "io_size":
-			newReq.SetValueNumber(uint64(drpc.PoolPolicyIoSize))
-		case "write_intensivity":
-			newReq.SetValueNumber(uint64(drpc.PoolPolicyWriteIntensivity))
-		default:
-			return nil, errors.Errorf("unhandled policy type %q", recType)
-		}
+	// 	recType := strings.TrimSpace(req.GetStrval())
+	// 	switch strings.ToLower(recType) {
+	// 	case "io_size":
+	// 		newReq.SetValueNumber(uint64(drpc.PoolPolicyIoSize))
+	// 	case "write_intensivity":
+	// 		newReq.SetValueNumber(uint64(drpc.PoolPolicyWriteIntensivity))
+	// 	default:
+	// 		return nil, errors.Errorf("unhandled policy type %q", recType)
+	// 	}
 
 	default:
 		return nil, errors.Errorf("unhandled pool property %q", propName)
