39f7b65bc564164b490491447e186c498e889120
Jeff Olivier <jeffrey.v.olivier@intel.com>
2021-05-27 20:31:04 -0600

    Try again with some messages
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- utils/check.sh
+++ utils/check.sh
@@ -6,12 +6,14 @@ if [ "$1" = "-n" ]; then
    shift
    for file in "$@"; do
        if [ -f "$file" ]; then
+           echo "$file should not exist but does."
            exit 1
        fi
    done
 else
    for file in "$@"; do
        if [ ! -f "$file" ]; then
+           echo "$file should exist but doesn't."
            exit 1
        fi
    done
