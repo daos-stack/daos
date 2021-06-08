ba0c845c35ee52ce3e2308613af37767a9c7d00d
rpadma2 <ravindran.padmanabhan@intel.com>
2021-06-08 14:41:54 -0400

    DAOS-7799 test: Offline reintegration :Enable rf. Test-tag-hw-medium: pr,hw,medium,ib2 offline_reintegration_full
    Signed-off-by: rpadma2 <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/osa/osa_offline_reintegration.py
+++ src/tests/ftest/osa/osa_offline_reintegration.py
@@ -9,7 +9,6 @@ from osa_utils import OSAUtils
 from daos_utils import DaosCommand
 from test_utils_pool import TestPool
 from write_host_file import write_host_file
-from apricot import skipForTicket
 
 
 class OSAOfflineReintegration(OSAUtils):
@@ -242,7 +241,6 @@ class OSAOfflineReintegration(OSAUtils):
         self.log.info("Offline Reintegration : Aggregation")
         self.run_offline_reintegration_test(1, data=True)
 
-    @skipForTicket("DAOS-7783,DAOS-7790")
     def test_osa_offline_reintegration_with_rf(self):
         """Test ID: DAOS-6923
         Test Description: Validate Offline Reintegration
