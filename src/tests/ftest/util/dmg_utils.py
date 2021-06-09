104577e21eca215c4286d11b010589a48f153468
Phil Henderson <phillip.henderson@intel.com>
2021-06-09 12:56:21 -0400

    DAOS-7667 tests: Adding functional telemetry tests. (#5860)
    Adding new functional utility code for verifying telemetry and the dmg
    telemetry command.  Added a basic test to verify container telemetry
    metrics.
    Added random POSIX/non-POSIX container creation as the open/close
    counters only update on POSIX containers.
    Updated telemetry list test to support server with or without nvme.
    Updating container telemetry metrics tracking counts only for the pool
    leader.
    Including stopping daos_servers found in the 'failed' state in
    launch.py
    Issue a systemctl reset-failed if a systemctl stop causes a failed
    status.
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/dmg_utils.py
+++ src/tests/ftest/util/dmg_utils.py
@@ -4,6 +4,8 @@
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
+# pylint: disable=too-many-lines
+
 from getpass import getuser
 from grp import getgrgid
 from pwd import getpwuid
@@ -919,6 +921,7 @@ class DmgCommand(DmgCommandBase):
     def config_generate(self, access_points, num_engines=None, min_ssds=None,
                         net_class=None):
         """Produce a server configuration.
+
         Args:
             access_points (str): Comma separated list of access point addresses.
             num_pmem (int): Number of SCM (pmem) devices required per
@@ -927,11 +930,14 @@ class DmgCommand(DmgCommandBase):
                 host in DAOS system. Defaults to None.
             net_class (str): Network class preferred. Defaults to None.
                 i.e. "best-available"|"ethernet"|"infiniband"
+
         Returns:
             dict: the contents of the generate config file.
+
         Raises:
             CommandFailure: if the dmg config generate command fails or if YAML
                 parser encounters an error condition while parsing the contents.
+
         """
         result = self._get_result(
             ("config", "generate"), access_points=access_points,
@@ -945,6 +951,62 @@ class DmgCommand(DmgCommandBase):
                     error)) from error
 
         return yaml_data
+
+    def telemetry_metrics_list(self, host):
+        """List telemetry metrics.
+
+        Args:
+            host (str): Server host from which to obtain the metrics
+
+        Raises:
+            CommandFailure: if the dmg system query command fails.
+
+        Returns:
+            dict: dictionary of output in JSON format
+
+        """
+        return self._get_json_result(
+            ("telemetry", "metrics", "list"), host=host)
+
+    def telemetry_metrics_query(self, host, metrics=None):
+        """Query telemetry metrics.
+
+        Args:
+            host (str): Server host from which to obtain the metrics
+            metrics (str, None): Comma-separated list of metric names to query.
+                Defaults to None which will query all metric names.
+
+        Raises:
+            CommandFailure: if the dmg system query command fails.
+
+        Returns:
+            dict: dictionary of output in JSON format
+
+        """
+        # Sample output (metric="process_start_time_seconds"):
+        # {
+        # "response": {
+        #   "metric_sets": [
+        #     {
+        #       "name": "process_start_time_seconds",
+        #       "description": "Start time of the process since unix epoch in
+        #                       seconds.",
+        #       "type": 3,
+        #       "metrics": [
+        #         {
+        #           "labels": {},
+        #           "value": 1622576326.6
+        #         }
+        #       ]
+        #     }
+        #   ]
+        # },
+        # "error": null,
+        # "status": 0
+        # }
+        return self._get_json_result(
+            ("telemetry", "metrics", "query"), host=host, metrics=metrics)
+
 
 def check_system_query_status(data):
     """Check if any server crashed.
