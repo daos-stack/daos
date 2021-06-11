1f4936d2b67a34f15628503045d0baec2d9e6fb8
Ethan Mallove <ethanx.a.mallove@intel.com>
2021-06-11 22:04:10 +0000

    CART-952 tests: Pass WITH_VALGRIND env var along
    Pass WITH_VALGRIND from script to script, e.g.,
      Jenkins runner => test_main.sh => ftest.sh => launch.py
    
    Skip-build-leap15-rpm: true
    Skip-func-test-leap15: true
    Skip-build-ubuntu-clang: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Parallel-build: true
    Quick-build: true
    Skip-unit-tests: true
    Skip-nlt: true
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-coverity-test: true
    Skip-func-test-leap15: false
    Skip-func-test-el8: false
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    
    Signed-off-by: Ethan Mallove <ethanx.a.mallove@intel.com>

--- ci/functional/test_main_node.sh
+++ ci/functional/test_main_node.sh
@@ -5,9 +5,11 @@ set -eux
 DAOS_TEST_SHARED_DIR=$(mktemp -d -p /mnt/share/)
 trap 'rm -rf $DAOS_TEST_SHARED_DIR' EXIT
 
+env
+
 export DAOS_TEST_SHARED_DIR
 export TEST_RPMS=true
 export REMOTE_ACCT=jenkins
+export WITH_VALGRIND="$WITH_VALGRIND
 
-env
 /usr/lib/daos/TESTING/ftest/ftest.sh "$TEST_TAG" "$TNODES" "$FTEST_ARG"
