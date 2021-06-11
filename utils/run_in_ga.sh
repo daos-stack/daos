5272de36239e849c0d008d15330545099805c8ee
Jeff Olivier <jeffrey.v.olivier@intel.com>
2021-06-11 16:57:57 -0600

    Modify the script to avoid too much building, use debug build
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- utils/run_in_ga.sh
+++ utils/run_in_ga.sh
@@ -14,45 +14,36 @@ rm -rf /opt/daos/prereq/release/spdk
 $SCONS PREFIX=/opt/daos --build-deps=yes --deps-only
 echo ::endgroup::
 
-echo ::group::Test client only build
-$SCONS --jobs 10 PREFIX=/opt/daos COMPILER="$COMPILER" TARGET_TYPE=release \
-       BUILD_TYPE=dev -c install
+echo ::group::Test client only debug build
+$SCONS --jobs 10 PREFIX=/opt/daos COMPILER="$COMPILER" BUILD_TYPE=debug -c \
+       install
 utils/check.sh -n /opt/daos/bin/dmg
-$SCONS --jobs 10 PREFIX=/opt/daos COMPILER="$COMPILER" TARGET_TYPE=release \
-       BUILD_TYPE=dev client install
+$SCONS --jobs 10 client install
 utils/check.sh -n /opt/daos/bin/daos_engine
 utils/check.sh -n /opt/daos/bin/vos_tests
 utils/check.sh /opt/daos/bin/dmg
 echo ::endgroup::
 
-echo ::group::Test server only build
-$SCONS --jobs 10 PREFIX=/opt/daos COMPILER="$COMPILER" TARGET_TYPE=release \
-       BUILD_TYPE=dev -c install
+echo ::group::Test server only debug build
+$SCONS --jobs 10 -c install
 utils/check.sh -n /opt/daos/bin/daos_engine
-$SCONS --jobs 10 PREFIX=/opt/daos COMPILER="$COMPILER" TARGET_TYPE=release \
-       BUILD_TYPE=dev server install
+$SCONS --jobs 10 server install
 utils/check.sh /opt/daos/bin/daos_engine
 utils/check.sh -n /opt/daos/bin/vos_tests
 utils/check.sh -n /opt/daos/bin/dmg
 echo ::endgroup::
 
-echo ::group::Test incremental build with test target
-$SCONS --jobs 10 PREFIX=/opt/daos COMPILER="$COMPILER" TARGET_TYPE=release \
-       BUILD_TYPE=dev test install
-utils/check.sh /opt/daos/bin/daos_engine
-utils/check.sh /opt/daos/bin/vos_tests
-utils/check.sh /opt/daos/bin/dmg
-echo ::endgroup::
-
-echo ::group::Build type debug.
-$SCONS --jobs 10 PREFIX=/opt/daos COMPILER="$COMPILER" TARGET_TYPE=release \
-       BUILD_TYPE=debug
+echo ::group::Test incremental debug build with test target
+$SCONS --jobs 10 test
 echo ::endgroup::
 
 cat daos.conf
 
 echo ::group::Install debug
 $SCONS install
+utils/check.sh /opt/daos/bin/daos_engine
+utils/check.sh /opt/daos/bin/vos_tests
+utils/check.sh /opt/daos/bin/dmg
 echo ::endgroup::
 
 echo ::group::Setting up daos_admin
