e1bf0e4f1f4ed210d2749cf1ddcad218d22b474b
Ethan Mallove <ethanx.a.mallove@intel.com>
2021-06-17 03:08:27 +0000

    CART-952 tests: No logtest needed for valgrind xml
     * No logtest needed for valgrind XML files
     * Files in shared dir needn't be archived from all hosts
    
    Skip-build-leap15-rpm: true
    Skip-func-test-leap15: true
    Skip-build-ubuntu-clang: true
    Skip-build-leap15-icc: true
    Skip-coverity-test: true
    Parallel-build: true
    Quick-build: true
    Skip-unit-tests: true
    Skip-nlt: true
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-coverity-test: true
    Skip-func-test-leap15: false
    Skip-func-test-el8: false
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    
    Signed-off-by: Ethan Mallove <ethanx.a.mallove@intel.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -1015,15 +1015,14 @@ def run_tests(test_files, tag_filter, args):
                 valgrind_logs_dir = os.environ.get("DAOS_TEST_SHARED_DIR",
                                                    os.environ['HOME'])
 
-                # Archive remote valgrind log files
+                # Archive valgrind log files from shared dir
                 return_code |= archive_files(
                     "valgrind log files",
                     os.path.join(avocado_logs_dir, "latest", "valgrind_logs"),
-                    test_hosts,
+                    [test_hosts[0]],
                     "{}/valgrind*".format(valgrind_logs_dir),
                     args,
-                    avocado_logs_dir,
-                    get_test_category(test_file["py"]))
+                    avocado_logs_dir)
 
             # Optionally rename the test results directory for this test
             if args.rename:
@@ -1204,7 +1203,6 @@ def archive_files(description, destination, hosts, source_files, args,
         destination (str): path in which to archive files
         hosts (list): hosts from which to archive files
         source_files (str): remote files to archive
-        cart (str): enable running cart_logtest.py
         args (argparse.Namespace): command line arguments for this program
         avocado_logs_dir (optional, str): path to the avocado log files.
             Required for checking for large log files - see 'test_name'.
