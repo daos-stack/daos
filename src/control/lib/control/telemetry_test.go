e5e92f3458fb87b749dff4064a80d8d331a6a4fb
Kris Jacque <kristin.jacque@intel.com>
2021-06-17 13:08:52 -0600

    DAOS-7797 control: Display JSON metric type as a string (#5983)
    - Translate the metric type to and from human-friendly string
      values in JSON.
    - Improve JSON marshaling and unmarshaling for some related
      structures.
    
    Signed-off-by: Kris Jacque <kristin.jacque@intel.com>

--- src/control/lib/control/telemetry_test.go
+++ src/control/lib/control/telemetry_test.go
@@ -14,6 +14,7 @@ import (
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/pkg/errors"
@@ -642,6 +643,162 @@ func TestControl_Metric_JSON(t *testing.T) {
 			expResult := tc.metric
 			if tc.metric == nil {
 				expResult = &SimpleMetric{}
+			}
+
+			if diff := cmp.Diff(expResult, unmarshaled); diff != "" {
+				t.Fatalf("unmarshaled different from original (-want, +got):\n%s\n", diff)
+			}
+		})
+	}
+}
+
+func TestControl_metricTypeFromString(t *testing.T) {
+	for name, tc := range map[string]struct {
+		input   string
+		expType MetricType
+	}{
+		"empty": {
+			expType: MetricTypeUnknown,
+		},
+		"counter": {
+			input:   "counter",
+			expType: MetricTypeCounter,
+		},
+		"gauge": {
+			input:   "gauge",
+			expType: MetricTypeGauge,
+		},
+		"summary": {
+			input:   "summary",
+			expType: MetricTypeSummary,
+		},
+		"histogram": {
+			input:   "histogram",
+			expType: MetricTypeHistogram,
+		},
+		"generic": {
+			input:   "generic",
+			expType: MetricTypeGeneric,
+		},
+		"invalid": {
+			input:   "some garbage text",
+			expType: MetricTypeUnknown,
+		},
+		"weird capitalization": {
+			input:   "CoUnTeR",
+			expType: MetricTypeCounter,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			gotType := metricTypeFromString(tc.input)
+
+			common.AssertEqual(t, tc.expType, gotType, "")
+		})
+	}
+}
+
+func TestControl_MetricSet_JSON(t *testing.T) {
+	for name, tc := range map[string]struct {
+		set *MetricSet
+	}{
+		"nil": {},
+		"generic type": {
+			set: &MetricSet{
+				Name:        "timespan",
+				Description: "It's been a while",
+				Type:        MetricTypeGeneric,
+				Metrics: []Metric{
+					newSimpleMetric(map[string]string{
+						"units": "nanoseconds",
+					}, float64(time.Second)),
+				},
+			},
+		},
+		"counter type": {
+			set: &MetricSet{
+				Name:        "one_ring",
+				Description: "Precious...",
+				Type:        MetricTypeCounter,
+				Metrics: []Metric{
+					newSimpleMetric(map[string]string{
+						"owner": "frodo",
+					}, 1),
+				},
+			},
+		},
+		"gauge type": {
+			set: &MetricSet{
+				Name:        "funny_hats",
+				Description: "Hilarious headgear in inventory",
+				Type:        MetricTypeGauge,
+				Metrics: []Metric{
+					newSimpleMetric(map[string]string{
+						"type": "tophat",
+					}, 1),
+					newSimpleMetric(map[string]string{
+						"type": "cowboy",
+					}, 6),
+					newSimpleMetric(map[string]string{
+						"type": "jester",
+					}, 0),
+				},
+			},
+		},
+		"summary type": {
+			set: &MetricSet{
+				Name:        "alpha",
+				Description: "The first letter! Everybody's favorite!",
+				Type:        MetricTypeSummary,
+				Metrics: []Metric{
+					&SummaryMetric{
+						Labels:      map[string]string{"beta": "b"},
+						SampleCount: 3,
+						SampleSum:   42,
+						Quantiles:   map[float64]float64{0.5: 2.2},
+					},
+				},
+			},
+		},
+		"histogram type": {
+			set: &MetricSet{
+				Name:        "my_histogram",
+				Description: "This is a histogram",
+				Type:        MetricTypeHistogram,
+				Metrics: []Metric{
+					&HistogramMetric{
+						Labels:      map[string]string{"owner": "me"},
+						SampleCount: 1024,
+						SampleSum:   12344,
+						Buckets: []*MetricBucket{
+							{
+								CumulativeCount: 789,
+								UpperBound:      500,
+							},
+							{
+								CumulativeCount: 456,
+								UpperBound:      1000,
+							},
+						},
+					},
+				},
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			marshaled, err := json.Marshal(tc.set)
+			if err != nil {
+				t.Fatalf("expected to marshal, got %q", err)
+			}
+
+			unmarshaled := new(MetricSet)
+			err = json.Unmarshal(marshaled, unmarshaled)
+			if err != nil {
+				t.Fatalf("expected to unmarshal, got %q", err)
+			}
+
+			expResult := tc.set
+			if tc.set == nil {
+				expResult = &MetricSet{}
 			}
 
 			if diff := cmp.Diff(expResult, unmarshaled); diff != "" {
