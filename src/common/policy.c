7ddc6ff7081a5715c9a121d8be50c2feb67b9a27
Krzysztof Majzerowicz-Jaszcz <krzysztof.majzerowicz-jaszcz@intel.com>
2021-06-23 15:58:25 +0200

    DAOS-7674 tiering: Pool tier placement policy
    Review changes applied
    
    Signed-off-by: Krzysztof Majzerowicz-Jaszcz <krzysztof.majzerowicz-jaszcz@intel.com>

--- src/common/policy.c
+++ src/common/policy.c
@@ -19,14 +19,60 @@ static const char *POLICY_NAMES[DAOS_MEDIA_POLICY_MAX] = {
 	"write_intensivity" };
 
 static const char *POLICY_PARAMS[DAOS_MEDIA_POLICY_MAX]
-				[DAOS_MEDIA_POLICY_PARAMS_MAX] =
-{
+				[DAOS_MEDIA_POLICY_PARAMS_MAX] = {
 {"th1", "th2", "", ""},				/* io_size */
 {"wr_size", "hot1", "hot2", ""}			/* write_intensivity */
 };
 
 static const char *PARAM_DELIM = ",";
 static const char *VALUE_DELIM = "=";
+
+static bool
+parse_param_val(int param_idx, char *tok, struct policy_desc_t *out_pd)
+{
+	char *endptr;
+	int val = strtol(tok, &endptr, 10);
+	if (*tok != '\0' && *endptr == '\0') {
+		if (out_pd != NULL) {
+			out_pd->params[param_idx] = val;
+		}
+		return true;
+	}
+
+	return false;
+}
+
+static int
+get_param_idx_for_policy(int policy_idx, char *name, int len)
+{
+	int param_idx = POLICY_BAD_PARAM;
+	int i;
+
+	for (i = 0; i < DAOS_MEDIA_POLICY_PARAMS_MAX; i++) {
+		if (strncmp(name, POLICY_PARAMS[policy_idx][i], len) == 0) {
+			param_idx = i;
+			break;
+		}
+	}
+
+	return param_idx;
+}
+
+static int
+policy_name_to_idx(char *name, int len)
+{
+	int policy_idx = DAOS_MEDIA_POLICY_MAX;
+	int i;
+
+	for (i = 0; i < DAOS_MEDIA_POLICY_MAX; i++) {
+		if (strncmp(name, POLICY_NAMES[i], len) == 0) {
+			policy_idx = i;
+			break;
+		}
+	}
+
+	return policy_idx;
+}
 
 bool
 daos_policy_try_parse(const char *policy_str, struct policy_desc_t *out_pd)
@@ -41,90 +87,170 @@ daos_policy_try_parse(const char *policy_str, struct policy_desc_t *out_pd)
 	int param_idx		= POLICY_BAD_PARAM;
 	int tok_len		= 0;
 	bool ret_val		= false;
-
-	int i,j,k;
-
-	size_t len = strlen(policy_str);
-
+	int i, j;
+	size_t len;
 	char *str;
+
+	len = strlen(policy_str);
 	D_STRNDUP(str, policy_str, len); /* for strtok_r */
 
 	unsigned int policy_idx = DAOS_MEDIA_POLICY_MAX;
 
 	/* tokenize input to "name=value" format */
-	for (i = 0; ;i++, str = NULL) {
+	for (i = 0; ; i++, str = NULL) {
 		tok = strtok_r(str, delim, &saveptr);
-		if (tok == NULL) {
+		if (tok == NULL)
 			break;
-		}
+
 		/* tokenize left and right items of "name=value" */
 		str2 = tok;
 
 		for (j = 0; ; j++, str2 = NULL) {
 			tok = strtok_r(str2, delim2, &saveptr2);
-			if (tok == NULL) {
+			if (tok == NULL)
 				break;
-			}
 
-			if (strncmp(tok, "type", 5) == 0) {
+			tok_len = strlen(tok);
+
+			/* type should always be the first parameter */
+			if (i == 0 && strncmp(tok, "type", 5) == 0) {
 				type_found = true;
 				continue;
 			}
 
-			tok_len = strlen(tok);
 			if (type_found) {
-				for (k = 0; k < DAOS_MEDIA_POLICY_MAX; k++) {
-					if (strncmp(tok, POLICY_NAMES[k],
-					    tok_len) == 0) {
-						policy_idx = k;
-						if (out_pd != NULL)
-							out_pd->policy = k;
-
-						break;
-					}
-				}
+				policy_idx = policy_name_to_idx(tok, tok_len);
 				if (policy_idx == DAOS_MEDIA_POLICY_MAX)
 					goto out;
 
+				if (out_pd != NULL)
+					out_pd->policy = policy_idx;
+
 				type_found = false;
 				continue;
-			} else if (policy_idx != DAOS_MEDIA_POLICY_MAX) {
+			}
+			if (policy_idx != DAOS_MEDIA_POLICY_MAX) {
 				if (param_idx == POLICY_BAD_PARAM) {
-					for (k = 0;
-					     k < DAOS_MEDIA_POLICY_PARAMS_MAX;
-					     k++) {
-						if (strncmp(tok,
-						  POLICY_PARAMS[policy_idx][k],
-						  tok_len) == 0) {
-							param_idx = k;
-							break;
-						}
-					}
+					param_idx = get_param_idx_for_policy(
+						    policy_idx, tok, tok_len);
 					if (param_idx == POLICY_BAD_PARAM)
 						goto out;
 
 					continue;
-				} else {
-					char *endptr;
-					int val = strtol(tok, &endptr, 10);
-
-					if (*tok != '\0' && *endptr == '\0') {
-						if (out_pd != NULL) {
-							out_pd->
-							params[param_idx] = val;
-						}
-					} else {
-						goto out;
-					}
-
-					param_idx = POLICY_BAD_PARAM;
-					continue;
 				}
+				bool ret = parse_param_val(param_idx, tok,
+							   out_pd);
+				if (!ret)
+					goto out;
+
+				param_idx = POLICY_BAD_PARAM;
 			}
 		}
 	}
-	ret_val = true;
+	if (policy_idx != DAOS_MEDIA_POLICY_MAX)
+		ret_val = true;
 out:
 	free(str);
 	return ret_val;
 }
+
+
+// bool
+// daos_policy_try_parse(const char *policy_str, struct policy_desc_t *out_pd)
+// {
+// 	char *tok		= NULL;
+// 	const char *delim	= PARAM_DELIM;
+// 	const char *delim2	= VALUE_DELIM;
+// 	char *saveptr		= NULL;
+// 	char *saveptr2		= NULL;
+// 	char *str2		= NULL;
+// 	bool type_found		= false;
+// 	int param_idx		= POLICY_BAD_PARAM;
+// 	int tok_len		= 0;
+// 	bool ret_val		= false;
+
+// 	int i,j,k;
+
+// 	size_t len = strlen(policy_str);
+
+// 	char *str;
+// 	D_STRNDUP(str, policy_str, len); /* for strtok_r */
+
+// 	unsigned int policy_idx = DAOS_MEDIA_POLICY_MAX;
+
+// 	/* tokenize input to "name=value" format */
+// 	for (i = 0; ;i++, str = NULL) {
+// 		tok = strtok_r(str, delim, &saveptr);
+// 		if (tok == NULL) {
+// 			break;
+// 		}
+// 		/* tokenize left and right items of "name=value" */
+// 		str2 = tok;
+
+// 		for (j = 0; ; j++, str2 = NULL) {
+// 			tok = strtok_r(str2, delim2, &saveptr2);
+// 			if (tok == NULL) {
+// 				break;
+// 			}
+
+// 			if (strncmp(tok, "type", 5) == 0) {
+// 				type_found = true;
+// 				continue;
+// 			}
+
+// 			tok_len = strlen(tok);
+// 			if (type_found) {
+// 				for (k = 0; k < DAOS_MEDIA_POLICY_MAX; k++) {
+// 					if (strncmp(tok, POLICY_NAMES[k], tok_len) == 0) {
+// 						policy_idx = k;
+// 						if (out_pd != NULL)
+// 							out_pd->policy = k;
+
+// 						break;
+// 					}
+// 				}
+// 				if (policy_idx == DAOS_MEDIA_POLICY_MAX)
+// 					goto out;
+
+// 				type_found = false;
+// 				continue;
+// 			} else if (policy_idx != DAOS_MEDIA_POLICY_MAX) {
+// 				if (param_idx == POLICY_BAD_PARAM) {
+// 					for (k = 0;
+// 					     k < DAOS_MEDIA_POLICY_PARAMS_MAX;
+// 					     k++) {
+// 						if (strncmp(tok,
+// 						  POLICY_PARAMS[policy_idx][k],
+// 						  tok_len) == 0) {
+// 							param_idx = k;
+// 							break;
+// 						}
+// 					}
+// 					if (param_idx == POLICY_BAD_PARAM)
+// 						goto out;
+
+// 					continue;
+// 				} else {
+// 					char *endptr;
+// 					int val = strtol(tok, &endptr, 10);
+
+// 					if (*tok != '\0' && *endptr == '\0') {
+// 						if (out_pd != NULL) {
+// 							out_pd->
+// 							params[param_idx] = val;
+// 						}
+// 					} else {
+// 						goto out;
+// 					}
+
+// 					param_idx = POLICY_BAD_PARAM;
+// 					continue;
+// 				}
+// 			}
+// 		}
+// 	}
+// 	ret_val = true;
+// out:
+// 	free(str);
+// 	return ret_val;
+// }
