181610ed0ece5934e581e47e011b2c52986d4c2a
Krzysztof Majzerowicz-Jaszcz <krzysztof.majzerowicz-jaszcz@intel.com>
2021-06-23 17:43:38 +0200

    DAOS-7674 tiering: Pool tier placement policy
    Conflicts resolved
    
    Signed-off-by: Krzysztof Majzerowicz-Jaszcz <krzysztof.majzerowicz-jaszcz@intel.com>

--- src/common/policy.c
+++ src/common/policy.c
@@ -32,6 +32,7 @@ parse_param_val(int param_idx, char *tok, struct policy_desc_t *out_pd)
 {
 	char *endptr;
 	int val = strtol(tok, &endptr, 10);
+
 	if (*tok != '\0' && *endptr == '\0') {
 		if (out_pd != NULL) {
 			out_pd->params[param_idx] = val;
@@ -153,104 +154,3 @@ out:
 	free(str);
 	return ret_val;
 }
-
-
-// bool
-// daos_policy_try_parse(const char *policy_str, struct policy_desc_t *out_pd)
-// {
-// 	char *tok		= NULL;
-// 	const char *delim	= PARAM_DELIM;
-// 	const char *delim2	= VALUE_DELIM;
-// 	char *saveptr		= NULL;
-// 	char *saveptr2		= NULL;
-// 	char *str2		= NULL;
-// 	bool type_found		= false;
-// 	int param_idx		= POLICY_BAD_PARAM;
-// 	int tok_len		= 0;
-// 	bool ret_val		= false;
-
-// 	int i,j,k;
-
-// 	size_t len = strlen(policy_str);
-
-// 	char *str;
-// 	D_STRNDUP(str, policy_str, len); /* for strtok_r */
-
-// 	unsigned int policy_idx = DAOS_MEDIA_POLICY_MAX;
-
-// 	/* tokenize input to "name=value" format */
-// 	for (i = 0; ;i++, str = NULL) {
-// 		tok = strtok_r(str, delim, &saveptr);
-// 		if (tok == NULL) {
-// 			break;
-// 		}
-// 		/* tokenize left and right items of "name=value" */
-// 		str2 = tok;
-
-// 		for (j = 0; ; j++, str2 = NULL) {
-// 			tok = strtok_r(str2, delim2, &saveptr2);
-// 			if (tok == NULL) {
-// 				break;
-// 			}
-
-// 			if (strncmp(tok, "type", 5) == 0) {
-// 				type_found = true;
-// 				continue;
-// 			}
-
-// 			tok_len = strlen(tok);
-// 			if (type_found) {
-// 				for (k = 0; k < DAOS_MEDIA_POLICY_MAX; k++) {
-// 					if (strncmp(tok, POLICY_NAMES[k], tok_len) == 0) {
-// 						policy_idx = k;
-// 						if (out_pd != NULL)
-// 							out_pd->policy = k;
-
-// 						break;
-// 					}
-// 				}
-// 				if (policy_idx == DAOS_MEDIA_POLICY_MAX)
-// 					goto out;
-
-// 				type_found = false;
-// 				continue;
-// 			} else if (policy_idx != DAOS_MEDIA_POLICY_MAX) {
-// 				if (param_idx == POLICY_BAD_PARAM) {
-// 					for (k = 0;
-// 					     k < DAOS_MEDIA_POLICY_PARAMS_MAX;
-// 					     k++) {
-// 						if (strncmp(tok,
-// 						  POLICY_PARAMS[policy_idx][k],
-// 						  tok_len) == 0) {
-// 							param_idx = k;
-// 							break;
-// 						}
-// 					}
-// 					if (param_idx == POLICY_BAD_PARAM)
-// 						goto out;
-
-// 					continue;
-// 				} else {
-// 					char *endptr;
-// 					int val = strtol(tok, &endptr, 10);
-
-// 					if (*tok != '\0' && *endptr == '\0') {
-// 						if (out_pd != NULL) {
-// 							out_pd->
-// 							params[param_idx] = val;
-// 						}
-// 					} else {
-// 						goto out;
-// 					}
-
-// 					param_idx = POLICY_BAD_PARAM;
-// 					continue;
-// 				}
-// 			}
-// 		}
-// 	}
-// 	ret_val = true;
-// out:
-// 	free(str);
-// 	return ret_val;
-// }
