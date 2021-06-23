f95f1f850876364e96eb474da8f9b4d4c428036e
Ashley Pittman <ashley.m.pittman@intel.com>
2021-06-23 08:52:50 +0100

    Increase reps and timeout Change threshold so test passes.
    Quick-Functional: true
    Test-tag: iorinterceptbasic
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/tests/ftest/ior/ior_intercept_basic.py
+++ src/tests/ftest/ior/ior_intercept_basic.py
@@ -39,7 +39,10 @@ class IorIntercept(IorTestBase):
             Compare the results and check whether using interception
                 library provides better performance.
 
-        :avocado: tags=all,pr,full_regression,hw,small,daosio,iorinterceptbasic,dfuse
+        :avocado: tags=all,full_regression
+        :avocado: tags=hw,small
+        :avocado: tags=daosio,dfuse
+        :avocado: tags=iorinterceptbasic
         """
         apis = self.params.get("ior_api", '/run/ior/iorflags/ssf/*')
         for api in apis:
