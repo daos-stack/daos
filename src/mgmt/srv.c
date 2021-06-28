07d2106b0ec60a2ecaf6c230dbbfb04942888df8
Michael MacDonald <mjmac.macdonald@intel.com>
2021-06-28 17:10:40 -0400

    Engine-side changes, broken out for easier review
    Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

--- src/mgmt/srv.c
+++ src/mgmt/srv.c
@@ -135,6 +135,9 @@ process_drpc_request(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 	case DRPC_METHOD_MGMT_POOL_SET_PROP:
 		ds_mgmt_drpc_pool_set_prop(drpc_req, drpc_resp);
 		break;
+	case DRPC_METHOD_MGMT_POOL_GET_PROP:
+		ds_mgmt_drpc_pool_get_prop(drpc_req, drpc_resp);
+		break;
 	case DRPC_METHOD_MGMT_POOL_QUERY:
 		ds_mgmt_drpc_pool_query(drpc_req, drpc_resp);
 		break;
