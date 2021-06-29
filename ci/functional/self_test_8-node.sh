50eb3c8c947313543db5a39a9be8504f89a6707a
Saurabh Tandan <saurabh.tandan@intel.com>
2021-06-29 22:14:53 +0000

    Adding self_test
    Skip-checkpatch: true
    RPM-test-version: 1.3.102
    Skip-build: true
    Skip-unit-tests: true
    Skip-coverity-test: true
    Skip-func-test-vm: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-scan-centos-rpms: true
    Skip-test-centos-rpms: true
    
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

---
+++ ci/functional/self_test_8-node.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+set -x
+
+daospath=$1
+nodes=$2
+IFS=', ' read -r -a nodes_array <<< "$nodes"
+for i in "${nodes_array[@]}"; do
+    tnodes+="${i}:5,"
+done
+tnodes=${tnodes%?}
+echo $tnodes
+
+/usr/lib64/openmpi3/bin/orterun \
+    --mca btl self,tcp \
+    -N 1 \
+    -x D_LOG_FILE=/var/tmp/daos_testing/self_test_np_sep_cart_server.log \
+    -x D_LOG_FILE_APPEND_PID=1 \
+    -x PATH \
+    -x D_LOG_MASK=WARN \
+    -x CRT_PHY_ADDR_STR=ofi+sockets \
+    -x CRT_CTX_SHARE_ADDR=0 \
+    -x CRT_ATTACH_INFO_PATH=/home/standan/tmp/ \
+    -x CRT_CTX_NUM=16 \
+        -x OFI_INTERFACE=ib0 \
+        --host "${tnodes}" \
+    crt_launch -e ${daospath}/lib/daos/TESTING/tests/test_group_np_srv --name selftest_srv_grp : \
+        -x OFI_INTERFACE=ib1 \
+        --host "${tnodes}" \
+    crt_launch -e ${daospath}/lib/daos/TESTING/tests/test_group_np_srv --name selftest_srv_grp&
+
+sleep 5
+
+/usr/lib64/openmpi3/bin/orterun \
+    --mca btl self,tcp \
+    -N 1 \
+    --host ${nodes_array[0]} \
+    -x D_LOG_FILE_APPEND_PID=1 \
+    -x D_LOG_FILE=/var/tmp/daos_testing/self_test_np_sep_cart_client.log \
+    -x PATH \
+    -x D_LOG_MASK=WARN \
+    -x CRT_PHY_ADDR_STR=ofi+sockets \
+    -x OFI_INTERFACE=ib0 \
+    -x CRT_CTX_SHARE_ADDR=0 \
+    -x CRT_ATTACH_INFO_PATH=/home/standan/tmp/ \
+    -x CRT_CTX_NUM=16 \
+    self_test --group-name selftest_srv_grp \
+        --endpoint 0-3:0 \
+        --message-sizes "b2000,b2000 0,0 b2000,b2000 i1000,i1000
+        b2000,i1000,i1000 0,0 i1000,1,0" \
+        --max-inflight-rpcs 16 \
+        --repetitions 1000 \
+        -t \
+        -n
+
+killall -9 test_group_np_srv
+killall -9 crt_launch
+killall -9 self_test
+killall -9 orterun
