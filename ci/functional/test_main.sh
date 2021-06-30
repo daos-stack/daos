d93f14e049e993ac10d0130dd28e20396a95174a
Saurabh Tandan <saurabh.tandan@intel.com>
2021-06-30 19:13:53 +0000

    Skip-checkpatch: true RPM-test-version: 1.3.102 Skip-build: true Skip-unit-tests: true Skip-coverity-test: true Skip-func-test-vm: true Skip-func-hw-test-small: true Skip-func-hw-test-medium: true Skip-scan-centos-rpms: true Skip-test-centos-rpms: true
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- ci/functional/test_main.sh
+++ ci/functional/test_main.sh
@@ -39,7 +39,7 @@ mkdir "${STAGE_NAME:?ERROR: STAGE_NAME is not defined}/"
 
 
 # run network test
-clush -B -S -o '-i ci_key' -l root -w "${NODELIST[1]}" \
+clush -B -S -o '-i ci_key' -l root -w "${first_node}" \
     "source ci/functional/self_test_8-node.sh '/usr/' $tnodes"
 
 # set DAOS_TARGET_OVERSUBSCRIBE env here
