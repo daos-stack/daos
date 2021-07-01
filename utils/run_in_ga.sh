cd8642ef40a29aebc11846dc3fecee736b944dab
Ashley Pittman <ashley.m.pittman@intel.com>
2021-07-01 10:59:54 +0100

    Add repeat.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/run_in_ga.sh
+++ utils/run_in_ga.sh
@@ -57,3 +57,19 @@ echo ::group::Container copy test
 export LD_LIBRARY_PATH=/opt/daos/prereq/debug/spdk/lib/
 ./utils/node_local_test.py --no-root --test cont_copy
 echo ::endgroup::
+
+echo ::group::Container copy test2
+./utils/node_local_test.py --no-root --test cont_copy
+echo ::endgroup::
+
+echo ::group::Container copy test3
+./utils/node_local_test.py --no-root --test cont_copy
+echo ::endgroup::
+
+echo ::group::Container copy test4
+./utils/node_local_test.py --no-root --test cont_copy
+echo ::endgroup::
+
+echo ::group::Container copy test5
+./utils/node_local_test.py --no-root --test cont_copy
+echo ::endgroup::
