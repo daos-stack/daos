db520dd9f5dd15e0995c58a8c768c908e6c206c4
Makito Kano <makito.kano@intel.com>
2021-07-03 18:12:27 +0000

    DAOS-3769 test: Refactored
    Quick-build: true
    Skip-unit-tests: true
    Test-tag: stat_parameters
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/io/posix_stat.py
+++ src/tests/ftest/io/posix_stat.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python
 """
-  (C) Copyright 2020 Intel Corporation.
+  (C) Copyright 2018-2021 Intel Corporation.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-  GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
-  The Government's rights to use, modify, reproduce, release, perform, display,
-  or disclose this software are subject to the terms of the Apache License as
-  provided in Contract No. B609815.
-  Any reproduction of computer software, computer software documentation, or
-  portions thereof marked with this legend must also reproduce the markings.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 """
 from avocado.core.exceptions import TestFail
 
@@ -48,7 +31,8 @@ class POSIXStatTest(IorTestBase):
         Create files of 1M, 10M, 100M, 500M, and verify the size and creation
         time.
 
-        :avocado: tags=all,small,full_regression,stat_parameters
+        :avocado: tags=all,full_regression
+        :avocado: tags=stat_parameters
         """
         block_sizes = self.params.get("block_sizes", "/run/*")
         testfile_path = "/tmp/daos_dfuse/testfile"
