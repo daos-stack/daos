6dfd7df669353b1d198d4be116f9c3f6f4bc2c69
Makito Kano <makito.kano@intel.com>
2021-07-14 16:34:38 +0000

    DAOS-3769 test: Added one server
    Also removed the debug code.
    
    Quick-build: true
    Skip-unit-tests: true
    Test-tag: stat_parameters
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/io/posix_stat.py
+++ src/tests/ftest/io/posix_stat.py
@@ -11,10 +11,6 @@ from general_utils import get_remote_file_size, run_task
 from apricot import TestWithServers
 
 
-# Added
-#from test_utils_container import TestContainer
-
-
 class POSIXStatTest(IorTestBase):
     # pylint: disable=too-many-ancestors
     """Test class description:
@@ -124,48 +120,3 @@ class POSIXStatTest(IorTestBase):
         if error_list:
             self.fail("\n----- Errors detected! -----\n{}".format(
                 "\n".join(error_list)))
-
-# class SimpleCreateDeleteTest(TestWithServers):
-#     # pylint: disable=too-few-public-methods
-#     """Tests container basics including create, destroy, open, query and close.
-
-#     :avocado: recursive
-#     """
-
-#     def __init__(self, *args, **kwargs):
-#         """Initialize a SimpleCreateDeleteTest object."""
-#         super().__init__(*args, **kwargs)
-#         self.pool = None
-#         self.container = None
-
-#     def test_container_basics(self):
-#         """Test basic container create/destroy/open/close/query.
-
-#         :avocado: tags=all,container,pr,daily_regression,medium,basic_cont
-#         """
-#         # Create a pool
-#         self.log.info("Create a pool")
-#         self.prepare_pool()
-
-#         # Check that the pool was created
-#         self.assertTrue(
-#             self.pool.check_files(self.hostlist_servers),
-#             "Pool data was not created")
-
-#         # Create a container
-#         self.container = TestContainer(self.pool)
-#         self.container.get_params(self)
-#         self.container.create()
-
-#         # TO Be Done:the cont info needs update (see C version daos_cont_info_t)
-#         # Open and query the container.Verify the UUID & No of Snapshot.
-#         checks = {
-#             "ci_uuid": self.container.uuid,
-#             "ci_nsnapshots": 0}
-
-#         self.assertTrue(self.container.check_container_info(**checks),
-#                         "Error confirming container info from query")
-
-#         # Close and destroy the container
-#         self.container.close()
-#         self.container.destroy()
