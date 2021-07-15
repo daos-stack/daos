0f382062f05113320f0ddd0922607b2784c75262
Dalton Bohning <daltonx.bohning@intel.com>
2021-07-15 16:12:35 +0000

    iorsmall wolf-51,wolf-112,wolf-113
    Test-tag: iorsmall
    
    Skip-checkpatch: true
    Quick-Functional: true
    Skip-build-distro-leap15: true
    Skip-build-distro-ubuntu: true
    Skip-build-ubuntu20-rpm: true
    Skip-build-centos8-rpm: true
    Skip-build-leap15-rpm: true
    Skip-unit-tests: true
    Skip-coverity-test: true
    Skip-func-test-vm: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-scan-centos-rpms: true
    Skip-test-centos-rpms: true
    Skip-python-bandit: true
    
    Signed-off-by: Dalton Bohning <daltonx.bohning@intel.com>

--- ci/functional/test_main.sh
+++ ci/functional/test_main.sh
@@ -62,7 +62,8 @@ fi
 
 # Override tnodes for debugging which node(s) is the culprit
 # wolf-[51,110-117]
-tnodes="wolf-51,wolf-110,wolf-111"
+#tnodes="wolf-51,wolf-110,wolf-111"
+tnodes="wolf-51,wolf-112,wolf-113"
 
 # set DAOS_TARGET_OVERSUBSCRIBE env here
 export DAOS_TARGET_OVERSUBSCRIBE=1
