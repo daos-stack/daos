2733b6dfc137d2856a63d9627546c88cb3ff2e8c
Makito Kano <makito.kano@intel.com>
2021-07-29 18:38:14 +0000

    DAOS-7942 test: Refactored the test to use functions
    Use verify_destroy(), verify_create() from the tests.
    
    Create report_errors() functions in general_utils.py to support
    the common error list parsing task.
    
    Test-tag: create_invalid_labels duplicate_label_create duplicate_label_destroy
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/pool/label.py
+++ src/tests/ftest/pool/label.py
@@ -4,9 +4,12 @@
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
-from apricot import TestWithServers
-from command_utils import CommandFailure
+import string
+
+from apricot import TestWithServers, skipForTicket
 from avocado.core.exceptions import TestFail
+from general_utils import report_errors, get_random_string
+from command_utils_base import CommandFailure
 
 
 class Label(TestWithServers):
@@ -15,13 +18,46 @@ class Label(TestWithServers):
     :avocado: recursive
     """
 
-    def verify_input(self, label, failure_expected):
-        """Pool create and destroy with given label.
+    def verify_destroy(self, pool, failure_expected, use_dmg=False):
+        """Verify pool destroy works/not work as expected.
+
+        Args:
+            pool (TestPool): Pool to destroy.
+            failure_expected (bool): Whether failure is expected from pool
+                destroy.
+            use_dmg (bool): Whether to use dmg object. Defaults to False.
+
+        Returns:
+            list: List of errors.
+
+        """
+        errors = []
+
+        try:
+            if use_dmg:
+                pool.dmg.pool_destroy(pool=pool.label.value, force=1)
+            else:
+                pool.destroy()
+            if failure_expected:
+                error_message = "dmg pool destroy is expected to fail, " +\
+                    "but worked!"
+                errors.append(error_message)
+        except (TestFail, CommandFailure):
+            if not failure_expected:
+                error_message = "dmg pool destroy failed! "
+                self.log.info(error_message)
+                errors.append(error_message)
+
+        return errors
+
+    def verify_create(self, label, failure_expected, expected_error=None):
+        """Verify pool create with given label works/not work as expected.
 
         Args:
             label (str): Label.
             failure_expected (bool): Whether failure is expected from pool
-                create or destroy.
+                create.
+            expected_error (str): Expected error message. Defaults to None.
 
         Returns:
             list: List of errors.
@@ -33,104 +69,119 @@ class Label(TestWithServers):
         self.pool[-1].label.update(label)
 
         try:
+            self.pool[-1].dmg.exit_status_exception = False
             self.pool[-1].create()
-            if failure_expected:
+            result_stdout = str(self.pool[-1].dmg.result.stdout)
+            exit_status = self.pool[-1].dmg.result.exit_status
+
+            if  exit_status == 0 and failure_expected:
                 error_message = "dmg pool create is expected to fail, " +\
                     "but worked! {}".format(label)
                 errors.append(error_message)
-        except TestFail:
-            if not failure_expected:
-                error_message = "dmg pool create with {} failed! ".format(label)
-                self.log.info(error_message)
+            elif exit_status != 0 and not failure_expected:
+                error_message = "dmg pool create failed unexpectedly! " +\
+                    "{}".format(label)
+                errors.append(error_message)
+            elif (exit_status != 0
+                  and failure_expected
+                  and expected_error not in result_stdout):
+                # Failed for the wrong reason.
+                error_message = "dmg pool create failed for the wrong " +\
+                    "reason! Expected to exist = {}".format(expected_error)
                 errors.append(error_message)
 
-        if not failure_expected:
-            if not self.pool[-1].destroy():
-                error_message = "dmg pool destroy with {} failed! ".format(
-                    label)
-                self.log.info(error_message)
-                errors.append(error_message)
+        except TestFail as error:
+            errors.append(error)
+            self.log.info("dmg failed!")
+
+        finally:
+            self.pool[-1].dmg.exit_status_exception = True
 
         return errors
 
-    def check_errors(self, errors):
-        """Check if there's any error in given list. If so, fail the test.
-
-        Args:
-            errors (list): List of errors.
-
-        """
-        if errors:
-            self.fail("\n----- Errors detected! -----\n{}".format(
-                "\n".join(errors)))
-
-    def test_various_labels(self):
+    @skipForTicket("DAOS-8183")
+    def test_valid_labels(self):
         """Test ID: DAOS-7942
 
-        Test Description: Create and destroy pool with the following.
-        1. UUID format string: 23ab123e-5296-4f95-be14-641de40b4d5a
-        2. Long label - 126 chars. Should pass.
-        3. Long label - 127 chars. Should pass.
-        4. Long label - 128 chars. Should fail.
+        Test Description: Create and destroy pool with the following labels.
+        * Random alpha numeric string of length 126.
+        * Random alpha numeric string of length 127.
+        * Random alpha numeric string of length 128.
+        * Random upper case string of length 50.
+        * Random lower case string of length 50.
+        * Random number string of length 50.
 
         :avocado: tags=all,full_regression
         :avocado: tags=small
-        :avocado: tags=pool,create_various_labels
+        :avocado: tags=pool,test_valid_labels
         """
         self.pool = []
         errors = []
+        labels = [
+            get_random_string(126),
+            get_random_string(127),
+            get_random_string(128),
+            get_random_string(length=50, include=string.ascii_uppercase),
+            get_random_string(length=50, include=string.ascii_lowercase),
+            get_random_string(length=50, include=string.digits)
+        ]
 
-        uuid_format = "23ab123e-5296-4f95-be14-641de40b4d5a"
-        long_label_126 = "1234567890" * 12 + "123456"
-        long_label_127 = "1234567890" * 12 + "1234567"
-        long_label_128 = "1234567890" * 12 + "12345678"
+        for label in labels:
+            errors.extend(self.verify_create(label, False))
+            errors.extend(self.verify_destroy(self.pool[-1], False))
 
-        errors.extend(self.verify_input(uuid_format, True))
-        errors.extend(self.verify_input(long_label_126, False))
-        # DAOS-8183
-        # errors.extend(self.verify_input(long_label_127, False))
-        errors.extend(self.verify_input(long_label_128, True))
+        report_errors(self, errors)
 
-        self.check_errors(errors)
+    def test_invalid_labels(self):
+        """Test ID: DAOS-7942
+
+        Test Description: Create pool with following invalid labels.
+        * UUID format string: 23ab123e-5296-4f95-be14-641de40b4d5a
+        * Long label - 129 random chars.
+
+        :avocado: tags=all,full_regression
+        :avocado: tags=small
+        :avocado: tags=pool,create_invalid_labels
+        """
+        self.pool = []
+        errors = []
+        label_outs = [
+            ("23ab123e-5296-4f95-be14-641de40b4d5a", "invalid label"),
+            (get_random_string(129), "value too long")
+        ]
+
+        for label_out in label_outs:
+            errors.extend(self.verify_create(label_out[0], True, label_out[1]))
+
+        report_errors(self, errors)
 
     def test_duplicate_create(self):
         """Test ID: DAOS-7942
 
         Test Description:
-        1. Create a pool with the same label twice. Second create should fail.
-        2. Destroy it.
-        3. Create a pool with the same label again. It should work this time.
-
-        Note that we can't use TestPool in this test because the create() does
-        destroy, so the duplicate create would always work.
+        1. Create a pool with a label.
+        2. Create another pool with the same label. Should fail.
+        3. Destroy the pool.
+        4. Create a pool with the same label again. It should work this time.
 
         :avocado: tags=all,full_regression
         :avocado: tags=small
         :avocado: tags=pool,duplicate_label_create
         """
-        dmg = self.get_dmg_command()
+        self.pool = []
         label = "TestLabel"
-        errors = []
 
-        dmg.pool_create(scm_size="1G", label=label)
+        # Step 1
+        report_errors(self, self.verify_create(label, False))
 
-        try:
-            dmg.pool_create(scm_size="1G", label=label)
-            error_msg = "dmg pool create with duplicate label worked!"
-            errors.append(error_msg)
-        except CommandFailure:
-            self.log.info(
-                "dmg pool create with duplicate label failed as expected")
+        # Step 2
+        report_errors(self, self.verify_create(label, True, "already exists"))
 
-        dmg.pool_destroy(pool=label)
+        # Step 3
+        report_errors(self, self.verify_destroy(self.pool[0], False))
 
-        try:
-            dmg.pool_create(scm_size="1G", label=label)
-        except CommandFailure:
-            error_msg = "dmg pool recreate with the same label failed!"
-            errors.append(error_msg)
-
-        self.check_errors(errors)
+        # Step 4
+        report_errors(self, self.verify_create(label, False))
 
     def test_duplicate_destroy(self):
         """Test ID: DAOS-7942
@@ -140,26 +191,17 @@ class Label(TestWithServers):
         2. Destroy it with the label.
         3. Destroy it with the label again. The second destroy should fail.
 
-        Note that we don't use TestPool in this test because destory() checks if
-        self.pool is None, but we just want to call dmg without this pre-check.
-
         :avocado: tags=all,full_regression
         :avocado: tags=small
         :avocado: tags=pool,duplicate_label_destroy
         """
-        dmg = self.get_dmg_command()
-        label = "TestLabel"
-        errors = []
+        self.pool = []
 
-        dmg.pool_create(scm_size="1G", label=label)
+        # Step 1
+        report_errors(self, self.verify_create("TestLabel", False))
 
-        dmg.pool_destroy(pool=label)
+        # Step 2
+        report_errors(self, self.verify_destroy(self.pool[-1], False))
 
-        try:
-            dmg.pool_destroy(pool=label)
-            error_message = "Duplicate destroy succeeded!"
-            errors.append(error_message)
-        except CommandFailure:
-            self.log.info("Duplicate destroy failed as expected")
-
-        self.check_errors(errors)
+        # Step 3
+        report_errors(self, self.verify_destroy(self.pool[-1], True, True))
