edf3f7eb29f631c3229a43032ebc700c9fed318b
Bruno Faccini <bruno.faccini@intel.com>
2021-08-03 17:50:52 +0200

    DAOS-4183 ci: can not sysctl in docker
    Remove previous change trying to sysctl in docker run
    of NLT smoke test, as it is forbidden in containers ...
    
    Change-Id: I48d9976d46d7bae2e2955c0b7c0433857234c499
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>

--- utils/docker_nlt.sh
+++ utils/docker_nlt.sh
@@ -10,6 +10,4 @@ set -e
 
 ./utils/setup_daos_admin.sh
 
-# using mmap()'ed ULT stacks requires to bump system default
-sysctl vm.max_map_count=1000000
 ./utils/node_local_test.py --no-root --memcheck no --server-debug WARN "$@"
