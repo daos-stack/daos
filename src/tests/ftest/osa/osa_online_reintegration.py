325a6cbde2299ee63caaf576ab09806c3cf0281a
rpadma2 <ravindran.padmanabhan@intel.com>
2021-08-09 10:42:57 -0400

    DAOS-8276 test: Reenable online reintegration test Test-tag-hw-medium: pr,hw,medium,ib2 online_reintegration
    Summary:
    The following tests are re-enabled:
    - test_osa_online_reintegration_with_aggregation
    - test_osa_online_reintegration_oclass
    
    Signed-off-by: rpadma2 <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/osa/osa_online_reintegration.py
+++ src/tests/ftest/osa/osa_online_reintegration.py
@@ -207,7 +207,6 @@ class OSAOnlineReintegration(OSAUtils):
                                                   '/run/checksum/*')
         self.run_online_reintegration_test(1)
 
-    @skipForTicket("DAOS-7996")
     def test_osa_online_reintegration_with_aggregation(self):
         """Test ID: DAOS-6715
         Test Description: Reintegrate rank while aggregation
@@ -223,7 +222,6 @@ class OSAOnlineReintegration(OSAUtils):
         self.log.info("Online Reintegration : Aggregation")
         self.run_online_reintegration_test(1)
 
-    @skipForTicket("DAOS-7996")
     def test_osa_online_reintegration_oclass(self):
         """Test ID: DAOS-6715
         Test Description: Reintegrate rank with different
