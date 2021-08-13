b5856e6b3ff3424ba608645dd24b89d51825d2fd
Dalton Bohning <daltonx.bohning@intel.com>
2021-08-13 19:21:54 +0000

    DAOS-8293 dfs: dfs_sys should not set size = -1
    Test-tag: pr fs_copy dcp
    
    Don't set size = -1 for daos_size_t types, since it is unsigned.
    The rc is set appropriately anyway, so this is redundant.
    
    Signed-off-by: Dalton Bohning <daltonx.bohning@intel.com>

--- src/client/dfs/dfs_sys.c
+++ src/client/dfs/dfs_sys.c
@@ -815,10 +815,8 @@ dfs_sys_listxattr(dfs_sys_t *dfs_sys, const char *path, char *list,
 
 listxattr:
 	rc = dfs_listxattr(dfs_sys->dfs, obj, list, &got_size);
-	if (rc != 0) {
-		*size = -1;
+	if (rc != 0)
 		D_GOTO(out_free_obj, rc);
-	}
 
 	if (*size < got_size)
 		rc = ERANGE;
@@ -871,10 +869,8 @@ dfs_sys_getxattr(dfs_sys_t *dfs_sys, const char *path, const char *name,
 
 getxattr:
 	rc = dfs_getxattr(dfs_sys->dfs, obj, name, value, &got_size);
-	if (rc != 0) {
-		*size = -1;
+	if (rc != 0)
 		D_GOTO(out_free_obj, rc);
-	}
 
 	if (*size < got_size)
 		rc = ERANGE;
@@ -1012,8 +1008,6 @@ dfs_sys_readlink(dfs_sys_t *dfs_sys, const char *path, char *buf,
 	}
 
 	rc = dfs_get_symlink_value(obj, buf, size);
-	if (rc != 0)
-		*size = -1;
 
 	dfs_release(obj);
 
@@ -1143,7 +1137,6 @@ int
 dfs_sys_read(dfs_sys_t *dfs_sys, dfs_obj_t *obj, void *buf, daos_off_t off,
 	     daos_size_t *size, daos_event_t *ev)
 {
-	int		rc;
 	d_iov_t		iov;
 	d_sg_list_t	sgl;
 
@@ -1159,18 +1152,13 @@ dfs_sys_read(dfs_sys_t *dfs_sys, dfs_obj_t *obj, void *buf, daos_off_t off,
 	sgl.sg_iovs = &iov;
 	sgl.sg_nr_out = 1;
 
-	rc = dfs_read(dfs_sys->dfs, obj, &sgl, off, size, ev);
-	if (rc != 0)
-		*size = -1;
-
-	return rc;
+	return dfs_read(dfs_sys->dfs, obj, &sgl, off, size, ev);
 }
 
 int
 dfs_sys_write(dfs_sys_t *dfs_sys, dfs_obj_t *obj, const void *buf,
 	      daos_off_t off, daos_size_t *size, daos_event_t *ev)
 {
-	int		rc;
 	d_iov_t		iov;
 	d_sg_list_t	sgl;
 
@@ -1186,11 +1174,7 @@ dfs_sys_write(dfs_sys_t *dfs_sys, dfs_obj_t *obj, const void *buf,
 	sgl.sg_iovs = &iov;
 	sgl.sg_nr_out = 1;
 
-	rc = dfs_write(dfs_sys->dfs, obj, &sgl, off, ev);
-	if (rc != 0)
-		*size = -1;
-
-	return rc;
+	return dfs_write(dfs_sys->dfs, obj, &sgl, off, ev);
 }
 
 int
