03a71084fff6dc8e256d47e384ac7ec0d598df59
Saurabh Tandan <saurabh.tandan@intel.com>
2021-08-19 16:55:49 +0000

    DAOS-6325 test: Revert DAOS-8207(#6464) keeping test enablement
    Reverting the changes landed as part of DAOS-8207(#6464)
    keeping the vol test enabling changes
    
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/daos_vol/daos_vol.py
+++ src/tests/ftest/daos_vol/daos_vol.py
@@ -14,7 +14,6 @@ class DaosVol(VolTestBase):
     :avocado: recursive
     """
 
-    @skipForTicket("DAOS-8207")
     def test_daos_vol_mpich(self):
         """Jira ID: DAOS-3656.
 
@@ -43,7 +42,6 @@ class DaosVol(VolTestBase):
         self.run_test(
             "/usr/lib64/mpich/lib", "/usr/lib64/hdf5_vol_daos/mpich/tests")
 
-    @skipForTicket("DAOS-8207")
     def test_daos_vol_openmpi(self):
         """Jira ID: DAOS-3656.
 
