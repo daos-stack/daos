e7f94b70f2b74e02f4fd675c3f4408a8583caf6a
Ashley Pittman <ashley.m.pittman@intel.com>
2021-08-20 11:24:02 +0100

    Remove check for pool handle size in ioctl query.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/client/dfuse/ops/ioctl.c
+++ src/client/dfuse/ops/ioctl.c
@@ -55,8 +55,6 @@ handle_size_ioctl(struct dfuse_obj_hdl *oh, fuse_req_t req)
 		D_GOTO(err, rc = daos_der2errno(rc));
 
 	hs_reply.fsr_pool_size = iov.iov_buf_len;
-	if (hs_reply.fsr_pool_size > MAX_IOCTL_SIZE)
-		D_GOTO(err, rc = EOVERFLOW);
 
 	rc = daos_cont_local2global(oh->doh_ie->ie_dfs->dfs_coh, &iov);
 	if (rc)
