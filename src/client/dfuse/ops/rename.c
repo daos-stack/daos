daos-stack/daos

commit dc7a4f24f7e260cd25e425a265c4e68e341219f6

DAOS-6540 dfuse: Detect clobbered files and update kernel accordingly. (#6154)
Use the oid option to dfs when calling move or rename to detect
what if any file has been removed, and check for that file in
the local inode table.
When files have been moved or are observed at a different location
to what dfuse expects then update the kernel to remove the old
dentries which should reduce kernel resources required.
When files are removed then check if the file is known and if so
mark is as removed.
For getattr on removed files return most recent data rather than error.
For setattr on removed files allow setting of times only as this can
happen with cached writes.

Update the open_rename() and others tests now that it's expected to pass.

Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

src/client/dfuse/ops/rename.c
@@ -7,12 +7,65 @@
 #include "dfuse_common.h"
 #include "dfuse.h"
 
+/* Handle a file that has been moved.
+ *
+ * Dfuse may not be aware of this file, but if it is find the inode and update it for the new
+ * location.
+ */
+
+static void
+dfuse_oid_moved(struct dfuse_projection_info *fs_handle, daos_obj_id_t *oid,
+		struct dfuse_inode_entry *parent, const char *name,
+		struct dfuse_inode_entry *newparent,
+		const char *newname)
+{
+	struct dfuse_inode_entry	*ie;
+	d_list_t			*rlink;
+	int				rc;
+	ino_t ino;
+
+	dfuse_compute_inode(parent->ie_dfs, oid, &ino);
+
+	DFUSE_TRA_DEBUG(fs_handle, "Renamed file was %#lx", ino);
+
+	rlink = d_hash_rec_find(&fs_handle->dpi_iet, &ino, sizeof(ino));
+	if (!rlink)
+		return;
+
+	ie = container_of(rlink, struct dfuse_inode_entry, ie_htl);
+
+	/* If the move is not from where we thought the file was then invalidate the old entry */
+	if ((ie->ie_parent != parent->ie_stat.st_ino) ||
+		(strncmp(ie->ie_name, name, NAME_MAX + 1) != 0)) {
+		DFUSE_TRA_DEBUG(ie, "Invalidating old name");
+
+		rc = fuse_lowlevel_notify_inval_entry(fs_handle->dpi_info->di_session,
+						      ie->ie_parent,
+						      ie->ie_name, strnlen(ie->ie_name, NAME_MAX));
+
+		if (rc)
+			DFUSE_TRA_ERROR(ie, "inval_entry returned %d: %s", rc, strerror(-rc));
+	}
+
+	/* Update the inode entry data */
+	ie->ie_parent = newparent->ie_parent;
+	strncpy(ie->ie_name, newname, NAME_MAX);
+
+	/* Set the new parent and name */
+	dfs_update_parentfd(ie->ie_obj, newparent->ie_obj, newname);
+
+	/* Drop the ref again */
+	d_hash_rec_decref(&fs_handle->dpi_iet, rlink);
+}
+
 void
 dfuse_cb_rename(fuse_req_t req, struct dfuse_inode_entry *parent,
 		const char *name, struct dfuse_inode_entry *newparent,
 		const char *newname, unsigned int flags)
 {
 	struct dfuse_projection_info	*fs_handle;
+	daos_obj_id_t			moid = {};
+	daos_obj_id_t			oid = {};
 	int				rc;
 
 	fs_handle = fuse_req_userdata(req);
@@ -23,27 +76,23 @@ dfuse_cb_rename(fuse_req_t req, struct dfuse_inode_entry *parent,
 	if (!newparent)
 		newparent = parent;
 
-	rc = dfs_move(parent->ie_dfs->dfs_ns, parent->ie_obj, (char *)name,
-		      newparent->ie_obj, (char *)newname, NULL);
+	rc = dfs_move_internal(parent->ie_dfs->dfs_ns, parent->ie_obj, (char *)name,
+			       newparent->ie_obj, (char *)newname, &moid, &oid);
 	if (rc)
 		D_GOTO(out, rc);
 
-	DFUSE_TRA_INFO(parent, "Renamed %s to %s in %p",
+	DFUSE_TRA_INFO(newparent, "Renamed '%s' to '%s' in %p",
 		       name, newname, newparent);
 
-	DFUSE_REPLY_ZERO(parent, req);
+	/* update moid */
+	dfuse_oid_moved(fs_handle, &moid, parent, name, newparent, newname);
 
-	if (parent->ie_dfs->dfc_dentry_timeout ||
-		parent->ie_dfs->dfc_dentry_dir_timeout ||
-		parent->ie_dfs->dfc_ndentry_timeout) {
-		/* The caller has a reference on newparent */
-		rc = fuse_lowlevel_notify_inval_entry(fs_handle->dpi_info->di_session,
-						      newparent->ie_stat.st_ino,
-						      newname, strnlen(newname, NAME_MAX));
-		if (rc)
-			DFUSE_TRA_ERROR(parent,
-					"inval_entry failed %d", rc);
-	}
+	/* Check if a file was unlinked and see if anything needs updating */
+	if (oid.lo || oid.hi)
+		dfuse_oid_unlinked(fs_handle, req, &oid, newparent, newname);
+	else
+		DFUSE_REPLY_ZERO(newparent, req);
+
 	return;
 
 out:

src/client/dfs/dfs.c
@@ -2952,7 +2952,6 @@ dfs_open_stat(dfs_t *dfs, dfs_obj_t *parent, const char *name, mode_t mode,
 	daos_size_t		file_size = 0;
 	int			rc;
 
-
 	if (dfs == NULL || !dfs->mounted)
 		return EINVAL;
 	if ((dfs->amode != O_RDWR) && (flags & O_CREAT))
@@ -3575,6 +3574,17 @@ dfs_update_parent(dfs_obj_t *obj, dfs_obj_t *src_obj, const char *name)
 	return 0;
 }
 
+/* Update a in-memory object to a new parent, taking the parent directly */
+void
+dfs_update_parentfd(dfs_obj_t *obj, dfs_obj_t *new_parent, const char *name)
+{
+	oid_cp(&obj->parent_oid, new_parent->oid);
+
+	D_ASSERT(name);
+	strncpy(obj->name, name, DFS_MAX_NAME);
+	obj->name[DFS_MAX_NAME] = '\0';
+}
+
 int
 dfs_stat(dfs_t *dfs, dfs_obj_t *parent, const char *name, struct stat *stbuf)
 {
@@ -4164,9 +4174,10 @@ out:
 	return rc;
 }
 
+/* Returns oids for both moved and clobbered files, but does not check either of them */
 int
-dfs_move(dfs_t *dfs, dfs_obj_t *parent, char *name, dfs_obj_t *new_parent,
-	 char *new_name, daos_obj_id_t *oid)
+dfs_move_internal(dfs_t *dfs, dfs_obj_t *parent, char *name, dfs_obj_t *new_parent, char *new_name,
+		  daos_obj_id_t *moid, daos_obj_id_t *oid)
 {
 	struct dfs_entry	entry = {0}, new_entry = {0};
 	daos_handle_t		th = DAOS_TX_NONE;
@@ -4218,6 +4229,9 @@ restart:
 	}
 	if (exists == false)
 		D_GOTO(out, rc = ENOENT);
+
+	if (moid)
+		oid_cp(moid, entry.oid);
 
 	rc = fetch_entry(new_parent->oh, th, new_name, new_len, true, &exists,
 			 &new_entry, 0, NULL, NULL, NULL);
@@ -4344,6 +4358,14 @@ out:
 		D_FREE(new_entry.value);
 	}
 	return rc;
+}
+
+/* Wrapper function, only permit oid as a input parameter and return if it has data */
+int
+dfs_move(dfs_t *dfs, dfs_obj_t *parent, char *name, dfs_obj_t *new_parent,
+	 char *new_name, daos_obj_id_t *oid)
+{
+	return dfs_move_internal(dfs, parent, name, new_parent, new_name, NULL, oid);
 }
 
 int

src/client/dfs/dfs_internal.h
@@ -53,6 +53,18 @@ dfs_lookupx(dfs_t *dfs, dfs_obj_t *parent, const char *name, int flags,
 	    dfs_obj_t **obj, mode_t *mode, struct stat *stbuf, int xnr,
 	    char *xnames[], void *xvals[], daos_size_t *xsizes);
 
+/* moid is moved oid, oid is clobbered file.
+ * This isn't yet fully compatible with dfuse because we also want to pass in a flag for if the
+ * destination exists.
+ */
+int
+dfs_move_internal(dfs_t *dfs, dfs_obj_t *parent, char *name, dfs_obj_t *new_parent, char *new_name,
+		  daos_obj_id_t *moid, daos_obj_id_t *oid);
+
+/* Set the in-memory parent, but takes the parent, rather than another file object */
+void
+dfs_update_parentfd(dfs_obj_t *obj, dfs_obj_t *new_parent, const char *name);
+
 /** update chunk size and oclass of obj with the ones from new_obj */
 void
 dfs_obj_copy_attr(dfs_obj_t *dst_obj, dfs_obj_t *src_obj);

src/client/dfuse/dfuse.h
@@ -531,6 +531,9 @@ struct dfuse_inode_entry {
 	bool			ie_truncated;
 
 	bool			ie_root;
+
+	/** File has been unlinked from daos */
+	bool			ie_unlinked;
 };
 
 /* Generate the inode to use for this dfs object.  This is generating a single
@@ -664,6 +667,11 @@ dfuse_reply_entry(struct dfuse_projection_info *fs_handle,
 		  struct fuse_file_info *fi_out,
 		  bool is_new,
 		  fuse_req_t req);
+
+/* Mark object as removed and invalidate any kernel data for it */
+void
+dfuse_oid_unlinked(struct dfuse_projection_info *fs_handle, fuse_req_t req, daos_obj_id_t *oid,
+		   struct dfuse_inode_entry *parent, const char *name);
 
 /* dfuse_cont.c */
 void

src/client/dfuse/dfuse_core.c
@@ -193,7 +193,7 @@ ih_free(struct d_hash_table *htable, d_list_t *rlink)
 
 	ie = container_of(rlink, struct dfuse_inode_entry, ie_htl);
 
-	DFUSE_TRA_DEBUG(ie, "parent %lu", ie->ie_parent);
+	DFUSE_TRA_DEBUG(ie, "parent %#lx", ie->ie_parent);
 	dfuse_ie_close(fs_handle, ie);
 }
 

src/client/dfuse/ops/fgetattr.c
@@ -13,6 +13,12 @@ dfuse_cb_getattr(fuse_req_t req, struct dfuse_inode_entry *ie)
 	struct stat	stat = {};
 	int		rc;
 
+	if (ie->ie_unlinked) {
+		DFUSE_TRA_DEBUG(ie, "File is unlinked, returning most recent data");
+		DFUSE_REPLY_ATTR(ie, req, &ie->ie_stat);
+		return;
+	}
+
 	rc = dfs_ostat(ie->ie_dfs->dfs_ns, ie->ie_obj, &stat);
 	if (rc != 0)
 		D_GOTO(err, rc);

src/client/dfuse/ops/lookup.c
@@ -20,6 +20,8 @@ dfuse_reply_entry(struct dfuse_projection_info *fs_handle,
 {
 	struct fuse_entry_param	entry = {0};
 	d_list_t		*rlink;
+	ino_t			wipe_parent = 0;
+	char			wipe_name[NAME_MAX + 1];
 	int			rc;
 
 	D_ASSERT(ie->ie_parent);
@@ -97,17 +99,20 @@ dfuse_reply_entry(struct dfuse_projection_info *fs_handle,
 
 		if (ie->ie_stat.st_ino == ie->ie_dfs->dfs_ino) {
 			DFUSE_TRA_DEBUG(inode, "Not updating parent");
-		} else {
-			rc = dfs_update_parent(inode->ie_obj, ie->ie_obj,
-					       ie->ie_name);
-			if (rc != 0)
-				DFUSE_TRA_ERROR(inode,
-						"dfs_update_parent() failed %d",
-						rc);
-		}
-		inode->ie_parent = ie->ie_parent;
-		strncpy(inode->ie_name, ie->ie_name, NAME_MAX + 1);
+		} else if ((inode->ie_parent != ie->ie_parent) ||
+			(strncmp(inode->ie_name, ie->ie_name, NAME_MAX + 1) != 0)) {
+			DFUSE_TRA_DEBUG(inode, "File has moved from '%s to '%s'",
+					inode->ie_name, ie->ie_name);
 
+			dfs_update_parent(inode->ie_obj, ie->ie_obj, ie->ie_name);
+
+			/* Save the old name so that we can invalidate it in later */
+			wipe_parent = inode->ie_parent;
+			strncpy(wipe_name, inode->ie_name, NAME_MAX + 1);
+
+			inode->ie_parent = ie->ie_parent;
+			strncpy(inode->ie_name, ie->ie_name, NAME_MAX + 1);
+		}
 		atomic_fetch_sub_relaxed(&ie->ie_ref, 1);
 		dfuse_ie_close(fs_handle, ie);
 		ie = inode;
@@ -117,6 +122,15 @@ dfuse_reply_entry(struct dfuse_projection_info *fs_handle,
 		DFUSE_REPLY_CREATE(ie, req, entry, fi_out);
 	else
 		DFUSE_REPLY_ENTRY(ie, req, entry);
+
+	if (wipe_parent == 0)
+		return;
+
+	rc = fuse_lowlevel_notify_inval_entry(fs_handle->dpi_info->di_session, wipe_parent,
+					      wipe_name, strnlen(wipe_name, NAME_MAX));
+	if (rc)
+		DFUSE_TRA_ERROR(ie, "inval_entry returned %d: %s", rc, strerror(-rc));
+
 	return;
 out_err:
 	DFUSE_REPLY_ERR_RAW(fs_handle, req, rc);

src/client/dfuse/ops/setattr.c
@@ -16,6 +16,26 @@ dfuse_cb_setattr(fuse_req_t req, struct dfuse_inode_entry *ie,
 
 	DFUSE_TRA_DEBUG(ie, "flags %#x", to_set);
 
+	if (ie->ie_unlinked) {
+		DFUSE_TRA_DEBUG(ie, "File is unlinked, returning most recent data");
+
+		/* This will happen on close with caching enabled if there are writes through the
+		 * cache so accept these two entries only and reject anything else.  This allows
+		 * the read/write case to work on unlinked files without triggering an error.
+		 */
+		if (to_set & ~(FUSE_SET_ATTR_MTIME | FUSE_SET_ATTR_CTIME))
+			D_GOTO(err, rc = ENOENT);
+
+		if (to_set & FUSE_SET_ATTR_MTIME)
+			ie->ie_stat.st_mtim = attr->st_mtim;
+
+		if (to_set & FUSE_SET_ATTR_CTIME)
+			ie->ie_stat.st_ctim = attr->st_ctim;
+
+		DFUSE_REPLY_ATTR(ie, req, &ie->ie_stat);
+		return;
+	}
+
 	if (to_set & (FUSE_SET_ATTR_UID | FUSE_SET_ATTR_GID)) {
 		DFUSE_TRA_INFO(ie, "File uid/gid support not enabled");
 		D_GOTO(err, rc = ENOTSUP);

commit 20e3bbbb31dd37cb9d723d7e283712850de1042e

DAOS-8424 dfuse: fix to update parent inode num for rename
We intent to udpate parent inode num with new parent inode number.
but it was updated as parent's parent inode number.

Fixes: dc7a4f24f7e ("DAOS-6540 dfuse: Detect clobbered files and update kernel accordingly")
Signed-off-by: Wang Shilong <shilong.wang@intel.com>

src/client/dfuse/ops/rename.c
@@ -48,7 +48,7 @@ dfuse_oid_moved(struct dfuse_projection_info *fs_handle, daos_obj_id_t *oid,
 	}
 
 	/* Update the inode entry data */
-	ie->ie_parent = newparent->ie_parent;
+	ie->ie_parent = newparent->ie_stat.st_ino;
 	strncpy(ie->ie_name, newname, NAME_MAX);
 
 	/* Set the new parent and name */