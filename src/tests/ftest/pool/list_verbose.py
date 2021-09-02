cac967ee3e1055cd1d2cd0ff0b170bad7fa45a1b
Makito Kano <makito.kano@intel.com>
2021-09-02 13:56:24 +0000

    DAOS-8267-2 test: Support pool list comparison
    We can call dmg pool list and verify all the pools
    instead of one at a time.
    
    Skip-unit-tests: true
    Test-tag: list_verbose_basic list_verbose_imbalance
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/pool/list_verbose.py
+++ src/tests/ftest/pool/list_verbose.py
@@ -5,7 +5,6 @@
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
 from ior_test_base import IorTestBase
-from ior_utils import IorCommand
 
 
 # pylint: disable=too-many-ancestors
@@ -24,9 +23,9 @@ class ListVerboseTest(IorTestBase):
     :avocado: recursive
     """
 
-    def create_expected(self, pool, scm_free, nvme_free, targets_total=None,
-                        targets_disabled=0, scm_size=None, scm_imbalance=0,
-                        nvme_size=None, nvme_imbalance=0):
+    def create_expected(self, pool, scm_free, nvme_free, scm_imbalance,
+                        nvme_imbalance, targets_disabled=0, scm_size=None,
+                        nvme_size=None):
         """Create expected dmg pool list output to compare against the actual.
 
         Args:
@@ -35,29 +34,32 @@ class ListVerboseTest(IorTestBase):
                 isn't easy.
             nvme_free (int): NVMe free from the actual since direct comparison
                 isn't easy.
-            targets_total (int, optional): Targets total to fill in the output.
-                Defaults to None.
+            scm_imbalance (int): SCM imbalance to fill in the output.
+            nvme_imbalance (int): NVMe imbalance to fill in the output.
             targets_disabled (int, optional): Targets total to fill in the
-                output. Defaults to None.
+                output. Defaults to 0.
             scm_size (int, optional): SCM size to fill in the output. Defaults
                 to None.
-            scm_imbalance (int, optional): SCM imbalance to fill in the output.
-                Defaults to 0.
             nvme_size (int, optional): NVMe size to fill in the output. Defaults
                 to None.
-            nvme_imbalance (int, optional): NVMe imbalance to fill in the
-                output. Defaults to 0.
 
         Returns:
             dict: Expected in the same format of actual.
 
         """
-        if targets_total is None:
-            targets_total = self.server_managers[0].get_config_value("targets")
+        # Prepare to calculate the size.
+        rank_count = len(pool.target_list.value)
+        # self.log.debug(
+        #     "## Rank count = {}".format(rank_count))
+
         if scm_size is None:
-            scm_size = pool.scm_size.value
+            scm_size = pool.scm_size.value * rank_count
         if nvme_size is None:
-            nvme_size = pool.nvme_size.value
+            nvme_size = pool.nvme_size.value * rank_count
+
+        targets_total = self.server_managers[0].get_config_value(
+            "targets") * rank_count
+        # self.log.debug("## targets_total = {}".format(targets_total))
 
         return {
             "uuid": pool.uuid.lower(),
@@ -84,73 +86,99 @@ class ListVerboseTest(IorTestBase):
         }
 
     @staticmethod
-    def get_scm_nvme_free(pool_list_out):
-        """Get SCM and NVMe free values.
+    def get_scm_nvme_free_imbalance(pool_list_out):
+        """Get SCM and NVMe free and imbalance.
 
         Args:
-            pool_list_out (dict): pool list output dictionary to obtain the free
-                values from.
+            pool_list_out (list): pool list output list of dictionaries to
+                obtain the free values from.
 
         Returns:
-            tuple: SCM free and actual NVMe free values.
+            dict: {uuid: {"scm": val, "nvme": val, "scm_imbalance": val,
+                "nvme_imbalance": val}, uuid:...}
+
         """
-        scm_free = -1
-        nvme_free = -1
-        for usage in pool_list_out["usage"]:
-            if usage["tier_name"] == "SCM":
-                scm_free = usage["free"]
-            elif usage["tier_name"] == "NVME":
-                nvme_free = usage["free"]
+        output = {}
 
-        return (scm_free, nvme_free)
+        for pool in pool_list_out:
+            scm_free = -1
+            nvme_free = -1
+            scm_imbalance = -1
+            nvme_imbalance = -1
+            for usage in pool["usage"]:
+                if usage["tier_name"] == "SCM":
+                    scm_free = usage["free"]
+                    scm_imbalance = usage["imbalance"]
+                elif usage["tier_name"] == "NVME":
+                    nvme_free = usage["free"]
+                    nvme_imbalance = usage["imbalance"]
 
-    def verify_two_pools(self, targets_disabled_1=0, scm_size_1=None,
-                         nvme_size_1=None):
-        """Verify two pools by comparing expected and actual dictionaries.
+            output[pool["uuid"]] = {
+                "scm": scm_free,
+                "nvme": nvme_free,
+                "scm_imbalance": scm_imbalance,
+                "nvme_imbalance": nvme_imbalance
+            }
+
+        return output
+
+    def verify_pool_lists(self, targets_disabled, scm_size, nvme_size):
+        """Call dmg pool list and verify.
+
+        self.pool should be a list. The elements of the inputs should
+        correspond to the pools in self.pool.
 
         Args:
-            targets_disabled_1 (int, optional): Targets disabled for pool 1.
-                Defaults to 0.
-            scm_size_1 (int, optional): SCM size for pool 1. Defaults to None.
-            nvme_size_1 (int, optional): NVMe size for pool 1. Defaults to None.
+            targets_disabled (list): List of targets disabled for pools.
+            scm_size (list): List of SCM size for pools.
+            nvme_size (list): List of NVMe size for pools.
         """
-        actual_pools = self.pool[0].dmg.get_pool_list_all(verbose=True)
+        expected_pools = []
 
-        # Get the dict of 1 and 2. Assume that the first two are what we want.
-        if actual_pools[0]["uuid"] == self.pool[0].uuid.lower():
-            actual_pool_1 = actual_pools[0]
-            actual_pool_2 = actual_pools[1]
-        else:
-            actual_pool_1 = actual_pools[1]
-            actual_pool_2 = actual_pools[0]
+        actual_pools = self.get_dmg_command().get_pool_list_all(verbose=True)
 
-        # Get the free values and create expected for pool 1.
-        actual_scm_free_1, actual_nvme_free_1 = self.get_scm_nvme_free(
-            actual_pool_1)
+        # Get free and imbalance from actual so that we can use them in
+        # expected.
+        free_data = self.get_scm_nvme_free_imbalance(actual_pools)
 
-        expected_pool_1 = self.create_expected(
-            pool=self.pool[0], scm_free=actual_scm_free_1,
-            nvme_free=actual_nvme_free_1, targets_disabled=targets_disabled_1,
-            scm_size=scm_size_1, nvme_size=nvme_size_1)
+        # Create expected_pools. Use data from actual and the parameters.
+        for index, pool in enumerate(self.pool):
+            pool_free_data = free_data[pool.uuid.lower()]
+            expected_pools.append(
+                self.create_expected(
+                    pool=pool, scm_free=pool_free_data["scm"],
+                    nvme_free=pool_free_data["nvme"],
+                    scm_imbalance=pool_free_data["scm_imbalance"],
+                    nvme_imbalance=pool_free_data["nvme_imbalance"],
+                    targets_disabled=targets_disabled[index],
+                    scm_size=scm_size[index], nvme_size=nvme_size[index]))
 
-        # Compare expected and actual data for pool 1.
-        self.assertDictEqual(expected_pool_1, actual_pool_1)
+        # Sort pools by UUID.
+        actual_pools.sort(key=lambda item: item.get("uuid"))
+        expected_pools.sort(key=lambda item: item.get("uuid"))
 
-        # Get the free values for pool 2.
-        actual_scm_free_2, actual_nvme_free_2 = self.get_scm_nvme_free(
-            actual_pool_2)
+        self.assertListEqual(expected_pools, actual_pools)
 
-        # Pool 2 uses two ranks, so need to double the value in TestPool.
-        scm_size_2 = self.pool[1].scm_size.value * 2
-        nvme_size_2 = self.pool[1].nvme_size.value * 2
+        # For convenience.
+        return actual_pools
 
-        expected_pool_2 = self.create_expected(
-            pool=self.pool[1], scm_free=actual_scm_free_2,
-            nvme_free=actual_nvme_free_2, scm_size=scm_size_2,
-            nvme_size=nvme_size_2, targets_total=16)
+    @staticmethod
+    def get_free_imbalance(pool_dict, storage):
+        """Get free and imbalance from a pool dictionary.
 
-        # Compare expected and actual data for pool 2.
-        self.assertDictEqual(expected_pool_2, actual_pool_2)
+        Args:
+            pool_dict (dict): Pool info from list pool.
+            storage (str): SCM or NVME.
+        """
+        free = 0
+        imbalance = 0
+        for usage in pool_dict["usage"]:
+            if usage["tier_name"] == storage:
+                free = usage["free"]
+                imbalance = usage["imbalance"]
+                break
+
+        return (free, imbalance)
 
     def test_fields_basic(self):
         """Verify all fields of dmg pool list --verbose except used and
@@ -180,56 +208,54 @@ class ListVerboseTest(IorTestBase):
         self.pool.append(self.get_pool(namespace="/run/pool_basic_1/*"))
 
         # 2. Verify the fields of pool 1.
-        actual = self.pool[0].dmg.get_pool_list_all(verbose=True)[0]
-        actual_scm_free, actual_nvme_free = self.get_scm_nvme_free(actual)
-
-        expected = self.create_expected(
-            pool=self.pool[0], scm_free=actual_scm_free,
-            nvme_free=actual_nvme_free)
-
-        self.assertDictEqual(expected, actual)
+        targets_disabled = [0]
+        scm_size = [None]
+        nvme_size = [None]
+        self.verify_pool_lists(
+            targets_disabled=targets_disabled, scm_size=scm_size,
+            nvme_size=nvme_size)
 
         # 3. Create second pool.
         self.pool.append(self.get_pool(namespace="/run/pool_basic_2/*"))
 
         # 4. Verify the fields for both pools.
-        self.verify_two_pools()
+        # Fill in the expected target and size and pass them into
+        # verify_pool_lists.
+        targets_disabled.append(0)
+        scm_size.append(None)
+        nvme_size.append(None)
+        self.verify_pool_lists(
+            targets_disabled=targets_disabled, scm_size=scm_size,
+            nvme_size=nvme_size)
 
         # 5. Exclude target 7 in rank 1 of pool 1.
         self.pool[0].exclude(ranks=[1], tgt_idx="7")
 
         # Sizes are reduced by 1/8.
-        scm_size_1 = self.pool[0].scm_size.value * 0.875
-        nvme_size_1 = self.pool[0].nvme_size.value * 0.875
+        reduced_scm_size = self.pool[0].scm_size.value * 0.875
+        reduced_nvme_size = self.pool[0].nvme_size.value * 0.875
 
         # 6. Verify the fields for both pools with expected disabled and size.
-        self.verify_two_pools(
-            targets_disabled_1=1, scm_size_1=scm_size_1,
-            nvme_size_1=nvme_size_1)
+        targets_disabled[0] = 1
+        scm_size[0] = reduced_scm_size
+        nvme_size[0] = reduced_nvme_size
+        self.verify_pool_lists(
+            targets_disabled=targets_disabled, scm_size=scm_size,
+            nvme_size=nvme_size)
 
-        # 7. Destroy pool 2.
-        self.pool[1].destroy()
+        # 7-11. Destroy and verify until the pools are gone.
+        while self.pool:
+            self.pool[-1].destroy()
+            self.pool.pop()
 
-        # 8. Verify the fields for pool 1.
-        actual = self.pool[0].dmg.get_pool_list_all(verbose=True)
-        actual_scm_free, actual_nvme_free = self.get_scm_nvme_free(actual[0])
+            # Update the expected targets/scm lists too.
+            targets_disabled.pop()
+            scm_size.pop()
+            nvme_size.pop()
 
-        expected = self.create_expected(
-            pool=self.pool[0], scm_free=actual_scm_free,
-            nvme_free=actual_nvme_free, targets_disabled=1, scm_size=scm_size_1,
-            nvme_size=nvme_size_1)
-
-        self.assertDictEqual(expected, actual[0])
-
-        # 9. Verify that there's only one pool in the list.
-        self.assertEqual(len(actual), 1)
-
-        # 10. Destroy pool 1.
-        self.pool[0].destroy()
-
-        # 11. Verify that the list is empty.
-        actual = self.pool[0].dmg.get_pool_list_all(verbose=True)
-        self.assertEqual(len(actual), 0)
+            self.verify_pool_lists(
+                targets_disabled=targets_disabled, scm_size=scm_size,
+                nvme_size=nvme_size)
 
     def verify_used_imbalance(self, storage):
         """Verification steps for test_used_imbalance.
@@ -241,51 +267,48 @@ class ListVerboseTest(IorTestBase):
             list: Errors.
 
         """
-        # 1. Create a pool with 80GB.
+        self.maxDiff = None
+
+        # 1. Create a pool of 80GB.
+        self.pool = []
         if storage == "NVME":
-            self.add_pool(namespace="/run/pool_both/*")
+            self.pool.append(self.get_pool(namespace="/run/pool_both/*"))
+            nvme_size = [None]
         else:
-            self.add_pool(namespace="/run/pool_scm_only/*")
+            self.pool.append(self.get_pool(namespace="/run/pool_scm_only/*"))
+            nvme_size = [0]
 
-        # 2. Store free.
-        before = self.pool.dmg.get_pool_list_all(verbose=True)[0]
-        free_before = -1
-        for usage in before["usage"]:
-            if usage["tier_name"] == storage:
-                free_before = usage["free"]
+        # 2. Verify the pool created.
+        targets_disabled = [0]
+        scm_size = [None]
+        actual_pools_before = self.verify_pool_lists(
+            targets_disabled=targets_disabled, scm_size=scm_size,
+            nvme_size=nvme_size)
 
-        # 3, 4. Create an S1 container and run 1GB IOR with S1.
+        # 3. Store free.
+        free_before, _ = self.get_free_imbalance(
+            actual_pools_before[0], storage)
+
+        # 4, 5. Create an S1 container and run 1GB IOR with S1.
+        self.pool = self.pool[0]
         cmd_result = self.run_ior_with_pool()
-        metrics = IorCommand.get_ior_metrics(cmd_result)
+        metrics = self.ior_cmd.get_ior_metrics(cmd_result)
         ior_write_size = int(metrics[0][22])
         self.log.info("IOR metrics = %s", metrics)
         self.log.info("ior_write_size = %d", ior_write_size)
+        self.pool = [self.pool]
+
+        # 6. Verify all fields except free and imbalance. Free and imbalance are
+        # obtained from actual.
+        actual_pools_after = self.verify_pool_lists(
+            targets_disabled=targets_disabled, scm_size=scm_size,
+            nvme_size=nvme_size)
 
         # Obtain the new free and imblanace.
-        after = self.pool.dmg.get_pool_list_all(verbose=True)[0]
-        free_after = 0
-        imbalance = 0
-        for usage in after["usage"]:
-            if usage["tier_name"] == storage:
-                free_after = usage["free"]
-                imbalance = usage["imbalance"]
-                break
+        free_after, imbalance_after = self.get_free_imbalance(
+            actual_pools_after[0], storage)
 
-        # Copy free_before and imbalance = 0 to after so that we can
-        # directly compare.
-        scm_free_before, nvme_free_before = self.get_scm_nvme_free(before)
-        for usage in after["usage"]:
-            if usage["tier_name"] == "NVME":
-                usage["free"] = nvme_free_before
-                usage["imbalance"] = 0
-            elif usage["tier_name"] == "SCM":
-                usage["free"] = scm_free_before
-                usage["imbalance"] = 0
-
-        # 5. Compare before and after except free and imbalance.
-        self.assertDictEqual(before, after)
-
-        # 6, 7. Verify the free and imbalance.
+        # 7, 8. Verify the free and imbalance.
         errors = []
         if free_after > free_before - ior_write_size:
             errors.append(
@@ -293,12 +316,12 @@ class ListVerboseTest(IorTestBase):
                     storage, free_before, free_after))
 
         # Imbalance can be 10 or 11 (expecting 10% for S1 with 10GB/target pool)
-        if imbalance not in [10, 11]:
+        if imbalance_after not in [10, 11]:
             errors.append(
                 "Unexpected {} imbalance! Expected 10 or 11; Actual {}".format(
-                    storage, imbalance))
+                    storage, imbalance_after))
 
-        # 8. Destroy the container and the pool.
+        # 9. Destroy the container and the pool.
         self.destroy_containers(containers=self.container)
         self.destroy_pools(pools=self.pool)
 
@@ -308,14 +331,15 @@ class ListVerboseTest(IorTestBase):
         """Verify NVMe/SCM Used and Imbalance fields.
 
         1. Create a pool with --nvme-size=80GB.
-        2. Store the NVMe Used.
-        3. Create a POSIX container with --oclass=S1.
-        4. Run IOR with oclass and dir_oclass = S1 to write a 1GB file.
-        5. Verify no change in before and after except in free and imbalance.
-        6. Verify the NVMe Free decreased by 1GB.
-        7. Verify NVMe imbalance is 10%.
-        8. Destroy the container and the pool.
-        9. Repeat above steps with SCM-only pool.
+        2. Verify the pool created.
+        3. Store the NVMe Used.
+        4. Create a POSIX container with --oclass=S1.
+        5. Run IOR with oclass and dir_oclass = S1 to write a 1GB file.
+        6. Verify no change in before and after except in free and imbalance.
+        7. Verify the NVMe Free decreased by 1GB.
+        8. Verify NVMe imbalance is 10%.
+        9. Destroy the container and the pool.
+        10. Repeat above steps with SCM-only pool.
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,medium,ib2
