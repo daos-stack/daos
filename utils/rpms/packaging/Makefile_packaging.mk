3da515d9888e0e56d28d9b848b4356979b7f668b
Brian J. Murrell <brian.murrell@intel.com>
2021-09-02 10:49:22 -0400

    Remove $ escaping
    #Pragmas from previous commit message:
    Skip-checkpatch: true
    Skip-python-bandit: true
    Quick-build: true
    Parallel-build: true
    Skip-unit-tests: true
    Skip-test: false
    Skip-coverity-test: true
    Skip-func-test: false
    Skip-func-test-vm: false
    Skip-func-test-el8: false
    Skip-func-test-leap15: false
    Skip-func-hw-test: true
    Skip-func-hw-test-small: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    Skip-scan-centos-rpms: true
    Skip-test-centos-rpms: true
    Allow-unstable-test: true
    Skip-PR-comments: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- utils/rpms/packaging/Makefile_packaging.mk
+++ utils/rpms/packaging/Makefile_packaging.mk
@@ -354,7 +354,7 @@ chrootbuild: $(SRPM) $(CALLING_MAKEFILE)
 	DISTRO_BASE_PR_REPOS="$($(DISTRO_BASE)_PR_REPOS)"                          \
 	JENKINS_URL="$${JENKINS_URL}"                                              \
 	JOB_REPOS="$(JOB_REPOS)"                                                   \
-	DISTRO_BASE_LOCAL_REPOS="$(subst $$,\$$,$($(DISTRO_BASE)_LOCAL_REPOS))" \
+	DISTRO_BASE_LOCAL_REPOS="$($(DISTRO_BASE)_LOCAL_REPOS)" \
 	MOCK_OPTIONS="$(MOCK_OPTIONS)"                                             \
 	RPM_BUILD_OPTIONS='$(RPM_BUILD_OPTIONS)'                                   \
 	DISTRO_REPOS='$(DISTRO_REPOS)'                                             \
