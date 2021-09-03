1c7fc28af62e0cbd49e63a60400cd091b8a111b0
Jeff Olivier <jeffrey.v.olivier@intel.com>
2021-09-03 15:56:58 -0600

    Modify it to use split(None, 8) to keep the max behavior while using the default split
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/tests/ftest/cart/util/cart_logparse.py
+++ src/tests/ftest/cart/util/cart_logparse.py
@@ -563,7 +563,7 @@ class LogIter():
 
         index = 0
         for line in self._fd:
-            fields = line.split()
+            fields = line.split(None, 8)
             index += 1
             l_pid = None
             if len(fields) < 6 or len(fields[0]) != 17 or fields[0][2] != '/':
@@ -588,7 +588,7 @@ class LogIter():
         index = 0
         position = 0
         for line in self._fd:
-            fields = line.split()
+            fields = line.split(None, 8)
             index += 1
             l_pid = None
             if len(fields) < 6 or len(fields[0]) != 17:
@@ -669,7 +669,7 @@ class LogIter():
             line = self._fd.readline()
             if not line:
                 raise StopIteration
-            fields = line.split()
+            fields = line.split(None, 8)
             if len(fields) < 6 or len(fields[0]) != 17 or fields[0][2] != '/':
                 return LogRaw(line)
             return LogLine(line)
