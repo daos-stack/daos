c3f55ed5fb84629b3626e76ccf9f1012e4130dcb
Maureen Jean <maureen.jean@intel.com>
2021-09-21 09:28:47 -0400

    Create single function to run test
    Test-tag: largefilecount_rc largefilecount
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/io/large_file_count.py
+++ src/tests/ftest/io/large_file_count.py
@@ -5,44 +5,39 @@
 """
 from ior_test_base import IorTestBase
 from mdtest_test_base import MdtestBase
-from apricot import skipForTicket
-
-import write_host_file
 
 
 # pylint: disable=attribute-defined-outside-init
 class LargeFileCount(MdtestBase, IorTestBase):
     """Test class Description: Runs IOR and MDTEST to create large number of files.
+
     :avocado: recursive
     """
 
     def create_cont(self, oclass):
-        """Override container create method
+        """Override container create method.
 
           Args:
             oclass(str): Object class type
         """
         # create container
-        self.container = self.get_container(self.pool, create=False)
+        self.add_container(self.pool, create=False)
         self.container.oclass.update(oclass)
         self.container.create()
 
-    def test_largefilecount(self):
-        """Jira ID: DAOS-3845.
-        Test Description:
-            Run IOR and MDTEST with 48 clients.
-        Use Cases:
-            Run IOR for 5 mints with DFS and POSIX
-            Run MDTEST to create 25K files with DFS and POSIX
-        :avocado: tags=all,daosio,full_regression
-        :avocado: tags=hw,large,dfuse
-        :avocado: tags=largefilecount
+    def run_largefile_count(self, rc=False):
+        """Run the large file count test.
+
+        Args:
+            rc (bool, optional): If release candidate set to true. Defaults to False.
         """
         apis = self.params.get("api", "/run/largefilecount/*")
         object_class = self.params.get("object_class", '/run/largefilecount/*')
 
-        # create pool
-        self.add_pool(connect=False)
+        if rc:
+            # update mdtest file count
+            num_of_files_dirs = self.params.get("num_of_files_dirs_rc", "/run/mdtest/*")
+            self.mdtest_cmd.num_of_files_dirs.update(num_of_files_dirs)
 
         for oclass in object_class:
             self.ior_cmd.dfs_oclass.update(oclass)
@@ -61,11 +56,26 @@ class LargeFileCount(MdtestBase, IorTestBase):
                 self.create_cont(oclass)
                 self.update_ior_cmd_with_pool(False)
                 self.run_ior_with_pool(create_pool=False)
-                # container destroy
-                self.container.destroy()
+
+    def test_largefilecount(self):
+        """Jira ID: DAOS-3845.
+
+        Test Description:
+            Run IOR and MDTEST with 48 clients.
+        Use Cases:
+            Run IOR for 5 mints with DFS and POSIX
+            Run MDTEST to create 25K files with DFS and POSIX
+        :avocado: tags=all,daosio,full_regression
+        :avocado: tags=hw,large,dfuse
+        :avocado: tags=largefilecount
+        """
+        # create pool
+        self.add_pool(connect=False)
+        self.run_largefile_count()
 
     def test_largefilecount_rc(self):
         """Jira ID: DAOS-3845.
+
         Test Description:
             Run IOR and MDTEST with 48 clients with very large file counts.
             This test is not supposed to run as part of pr or weekly runs.
@@ -78,33 +88,6 @@ class LargeFileCount(MdtestBase, IorTestBase):
         :avocado: tags=daosio,dfuse
         :avocado: tags=rc,largefilecount_rc
         """
-        apis = self.params.get("api", "/run/largefilecount/*")
-        num_of_files_dirs_rc = self.params.get("num_of_files_dirs_rc",
-                                               "/run/mdtest/*")
-        object_class = self.params.get("object_class", '/run/largefilecount/*')
-
-        # update mdtest file count
-        self.mdtest_cmd.num_of_files_dirs.update(num_of_files_dirs_rc)
-
         # create pool
         self.add_pool(connect=False)
-
-        for oclass in object_class:
-            self.ior_cmd.dfs_oclass.update(oclass)
-            self.mdtest_cmd.dfs_oclass.update(oclass)
-            for api in apis:
-                self.ior_cmd.api.update(api)
-                self.mdtest_cmd.api.update(api)
-                # update test_dir for mdtest if api is DFS
-                if api == "DFS":
-                    self.mdtest_cmd.test_dir.update("/")
-                # create container for mdtest
-                self.create_cont(oclass)
-                # run mdtest and ior
-                self.execute_mdtest()
-                # create container for ior
-                self.create_cont(oclass)
-                self.update_ior_cmd_with_pool(False)
-                self.run_ior_with_pool(create_pool=False)
-                # container destroy
-                self.container.destroy()
+        self.run_largefile_count(rc=True)
