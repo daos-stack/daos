ee45be90afd6fc935a10df2a746b0219fe1d14aa
Ding Ho <ding-hwa.ho@intel.com>
2021-09-21 15:57:36 +0000

    Update per review comment
    Skip-func-test-el7: false
    Skip-func-test-el8: false
    Skip-func-test-leap15: false
    Test-tag: pr pool
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/pool/bad_connect.py
+++ src/tests/ftest/pool/bad_connect.py
@@ -68,7 +68,7 @@ class BadConnectTest(TestWithServers):
             if expected_result in ['FAIL']:
                 self.fail("Test was expected to fail but it passed.\n")
 
-        except (TestFail, TypeError) as excep:
+        except TestFail as excep:
             print(excep)
             print(traceback.format_exc())
             if expected_result in ['PASS']:
