d60116a7042146b26795a810ed9bb9f29d168a43
Maureen Jean <maureen.jean@intel.com>
2021-09-23 11:52:03 -0400

    split tests into small/large daily/weekly
    Test-tag: smallfilecount largefilecount
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/io/small_file_count.py
+++ src/tests/ftest/io/small_file_count.py
@@ -10,24 +10,23 @@ from avocado.core.exceptions import TestFail
 from file_count_test_base import FileCountTestBase
 
 
-class LargeFileCount(FileCountTestBase):
+class SmallFileCount(FileCountTestBase):
     """Test class Description: Runs IOR and MDTEST to create large number of files.
 
     :avocado: recursive
     """
 
-    def test_largefilecount(self):
+    def test_smallfilecount(self):
         """Jira ID: DAOS-3845.
 
         Test Description:
-            Run IOR and MDTEST with 30 clients with very large file counts.
-            This test is run as part of weekly runs.
+            Run IOR and MDTEST with 30 clients with smaller file counts.
         Use Cases:
-            Run IOR for 5 mints with DFS and POSIX and create 600GB file
-            Run MDTEST to create 1M files with DFS and POSIX
-        :avocado: tags=all,full_regression
+            Run IOR for 5 mints with DFS and POSIX and create 30 2G files
+            Run MDTEST to create 50K files with DFS and POSIX
+        :avocado: tags=all,daily_regression
         :avocado: tags=hw,large
         :avocado: tags=daosio,dfuse
-        :avocado: tags=largefilecount
+        :avocado: tags=smallfilecount
         """
         self.run_file_count()
