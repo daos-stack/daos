965cd2662f8f667b1991040245bba32706779d62
Maureen Jean <maureen.jean@intel.com>
2021-09-23 08:46:41 -0400

    Revert test params to original
    Test-tag: largefilecount_rc largefilecount
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/io/large_file_count.py
+++ src/tests/ftest/io/large_file_count.py
@@ -3,8 +3,32 @@
   (C) Copyright 2020-2021 Intel Corporation.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
+import re
 from ior_test_base import IorTestBase
 from mdtest_test_base import MdtestBase
+
+
+def get_rf(oclass):
+    """Return redundancy factor based on the oclass.
+
+    Args:
+        oclass(string): object class.
+
+    return:
+        redundancy factor(int) from object type
+    """
+    rf = 0
+    if "EC" in oclass:
+        tmp = re.findall(r'\d+', oclass)
+        if tmp:
+            rf = int(tmp[1])
+    elif "RP" in oclass:
+        tmp = re.findall(r'\d+', oclass)
+        if tmp:
+            rf = int(tmp[0]) - 1
+    else:
+        rf = 0
+    return rf
 
 
 # pylint: disable=attribute-defined-outside-init
@@ -14,7 +38,7 @@ class LargeFileCount(MdtestBase, IorTestBase):
     :avocado: recursive
     """
 
-    def add_containers(self, path="/run/container/*"):
+    def add_containers(self, oclass=None):
         """Create a list of containers that the various jobs use for storage.
 
         Args:
@@ -24,10 +48,18 @@ class LargeFileCount(MdtestBase, IorTestBase):
 
         """
         # Create a container and add it to the overall list of containers
-        self.container.append(self.get_container(self.pool))
-        self.container[-1].namespace = path
-        self.container[-1].get_params(self)
-        self.container[-1].create()
+        container = self.get_container(self.pool)
+        # don't include oclass in daos cont cmd; include rf based on the class
+        if oclass:
+            container.oclass.update(oclass)
+            redundancy_factor = get_rf(oclass)
+            rf = 'rf:{}'.format(str(redundancy_factor))
+        properties = container.properties.value
+        cont_properties = (",").join(filter(None, [properties, rf]))
+        if cont_properties is not None:
+            container.properties.update(cont_properties)
+        container.create()
+        return container
 
     def run_largefile_count(self, rc=False):
         """Run the large file count test.
@@ -37,6 +69,9 @@ class LargeFileCount(MdtestBase, IorTestBase):
         """
         saved_container = []
         apis = self.params.get("api", "/run/largefilecount/*")
+        object_class = self.params.get("object_class", '/run/largefilecount/*')
+        # create pool
+        self.add_pool(connect=False)
 
         if rc:
             # update mdtest file count
@@ -44,21 +79,28 @@ class LargeFileCount(MdtestBase, IorTestBase):
             self.mdtest_cmd.num_of_files_dirs.update(num_of_files_dirs)
             block_size = self.params.get("block_size_rc", "/run/ior/*")
             self.ior_cmd.block_size.update(block_size)
-
-        for api in apis:
-            self.ior_cmd.api.update(api)
-            self.mdtest_cmd.api.update(api)
-            # update test_dir for mdtest if api is DFS
-            if api == "DFS":
-                self.mdtest_cmd.test_dir.update("/")
-            # run mdtest
-            self.execute_mdtest()
-            # save the current container; to be destroyed later
-            saved_container.append(self.container)
-            # run ior
-            self.run_ior_with_pool(create_pool=False)
-            # save the current container
-            saved_container.append(self.container)
+        for oclass in object_class:
+            self.ior_cmd.dfs_oclass.update(oclass)
+            self.mdtest_cmd.dfs_oclass.update(oclass)
+            for api in apis:
+                self.ior_cmd.api.update(api)
+                self.mdtest_cmd.api.update(api)
+                # update test_dir for mdtest if api is DFS
+                if api == "DFS":
+                    self.mdtest_cmd.test_dir.update("/")
+                # run mdtest
+                self.container = self.add_containers(oclass)
+                self.log.info("Starting MDTEST with %s and %s", api, oclass)
+                self.execute_mdtest()
+                # save the current container; to be destroyed later
+                saved_container.append(self.container)
+                # run ior
+                self.container = self.add_containers(oclass)
+                self.update_ior_cmd_with_pool(False)
+                self.log.info("Starting IOR with %s and %s", api, oclass)
+                self.run_ior_with_pool(create_pool=False)
+                # save the current container
+                saved_container.append(self.container)
         # copy saved containers to self.container
         self.container = saved_container
 
@@ -66,33 +108,29 @@ class LargeFileCount(MdtestBase, IorTestBase):
         """Jira ID: DAOS-3845.
 
         Test Description:
-            Run IOR and MDTEST with 48 clients.
+            Run IOR and MDTEST with 30 clients.
         Use Cases:
-            Run IOR for 5 mints with DFS and POSIX and create 1OOGB file
+            Run IOR for 5 mints with DFS and POSIX and create 60GB file
             Run MDTEST to create 50K files with DFS and POSIX
         :avocado: tags=all,daosio,full_regression
         :avocado: tags=hw,large,dfuse
         :avocado: tags=largefilecount
         """
-        # create pool
-        self.add_pool(connect=False)
         self.run_largefile_count()
 
     def test_largefilecount_rc(self):
         """Jira ID: DAOS-3845.
 
         Test Description:
-            Run IOR and MDTEST with 48 clients with very large file counts.
+            Run IOR and MDTEST with 30 clients with very large file counts.
             This test is not supposed to run as part of pr or weekly runs.
             This should only be run before release as desired.
         Use Cases:
-            Run IOR for 5 mints with DFS and POSIX and create 1TB file
-            Run MDTEST to create 1B files with DFS and POSIX
+            Run IOR for 5 mints with DFS and POSIX and create 600GB file
+            Run MDTEST to create 1M files with DFS and POSIX
         :avocado: tags=all,full_regression
         :avocado: tags=hw,large
         :avocado: tags=daosio,dfuse
         :avocado: tags=rc,largefilecount_rc
         """
-        # create pool
-        self.add_pool(connect=False)
         self.run_largefile_count(rc=True)
