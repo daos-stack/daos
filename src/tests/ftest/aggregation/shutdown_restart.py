e14a752facbf0e887f5b05dedf8112083e3a6bb4
Omar Ocampo <omar.ocampo.coronado@intel.com>
2021-09-30 13:29:14 -0500

    DAOS-7312 Test: Functional testing for EC auto object class selection
        Updated from SX to EC objects within large file cound and seg count
    
    Quick-build: true
    Skip-unit-tests: true
    Skip-nlt: true
    Skip-unit-test: true
    Skip-func-test-vm: true
    Test-tag-hw-small: dfusecachingcheck
    Test-tag-hw-medium: parallelio ioaggregation
    Test-tag-hw-large: largefilecount ior_segcount
    
    Signed-off-by: Omar Ocampo <omar.ocampo.coronado@intel.com>

--- src/tests/ftest/aggregation/shutdown_restart.py
+++ src/tests/ftest/aggregation/shutdown_restart.py
@@ -47,7 +47,7 @@ class IoAggregation(IorTestBase):
             for 4 attempts.
 
         :avocado: tags=all,full_regression
-        :avocado: tags=hw,medium
+        :avocado: tags=hw,medium,ib2
         :avocado: tags=daosio,ioaggregation,tx
         """
         # update ior signature option
