95a86dfc065de8c265733bf9840dacfb910fec4d
Samir Raval <samir.raval@intel.com>
2021-09-30 17:43:16 +0000

    Updated code based on review comments.
    Test-tag-hw-large: pr ec_disabled_rebuild_array ec_offline_rebuild_agg_disabled
    
    Signed-off-by: Samir Raval <samir.raval@intel.com>

--- src/tests/ftest/erasurecode/offline_rebuild_aggregation.py
+++ src/tests/ftest/erasurecode/offline_rebuild_aggregation.py
@@ -15,27 +15,15 @@ class EcodAggregationOffRebuild(ErasureCodeIor):
                         with different Aggregation mode for Partial strip data.
     :avocado: recursive
     """
-    def test_ec_offline_rebuild_agg_disabled(self):
-        """Jira ID: DAOS-7313.
-
-        Test Description: Test Erasure code object aggregation disabled mode
-                          with IOR.
-        Use Case: Create the pool, disabled aggregation, run IOR with supported
-                  EC object type with partial strip.
-                  Verify that Aggregation should not triggered.
-                  Verify the IOR read data at the end.
-                  Kill single server and wait for rebuild.
-                  Read and verify all the data.
-                  Kill second server and wait for rebuild.
-                  Read and verify data with +2 Parity with no data corruption.
-
-        :avocado: tags=all,full_regression
-        :avocado: tags=hw,large,ib2
-        :avocado: tags=ec,aggregation,ec_array,ec_aggregation
-        :avocado: tags=ec_offline_rebuild_agg_disabled
+    def execution(self, agg_trigger=False):
         """
-        # Disable the aggregation
-        self.pool.set_property("reclaim", "disabled")
+        Common method to run the tests.
+
+        Args:
+           agg_trigger(Boolean): Check if Aggregation is happening after IO
+                                 or not, By default it's False.
+
+        """
         self.pool.connect()
         self.log.info("pool_percentage Before = %s",
                       self.pool.pool_percentage_used())
@@ -43,9 +31,14 @@ class EcodAggregationOffRebuild(ErasureCodeIor):
         # Write the IOR data set with given all the EC object type
         self.ior_write_dataset()
 
-        # Verify if Aggregation is getting started
-        if any(check_aggregation_status(self.pool).values()) is True:
-            self.fail("Aggregation should not happens...")
+        if agg_trigger:
+            # Verify that Aggregation is getting started
+            if not any(check_aggregation_status(self.pool).values()):
+                self.fail("Aggregation failed to start..")
+        else:
+            # Verify that Aggregation is not starting
+            if any(check_aggregation_status(self.pool).values()) is True:
+                self.fail("Aggregation should not happens...")
 
         # Read IOR data and verify content
         self.ior_read_dataset()
@@ -73,6 +66,29 @@ class EcodAggregationOffRebuild(ErasureCodeIor):
         # Only +2 (Parity) data will be intact so read and verify only +2 IOR
         # data set
         self.ior_read_dataset(parity=2)
+
+    def test_ec_offline_rebuild_agg_disabled(self):
+        """Jira ID: DAOS-7313.
+
+        Test Description: Test Erasure code object aggregation disabled mode
+                          with IOR.
+        Use Case: Create the pool, disabled aggregation, run IOR with supported
+                  EC object type with partial strip.
+                  Verify that Aggregation should not triggered.
+                  Verify the IOR read data at the end.
+                  Kill single server and wait for rebuild.
+                  Read and verify all the data.
+                  Kill second server and wait for rebuild.
+                  Read and verify data with +2 Parity with no data corruption.
+
+        :avocado: tags=all,full_regression
+        :avocado: tags=hw,large,ib2
+        :avocado: tags=ec,aggregation,ec_array,ec_aggregation
+        :avocado: tags=ec_offline_rebuild_agg_disabled
+        """
+        # Disable the aggregation
+        self.pool.set_property("reclaim", "disabled")
+        self.execution()
 
     @skipForTicket("DAOS-8542")
     def test_ec_offline_rebuild_agg_default(self):
@@ -95,44 +111,7 @@ class EcodAggregationOffRebuild(ErasureCodeIor):
         :avocado: tags=ec,aggregation,ec_array,ec_aggregation
         :avocado: tags=ec_offline_rebuild_agg_default
         """
-        self.pool.connect()
-
-        self.log.info("pool_percentage Before = %s",
-                      self.pool.pool_percentage_used())
-
-        # Write the IOR data set with given all the EC object type
-        self.ior_write_dataset()
-
-        # Verify if Aggregation is getting started
-        if not any(check_aggregation_status(self.pool).values()):
-            self.fail("Aggregation failed to start..")
-
-        # Read IOR data and verify content
-        self.ior_read_dataset()
-
-        # Kill the last server rank
-        self.server_managers[0].stop_ranks([self.server_count - 1], self.d_log,
-                                           force=True)
-
-        # Wait for rebuild to complete
-        self.pool.wait_for_rebuild(False)
-
-        # Read IOR data and verify for different EC object data still OK
-        # written before killing the single server
-        self.ior_read_dataset()
-
-        # Kill the another server rank
-        self.server_managers[0].stop_ranks([self.server_count - 2], self.d_log,
-                                           force=True)
-
-        # Wait for rebuild to complete
-        self.pool.wait_for_rebuild(False)
-
-        # Read IOR data and verify for different EC object and different sizes
-        # written before killing the second server.
-        # Only +2 (Parity) data will be intact so read and verify only +2 IOR
-        # data set
-        self.ior_read_dataset(parity=2)
+        self.execution(agg_trigger=True)
 
     @skipForTicket("DAOS-8542")
     def test_ec_offline_agg_during_rebuild(self):
