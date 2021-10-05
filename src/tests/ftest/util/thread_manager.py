42dfea615d43dbe2a1b186c86380c04ee9970782
Phillip Henderson <phillip.henderson@intel.com>
2021-10-05 13:43:40 -0400

    Fixing threading
    Quick-functional: true
    Test-tag: metadata_ior offline_extend_oclass
    Test-repeat: 10
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

---
+++ src/tests/ftest/util/thread_manager.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+(C) Copyright 2021 Intel Corporation.
+
+SPDX-License-Identifier: BSD-2-Clause-Patent
+"""
+from concurrent.futures import ThreadPoolExecutor, as_completed, CancelledError, TimeoutError
+
+
+class ThreadManager():
+    """Class to manage running any method as multiple threads."""
+
+    def __init__(self, method, timeout=None):
+        """Initialize a ThreadManager object with the the method to run as a thread.
+
+        Args:
+            method (callable): [description]
+            timeout (int, optional): [description]. Defaults to None.
+        """
+        self.method = method
+        self.timeout = timeout
+        self.job_kwargs = []
+
+    def add(self, **kwargs):
+        """Add a thread to run by specifying the keyword arguments for the thread method."""
+        self.job_kwargs.append(kwargs)
+
+    def run(self):
+        """Asynchronously run the method as a thread for each set of method arguments.
+
+        Returns:
+            dict: a dictionary of lists of results returned by each thread under either a "PASS" or
+                "FAIL" key
+
+        """
+        results = {"PASS": [], "FAIL": []}
+        with ThreadPoolExecutor() as thread_executor:
+            futures = {thread_executor.submit(self.method, **kwargs) for kwargs in self.job_kwargs}
+            for future in as_completed(futures, self.timeout):
+                try:
+                    results["PASS"].append(future.result())
+                except CancelledError as error:
+                    results["FAIL"].append("{} was cancelled: {}".format(future, error))
+                except TimeoutError as error:
+                    results["FAIL"].append("{} timed out: {}".format(future, error))
+                except Exception as error:
+                    results["FAIL"].append("{} failed with an exception: {}".format(future, error))
+        return results
