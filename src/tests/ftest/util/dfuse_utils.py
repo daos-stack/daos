8333ee089ad99a2e7c40c907d14495c35361e627
Dalton Bohning <dalton.bohning@intel.com>
2021-10-18 20:26:32 +0000

    quote grep expression
    Quick-functional: true
    Test-tag: pr dfuse,-findcmd_perf
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- src/tests/ftest/util/dfuse_utils.py
+++ src/tests/ftest/util/dfuse_utils.py
@@ -122,7 +122,7 @@ class Dfuse(DfuseCommand):
                 if retcode == 0:
                     check_mounted.add(host)
                 else:
-                    command = "cat /proc/mounts | grep dfuse {}".format(self.mount_dir.value)
+                    command = "cat /proc/mounts | grep 'dfuse {}'".format(self.mount_dir.value)
                     retcodes = pcmd([host], command, expect_rc=None)
                     for ret_code, host_names in list(retcodes.items()):
                         for node in host_names:
