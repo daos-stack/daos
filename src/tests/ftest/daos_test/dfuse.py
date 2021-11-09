21434d1bd6c60aba6456fec4d2e43b67f3a6be24
Ashley Pittman <ashley.m.pittman@intel.com>
2021-11-09 10:25:18 +0000

    ftest: Add dfuse_test code and call it from ftest.
    Quick-Functional: true
    Test-tag: dfuse_test
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

---
+++ src/tests/ftest/daos_test/dfuse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+  (C) Copyright 2021 Intel Corporation.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+"""
+
+import os
+from daos_core_base import DaosCoreBase
+
+class DaosCoreTestDfuse(DaosCoreBase):
+    # pylint: disable=too-many-ancestors
+    """Runs DAOS DFuse tests.
+
+    :avocado: recursive
+    """
+
+    def test_daos_dfuse_openat(self):
+        """
+
+        Test Description:
+            Run dfuse_test -o
+
+        Use cases:
+            DAOS DFuse unit tests
+
+        :avocado: tags=all,pr,daily_regression
+        :avocado: tags=vm
+        :avocado: tags=dfuse,dfuse_test
+        :avocado: tags=dfuse_test_openat
+        """
+        self.daos_test = os.path.join(self.bin, 'dfuse_test')
+        self.run_subtest()
---
+++ src/tests/suite/dfuse_test.c
@@ -0,0 +1,71 @@
+/**
+ * (C) Copyright 2021 Intel Corporation.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ */
+/**
+ * This file is part of daos
+ *
+ * tests/suite/dfuse_test
+ */
+
+#include <stdio.h>
+#include <getopt.h>
+
+static void
+print_usage()
+{
+	printf("DFuse tests\n");
+	printf("dfuse_test -m <path to test>\n");
+	printf("dfute_test --open-at\n");
+}
+
+void
+do_openat(char *test_dir)
+{
+	printf("Hello\n");
+}
+
+typedef void (dfuse_test)(char *);
+
+int main(int argc, char **argv)
+{
+	int index = 0;
+	int opt;
+	char *test_dir = NULL;
+	dfuse_test *test = NULL;
+	struct option long_options[] = {
+		{"test-dir",	required_argument,	NULL,	'M'},
+		{"open-at",	no_argument,		NULL,	'o'},
+		{NULL,		0,			NULL,	0}
+	};
+
+	while ((opt = getopt_long(argc, argv, "M:o", long_options, &index)) != -1) {
+		switch (opt) {
+		case 'o':
+			test = do_openat;
+			break;
+		case 'M':
+			test_dir = optarg;
+			break;
+		default:
+			printf("Unknown Option\n");
+			print_usage();
+			return 1;
+		}
+	}
+
+	if (test_dir == NULL) {
+		printf("--test-dir option required\n");
+		return 1;
+	}
+
+	/* Check that test_dir exists, and is a directory */
+
+	if (test == NULL) {
+		printf("No test specified\n");
+		return 1;
+	}
+	test(test_dir);
+	return 0;
+}
