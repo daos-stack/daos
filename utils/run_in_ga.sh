9cf8e98a132e390de6f03ca5c9f4cea0906f43dc
Ashley Pittman <ashley.m.pittman@intel.com>
2021-11-16 14:12:27 +0000

    Back out NLT perms changes.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/run_in_ga.sh
+++ utils/run_in_ga.sh
@@ -62,5 +62,5 @@ echo ::group::Setting up daos_admin
 echo ::endgroup::
 
 echo ::group::Container copy test
-./utils/node_local_test.py --memcheck no --test cont_copy
+./utils/node_local_test.py --no-root --memcheck no --test cont_copy
 echo ::endgroup::
--- utils/run_in_ga_wrapper.sh
+++ utils/run_in_ga_wrapper.sh
@@ -4,7 +4,8 @@ set -e
 
 IMAGE=$(docker images --all --filter label=DAOS=true --quiet | tail -n 1)
 
-docker run --name build-post --cap-add SYS_ADMIN --cap-add SYS_PTRACE \
+docker run --name build-post \
+       --mount type=tmpfs,destination=/mnt/daos_0,tmpfs-mode=1777 \
        --env COMPILER --env DEPS_JOBS "$IMAGE" ./utils/run_in_ga.sh
 
 docker cp build-post:/home/daos/daos/nlt-junit.xml ./
