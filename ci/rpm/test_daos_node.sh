d657fd62b6131064dc44a8cd2b5aac80ec94db93
Brian J. Murrell <brian.murrell@intel.com>
2021-11-15 21:09:30 -0500

    Back to mounting
    Skip-PR-comments: true
    Quick-functional: true
    Skip-func-test: true
    PR-repos: daos@PR-7345:47
    RPM-test-version: 2.1.100-5.7383.g54f335c7
    
    
    #Pragmas from previous commit message:
    Skip-PR-comments: true
    Quick-functional: true
    Skip-func-test: true
    PR-repos: daos@PR-7345:47
    RPM-test-version: 2.1.100-5.7383.g54f335c7
    Skip-PR-comments: true
    Quick-functional: true
    Skip-func-test: true
    PR-repos: daos@PR-7345:47
    RPM-test-version: 2.1.100-5.7383.g54f335c7
    Skip-PR-comments: true
    Quick-functional: true
    Skip-func-test: true
    PR-repos: daos@PR-7345:47
    RPM-test-version: 2.1.100-5.7383.g54f335c7
    Skip-PR-comments: true
    Quick-functional: true
    Skip-func-test: true
    PR-repos: daos@PR-7345:47
    RPM-test-version: 2.1.100-5.7383.g54f335c7
    Skip-checkpatch: true
    Skip-python-bandit: true
    Skip-build: true
    Quick-build: true
    Quick-Functional: true
    Skip-unit-tests: true
    Skip-test: true
    Allow-unstable-test: true
    #RPM-test-version: version[-release]
    #RPM-test-version: 2.0.0
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/rpm/test_daos_node.sh
+++ ci/rpm/test_daos_node.sh
@@ -90,6 +90,7 @@ sudo chmod 0755 /tmp/daos_sockets
 sudo chown "$me:$me" /tmp/daos_sockets
 if [ ! -d /mnt/daos ]; then
     sudo mkdir -p /mnt/daos
+    sudo mount -t tmpfs -o size=16777216k tmpfs /mnt/daos
 fi
 sudo cp /tmp/daos_server.yml /etc/daos/daos_server.yml
 sudo cp /tmp/daos_agent.yml /etc/daos/daos_agent.yml
