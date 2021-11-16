fe436bb6d2e99adffa07bf74f3fc806c8196774c
Phillip Henderson <phillip.henderson@intel.com>
2021-11-16 11:38:44 -0500

    Adding uname -a confirmation
    Test-tag: pr daily_regression
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/scripts/setup_nodes.sh
+++ src/tests/ftest/scripts/setup_nodes.sh
@@ -60,6 +60,7 @@ if [ \"\$(ulimit -l)\" != \"unlimited\" ]; then
     echo \"*  soft  memlock  unlimited\" >> /etc/security/limits.conf
     echo \"*  hard  memlock  unlimited\" >> /etc/security/limits.conf
 fi
+uname -a
 echo \"/var/tmp/core.%e.%t.%p\" > /proc/sys/kernel/core_pattern"
 rm -f /var/tmp/core.*
 if [ "${HOSTNAME%%.*}" != "$FIRST_NODE" ]; then
