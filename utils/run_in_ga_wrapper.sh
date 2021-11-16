be9f6f02d0b16f6da8bc3d3461a944a6f57cff27
Ashley Pittman <ashley.m.pittman@intel.com>
2021-11-16 12:46:02 +0000

    tweaks.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/run_in_ga_wrapper.sh
+++ utils/run_in_ga_wrapper.sh
@@ -5,6 +5,6 @@ set -e
 IMAGE=$(docker images --all --filter label=DAOS=true --quiet | tail -n 1)
 
 docker run --name build-post --cap-add SYS_ADMIN --cap-add SYS_PTRACE \
-       --env COMPILER "$IMAGE" ./utils/run_in_ga.sh
+       --env COMPILER --env DEPS_JOBS "$IMAGE" ./utils/run_in_ga.sh
 
 docker cp build-post:/home/daos/daos/nlt-junit.xml ./
