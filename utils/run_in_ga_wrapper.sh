cb0bd87f817ebe45422e2e9358961c44e5844545
Ashley Pittman <ashley.m.pittman@intel.com>
2021-11-19 17:32:56 +0000

    Improve things.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/run_in_ga_wrapper.sh
+++ utils/run_in_ga_wrapper.sh
@@ -3,6 +3,6 @@
 set -e
 
 docker run --name build-post --mount type=tmpfs,destination=/mnt/daos_0,tmpfs-mode=1777 \
-       --env COMPILER --env DEPS_JOBS build-image ./daos/utils/run_in_ga.sh
+       --env COMPILER --env DEPS_JOBS --user root:root build-image ./daos/utils/run_in_ga.sh
 
 docker cp build-post:/home/daos/daos/nlt-junit.xml ./
