e8d28ea48a5101ae777394d48901b167a6ff924f
Ashley Pittman <ashley.m.pittman@intel.com>
2021-11-19 12:27:02 +0000

    Add a fest for this.
    Test-tag: dfusedaosbuild
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

---
+++ src/tests/ftest/dfuse/daos_build.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+"""
+  (C) Copyright 2020-2021 Intel Corporation.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+"""
+
+import os
+import errno
+
+from dfuse_test_base import DfuseTestBase
+
+class BuildDaos(DfuseTestBase):
+    # pylint: disable=too-many-ancestors,too-few-public-methods
+    """Build DAOS over dfuse
+
+    :avocado: recursive
+    """
+
+    def test_daos_build(self):
+        """Jira ID: DAOS-8937.
+
+        Test Description:
+            This tests builds DAOS on a dfuse filesystem.
+        Use cases:
+            Create Pool
+            Create Posix container
+            Mount dfuse
+            Checkout and build DAOS sources.
+        :avocado: tags=all,daily_regression
+        :avocado: tags=small
+        :avocado: tags=daosio,dfuse
+        :avocado: tags=dfusedaosbuild
+        """
+        # Create a pool, container and start dfuse.
+        self.add_pool(connect=False)
+        self.add_container(self.pool)
+        self.start_dfuse(self.hostlist_clients, self.pool, self.container)
+
+        mount_dir = self.dfuse.mount_dir.value
+        build_dir = os.path.join(mount_dir, 'daos')
+
+        cmds = ['git clone https://github.com/daos-stack/daos.git {}'.format(build_dir),
+                'git -C {} submodule init'.format(build_dir),
+                'git -C {} submodule update'.format(build_dir),
+                'scons-3 -C {} build --build-deps=yes'.format(build_dir)]
+        try:
+            ret_code = general_utils.pcmd(self.hostlist_clients, cmd, timeout=300)
+            print(ret)
+            if 0 not in ret_code:
+                error_hosts = NodeSet(",".join(
+                    [str(node_set) for code, node_set in
+                     list(ret_code.items()) if code != 0]))
+                raise CommandFailure("Error running '{}'".format(cmd))
+            # report error if any command fails
+        except CommandFailure as error:
+            self.log.error("BashCmd Test Failed: %s", str(error))
+            self.fail("Test was expected to pass but it failed.\n")
