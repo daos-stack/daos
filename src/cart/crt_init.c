084cf1d04eeba3118b2b8f092866d06830f88d72
Ashley Pittman <ashley.m.pittman@intel.com>
2021-11-22 16:26:54 +0000

    Try and use a unique key for master builds.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -18,8 +18,6 @@ struct crt_gdata crt_gdata;
 static volatile int   gdata_init_flag;
 struct crt_plugin_gdata crt_plugin_gdata;
 
-/* Hello world */
-
 static void
 dump_envariables(void)
 {
@@ -301,7 +299,8 @@ crt_plugin_fini(void)
 
 	for (i = 0; i < CRT_SRV_CONTEXT_NUM; i++) {
 		D_FREE(crt_plugin_gdata.cpg_prog_cbs[i]);
-		D_FREE(crt_plugin_gdata.cpg_prog_cbs_old[i]);
+		if (crt_plugin_gdata.cpg_prog_cbs_old[i])
+			D_FREE(crt_plugin_gdata.cpg_prog_cbs_old[i]);
 	}
 
 	D_FREE(crt_plugin_gdata.cpg_timeout_cbs);
