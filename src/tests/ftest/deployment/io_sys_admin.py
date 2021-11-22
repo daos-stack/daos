d0f5007717a9a9866518ec5aa8929e6595584750
Saurabh Tandan <saurabh.tandan@intel.com>
2021-11-22 17:42:11 +0000

    WIP
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/deployment/io_sys_admin.py
+++ src/tests/ftest/deployment/io_sys_admin.py
@@ -10,6 +10,7 @@ import time
 from file_count_test_base import FileCountTestBase
 from data_mover_test_base import DataMoverTestBase
 from general_utils import human_to_bytes
+from pool_test_base import PoolTestBase
 
 class IoSysAdmin(DataMoverTestBase, FileCountTestBase):
     # pylint: disable=too-many-ancestors
@@ -42,7 +43,21 @@ class IoSysAdmin(DataMoverTestBase, FileCountTestBase):
         # local param
         ec_ior_flags = self.params.get("ec_ior_flags", "/run/ior/*")
         hdf5_plugin_path = self.params.get("plugin_path", '/run/hdf5_vol/*')
+#        sys_admin_pool_sizes = self.params.get("pool_sizes", '/run/sys_admin/*')
 
+        self.add_pool_qty(1, namespace="/run/pool_1/", create=False)
+        PoolTestBase.check_pool_creation(self, 30)
+        self.destroy_pools(self.pool)
+        self.pool = None
+        self.add_pool_qty(1, namespace="/run/pool_2/", create=False)
+        PoolTestBase.check_pool_creation(self, 30)
+        self.destroy_pools(self.pool)
+        self.pool = None
+        self.add_pool_qty(1, namespace="/run/pool_3/", create=False)
+        PoolTestBase.check_pool_creation(self, 60)
+
+        dmg = self.get_dmg_command()
+        dmg.pool_list()
         # run tests
 #        self.test_dmg_storage_scan_scm()
 
@@ -56,55 +71,46 @@ class IoSysAdmin(DataMoverTestBase, FileCountTestBase):
 #        self.test_net_class()
 
 
-#        self.test_largefilecount()
-        self.run_file_count()
+#        self.run_file_count()
         # create snapshot
-        self.container[-1].create_snap()        
+#        self.container[-1].create_snap()        
         # overwrite the last ior file
-        self.ior_cmd.signature.update('456')
-        self.run_ior_with_pool(create_pool=False, create_cont=False)
+#        self.ior_cmd.signature.update('456')
+#        self.run_ior_with_pool(create_pool=False, create_cont=False)
 
-        nvme_free_space_before_snap_destroy = self.get_free_space()[1]
+#        nvme_free_space_before_snap_destroy = self.get_free_space()[1]
         # delete snapshot
-        self.container[-1].destroy_snap(epc=self.container[-1].epoch)
+#        self.container[-1].destroy_snap(epc=self.container[-1].epoch)
         # Now check if the space is returned back.
-        counter = 1
-        returned_space = (self.get_free_space()[1] -
-                          nvme_free_space_before_snap_destroy)
+#        counter = 1
+#        returned_space = (self.get_free_space()[1] -
+#                          nvme_free_space_before_snap_destroy)
 
-        data_written = (int(self.processes) * human_to_bytes(self.ior_cmd.block_size.value))
-        while returned_space < int(data_written):
+#        data_written = (int(self.processes) * human_to_bytes(self.ior_cmd.block_size.value))
+#        while returned_space < int(data_written):
             # try to wait for 4 x 60 secs for aggregation to be completed or
             # else exit the test with a failure.
-            if counter > 4:
-                self.log.info("Free space before snapshot destroy: %s",
-                              nvme_free_space_before_snap_destroy)
-                self.log.info("Free space when test terminated: %s",
-                              self.get_free_space()[1])
-                self.fail("Aggregation did not complete as expected")
+#            if counter > 4:
+#                self.log.info("Free space before snapshot destroy: %s",
+#                              nvme_free_space_before_snap_destroy)
+#                self.log.info("Free space when test terminated: %s",
+#                              self.get_free_space()[1])
+#                self.fail("Aggregation did not complete as expected")
 
-            time.sleep(60)
-            returned_space = (self.get_free_space()[1] -
-                              nvme_free_space_before_snap_destroy)
-            counter += 1
+#            time.sleep(60)
+#            returned_space = (self.get_free_space()[1] -
+#                              nvme_free_space_before_snap_destroy)
+#            counter += 1
 
-#        for container in self.container:
-#            print("Container: {}".format(container.uuid))
+#        print("#####Starting FS_COPY Test")
+#        self.run_dm_activities_with_ior("FS_COPY", pool=self.pool)
+#        print("#####Starting DCP Test")
+#        self.run_dm_activities_with_ior("DCP", pool=self.pool)
+#        print("#####Starting DSERIAL Test")
+#        self.run_dm_activities_with_ior("DSERIAL", pool=self.pool)
+#        print("#####Completed all Datamover tests")
+#        self.container.pop(0)
 
-        print("#####Starting FS_COPY Test")
-        self.run_dm_activities_with_ior("FS_COPY", pool=self.pool)
-#        for container in self.container:
-#            print("Container: {}".format(container.uuid))
-        print("#####Starting DCP Test")
-        self.run_dm_activities_with_ior("DCP", pool=self.pool)
-#        for container in self.container:
-#            print("Container: {}".format(container.uuid))
-        print("#####Starting DSERIAL Test")
-        self.run_dm_activities_with_ior("DSERIAL", pool=self.pool)
-        print("#####Completed all Datamover tests")
-#        for container in self.container:
-#            print("Container: {}".format(container.uuid))
-        self.container.pop(0)
 #        self.run_dm_large_file_dep("FS_COPY")
 #        self.run_dm_large_file_dep("DCP")
 ##        self.run_dm_large_file_dep("CONT_CLONE")
