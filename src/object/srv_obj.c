501790a74add24b0a9cf476c187abdc481e12865
Nasf-Fan <fan.yong@intel.com>
2021-11-22 23:30:51 +0800

    DAOS-7878 dtx: try DTX refresh by default (#7414)
    master-commit: a2d437d7f6dcd88cace7ab3e7e19a15e53e79955
    
    Originally, when hit non-committed DTX on non-leader, we may ask related
    operation sponsor (the client) to retry without refreshing the DTX state
    with the leader. But sometimes, the sponsor retry cannot resolve related
    DTX state issue in time until such DTX is batched committed by th leader.
    
    Then we can make DTX refresh as the default behavior under such case.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/object/srv_obj.c
+++ src/object/srv_obj.c
@@ -2517,8 +2517,6 @@ ds_obj_rw_handler(crt_rpc_t *rpc)
 
 		if (orw->orw_flags & ORF_FOR_MIGRATION)
 			dtx_flags = DTX_FOR_MIGRATION;
-		if (orw->orw_flags & ORF_DTX_REFRESH)
-			dtx_flags |= DTX_FORCE_REFRESH;
 
 		rc = dtx_begin(ioc.ioc_vos_coh, &orw->orw_dti, &epoch, 0,
 			       orw->orw_map_ver, &orw->orw_oid,
@@ -2880,8 +2878,6 @@ obj_local_enum(struct obj_io_context *ioc, crt_rpc_t *rpc,
 
 	if (oei->oei_flags & ORF_FOR_MIGRATION)
 		flags = DTX_FOR_MIGRATION;
-	if (oei->oei_flags & ORF_DTX_REFRESH)
-		flags |= DTX_FORCE_REFRESH;
 
 	rc = dtx_begin(ioc->ioc_vos_coh, &oei->oei_dti, &epoch, 0,
 		       oei->oei_map_ver, &oei->oei_oid, NULL, 0, flags,
