bc0707a6b75b8fb21604f6c89eca71750937e9b7
Ashley Pittman <ashley.m.pittman@intel.com>
2021-11-29 12:24:44 +0000

    DAOS-7821 gha: Update triggers for doxygen and release jobs.
    Use the 'paths' setting in the GHA yaml file to decide when
    to run jobs, rather than having them trigger for every PR
    then exiting when there is nothing do to.
    
    Run all installed headers under doxygen, we've got a much lower
    error count now than when the trigger was added.
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/copy_doxygen_files.sh
+++ ci/copy_doxygen_files.sh
@@ -2,36 +2,10 @@
 
 # Script to run doxygen against modified headers and report results.
 # see .github/workflows/doxygen.yml
+# Just run against all installed headers, use github actions trigger to control when
+# job runs.
 
 set -e
-
-echo ::group::Setting up src
-
-# Load the list of modified files.
-git fetch
-mapfile -t FILES < <(git diff --name-only origin/master... src/include/*.h)
-
-# Ensure there is at least one file present in new dir.
-if [ ${#FILES[@]} -eq 0 ];
-then
-    echo No headers modified, exiting.
-    echo ::endgroup::
-    exit 0
-fi
-
-# Make a new directory.
-mkdir src/include2
-
-# Copy the files to new dir.
-cp "${FILES[@]}" ./src/include2
-
-# Move the real files aside
-mv src/include src/include.old
-
-# Move in the ones to test.
-mv src/include2 src/include
-
-echo ::endgroup::
 
 echo ::group::Installing doxygen
 sudo apt-get install doxygen
