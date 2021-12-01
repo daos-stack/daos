6283ee8a0f172f9b4b013e9f372a6fd123c83be4
Brian J. Murrell <brian.murrell@intel.com>
2021-12-01 15:30:22 -0500

    DAOS-8432 ci: Move EL8 testing to 8.5
    Accordingly, add a sanity testing stage for 8.4.
    
    Skip-PR-comments: true
    
    
    #Pragmas from previous commit message:
    Skip-PR-comments: true
    Skip-PR-comments: true
    Skip-PR-comments: true
    Skip-PR-comments: true
    Skip-PR-comments: true
    Skip-PR-comments: true
    Skip-PR-comments: true
    Skip-PR-comments: true
    Skip-PR-comments: true
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100-8.7446.g0bf6c932
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100-8.7446.g0bf6c932
    # RPM-test-version: 2.1.100-8.7446.g0bf6c932
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100
    # RPM-test-version: 2.1.100-8.7446.g0bf6c932
    # RPM-test-version: 2.1.100-8.7446.g0bf6c932
    # RPM-test-version: 2.1.100-8.7446.g0bf6c932
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/provisioning/post_provision_config.sh
+++ ci/provisioning/post_provision_config.sh
@@ -48,6 +48,7 @@ retry_cmd 2400 clush -B -S -l root -w "$NODESTRING" \
            BUILD_URL=\"${BUILD_URL}\"
            STAGE_NAME=\"${STAGE_NAME}\"
            OPERATIONS_EMAIL=\"${OPERATIONS_EMAIL}\"
+           COMMIT_MESSAGE=\"${COMMIT_MESSAGE}\"
            $(cat ci/stacktrace.sh)
            $(cat ci/provisioning/post_provision_config_common.sh)
            $(cat ci/provisioning/post_provision_config_nodes_"${DISTRO}".sh)
