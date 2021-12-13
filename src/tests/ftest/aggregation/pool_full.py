c15c9f306d3729e76af51ceee4358f2749816d61
Ding Ho <ding-hwa.ho@intel.com>
2021-12-13 21:39:59 +0000

    Update general Exception for error handling.
    Skip-unit-tests: true
    Test-tag: aggregate_full
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/aggregation/pool_full.py
+++ src/tests/ftest/aggregation/pool_full.py
@@ -7,8 +7,6 @@
 
 from time import sleep, time
 from ior_test_base import IorTestBase
-from avocado.core.exceptions import TestFail
-from general_utils import DaosTestError
 
 class DaosAggregationFull(IorTestBase):
     # pylint: disable=too-many-ancestors
@@ -99,7 +97,7 @@ class DaosAggregationFull(IorTestBase):
                 self.assertTrue(free_space[ind] <= free_space[ind-1],
                                 "IOR run was not successful.")
                 ind += 1
-            except (TestFail, DaosTestError) as excep:
+            except Exception as excep:
                 out_of_space = True
                 self.log.info("=Error on run_ior_with_pool, out of space.. %s",
                               repr(excep))
