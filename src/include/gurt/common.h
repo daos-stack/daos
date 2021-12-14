43ff20587de09d3c39b53be22935e62395134232
Ashley Pittman <ashley.m.pittman@intel.com>
2021-12-14 17:44:20 +0000

    Tweak realpath macro and job names.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/include/gurt/common.h
+++ src/include/gurt/common.h
@@ -162,13 +162,9 @@ char *d_realpath(const char *path, char *resolved_path);
  * realpath, however if it doesn't fail then we want to preserve the previous errno, in
  * addition the fault injection code could insert an error in the D_CHECK_ALLOC() macro
  * so if that happens then we want to set ENOMEM there.
- * Save the original error on the way in, overwrite it and then if realpath does not change
- * it then re-instate it.
  */
 #define D_REALPATH(ptr, path)						\
 	do {								\
-		int _errno = errno;					\
-		errno = 0;						\
 		(ptr) = d_realpath((path), NULL);			\
 		if ((ptr) != NULL) {					\
 			int _size = 0;					\
@@ -176,10 +172,8 @@ char *d_realpath(const char *path, char *resolved_path);
 			if (_ptr)					\
 				_size = strnlen(_ptr, PATH_MAX + 1) + 1 ; \
 			D_CHECK_ALLOC(realpath, true, ptr, #ptr, _size,	0, #ptr, 0); \
-			if (errno == 0 && ((ptr) == NULL) && _ptr != NULL) \
+			if (((ptr) == NULL))				\
 				errno = ENOMEM;				\
-			else if (errno == 0)				\
-				errno = _errno;				\
 		}							\
 	} while (0)
 
