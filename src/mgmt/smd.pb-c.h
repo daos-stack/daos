b8baf83d0da097cc0032c968c32a0ce4101aa61a
Tom Nabarro <tom.nabarro@intel.com>
2021-12-17 15:48:09 +0000

    DAOS-9240 control: add --show-evicted option to list-devices cmd (#7613)
    Add --show-evicted (-e) option to dmg storage query list-devices command
    to show only faulty devices in an evicted state. This allows admins to
    quickly identify devices that need replacing.
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/mgmt/smd.pb-c.h
+++ src/mgmt/smd.pb-c.h
@@ -352,19 +352,19 @@ struct  _Ctl__SmdQueryReq
   /*
    * query should omit devices
    */
-  protobuf_c_boolean omitdevices;
+  protobuf_c_boolean omit_devices;
   /*
    * query should omit pools
    */
-  protobuf_c_boolean omitpools;
+  protobuf_c_boolean omit_pools;
   /*
    * query should include BIO health for devices
    */
-  protobuf_c_boolean includebiohealth;
+  protobuf_c_boolean include_bio_health;
   /*
    * set the specified device to FAULTY
    */
-  protobuf_c_boolean setfaulty;
+  protobuf_c_boolean set_faulty;
   /*
    * constrain query to this UUID (pool or device)
    */
@@ -378,21 +378,25 @@ struct  _Ctl__SmdQueryReq
    */
   char *target;
   /*
-   * UUID of new device to replace storage with
-   */
-  char *replaceuuid;
-  /*
    * specify if device reint is needed (used for replace cmd)
    */
-  protobuf_c_boolean noreint;
+  protobuf_c_boolean no_reint;
   /*
    * set the VMD LED state to quickly blink
    */
   protobuf_c_boolean identify;
+  /*
+   * show only devices with state matching bit-mask
+   */
+  uint32_t state_mask;
+  /*
+   * UUID of new device to replace storage with
+   */
+  char *replace_uuid;
 };
 #define CTL__SMD_QUERY_REQ__INIT \
  { PROTOBUF_C_MESSAGE_INIT (&ctl__smd_query_req__descriptor) \
-    , 0, 0, 0, 0, (char *)protobuf_c_empty_string, 0, (char *)protobuf_c_empty_string, (char *)protobuf_c_empty_string, 0, 0 }
+    , 0, 0, 0, 0, (char *)protobuf_c_empty_string, 0, (char *)protobuf_c_empty_string, 0, 0, 0, (char *)protobuf_c_empty_string }
 
 
 struct  _Ctl__SmdQueryResp__Device
