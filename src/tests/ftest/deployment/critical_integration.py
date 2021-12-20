6089f05011cb51afedcc467246124710e2f0dbb8
Saurabh Tandan <saurabh.tandan@intel.com>
2021-12-20 12:33:26 +0000

    Corrected lint erros.
    Quick-Functional: true
    Test-tag: criticalintegration
    
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>

--- src/tests/ftest/deployment/critical_integration.py
+++ src/tests/ftest/deployment/critical_integration.py
@@ -7,12 +7,10 @@
 
 import time
 
-from general_utils import run_command
+from general_utils import run_command, DaosTestError
 from ior_test_base import IorTestBase
 from apricot import TestWithServers
 from command_utils_base import CommandFailure
-from general_utils import DaosTestError
-from server_utils import DaosServerManager
 
 class CriticalIntegration(TestWithServers):
     """Test Class Description: Verify the basic integration of
@@ -21,13 +19,17 @@ class CriticalIntegration(TestWithServers):
     :avocado: recursive
     """
 
+    #TO-DO
+    #Provision all daos nodes using provisioning tool provided by HPCM
 
-    def test_passwdlessssh(self):
+    def test_passwdlessssh_versioncheck(self):
         """
         Test Description: Verify passwordless ssh amongst the server
-                          server nodes available.
-        :avocado: tags=hw,small
-        :avocado: tags=installation,criticalintegration,passwdlessssh
+                          server nodes available and verify all server
+                          and client nodes have same daos versions.
+        :avocado: tags=all,deployment,full_regression
+        :avocado: tags=hw,large
+        :avocado: tags=criticalintegration,passwdlessssh_versiocheck
         """
 
         check_remote_root_access = self.params.get("check_remote_root_access", "/run/*")
@@ -35,7 +37,7 @@ class CriticalIntegration(TestWithServers):
         dmg_version_list = []
         for host in self.hostlist_servers:
             daos_server_cmd = ("ssh -oNumberOfPasswordPrompts=0 {}"
-                              " 'daos_server version'".format(host))
+                               " 'daos_server version'".format(host))
             remote_root_access = ("ssh -oNumberOfPasswordPrompts=0 root@{}"
                                   " 'echo hello'".format(host))
             command_for_inter_node = ("pdsh -S -w {}"
@@ -61,10 +63,10 @@ class CriticalIntegration(TestWithServers):
 
 
         result_daos_server = (daos_server_version_list.count(daos_server_version_list[0])
-                             == len(daos_server_version_list))
+                              == len(daos_server_version_list))
         result_dmg = dmg_version_list.count(dmg_version_list[0]) == len(dmg_version_list)
         result_client_server = daos_server_version_list[0][1:] == dmg_version_list[0]
-        
+
         if (result_daos_server and result_dmg and result_client_server):
             self.log.info("All servers have same daos version")
             self.log.info("All clients have same daos version")
@@ -77,10 +79,11 @@ class CriticalIntegration(TestWithServers):
 
     def test_ras(self):
         """
-        Test Description: Verify RAS event on all server nodes from testrunner
-
-        :avocado: tags=hw,small
-        :avocado: tags=installation,criticalintegration,ras
+        Test Description: Verify RAS event on all server nodes from testrunner.
+                          Verify network scan and storage scan for server nodes.
+        :avocado: tags=all,deployment,full_regression
+        :avocado: tags=hw,large
+        :avocado: tags=criticalintegration,ras
         """
 
         dmg = self.get_dmg_command()
