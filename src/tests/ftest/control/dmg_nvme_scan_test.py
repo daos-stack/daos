24a5c467cd18777a605396d039b8c4177a733877
Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>
2021-12-21 20:51:16 +0000

    DAOS-9354 Test: Fix daos_control_config and dmg_nvme_scan_test
    Run daos_control_config and dmg_nvme_scan_test in hw small
    
    Test-tag: control_start nvme_scan
    
    Signed-off-by: Sylvia Oi Yee Chan <sylvia.oi.yee.chan@intel.com>

--- src/tests/ftest/control/dmg_nvme_scan_test.py
+++ src/tests/ftest/control/dmg_nvme_scan_test.py
@@ -31,7 +31,7 @@ class DmgNvmeScanTest(TestWithServers):
         JIRA ID: DAOS-2485
         Test Description: Test basic dmg functionality to scan the nvme storage.
         on the system.
-        :avocado: tags=all,tiny,daily_regression,dmg,nvme_scan,basic
+        :avocado: tags=all,tiny,hw,small,daily_regression,dmg,nvme_scan,basic
         """
         # Create dmg command
         dmg = DmgCommand(os.path.join(self.prefix, "bin"))
