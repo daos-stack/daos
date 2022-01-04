642b57835f689ec3303bf1199aedbc04141f5676
Maureen Jean <maureen.jean@intel.com>
2022-01-04 14:12:22 -0500

    change yum to dnf
    Skip-build-ubuntu20-rpm: true
    Quick-Functional: true
    Test-tag: soak_smoke offline_extend_oclass
    PR-repos: slurm@PR-9
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/slurm_setup.py
+++ src/tests/ftest/slurm_setup.py
@@ -160,14 +160,14 @@ def configuring_packages(args, action):
         action (str):  install or remove
 
     """
-    # Install yum packages on control and compute nodes
+    # Install packages on control and compute nodes
     all_nodes = NodeSet("{},{}".format(str(args.control), str(args.nodes)))
     cmd_list = []
     for package in PACKAGE_LIST:
         if PACKAGE_VERSION:
             package = package + "-" + PACKAGE_VERSION
         logging.info("%s %s on %s", action, package, all_nodes)
-        cmd_list.append("yum {} -y ".format(action) + package)
+        cmd_list.append("dnf {} -y ".format(action) + package)
     return execute_cluster_cmds(all_nodes, cmd_list, args.sudo)
 
 
