2130a5ad5fc1b70ce533a9dddf6a786a0b382562
jiafu zhang <jiafu.zhang@intel.com>
2022-01-07 02:11:12 +0000

    DAOS-7963 Java Client build failing on Fedora
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- utils/scripts/install-centos8-java.sh
+++ utils/scripts/install-centos8-java.sh
@@ -5,16 +5,8 @@
 # installed specifically.
 # For centos, we can install both java-8 and maven from repo.
 
-fedora=$1
-
-if "$fedora"; then
-	dnf install java-1.8.0-openjdk java-1.8.0-openjdk-devel
-        curl --output ./apache-maven-3.6.3-bin.tar.gz \
-https://archive.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz \
-&& tar -xf apache-maven-3.6.3-bin.tar.gz \
-&& find ./ -name "mvn" | \
-{ read -r d; f="$(dirname "$d")"; cp -r "$(readlink -f "$f")"/../* /usr/local; }
-
+if [ -e /etc/fedora-release ]; then
+	dnf install java-1.8.0-openjdk java-1.8.0-openjdk-devel maven-openjdk8
 else
         dnf install java-1.8.0-openjdk maven
 fi
