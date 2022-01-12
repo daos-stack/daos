989a14765bcbd5df018857b427f308f82e03207f
Ashley Pittman <ashley.m.pittman@intel.com>
2022-01-12 18:01:15 +0000

    Increase dmg timeout.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/control/server/harness.go
+++ src/control/server/harness.go
@@ -1,5 +1,5 @@
 //
-// (C) Copyright 2019-2021 Intel Corporation.
+// (C) Copyright 2019-2022 Intel Corporation.
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -27,7 +27,7 @@ import (
 )
 
 const (
-	rankReqTimeout   = 30 * time.Second
+	rankReqTimeout   = 120 * time.Second
 	rankStartTimeout = 2 * rankReqTimeout
 )
 
