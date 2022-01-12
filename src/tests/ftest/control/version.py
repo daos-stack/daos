5783353f26803f017ce082ea2c0a293281d8eebf
Makito Kano <makito.kano@intel.com>
2022-01-12 11:55:13 +0000

    DAOS-8539 test: Added test files
    Skip-unit-tests: true
    Test-tag-vm: pr version_number
    Signed-off-by: Makito Kano <makito.kano@intel.com>

---
+++ src/tests/ftest/control/version.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+'''
+  (C) Copyright 2018-2021 Intel Corporation.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+'''
+import re
+
+from apricot import TestWithServers
+from general_utils import run_pcmd
+
+
+class DAOSVersion(TestWithServers):
+    """JIRA ID: DAOS-8539
+
+    Verify version number and the output string.
+
+    :avocado: recursive
+    """
+    def test_version(self):
+        """Verify version number for dmg, daos, daos_server, and daos_agent
+        against RPM.
+
+        :avocado: tags=all,full_regression
+        :avocado: tags=vm
+        :avocado: tags=control,version_number
+        """
+        errors = []
+
+        # Get RPM version.
+        rpm_command = "rpm -qa|grep daos-server"
+        output = run_pcmd(hosts=self.hostlist_servers, command=rpm_command)
+        stdout = output[0]["stdout"][0]
+        result = re.findall("daos-server-([\d.]+)", stdout)
+        rpm_version = result[0]
+        self.log.info("RPM version = %s", rpm_version)
+
+        # Get dmg version.
+        dmg_cmd = self.get_dmg_command()
+        output = dmg_cmd.version().stdout.decode("utf-8")
+
+        # Verify that "dmg version" is in the output.
+        if "dmg version" not in output:
+            errors.append("dmg version is not in the output! {}".format(output))
+
+        result = re.findall("dmg version ([\d.]+)", output)
+        dmg_version = result[0]
+        self.log.info("dmg version = %s", dmg_version)
+
+        # Get daos version.
+        daos_cmd = self.get_daos_command()
+        output = daos_cmd.version().stdout.decode("utf-8")
+
+        # Verify that "daos version" is in the output.
+        if "daos version" not in output:
+            errors.append(
+                "daos version is not in the output! {}".format(output))
+
+        result = re.findall("daos version ([\d.]+)", output)
+        daos_version = result[0]
+        self.log.info("daos version = %s", daos_version)
+
+        # Get daos_agent version.
+        daos_agent_cmd = "daos_agent version"
+        output = run_pcmd(hosts=self.hostlist_servers, command=daos_agent_cmd)
+        stdout = output[0]["stdout"][0]
+
+        # Verify that "DAOS Agent" is in the output.
+        if "DAOS Agent" not in stdout:
+            errors.append("DAOS Agent is not in the output! {}".format(stdout))
+
+        result = re.findall("DAOS Agent v([\d.]+)", stdout)
+        daos_agent_version = result[0]
+        self.log.info("daos_agent version = %s", daos_agent_version)
+
+        # Get daos_server version
+        daos_server_cmd = "daos_server version"
+        output = run_pcmd(hosts=self.hostlist_servers, command=daos_server_cmd)
+        stdout = output[0]["stdout"][0]
+
+        # Verify that "DAOS Control Server" is in the output.
+        if "DAOS Control Server" not in stdout:
+            errors.append(
+                "DAOS Control Server is not in the output! {}".format(stdout))
+
+        result = re.findall("DAOS Control Server v([\d.]+)", stdout)
+        daos_server_version = result[0]
+        self.log.info("daos_server version = %s", daos_server_version)
+
+        # Verify the tool versions against the RPM.
+        tool_versions = [
+            ("dmg", dmg_version),
+            ("daos", daos_version),
+            ("daos_agent", daos_agent_version),
+            ("daos_server", daos_server_version)
+        ]
+
+        for tool_version in tool_versions:
+            tool = tool_version[0]
+            version = tool_version[1]
+            if version != rpm_version:
+                msg = "Unexpected {} version! {} = {}, RPM = {}".format(
+                    tool, tool, version, rpm_version)
+                errors.append(msg)
+
+        if errors:
+            self.fail("\n----- Errors detected! -----\n{}".format(
+                "\n".join(errors)))
