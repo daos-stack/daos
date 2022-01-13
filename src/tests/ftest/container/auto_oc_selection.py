56133b192635e2d0ac6f00e9a0980e7fa24508fb
Makito Kano <makito.kano@intel.com>
2022-01-13 13:18:37 +0000

    DAOS-7595 test: Refactored
    Skip-unit-tests: true
    Test-tag: oc_selection
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/container/auto_oc_selection.py
+++ src/tests/ftest/container/auto_oc_selection.py
@@ -96,5 +96,5 @@ class AutoOCSelectionTest(TestWithServers):
                     errors.append(msg)
 
         if errors:
-            self.fail("\n----- Errors detected! -----\n{}".format(
+            self.fail("\n---- Errors detected! ----\n{}".format(
                 "\n".join(errors)))
