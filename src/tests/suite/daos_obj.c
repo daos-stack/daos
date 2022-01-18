6fddb93db736549880171b5d6e26365e855c623e
Xuezhao Liu <xuezhao.liu@intel.com>
2022-01-18 09:10:17 +0000

    DAOS-9654 obj: check iov_buf_len in daos_iov_left()
    User may provide fetch buffer iov with zero iov_len, so need to check
    iov_buf_len rather than iov_len in daos_iov_left().
    
    Signed-off-by: Xuezhao Liu <xuezhao.liu@intel.com>

--- src/tests/suite/daos_obj.c
+++ src/tests/suite/daos_obj.c
@@ -2203,8 +2203,21 @@ next_step:
 	assert_rc_equal(rc, 0);
 
 	/** fetch */
-	iod.iod_size	= DAOS_REC_ANY;
+	print_message("fetch with zero iov_len\n");
+	iod.iod_size	= 1;
+	recx[0].rx_idx	= 0;
+	recx[0].rx_nr	= tmp_len * 1;
+	iod.iod_nr	= 1;
+	sg_iov[0].iov_len = 0;
+	rc = daos_obj_fetch(oh, DAOS_TX_NONE, 0, &dkey, 1, &iod, &sgl, NULL,
+			    NULL);
+	assert_rc_equal(rc, 0);
+
 	print_message("reading data back with less buffer ...\n");
+	recx[0].rx_idx	= 0;
+	recx[0].rx_nr	= tmp_len;
+	iod.iod_nr	= 3;
+	iod.iod_size	= DAOS_REC_ANY;
 	buf_out = step == 1 ? stack_buf_out : bulk_buf_out;
 	memset(buf_out, 0, buf_len);
 	tmp_len = buf_len / 2;
