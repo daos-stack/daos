7289e186c0fb56bbafe82a61b3dd2eea2c37fe10
Wang Shilong <shilong.wang@intel.com>
2022-01-20 10:01:14 +0800

    bump container version as well
    Signed-off-by: Wang Shilong <shilong.wang@intel.com>

--- src/container/srv_layout.h
+++ src/container/srv_layout.h
@@ -32,7 +32,7 @@
 #include <daos_types.h>
 
 /* Default layout version */
-#define DS_CONT_MD_VERSION 4
+#define DS_CONT_MD_VERSION 5
 
 /* Lowest compatible layout version */
 #define DS_CONT_MD_VERSION_LOW 4
