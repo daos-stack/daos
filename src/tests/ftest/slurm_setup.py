32d854ffe57990489717b7735d6301a909c0b463
Maureen Jean <maureen.jean@intel.com>
2022-01-20 16:05:50 -0500

    remove version from slurm setup
    Quick-Functional: true
    Test-tag: offline_extend_oclass soak_smoke
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/slurm_setup.py
+++ src/tests/ftest/slurm_setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-  (C) Copyright 2018-2021 Intel Corporation.
+  (C) Copyright 2018-2022 Intel Corporation.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
@@ -20,16 +20,14 @@ from avocado.utils.distro import detect
 
 distro_info = detect()
 SLURM_CONF = "/etc/slurm/slurm.conf"
-
+PACKAGE_VERSION = None
 if "suse" in distro_info.name.lower():
     distro = "suse.lp153"
-    PACKAGE_VERSION = "21.08.5.1-1.{}.x86_64".format(distro)
 elif "centos" in distro_info.name.lower() and distro_info.version == "7":
     distro = "el7"
     PACKAGE_VERSION = "18.08.8-1.{}.x86_64".format(distro)
 elif "centos" in distro_info.name.lower() and distro_info.version == "8":
     distro = "el8"
-    PACKAGE_VERSION = "21.08.5.1-1.{}.x86_64".format(distro)
 
 PACKAGE_LIST = ["slurm", "slurm-example-configs",
                 "slurm-slurmctld", "slurm-slurmd"]
