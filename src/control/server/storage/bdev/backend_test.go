daos-stack/daos

commit ccf0027af99ce803c3642787426f00f6d1924d19

DAOS-9146 engine: some fixes for daos server (#7490)
Some codes improvements:

1. some required arguments could be not negative, otherwise
it will trigger assertions in bio_nvme_init(), declare them as
unsigned int and use strtoul() for better error handling.

2. If nvme isn't configured, it is unnecessary to start NVME poll
ULT which will waste CPU cycles and introduce overhead to ARGOBOT
scheduling.

3. Document min hugepages requirement in the daos_server.yml

4. When NVMe isn't configured, BIO will just allocate normal memory as
DMA buffer no matter if hugepages is configured (see dma_alloc_chunk()).
It should be changed to always using hugepages when hugepage is configured.
When NVMe isn't configured, the hugepage settings will be ignored (see bio_nvme_init()).
we shouldn't ignore hugepages setting no matter if NVMe is present.

5. fix hugepages parsing
- fix to check if SPDK init properly in engine
- now we support hugepages for SCM only case, don't
skip related codes in control parts.
- Fix to use configured hugepages rather than system
avail free hugepages.
- support setting hugepages as 0 which means disable
hugepages for SCM only case.

Signed-off-by: Wang Shilong <shilong.wang@intel.com>

src/control/server/storage/bdev/runner.go
@@ -99,7 +99,7 @@ func defaultScriptRunner(log logging.Logger) *spdkSetupScript {
 // NOTE: will make the controller disappear from /dev until reset() called.
 func (s *spdkSetupScript) Prepare(req *storage.BdevPrepareRequest) error {
 	nrHugepages := req.HugePageCount
-	if nrHugepages <= 0 {
+	if nrHugepages < 0 {
 		nrHugepages = defaultNrHugepages
 	}
 

src/control/server/storage/bdev/backend_test.go
@@ -934,6 +934,7 @@ func TestBackend_Prepare(t *testing.T) {
 		},
 		"prepare setup; defaults": {
 			req: storage.BdevPrepareRequest{
+				HugePageCount:         -1,
 				TargetUser:            username,
 				EnableVMD:             false,
 				DisableCleanHugePages: true,

src/bio/bio_buffer.c
@@ -17,7 +17,7 @@ dma_free_chunk(struct bio_dma_chunk *chunk)
 	D_ASSERT(chunk->bdc_ref == 0);
 	D_ASSERT(d_list_empty(&chunk->bdc_link));
 
-	if (bio_nvme_configured())
+	if (bio_spdk_inited)
 		spdk_dma_free(chunk->bdc_ptr);
 	else
 		free(chunk->bdc_ptr);
@@ -39,7 +39,7 @@ dma_alloc_chunk(unsigned int cnt)
 		return NULL;
 	}
 
-	if (bio_nvme_configured()) {
+	if (bio_spdk_inited) {
 		chunk->bdc_ptr = spdk_dma_malloc(bytes, BIO_DMA_PAGE_SZ, NULL);
 	} else {
 		rc = posix_memalign(&chunk->bdc_ptr, BIO_DMA_PAGE_SZ, bytes);

src/bio/bio_internal.h
@@ -487,6 +487,7 @@ struct media_error_msg {
 
 /* bio_xstream.c */
 extern bool		bio_scm_rdma;
+extern bool		bio_spdk_inited;
 extern unsigned int	bio_chk_sz;
 extern unsigned int	bio_chk_cnt_max;
 int xs_poll_completion(struct bio_xs_context *ctxt, unsigned int *inflights,

src/bio/bio_xstream.c
@@ -46,6 +46,8 @@ unsigned int bio_chk_cnt_max;
 static unsigned int bio_chk_cnt_init;
 /* Diret RDMA over SCM */
 bool bio_scm_rdma;
+/* Whether SPDK inited */
+bool bio_spdk_inited;
 
 struct bio_nvme_data {
 	ABT_mutex		 bd_mutex;
@@ -79,21 +81,19 @@ bio_spdk_env_init(void)
 	struct spdk_env_opts	 opts;
 	int			 rc;
 
-	D_ASSERT(nvme_glb.bd_nvme_conf != NULL);
-
 	/* Only print error and more severe to stderr. */
 	spdk_log_set_print_level(SPDK_LOG_ERROR);
 
 	spdk_env_opts_init(&opts);
 	opts.name = "daos_engine";
 
-	rc = bio_add_allowed_alloc(nvme_glb.bd_nvme_conf, &opts);
-	if (rc != 0) {
-		D_ERROR("Failed to add allowed devices to SPDK env, "DF_RC"\n",
-			DP_RC(rc));
-		if (opts.pci_allowed != NULL)
-			D_FREE(opts.pci_allowed);
-		return rc;
+	if (nvme_glb.bd_nvme_conf != NULL) {
+		rc = bio_add_allowed_alloc(nvme_glb.bd_nvme_conf, &opts);
+		if (rc != 0) {
+			D_ERROR("Failed to add allowed devices to SPDK env, "DF_RC"\n",
+				DP_RC(rc));
+			goto out;
+		}
 	}
 
 	/*
@@ -142,13 +142,23 @@ bypass_health_collect()
 }
 
 int
-bio_nvme_init(const char *nvme_conf, int shm_id, int mem_size,
-	      int hugepage_size, int tgt_nr, struct sys_db *db,
-	      bool bypass_health_collect)
+bio_nvme_init(const char *nvme_conf, int shm_id, unsigned int mem_size,
+	      unsigned int hugepage_size, unsigned int tgt_nr,
+	      struct sys_db *db, bool bypass_health_collect)
 {
 	char		*env;
 	int		 rc, fd;
 	unsigned int	 size_mb = DAOS_DMA_CHUNK_MB;
+
+	if (tgt_nr <= 0) {
+		D_ERROR("tgt_nr: %u should be > 0\n", tgt_nr);
+		return -DER_INVAL;
+	}
+
+	if (nvme_conf && strlen(nvme_conf) > 0 && mem_size == 0) {
+		D_ERROR("Hugepages must be configured when NVMe SSD is configured\n");
+		return -DER_INVAL;
+	}
 
 	nvme_glb.bd_xstream_cnt = 0;
 	nvme_glb.bd_init_thread = NULL;
@@ -174,22 +184,14 @@ bio_nvme_init(const char *nvme_conf, int shm_id, int mem_size,
 	d_getenv_bool("DAOS_SCM_RDMA_ENABLED", &bio_scm_rdma);
 	D_INFO("RDMA to SCM is %s\n", bio_scm_rdma ? "enabled" : "disabled");
 
-	if (nvme_conf == NULL || strlen(nvme_conf) == 0) {
-		D_INFO("NVMe config isn't specified, skip NVMe setup.\n");
+	/* Hugepages disabled */
+	if (mem_size == 0) {
+		D_INFO("Set per-xstream DMA buffer upper bound to %u %uMB chunks\n",
+			bio_chk_cnt_max, size_mb);
+		D_INFO("Hugepages are not specified, skip NVMe setup.\n");
 		return 0;
 	}
 
-	fd = open(nvme_conf, O_RDONLY, 0600);
-	if (fd < 0) {
-		D_WARN("Open %s failed, skip DAOS NVMe setup "DF_RC"\n",
-		       nvme_conf, DP_RC(daos_errno2der(errno)));
-		return 0;
-	}
-	close(fd);
-
-	D_ASSERT(tgt_nr > 0);
-	D_ASSERT(mem_size > 0);
-	D_ASSERT(hugepage_size > 0);
 	/*
 	 * Hugepages are not enough to sustain average I/O workload
 	 * (~1GB per xstream).
@@ -201,6 +203,16 @@ bio_nvme_init(const char *nvme_conf, int shm_id, int mem_size,
 		return -DER_INVAL;
 	}
 
+	if (nvme_conf && strlen(nvme_conf) > 0) {
+		fd = open(nvme_conf, O_RDONLY, 0600);
+		if (fd < 0)
+			D_WARN("Open %s failed, skip DAOS NVMe setup "DF_RC"\n",
+			       nvme_conf, DP_RC(daos_errno2der(errno)));
+		else
+			close(fd);
+	}
+
+	D_ASSERT(hugepage_size > 0);
 	bio_chk_cnt_max = (mem_size / tgt_nr) / size_mb;
 	D_INFO("Set per-xstream DMA buffer upper bound to %u %uMB chunks\n",
 	       bio_chk_cnt_max, size_mb);
@@ -235,6 +247,7 @@ bio_nvme_init(const char *nvme_conf, int shm_id, int mem_size,
 		nvme_glb.bd_nvme_conf = NULL;
 		goto fini_smd;
 	}
+	bio_spdk_inited = true;
 
 	return 0;
 
@@ -251,7 +264,7 @@ free_mutex:
 static void
 bio_spdk_env_fini(void)
 {
-	if (nvme_glb.bd_nvme_conf != NULL) {
+	if (bio_spdk_inited) {
 		spdk_thread_lib_fini();
 		spdk_env_fini();
 	}

src/bio/smd/smd_store.c
@@ -97,6 +97,9 @@ smd_db_unlock(void)
 void
 smd_fini(void)
 {
+	if (!smd_db_ready())
+		return;
+
 	smd_db = NULL;
 }
 

src/control/server/config/server.go
@@ -314,6 +314,7 @@ func DefaultServer() *Server {
 	return &Server{
 		SystemName:         build.DefaultSystemName,
 		SocketDir:          defaultRuntimeDir,
+		NrHugepages:        -1, // mapped to 4096 by default.
 		AccessPoints:       []string{fmt.Sprintf("localhost:%d", build.DefaultControlPort)},
 		ControlPort:        build.DefaultControlPort,
 		TransportConfig:    security.DefaultServerTransportConfig(),

src/control/server/server_utils.go
@@ -40,7 +40,7 @@ type resolveTCPFn func(string, string) (*net.TCPAddr, error)
 
 const (
 	iommuPath        = "/sys/class/iommu"
-	minHugePageCount = 128
+	minHugePageCount = 0
 )
 
 func cfgHasBdevs(cfg *config.Server) bool {
@@ -176,11 +176,14 @@ func prepBdevStorage(srv *server, iommuEnabled bool, hpiGetter common.GetHugePag
 	}
 
 	hasBdevs := cfgHasBdevs(srv.cfg)
+	// Use default value
+	if srv.cfg.NrHugepages < 0 {
+		srv.cfg.NrHugepages = 4096
+	}
+	// The config value is intended to be per-engine, so we need to adjust
+	// based on the number of engines.
+	prepReq.HugePageCount = srv.cfg.NrHugepages * len(srv.cfg.Engines)
 	if hasBdevs {
-		// The config value is intended to be per-engine, so we need to adjust
-		// based on the number of engines.
-		prepReq.HugePageCount = srv.cfg.NrHugepages * len(srv.cfg.Engines)
-
 		// Perform these checks to avoid even trying a prepare if the system
 		// isn't configured properly.
 		if srv.runningUser != "root" {
@@ -213,24 +216,22 @@ func prepBdevStorage(srv *server, iommuEnabled bool, hpiGetter common.GetHugePag
 		return errors.Wrap(err, "unable to read system hugepage info")
 	}
 
-	if hasBdevs {
-		// Double-check that we got the requested number of huge pages after prepare.
-		if hugePages.Free < prepReq.HugePageCount {
-			return FaultInsufficientFreeHugePages(hugePages.Free, prepReq.HugePageCount)
-		}
+	// Double-check that we got the requested number of huge pages after prepare.
+	if hugePages.Free < prepReq.HugePageCount {
+		return FaultInsufficientFreeHugePages(hugePages.Free, prepReq.HugePageCount)
 	}
 
 	for _, engineCfg := range srv.cfg.Engines {
 		// Calculate mem_size per I/O engine (in MB)
 		PageSizeMb := hugePages.PageSizeKb >> 10
-		engineCfg.MemSize = hugePages.Free / len(srv.cfg.Engines)
+		engineCfg.MemSize = prepReq.HugePageCount / len(srv.cfg.Engines)
 		engineCfg.MemSize *= PageSizeMb
 		// Pass hugepage size, do not assume 2MB is used
 		engineCfg.HugePageSz = PageSizeMb
 		srv.log.Debugf("MemSize:%dMB, HugepageSize:%dMB", engineCfg.MemSize, engineCfg.HugePageSz)
 		// Warn if hugepages are not enough to sustain average
-		// I/O workload (~1GB), ignore warning if only using SCM backend
-		if !hasBdevs {
+		// I/O workload (~1GB), ignore warning if using SCM backend with 0 hugepages
+		if !hasBdevs && engineCfg.MemSize == 0 {
 			continue
 		}
 		if (engineCfg.MemSize / engineCfg.TargetCount) < 1024 {

commit 91116ba76079f70b27f5b7f339827ed700e9170c

DAOS-9774 control: fix daos_server storage prepare nr_hugepage bug
Regression was introduced in commit ccf0027af99ce803c3642787426f00f6d1924d19.

daos_server storage prepare can call SPDK setup with NRHUGE set to 0
which results in the NVMe controllers being inaccessible and reporting
permission denied.

The fix applied is to always set hugepages explicitly to a non-zero
value when calling storage prepare for NVMe, otherwise devices will
not be accessible. Use default value recommended by SPDK and if no
hugepages are required then storage prepare for NVMe should not be
called (only call if NVMe devices are to be scanned or used by engine).

Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

src/control/server/storage/bdev/runner.go
@@ -20,7 +20,7 @@ import (
 
 const (
 	spdkSetupPath      = "../share/daos/control/setup_spdk.sh"
-	defaultNrHugepages = 4096
+	defaultNrHugepages = 1024 // default number applied by SPDK
 	nrHugepagesEnv     = "_NRHUGE"
 	targetUserEnv      = "_TARGET_USER"
 	pciAllowListEnv    = "_PCI_ALLOWED"
@@ -99,7 +99,9 @@ func defaultScriptRunner(log logging.Logger) *spdkSetupScript {
 // NOTE: will make the controller disappear from /dev until reset() called.
 func (s *spdkSetupScript) Prepare(req *storage.BdevPrepareRequest) error {
 	nrHugepages := req.HugePageCount
-	if nrHugepages < 0 {
+	// Always supply non-zero number of hugepages in request otherwise devices cannot be
+	// accessed.
+	if nrHugepages <= 0 {
 		nrHugepages = defaultNrHugepages
 	}
 

src/control/server/storage/bdev/backend_test.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
+	"sort"
 	"syscall"
 	"testing"
 	"time"
@@ -938,7 +939,6 @@ func TestBackend_Prepare(t *testing.T) {
 		},
 		"prepare setup; defaults": {
 			req: storage.BdevPrepareRequest{
-				HugePageCount:         -1,
 				TargetUser:            username,
 				EnableVMD:             false,
 				DisableCleanHugePages: true,
@@ -1216,6 +1216,21 @@ func TestBackend_Prepare(t *testing.T) {
 				},
 			},
 		},
+		"prepare setup; -1 huge pages": {
+			req: storage.BdevPrepareRequest{
+				HugePageCount: -1,
+				TargetUser:    username,
+			},
+			expScriptCalls: &[]scriptCall{
+				{
+					Env: []string{
+						fmt.Sprintf("PATH=%s", os.Getenv("PATH")),
+						fmt.Sprintf("%s=%d", nrHugepagesEnv, defaultNrHugepages),
+						fmt.Sprintf("%s=%s", targetUserEnv, username),
+					},
+				},
+			},
+		},
 		"prepare setup; huge page clean enabled; clean fail": {
 			req: storage.BdevPrepareRequest{
 				HugePageCount:         testNrHugePages,
@@ -1270,8 +1285,19 @@ func TestBackend_Prepare(t *testing.T) {
 				return
 			}
 
-			if diff := cmp.Diff(tc.expScriptCalls, calls); diff != "" {
-				t.Fatalf("\nunexpected cmd env (-want, +got):\n%s\n", diff)
+			if len(*tc.expScriptCalls) != len(*calls) {
+				t.Fatalf("\nunmatched number of calls (-want, +got):\n%s\n",
+					cmp.Diff(tc.expScriptCalls, calls))
+			}
+			for i, expected := range *tc.expScriptCalls {
+				// env list doesn't need to be ordered
+				sort.Strings(expected.Env)
+				actual := (*calls)[i]
+				sort.Strings(actual.Env)
+
+				if diff := cmp.Diff(expected, actual); diff != "" {
+					t.Fatalf("\nunexpected cmd env (-want, +got):\n%s\n", diff)
+				}
 			}
 
 		})