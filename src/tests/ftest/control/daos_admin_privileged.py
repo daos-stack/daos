9f483415211227e26f6c49a33bbea52ea9382eec
Ding Ho <ding-hwa.ho@intel.com>
2022-01-25 14:33:39 +0000

    Update with NVME storage prepare and test.
    Skip-unit-tests: true
    Test-repeat: 30
    Test-tag: daos_admin
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/control/daos_admin_privileged.py
+++ src/tests/ftest/control/daos_admin_privileged.py
@@ -31,7 +31,9 @@ class DaosAdminPrivTest(TestWithServers):
             Test daos_admin functionality to perform format privileged
             operations while daos_server is run as normal user.
 
-        :avocado: tags=all,pr,daily_regression,hw,small,daos_admin,basic
+        :avocado: tags=all,pr,daily_regression
+        :avocado: tags=hw,small
+        :avocado: tags=daos_admin,basic
         """
         # Verify that daos_admin has the correct permissions
         self.log.info("Checking daos_admin binary permissions")
@@ -57,42 +59,44 @@ class DaosAdminPrivTest(TestWithServers):
         # Get user
         user = getpass.getuser()
 
-        # Uncomment the below line after DAOS-9352 is resolved
         # Prep server for format, run command under non-root user
         self.log.info("Performing SCM storage prepare")
         try:
             self.server_managers[0].prepare_storage(user, True, False)
         except ServerFailed as error:
-        #     self.fail("(1)Failed preparing SCM as user {}: {}".format(user, error))
-            self.log.info("##Failed preparing SCM as user {}: {}".format(user, error))
+            self.fail(
+                "(1)Failed preparing SCM as user {}: {}".format(user, error))
 
-#        self.log.info("(1)Performing NVMe storage prepare")
-#        try:
-#            self.server_managers[0].prepare_storage(user, False, True)
-#        except ServerFailed as err:
-#            self.fail(
-#                "##(1)Failed preparing NVMe as user {}: {}".format(user, err))
+        self.log.info("(2)Performing NVMe storage prepare")
+        try:
+            self.server_managers[0].prepare_storage(user, False, True)
+        except ServerFailed as err:
+            self.fail(
+                "##(2)Failed preparing NVMe as user {}: {}".format(user, err))
 
         # Start server
-        self.log.info("(2)Starting server as non-root")
+        self.log.info("(3)Starting server as non-root")
         try:
             self.server_managers[0].detect_format_ready()
         except ServerFailed as error:
             self.fail(
-                "##(2)Failed starting server before format as non-root user: "
+                "##(3)Failed starting server before format as non-root user: "
                 "{}".format(error))
 
         # Run format command under non-root user
-        self.log.info("(3)Performing SCM format")
+        self.log.info("(4)Performing SCM format")
         result = self.server_managers[0].dmg.storage_format()
         if result is None:
-            self.fail("##(3)Failed to format storage")
+            self.fail("##(4)Failed to format storage")
 
-        # Verify format success when all the daos_engine start. Use dmg to detect server start.
+        # Verify format success when all the daos_engine start.
+        # Use dmg to detect server start.
+        self.log.info(
+            "(5)Verify format success when all the daos_engine start")
         try:
             self.server_managers[0].detect_start_via_dmg = True
             self.server_managers[0].detect_engine_start()
         except ServerFailed as error:
             self.fail(
-                "Failed starting server after format as non-root user: "
+                "##(5)Failed starting server after format as non-root user: "
                 "{}".format(error))
