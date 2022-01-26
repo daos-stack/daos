5d3e08dbc7542fb5f07f0751b047d93394f4982f
Ashley Pittman <ashley.m.pittman@intel.com>
2022-01-26 22:56:44 +0000

    Add the numactl pacakge.
    Quick-Functional: true
    Test-tag: fiosmall
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/functional/required_packages.sh
+++ ci/functional/required_packages.sh
@@ -21,7 +21,7 @@ elif [[ $distro = el* ]] || [[ $distro = centos* ]] ||
         openmpi="openmpi3"
     fi
 
-    pkgs="$prefix ndctl                \
+    pkgs="$prefix ndctl numactl        \
           fio patchutils ior           \
           romio-tests                  \
           testmpio                     \
