f9b320866cf4391dfe29f8f27230b0152dd18db6
Makito Kano <makito.kano@intel.com>
2022-02-01 17:41:30 +0000

    DAOS-9776 test: Update the ticket number
    Skip-unit-tests: true
    Test-tag: ec_online_rebuild_fio ec_offline_rebuild_fio
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/erasurecode/rebuild_fio.py
+++ src/tests/ftest/erasurecode/rebuild_fio.py
@@ -60,6 +60,7 @@ class EcodFioRebuild(ErasureCodeFio):
             # Read and verify the original data.
             self.fio_cmd.run()
 
+    @skipForTicket("DAOS-9783","DAOS-9785")
     def test_ec_online_rebuild_fio(self):
         """Jira ID: DAOS-7320.
 
@@ -82,6 +83,7 @@ class EcodFioRebuild(ErasureCodeFio):
         """
         self.execution('on-line')
 
+    @skipForTicket("DAOS-9783","DAOS-9785")
     def test_ec_offline_rebuild_fio(self):
         """Jira ID: DAOS-7320.
 
