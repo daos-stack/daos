9571fb53152a36f902fbaca1507d8d7e45da6eec
Jeff Olivier <jeffrey.v.olivier@intel.com>
2022-02-03 14:18:51 -0700

    DAOS-623 test: Test code owners setting
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/vos/vos_obj.c
+++ src/vos/vos_obj.c
@@ -16,6 +16,7 @@
 #include <daos_types.h>
 #include <daos_srv/vos.h>
 #include <vos_internal.h>
+/** This is just a test */
 
 /** Ensure the values of recx flags map to those exported by evtree */
 D_CASSERT((uint32_t)VOS_VIS_FLAG_UNKNOWN == (uint32_t)EVT_UNKNOWN);
