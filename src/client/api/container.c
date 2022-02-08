c875f8900f1fee0551b79e71b5e7b8fb5e4b1a82
Lei Huang <wiliam_huang@hotmail.com>
2022-02-08 22:18:58 +0000

    DAOS-9199 API: Provide weak symbols for daos_cont_open and daos_cont_destroy.
    Expose daos_cont_open and daos_cont_destroy in binary. Generate weak
    symbols daos_cont_open2() pointing to daos_cont_open and
    daos_cont_destroy2 pointing to daos_cont_destroy.
    
    Signed-off-by: Lei Huang <wiliam_huang@hotmail.com>

--- src/client/api/container.c
+++ src/client/api/container.c
@@ -172,6 +172,11 @@ daos_cont_open(daos_handle_t poh, const char *cont, unsigned int flags,
 }
 
 int
+daos_cont_open2(daos_handle_t poh, const char *cont, unsigned int flags,
+		daos_handle_t *coh, daos_cont_info_t *info, daos_event_t *ev)
+		__attribute__ ((weak, alias("daos_cont_open")));
+
+int
 daos_cont_close(daos_handle_t coh, daos_event_t *ev)
 {
 	daos_cont_close_t	*args;
@@ -216,6 +221,11 @@ daos_cont_destroy(daos_handle_t poh, const char *cont, int force,
 
 	return dc_task_schedule(task, true);
 }
+
+int
+daos_cont_destroy2(daos_handle_t poh, const char *cont, int force,
+		   daos_event_t *ev)
+		   __attribute__ ((weak, alias("daos_cont_destroy")));
 
 int
 daos_cont_query(daos_handle_t coh, daos_cont_info_t *info,
