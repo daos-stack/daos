16156ba93d1250515ea464ff6b0254ca512a3d28
Ding Ho <ding-hwa.ho@intel.com>
2022-02-08 01:35:10 +0000

    Update fixes of pylint error.
    Skip-unit-tests: true
    Test-repeat: 1
    Test-tag: posix
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/posix/simul.py
+++ src/tests/ftest/posix/simul.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 """
-  (C) Copyright 2018-2021 Intel Corporation.
+  (C) Copyright 2018-2022 Intel Corporation.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
 
-import distro
 from avocado.core.exceptions import TestFail
 from dfuse_test_base import DfuseTestBase
 from env_modules import load_mpi
