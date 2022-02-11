baacc9c9944a81bb97924d5facea067930c18e7b
Lei Huang <wiliam_huang@hotmail.com>
2022-02-11 04:09:25 +0000

    DAOS-9817 NULL: Testing with repeating group_test
    Skip-checkpatch: true
    Parallel-build: true
    Quick-Functional: true
    Skip-unit-tests: true
    Skip-nlt: true
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-coverity-test: true
    Skip-func-test: true
    Skip-func-test-vm: true
    Skip-func-test-memcheck-centos7: true
    Skip-func-test-memcheck-centos8: false
    Skip-func-hw-test: true
    Skip-scan-centos-rpms: true
    Skip-test-centos-rpms: true
    Skip-fault-injection-test: true
    RPM-test: false
    Skip-python-bandit: true
    Test-tag: group_test
    
    Signed-off-by: Lei Huang <wiliam_huang@hotmail.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -2229,7 +2229,7 @@ def main():
     parser.add_argument(
         "-re", "--repeat",
         action="store",
-        default=1,
+        default=100,
         type=int,
         help="number of times to repeat test execution")
     parser.add_argument(
