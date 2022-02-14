18aeb2ecba6e8e6fa0cb118495b4f193cab7fc4a
Brian J. Murrell <brian.murrell@intel.com>
2022-02-14 13:14:13 -0500

    Initial linting fixes
    Skip-PR-comments: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- utils/githooks/submodule_update_check.py
+++ utils/githooks/submodule_update_check.py
@@ -1,3 +1,4 @@
+"""Adds a warning message to the commit if a module is updated"""
 import re
 import subprocess
 import sys
@@ -5,45 +6,49 @@ import sys
 modified_re = re.compile(r'^(?:M|A)(\s+)(?P<name>.*)')
 
 def rebasing():
-    process = subprocess.Popen(["git", "branch"],
+    """Detemrines if the current operation is a rebase"""
+    with subprocess.Popen(["git", "branch"],
                                stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+                               stderr=subprocess.PIPE) as process:
 
-    if process.wait():
-        print("prepare-commit-hook error running git: %s" % process.communicate()[1])
-        sys.exit(-1)
+        if process.wait():
+            print("prepare-commit-hook error running git: %s" % process.communicate()[1])
+            sys.exit(-1)
 
-    return process.communicate()[0].decode().split('\n')[0].startswith("* (no branch, rebasing")
+        return process.communicate()[0].decode().split('\n', maxsplit=1)[0].startswith("* (no branch, rebasing")
 
 def submodule_check(modname, msg_file):
+    """Detemrines if a submodule has been updated"""
     modified = False
 
-    p = subprocess.Popen(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)
-    out, _ = p.communicate()
-    for line in out.decode().splitlines():
-        match = modified_re.match(line)
-        if match:
-            modified = modified | (match.group('name') == modname)
+    with subprocess.Popen(['git', 'status', '--porcelain'], stdout=subprocess.PIPE) as p:
+        out, _ = p.communicate()
+        for line in out.decode().splitlines():
+            match = modified_re.match(line)
+            if match:
+                modified = modified | (match.group('name') == modname)
 
-    if not rebasing() and modified:
-        with open(msg_file, 'r') as f:
-            lines = f.readlines()
+        if not rebasing() and modified:
+            with open(msg_file, 'r') as f:
+                lines = f.readlines()
 
-        message = '# WARNING *** This patch modifies the {0} reference.  ' \
-                  'Are you sure this is intended? *** WARNING'.format(modname)
+            message = '# WARNING *** This patch modifies the {0} reference.  ' \
+                      'Are you sure this is intended? *** WARNING'.format(modname)
 
-        if lines[0] != message:
-            lines = [message, "\n", "\n"] + lines
+            if lines[0] != message:
+                lines = [message, "\n", "\n"] + lines
 
-        with open(msg_file, 'w') as f:
-            f.writelines(lines)
+            with open(msg_file, 'w') as f:
+                f.writelines(lines)
 
 
 def main(msg_file):
+    """main"""
     for l in subprocess.check_output(['git', 'submodule',
                                       'status']).decode().rstrip().split('\n'):
         if l:
             submodule_check(l[1:].split(' ')[1], msg_file)
 
 if __name__ == '__main__':
+
     main(sys.argv[1])
