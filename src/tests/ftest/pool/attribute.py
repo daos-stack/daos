969978f9b641fd0b8315cacdb54322300bb64db4
Makito Kano <makito.kano@intel.com>
2022-02-20 17:53:16 +0000

    DAOS-9957 test: Test set_attr, list_attr, get_attr with async mode in pool/attribute.py
    Test set_attr, list_attr, get_attr with async mode in pool/attribute.py
    The original code was only testing set_attr.
    Also use CallbackHandler from our infrastructure code.
    
    Skip-unit-tests: true
    Test-tag: pool_attribute
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/pool/attribute.py
+++ src/tests/ftest/pool/attribute.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
 '''
-  (C) Copyright 2020-2021 Intel Corporation.
+  (C) Copyright 2020-2022 Intel Corporation.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 '''
 import traceback
-import threading
 import random
-from apricot import TestWithServers
 
+from apricot import TestWithServers
 from general_utils import get_random_bytes
 from pydaos.raw import DaosApiError
-
-# pylint: disable=global-variable-not-assigned, global-statement
-GLOB_SIGNAL = None
-GLOB_RC = -99000000
-
-
-def cb_func(event):
-    """Callback Function for asynchronous mode."""
-    global GLOB_SIGNAL
-    global GLOB_RC
-    GLOB_RC = event.event.ev_error
-    GLOB_SIGNAL.set()
+from test_utils_base import CallbackHandler
 
 
 class PoolAttributeTest(TestWithServers):
@@ -56,7 +44,6 @@ class PoolAttributeTest(TestWithServers):
             indata (dict): dictionary of data sent to set_attr
             size (int): size of attribute names from list_attr
             buff (bytearray): attribute names from list_attr
-
         """
         # length of all the attribute names
         aggregate_len = sum(
@@ -88,7 +75,6 @@ class PoolAttributeTest(TestWithServers):
         Args:
             indata (dict): dictionary of data sent to set_attr
             outdata (dict): dictionary of data from get_attr
-
         """
         self.log.info("Verifying get_attr output:")
         self.log.info("  set_attr data: %s", indata)
@@ -99,6 +85,20 @@ class PoolAttributeTest(TestWithServers):
                     "FAIL: Value does not match after get attr, Expected "
                     "val={} and received val={}".format(value, outdata[attr]))
 
+    def verify_async_return_code(self, ret_code, expected_result, method_name):
+        """Verify return code from the asynchronous method call.
+
+        Args:
+            ret_code (int): Return code.
+            expected_result (str): Expected result; either PASS or FAIL.
+            method_name (str): Method called.
+        """
+        if expected_result == "PASS" and ret_code != 0:
+            self.fail("Unexpected RC! Method: {}; RC: {}".format(method_name, ret_code))
+        if expected_result == "FAIL" and ret_code == 0:
+            self.fail("Successful RC(0) is returned after failure! Method: {}".format(
+                method_name))
+
     def test_pool_large_attributes(self):
         """Test pool attributes with large data set.
 
@@ -107,7 +107,10 @@ class PoolAttributeTest(TestWithServers):
         Test description: Test large randomly created pool attribute.
 
         :avocado: tags=all,daily_regression
-        :avocado: tags=regression,pool,pool_attr,attribute,large_poolattribute
+        :avocado: tags=vm
+        :avocado: tags=pool
+        :avocado: tags=pool_attribute
+        :avocado: tags=regression,pool_attr,large_pool_attribute
         """
         self.add_pool()
         attr_dict = self.create_data_set()
@@ -133,7 +136,11 @@ class PoolAttributeTest(TestWithServers):
 
         Test description: Test basic pool attribute tests (sync).
 
-        :avocado: tags=all,pool,daily_regression,tiny,sync_poolattribute
+        :avocado: tags=all,daily_regression
+        :avocado: tags=vm
+        :avocado: tags=pool
+        :avocado: tags=pool_attribute
+        :avocado: tags=sync_pool_attribute
         """
         self.add_pool()
         expected_for_param = []
@@ -177,27 +184,50 @@ class PoolAttributeTest(TestWithServers):
             if expected_result == 'PASS':
                 self.fail("Test was expected to pass but it failed.\n")
 
-    def test_pool_attribute_asyn(self):
-        """Test pool attribute with callback.
+    def test_pool_attribute_async(self):
+        """Test pool set/get/list attribute with callback.
 
         Test ID: DAOS-1359
 
-        Test description: Test basic pool attribute tests (async).
+        Test Steps:
+        1. Create a pool
+        2. Call set_attr asynchronously.
+        3. Call list_attr asynchronously and verify the returned list.
+        4. Call get_attr asynchronously and verify the returned name and value.
 
-        :avocado: tags=all,pool,full_regression,tiny,async_poolattribute
+        Return code from set_attr and get_attr. list_attr should always return 0.
+
+        set_attr: (Valid is for long, special, and "Negative-Name".)
+        Name     Value     RC
+        Valid    Valid     0
+        Valid    None      -1003
+        None     Valid     -1003
+        None     None      -1003
+
+        get_attr:
+        Name     Value     RC
+        Valid    Valid     0
+        Valid    None      -1005
+        Wrong    Valid     -1005
+        Wrong    None      -1005
+        None     Valid     -1003
+        None     None      -1003
+
+        :avocado: tags=all,full_regression
+        :avocado: tags=vm
+        :avocado: tags=pool
+        :avocado: tags=pool_attribute
+        :avocado: tags=async_pool_attribute
         """
-        global GLOB_SIGNAL
-        global GLOB_RC
-
         self.add_pool()
+
         expected_for_param = []
+
         name = self.params.get("name", '/run/attrtests/name_handles/*/')
-        # workaround until async functions are fixed
-        if name[0] is not None and "Negative" in name[0]:
-            pass
-        else:
-            expected_for_param.append(name[1])
+        attr_name = name[0]
+        expected_for_param.append(name[1])
         value = self.params.get("value", '/run/attrtests/value_handles/*/')
+        attr_val = value[0]
         expected_for_param.append(value[1])
 
         expected_result = 'PASS'
@@ -207,25 +237,82 @@ class PoolAttributeTest(TestWithServers):
                 break
 
         # Convert any test yaml string to bytes
-        if isinstance(name[0], str):
-            name[0] = name[0].encode("utf-8")
-        if isinstance(value[0], str):
-            value[0] = value[0].encode("utf-8")
+        if isinstance(attr_name, str):
+            attr_name = attr_name.encode("utf-8")
+        if isinstance(attr_val, str):
+            attr_val = attr_val.encode("utf-8")
 
-        attr_dict = {name[0]: value[0]}
+        attr_dict = {attr_name: attr_val}
+
+        callback_handler = CallbackHandler()
+
+        # Test set_attr()
         try:
-            GLOB_SIGNAL = threading.Event()
-            self.pool.pool.set_attr(attr_dict, None, cb_func)
-            GLOB_SIGNAL.wait()
-            if expected_result == 'PASS' and GLOB_RC != 0:
-                self.fail("RC not as expected after set_attr {0}"
-                          .format(GLOB_RC))
-            if expected_result == 'FAIL' and GLOB_RC == 0:
-                self.fail("RC not as expected after set_attr {0}"
-                          .format(GLOB_RC))
+            self.pool.pool.set_attr(
+                data=attr_dict, cb_func=callback_handler.callback)
+            callback_handler.wait()
 
+            # Verify the return code.
+            # One exception is when we use the "Negative-Name" and valid value. In this
+            # case, the method works, but we consider it as failure during get_attr test,
+            # so temporarily use "PASS" here.
+            if attr_name and b"Negative" in attr_name and attr_val:
+                set_exp_result = "PASS"
+            else:
+                set_exp_result = expected_result
+            self.verify_async_return_code(
+                ret_code=callback_handler.ret_code, expected_result=set_exp_result,
+                method_name="set_attr")
         except DaosApiError as excep:
             print(excep)
             print(traceback.format_exc())
             if expected_result == 'PASS':
-                self.fail("Test was expected to pass but it failed.\n")
+                self.fail(
+                    "set_attr was supposed to work, but failed! name-value = {}".format(
+                        attr_dict))
+
+        # Test list_attr()
+        try:
+            size, buf = self.pool.pool.list_attr(cb_func=callback_handler.callback)
+            callback_handler.wait()
+
+            # Verify the return code.
+            ret_code = callback_handler.ret_code
+            if ret_code != 0:
+                self.fail("Unexpected RC after list_attr! {}".format(ret_code))
+        except DaosApiError as excep:
+            print(excep)
+            print(traceback.format_exc())
+            if expected_result == "PASS":
+                self.fail("list_attr failed!")
+
+        if expected_result == "PASS":
+            # Verify the list output.
+            self.verify_list_attr(attr_dict, size.value, buf)
+
+        # Verify get_attr()
+        results = {}
+        if attr_name and b"Negative" in attr_name:
+            attr_name = b"rubbish"
+
+        try:
+            (buff, sizes) = self.pool.pool.get_attr(
+                attr_names=[attr_name], cb_func=callback_handler.callback)
+            callback_handler.wait()
+
+            # Construct the result dictionary from buff and sizes.
+            results = {attr_name: buff[0][:sizes[0]]}
+
+            # Verify the return code.
+            self.verify_async_return_code(
+                ret_code=callback_handler.ret_code, expected_result=expected_result,
+                method_name="get_attr")
+        except DaosApiError as excep:
+            print(excep)
+            print(traceback.format_exc())
+            if expected_result == "PASS":
+                self.fail("get_attr failed! name = {}".format(attr_name))
+
+        if expected_result == "PASS":
+            # Verify the get_attr output.
+            self.verify_get_attr(attr_dict, results)
