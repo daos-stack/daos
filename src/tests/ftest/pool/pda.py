b53ec15551ee66e1f797e94e03440855c6366e5b
Wang Shilong <shilong.wang@intel.com>
2022-02-22 16:28:58 +0800

    fix flake errors
    Test-tag: pr pool_pda_property pool_rf_property

--- src/tests/ftest/pool/pda.py
+++ src/tests/ftest/pool/pda.py
@@ -40,7 +40,7 @@ class PoolPDAProperty(TestWithServers):
 
         Use Case:
             Create the Pool with default.
-	    Create the pool with specified ec_pda,rp_pda
+            Create the pool with specified ec_pda,rp_pda
             Create the container and verify the pda is same for both
                 pool and container.
             Create the container with different pda value
@@ -59,13 +59,13 @@ class PoolPDAProperty(TestWithServers):
         self.assertEqual(1, self.pool.get_property("ec_pda"))
         self.assertEqual(3, self.pool.get_property("rp_pda"))
 
-	# destroy pool
+        # destroy pool
         self.destroy_pools(pools=self.pool)
 
-	# create pool
+        # create pool
         self.add_pool(namespace="/run/pool_1/*")
 
-	# create container with default
+        # create container with default
         self.add_container(self.pool, create=True)
         ec_pda = self.pool.get_property("ec_pda")
         rp_pda = self.pool.get_property("rp_pda")
@@ -74,20 +74,20 @@ class PoolPDAProperty(TestWithServers):
         match_rp_str = 'Performance domain affinity level of RP'
         self.verify_cont_pda(ec_pda, match_ec_str)
         self.verify_cont_pda(rp_pda, match_rp_str)
-	# Destroy the container.
+        # Destroy the container.
         self.container.destroy()
 
         ec_pda, rp_pda = self.params.get("pda_properties", '/run/container_1/*')
         self.add_container(self.pool, namespace="/run/container_1/*", create=False)
         self.container.properties.update("ec_pda:{},rp_pda:{}".format(ec_pda, rp_pda))
 
-	# Create the container
+        # Create the container
         self.container.create()
 
         self.verify_cont_pda(ec_pda, match_ec_str)
         self.verify_cont_pda(rp_pda, match_rp_str)
 
-	# Destroy the container.
+        # Destroy the container.
         self.container.destroy()
 
         self.destroy_pools(pools=self.pool)
