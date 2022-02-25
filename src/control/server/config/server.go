d8cef3803952ea9fa70c83bb8e2acee85deaa3e0
Kris Jacque <kristin.jacque@intel.com>
2022-02-25 12:40:28 -0700

    DAOS-9643 agent: Add log level to agent config file
    - Add control_log_mask to agent config file to specify agent log
      level.
    - Set default test agent log level to debug.
    
    Features: control
    
    Signed-off-by: Kris Jacque <kristin.jacque@intel.com>

--- src/control/server/config/server.go
+++ src/control/server/config/server.go
@@ -42,22 +42,22 @@ type Server struct {
 	ControlPort     int                       `yaml:"port"`
 	TransportConfig *security.TransportConfig `yaml:"transport_config"`
 	// Detect outdated "servers" config, to direct users to change their config file
-	Servers             []*engine.Config `yaml:"servers,omitempty"`
-	Engines             []*engine.Config `yaml:"engines"`
-	BdevInclude         []string         `yaml:"bdev_include,omitempty"`
-	BdevExclude         []string         `yaml:"bdev_exclude,omitempty"`
-	DisableVFIO         bool             `yaml:"disable_vfio"`
-	EnableVMD           bool             `yaml:"enable_vmd"`
-	EnableHotplug       bool             `yaml:"enable_hotplug"`
-	NrHugepages         int              `yaml:"nr_hugepages"` // total for all engines
-	ControlLogMask      ControlLogLevel  `yaml:"control_log_mask"`
-	ControlLogFile      string           `yaml:"control_log_file"`
-	ControlLogJSON      bool             `yaml:"control_log_json,omitempty"`
-	HelperLogFile       string           `yaml:"helper_log_file"`
-	FWHelperLogFile     string           `yaml:"firmware_helper_log_file"`
-	RecreateSuperblocks bool             `yaml:"recreate_superblocks,omitempty"`
-	FaultPath           string           `yaml:"fault_path"`
-	TelemetryPort       int              `yaml:"telemetry_port,omitempty"`
+	Servers             []*engine.Config       `yaml:"servers,omitempty"`
+	Engines             []*engine.Config       `yaml:"engines"`
+	BdevInclude         []string               `yaml:"bdev_include,omitempty"`
+	BdevExclude         []string               `yaml:"bdev_exclude,omitempty"`
+	DisableVFIO         bool                   `yaml:"disable_vfio"`
+	EnableVMD           bool                   `yaml:"enable_vmd"`
+	EnableHotplug       bool                   `yaml:"enable_hotplug"`
+	NrHugepages         int                    `yaml:"nr_hugepages"` // total for all engines
+	ControlLogMask      common.ControlLogLevel `yaml:"control_log_mask"`
+	ControlLogFile      string                 `yaml:"control_log_file"`
+	ControlLogJSON      bool                   `yaml:"control_log_json,omitempty"`
+	HelperLogFile       string                 `yaml:"helper_log_file"`
+	FWHelperLogFile     string                 `yaml:"firmware_helper_log_file"`
+	RecreateSuperblocks bool                   `yaml:"recreate_superblocks,omitempty"`
+	FaultPath           string                 `yaml:"fault_path"`
+	TelemetryPort       int                    `yaml:"telemetry_port,omitempty"`
 
 	// duplicated in engine.Config
 	SystemName string              `yaml:"name"`
@@ -242,7 +242,7 @@ func (cfg *Server) WithNrHugePages(nr int) *Server {
 }
 
 // WithControlLogMask sets the daos_server log level.
-func (cfg *Server) WithControlLogMask(lvl ControlLogLevel) *Server {
+func (cfg *Server) WithControlLogMask(lvl common.ControlLogLevel) *Server {
 	cfg.ControlLogMask = lvl
 	return cfg
 }
@@ -288,7 +288,7 @@ func DefaultServer() *Server {
 		TransportConfig: security.DefaultServerTransportConfig(),
 		Hyperthreads:    false,
 		Path:            defaultConfigPath,
-		ControlLogMask:  ControlLogLevel(logging.LogLevelInfo),
+		ControlLogMask:  common.ControlLogLevel(logging.LogLevelInfo),
 		EnableVMD:       false, // disabled by default
 		EnableHotplug:   false, // disabled by default
 	}
