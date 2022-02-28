3d190c265504a4f55bcf3abdad8a93eae49edb7b
Tom Nabarro <tom.nabarro@intel.com>
2022-02-28 16:23:08 +0000

    DAOS-9972 control: Fail service start-up if initial scan errors (#8335)
    Workaround VMD cluster failure on degraded test.
    
    Test-tag: pr daily_regression control test_daos_degraded_mode
    Allow-unstable-test: true
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/server_utils.go
+++ src/control/server/server_utils.go
@@ -268,13 +268,9 @@ func scanBdevStorage(srv *server) (*storage.BdevScanResponse, error) {
 		BypassCache: true, // init cache on first scan
 	})
 	if err != nil {
-		// Return error if fault code is for BdevNotFound.
-		if storage.FaultBdevNotFound().Equals(err) {
-			return nil, err
-		}
-		// Keep going for other scan related failures.
-		srv.log.Errorf("%s\n", errors.Wrap(err, "NVMe Scan Failed"))
-		return &storage.BdevScanResponse{}, nil
+		err = errors.Wrap(err, "NVMe Scan Failed")
+		srv.log.Errorf("%s", err)
+		return nil, err
 	}
 
 	return nvmeScanResp, nil
