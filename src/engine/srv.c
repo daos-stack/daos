c813b5d74fbf33440d3ddc204062ccc3709a5cec
Niu Yawei <yawei.niu@intel.com>
2022-03-02 16:34:58 +0800

    DAOS-9896 sched: check if network poll started (#8197)
    Before network poll started, for instance, when the cart progress
    ULT is blocked on open barrier (waiting for join PG), scheduler
    shouldn't rely on the blocking crt_progress(timeout) call to relax
    CPU, instead, it should just call usleep().
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>

--- src/engine/srv.c
+++ src/engine/srv.c
@@ -478,6 +478,9 @@ dss_srv_handler(void *arg)
 		ABT_cond_wait(xstream_data.xd_ult_barrier, xstream_data.xd_mutex);
 	ABT_mutex_unlock(xstream_data.xd_mutex);
 
+	if (dx->dx_comm)
+		dx->dx_progress_started = true;
+
 	signal_caller = false;
 	/* main service progress loop */
 	for (;;) {
@@ -497,6 +500,9 @@ dss_srv_handler(void *arg)
 
 		ABT_thread_yield();
 	}
+
+	if (dx->dx_comm)
+		dx->dx_progress_started = false;
 
 	wait_all_exited(dx);
 	if (dmi->dmi_dp) {
