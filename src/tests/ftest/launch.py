88ced71f4c7459ac4db4dfe2b73e064f01683f4a
Brian J. Murrell <brian.murrell@intel.com>
2022-03-02 11:58:41 -0500

    DAOS-10006 test: Provide a module config
    For MOFED openmpi on EL 8.
    
    Also add necessary debuginfo repos for Rocky 8.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -1714,6 +1714,11 @@ def resolve_debuginfo(pkg):
     return package_info
 
 
+def is_el(distro):
+    """Return True if a distro is an EL"""
+    return [d for d in ["almalinux", "rocky", "centos", "rhel"] if d in distro.name.lower()]
+
+
 def install_debuginfos():
     """Install debuginfo packages.
 
@@ -1728,7 +1733,7 @@ def install_debuginfos():
 
     distro_info = detect()
     install_pkgs = [{'name': 'gdb'}]
-    if "centos" in distro_info.name.lower():
+    if is_el(distro_info):
         install_pkgs.append({'name': 'python3-debuginfo'})
 
     cmds = []
@@ -1762,8 +1767,7 @@ def install_debuginfos():
                 dnf_args.extend(["--enablerepo=*-debuginfo", "--exclude",
                                  "nvml-debuginfo", "libpmemobj",
                                  "python36", "openmpi3", "gcc"])
-            elif "centos" in distro_info.name.lower() and \
-                 distro_info.version == "8":
+            elif is_el(distro_info) and distro_info.version == "8":
                 dnf_args.extend(["--enablerepo=*-debuginfo", "libpmemobj",
                                  "python3", "openmpi", "gcc"])
             else:
@@ -1798,7 +1802,7 @@ def install_debuginfos():
 
     # Now install a few pkgs that debuginfo-install wouldn't
     cmd = ["sudo", "dnf", "-y"]
-    if "centos" in distro_info.name.lower():
+    if is_el(distro_info):
         cmd.append("--enablerepo=*debug*")
     cmd.append("install")
     for pkg in install_pkgs:
@@ -1823,7 +1827,7 @@ def install_debuginfos():
     if retry:
         print("Going to refresh caches and try again")
         cmd_prefix = ["sudo", "dnf"]
-        if "centos" in distro_info.name.lower():
+        if is_el(distro_info):
             cmd_prefix.append("--enablerepo=*debug*")
         cmds.insert(0, cmd_prefix + ["clean", "all"])
         cmds.insert(1, cmd_prefix + ["makecache"])
