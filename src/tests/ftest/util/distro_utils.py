dc62a5da49c0f3ef10253689fce4356b08851fb1
Phillip Henderson <phillip.henderson@intel.com>
2022-03-02 15:29:44 -0500

    Adding AlmaLinux support.
    Provisioning-pool: bmurrell-centos-to-el
    Skip-unit-tests: true
    Allow-unstable-test: true
    Features: test_core_files
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/distro_utils.py
+++ src/tests/ftest/util/distro_utils.py
@@ -9,15 +9,25 @@ import re
 
 
 class RockyProbe(distro.Probe):
-    """Probe with version checks for Rocky systems."""
+    """Probe with version checks for Rocky Linux systems."""
 
-    CHECK_FILE = '/etc/rocky-release'
-    CHECK_FILE_CONTAINS = 'Rocky Linux'
-    CHECK_FILE_DISTRO_NAME = 'rocky'
-    CHECK_VERSION_REGEX = re.compile(r'Rocky Linux release (\d{1,2})\.(\d{1,2}).*')
+    CHECK_FILE = "/etc/rocky-release"
+    CHECK_FILE_CONTAINS = "Rocky Linux"
+    CHECK_FILE_DISTRO_NAME = "rocky"
+    CHECK_VERSION_REGEX = re.compile(r"Rocky Linux release (\d{1,2})\.(\d{1,2}).*")
+
+
+class AlmaProbe(distro.Probe):
+    """Probe with version checks for AlmaLinux systems."""
+
+    CHECK_FILE = "/etc/almalinux-release"
+    CHECK_FILE_CONTAINS = "AlmaLinux"
+    CHECK_FILE_DISTRO_NAME = "alma"
+    CHECK_VERSION_REGEX = re.compile(r"AlmaLinux release (\d{1,2})\.(\d{1,2}).*")
 
 
 distro.register_probe(RockyProbe)
+distro.register_probe(AlmaProbe)
 
 
 def detect():
