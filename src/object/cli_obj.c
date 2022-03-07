4aae0dd59e1f13182be7d9d47a60ac5bec3799f5
David Quigley <david.quigley@intel.com>
2022-03-07 13:55:41 -0700

    Fix a typo in which member were checking
    Signed-off-by: David Quigley <david.quigley@intel.com>

--- src/object/cli_obj.c
+++ src/object/cli_obj.c
@@ -3657,7 +3657,7 @@ update_sub_anchor_cb(struct shard_auxi_args *shard_auxi,
 		struct shard_anchors *sub_anchors;
 
 		sub_anchors = (struct shard_anchors *)obj_arg->akey_anchor->da_sub_anchors;
-		if (sub_anchors->sa_anchors[shard].ssa_akey_anchor && shard_arg->la_akey_anchor)
+		if (sub_anchors->sa_anchors[shard].ssa_anchor && shard_arg->la_akey_anchor)
 			memcpy(&sub_anchors->sa_anchors[shard].ssa_anchor,
 			       shard_arg->la_akey_anchor, sizeof(daos_anchor_t));
 	}
