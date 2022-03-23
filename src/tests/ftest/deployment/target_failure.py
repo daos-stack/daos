03aa6e4f191ed8e4fdb86ed34233911096c3aa3b
Makito Kano <makito.kano@intel.com>
2022-03-23 17:37:13 +0000

    DAOS-10001 test: Add container health check and time exclude/rebuild
    Skip-unit-tests: true
    Test-tag: target_failure
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/deployment/target_failure.py
+++ src/tests/ftest/deployment/target_failure.py
@@ -12,6 +12,7 @@ from ior_test_base import IorTestBase
 from ior_utils import IorCommand
 from general_utils import report_errors
 from command_utils_base import CommandFailure
+from job_manager_utils import get_job_manager
 
 
 class TargetFailure(IorTestBase):
@@ -45,7 +46,9 @@ class TargetFailure(IorTestBase):
         testfile = os.path.join("/", file_name)
         ior_cmd.test_file.update(testfile)
 
-        manager = self.get_ior_job_manager_command(custom_ior_cmd=ior_cmd)
+        manager = get_job_manager(
+            test=self, class_name="Mpirun", job=ior_cmd, subprocess=self.subprocess,
+            mpi_type="mpich")
         manager.assign_hosts(
             self.hostlist_clients, self.workdir, self.hostfile_clients_slots)
 
@@ -63,6 +66,44 @@ class TargetFailure(IorTestBase):
             self.log.info("--- IOR command %d failed ---", job_num)
             results[job_num] = [False, "IOR failed: {}".format(error)]
 
+    @staticmethod
+    def check_container_health(container, expected_health):
+        """Check container property's Health field by calling daos container get-prop.
+
+        Args:
+            container (TestContainer): Container to call get-prop.
+            expected_health (str): Expected value in the Health field.
+
+        Returns:
+            bool: True if expected_health matches the one obtained from get-prop.
+
+        """
+        actual_health = None
+
+        output = container.get_prop()
+        for cont_prop in output["response"]:
+            if cont_prop["name"] == "status":
+                actual_health = cont_prop["value"]
+                break
+
+        return actual_health == expected_health
+
+    def measure_rebuild_time(self, operation, pool):
+        """Measure rebuild time.
+
+        This method is mainly for debugging purpose. We'll analyze the output when we
+        realize that rebuild is taking too long.
+
+        Args:
+            operation (str): Type of operation to print in the log.
+            pool (TestPool): Pool to call wait_for_rebuild().
+        """
+        start = float(time.time())
+        pool.wait_for_rebuild(to_start=True)
+        pool.wait_for_rebuild(to_start=False)
+        duration = float(time.time()) - start
+        self.log.info("%s duration: %.1f sec", operation, duration)
+
     def test_target_failure_wo_rf(self):
         """Jira ID: DAOS-10001.
 
@@ -74,9 +115,11 @@ class TargetFailure(IorTestBase):
         2. Run IOR with oclass S1.
         3. Exclude one target while IOR is running.
         4. Verify the IOR failed.
-        5. Reintegrate the evicted target.
-        6. Run IOR again.
-        7. Verify that there's no error this time.
+        5. Verify that the container's Health property is UNCLEAN.
+        6. Reintegrate the excluded target.
+        7. Verify that the container's Health property is HEALTHY.
+        8. Run IOR again.
+        9. Verify that there's no error this time.
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,small
@@ -84,7 +127,7 @@ class TargetFailure(IorTestBase):
         :avocado: tags=target_failure_wo_rf
         """
         # 1. Create a pool and a container.
-        self.add_pool()
+        self.add_pool(namespace="/run/pool_size_ratio/*")
         self.add_container(pool=self.pool, namespace="/run/container_wo_rf/*")
 
         # 2. Run IOR with oclass SX so that excluding one target will result in a failure.
@@ -105,6 +148,7 @@ class TargetFailure(IorTestBase):
 
         # 3. Exclude one target while IOR is running.
         self.pool.exclude(ranks=[1], tgt_idx="0")
+        self.measure_rebuild_time(operation="Exclude 1 target", pool=self.pool)
 
         # Wait until the IOR thread ends.
         job.join()
@@ -117,20 +161,27 @@ class TargetFailure(IorTestBase):
             errors.append(
                 "First IOR was supposed to fail, but worked! {}".format(ior_error))
 
-        # 5. Reintegrate the evicted target.
-        self.pool.wait_for_rebuild(to_start=True)
-        self.pool.wait_for_rebuild(to_start=False)
-        self.pool.reintegrate(rank="1", tgt_idx="0")
-        self.pool.wait_for_rebuild(to_start=True)
-        self.pool.wait_for_rebuild(to_start=False)
+        # 5. Verify that the container's Health property is UNCLEAN.
+        if not self.check_container_health(
+                container=self.container, expected_health="UNCLEAN"):
+            errors.append("Container health isn't UNCLEAN after first IOR!")
 
-        # 6. Run IOR again.
+        # 6. Reintegrate the excluded target.
+        self.pool.reintegrate(rank="1", tgt_idx="0")
+        self.measure_rebuild_time(operation="Reintegrate 1 target", pool=self.pool)
+
+        # 7. Verify that the container's Health property is HEALTHY.
+        if not self.check_container_health(
+                container=self.container, expected_health="HEALTHY"):
+            errors.append("Container health isn't HEALTHY after reintegrate!")
+
+        # 8. Run IOR again.
         ior_results = {}
         self.run_ior_report_error(
             job_num=job_num, results=ior_results, file_name="test_file_2", oclass=oclass,
             pool=self.pool, container=self.container)
 
-        # 7. Verify that there's no error this time.
+        # 9. Verify that there's no error this time.
         self.log.info("----- IOR results 2 -----")
         self.log.info(ior_results)
         if not ior_results[job_num][0]:
@@ -151,9 +202,11 @@ class TargetFailure(IorTestBase):
         3. While the IOR is running, exclude one target from each server rank so that IO
         fails even with replication.
         4. Verify the IOR failed.
-        5. Reintegrate the excluded targets and wait for the rebuild to finish.
-        6. Restart the IOR and verify that it works. (Recovery test)
-        7. Verify that a new container can be created and IOR works. (Recovery test)
+        5. Verify that the container's Health property is UNCLEAN.
+        6. Reintegrate the excluded targets and wait for the rebuild to finish.
+        7. Verify that the container's Health property is HEALTHY.
+        8. Restart the IOR and verify that it works. (Recovery test)
+        9. Verify that a new container can be created and IOR works. (Recovery test)
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,small
@@ -161,7 +214,7 @@ class TargetFailure(IorTestBase):
         :avocado: tags=target_failure_with_rf
         """
         # 1. Create a pool and a container.
-        self.add_pool()
+        self.add_pool(namespace="/run/pool_size_ratio/*")
         self.add_container(pool=self.pool, namespace="/run/container_with_rf/*")
 
         # 2. Run IOR with oclass RP_2G1.
@@ -187,6 +240,7 @@ class TargetFailure(IorTestBase):
         # start before excluding the next target.
         self.pool.wait_for_rebuild(to_start=True)
         self.pool.exclude(ranks=[0], tgt_idx="1")
+        self.measure_rebuild_time(operation="Exclude 2 targets", pool=self.pool)
 
         # Wait until the IOR thread ends.
         job.join()
@@ -199,21 +253,29 @@ class TargetFailure(IorTestBase):
         if ior_results[job_num][0]:
             errors.append("First IOR was supposed to fail, but worked!")
 
-        # 5. Reintegrate the evicted target.
-        self.pool.wait_for_rebuild(to_start=True)
-        self.pool.wait_for_rebuild(to_start=False)
+        # 5. Verify that the container's Health property is UNCLEAN.
+        if not self.check_container_health(
+                container=self.container, expected_health="UNCLEAN"):
+            errors.append("Container health isn't UNCLEAN after first IOR!")
+
+        # 6. Reintegrate the excluded target.
         self.log.info("Reintegrate rank 1 target 1")
         # Reintegrate one target and wait for rebuild to finish before reintegrating the
         # next one.
         self.pool.reintegrate(rank="1", tgt_idx="1")
-        self.pool.wait_for_rebuild(to_start=True)
-        self.pool.wait_for_rebuild(to_start=False)
+        self.measure_rebuild_time(
+            operation="Reintegrate rank 1 -> target 1", pool=self.pool)
         self.log.info("Reintegrate rank 0 target 1")
         self.pool.reintegrate(rank="0", tgt_idx="1")
-        self.pool.wait_for_rebuild(to_start=True)
-        self.pool.wait_for_rebuild(to_start=False)
+        self.measure_rebuild_time(
+            operation="Reintegrate rank 0 -> target 1", pool=self.pool)
 
-        # 6. Restart IOR. Should work.
+        # 7. Verify that the container's Health property is HEALTHY.
+        if not self.check_container_health(
+                container=self.container, expected_health="HEALTHY"):
+            errors.append("Container health isn't HEALTHY after reintegrate!")
+
+        # 8. Restart IOR. Should work.
         ior_results = {}
         self.run_ior_report_error(
             job_num=job_num, results=ior_results, file_name="test_file_2", oclass=oclass,
@@ -226,7 +288,7 @@ class TargetFailure(IorTestBase):
             errors.append(
                 "Error found in second IOR run! {}".format(ior_results[job_num][1]))
 
-        # 7. Create a new container and run IOR.
+        # 9. Create a new container and run IOR.
         self.add_container(pool=self.pool, namespace="/run/container_with_rf/*")
         ior_results = {}
         self.run_ior_report_error(
@@ -252,9 +314,11 @@ class TargetFailure(IorTestBase):
         3. Exclude one target from self.pool[1] while IOR is running.
         4. Verify the IOR failed for self.pool[1].
         5. Verify the IOR worked for self.pool[0].
-        6. Reintegrate the evicted target.
-        7. Run IOR again.
-        8. Verify that there's no error this time.
+        6. Verify that the self.pool[1].container's Health property is UNCLEAN.
+        7. Reintegrate the excluded target.
+        8. Verify that the self.pool[1].container's Health property is HEALTHY.
+        9. Run IOR again.
+        10. Verify that there's no error this time.
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,small
@@ -264,8 +328,11 @@ class TargetFailure(IorTestBase):
         pool_qty = 2
         self.container = []
 
-        # 1. Create 2 pools and a container in each pool.
-        self.add_pool_qty(quantity=pool_qty)
+        # 1. Create 2 pools and a container in each pool. Use the actual size value
+        # rather than percentage because percentage doesn't work well with multiple
+        # pools. Also, this test focuses on the isolation rather than creating the largest
+        # pools possible.
+        self.add_pool_qty(quantity=pool_qty, namespace="/run/pool_size_value/*")
         for i in range(pool_qty):
             self.container.append(
                 self.get_container(pool=self.pool[i], namespace="/run/container_wo_rf/*"))
@@ -295,6 +362,8 @@ class TargetFailure(IorTestBase):
         non_excluded_pool_num = 0
         self.log.info("Exclude rank 1 target 0")
         self.pool[excluded_pool_num].exclude(ranks=[1], tgt_idx="0")
+        self.measure_rebuild_time(
+            operation="Exclude 1 target", pool=self.pool[excluded_pool_num])
 
         # Wait until all the IOR threads end.
         for thread in threads:
@@ -316,21 +385,29 @@ class TargetFailure(IorTestBase):
                 non_excluded_pool_num, succeeded_ior_result)
             errors.append(msg)
 
-        # 6. Reintegrate the evicted target.
-        self.log.info("Reintegrate target")
-        self.pool[excluded_pool_num].wait_for_rebuild(to_start=True)
-        self.pool[excluded_pool_num].wait_for_rebuild(to_start=False)
-        self.pool[excluded_pool_num].reintegrate(rank="1", tgt_idx="0")
-        self.pool[excluded_pool_num].wait_for_rebuild(to_start=True)
-        self.pool[excluded_pool_num].wait_for_rebuild(to_start=False)
+        # 6. Verify that self.container[1]'s Health property is UNCLEAN.
+        if not self.check_container_health(
+                container=self.container[1], expected_health="UNCLEAN"):
+            errors.append("Container health isn't UNCLEAN after first IOR!")
 
-        # 7. Run IOR again.
+        # 7. Reintegrate the excluded target.
+        self.log.info("Reintegrate target")
+        self.pool[excluded_pool_num].reintegrate(rank="1", tgt_idx="0")
+        self.measure_rebuild_time(
+            operation="Reintegrate 1 target", pool=self.pool[excluded_pool_num])
+
+        # 8. Verify that self.container[1]'s Health property is HEALTHY.
+        if not self.check_container_health(
+                container=self.container[1], expected_health="HEALTHY"):
+            errors.append("Container health isn't HEALTHY after first IOR!")
+
+        # 9. Run IOR again.
         self.run_ior_report_error(
             job_num=excluded_pool_num, results=ior_results, file_name="test_file_2",
             oclass=oclass, pool=self.pool[excluded_pool_num],
             container=self.container[excluded_pool_num])
 
-        # 8. Verify that there's no error this time.
+        # 10. Verify that there's no error this time.
         self.log.info("----- IOR results 2 -----")
         ior_result = ior_results[excluded_pool_num]
         self.log.debug(ior_result)
