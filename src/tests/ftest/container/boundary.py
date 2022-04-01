d04a1f58f4553713a2d15ebee3dbfc527d1bbc61
Ding Ho <ding-hwa.ho@intel.com>
2022-04-01 19:20:38 +0000

    Update fixes of Lint error.
    Skip-unit-tests: true
    Test-tag: boundary_test
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/container/boundary.py
+++ src/tests/ftest/container/boundary.py
@@ -60,7 +60,7 @@ class BoundaryTest(TestWithServers):
             self.log.info("===(4.%d.%d)create_container_and_test, container closed.",
                 pool_num, cont_num)
         except (DaosTestError, TestFail) as err:
-            msg  = "#(4.{}.{}) container close fail, err: {}".format(pool_num, cont_num, err)
+            msg = "#(4.{}.{}) container close fail, err: {}".format(pool_num, cont_num, err)
             self.fail(msg)
 
     def create_containers(self, pool=None, pool_num=10, num_containers=100, with_io=False):
