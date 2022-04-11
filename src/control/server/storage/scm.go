68a85d52a55d6933b03f77830af8dd0fa81555d3
Tom Nabarro <tom.nabarro@intel.com>
2022-04-11 09:25:49 -0400

    Endeavour hacks for REL2_4
    Required-githooks: true
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/storage/scm.go
+++ src/control/server/storage/scm.go
@@ -450,6 +450,9 @@ func (f *ScmAdminForwarder) CheckFormat(req ScmFormatRequest) (*ScmFormatRespons
 
 // Scan forwards an SCM scan request.
 func (f *ScmAdminForwarder) Scan(req ScmScanRequest) (*ScmScanResponse, error) {
+	// Make Scan a no-op on Endeavour
+	return &ScmScanResponse{}, nil
+
 	req.Forwarded = true
 
 	res := new(ScmScanResponse)
@@ -462,6 +465,9 @@ func (f *ScmAdminForwarder) Scan(req ScmScanRequest) (*ScmScanResponse, error) {
 
 // Prepare forwards a request to prep the SCM.
 func (f *ScmAdminForwarder) Prepare(req ScmPrepareRequest) (*ScmPrepareResponse, error) {
+	// Make Prepare a no-op on Endeavour
+	return &ScmPrepareResponse{}, nil
+
 	req.Forwarded = true
 
 	res := new(ScmPrepareResponse)
