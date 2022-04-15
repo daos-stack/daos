c59c85598cb3635073b83008d1ba08e2808002b5
rpadma2 <ravindran.padmanabhan@intel.com>
2022-04-15 12:46:40 -0400

    DAOS-7300 test: Review update/faults avocado tag. Skip-unit-tests: true Skip-nlt: true Test-tag: pr scrubber
    Signed-off-by: rpadma2 <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/scrubber/target_auto_eviction.py
+++ src/tests/ftest/scrubber/target_auto_eviction.py
@@ -19,34 +19,24 @@ class TestWithScrubberTargetEviction(TestWithScrubber):
     def test_scrubber_ssd_auto_eviction(self):
         """JIRA ID: DAOS-7300
 
-            1. Create checksum faults above scrubber threshold
-            and see whether SSD auto eviction works as expected.
+        1. Create checksum faults above scrubber threshold
+        and see whether SSD auto eviction works as expected.
         :avocado: tags=all,pr,daily_regression
         :avocado: tags=hw,medium,ib2
-        :avocado: tags=scrubber
+        :avocado: tags=scrubber,faults
         :avocado: tags=test_scrubber_target_auto_eviction
 
         """
-        flags = self.params.get("ior_flags", '/run/ior/iorflags/*')
-        apis = self.params.get("ior_api", '/run/ior/iorflags/*')
-        transfer_block_size = self.params.get("transfer_block_size",
-                                              '/run/ior/iorflags/*')
-        obj_class = self.params.get("obj_class", '/run/ior/iorflags/*')
         pool_prop = self.params.get("properties", '/run/pool/*')
         cont_prop = self.params.get("properties", '/run/container/*')
         initial_metrics = {}
         final_metrics = {}
-        self.ior_cmd.api.update(apis[0])
-        self.ior_cmd.flags.update(flags[0], "ior.flags")
-        self.ior_cmd.dfs_oclass.update(obj_class[0])
-        self.ior_cmd.dfs_dir_oclass.update(obj_class[0])
         self.create_pool_cont_with_scrubber(pool_prop=pool_prop, cont_prop=cont_prop)
         self.dmg_cmd.pool_query(self.pool.uuid)
         initial_metrics = self.scrubber.get_scrub_corrupt_metrics()
-        for test in transfer_block_size:
-            self.ior_cmd.transfer_size.update(test[0])
-            self.ior_cmd.block_size.update(test[1])
         self.run_ior_and_check_scruber_status(pool=self.pool, cont=self.container)
+        # Wait for a minute for the scrubber to take action and evict target
+        # after corruption threshold reached.
         time.sleep(60)
         self.dmg_cmd.pool_query(self.pool.uuid)
         final_metrics = self.scrubber.get_scrub_corrupt_metrics()
