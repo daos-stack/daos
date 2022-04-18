fa8a91b7e24243b97b52f678bbd1c0277e9b9033
Ding Ho <ding-hwa.ho@intel.com>
2022-04-18 21:53:19 +0000

    Update script to address review comments.
    Quick-Functional: true
    Test-tag: pool_mgmt_race
    Test-repeat: 5
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/pool/management_race.py
+++ src/tests/ftest/pool/management_race.py
@@ -38,20 +38,17 @@ class PoolManagementRace(TestWithServers):
         label = pool_label_prefix + str(pool_num)
         for test_num in range(test_loop):
             if del_recreate:
-                destroy_result = self.get_dmg_command().pool_destroy(
-                    pool=self.pool[pool_num].label.value, force=1)
+                self.pool[pool_num].destroy()
                 self.log.info(
-                    "--(%d.2.%d.%d)Pool %s deleted.\n",
-                    thread_num, pool_num, test_num, label)
-                new_pool = self.get_pool()
-                new_pool.set_property(prop_name="label", prop_value=label)
+                    "--(%d.2.%d.%d)Pool %s deleted.\n", thread_num, pool_num, test_num, label)
+                new_pool = self.get_pool(create=False)
                 new_pool.label.update(label)
+                new_pool.create()
                 self.pool[pool_num] = new_pool
-                self.log.info(
-                    "--(%d.3.%d.%d)Pool %s recreated.\n",
-                    thread_num, pool_num, test_num, label)
+                self.log.info("--(%d.3.%d.%d)Pool %s recreated.\n",
+                              thread_num, pool_num, test_num, label)
                 # pool stays with a random time before destroy
-                pool_stay_time = random.randint(2, 5) #nosec
+                pool_stay_time = random.randint(1, 3) #nosec
                 time.sleep(pool_stay_time)
             else:
                 completed = False
@@ -60,10 +57,10 @@ class PoolManagementRace(TestWithServers):
                     try:
                         self.log.info("--(%d.4.%d.%d)--pool querying.. \n",
                                       thread_num, pool_num, test_num)
-                        query_result = self.get_dmg_command().pool_query(pool=label)
+                        self.get_dmg_command().pool_query(pool=label)
                         self.log.info("--(%d.5.%d.%d)--get pool list all.. \n",
                                       thread_num, pool_num, test_num)
-                        pool_list_result = self.get_dmg_command().get_pool_list_all()
+                        self.get_dmg_command().get_pool_list_all()
                         completed = True
                     except CommandFailure as error:
                         self.log.info(
@@ -91,7 +88,7 @@ class PoolManagementRace(TestWithServers):
             5. Check for failure from thread_manager
         :avocado: tags=all,full_regression
         :avocado: tags=hw,medium,ib2
-        :avocado: tags=boundary_test
+        :avocado: tags=pool,boundary_test
         :avocado: tags=pool_mgmt_race
         Args:
         """
@@ -103,11 +100,10 @@ class PoolManagementRace(TestWithServers):
         self.pool = []
         for pool_number in range(num_pools):
             label = pool_label_prefix + str(pool_number)
-            pool = self.get_pool()
-            pool.set_property(prop_name="label", prop_value=label)
-            pool.label.update(label)
-            self.pool.append(pool)
-            self.log.info("==(1.%d) pool created, %d.", pool_number, pool)
+            self.pool.append(self.get_pool(create=False))
+            self.pool[-1].label.update(label)
+            self.pool[-1].create()
+            self.log.info("==(1.%d) pool created, %d.", pool_number, self.pool[-1])
 
         # Randomly select a pool for delete, recreate and query
         pool_number = random.randint(0, len(self.pool)-1) #nosec
