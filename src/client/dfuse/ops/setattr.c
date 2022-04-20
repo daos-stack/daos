2e51c0b07530b4daa0e529433ab6b0fc06a64e53
Ashley Pittman <ashley.m.pittman@intel.com>
2022-04-20 18:01:42 +0100

    Save the attr on setattr.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/client/dfuse/ops/setattr.c
+++ src/client/dfuse/ops/setattr.c
@@ -112,11 +112,7 @@ dfuse_cb_setattr(fuse_req_t req, struct dfuse_inode_entry *ie,
 
 	attr->st_ino = ie->ie_stat.st_ino;
 
-	/* Update the size as dfuse knows about it for future use, but only if it was set as part
-	 * of this call.  See DAOS-8333
-	 */
-	if (dfs_flags & DFS_SET_ATTR_SIZE)
-		ie->ie_stat.st_size = attr->st_size;
+	ie->ie_stat = attr;
 
 	DFUSE_REPLY_ATTR(ie, req, attr);
 	return;
