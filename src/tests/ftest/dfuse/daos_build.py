cef2b427cbb3e86856eb4aec3b38f816633071e9
Ashley Pittman <ashley.m.pittman@intel.com>
2022-04-22 16:07:21 +0100

    Change something to have another bite at CI
    Test-tag: dfuse
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>
    # ------------------------ >8 ------------------------
    Skip-func-hw-test: true
    Skip-func-test: true
    Quick-Functional: true
    Test-tag: dfuse

--- src/tests/ftest/dfuse/daos_build.py
+++ src/tests/ftest/dfuse/daos_build.py
@@ -119,7 +119,7 @@ class DaosBuild(DfuseTestBase):
         for cmd in cmds:
             try:
                 command = '{};{}'.format(preload_cmd, cmd)
-                ret_code = general_utils.pcmd(self.hostlist_clients, command, timeout=15*60)
+                ret_code = general_utils.pcmd(self.hostlist_clients, command, timeout=16*60)
                 if 0 in ret_code:
                     continue
                 self.log.info(ret_code)
