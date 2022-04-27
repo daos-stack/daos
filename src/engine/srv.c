47b8b5a908ba237442e69d286d7afb20c46b7746
Wang Shilong <shilong.wang@intel.com>
2022-04-27 17:14:38 +0800

    DAOS-10051 engine: abort timeout RPC before stopping engine
    After xstream breaking progress loop, there will be no chance
    to run rpc timeout checking for this transport context, if one
    ult(eg dtx) is sending RPC to a died remote server, ULT will be
    blocked forever in wait_all_exited().
    
    Fix this problem by running cart progress in wait_all_exited()
    which will run timeout check and abort RPC properly.
    
    Test-tag: pr cascading
    Signed-off-by: Wang Shilong <shilong.wang@intel.com>

--- src/engine/srv.c
+++ src/engine/srv.c
@@ -310,8 +310,10 @@ dss_nvme_poll_ult(void *args)
  * be destroyed on server handler ULT exiting.
  */
 static void
-wait_all_exited(struct dss_xstream *dx)
+wait_all_exited(struct dss_xstream *dx, struct dss_module_info *dmi)
 {
+	int	rc;
+
 	D_DEBUG(DB_TRACE, "XS(%d) draining ULTs.\n", dx->dx_xs_id);
 
 	sched_stop(dx);
@@ -322,7 +324,6 @@ wait_all_exited(struct dss_xstream *dx)
 
 		for (i = 0; i < DSS_POOL_CNT; i++) {
 			size_t	pool_size;
-			int	rc;
 			rc = ABT_pool_get_total_size(dx->dx_pools[i],
 						     &pool_size);
 			D_ASSERTF(rc == ABT_SUCCESS, "%d\n", rc);
@@ -334,6 +335,17 @@ wait_all_exited(struct dss_xstream *dx)
 		 */
 		if (total_size == 0)
 			break;
+
+		/*
+		 * Abort timeout rpc properly, otherwise some ult like(DTX)
+		 * will wait forever.
+		 */
+		if (dx->dx_comm) {
+			rc = crt_progress(dmi->dmi_ctx, 0);
+			if (rc != 0 && rc != -DER_TIMEDOUT)
+				D_ERROR("failed to progress CART context: %d\n",
+					rc);
+		}
 
 		ABT_thread_yield();
 	}
@@ -472,7 +484,7 @@ dss_srv_handler(void *arg)
 		if (rc != ABT_SUCCESS) {
 			D_ERROR("create NVMe poll ULT failed: %d\n", rc);
 			ABT_future_set(dx->dx_shutdown, dx);
-			wait_all_exited(dx);
+			wait_all_exited(dx, dmi);
 			D_GOTO(nvme_fini, rc = dss_abterr2der(rc));
 		}
 	}
@@ -523,7 +535,7 @@ dss_srv_handler(void *arg)
 	if (dx->dx_comm)
 		dx->dx_progress_started = false;
 
-	wait_all_exited(dx);
+	wait_all_exited(dx, dmi);
 	if (dmi->dmi_dp) {
 		daos_profile_destroy(dmi->dmi_dp);
 		dmi->dmi_dp = NULL;
