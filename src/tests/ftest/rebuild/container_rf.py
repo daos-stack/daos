fcb49106dbdab36db2d9fbe326787f45665030a4
Wang Shilong <shilong.wang@intel.com>
2022-04-29 22:27:59 +0800

    remove unused import
    Test-tag: pr cascading rebuild_container_rf
    Signed-off-by: Wang Shilong <shilong.wang@intel.com>

--- src/tests/ftest/rebuild/container_rf.py
+++ src/tests/ftest/rebuild/container_rf.py
@@ -5,7 +5,6 @@
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
 from container_rf_test_base import ContRedundancyFactor
-from apricot import skipForTicket
 
 
 class RbldContRfTest(ContRedundancyFactor):
