db41a03ae76082146f75ec96a850fdd042e6f942
Ashley Pittman <ashley.m.pittman@intel.com>
2022-05-06 18:07:47 +0100

    Correct IL install path.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/tests/ftest/daos_test/dfuse.py
+++ src/tests/ftest/daos_test/dfuse.py
@@ -82,7 +82,7 @@ class DaosCoreTestDfuse(DfuseTestBase):
         if intercept:
             remote_env = OrderedDict()
 
-            remote_env['LD_PRELOAD'] = os.path.join(self.prefix, 'libioil.so')
+            remote_env['LD_PRELOAD'] = os.path.join(self.prefix, 'lib64', 'libioil.so')
             remote_env['D_LOG_FILE'] = '/var/tmp/daos_testing/daos-il.log'
             remote_env['DD_MASK'] = 'all'
             remote_env['DD_SUBSYS'] = 'all'
