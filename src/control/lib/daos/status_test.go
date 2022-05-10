30fbfb4a099d090eebbb5bfe3a23a55c50401f63
Michael MacDonald <mjmac.macdonald@intel.com>
2022-05-10 09:45:50 -0400

    DAOS-10019 control: Create control/lib/daos (#8847)
    The drpc package pulls in dependencies like protobufs
    that aren't needed by many consumers of the non-dRPC
    constants and functions. Create a new standalone daos
    package to allow for cleaner imports.
    
    Also fixes cmd/daos property tests to use exported consts
    so that they don't need to use cgo directly. This keeps
    test-only code out of the produced binary.
    
    Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

--- src/control/lib/daos/status_test.go
+++ src/control/lib/daos/status_test.go
@@ -3,7 +3,7 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
-package drpc_test
+package daos_test
 
 import (
 	"testing"
@@ -11,20 +11,20 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/daos-stack/daos/src/control/common/test"
-	"github.com/daos-stack/daos/src/control/drpc"
+	"github.com/daos-stack/daos/src/control/lib/daos"
 )
 
-func TestDrpc_Status(t *testing.T) {
+func Testdaos_Status(t *testing.T) {
 	for name, tc := range map[string]struct {
 		in     int32
 		expErr error
 	}{
 		"rc 0": {
-			expErr: drpc.DaosSuccess,
+			expErr: daos.Success,
 		},
 		"-1007": {
 			in:     -1007,
-			expErr: drpc.DaosNoSpace,
+			expErr: daos.NoSpace,
 		},
 		"-424242": {
 			in:     -424242,
@@ -32,19 +32,19 @@ func TestDrpc_Status(t *testing.T) {
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
-			ds := drpc.DaosStatus(tc.in)
+			ds := daos.Status(tc.in)
 			test.CmpErr(t, tc.expErr, ds)
 		})
 	}
 }
 
-func TestDrpc_Error(t *testing.T) {
+func Testdaos_Error(t *testing.T) {
 	// Light test to make sure the error stringer works as expected.
-	for ds, expStr := range map[drpc.DaosStatus]string{
-		drpc.DaosSuccess:        "DER_SUCCESS(0): Success",
-		drpc.DaosProtocolError:  "DER_PROTO(-1014): Incompatible protocol",
-		drpc.DaosNotReplica:     "DER_NOTREPLICA(-2020): Not a service replica",
-		drpc.DaosStatus(424242): "DER_UNKNOWN(424242): Unknown error code 424242",
+	for ds, expStr := range map[daos.Status]string{
+		daos.Success:        "DER_SUCCESS(0): Success",
+		daos.ProtocolError:  "DER_PROTO(-1014): Incompatible protocol",
+		daos.NotReplica:     "DER_NOTREPLICA(-2020): Not a service replica",
+		daos.Status(424242): "DER_UNKNOWN(424242): Unknown error code 424242",
 	} {
 		t.Run(expStr, func(t *testing.T) {
 			test.AssertEqual(t, expStr, ds.Error(), "not equal")
