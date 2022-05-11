14e2406744275bcc499ca0d623a42ace6fd1743e
Mohamad Chaarawi <mohamad.chaarawi@intel.com>
2022-05-11 09:02:53 -0500

    DAOS-10569 client: fix build on aarch64 (#8986)
    * New mutex added in private event did not take into consideration extra
    8 byte of mutex size on aarch64. add that padding to private event size.
    * move the magic int to be next to the pthread key since both are
    4 bytes so compiler doesn't insert an extra 8 byte pads (4 each) when
    statically checking the size.
    
    Signed-off-by: Mohamad Chaarawi <mohamad.chaarawi@intel.com>

--- src/client/dfuse/dfuse_obj_da.c
+++ src/client/dfuse/dfuse_obj_da.c
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2017-2021 Intel Corporation.
+ * (C) Copyright 2017-2022 Intel Corporation.
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -26,6 +26,7 @@ struct da_entry {
 };
 
 struct obj_da {
+	int magic;                 /* magic number for sanity */
 	pthread_key_t key;         /* key to threadprivate data */
 	pthread_mutex_t lock;      /* lock thread events */
 	d_list_t free_entries;     /* entries put in da by dead thread */
@@ -34,7 +35,6 @@ struct obj_da {
 	size_t obj_size;           /* size of objects in da */
 	size_t padded_size;        /* real size of objects in da */
 	size_t block_size;         /* allocation size */
-	int magic;                 /* magic number for sanity */
 };
 
 #define PAD8(size) ((size + 7) & ~7)
