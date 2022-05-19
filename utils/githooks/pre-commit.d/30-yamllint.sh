216ba29a7bbe990fe3694982987efd93132d7e43
Brian J. Murrell <brian.murrell@intel.com>
2022-05-19 13:35:48 -0400

    DAOS-623 lint: Only run yamllint on changed files
    Skip-test: true
    Skip-PR-comments: true
    Required-githooks: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- utils/githooks/pre-commit.d/30-yamllint.sh
+++ utils/githooks/pre-commit.d/30-yamllint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Runs yamllint for the DAOS project.
 #
@@ -6,12 +6,14 @@
 
 set -ue
 
-if ! command -v yamllint > /dev/null 2>&1
-then
+if ! command -v yamllint > /dev/null 2>&1; then
     echo "No yaml checking, install yamllint command to improve pre-commit checks"
     echo "python3 -m pip install yamllint"
     exit 0
 fi
 
 echo "Checking yaml formatting"
-yamllint --strict .
+IFS=' ' read -r -a yaml_files <<< "$(git diff --cached --name-only --diff-filter=ACMRTUXB | grep -E '\.ya?ml$')"
+if [ ${#yaml_files[@]} -gt 0 ]; then
+    yamllint --strict "${yaml_files[@]}"
+fi
