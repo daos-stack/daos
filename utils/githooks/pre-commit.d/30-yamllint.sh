f6d66dd35199594499cb08da125ecf49d570c87a
Jeff Olivier <jeffrey.v.olivier@intel.com>
2022-05-19 15:18:11 -0600

    DAOS-623 hooks: Make the yamllint hook only run on diffs
    Also fix some comments to make it clear how a user might workaround
    issues locally.
    
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- utils/githooks/pre-commit.d/30-yamllint.sh
+++ utils/githooks/pre-commit.d/30-yamllint.sh
@@ -2,8 +2,9 @@
 
 # Runs yamllint for the DAOS project.
 #
-# Picks up yamllint config settings from .yamllint
-
+# Picks up yamllint config settings from .yamllint.yaml
+# The yamllint tool will use .yamllint in preference to this file and it is in .gitignore so users
+# can create one locally to overrule these settings for local commit hooks.
 set -ue
 
 if ! command -v yamllint > /dev/null 2>&1
@@ -14,4 +15,12 @@ then
 fi
 
 echo "Checking yaml formatting"
-yamllint --strict .
+targets=(
+    '*.yml'
+    '*.yaml'
+)
+
+files=${files:=$(git diff --diff-filter=ACMRTUXB --name-only --cached -- "${targets[@]}")}
+if [ -n "$files" ]; then
+    yamllint --strict $files
+fi
