aeaa76c6aac2bd2fac2f3a27c572da3afe10b404
Phillip Henderson <phillip.henderson@intel.com>
2022-06-02 15:54:50 -0400

    DAOS-6004 test: Re-enabled skipped tests
    Removing skipForTicket decorators and CANCEL_FOR_TICKET entries for
    tests whose tickets have been resolved.
    
    Skip-unit-tests: true
    Test-tag: dmg_system_reformat test_ior_latency_telemetry ec_offline_rebuild_agg_default ec_offline_agg_during_rebuild ec_online_rebuild_fio ec_offline_rebuild_fio pool,permission
    
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/control/dmg_system_reformat.py
+++ src/tests/ftest/control/dmg_system_reformat.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 """
-  (C) Copyright 2020-2021 Intel Corporation.
+  (C) Copyright 2020-2022 Intel Corporation.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
-from apricot import skipForTicket
 from avocado.core.exceptions import TestFail
 from pool_test_base import PoolTestBase
 
@@ -19,7 +18,6 @@ class DmgSystemReformatTest(PoolTestBase):
     :avocado: recursive
     """
 
-    @skipForTicket("DAOS-6004")
     def test_dmg_system_reformat(self):
         """
         JIRA ID: DAOS-5415
