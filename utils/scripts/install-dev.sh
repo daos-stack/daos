267cba1a02e68becede248e7a105c5beba0ae729
Kris Jacque <kristin.jacque@intel.com>
2022-06-10 16:47:55 -0600

    DAOS-10816 utils: Add setup-dev.sh script
    Added a script for developers setting up a new environment, to
    install both system packages and user-level Python modules needed
    to build DAOS.
    
    Signed-off-by: Kris Jacque <kristin.jacque@intel.com>

---
+++ utils/scripts/install-dev.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script sets up a new development node with all dependencies needed to
+# build DAOS from source.
+
+# Requirements:
+# - Run as a non-root user with sudo access.
+
+if [ `whoami` == "root" ]; then
+	echo "Run this script as the non-root user you will use to build DAOS"
+	exit 1
+fi
+
+distro=$1
+case "$distro" in
+	centos7 | el8 | leap15 | ubuntu)
+		echo "Setting up node for $distro...";;
+
+	*)
+		echo "Invalid distro option: \"$distro\""
+		echo "syntax: $0 <el8|leap15|ubuntu|centos7>"
+		exit 1
+esac
+
+# Install dependencies from the OS repos.
+SCRIPT_DIR="$(dirname -- "$(readlink -f "$BASH_SOURCE")")"
+sudo "$SCRIPT_DIR/install-$distro.sh"
+
+if [ $? -ne 0 ]; then
+	exit $?
+fi
+
+# Install user dependencies
+python3 -m pip install -r "$SCRIPT_DIR/../../requirements.txt"
