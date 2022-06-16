7ee97ca9983eaf5b947c97e53c2c4b2ff41e3a41
Jeff Olivier <jeffrey.v.olivier@intel.com>
2022-06-16 14:25:36 -0600

    don't clone the env unnecessarily
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>
    
    Required-githooks: true
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- site_scons/site_tools/extra/extra.py
+++ site_scons/site_tools/extra/extra.py
@@ -96,8 +96,7 @@ def _find_indent():
 def _pp_gen(source, target, env, indent):
     """generate commands for preprocessor builder"""
     action = []
-    nenv = env.Clone()
-    cccom = nenv.subst("$CCCOM").replace(" -o ", " ")
+    cccom = env.subst("$CCCOM").replace(" -o ", " ")
     for src, tgt in zip(source, target):
         if indent:
             action.append(f'{cccom} -E -P {src} | {indent} > {tgt}')
@@ -122,7 +121,7 @@ def _preprocess_emitter(source, target, env):
                 continue
             if mod != "":
                 prefix = prefix + "_" + mod
-        newtarget = os.path.join(dirname, f'{prefix}{base}_{ext}')
+        newtarget = os.path.join(dirname, f'{prefix}{base}_pp{ext}')
         target.append(newtarget)
     return target, source
 
