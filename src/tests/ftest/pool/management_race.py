262087cb7c25e0f20fcb445f5976a725acc787a8
Ding Ho <ding-hwa.ho@intel.com>
2022-06-22 20:47:47 +0000

    Update per review comments.
    Quick-Functional: true
    Test-tag: pool_mgmt_race
    Test-repeat: 3
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/pool/management_race.py
+++ src/tests/ftest/pool/management_race.py
@@ -10,6 +10,7 @@ import random
 from apricot import TestWithServers
 from thread_manager import ThreadManager
 from command_utils_base import CommandFailure
+from pydaos.raw import DaosApiError
 
 class PoolManagementRace(TestWithServers):
     """
@@ -23,30 +24,26 @@ class PoolManagementRace(TestWithServers):
     """
 
     def del_recreate_query_and_list_pools(
-            self, del_recreate=True, pool_num=1, thread_num=1, test_loop=2):
+            self, del_recreate=True, test_pool=None, thread_num=1, test_loop=2):
         """ To delete and recreate pool with the same label, to query and list pools.
         Args:
             del_recreate (bool): flag to delete and recreate pool with the same label.
-            pool_num (int): pool number.
+            test_pool (TestPool): the pool to delete/create/query.
             thread_num (int): thread number.
             test_loop (int): number of test loop to be executed.
         """
 
         wait_time = self.params.get("wait_time", '/run/boundary_test/*')
         max_query_time = self.params.get("max_query_time", '/run/boundary_test/*')
-        pool_label_prefix = self.params.get("pool_label_prefix", '/run/boundary_test/*')
-        label = pool_label_prefix + str(pool_num)
+        pool_id = test_pool.identifier
         for test_num in range(test_loop):
             if del_recreate:
-                self.pool[pool_num].destroy()
+                test_pool.destroy()
                 self.log.info(
-                    "--(%d.2.%d.%d)Pool %s deleted.\n", thread_num, pool_num, test_num, label)
-                new_pool = self.get_pool(create=False)
-                new_pool.label.update(label)
-                new_pool.create()
-                self.pool[pool_num] = new_pool
-                self.log.info("--(%d.3.%d.%d)Pool %s recreated.\n",
-                              thread_num, pool_num, test_num, label)
+                    "--(%d.2.%s.%d)Pool %s deleted.\n", thread_num, pool_id, test_num, pool_id)
+                test_pool.create()
+                self.log.info("--(%d.3.%s.%d)Pool %s recreated.\n",
+                              thread_num, pool_id, test_num, pool_id)
                 # pool stays with a random time before destroy
                 pool_stay_time = random.randint(1, 3) #nosec
                 time.sleep(pool_stay_time)
@@ -55,21 +52,23 @@ class PoolManagementRace(TestWithServers):
                 start = float(time.time())
                 while not completed:
                     try:
-                        self.log.info("--(%d.4.%d.%d)--pool querying.. \n",
-                                      thread_num, pool_num, test_num)
-                        self.get_dmg_command().pool_query(pool=label)
-                        self.log.info("--(%d.5.%d.%d)--get pool list all.. \n",
-                                      thread_num, pool_num, test_num)
-                        self.get_dmg_command().get_pool_list_all()
+                        self.log.info("--(%d.4.%s.%d)--pool querying.. \n",
+                                      thread_num, pool_id, test_num)
+                        self.get_dmg_command().pool_query(pool=pool_id)
+                        daos_tool = self.get_daos_command()
+                        daos_tool.exit_status_exception = False
+                        daos_tool.pool_query(pool=pool_id)
                         completed = True
-                    except CommandFailure as error:
+                    except (DaosApiError, CommandFailure) as error:
                         self.log.info(
                             "-->Pool %s deleted by another thread, "
-                            "race condition detected, retry.. %s", label, error)
+                            "race condition detected by dmg, retry.. %s", pool_id, error)
                         time.sleep(wait_time)
                         if float(time.time()) - start > max_query_time:
-                            self.fail("#(%d.5.%d.%d)Pool query failed and retry timeout.",
-                                      thread_num, pool_num, test_num)
+                            self.fail("#(%d.5.%s.%d)Pool query failed and retry timeout.",
+                                      thread_num, pool_id, test_num)
+                daos_tool.exit_status_exception = True
+                self.log.info("-->Test thread %d, test_loop %d completed.", thread_num, test_num)
 
     def test_pool_management_race(self):
         """JIRA ID: DAOS-8463: Pool management integrity with race boundary conditions
@@ -96,28 +95,22 @@ class PoolManagementRace(TestWithServers):
         num_pools = self.params.get("num_pools", '/run/boundary_test/*')
         test_loop = self.params.get("test_loop", '/run/boundary_test/*')
         num_query_threads = self.params.get("num_query_threads", '/run/boundary_test/*')
-        pool_label_prefix = self.params.get("pool_label_prefix", '/run/boundary_test/*')
         self.pool = []
         for pool_number in range(num_pools):
-            label = pool_label_prefix + str(pool_number)
-            self.pool.append(self.get_pool(create=False))
-            self.pool[-1].label.update(label)
-            self.pool[-1].create()
-            self.log.info("==(1.%d) pool created, %d.", pool_number, self.pool[-1])
+            self.pool.append(self.get_pool())
+            self.log.info("==(1.%d) pool created, %s.", pool_number, self.pool[-1].identifier)
 
         # Randomly select a pool for delete, recreate and query
-        pool_number = random.randint(0, len(self.pool)-1) #nosec
+        pool_number = random.randint(0, len(self.pool)-1)   # nosec
 
         # Setup the thread manager for del_and_recreate_pool
-        thread_num = 1
         thread_manager = ThreadManager(self.del_recreate_query_and_list_pools, self.timeout - 30)
-        thread_manager.add(
-            del_recreate=True, pool_num=pool_number, thread_num=thread_num, test_loop=test_loop)
-        thread_num += 1
-        # Setup multiple threads query_and_list_pools
-        for thd in range(thread_num, num_query_threads+thread_num):
+        for index, t_pool in enumerate(self.pool):
             thread_manager.add(
-                del_recreate=False, pool_num=pool_number, thread_num=thd, test_loop=test_loop)
+                del_recreate=False, test_pool=t_pool, thread_num=index, test_loop=test_loop)
+        # Setup the thread manager for del_and_recreate_pool
+        thread_manager.add(
+            del_recreate=True, test_pool=self.pool[pool_number], thread_num=index, test_loop=test_loop)
 
         # Launch all the threads
         self.log.info("==Launching %d delete_and_recreate_pool and query_and_list_pools threads",
