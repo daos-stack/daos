4fe6aed001e470f13323ed650f02d3f460899fb8
Ashley Pittman <ashley.m.pittman@intel.com>
2022-06-24 09:39:04 +0100

    Bump timeouts again.
    Test-tag: dfusedaosbuild
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/tests/ftest/dfuse/daos_build.py
+++ src/tests/ftest/dfuse/daos_build.py
@@ -65,7 +65,7 @@ class DaosBuild(DfuseTestBase):
 
         # How long to cache things for, if caching is enabled.
         cache_time = '60m'
-        build_time = 90
+        build_time = 180
 
         if cache_mode == 'writeback':
             cont_attrs['dfuse-data-cache'] = 'on'
