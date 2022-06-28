6f3e38efae0c982624c4551fd279f9d4526a2cdf
Ashley Pittman <ashley.m.pittman@intel.com>
2022-06-28 17:01:11 +0100

    Turn down IL debugging.
    Test-tag: dfusedaosbuild
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/tests/ftest/dfuse/daos_build.py
+++ src/tests/ftest/dfuse/daos_build.py
@@ -111,7 +111,7 @@ class DaosBuild(DfuseTestBase):
             remote_env['D_LOG_FILE'] = '/var/tmp/daos_testing/daos-il.log'
             remote_env['DD_MASK'] = 'all'
             remote_env['DD_SUBSYS'] = 'all'
-            remote_env['D_LOG_MASK'] = 'INFO'
+            remote_env['D_LOG_MASK'] = 'WARN,IL=INFO'
 
         envs = ['export {}={}'.format(env, value) for env, value in remote_env.items()]
 
