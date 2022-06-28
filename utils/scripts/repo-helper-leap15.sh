d40637a5c8a62ef26ecddc97dc12c49c22bc04ff
John E Malmberg <john.e.malmberg@intel.com>
2022-06-28 15:01:41 -0500

    CORCI-1122 Fix: Shellcheck issue
    Skip-scan-rpms: false
    Skip-scan-centos-rpms: false
    Skip-scan-el-8-rpms: false
    Skip-scan-leap-15-rpms: false
    
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>
    
    Required-githooks: true

--- utils/scripts/repo-helper-leap15.sh
+++ utils/scripts/repo-helper-leap15.sh
@@ -7,7 +7,7 @@ if [ -n "$REPO_FILE_URL" ]; then
     cd /etc/dnf/repos.d/ &&                 \
     mv daos_ci-leap15-artifactory.repo{,.tmp}
     for file in *.repo; do
-        true > $file
+        true > "$file"
     done
     mv daos_ci-leap15-artifactory.repo{.tmp,}
 fi
