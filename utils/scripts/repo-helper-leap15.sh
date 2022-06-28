f94c551bfe38bfae29fa02bdb61d1c49715a0366
John E Malmberg <john.e.malmberg@intel.com>
2022-06-28 14:31:46 -0500

    CORCI-1122 Build: Disable mirrorcache lookup
    Skip-scan-rpms: false
    Skip-scan-centos-rpms: false
    Skip-scan-el-8-rpms: false
    Skip-scan-leap-15-rpms: false
    
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>
    
    Required-githooks: true

---
+++ utils/scripts/repo-helper-leap15.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -uex
+
+: "${REPO_FILE_URL:=}"
+
+if [ -n "$REPO_FILE_URL" ]; then
+    cd /etc/dnf/repos.d/ &&                 \
+    mv daos_ci-leap15-artifactory.repo{,.tmp}
+    for file in *.repo; do
+        true > $file
+    done
+    mv daos_ci-leap15-artifactory.repo{.tmp,}
+fi
