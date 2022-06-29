f9d260df7b2672275483b310539f86e42572653a
Eduardo Berrocal <eduardo.berrocal@intel.com>
2022-06-29 17:56:57 +0000

    DAOS-10896: Fixing wrong mod num for pipeline API
    Signed-off-by: Eduardo Berrocal <eduardo.berrocal@intel.com>

--- src/include/daos/rpc.h
+++ src/include/daos/rpc.h
@@ -51,9 +51,8 @@ enum daos_module_id {
 	DAOS_RDBT_MODULE	= 8, /** rdb test */
 	DAOS_SEC_MODULE		= 9, /** security framework */
 	DAOS_DTX_MODULE		= 10, /** DTX */
-
-	DAOS_NR_MODULE		= 11, /** number of defined modules */
-	DAOS_PIPELINE_MODULE	= 12,
+	DAOS_PIPELINE_MODULE	= 11,
+	DAOS_NR_MODULE		= 12, /** number of defined modules */
 	DAOS_MAX_MODULE		= 64  /** Size of uint64_t see dmg profile */
 };
 
