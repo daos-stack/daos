ae4a4532b5bba820668cc37daaf565643da831e0
Tom Nabarro <tom.nabarro@intel.com>
2022-07-04 05:20:09 -0400

    update and expand unit test coverage
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>
    Required-githooks: true

--- src/control/server/mgmt_cont_test.go
+++ src/control/server/mgmt_cont_test.go
@@ -35,111 +35,115 @@ func makeBadBytes(count int) (badBytes []byte) {
 	return
 }
 
-func newTestListContReq() *mgmtpb.ListContReq {
-	return &mgmtpb.ListContReq{
-		Sys: build.DefaultSystemName,
-		Id:  "12345678-1234-1234-1234-123456789abc",
-	}
-}
-
-func TestListCont_NoMS(t *testing.T) {
-	log, buf := logging.NewTestLogger(t.Name())
-	defer test.ShowBufferOnFailure(t, buf)
-
-	db := raft.MockDatabase(t, log)
-	ms := system.MockMembership(t, log, db, mockTCPResolver)
-	svc := newMgmtSvc(NewEngineHarness(log), ms, db, nil,
-		events.NewPubSub(context.Background(), log))
-
-	resp, err := svc.ListContainers(context.TODO(), newTestListContReq())
-
-	if resp != nil {
-		t.Errorf("Expected no response, got: %+v", resp)
-	}
-
-	test.CmpErr(t, FaultHarnessNotStarted, err)
-}
-
-func TestListCont_DrpcFailed(t *testing.T) {
-	log, buf := logging.NewTestLogger(t.Name())
-	defer test.ShowBufferOnFailure(t, buf)
-
-	svc := newTestMgmtSvc(t, log)
-	expectedErr := errors.New("mock error")
-	setupMockDrpcClient(svc, nil, expectedErr)
-
-	resp, err := svc.ListContainers(context.TODO(), newTestListContReq())
-
-	if resp != nil {
-		t.Errorf("Expected no response, got: %+v", resp)
-	}
-
-	test.CmpErr(t, expectedErr, err)
-}
-
-func TestPoolListCont_BadDrpcResp(t *testing.T) {
-	log, buf := logging.NewTestLogger(t.Name())
-	defer test.ShowBufferOnFailure(t, buf)
-
-	svc := newTestMgmtSvc(t, log)
-	// dRPC call returns junk in the message body
-	badBytes := makeBadBytes(12)
-
-	setupMockDrpcClientBytes(svc, badBytes, nil)
-
-	resp, err := svc.ListContainers(context.TODO(), newTestListContReq())
-
-	if resp != nil {
-		t.Errorf("Expected no response, got: %+v", resp)
-	}
-
-	test.CmpErr(t, errors.New("unmarshal"), err)
-}
-
-func TestListCont_ZeroContSuccess(t *testing.T) {
-	log, buf := logging.NewTestLogger(t.Name())
-	defer test.ShowBufferOnFailure(t, buf)
-
-	svc := newTestMgmtSvc(t, log)
-
-	expectedResp := &mgmtpb.ListContResp{}
-	setupMockDrpcClient(svc, expectedResp, nil)
-
-	resp, err := svc.ListContainers(context.TODO(), newTestListContReq())
-
-	if err != nil {
-		t.Errorf("Expected no error, got: %v", err)
-	}
-
-	if diff := cmp.Diff(expectedResp, resp, test.DefaultCmpOpts()...); diff != "" {
-		t.Fatalf("bad response (-want, +got): \n%s\n", diff)
-	}
-}
-
-func TestListCont_ManyContSuccess(t *testing.T) {
-	log, buf := logging.NewTestLogger(t.Name())
-	defer test.ShowBufferOnFailure(t, buf)
-
-	svc := newTestMgmtSvc(t, log)
-
-	expectedResp := &mgmtpb.ListContResp{
-		Containers: []*mgmtpb.ListContResp_Cont{
-			{Uuid: "56781234-5678-5678-5678-123456789abc"},
-			{Uuid: "67812345-6781-6781-6781-123456789abc"},
-			{Uuid: "78123456-7812-7812-7812-123456789abc"},
-			{Uuid: "81234567-8123-8123-8123-123456789abc"},
+func testPoolService() *system.PoolService {
+	return &system.PoolService{
+		PoolLabel: "test-pool",
+		PoolUUID:  uuid.MustParse(mockUUID),
+		Replicas:  []system.Rank{0, 1, 2},
+		State:     system.PoolServiceStateReady,
+		Storage: &system.PoolServiceStorage{
+			CreationRankStr: system.MustCreateRankSet("0-2").String(),
 		},
 	}
-	setupMockDrpcClient(svc, expectedResp, nil)
+}
 
-	resp, err := svc.ListContainers(context.TODO(), newTestListContReq())
-
-	if err != nil {
-		t.Errorf("Expected no error, got: %v", err)
+func TestMgmt_ListContainers(t *testing.T) {
+	validListContReq := func() *mgmtpb.ListContReq {
+		return &mgmtpb.ListContReq{
+			Sys: build.DefaultSystemName,
+			Id:  mockUUID,
+		}
 	}
 
-	if diff := cmp.Diff(expectedResp, resp, test.DefaultCmpOpts()...); diff != "" {
-		t.Fatalf("bad response (-want, +got): \n%s\n", diff)
+	multiConts := []*mgmtpb.ListContResp_Cont{
+		{Uuid: "56781234-5678-5678-5678-123456789abc"},
+		{Uuid: "67812345-6781-6781-6781-123456789abc"},
+		{Uuid: "78123456-7812-7812-7812-123456789abc"},
+		{Uuid: "81234567-8123-8123-8123-123456789abc"},
+	}
+
+	for name, tc := range map[string]struct {
+		createMS  func(*testing.T, logging.Logger) *mgmtSvc
+		setupDrpc func(*testing.T, *mgmtSvc)
+		req       *mgmtpb.ListContReq
+		expResp   *mgmtpb.ListContResp
+		expErr    error
+	}{
+		"nil req": {
+			expErr: errors.New("nil"),
+		},
+		"pool svc not found": {
+			req: &mgmtpb.ListContReq{
+				Sys: build.DefaultSystemName,
+				Id:  "fake",
+			},
+			expErr: errors.New("unable to find pool"),
+		},
+		"harness not started": {
+			createMS: func(t *testing.T, log logging.Logger) *mgmtSvc {
+				db := raft.MockDatabase(t, log)
+				ms := system.MockMembership(t, log, db, mockTCPResolver)
+				return newMgmtSvc(NewEngineHarness(log), ms, db, nil,
+					events.NewPubSub(context.Background(), log))
+			},
+			req:    validListContReq(),
+			expErr: FaultHarnessNotStarted,
+		},
+		"drpc error": {
+			setupDrpc: func(t *testing.T, svc *mgmtSvc) {
+				setupMockDrpcClient(svc, nil, errors.New("mock drpc"))
+			},
+			req:    validListContReq(),
+			expErr: errors.New("mock drpc"),
+		},
+		"bad drpc resp": {
+			setupDrpc: func(t *testing.T, svc *mgmtSvc) {
+				badBytes := makeBadBytes(16)
+				setupMockDrpcClientBytes(svc, badBytes, nil)
+			},
+			req:    validListContReq(),
+			expErr: errors.New("unmarshal"),
+		},
+		"success; zero containers": {
+			setupDrpc: func(t *testing.T, svc *mgmtSvc) {
+				setupMockDrpcClient(svc, &mgmtpb.ListContResp{}, nil)
+			},
+			req:     validListContReq(),
+			expResp: &mgmtpb.ListContResp{},
+		},
+		"success; multiple containers": {
+			setupDrpc: func(t *testing.T, svc *mgmtSvc) {
+				setupMockDrpcClient(svc, &mgmtpb.ListContResp{
+					Containers: multiConts,
+				}, nil)
+			},
+			req: validListContReq(),
+			expResp: &mgmtpb.ListContResp{
+				Containers: multiConts,
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			log, buf := logging.NewTestLogger(t.Name())
+			defer test.ShowBufferOnFailure(t, buf)
+
+			if tc.createMS == nil {
+				tc.createMS = newTestMgmtSvc
+			}
+			svc := tc.createMS(t, log)
+			addTestPoolService(t, svc.sysdb, testPoolService())
+
+			if tc.setupDrpc != nil {
+				tc.setupDrpc(t, svc)
+			}
+
+			resp, err := svc.ListContainers(context.TODO(), tc.req)
+
+			test.CmpErr(t, tc.expErr, err)
+			if diff := cmp.Diff(tc.expResp, resp, test.DefaultCmpOpts()...); diff != "" {
+				t.Fatalf("(-want, +got): \n%s\n", diff)
+			}
+		})
 	}
 }
 
@@ -152,16 +156,6 @@ func TestMgmt_ContSetOwner(t *testing.T) {
 			Owneruser:  "user@",
 			Ownergroup: "group@",
 		}
-	}
-
-	testPoolService := &system.PoolService{
-		PoolLabel: "test-pool",
-		PoolUUID:  uuid.MustParse(mockUUID),
-		Replicas:  []system.Rank{0, 1, 2},
-		State:     system.PoolServiceStateReady,
-		Storage: &system.PoolServiceStorage{
-			CreationRankStr: system.MustCreateRankSet("0-2").String(),
-		},
 	}
 
 	for name, tc := range map[string]struct {
@@ -227,7 +221,7 @@ func TestMgmt_ContSetOwner(t *testing.T) {
 				tc.createMS = newTestMgmtSvc
 			}
 			svc := tc.createMS(t, log)
-			addTestPoolService(t, svc.sysdb, testPoolService)
+			addTestPoolService(t, svc.sysdb, testPoolService())
 
 			if tc.setupDrpc != nil {
 				tc.setupDrpc(t, svc)
