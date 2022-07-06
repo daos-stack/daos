fa5c9d89d65c461502d6a3cf2f9978aa23814e11
Ding Ho <ding-hwa.ho@intel.com>
2022-07-06 13:58:09 +0000

    Set the CMOCKA* environment variables correctly to ensure cmocka results are generated in the expected location to correctly log any test errors. (Add codes to verify cmocka_result.xml file exists before scp them)
    Skip-unit-tests: true
    Test-tag: dfs_test dfuse_test daos_test
    
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/util/daos_core_base.py
+++ src/tests/ftest/util/daos_core_base.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 """
   (C) Copyright 2018-2022 Intel Corporation.
-
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
 
 import os
-import write_host_file
+from pathlib import Path
 
 from avocado import fail_on
 from avocado.utils import process
 from apricot import TestWithServers
-from general_utils import get_log_file
+from general_utils import get_log_file, get_clush_command, run_command, log_task
+from command_utils_base import EnvironmentVariables
 from command_utils import ExecutableCommand
 from exception_utils import CommandFailure
 from agent_utils import include_local_host
@@ -21,7 +21,6 @@ from test_utils_pool import POOL_TIMEOUT_INCREMENT
 
 class DaosCoreBase(TestWithServers):
     """Runs the daos_test subtests with multiple servers.
-
     :avocado: recursive
     """
 
@@ -40,23 +39,17 @@ class DaosCoreBase(TestWithServers):
 
         super().setUp()
 
-        # if no client specified update self.hostlist_clients to local host
-        # and create a new self.hostfile_clients.
+        # if no clients are specified update self.hostlist_clients to be the local host
         if self.hostlist_clients is None:
             self.hostlist_clients = include_local_host(self.hostlist_clients)
-            self.hostfile_clients = write_host_file.write_host_file(
-                self.hostlist_clients, self.workdir, None)
 
     def get_test_param(self, name, default=None):
         """Get the test-specific test yaml parameter value.
-
         Args:
             name (str): name of the test yaml parameter to get
             default (object): value to return if a value is not found
-
         Returns:
             object: the test-specific test yaml parameter value
-
         """
         path = "/".join(["/run/daos_tests", name, "*"])
         return self.params.get(self.get_test_name(), path, default)
@@ -64,17 +57,13 @@ class DaosCoreBase(TestWithServers):
     @fail_on(CommandFailure)
     def start_server_managers(self, force=False):
         """Start the daos_server processes on each specified list of hosts.
-
         Enable scalable endpoint if requested with a test-specific
         'scalable_endpoint' yaml parameter.
-
         Args:
             force (bool, optional): whether or not to force starting the
                 servers. Defaults to False.
-
         Returns:
             bool: whether or not to force the starting of the agents
-
         """
         # Enable scalable endpoint (if requested) prior to starting the servers
         scalable_endpoint = self.get_test_param("scalable_endpoint")
@@ -114,7 +103,7 @@ class DaosCoreBase(TestWithServers):
             num_clients = self.params.get("num_clients", '/run/daos_tests/*')
 
         scm_size = self.params.get("scm_size", '/run/pool/*')
-        nvme_size = self.params.get("nvme_size", '/run/pool/*')
+        nvme_size = self.params.get("nvme_size", '/run/pool/*', 0)
         args = self.get_test_param("args", "")
         stopped_ranks = self.get_test_param("stopped_ranks", [])
         pools_created = self.get_test_param("pools_created", 1)
@@ -126,34 +115,30 @@ class DaosCoreBase(TestWithServers):
                 get_log_file("daosCA/certs"), self.hostlist_clients)
             dmg.copy_configuration(self.hostlist_clients)
 
-        cmd = " ".join(
-            [
-                "-x", "=".join(["D_LOG_FILE", get_log_file(self.client_log)]),
-                "--map-by node", "-x", "D_LOG_MASK=DEBUG",
-                "-x", "DD_MASK=mgmt,io,md,epc,rebuild",
-                "-x", "COVFILE=/tmp/test.cov",
-                self.daos_test,
-                "-n", dmg_config_file,
-                "".join(["-", subtest]),
-                str(args)
-            ]
-        )
+        # Temporarily place cmocka results in a test_dir subdirectory
+        cmocka_dir = os.path.join(self.test_dir, "cmocka")
+        log_task(self.hostlist_clients, " ".join(["mkdir", "-p", cmocka_dir]))
 
+        # Set up the daos test command and environment settings
+        cmd = " ".join([self.daos_test, "-n", dmg_config_file, "".join(["-", subtest]), str(args)])
+        env = EnvironmentVariables({
+            "D_LOG_FILE": get_log_file(self.client_log),
+            "D_LOG_MASK": "DEBUG",
+            "DD_MASK": "mgmt,io,md,epc,rebuild",
+            "COVFILE": "/tmp/test.cov",
+            "CMOCKA_XML_FILE": os.path.join(cmocka_dir, "%g_cmocka_results.xml"),
+            "CMOCKA_MESSAGE_OUTPUT": "xml",
+            "POOL_SCM_SIZE": str(scm_size),
+            "POOL_NVME_SIZE": str(nvme_size),
+        })
+
+        # Assign the test to run
         job_cmd = ExecutableCommand(namespace=None, command=cmd)
         job = get_job_manager(self, "Orterun", job_cmd, mpi_type="openmpi")
-        # Assign the test to run
-        job.hostfile.update(self.hostfile_clients)
-        job.processes.update(num_clients)
+        job.assign_hosts(self.hostlist_clients, self.workdir, None)
+        job.assign_processes(num_clients)
+        job.assign_environment(env)
         job_str = str(job)
-
-        env = {}
-        env['CMOCKA_XML_FILE'] = os.path.join(self.outputdir,
-                                              "%g_cmocka_results.xml")
-        env['CMOCKA_MESSAGE_OUTPUT'] = "xml"
-        env['POOL_SCM_SIZE'] = "{}".format(scm_size)
-        if not nvme_size:
-            nvme_size = 0
-        env['POOL_NVME_SIZE'] = "{}".format(nvme_size)
 
         # Update the expected status for each ranks that will be stopped by this
         # test to avoid a false failure during tearDown().
@@ -170,7 +155,7 @@ class DaosCoreBase(TestWithServers):
                         rank, ["Stopped", "Excluded"])
 
         try:
-            process.run(job_str, env=env)
+            process.run(job_str)
         except process.CmdError as result:
             if result.result.exit_status != 0:
                 # fake a JUnit failure output
@@ -179,10 +164,31 @@ class DaosCoreBase(TestWithServers):
                 self.fail(
                     "{0} failed with return code={1}.\n".format(
                         job_str, result.result.exit_status))
+        finally:
+            # Copy any remote cmocka files back to this host
+            cmocka_result_file = os.path.join(cmocka_dir, "*_cmocka_results.xml")
+            host_list = []
+            for host in self.hostlist_clients:
+                if check_file_exists(host, cmocka_result_file):
+                    host_list.append(host)
+            if host_list != []:
+                command = "{} cp {} {}".format(
+                    get_clush_command(host_list, "-S -v --rcopy"),
+                    cmocka_result_file, cmocka_dir)
+                run_command(command)
+
+            # Move local files to the avocado test variant data directory
+            for cmocka_file in os.listdir(cmocka_dir):
+                if "_cmocka_results." in cmocka_file:
+                    # Rename *_cmocka_results.xml.node1 to *_cmocka_results.node1.xml
+                    cmocka_path = Path(cmocka_file)
+                    rename = cmocka_file.name.replace(
+                        "".join(cmocka_path.suffixes), "".join(reversed(cmocka_path.suffixes)))
+                    command = "mv {} {}".format(cmocka_file, os.path.join(self.outputdir, rename))
+                    run_command(command)
 
     def create_results_xml(self, testname, result, error_message="Test failed to start up"):
         """Create a JUnit result.xml file for the failed command.
-
         Args:
             testname (str): name of the test
             result (CmdResult): result of the failed command.
