a638d260e4190c79cf9ca88ce17f62d8f998cb67
Ashley Pittman <ashley.m.pittman@intel.com>
2022-07-14 00:11:45 +0100

    DAOS-8722 dfuse: Add timeouts to readdir caching. (#9610)
    Rather than let the kernel cache readdir data forever keep
    track of access times and honour the duration settings for
    caching.
    
    Fix a bug where dentry-time and data-cache both needed to
    be enabled for readdir caching, it's not entirely based
    on dentry-time.
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/client/dfuse/ops/create.c
+++ src/client/dfuse/ops/create.c
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2016-2021 Intel Corporation.
+ * (C) Copyright 2016-2022 Intel Corporation.
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -168,8 +168,6 @@ dfuse_cb_create(fuse_req_t req, struct dfuse_inode_entry *parent,
 	if (rc)
 		D_GOTO(release, rc);
 
-	oh->doh_dfs = dfs->dfs_ns;
-	oh->doh_ie = ie;
 	oh->doh_writeable = true;
 
 	if (dfs->dfc_data_caching) {
