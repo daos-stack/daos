980e505b47d7c6656ce43e04ab86a9bccbc4ded6
Joseph Moore <joseph.moore@intel.com>
2022-07-15 15:49:59 +0000

    CART-89 mercury: Fix UCX.
    Skip-build-centos7-gcc: true
    Skip-build-leap15-icc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-small: true
    Test-tag: pr daily_regression
    Test-provider: ucx+dc_x
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/tests/ftest/util/server_utils_params.py
+++ src/tests/ftest/util/server_utils_params.py
@@ -107,7 +107,7 @@ class DaosServerYamlParameters(YamlParameters):
         log_dir = os.environ.get("DAOS_TEST_LOG_DIR", "/tmp")
 
         self.provider = BasicParameter(None, default_provider)
-        self.crt_timeout = BasicParameter(None, 10)
+        self.crt_timeout = BasicParameter(None, 60)
         self.hyperthreads = BasicParameter(None, False)
         self.socket_dir = BasicParameter(None, "/var/run/daos_server")
         # Auto-calculate if unset or set to zero
