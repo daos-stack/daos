2610fd2842b29f1c1369f413c464c7abfbf6dc1b
Joseph Moore <joseph.moore@intel.com>
2022-07-18 16:37:32 +0000

    CART-89 mercury: Fix UCX.
    Skip-build-centos7-gcc: true
    Skip-build-leap15-icc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-small: true
    Test-tag: pr daily_regression
    Test-provider: ucx+dc_x
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -30,7 +30,7 @@ dump_envariables(void)
 		"CRT_CTX_SHARE_ADDR", "CRT_CTX_NUM", "D_FI_CONFIG",
 		"FI_UNIVERSE_SIZE", "CRT_ENABLE_MEM_PIN",
 		"FI_OFI_RXM_USE_SRX", "D_LOG_FLUSH", "CRT_MRC_ENABLE",
-		"RDMAV_HUGEPAGES_SAFE" };
+		"RDMAV_HUGEPAGES_SAFE", "UCX_LOG_LEVEL" };
 
 	D_INFO("-- ENVARS: --\n");
 	for (i = 0; i < ARRAY_SIZE(envars); i++) {
@@ -115,7 +115,12 @@ static int data_init(int server, crt_init_options_t *opt)
 	uint64_t	start_rpcid;
 	int		rc = 0;
 
+	if (crt_is_service())
+		sleep(30);
+
 	D_DEBUG(DB_ALL, "initializing crt_gdata...\n");
+
+	setenv("UCX_LOG_LEVEL", "info", 1);
 
 	dump_envariables();
 
