7010f2eb822f8b81547386230d7e84620c8f0c74
wangdi <di.wang@intel.com>
2022-07-20 07:45:43 -0700

    DAOS-10525 rebuild: inflight I/O after discarding object (#9196)
    During reintegration, if inflight I/O arrives before dicarding,
    then vos_dicard might dicard those data written by inflight I/O,
    so let's discard all of the original data first, and if inflight
    I/O arrives before discard finished, then it will return
    DER_UPDATE_AGAIN and retry.
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/include/daos_errno.h
+++ src/include/daos_errno.h
@@ -280,7 +280,10 @@ extern "C" {
 	       No service available)					\
 	/** The TX ID may be reused. */				\
 	ACTION(DER_TX_ID_REUSED,	(DER_ERR_DAOS_BASE + 40),	\
-	       TX ID may be reused)
+	       TX ID may be reused)					\
+	/** Re-update again */						\
+	ACTION(DER_UPDATE_AGAIN,	(DER_ERR_DAOS_BASE + 41),	\
+	       update again)						\
 
 /** Defines the gurt error codes */
 #define D_FOREACH_ERR_RANGE(ACTION)	\
