3a1c9f5c0ed6a87aca64a691323cc9a947c0fd3d
Wang Shilong <shilong.wang@intel.com>
2022-07-20 17:23:44 +0800

    DAOS-11128 object: return error properly in daos_oclass_fit_max()
    If system domain_nr don't require group size, return error directly.
    And if system's targets could not meet group size * groups number,
    we should return error rather than search for the highest scalability
    in the allowed range automatically which could be confusing for users.
    
    data_mover test now use SX for object creation, because this test
    run in VM, there will be only 2 targets available for pool, S4 will fail.
    
    Signed-off-by: Wang Shilong <shilong.wang@intel.com>
    Required-githooks: true

--- src/pool/srv_pool.c
+++ src/pool/srv_pool.c
@@ -512,6 +512,7 @@ init_pool_metadata(struct rdb_tx *tx, const rdb_path_t *kvs, uint32_t nnodes, co
 	int			ntargets = nnodes * dss_tgt_nr;
 	uint32_t		upgrade_global_version = DAOS_POOL_GLOBAL_VERSION;
 	int			rc;
+	struct daos_prop_entry *entry;
 
 	/* Generate the pool buffer. */
 	rc = gen_pool_buf(NULL /* map */, &map_buf, map_version, ndomains, nnodes, ntargets,
@@ -519,6 +520,15 @@ init_pool_metadata(struct rdb_tx *tx, const rdb_path_t *kvs, uint32_t nnodes, co
 	if (rc != 0) {
 		D_ERROR("failed to generate pool buf, "DF_RC"\n", DP_RC(rc));
 		goto out;
+	}
+
+	entry = daos_prop_entry_get(prop, DAOS_PROP_PO_REDUN_FAC);
+	if (entry) {
+		if (entry->dpe_val + 1 > map_buf->pb_domain_nr) {
+			D_ERROR("ndomains(%u) could not meet redunc factor(%lu)\n",
+				map_buf->pb_domain_nr, entry->dpe_val);
+			D_GOTO(out, rc = -DER_INVAL);
+		}
 	}
 
 	/* Initialize the pool map properties. */
