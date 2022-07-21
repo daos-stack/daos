0a830fd3f8ac2308344f12c68cf3140899414f91
Zuhair AlSader <zuhair.alsader@seagate.com>
2022-07-20 23:40:07 -0400

    Implement ds3_read_user
    Signed-off-by: Zuhair AlSader <zuhair.alsader@seagate.com>

--- src/client/libds3/user.c
+++ src/client/libds3/user.c
@@ -124,20 +124,57 @@ ds3_user_remove(const char *name, struct ds3_user_info *info, ds3_t *ds3, daos_e
 	return 0;
 }
 
+/**
+ * Helper function to read user by metadir
+ */
+static int
+ds3_read_user(const char *name, enum meta_dir by, struct ds3_user_info *info, ds3_t *ds3,
+	      daos_event_t *ev)
+{
+	if (ds3 == NULL || name == NULL || info == NULL)
+		return EINVAL;
+
+	// Open file
+	int        rc;
+	dfs_obj_t *user_obj;
+	rc = dfs_lookup_rel(ds3->meta_dfs, ds3->meta_dirs[by], name, O_RDWR, &user_obj, NULL, NULL);
+	if (rc != 0) {
+		return -ENOENT;
+	}
+
+	// Reserve buffers
+	d_iov_t     iov;
+	d_sg_list_t rsgl;
+	d_iov_set(&iov, info->encoded, info->encoded_length);
+	rsgl.sg_nr     = 1;
+	rsgl.sg_iovs   = &iov;
+	rsgl.sg_nr_out = 1;
+
+	// Read file
+	rc = dfs_read(ds3->meta_dfs, user_obj, &rsgl, 0, &info->encoded_length, ev);
+	if (rc != 0) {
+		D_ERROR("Failed to read user file, name = %s, rc = %d\n", name, rc);
+	}
+
+	// Close file
+	dfs_release(user_obj);
+	return rc;
+}
+
 int
 ds3_user_get(const char *name, struct ds3_user_info *info, ds3_t *ds3, daos_event_t *ev)
 {
-	return 0;
+	return ds3_read_user(name, USERS_DIR, info, ds3, ev);
 }
 
 int
 ds3_user_get_by_email(const char *email, struct ds3_user_info *info, ds3_t *ds3, daos_event_t *ev)
 {
-	return 0;
+	return ds3_read_user(email, EMAILS_DIR, info, ds3, ev);
 }
 
 int
 ds3_user_get_by_key(const char *key, struct ds3_user_info *info, ds3_t *ds3, daos_event_t *ev)
 {
-	return 0;
+	return ds3_read_user(key, ACCESS_KEYS_DIR, info, ds3, ev);
 }
