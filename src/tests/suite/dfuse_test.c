62c040134191bd0f5b8f688b1ffa0fc54d8003a8
Dalton Bohning <dalton.bohning@intel.com>
2022-07-28 14:10:43 +0000

    DAOS-11171 test: cleanup timespec_gt
    Test-tag: dfuse,pr dfuse,daily_regression,-dfuseenospace dfuse_unit
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>
    
    Required-githooks: true

--- src/tests/suite/dfuse_test.c
+++ src/tests/suite/dfuse_test.c
@@ -187,10 +187,10 @@ do_ioctl(void **state)
 static bool
 timespec_gt(struct timespec t1, struct timespec t2)
 {
-	if (t2.tv_sec == t1.tv_sec)
-		return t2.tv_nsec < t1.tv_nsec;
+	if (t1.tv_sec == t2.tv_sec)
+		return t1.tv_nsec > t2.tv_nsec;
 	else
-		return t2.tv_sec < t1.tv_sec;
+		return t1.tv_sec > t2.tv_sec;
 }
 
 void
