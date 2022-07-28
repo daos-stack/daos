2ff935a57a881cea605120a561c7e59e1014b7ef
Wang Shilong <shilong.wang@intel.com>
2022-07-28 23:49:37 +0800

    DAOS-10300 pool: report pool global version for list  (#9678)
    dmg pool query/list will output pool's current global version
    and latest global version, so that an Admin could know if pool
    upgrading is needed or not.
    
    Since this change pool protocol anyway, extend pool connection
    to return pool's redun fac as well.
    
    Signed-off-by: Wang Shilong shilong.wang@intel.com
    Co-authored-by: Tom Nabarro tom.nabarro@intel.com

--- src/control/lib/control/pool.go
+++ src/control/lib/control/pool.go
@@ -402,16 +402,18 @@ type (
 
 	// PoolInfo contains information about the pool.
 	PoolInfo struct {
-		TotalTargets    uint32               `json:"total_targets"`
-		ActiveTargets   uint32               `json:"active_targets"`
-		TotalEngines    uint32               `json:"total_engines"`
-		DisabledTargets uint32               `json:"disabled_targets"`
-		Version         uint32               `json:"version"`
-		Leader          uint32               `json:"leader"`
-		Rebuild         *PoolRebuildStatus   `json:"rebuild"`
-		TierStats       []*StorageUsageStats `json:"tier_stats"`
-		EnabledRanks    *system.RankSet      `json:"-"`
-		DisabledRanks   *system.RankSet      `json:"-"`
+		TotalTargets     uint32               `json:"total_targets"`
+		ActiveTargets    uint32               `json:"active_targets"`
+		TotalEngines     uint32               `json:"total_engines"`
+		DisabledTargets  uint32               `json:"disabled_targets"`
+		Version          uint32               `json:"version"`
+		Leader           uint32               `json:"leader"`
+		Rebuild          *PoolRebuildStatus   `json:"rebuild"`
+		TierStats        []*StorageUsageStats `json:"tier_stats"`
+		EnabledRanks     *system.RankSet      `json:"-"`
+		DisabledRanks    *system.RankSet      `json:"-"`
+		PoolLayoutVer    uint32               `json:"pool_layout_ver"`
+		UpgradeLayoutVer uint32               `json:"upgrade_layout_ver"`
 	}
 
 	// PoolQueryResp contains the pool query response.
@@ -903,6 +905,11 @@ type (
 		// TargetsDisabled is the number of inactive targets in pool.
 		TargetsDisabled uint32 `json:"targets_disabled"`
 
+		// Latest pool global version
+		UpgradeLayoutVer uint32 `json:"upgrade_layout_ver"`
+		// Current pool global version
+		PoolLayoutVer uint32 `json:"pool_layout_ver"`
+
 		// QueryErrorMsg reports an RPC error returned from a query.
 		QueryErrorMsg string `json:"query_error_msg"`
 		// QueryStatusMsg reports any DAOS error returned from a query
@@ -1062,6 +1069,8 @@ func ListPools(ctx context.Context, rpcClient UnaryInvoker, req *ListPoolsReq) (
 
 		p.TargetsTotal = resp.TotalTargets
 		p.TargetsDisabled = resp.DisabledTargets
+		p.PoolLayoutVer = resp.PoolLayoutVer
+		p.UpgradeLayoutVer = resp.UpgradeLayoutVer
 		p.setUsage(resp)
 	}
 
