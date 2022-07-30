833e59483762884c512ee8f8f92b2de331ed8a7f
Joseph Moore <joseph.moore@intel.com>
2022-07-30 23:13:32 +0000

    CART-89 cart: Fix UCX.
    Skip-build-centos7-gcc: true
    Skip-build-leap15-icc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-small: true
    Test-tag: pr daily_regression
    Test-provider: ucx+rc_x
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -29,7 +29,8 @@ dump_envariables(void)
 		"OFI_PORT", "OFI_INTERFACE", "OFI_DOMAIN", "CRT_CREDIT_EP_CTX",
 		"CRT_CTX_SHARE_ADDR", "CRT_CTX_NUM", "D_FI_CONFIG",
 		"FI_UNIVERSE_SIZE", "CRT_ENABLE_MEM_PIN",
-		"FI_OFI_RXM_USE_SRX", "D_LOG_FLUSH", "CRT_MRC_ENABLE" };
+		"FI_OFI_RXM_USE_SRX", "D_LOG_FLUSH", "CRT_MRC_ENABLE",
+		"RDMAV_HUGEPAGES_SAFE", "UCX_LOG_LEVEL", "UCX_IB_FORK_INIT" };
 
 	D_INFO("-- ENVARS: --\n");
 	for (i = 0; i < ARRAY_SIZE(envars); i++) {
@@ -116,6 +117,12 @@ static int data_init(int server, crt_init_options_t *opt)
 
 	D_DEBUG(DB_ALL, "initializing crt_gdata...\n");
 
+	/* setenv("UCX_LOG_LEVEL", "error", 1); */
+	setenv("UCX_IB_FORK_INIT", "n", 1);
+	/* setenv("D_LOG_MASK", "DEBUG", 1);
+	setenv("HG_LOG_LEVEL", "debug", 1);
+	setenv("HG_LOG_SUBSYS", "na", 1);
+*/
 	dump_envariables();
 
 	/*
