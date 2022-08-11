f3e9bdae9603a96e3918410cddae232b324940b5
Dalton Bohning <dalton.bohning@intel.com>
2022-08-11 18:49:35 +0000

    DAOS-11156 test: improve nvme/health.py
    Test-tag: test_monitor_for_large_pools pr,hw,medium
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    - Verify there is enough space available before creating each pool
    - Use self.get_dmg_command
    - Misc cleanup
    
    Required-githooks: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- src/tests/ftest/nvme/health.py
+++ src/tests/ftest/nvme/health.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 '''
   (C) Copyright 2020-2022 Intel Corporation.
 
@@ -6,9 +5,11 @@
 '''
 from __future__ import division
 
+from avocado.core.exceptions import TestFail
+
 from nvme_utils import ServerFillUp, get_device_ids
-from dmg_utils import DmgCommand
 from exception_utils import CommandFailure
+
 
 class NvmeHealth(ServerFillUp):
     # pylint: disable=too-many-ancestors
@@ -16,6 +17,7 @@ class NvmeHealth(ServerFillUp):
     Test Class Description: To validate NVMe health test cases
     :avocado: recursive
     """
+
     def test_monitor_for_large_pools(self):
         """Jira ID: DAOS-4722.
 
@@ -27,20 +29,18 @@ class NvmeHealth(ServerFillUp):
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,medium
-        :avocado: tags=nvme
-        :avocado: tags=nvme_health
+        :avocado: tags=nvme,pool
+        :avocado: tags=nvme_health,test_monitor_for_large_pools
         """
-        # pylint: disable=attribute-defined-outside-init
-        # pylint: disable=too-many-branches
         max_num_pools = self.params.get("max_num_pools", '/run/pool/*')
         total_pool_percentage = self.params.get("total_pool_percentage", '/run/pool/*') / 100
         min_nvme_per_target = self.params.get("min_nvme_per_target", '/run/pool/*')
         targets_per_engine = self.server_managers[0].get_config_value("targets")
 
         # Calculate the space per engine based on the percentage to use
-        space_per_engine = self.get_max_storage_sizes()
-        scm_per_engine = int(space_per_engine[0] * total_pool_percentage)
-        nvme_per_engine = int(space_per_engine[1] * total_pool_percentage)
+        space_per_engine = self.server_managers[0].get_available_storage()
+        scm_per_engine = int(space_per_engine['scm'] * total_pool_percentage)
+        nvme_per_engine = int(space_per_engine['nvme'] * total_pool_percentage)
 
         # Calculate the potential number of pools and use up to the max from config
         potential_num_pools = int((nvme_per_engine / (min_nvme_per_target * targets_per_engine)))
@@ -52,52 +52,47 @@ class NvmeHealth(ServerFillUp):
 
         # Create the pools
         self.pool = []
-        for _pool in range(actual_num_pools):
-            self.log.info("-- Creating pool number = %s", _pool)
-            self.pool.append(self.get_pool(create=False))
-            self.pool[-1].scm_size.update(scm_per_pool, "scm_size")
-            self.pool[-1].nvme_size.update(nvme_per_pool, "nvme_size")
-            self.pool[-1].create()
+        for pool_num in range(actual_num_pools):
+            self.log.info("-- Creating pool number = %s", pool_num)
+            try:
+                self.pool.append(self.get_pool(scm_size=scm_per_pool, nvme_size=nvme_per_pool))
+            except TestFail as error:
+                if 'DER_NOSPACE' in str(error):
+                    self.log.info('-- No more storage space. Skip creating more pools.')
+                    break
+                raise
 
         # initialize the dmg command
-        self.dmg = DmgCommand(self.bin)
-        self.dmg.get_params(self)
-        self.dmg.insecure.update(
-            self.server_managers[0].get_config_value("allow_insecure"),
-            "dmg.insecure")
+        dmg = self.get_dmg_command()
 
         # List all pools
-        self.dmg.set_sub_command("storage")
-        self.dmg.sub_command_class.set_sub_command("query")
-        self.dmg.sub_command_class.sub_command_class.set_sub_command("list-pools")
         for host in self.hostlist_servers:
-            self.dmg.hostlist = host
+            dmg.hostlist = host
             try:
-                result = self.dmg.run()
+                result = dmg.storage_query_list_pools()
             except CommandFailure as error:
                 self.fail("dmg command failed: {}".format(error))
-            #Verify all pools UUID listed as part of query
+            # Verify all pools UUID listed as part of query
             for pool in self.pool:
                 if pool.uuid.lower() not in result.stdout_text:
-                    self.fail('Pool uuid {} not found in smd query'
-                              .format(pool.uuid.lower()))
+                    self.fail('Pool uuid {} not found in smd query'.format(pool.uuid.lower()))
 
         # Get the device ID from all the servers.
-        device_ids = get_device_ids(self.dmg, self.hostlist_servers)
+        device_ids = get_device_ids(dmg, self.hostlist_servers)
 
         # Get the device health
         for host, dev_list in device_ids.items():
-            self.dmg.hostlist = host
-            for _dev in dev_list:
+            dmg.hostlist = host
+            for device in dev_list:
                 try:
-                    result = self.dmg.storage_query_device_health(_dev)
+                    result = dmg.storage_query_device_health(device)
                 except CommandFailure as error:
                     self.fail("dmg get device states failed {}".format(error))
                 if 'State:NORMAL' not in result.stdout_text:
-                    self.fail("device {} on host {} is not NORMAL".format(_dev, host))
+                    self.fail("device {} on host {} is not NORMAL".format(device, host))
 
         # Get the nvme-health
         try:
-            self.dmg.storage_scan_nvme_health()
+            dmg.storage_scan_nvme_health()
         except CommandFailure as error:
             self.fail("dmg storage scan --nvme-health failed {}".format(error))
