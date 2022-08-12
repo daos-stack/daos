01ded674c7bc6b4e2149fedff4d647657b3312cc
Nasf-Fan <fan.yong@intel.com>
2022-08-13 02:55:08 +0800

    DAOS-11180 dtx: trigger DTX reindex before DTX resync (#9904)
    master-commit: 8daa893750b6ca5d4a34c8ff080471a565f0d5f6
    
    During DTX resync, the new DTX leader will send DTX_CHECK RPC
    to other alive DTX participant(s) to query related DTX status.
    If related DTX entry does not exist on the non-leader and DTX
    reindex on such non-leader is not completed yet, then the non
    leader will reply to the new leader with -DER_INPROGRESS that
    will cause the new leader to retry the DTX_CHECK RPC sometime
    later. But if DTX reindex is not triggerred on the non-leader,
    then the new leader will retry for ever. So the patch triggers
    DTX reindex before DTX resync scanning.
    
    The patch also adjusts the logic of filter DTX entries when
    DTX resync to avoid skipping some DTX entries.
    
    It also contains the fixes from PR#9938:
    
    If the DTX_CHECK RPC handler hits -DER_INPROGRESS failure on some
    target, then it means that the committed DTX entries on such target
    are not re-indexed. Under such case, we need to trigger DTX re-index
    on the target for subsequent (retry) DTX_CHECK RPC. Otherwise, it may
    cause more DTX_CHECK RPCs to be retried (for ever), as to related DTX
    resync cannot complete on some DTX leaders.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/dtx/dtx_common.c
+++ src/dtx/dtx_common.c
@@ -1460,37 +1460,28 @@ dtx_reindex_ult(void *arg)
 	struct ds_cont_child		*cont	= arg;
 	struct dss_module_info		*dmi	= dss_get_module_info();
 	uint64_t			 hint	= 0;
-	int				 rc;
+	int				 rc	= 0;
 
-	D_DEBUG(DB_ANY, DF_CONT": starting DTX reindex ULT on xstream %d\n",
-		DP_CONT(NULL, cont->sc_uuid), dmi->dmi_tgt_id);
+	D_INFO(DF_CONT": starting DTX reindex ULT on xstream %d, ver %u\n",
+	       DP_CONT(NULL, cont->sc_uuid), dmi->dmi_tgt_id, dtx_cont2ver(cont));
 
 	while (!cont->sc_dtx_reindex_abort && !dss_xstream_exiting(dmi->dmi_xstream)) {
 		rc = vos_dtx_cmt_reindex(cont->sc_hdl, &hint);
-		if (rc < 0) {
-			D_ERROR(DF_UUID": DTX reindex failed: "DF_RC"\n",
-				DP_UUID(cont->sc_uuid), DP_RC(rc));
-			goto out;
-		}
-
-		if (rc > 0) {
-			D_DEBUG(DB_ANY, DF_CONT": DTX reindex done\n",
-				DP_CONT(NULL, cont->sc_uuid));
-			goto out;
-		}
+		if (rc != 0)
+			break;
 
 		ABT_thread_yield();
 	}
 
-	D_DEBUG(DB_ANY, DF_CONT": stopping DTX reindex ULT on stream %d\n",
-		DP_CONT(NULL, cont->sc_uuid), dmi->dmi_tgt_id);
+	D_CDEBUG(rc < 0, DLOG_ERR, DLOG_INFO,
+		 DF_CONT": stopping DTX reindex ULT on stream %d, ver %u: rc = %d\n",
+		 DP_CONT(NULL, cont->sc_uuid), dmi->dmi_tgt_id, dtx_cont2ver(cont), rc);
 
-out:
 	cont->sc_dtx_reindex = 0;
 	ds_cont_child_put(cont);
 }
 
-static int
+int
 start_dtx_reindex_ult(struct ds_cont_child *cont)
 {
 	int rc;
@@ -1513,10 +1504,14 @@ start_dtx_reindex_ult(struct ds_cont_child *cont)
 	return rc;
 }
 
-static void
+void
 stop_dtx_reindex_ult(struct ds_cont_child *cont)
 {
 	if (!cont->sc_dtx_reindex || dtx_cont_opened(cont))
+		return;
+
+	/* Do not stop DTX reindex if DTX resync is still in-progress. */
+	if (cont->sc_dtx_resyncing)
 		return;
 
 	cont->sc_dtx_reindex_abort = 1;
