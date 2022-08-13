3545f1ae622e4d9c2208fdce9bbbec7477bd4458
Michael MacDonald <mjmac.macdonald@intel.com>
2022-08-13 16:33:02 +0000

    address review feedback re: positional parameters
    Required-githooks: true
    
    Signed-off-by: Michael MacDonald <mjmac.macdonald@intel.com>

--- src/tests/ftest/util/daos_utils_base.py
+++ src/tests/ftest/util/daos_utils_base.py
@@ -240,6 +240,9 @@ class DaosCommandBase(CommandWithSubCommand):
                 """Create a daos container create command object."""
                 super().__init__("create")
                 # Additional daos container create parameters:
+                #  [label]
+                #       Optional container label
+                self.label = PositionalParameter(3)
                 #   --type=<type>
                 #           container type (HDF5, POSIX)
                 self.type = FormattedParameter("--type={}")
@@ -268,7 +271,6 @@ class DaosCommandBase(CommandWithSubCommand):
                 #   --acl-file=PATH
                 #           input file containing ACL
                 self.acl_file = FormattedParameter("--acl-file={}", None)
-                self.label = FormattedParameter("{}", None)
 
         class CreateSnapSubCommand(CommonContainerSubCommand):
             """Defines an object for the daos container create-snap command."""
@@ -473,9 +475,9 @@ class DaosCommandBase(CommandWithSubCommand):
                     sub_command (str): sub-command name
                 """
                 super().__init__("/run/daos/object/{}/*".format(sub_command), sub_command)
-                self.pool = FormattedParameter("{}", None)
+                self.pool = PositionalParameter(1)
+                self.cont = PositionalParameter(2)
                 self.sys_name = FormattedParameter("--sys-name={}")
-                self.cont = FormattedParameter("{}", None)
                 self.oid = FormattedParameter("--oid={}")
 
         class QuerySubCommand(CommonObjectSubCommand):
