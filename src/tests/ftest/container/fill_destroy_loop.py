fbf356fa3167fddd268560187d93d0e21efac4ff
Ding Ho <ding-hwa.ho@intel.com>
2022-08-16 18:05:42 +0000

    Update with more test log per test_loop and add nvme test.
    Test-tag: fill_cont_pool_stress
    Test-repeat: 20
    Required-githooks: true
    
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/container/fill_destroy_loop.py
+++ src/tests/ftest/container/fill_destroy_loop.py
@@ -22,13 +22,18 @@ class BoundaryPoolContainerSpace(TestWithServers):
 
     DER_NOSPACE = "-1007"
 
-    def write_pool_until_nospace(self):
-        """write pool and container until pool is full. """
+    def write_pool_until_nospace(self, test_loop):
+        """write pool and container until pool is full.
+
+        Args:
+            test_loop (int): test loop for log info.
+        """
 
         # Create a container and get pool free space before write
         container = self.get_container(self.pool)
         free_space = self.pool.get_pool_free_space()
-        self.log.info("--pool free space before writing data to container= %s", free_space)
+        self.log.info("--%s.(3)Pool free space before writing data to container= %s",
+                      test_loop, free_space)
 
         # Write random data to container until pool out of space
         base_data_size = container.data_size.value
@@ -44,21 +49,24 @@ class BoundaryPoolContainerSpace(TestWithServers):
                 # self.log.info("%s", repr(excep))
                 if self.DER_NOSPACE in str(excep):
                     self.log.info(
-                        "--(3) DER_NOSPACE %s detected, pool is unable for an additional"
-                        " %s byte object", self.DER_NOSPACE, container.data_size.value)
+                        "--%s.(4)DER_NOSPACE %s detected, pool is unable for an additional"
+                        " %s byte object", test_loop, self.DER_NOSPACE, container.data_size.value)
                     break
-                self.fail("Exception while writing object: {}".format(repr(excep)))
+                self.fail("Test-loop {0} exception while writing object: {1}".format(
+                    test_loop, repr(excep)))
             data_written += new_data_size
 
         # display free space and data written
         free_space_before = self.pool.get_pool_free_space()
-        self.log.info("--(4)%s bytes written when pool is full.", data_written)
+        self.log.info("--%s.(5) %s bytes written when pool is full.", test_loop, data_written)
 
         # destroy container and check for free space increase
         container.destroy()
         free_space_after = self.pool.get_pool_free_space()
-        self.log.info("--(5)pooll full, free_space before container delete= %s", free_space_before)
-        self.log.info("--(6)pooll full, free_space after  container deleted= %s", free_space_after)
+        self.log.info("--%s.(6)Pool full, free_space before container delete = %s",
+                      test_loop, free_space_before)
+        self.log.info("--%s.(7)Pool full, free_space after  container deleted= %s",
+                      test_loop, free_space_after)
         if free_space_after <= free_space_before:
             self.fail("Deleting container did not free up pool space.")
 
@@ -67,17 +75,18 @@ class BoundaryPoolContainerSpace(TestWithServers):
 
         Test Description:
             Purpose of the test is to stress pool and container space usage
-            boundary, test by looping of fulfill container and destroy
+            boundary, test by looping of container object_write unitl pool full and destroy
 
         Use Case:
             repeat following steps:
             (1)Create Pool and Container.
             (2)Fill the pool with random block data size, verify return code is as expected
                when no more data can be written to the container.
-            (3)Check for DER_NOSPACE -1007.
-            (4)Display bytes written when pool is full before container delete.
-            (5)Display free space before container delete.
-            (6)Display and verify free space after container delete.
+            (3)Pool free space before writing data to container
+            (4)Check for DER_NOSPACE -1007.
+            (5)Display bytes written when pool is full before container delete.
+            (6)Display free space before container delete.
+            (7)Display and verify free space after container delete.
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,medium
@@ -94,9 +103,10 @@ class BoundaryPoolContainerSpace(TestWithServers):
             self.log.info("==>Starting test loop: %s ...", test_loop)
             self.pool.set_query_data()
             self.log.info(
-                "--(1)Pool Query before write:\n"
-                "--Pool %s query data: %s\n", self.pool.uuid, self.pool.query_data)
+                "--%s.(1)Pool Query before write:\n"
+                "--Pool %s query data: %s\n", test_loop, self.pool.uuid, self.pool.query_data)
             free_space = self.pool.get_pool_free_space()
-            self.log.info("--(2)Pool free space before write: %s", free_space)
+            self.log.info("--%s.(2)Pool free space before container create: %s",
+                test_loop, free_space)
 
-            self.write_pool_until_nospace()
+            self.write_pool_until_nospace(test_loop)
