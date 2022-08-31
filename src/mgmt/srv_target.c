521779e24b650b66097a4b828d58b5e971559958
Li Wei <wei.g.li@intel.com>
2022-08-31 14:21:00 +0800

    DAOS-11498 mgmt: Fix PS replica leaks
    For each pool, the PS ranks stored on the MS may not cover all PS
    replicas, in and out of the latest PS membership. The PS may have not
    been able to send the latest PS ranks to the MS. Or, it may have been
    unable to destroy some PS replicas after removing them from the
    membership. The ds_mgmt_destroy_pool function currently only destroys
    the PS replicas in the latest membership known to the MS, leaking any
    other PS replicas.
    
    This patch removes the step that only destroys the known PS replicas,
    combines it with the target destroy CoRPC, and attempts to stop any PS
    replicas on all PS engines. Note that the RDB files shall be deleted
    along with other pool files. And, ds_pool_svc_create is renamed to
    ds_pool_svc_dist_create to avoid confusion.
    
    We then have a few interesting directions to explore?
    
      - Try releasing local pool handles and associated data structures
        (e.g., container handles) when destroy targets for a force-destroy
        operation.
    
      - Allow force-destroy to blindly, repeatedly attempt to destroy a
        pool, because it no longer needs to know what the PS ranks are and
        may go straightly to the target destroy CoRPC without contacting
        the PS, who may have already become unavailable due to previous
        destroy attempts.
    
    Additionally, this patch fixes an unrelated assertion failure in
    ds_mgmt_group_update that has happened during its CI testing:
    
      mgmt EMRG src/mgmt/srv_util.c:33 ds_mgmt_group_update() Assertion
      'version_current < version' failed: 10 < 9
    
    Signed-off-by: Li Wei <wei.g.li@intel.com>
    Required-githooks: true

--- src/mgmt/srv_target.c
+++ src/mgmt/srv_target.c
@@ -977,6 +977,13 @@ ds_mgmt_hdlr_tgt_destroy(crt_rpc_t *td_req)
 	D_DEBUG(DB_MGMT, DF_UUID": ready to destroy targets\n",
 		DP_UUID(td_in->td_pool_uuid));
 
+	rc = ds_pool_svc_stop(td_in->td_pool_uuid);
+	if (rc != 0) {
+		D_ERROR(DF_UUID": failed to stop pool service replica: "DF_RC"\n",
+			DP_UUID(td_in->td_pool_uuid), DP_RC(rc));
+		goto out;
+	}
+
 	ds_pool_stop(td_in->td_pool_uuid);
 
 	/** generate path to the target directory */
@@ -1102,23 +1109,9 @@ int
 ds_mgmt_tgt_map_update_pre_forward(crt_rpc_t *rpc, void *arg)
 {
 	struct mgmt_tgt_map_update_in  *in = crt_req_get(rpc);
-	uint32_t			version;
-	int				rc;
 
-	rc = crt_group_version(NULL /* grp */, &version);
-	D_ASSERTF(rc == 0, "%d\n", rc);
-	D_DEBUG(DB_MGMT, "in=%u current=%u\n", in->tm_map_version, version);
-	if (in->tm_map_version <= version)
-		return 0;
-
-	rc = ds_mgmt_group_update(CRT_GROUP_MOD_OP_REPLACE,
-				  in->tm_servers.ca_arrays,
-				  in->tm_servers.ca_count, in->tm_map_version);
-	if (rc != 0)
-		return rc;
-
-	D_INFO("updated group: %u -> %u\n", version, in->tm_map_version);
-	return 0;
+	return ds_mgmt_group_update(in->tm_servers.ca_arrays, in->tm_servers.ca_count,
+				    in->tm_map_version);
 }
 
 void
