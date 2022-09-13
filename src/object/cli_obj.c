050af2844c91f14de90b806e2eb237adcd8ace79
Lei Huang <lei.huang@intel.com>
2022-09-13 18:21:12 +0000

    DAOS-11482 object: use D_GOTO for style consistency
    Skip-func-hw-test: true
    
    Required-githooks: true
    
    Signed-off-by: Lei Huang <lei.huang@intel.com>

--- src/object/cli_obj.c
+++ src/object/cli_obj.c
@@ -568,10 +568,9 @@ obj_ec_leader_select(struct dc_object *obj, int grp_idx, bool cond_modify, uint3
 	 * If no parity node is available, then handle related task that has conditional
 	 * modification via distributed tranasaction.
 	 */
-	if (cond_modify) {
-		D_RWLOCK_UNLOCK(&obj->cob_lock);
-		return -DER_NEED_TX;
-	}
+	if (cond_modify)
+		D_GOTO(unlock, rc = -DER_NEED_TX);
+
 	/* Choose one from data shards within bit_map, and also make sure there are
 	 * no further data shards failed.
 	 **/
