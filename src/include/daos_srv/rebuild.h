fb545a3ca989fa9ac8de21a9795426b64c7eb488
Di Wang <di.wang@intel.com>
2022-09-20 19:53:35 +0000

    DAOS-11380 rebuild: do not set rebuild status for reclaim job
    Add RB_OP_FAIL_RECLAIM for reclaim if there are failing rebuild
    and reintegration job, and do not set rebuild status for this
    reclaim job to avoid messing up the real rebuild status.
    
    Required-githooks: true
    Test-tags: pr no_cap
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/include/daos_srv/rebuild.h
+++ src/include/daos_srv/rebuild.h
@@ -26,6 +26,7 @@ typedef enum {
 	RB_OP_REINT,
 	RB_OP_EXTEND,
 	RB_OP_RECLAIM,
+	RB_OP_FAIL_RECLAIM,
 } daos_rebuild_opc_t;
 
 #define RB_OP_STR(rb_op) ((rb_op) == RB_OP_FAIL ? "Rebuild" : \
@@ -33,6 +34,7 @@ typedef enum {
 			  (rb_op) == RB_OP_REINT ? "Reintegrate" : \
 			  (rb_op) == RB_OP_EXTEND ? "Extend" : \
 			  (rb_op) == RB_OP_RECLAIM ? "Reclaim" : \
+			  (rb_op) == RB_OP_FAIL_RECLAIM ? "Reclaim fail" : \
 			  "Unknown")
 
 int ds_rebuild_schedule(struct ds_pool *pool, uint32_t map_ver,
--- src/rebuild/scan.c
+++ src/rebuild/scan.c
@@ -603,7 +603,8 @@ rebuild_obj_scan_cb(daos_handle_t ch, vos_iter_entry_t *ent,
 				DP_UOID(oid), DP_RC(rc));
 			D_GOTO(out, rc);
 		}
-	} else if (rpt->rt_rebuild_op == RB_OP_RECLAIM) {
+	} else if (rpt->rt_rebuild_op == RB_OP_RECLAIM ||
+		   rpt->rt_rebuild_op == RB_OP_FAIL_RECLAIM) {
 		struct pl_obj_layout *layout = NULL;
 		bool still_needed;
 		uint32_t mytarget = dss_get_module_info()->dmi_tgt_id;
@@ -673,7 +674,8 @@ rebuild_obj_scan_cb(daos_handle_t ch, vos_iter_entry_t *ent,
 			 rpt->rt_rebuild_op == RB_OP_DRAIN ||
 			 rpt->rt_rebuild_op == RB_OP_REINT ||
 			 rpt->rt_rebuild_op == RB_OP_EXTEND ||
-			 rpt->rt_rebuild_op == RB_OP_RECLAIM);
+			 rpt->rt_rebuild_op == RB_OP_RECLAIM ||
+			 rpt->rt_rebuild_op == RB_OP_FAIL_RECLAIM);
 	}
 	if (rebuild_nr <= 0) /* No need rebuild */
 		D_GOTO(out, rc = rebuild_nr);
@@ -851,7 +853,7 @@ rebuild_scanner(void *data)
 		D_GOTO(out, rc);
 	}
 
-	if (rpt->rt_rebuild_op != RB_OP_RECLAIM) {
+	if (rpt->rt_rebuild_op != RB_OP_RECLAIM && rpt->rt_rebuild_op != RB_OP_FAIL_RECLAIM) {
 		rpt_get(rpt);
 		rc = dss_ult_create(rebuild_objects_send_ult, rpt, DSS_XS_SELF,
 				    0, 0, &ult_send);
