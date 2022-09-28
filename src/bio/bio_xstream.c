b56c0a41da59a230dd68f08438266ace0971ebed
Ashley Pittman <ashley.m.pittman@intel.com>
2022-09-28 17:47:27 +0100

    Fix compile.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/bio/bio_xstream.c
+++ src/bio/bio_xstream.c
@@ -754,7 +754,7 @@ create_bio_bdev(struct bio_xs_context *ctxt, const char *bdev_name,
 	D_INIT_LIST_HEAD(&d_bdev->bb_link);
 	D_STRNDUP(d_bdev->bb_name, bdev_name, strlen(bdev_name));
 	if (d_bdev->bb_name == NULL) {
-		D_GOTO(out, rc = -DER_NOMEM);
+		D_GOTO(error, rc = -DER_NOMEM);
 	}
 
 	/*
