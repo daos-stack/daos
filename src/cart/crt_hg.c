cf74f08b74afa8bf8576df9ba9a434e759ad8513
Joseph Moore <joseph.moore@intel.com>
2022-10-05 18:20:24 +0000

    CART-89 cart: Test UCX.
    Skip-build-leap15-icc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Test-tag: pr daily_regression
    PR-repos: mercury@PR-88:2
    Test-provider: ucx+dc_x
    
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_hg.c
+++ src/cart/crt_hg.c
@@ -738,6 +738,7 @@ crt_hg_class_init(int provider, int idx, hg_class_t **ret_hg_class)
 		init_info.na_init_info.max_unexpected_size = prov_data->cpg_max_unexp_size;
 
 	hg_class = HG_Init_opt(info_string, crt_is_service(), &init_info);
+
 	if (hg_class == NULL) {
 		D_ERROR("Could not initialize HG class.\n");
 		D_GOTO(out, rc = -DER_HG);
