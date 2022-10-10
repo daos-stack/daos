ea16cbd773815172b0861e8839d98313731e017f
Liu Xuezhao <xuezhao.liu@intel.com>
2022-10-10 14:40:07 +0800

    DAOS-11605 common: basic ad tx undo/redo (#10421)
    * DAOS-11605 common: basic ad tx undo/redo
    
    - Add basic implementation for umem tx undo/redo.
    Required-githooks: true
    
    - Remove ad_tx_touch_region/ad_tx_touch_done.
    Change ad_tx_copy().
    
    - add 3 ad dx APIs -
       ad_tx_redo_act_nr/ad_tx_redo_payload_len/ad_tx_redo_act_next
    - change ac_csum to always use uint32_t csum.
    
    
    Required-githooks: true
    Signed-off-by: Xuezhao Liu <xuezhao.liu@intel.com>

--- src/include/daos_srv/ad_mem.h
+++ src/include/daos_srv/ad_mem.h
@@ -10,9 +10,15 @@ struct ad_blob;
 struct ad_tx {
 	struct ad_blob		*tx_blob;
 	uint64_t		 tx_id;
-	d_list_t		 tx_redo;
 	d_list_t		 tx_undo;
+	d_list_t		 tx_redo;
+	uint32_t		 tx_redo_act_nr;
+	uint32_t		 tx_redo_payload_len;
+	struct umem_act_item	*tx_redo_act_pos;
 };
+
+#define ad_ptr2addr(ptr)	((uintptr_t)ptr)
+#define ad_addr2ptr(addr)	((void *)addr)
 
 /** start a ad-hoc memory transaction */
 int
@@ -22,27 +28,22 @@ ad_tx_begin(struct ad_blob *blob, struct ad_tx *tx);
 int
 ad_tx_end(struct ad_tx *tx, int err);
 
+enum ad_tx_copy_flags {
+	AD_TX_UNDO	= (1 << 0),
+	AD_TX_REDO	= (1 << 1),
+	AD_TX_COPY_PTR	= (1 << 2),
+};
+
 /**
- * declare this storage region is going to be changed (for redo and undo), it does memset
- * the region if @reset is true.
- */
-int
-ad_tx_touch_region(struct ad_tx *tx, void *addr, daos_size_t size, bool reset);
-
-/** finished the change */
-int
-ad_tx_touch_done(struct ad_tx *tx);
-
-/**'
- * copy data from buffer @ptr to storage address @addr, both old and new data will be saved
+ * copy data from buffer @ptr to storage address @addr, both old and new data can be saved
  * for TX redo and undo.
  */
 int
-ad_tx_copy(struct ad_tx *tx, void *addr, daos_size_t size, void *ptr, bool ptr_only);
+ad_tx_copy(struct ad_tx *tx, void *addr, daos_size_t size, void *ptr, uint32_t flags);
 
 /** assign integer value to @addr, both old and new value should be saved for redo and undo */
 int
-ad_tx_assign(struct ad_tx *tx, void *addr, daos_size_t size, uint64_t val);
+ad_tx_assign(struct ad_tx *tx, void *addr, daos_size_t size, uint32_t val);
 
 /**
  * memset a storage region, save the operation for redo (and old value for undo if it's
@@ -52,17 +53,41 @@ int
 ad_tx_set(struct ad_tx *tx, void *addr, char c, daos_size_t size, bool save_old);
 
 /**
- * memset a storage region, save the operation for redo and old memory content for undo.
+ * memmove a storage region, save the operation for redo and old memory content for undo.
  */
 int
 ad_tx_move(struct ad_tx *tx, void *dst, void *src, daos_size_t size);
 
-/** setbit in the bitmap, save the operation for redo and the old bit for undo */
+/** setbit in the bitmap, save the operation for redo and the reversed operation for undo */
 int
-ad_tx_setbit(struct ad_tx *tx, void *bmap, int pos, int nbits);
+ad_tx_setbit(struct ad_tx *tx, void *bmap, uint32_t pos, uint16_t nbits);
 
-/** clear bit in the bitmap, save the operation for redo and  the old bit for undo */
+/** clear bit in the bitmap, save the operation for redo and the reversed operation for undo */
 int
-ad_tx_clrbit(struct ad_tx *tx, void *bmap, int pos, int nbits);
+ad_tx_clrbit(struct ad_tx *tx, void *bmap, uint32_t pos, uint16_t nbits);
+
+/**
+ * query action number in redo list.
+ */
+uint32_t
+ad_tx_redo_act_nr(struct ad_tx *tx);
+
+/**
+ * query payload length in redo list.
+ */
+uint32_t
+ad_tx_redo_payload_len(struct ad_tx *tx);
+
+/**
+ * get first action pointer, NULL for list empty.
+ */
+struct umem_action *
+ad_tx_redo_act_first(struct ad_tx *tx);
+
+/**
+ * get next action pointer, NULL for done or list empty.
+ */
+struct umem_action *
+ad_tx_redo_act_next(struct ad_tx *tx);
 
 #endif /* __DAOS_AD_HOC_MEM_H__ */
