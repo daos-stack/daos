6669c0aa10a7ceda1682c633052e3209892675f3
Joseph Moore <joseph.moore@intel.com>
2022-10-13 21:26:51 +0000

    DAOS-8714 cart: Add debug for h5_suite.
    Skip-build-leap15-icc: true
    Skip-build-el8-gcc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Skip-func-hw-test-medium: true
    Skip-func-hw-test-large: true
    Test-tag: pr daily_regression
    Test-provider: ofi+tcp
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -29,7 +29,8 @@ dump_envariables(void)
 		"OFI_PORT", "OFI_INTERFACE", "OFI_DOMAIN", "CRT_CREDIT_EP_CTX",
 		"CRT_CTX_SHARE_ADDR", "CRT_CTX_NUM", "D_FI_CONFIG",
 		"FI_UNIVERSE_SIZE", "CRT_ENABLE_MEM_PIN", "FI_LOG_LEVEL",
-		"FI_OFI_RXM_USE_SRX", "D_LOG_FLUSH", "CRT_MRC_ENABLE" };
+		"FI_OFI_RXM_USE_SRX", "D_LOG_FLUSH", "CRT_MRC_ENABLE",
+		"D_LOG_STDERR_IN_LOG" };
 
 	D_INFO("-- ENVARS: --\n");
 	for (i = 0; i < ARRAY_SIZE(envars); i++) {
@@ -118,6 +119,7 @@ static int data_init(int server, crt_init_options_t *opt)
 	D_DEBUG(DB_ALL, "initializing crt_gdata...\n");
 
 	setenv("FI_LOG_LEVEL", "warn", 1);
+	setenv("D_LOG_STDERR_IN_LOG", "1", 1);
 	dump_envariables();
 
 	/*
