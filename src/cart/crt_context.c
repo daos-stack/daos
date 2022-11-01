7761e7224c4fb9500e64845be7267745f20f8747
Alexander A Oganezov <alexander.a.oganezov@intel.com>
2022-11-01 16:14:57 +0000

    fix
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/cart/crt_context.c
+++ src/cart/crt_context.c
@@ -223,8 +223,8 @@ crt_context_provider_create(crt_context_t *crt_ctx, crt_provider_t provider, boo
 	}
 
 	D_RWLOCK_WRLOCK(&crt_gdata.cg_rwlock);
-	cur_ctx_num = crt_provider_get_cur_ctx_num(provider);
-	max_ctx_num = crt_provider_get_max_ctx_num(provider);
+	cur_ctx_num = crt_provider_get_cur_ctx_num(primary, provider);
+	max_ctx_num = crt_provider_get_max_ctx_num(primary, provider);
 
 	if (cur_ctx_num >= max_ctx_num) {
 		D_WARN("Number of active contexts (%d) reached limit (%d).\n",
