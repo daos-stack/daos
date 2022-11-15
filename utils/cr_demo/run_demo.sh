50bf712ee8464a725cf273f1d34a02503ffdd655
Makito Kano <makito.kano@intel.com>
2022-11-15 22:57:28 +0000

    DAOS-12097 test: Use eval to execute command in variable
    Add sleep to necessary places.
    Fix command typo.
    
    Skip-test: true
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- utils/cr_demo/run_demo.sh
+++ utils/cr_demo/run_demo.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-dmg_pool_list=`dmg pool list`
-dmg_check_enable=`dmg check enable`
-dmg_check_start=`dmg check start`
-dmg_check_query=`dmg check query`
-dmg_check_disable=`dmg check disable`
+dmg_pool_list="dmg pool list"
+dmg_check_enable="dmg check enable"
+dmg_check_start="dmg check start"
+dmg_check_query="dmg check query"
+dmg_check_disable="dmg check disable"
 POOL_SIZE="4G"
 POOL_LABEL="tank"
 
 format_storage() {
     hostlist=$1
-    dmg_cmd=`dmg storage format --host-list=$hostlist`
-    dmg_cmd
+    dmg_cmd="dmg storage format --host-list=$hostlist"
+    eval $dmg_cmd
 }
 
 create_pool() {
     pool_size=$1
     pool_label=$2
-    dmg_cmd=`dmg pool create --size=$pool_size --label=$pool_label`
-    dmg_cmd
+    dmg_cmd="dmg pool create --size=$pool_size --label=$pool_label"
+    eval $dmg_cmd
 }
 
 inject_fault_mgmt() {
     pool_label=$1
     fault_type=$2
-    dmg_cmd=`dmg fault mgmt-svc pool $pool_label $fault_type`
-    dmg_cmd
+    dmg_cmd="dmg faults mgmt-svc pool $pool_label $fault_type"
+    eval $dmg_cmd
 }
 
 host1=$1
@@ -36,34 +36,38 @@ echo "1. Format storage. Hit enter..."
 read
 hosts="$1,$2"
 format_storage $hosts
+# Wait for the storage to be ready before creating a pool.
+sleep 5
 
-echo "2. Create a 4GB pool. Hit enter..."
+echo $'\n2. Create a 4GB pool. Hit enter...'
 read
 create_pool $POOL_SIZE $POOL_LABEL
 
-echo "3. Remove PS entry on MS. Hit enter..."
+echo $'\n3. Remove PS entry on MS. Hit enter...'
 read
 inject_fault_mgmt $POOL_LABEL "CIC_POOL_NONEXIST_ON_MS"
 
-echo "4. MS doesn't recognize any pool (it exists on engine). Hit enter..."
+echo $'\n4. MS doesn\'t recognize any pool (it exists on engine). Hit enter...'
 read
-dmg_pool_list
+eval $dmg_pool_list
 
-echo "5. Enable and start checker. Hit enter..."
+echo $'\n5. Enable and start checker. Hit enter...'
 read
-dmg_check_enable
-dmg_check_start
+eval $dmg_check_enable
+eval $dmg_check_start
+# Wait for a few seconds for the checker to complete.
+sleep 5
 
-echo "6-1. Query the checker. Wait for a few seconds and hit enter..."
+echo $'\n6-1. Query the checker. Wait for a few seconds and hit enter...'
 read
-dmg_check_query
+eval $dmg_check_query
 
-echo "6-2. Checker shows the inconsistency that was repaired."
+echo $'\n6-2. Checker shows the inconsistency that was repaired.'
 
-echo "7. Disable the checker. Hit enter..."
+echo $'\n7. Disable the checker. Hit enter...'
 read
-dmg_check_disable
+eval $dmg_check_disable
 
-echo "8. Verify that the missing pool was reconstructed. Hit enter..."
+echo $'\n8. Verify that the missing pool was reconstructed. Hit enter...'
 read
-dmg_pool_list
+eval $dmg_pool_list
