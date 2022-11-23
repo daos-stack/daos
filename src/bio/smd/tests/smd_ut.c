36f4ccbe44afa31c7d04fa0fa2b66b6fe340aa9e
Ashley Pittman <ashley.m.pittman@intel.com>
2022-11-23 12:50:53 +0000

    Add back in protobufc
    Test-tag: test_dfuse_daos_build_wt_il
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/bio/smd/tests/smd_ut.c
+++ src/bio/smd/tests/smd_ut.c
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2018-2022 Intel Corporation.
+ * (C) Copyright 2018-2021 Intel Corporation.
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -20,7 +20,6 @@
 
 #define SMD_STORAGE_PATH	"/mnt/daos"
 #define DB_LIST_NR	3
-
 
 struct ut_db {
 	struct sys_db	ud_db;
