6d653eada00bfe749d15438c8b61fd51854ed906
Ashley Pittman <ashley.m.pittman@intel.com>
2022-11-28 17:04:42 +0000

    Back out GHA changes.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/scripts/install-el9.sh
+++ utils/scripts/install-el9.sh
@@ -10,6 +10,8 @@
 
 set -e
 
+#arch=$(uname -i)
+
 dnf --nodocs install \
     boost-python3-devel \
     bzip2 \
@@ -31,13 +33,11 @@ dnf --nodocs install \
     graphviz \
     help2man \
     hwloc-devel \
-    ipmctl \
     java-1.8.0-openjdk \
     json-c-devel \
     libaio-devel \
     libcmocka-devel \
     libevent-devel \
-    libipmctl-devel \
     libiscsi-devel \
     libtool \
     libtool-ltdl-devel \
@@ -60,3 +60,16 @@ dnf --nodocs install \
     valgrind-devel \
     which \
     yasm
+
+# No packages for the one below have been
+# identified yet. Limit build to client only for now
+#    ipmctl \
+#    libipmctl-devel \
+#    Lmod \
+#
+# ipmctl is only available on x86_64
+#if [ "$arch" = x86_64 ]; then
+#    dnf --nodocs install \
+#        ipmctl \
+#        libipmctl-devel
+#fi
