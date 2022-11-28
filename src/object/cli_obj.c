09c04f100dd080f2ed4ae4bf77369231fcf1be85
Mohamad Chaarawi <mohamad.chaarawi@intel.com>
2022-11-28 19:05:30 +0000

    fix indent
    Required-githooks: true
    
    Signed-off-by: Mohamad Chaarawi <mohamad.chaarawi@intel.com>

--- src/object/cli_obj.c
+++ src/object/cli_obj.c
@@ -6295,7 +6295,7 @@ dc_obj_key2anchor(tse_task_t *task)
 		rc = obj_ec_get_parity_or_alldata_shard(obj_auxi, map_ver, grp_idx, args->dkey,
 							&shard_cnt, NULL);
 		if (obj_ec_parity_rotate_enabled(obj))
-		    shard_cnt = obj_get_grp_size(obj);
+			shard_cnt = obj_get_grp_size(obj);
 		sub_anchors = shard_anchors_alloc(obj_auxi, shard_cnt, 1, args->dkey->iov_buf_len);
 		if (sub_anchors == NULL)
 			D_GOTO(err_obj, rc = -DER_NOMEM);
