4793ff4b1aa917e88be4951c8f9e464767951eb8
Joseph Moore <joseph.moore@intel.com>
2022-12-03 16:02:48 +0000

    DAOS-12032 mercury: Add connection mode to UCX.
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -254,7 +254,7 @@ static int data_init(int server, crt_init_options_t *opt)
 	d_getenv_char("UCX_IB_FORK_INIT", &ucx_ib_fork_init);
 	if (ucx_ib_fork_init) {
 		if (server) {
-			D_INFO("UCX_IB_FORK_INIT was set to %c, resetting to n\n", ucx_ib_fork_init);
+			D_INFO("UCX_IB_FORK_INIT was set to %c, setting to n\n", ucx_ib_fork_init);
 		} else {
 			D_INFO("UCX_IB_FORK_INIT was set to %c on client\n", ucx_ib_fork_init);
 		}
