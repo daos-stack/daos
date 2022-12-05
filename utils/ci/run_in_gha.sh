67c062e7ec9eddddf6dc72c7f0e8298a779b2044
Ashley Pittman <ashley.m.pittman@intel.com>
2022-12-05 09:15:15 +0000

    DAOS-0000 build: Add a test for rebuilding under scons.
    Verify that nothing is re-built for a second scons invocation
    and that install only installs, not builds.
    
    Required-githooks: true
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/ci/run_in_gha.sh
+++ utils/ci/run_in_gha.sh
@@ -3,21 +3,20 @@
 cd daos
 
 # Probably not needed now, but leave on PRs until we have confidence of landings builds.
-echo ::group::Rebuild spdk
+echo "::group::Rebuild spdk"
 rm -rf /opt/daos/prereq/release/spdk
 scons --jobs "$DEPS_JOBS" PREFIX=/opt/daos --build-deps=only
-echo ::endgroup::
+echo "::endgroup::"
 
 echo "::group::Stack analyzer output (post build)"
-# $SCONS --jobs "$DEPS_JOBS" PREFIX=/opt/daos --analyze-stack="-x tests -c 128" server
 scons --jobs "$DEPS_JOBS" --analyze-stack="-x tests -c 128" server
-echo ::endgroup::
+echo "::endgroup::"
 
 echo "::group::Stack analyzer output (immediate)"
 scons -Q --jobs "$DEPS_JOBS" --analyze-stack="-x tests -c 128 -e" server
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Test client only debug build
+echo "::group::Test client only debug build"
 scons --jobs "$DEPS_JOBS" PREFIX=/opt/daos COMPILER="$COMPILER" BUILD_TYPE=debug \
        TARGET_TYPE=release -c install
 utils/ci/gha-file-check.sh -n /opt/daos/bin/dmg
@@ -25,61 +24,91 @@ scons --jobs "$DEPS_JOBS" client install
 utils/ci/gha-file-check.sh -n /opt/daos/bin/daos_engine
 utils/ci/gha-file-check.sh -n /opt/daos/bin/vos_tests
 utils/ci/gha-file-check.sh /opt/daos/bin/dmg
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Test server only debug build
+echo "::group::Test server only debug build"
 scons --jobs "$DEPS_JOBS" -c install
 utils/ci/gha-file-check.sh -n /opt/daos/bin/daos_engine
 scons --jobs "$DEPS_JOBS" server install
 utils/ci/gha-file-check.sh /opt/daos/bin/daos_engine
 utils/ci/gha-file-check.sh -n /opt/daos/bin/vos_tests
 utils/ci/gha-file-check.sh -n /opt/daos/bin/dmg
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Test incremental debug build with test target
+echo "::group::Test incremental debug build with test target"
 scons --jobs "$DEPS_JOBS" test client
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Config file
+echo "::group::Config file"
 cat daos.conf
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Install debug
+echo "::group::Install debug"
 scons install
 utils/ci/gha-file-check.sh /opt/daos/bin/daos_engine
 utils/ci/gha-file-check.sh /opt/daos/bin/vos_tests
 utils/ci/gha-file-check.sh /opt/daos/bin/dmg
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Rebuild ofi in alternative location
+echo "::group::Clean all"
+scons -c
+scons -c install
+echo "::endgroup::"
+
+echo "::group::Build all"
+scons
+echo "::endgroup::"
+
+echo "::group::Re-build all"
+scons --debug=explain
+echo "::endgroup::"
+
+echo "::group::Re-re-build all"
+scons --debug=explain
+echo "::endgroup::"
+
+echo "::group::install all"
+scons install
+echo "::endgroup::"
+
+echo "::group::Re-install all"
+scons install --debug=explain
+echo "::endgroup::"
+
+echo "::group::Rebuild ofi in alternative location"
 rm -rf /opt/daos/prereq/release/{ofi,mercury} build/external/release/{ofi,mercury*}
 scons PREFIX=/opt/daos/dep TARGET_TYPE=release --build-deps=only DEPS=ofi --jobs \
       "$DEPS_JOBS"
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Rebuild mercury and daos with ofi from ALT_PREFIX
+echo "::group::Rebuild mercury and daos with ofi from ALT_PREFIX"
 scons install ALT_PREFIX=/opt/daos/dep/prereq/release/ofi PREFIX=/opt/daos --build-deps=yes \
       DEPS=all BUILD_TYPE=dev --jobs "$DEPS_JOBS"
 utils/ci/gha-file-check.sh /opt/daos/bin/daos_engine
 utils/ci/gha-file-check.sh /opt/daos/bin/vos_tests
 utils/ci/gha-file-check.sh /opt/daos/bin/dmg
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Config file after ALT_PREFIX build
+echo "::group::Config file after ALT_PREFIX build"
 cat daos.conf
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Install pydaos
+echo "::group::Install pydaos"
 cd src/client
 python3 setup.py install
 cd -
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Setting up daos_server_helper
+echo "::Cleaning tree::"
+rm -rf install
+git status --ignored=matching
+echo "::endgroup::"
+
+echo "::group::Setting up daos_server_helper"
 . utils/sl/setup_local.sh
 ./utils/setup_daos_server_helper.sh
-echo ::endgroup::
+echo "::endgroup::"
 
-echo ::group::Container copy test
+echo "::group::Container copy test"
 ./utils/node_local_test.py --no-root --memcheck no --test cont_copy
-echo ::endgroup::
+echo "::endgroup::"
