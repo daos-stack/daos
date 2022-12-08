cc2a371b5ac39ef006cfd8abeb870406b7c1d489
Mark Olson <115657904+mark-olson@users.noreply.github.com>
2022-12-08 07:16:05 -0800

    DAOSGCP-143 Fix issue with git command (#68)
    Fixed issue that causes a git command error When running
    the tools/autodoc/cloudshell_urls.sh script
    from the root of the repo.
    
    Signed-off-by: Mark Olson <115657904+mark-olson@users.noreply.github.com>
    Signed-off-by: Mark A. Olson <115657904+mark-olson@users.noreply.github.com>

--- tools/autodoc/cloudshell_urls.sh
+++ tools/autodoc/cloudshell_urls.sh
@@ -30,9 +30,28 @@ trap 'echo "Unexpected and unchecked error. Exiting."' ERR
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 SCRIPT_FILENAME=$(basename "${BASH_SOURCE[0]}")
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-# shellcheck disable=SC2046
-CURRENT_REMOTE_URL=$(git remote get-url $(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)|cut -d/ -f1) | sed 's|git@github.com:|https://github.com/|g' | sed 's|\.git||g')
+# BEGIN: Logging variables and functions
+declare -A LOG_LEVELS=([DEBUG]=0 [INFO]=1  [WARN]=2 [ERROR]=3 [FATAL]=4 [OFF]=5)
+declare -A LOG_COLORS=([DEBUG]=2 [INFO]=12 [WARN]=3 [ERROR]=1 [FATAL]=9 [OFF]=0 [OTHER]=15)
+LOG_LEVEL=INFO
+
+log() {
+  local msg="$1"
+  local lvl=${2:-INFO}
+  if [[ ${LOG_LEVELS[$LOG_LEVEL]} -le ${LOG_LEVELS[$lvl]} ]]; then
+    if [[ -t 1 ]]; then tput setaf "${LOG_COLORS[$lvl]}"; fi
+    printf "[%-5s] %s\n" "$lvl" "${msg}" 1>&2
+    if [[ -t 1 ]]; then tput sgr0; fi
+  fi
+}
+
+log.debug() { log "${1}" "DEBUG" ; }
+log.info()  { log "${1}" "INFO"  ; }
+log.warn()  { log "${1}" "WARN"  ; }
+log.error() { log "${1}" "ERROR" ; }
+log.fatal() { log "${1}" "FATAL" ; }
+# END: Logging variables and functions
+
 
 show_help() {
    cat <<EOF
@@ -57,11 +76,11 @@ Examples:
 
   Set "Open in Google Cloud Shell" links before merging to main
 
-    ${SCRIPT_FILENAME} --branch main --repo-url https://github.com/daos-stack/google-cloud-daos
+    ${SCRIPT_DIR}/${SCRIPT_FILENAME} --branch main --repo-url https://github.com/daos-stack/google-cloud-daos
 
   Set "Open in Google Cloud Shell" links when submitting a PR to the develop branch
 
-    ${SCRIPT_FILENAME} --branch develop --repo-url https://github.com/daos-stack/google-cloud-daos
+    ${SCRIPT_DIR}/${SCRIPT_FILENAME} --branch develop --repo-url https://github.com/daos-stack/google-cloud-daos
 
 EOF
 }
@@ -99,14 +118,15 @@ opts() {
       ;;
     esac
   done
-  set -e
+  set -eo pipefail
 
-  if [[ "${BRANCH}" = "" ]] || [[ -z ${BRANCH} ]]; then
-    BRANCH="${CURRENT_BRANCH}"
+  if [[ "${BRANCH}" == "" ]] || [[ -z ${BRANCH} ]]; then
+    BRANCH="$(git rev-parse --abbrev-ref HEAD)"
   fi
 
-  if [[ "${REMOTE_URL}" = "" ]] || [[ -z ${REMOTE_URL} ]]; then
-    REMOTE_URL="${CURRENT_REMOTE_URL}"
+  if [[ "${REMOTE_URL}" == "" ]] || [[ -z ${REMOTE_URL} ]]; then
+    # shellcheck disable=SC2046
+    REMOTE_URL=$(git config --get remote.origin.url | sed 's|git@github.com:|https://github.com/|g' | sed 's|\.git||g')
   fi
 }
 
@@ -115,9 +135,11 @@ update_cloud_shell_urls() {
                    --exclude "development.md" \
                    "https://ssh.cloud.google.com/cloudshell/open" \
                    "${REPO_DIR}"); do
-    echo "Updating cloud shell URLs in file: ${mdf}"
-    sed -r -i "s|git_repo=[^\&]*\&|git_repo=${REMOTE_URL}\&|g" "${mdf}"
+    log.info "Updating cloud shell URLs in ${mdf}"
+    sed -r -i "s|git_repo=[^\&]*\&|git_repo=${REMOTE_URL}.git\&|g" "${mdf}"
     sed -r -i "s|cloudshell_git_branch=[^\&]*\&|cloudshell_git_branch=${BRANCH}\&|g" "${mdf}"
+    grep -o -E 'cloudshell_git_repo=.*cloudshell_git_branch=.*' "${mdf}" --color=auto
+    echo
   done
   echo
 }
@@ -127,15 +149,18 @@ update_git_urls() {
                    --exclude "development.md" \
                    "git clone" \
                    "${REPO_DIR}"); do
-    echo "Updating git clone URLs in file: ${mdf}"
-     sed -r -i "s|^(.*git clone) https://.*/google-cloud-daos.git|\1 ${REMOTE_URL}.git|g" "${mdf}"
+    log.info "Updating git clone URLs in ${mdf}"
+     sed -r -i "s|^(.*git clone) .*google-cloud-daos.*|\1 ${REMOTE_URL}.git|g" "${mdf}"
+    grep -o -E 'git clone.*' "${mdf}" --color=auto
+    echo
   done
   echo
 }
 
 main() {
   opts "$@"
-  echo "Updating 'Open in Google Cloud Shell' links in *.md files
+  log.info "Updating 'Open in Google Cloud Shell' links in *.md files
+
     git_repo=${REMOTE_URL}
     cloudshell_git_branch=${BRANCH}
   "
