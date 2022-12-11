084ae60ab3997018678d78b9d01a5e3f45fc5424
Makito Kano <makito.kano@intel.com>
2022-12-11 04:56:21 +0000

    DAOS-12097 test: Use dictionary to store rank to IP mapping
    Improve output readability.
    
    Skip-test: true
    Skip-unit-tests: true
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- utils/cr_demo/run_demo.py
+++ utils/cr_demo/run_demo.py
@@ -19,6 +19,10 @@ POOL_SIZE_1GB = "1GB"
 POOL_SIZE_5GB = "5GB"
 POOL_LABEL = "tank"
 CONT_LABEL = "bucket"
+# Rank to create F5 pool.
+F5_RANK_ORIG = 1
+# Rank to copy the F5 pool dir to.
+F5_RANK_FAULT = 3
 
 print("Test all CR features")
 
@@ -28,7 +32,6 @@ ARGS = vars(PARSER.parse_args())
 
 HOSTLIST = ARGS["hostlist"]
 
-input(f"Prepare environment on {HOSTLIST}. Hit enter...")
 print(f"\n1. Format storage on {HOSTLIST}.")
 format_storage(host_list=HOSTLIST)
 
@@ -36,24 +39,12 @@ print("\nWait for 10 sec for format...")
 time.sleep(10)
 
 # Call dmg system query to obtain the IP address of necessary ranks.
-rank_0_ip = None
-rank_1_ip = None
-rank_2_ip = None
-rank_3_ip = None
+rank_to_ip = {}
 stdout = system_query(json=True)
 print("## dmg system query stdout = {}".format(stdout))
 generated_yaml = yaml.safe_load(stdout)
 for member in generated_yaml["response"]["members"]:
-    if member["rank"] == 0:
-        rank_0_ip = member["addr"].split(":")[0]
-    elif member["rank"] == 1:
-        rank_1_ip = member["addr"].split(":")[0]
-    elif member["rank"] == 2:
-        rank_2_ip = member["addr"].split(":")[0]
-    elif member["rank"] == 3:
-        rank_3_ip = member["addr"].split(":")[0]
-
-# print("## Rank 0 IP = {}; Rank 2 IP = {}".format(rank_0_ip, rank_2_ip))
+    rank_to_ip[member["rank"]] = member["addr"].split(":")[0]
 
 # Add input here to make sure all ranks are joined before starting the script.
 input(f"\n2. Create 7 pools and containers. Hit enter...")
@@ -74,25 +65,29 @@ create_pool(pool_size=POOL_SIZE_1GB, pool_label=pool_label_2, nsvc="3")
 # F4. CIC_POOL_BAD_LABEL - inconsistent pool label between MS and PS
 create_pool(pool_size=POOL_SIZE_1GB, pool_label=pool_label_4)
 # F5. CIC_ENGINE_NONEXIST_IN_MAP - orphan pool shard
-create_pool(pool_size=POOL_SIZE_1GB, pool_label=pool_label_5, ranks="1")
+create_pool(pool_size=POOL_SIZE_1GB, pool_label=pool_label_5, ranks=str(F5_RANK_ORIG))
 # F6. CIC_ENGINE_HAS_NO_STORAGE - dangling pool map
 create_pool(pool_size=POOL_SIZE_1GB, pool_label=pool_label_6, ranks="0,1")
 # F7. CIC_CONT_NONEXIST_ON_PS - orphan container
 create_pool(pool_size=POOL_SIZE_1GB, pool_label=pool_label_7)
 create_container(pool_label=pool_label_7, cont_label=cont_label_7)
+print()
 # F8. CIC_CONT_BAD_LABEL
 create_pool(pool_size=POOL_SIZE_1GB, pool_label=pool_label_8)
 create_container(pool_label=pool_label_8, cont_label=cont_label_8)
 
-print(f"\n3-F5. Print storage usage to show that original usage of {pool_label_5}.")
-# We'll copy the pool dir from rank 1 to 3.
-f5_host_list = "{},{}".format(rank_1_ip, rank_3_ip)
+print(f"\n3-F5. Print storage usage to show original usage of {pool_label_5}. "
+      f"Pool is created on {rank_to_ip[F5_RANK_ORIG]}.")
+# F5 pool is created on rank 1, but we'll copy the pool dir from rank 1 to 3, so show
+# both.
+f5_host_list = "{},{}".format(rank_to_ip[F5_RANK_ORIG], rank_to_ip[F5_RANK_FAULT])
 storage_query_usage(host_list=f5_host_list)
 
 ####################################################################
 print("\n4. Inject fault with dmg (except F5, F6).")
 # F1
 inject_fault_pool(pool_label=pool_label_1, fault_type="CIC_POOL_NONEXIST_ON_ENGINE")
+
 # F4
 inject_fault_mgmt(pool_label=pool_label_4, fault_type="CIC_POOL_BAD_LABEL")
 
@@ -122,7 +117,7 @@ input("\n5-1. Stop servers to manipulate for F2, F5, F6, F7. Hit enter...")
 system_stop()
 
 # F2: Destroy tank_2 rdb-pool on rank 0 and 2.
-rank_0_2_ips = "{},{}".format(rank_0_ip, rank_2_ip)
+rank_0_2_ips = "{},{}".format(rank_to_ip[0], rank_to_ip[2])
 rm_cmd = f"sudo rm /mnt/daos/{label_to_uuid[pool_label_2]}/rdb-pool"
 remove_rdb_rank_0_2 = ["clush", "-w", rank_0_2_ips, rm_cmd]
 print("## Command to remove rdb-pool = {}".format(remove_rdb_rank_0_2))
@@ -142,28 +137,28 @@ subprocess.run(remove_rdb_rank_0_2, check=False)
 pool_uuid_5 = label_to_uuid[pool_label_5]
 chmod_cmd = f"sudo chmod 777 /mnt/daos; sudo chmod -R 777 /mnt/daos/{pool_uuid_5}"
 print("## chmod command = {}".format(chmod_cmd))
-clush_chmod_cmd = ["clush", "-w", rank_1_ip, chmod_cmd]
+clush_chmod_cmd = ["clush", "-w", rank_to_ip[1], chmod_cmd]
 print("## clush chmod command = {}".format(clush_chmod_cmd))
 subprocess.run(clush_chmod_cmd, check=False)
 
 # sudo scp -rp /mnt/daos/<pool_uuid_5> root@<rank_3>:/mnt/daos/
 scp_cmd = (f"scp -rp /mnt/daos/{pool_uuid_5} "
-           f"root@{rank_3_ip}:/mnt/daos/")
+           f"root@{rank_to_ip[3]}:/mnt/daos/")
 print("## scp command = {}".format(scp_cmd))
-copy_pool_dir = ["clush", "-w", rank_1_ip, scp_cmd]
+copy_pool_dir = ["clush", "-w", rank_to_ip[1], scp_cmd]
 print("## Command to copy tank_5 from 1 to 3 = {}".format(copy_pool_dir))
 subprocess.run(copy_pool_dir, check=False)
 
 # Set owner for the copied dir and files to daos_server:daos_server.
 chown_cmd = f"sudo chown -R daos_server:daos_server /mnt/daos/{pool_uuid_5}"
-clush_chown_cmd = ["clush", "-w", rank_3_ip, chown_cmd]
+clush_chown_cmd = ["clush", "-w", rank_to_ip[3], chown_cmd]
 print("## clush chown command = {}".format(clush_chown_cmd))
 subprocess.run(clush_chown_cmd, check=False)
 
 # F6: Remove pool directory from rank 0.
 pool_uuid_6 = label_to_uuid[pool_label_6]
 rm_cmd = f"sudo rm -rf /mnt/daos/{pool_uuid_6}"
-clush_rm_cmd = ["clush", "-w", rank_0_ip, rm_cmd]
+clush_rm_cmd = ["clush", "-w", rank_to_ip[0], rm_cmd]
 print("## clush rm command = {}".format(clush_rm_cmd))
 subprocess.run(clush_rm_cmd, check=False)
 
@@ -171,15 +166,12 @@ subprocess.run(clush_rm_cmd, check=False)
 pool_uuid_7 = label_to_uuid[pool_label_7]
 # ddb -R "ls" /mnt/daos/<pool_uuid_7>/vos-0
 ddb_cmd = f"sudo ddb -R \"ls\" /mnt/daos/{pool_uuid_7}/vos-0"
-clush_ddb_cmd = ["clush", "-w", rank_0_ip, ddb_cmd]
+clush_ddb_cmd = ["clush", "-w", rank_to_ip[0], ddb_cmd]
 print("## clush ddb command = {}".format(clush_ddb_cmd))
 subprocess.run(clush_ddb_cmd, check=False)
 
 print("\n5-2. Restart servers.")
 system_start()
-
-print("## Show system query 1")
-system_query(verbose=True)
 
 ####################################################################
 input("\n6. Show the faults inserted for each pool/container except "
@@ -197,8 +189,9 @@ print("\n6-F4-2. Labels in PS are still original.")
 pool_label_4_fault = pool_label_4 + "-fault"
 pool_get_prop(pool_label=pool_label_4_fault, properties="label")
 
-# F5: Call dmg storage query usage to see that the pool is using more space.
-print(f"\n6-F5. Print storage usage to show that the {pool_label_5} is using more space.")
+# F5: Call dmg storage query usage to show that the pool is using more space.
+print(f"\n6-F5. Print storage usage to show that {pool_label_5} is using more space. "
+      f"Pool directory is copied to {rank_to_ip[F5_RANK_FAULT]}.")
 storage_query_usage(host_list=f5_host_list)
 
 # F8: Show inconsistency by getting the container label.
@@ -206,19 +199,22 @@ print("\n6-F8. Show container label inconsistency.")
 # daos container get-prop tank_8 bucket_8
 # It should show error because the label has been changed.
 cont_get_prop(pool_label=pool_label_8, cont_label=cont_label_8)
+
+print()
 # daos container get-prop --properties=label tank_8 new-label
 # It should show bucket_8
 cont_get_prop(pool_label=pool_label_8, cont_label="new-label", properties="label")
 
-print("## Show system query 2")
-system_query(verbose=True)
-
+####################################################################
 input("\n7. Enable checker. Hit enter...")
 check_enable()
 
 input("\n8. Start checker with interactive mode. Hit enter...")
 check_set_policy(all_interactive=True)
+print()
 check_start()
+print()
+repeat_check_query()
 
 # input("\n8. Select suggested repair option for all faults. Hit enter...")
 # check_repair(...)
