88911ca571947f51e774502b2edccab6e15d3df5
Maureen Jean <maureen.jean@intel.com>
2022-12-19 16:49:34 -0500

    Fix core file
    Quick-Functional: true
    Test-tag: pr test_core_files test_always_passes
    
    Required-githooks: true
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/harness/advanced.py
+++ src/tests/ftest/harness/advanced.py
@@ -52,11 +52,13 @@ class HarnessAdvancedTest(TestWithServers):
             self.fail("Unable to find local core file pattern")
         core_path = os.path.split(results.stdout.splitlines()[-1])[0]
         core_file = "{}/core.gdb.harness.advanced".format(core_path)
-        command = ["touch {}".format(core_file), "echo \"Test core file\" > {}".format(core_file)]
+        command = "touch {}".format(core_file)
+        command2 = "echo \"Test core file\" > {}".format(core_file)
         try:
             run_local(self.log, ";".join(command), check=True)
+            run_local(self.log, ";".join(command2), check=True)
         except RunException:
-            self.fail("Failed to create core.gdb file in %s", )
+            self.fail("Failed to create core.gdb.harness.advanced in %s", core_path)
 
         # Choose a server find the pid of its daos_engine process
         host = NodeSet(choice(self.server_managers[0].hosts))   # nosec
