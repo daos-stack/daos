4bb789d156adbf15d779c7e91f7d06fa1e8a485c
Makito Kano <makito.kano@intel.com>
2022-12-21 05:12:14 +0000

    DAOS-12097 test: Split steps to show issues fixed to show_fixed.py
    Skip-test: true
    Skip-unit-tests: true
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- utils/cr_demo/run_demo.py
+++ utils/cr_demo/run_demo.py
@@ -11,7 +11,7 @@ from demo_utils import format_storage, inject_fault_mgmt, list_pool, check_enabl
     check_start, check_disable, repeat_check_query, check_repair, create_uuid_to_seqnum,\
     pool_get_prop, create_pool, inject_fault_pool, create_container, inject_fault_daos,\
     system_stop, system_query, storage_query_usage, cont_get_prop, system_start,\
-    check_set_policy, pool_query
+    check_set_policy
 
 # Need to use at least "scm_size: 15" for server config to create 8 1GB-pools.
 POOL_SIZE_1GB = "1GB"
@@ -283,48 +283,4 @@ repeat_check_query()
 print("\n9. Disable the checker.")
 check_disable()
 
-####################################################################
-input("\n10. Show the issues fixed. Hit enter...")
-print(f"10-F1. Dangling pool ({POOL_LABEL_1}) was removed.")
-print(f"10-F3. Orphan pool ({POOL_LABEL_3}) was reconstructed.")
-list_pool()
-
-print(f"10-F2. Create a container on {POOL_LABEL_2}. Pool can be started now, so it "
-      f"should succeed.")
-CONT_LABEL_2 = CONT_LABEL + "_2"
-create_container(pool_label=POOL_LABEL_2, cont_label=CONT_LABEL_2)
-# (optional) Show that rdb-pool file in rank 0 and 2 are recovered.
-
-print(f"\n10-F4. Label inconsistency for {POOL_LABEL_4} was resolved. "
-      f"See pool list above.")
-pool_get_prop(pool_label=POOL_LABEL_4, properties="label")
-
-# F5: Call dmg storage query usage to verify the storage was reclaimed. - Not working due
-# to a bug. Instead, show that pool directory on dst node (rank 3 for 4-VM) was removed.
-print(f"\n10-F5-1. Print storage usage to show that storage used by {POOL_LABEL_5} is "
-      f"reclaimed after pool directory is removed from {rank_to_ip[F5_RANK_FAULT]}.")
-storage_query_usage(host_list=f5_host_list)
-
-print(f"\n10-F5-2. {label_to_uuid[POOL_LABEL_5]} pool directory on rank 3 "
-      f"({rank_to_ip[3]}) was removed.")
-LS_CMD = "sudo ls /mnt/daos"
-clush_ls_cmd = ["clush", "-w", rank_to_ip[3], LS_CMD]
-print(f"Command: {clush_ls_cmd}\n")
-subprocess.run(clush_ls_cmd, check=False)
-
-print(f"\n10-F6. {POOL_LABEL_6} has one less ranks (4 -> 3).")
-pool_query(pool_label=POOL_LABEL_6)
-# (optional) Reintegrate rank 1 on pool 6. Wait for rebuild to finish. Then verify the
-# target count.
-
-# F8: Verify that the inconsistency is fixed. The label is back to the original.
-print(f"\n10-F8. Container label inconsistency for {CONT_LABEL_8} was fixed.")
-cont_get_prop(pool_label=POOL_LABEL_8, cont_label=CONT_LABEL_8, properties="label")
-
-# F7: Stop server. Call the same ddb command to verify that the container is removed from
-# shard.
-print(f"\n10-F7. Use ddb to verify that the container in {POOL_LABEL_8} is removed "
-      f"from shards.")
-system_stop()
-print(f"Command: {clush_ddb_cmd}")
-subprocess.run(clush_ddb_cmd, check=False)
+print("\nRun show_fixed.py to show the issues fixed...")
