a77807480b018771022273d5a1c76ed96213dbf9
Ashley Pittman <ashley.m.pittman@intel.com>
2023-01-05 14:41:40 +0000

    Remove wrapper function.
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- site_scons/site_tools/go_builder.py
+++ site_scons/site_tools/go_builder.py
@@ -49,7 +49,7 @@ def _scan_go_file(node, env, _path):
     return includes
 
 
-def _setup_go(env):
+def generate(env):
     """Setup the go compiler"""
 
     def _check_go_version(context):
@@ -95,11 +95,6 @@ def _setup_go(env):
         env['ENV']['GOCACHE'] = os.environ['GOCACHE']
 
     env.Append(SCANNERS=Scanner(function=_scan_go_file, skeys=['.go']))
-
-
-def generate(env):
-    """Add daos specific methods to environment"""
-    _setup_go(env)
 
 
 def exists(_env):
