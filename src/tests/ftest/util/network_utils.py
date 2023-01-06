42bb42fd5843f519525f444f2457eda5ab4e5989
Joseph Moore <joseph.moore@intel.com>
2023-01-06 20:11:16 +0000

    DAOS-12221 test: Fix dmg stop.
    Skip-build-leap15-icc: true
    Skip-build-el8-gcc: true
    Skip-build-ubuntu20-rpm: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Test-tag: pr daily_regression
    Test-provider: ucx+tcp
    
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/tests/ftest/util/network_utils.py
+++ src/tests/ftest/util/network_utils.py
@@ -14,7 +14,7 @@ from ClusterShell.NodeSet import NodeSet
 from exception_utils import CommandFailure
 from general_utils import run_task, display_task, run_pcmd
 
-SUPPORTED_PROVIDERS = ("ofi+sockets", "ofi+tcp;ofi_rxm", "ofi+verbs;ofi_rxm", "ucx+dc_x", "ucx+tcp", "ofi+cxi")
+SUPPORTED_PROVIDERS = ("ofi+sockets", "ofi+tcp;ofi_rxm", "ofi+verbs;ofi_rxm", "ucx+tcp", "ofi+cxi")
 
 
 class NetworkDevice():
