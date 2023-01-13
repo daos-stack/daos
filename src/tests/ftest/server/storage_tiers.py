bfab3022d2576fdc86c50efc2dc6dec2fe01e387
Phil Henderson <phillip.henderson@intel.com>
2023-01-13 18:04:38 -0500

    Move /storage_tiers.py back to Medium.
    Test-tag: test_ior_intercept test_ior_small mdtest_small test_tiers
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/server/storage_tiers.py
+++ src/tests/ftest/server/storage_tiers.py
@@ -1,5 +1,5 @@
 """
-  (C) Copyright 2020-2022 Intel Corporation.
+  (C) Copyright 2020-2023 Intel Corporation.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
@@ -27,7 +27,7 @@ class StorageTiers(TestWithServers):
             Verify storage tiers are correctly obtained from the test yaml file.
 
         :avocado: tags=all,pr
-        :avocado: tags=vm
+        :avocado: tags=hw,medium
         :avocado: tags=server,storage,storage_tiers
         :avocado: tags=StorageTiers,test_tiers
         """
