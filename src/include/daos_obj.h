02e66f5dfe867e72073aecd3529fde22d4f50645
wangdi <di.wang@intel.com>
2023-01-17 10:55:59 -0800

    DAOS-10681 object: misc fixes for layout upgrade and rebuild (#11129)
    Add object layout upgrade path for this layout fix.
    
    Add dst_reclaim_ver to avoid deleting wrong OID once
    rebuild failes.
    
    Remove unnecessary assert once we allow put multiple objects in
    the same target, i.e. any objects can be rebuilt.
    
    Add boundry in merge_recx() to avoid the merge the recxs from
    different stripes to mess up the rebuild.
    
    Add mpt_init_cond to initialize the mpt tls asynchronously to avoid
    mpt is initialized multiple times.
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/include/daos_obj.h
+++ src/include/daos_obj.h
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2015-2022 Intel Corporation.
+ * (C) Copyright 2015-2023 Intel Corporation.
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -477,13 +477,19 @@ typedef struct {
 	uint32_t	kd_val_type;
 } daos_key_desc_t;
 
+static uint32_t
+daos_obj_id2ord(daos_obj_id_t oid)
+{
+	return (oid.hi & OID_FMT_CLASS_MASK) >> OID_FMT_CLASS_SHIFT;
+}
+
 static inline daos_oclass_id_t
 daos_obj_id2class(daos_obj_id_t oid)
 {
 	enum daos_obj_redun ord;
 	uint32_t nr_grps;
 
-	ord = (enum daos_obj_redun)((oid.hi & OID_FMT_CLASS_MASK) >> OID_FMT_CLASS_SHIFT);
+	ord = daos_obj_id2ord(oid);
 	nr_grps = (oid.hi & OID_FMT_META_MASK) >> OID_FMT_META_SHIFT;
 
 	return (ord << OC_REDUN_SHIFT) | nr_grps;
