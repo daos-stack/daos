5c8ac9234c21e7675de1d73dcff5e36853876130
Jeff Olivier <jeffrey.v.olivier@intel.com>
2023-01-19 13:15:45 -0700

    DAOS-623 ci: Allow user to set alternative origin for github
    Some users may have the remote set to a different remote than
    origin.   Allow users to set DAOS_ORIGIN to accomodate this.
    
    Required-githooks: true
    
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- utils/githooks/find_base.sh
+++ utils/githooks/find_base.sh
@@ -5,18 +5,25 @@ if ! BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null); then
     exit 1
 fi
 
+ORIGIN="origin"
+if [ -n "$DAOS_ORIGIN" ]; then
+    ORIGIN="$DAOS_ORIGIN"
+else
+    echo "Using origin as remote repo.  If this is incorrect, set DAOS_ORIGIN in environment"
+fi
+
 # Try and use the gh command to work out the target branch, or if not installed
 # then assume origin/master.
 if command -v gh > /dev/null 2>&1; then
     # If there is no PR created yet then do not check anything.
-    if ! TARGET=origin/$(gh pr view "$BRANCH" --json baseRefName -t "{{.baseRefName}}"); then
+    if ! TARGET="$ORIGIN"/$(gh pr view "$BRANCH" --json baseRefName -t "{{.baseRefName}}"); then
         TARGET=HEAD
     fi
 else
     # With no 'gh' command installed then check against origin/master.
     echo "  Install gh command to auto-detect target branch, assuming origin/master."
     # shellcheck disable=SC2034
-    TARGET=origin/master
+    TARGET="$ORIGIN"/master
 fi
 
 # get the actual commit in $TARGET that is our base, if we are working on a commit in the history
