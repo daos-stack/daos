662d6527abca2a38b2e88e0186f3a8fbe7021313
Tom Nabarro <tom.nabarro@intel.com>
2023-01-30 14:53:36 +0000

    allow DAOS_MD_ON_SSD envar to override allocater used in umem
    Required-githooks: true
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/common/mem.c
+++ src/common/mem.c
@@ -38,6 +38,7 @@ enum {
 	DAOS_MD_PMEM	= 0,
 	DAOS_MD_BMEM	= 1,
 	DAOS_MD_ADMEM	= 2,
+	DAOS_MD_INVAL	= 4,
 };
 
 static int daos_md_backend = DAOS_MD_PMEM;
@@ -53,9 +54,19 @@ umempobj_settings_init(bool md_on_ssd)
 {
 	int					rc;
 	enum pobj_arenas_assignment_type	atype;
-	unsigned int				md_mode = DAOS_MD_BMEM;
+	unsigned int				md_mode = DAOS_MD_INVAL;
 
-	if (!md_on_ssd) {
+	d_getenv_int("DAOS_MD_ON_SSD", &md_mode);
+
+	if (md_on_ssd && md_mode == DAOS_MD_PMEM) {
+		D_ERROR("MD on SSD mode detected but DAOS_MD_ON_SSD set to MD_PMEM (0)");
+	}
+
+	/* Use input argument (mode detected) if not set explicitly by env var */
+	md_mode = (md_mode == DAOS_MD_INVAL) ? (int)md_on_ssd : md_mode;
+
+	switch (md_mode) {
+	case DAOS_MD_PMEM:
 		daos_md_backend = DAOS_MD_PMEM;
 		atype = POBJ_ARENAS_ASSIGNMENT_GLOBAL;
 
@@ -64,22 +75,16 @@ umempobj_settings_init(bool md_on_ssd)
 			D_ERROR("Could not configure PMDK for global arena: %s\n",
 				strerror(errno));
 		return rc;
-	}
-
-	d_getenv_int("DAOS_MD_ON_SSD_MODE", &md_mode);
-	if (md_mode > DAOS_MD_ADMEM) {
+	case DAOS_MD_BMEM:
+		D_INFO("UMEM will use Blob Backed Memory as the metadata backend interface\n");
+	case DAOS_MD_ADMEM:
+		D_INFO("UMEM will use AD-hoc Memory as the metadata backend interface\n");
+	default:
 		D_ERROR("invalid MD mode value %d\n", md_mode);
 		return -DER_INVAL;
-	}
+	};
 
-	if (md_mode == DAOS_MD_ADMEM) {
-		daos_md_backend = DAOS_MD_ADMEM;
-		D_INFO("UMEM will use AD-hoc Memory as the metadata backend interface\n");
-		return 0;
-	}
-
-	daos_md_backend = DAOS_MD_BMEM;
-	D_INFO("UMEM will use Blob Backed Memory as the metadata backend interface\n");
+	daos_md_backend = md_mode;
 	return 0;
 }
 
