e081db4d666ea74289c266178f93bc475cc7ba9f
Bruno Faccini <bruno.faccini@intel.com>
2023-02-06 19:16:47 +0100

    DAOS-12407 ci: report when no corefile
    Add an explicit message when no corefile are found.
    
    PR-repos: libfabric@PR-6
    Skip-func-hw-test: true
    Required-githooks: true
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>

--- utils/ci/corefile_in_gha.sh
+++ utils/ci/corefile_in_gha.sh
@@ -10,6 +10,11 @@ then
 else
     COREFILE_DIR="/var/lib/systemd/coredump/"
 fi
+if [ $(ls -1 /var/lib/systemd/coredump/ | wc -l) == 0 ]
+then
+    echo "no corefile in $COREFILE_DIR."
+    exit 1
+fi
 for i in `ls -1 $COREFILE_DIR/` ; do
     ls -l $i
     file $i
