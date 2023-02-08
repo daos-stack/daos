e59adbe7eae0ee05efce39fdc6368f098dc60b2e
Bruno Faccini <bruno.faccini@intel.com>
2023-02-08 10:33:14 +0100

    DAOS-12407 ci: fix lint warnings in corefile_in_gha.sh
    And also re-add commit-tags !!...
    
    PR-repos: libfabric@PR-66
    Skip-func-hw-test: true
    Required-githooks: true
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>

--- utils/ci/corefile_in_gha.sh
+++ utils/ci/corefile_in_gha.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-cd daos
+cd daos || exit 1
 
 echo ::group::Check for corefiles
-if [ $BASE_DISTRO == "ubuntu" ]
+if [ "$BASE_DISTRO" == "ubuntu" ]
 then
     # "apport" corefile repo
     COREFILE_DIR="/var/crash/"
 else
     COREFILE_DIR="/var/lib/systemd/coredump/"
 fi
-if [ $(ls -1 /var/lib/systemd/coredump/ | wc -l) == 0 ]
+if [ $(ls -1 "$COREFILE_DIR" | wc -l) == 0 ]
 then
     echo "no corefile in $COREFILE_DIR."
     exit 0
 fi
-for i in `ls -1 $COREFILE_DIR/` ; do
-    ls -l $i
-    file $i
+for i in "$COREFILE_DIR"/* ; do
+    ls -l "$i"
+    file "$i"
 done
 echo ::endgroup::
 
