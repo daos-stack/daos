887458a0d5283e7452f80ab03caebab1897077a2
Padmanabhan <ravindran.padmanabhan@intel.com>
2023-02-09 12:46:54 -0500

    DAOS-12073 test: Reduce changes based on review discussion. Test-tag: test_nvme_pool_extend Test-repeat: 4
    Required-githooks: true
    
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/nvme/pool_extend.py
+++ src/tests/ftest/nvme/pool_extend.py
@@ -53,18 +53,9 @@ class NvmePoolExtend(OSAUtils):
 
         # Create the pools
         pools = []
-        for _ in range(0, num_pool):
-            pools.append(self.get_pool(properties="reclaim:disabled", create=False))
-            # Split total SCM and NVME size for creating multiple pools.
-            tmp = ior_test_sequence[0]
-            scm_size_string = "{}{}".format(str(int(tmp[0]) / num_pool), "%")
-            nvme_size_string = "{}{}".format(str(int(tmp[1]) / num_pool), "%")
-            params = self.server_managers[0].autosize_pool_params(
-                    None, None, scm_size=scm_size_string, nvme_size=nvme_size_string)
-            pools[-1].scm_size.update(params["scm_size"])
-            pools[-1].nvme_size.update(params["nvme_size"])
-            pools[-1].create()
-            # pools[-1].set_property("reclaim", "disabled")
+        for index in range(0, num_pool):
+            pools.append(self.get_pool(namespace="/run/pool_qty_{}".format(index),
+                         properties="reclaim:disabled"))
 
         # On each pool (max 3), extend the ranks
         # eg: ranks : 4,5 ; 6,7; 8,9.
