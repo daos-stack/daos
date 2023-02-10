c7539d0ceb89f922fa394a58bf1ebe044a646ddd
Bruno Faccini <bruno.faccini@intel.com>
2023-02-10 18:36:32 +0100

    DAOS-12407 ci: try to get corefile in cwd
    Avoid usage of apport in GHAs to gather corefile infos and
    try to get it in current dir !!...
    
    PR-repos: libfabric@PR-66
    Skip-func-hw-test: true
    Required-githooks: true
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>

--- utils/ci/corefile_in_gha.sh
+++ utils/ci/corefile_in_gha.sh
@@ -5,24 +5,26 @@ cd daos || exit 1
 echo ::group::Check for corefiles
 
 echo BASE_DISTRO=${BASE_DISTRO}
-echo -n core_pattern ; cat /proc/sys/kernel/core_pattern
+echo -n core_pattern= ; cat /proc/sys/kernel/core_pattern
 
 if [ $(echo "$BASE_DISTRO" | egrep -i 'ubuntu|alma|rocky') ]
 then
     # on GHA internal runners running 'ubuntu|alma|rocky', "apport" seems to be
     # used ...
-    COREFILE_DIR="/var/crash/"
+    # COREFILE_DIR="/var/crash/"
+    # trying to set corefile repo in current dir
+    COREFILE_DIR="./"
 else
     COREFILE_DIR="/var/lib/systemd/coredump/"
 fi
 
-if [ $(find "$COREFILE_DIR" -maxdepth 1 -type f | wc -l) == 0 ]
+if [ $(find "$COREFILE_DIR" -maxdepth 1 -type f | grep core | wc -l) == 0 ]
 then
     echo "no corefile in $COREFILE_DIR."
     exit 0
 fi
-for i in "$COREFILE_DIR"/* ; do
-    ls -l "$i"
+for i in "$COREFILE_DIR/core*" ; do
+    ls -ltr "$i"
     file "$i"
 done
 echo ::endgroup::
