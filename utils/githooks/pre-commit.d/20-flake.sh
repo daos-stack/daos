a0e3b7f3b93b18e728a50f1721d05597a930a52a
Ashley Pittman <ashley.m.pittman@intel.com>
2023-02-14 11:48:16 +0000

    Back out unrelated code.
    Test-tag: dfuse
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- utils/githooks/pre-commit.d/20-flake.sh
+++ utils/githooks/pre-commit.d/20-flake.sh
@@ -24,12 +24,12 @@ fi
 echo "  Checking uncommitted code with flake."
 git diff -u | flake8 --diff
 
-if ! BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null); then
+if ! BRANCH=origin/$(git rev-parse --abbrev-ref HEAD 2>/dev/null); then
     echo "  Failed to determine branch with git rev-parse"
     exit 1
 fi
 
-if [ "$BRANCH" = "master" ]; then
+if [ "$BRANCH" = "origin/master" ]; then
     echo "  Checking tree"
     flake8 --statistics
 else
