050780c4ca8ee839bf244b270c9d3793cea242c9
Ding Ho <ding-hwa.ho@intel.com>
2023-02-15 14:16:03 +0000

    Update pool_exclude.py with thread_queue.

--- src/tests/ftest/nvme/pool_exclude.py
+++ src/tests/ftest/nvme/pool_exclude.py
@@ -7,7 +7,10 @@ import time
 import random
 import threading
 import re
+import queue
 
+from avocado.utils.process import CmdResult
+from ior_utils import run_ior
 from test_utils_pool import add_pool
 from osa_utils import OSAUtils
 from write_host_file import write_host_file
@@ -35,6 +38,70 @@ class NvmePoolExclude(OSAUtils):
         self.pool = None
         self.cont_list = []
         self.dmg_command.exit_status_exception = True
+
+    def run_ior_with_thread_q(thread_queue, test, manager, log, hosts, path, slots, group, pool,
+                              container, processes, ppn, intercept, plugin_path, dfuse,
+                              display_space, fail_on_warning, namespace, ior_params):
+        """Start an IOR thread with thread queue for failure analysis.
+
+        Args:
+            test (Test): avocado Test object
+            manager (JobManager): command to manage the multi-host execution of ior
+            log (str): log file.
+            hosts (NodeSet): hosts on which to run the ior command
+            path (str): hostfile path.
+            slots (int): hostfile number of slots per host.
+            group (str): DAOS server group name
+            pool (TestPool): DAOS test pool object
+            container (TestContainer): DAOS test container object.
+            processes (int): number of processes to run
+            ppn (int, optional): number of processes per node to run.  If specified it will override
+                the processes input. Defaults to None.
+            intercept (str, optional): path to interception library. Defaults to None.
+            plugin_path (str, optional): HDF5 vol connector library path. This will enable dfuse
+                working directory which is needed to run vol connector for DAOS. Default is None.
+            dfuse (Dfuse, optional): DAOS test dfuse object required when specifying a plugin_path.
+                Defaults to None.
+            display_space (bool, optional): Whether to display the pool space. Defaults to True.
+            fail_on_warning (bool, optional): Controls whether the test should fail if a 'WARNING'
+                is found. Default is False.
+            namespace (str, optional): path to yaml parameters. Defaults to "/run/ior/*".
+            ior_params (dict, optional): dictionary of IorCommand attributes to override from
+                get_params(). Defaults to None.
+
+        Raises:
+            CommandFailure: if there is an error running the ior command
+
+        Returns:
+            CmdResult: result of the ior command
+
+        """
+        try:
+            result = run_ior(test, manager, log, hosts, path, slots, group, pool, container,
+                             processes, ppn, intercept, plugin_path, dfuse, display_space,
+                             fail_on_warning, namespace, ior_params)
+            thread_queue.put(result)
+        except Exception as error:
+            thread_queue.put(CmdResult(command="", stdout=str(error), exit_status=1))
+
+    def test_run_ior_with_thread_q(self, thread_queue, pool, oclass, test, flags, single_cont_read=True,
+                              fail_on_warning=True):
+        """Start an IOR thread with thread queue for failure analysis.
+
+        Args:
+            thread_queue (str): ior test thread queue
+            pool (object): pool handle
+            oclass (str): IOR object class, container class.
+            test (list): IOR test sequence
+            flags (str): IOR flags
+            single_cont_read (bool, optional): Always read from the 1st container. Defaults to True.
+            fail_on_warning (bool, optional): Test terminates for IOR warnings. Defaults to True.
+        """
+        try:
+            result = self.ior_thread(pool, oclass, test, flags, single_cont_read, fail_on_warning)
+            thread_queue.put(result)
+        except Exception as error:
+            thread_queue.put(CmdResult(command="", stdout=str(error), exit_status=1))
 
     def run_nvme_pool_exclude(self, num_pool, oclass=None):
         """Perform the actual testing.
@@ -74,8 +141,11 @@ class NvmePoolExclude(OSAUtils):
             for test in range(0, rf_num):
                 ior_test_seq = self.params.get("ior_test_sequence", '/run/ior/iorflags/*')[test]
                 threads = []
-                threads.append(threading.Thread(target=self.ior_thread,
-                                                kwargs={"pool": self.pool,
+                job_manager = self.get_ior_job_manager_command()
+                job_manager.timeout = timeout
+                threads.append(threading.Thread(target=run_ior_with_thread_q,
+                                                kwargs={"thread_queue": "",
+                                                        "pool": self.pool,
                                                         "flags": self.ior_w_flags,
                                                         "oclass": oclass,
                                                         "single_cont_read": True,
@@ -106,8 +176,19 @@ class NvmePoolExclude(OSAUtils):
                 # Wait to finish the threads
                 for thrd in threads:
                     thrd.join()
-                    if not self.out_queue.empty():
-                        self.assert_on_exception(self.out_queue)
+#                    if not self.out_queue.empty():
+#                        self.assert_on_exception(self.out_queue)
+                errors = 0
+                while not thread_queue.empty():
+                    result = thread_queue.get()
+                    if result.exit_status != 0:
+                        errors += 1
+                    print(result)
+                if errors:
+                     self.fail("Errors running {} threads".format(errors))
+                     # if not self.out_queue.empty():
+                     #     self.assert_on_exception()
+
                 # Verify the data after pool exclude
                 self.run_ior_thread("Read", oclass, ior_test_seq)
                 display_string = "Pool{} space at the End".format(val)
