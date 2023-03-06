5707168a701c3439ee7fc30d7a3868827ff6dcb2
Bruno Faccini <bruno.faccini@intel.com>
2023-03-06 14:01:44 +0100

    DAOS-12407 ci: build ABT with --disable-mem-pool
    In order to manage ULT stacks with old "malloc()-based"
    way, and see if it fixes or helps to investigate ...
    
    PR-repos: libfabric@PR-66
    Skip-func-hw-test: true
    Required-githooks: true
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>

--- site_scons/components/__init__.py
+++ site_scons/components/__init__.py
@@ -294,7 +294,8 @@ def define_components(reqs):
                  '--enable-stack-unwind']
 
     if reqs.target_type == 'debug':
-        abt_build.append('--enable-debug=most')
+        abt_build.append('--enable-debug=all')
+        abt_build.append('--disable-mem-pool')
     else:
         abt_build.append('--disable-debug')
 
--- utils/ci/corefile_in_gha.sh
+++ utils/ci/corefile_in_gha.sh
@@ -4,10 +4,10 @@ cd daos || exit 1
 
 echo ::group::Check for corefiles
 
-echo BASE_DISTRO=${BASE_DISTRO}
+echo BASE_DISTRO="${BASE_DISTRO}"
 echo -n core_pattern= ; cat /proc/sys/kernel/core_pattern
 
-if [ $(echo "$BASE_DISTRO" | egrep -i 'ubuntu|alma|rocky') ]
+if [ $(echo "$BASE_DISTRO" | grep -E -q -i 'ubuntu|alma|rocky') ]
 then
     # on GHA internal runners running 'ubuntu|alma|rocky', "apport" seems to be
     # used ...
@@ -19,12 +19,12 @@ else
     COREFILE_DIR="/var/lib/systemd/coredump/"
 fi
 
-if [ $(find "$COREFILE_DIR" -maxdepth 1 -type f | grep core | wc -l) == 0 ]
+if [ $(find "$COREFILE_DIR" -maxdepth 1 -type f | grep -c core) == 0 ]
 then
     echo "no corefile in $COREFILE_DIR"
     exit 0
 fi
-for i in $COREFILE_DIR/core* ; do
+for i in $(find "$COREFILE_DIR"/core* -print 2>/dev/null) ; do
     ls -ltr "$i"
     file "$i"
     gdb /opt/daos/bin/daos_engine "$i" <<eof
