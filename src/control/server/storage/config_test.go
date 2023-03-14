1683b8d7eee7e82fe1e24cea046dd7218b1fb97a
Tom Nabarro <tom.nabarro@intel.com>
2023-03-14 14:15:37 +0000

    DAOS-12826 control: Handle empty storage tier in config unmarshal (#11647) (#11682)
    Avoid segfault when control-plane parses a server config file with an
    empty storage tier yaml list entry by ignoring nil when iterating
    tiers during custom unmarshal.
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/storage/config_test.go
+++ src/control/server/storage/config_test.go
@@ -1,5 +1,5 @@
 //
-// (C) Copyright 2019-2022 Intel Corporation.
+// (C) Copyright 2019-2023 Intel Corporation.
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -315,6 +315,147 @@ func TestStorage_parsePCIBusRange(t *testing.T) {
 
 			test.AssertEqual(t, tc.expBegin, begin, "bad beginning limit")
 			test.AssertEqual(t, tc.expEnd, end, "bad ending limit")
+		})
+	}
+}
+
+func TestStorage_TierConfigs_FromYAML(t *testing.T) {
+	for name, tc := range map[string]struct {
+		input    string
+		expTiers TierConfigs
+		expErr   error
+	}{
+		"single tier": {
+			input: `
+storage:
+-
+  class: ram
+  scm_mount: /mnt/daos/1
+  scm_size: 16
+`,
+			expTiers: TierConfigs{
+				&TierConfig{
+					Class: ClassRam,
+					Scm: ScmConfig{
+						MountPoint:  "/mnt/daos/1",
+						RamdiskSize: 16,
+					},
+				},
+			},
+		},
+		"empty tier first": {
+			input: `
+storage:
+-
+-
+  class: ram
+  scm_mount: /mnt/daos/1
+  scm_size: 16
+-
+  class: dcpm
+`,
+			expTiers: TierConfigs{
+				&TierConfig{
+					Class: ClassRam,
+					Scm: ScmConfig{
+						MountPoint:  "/mnt/daos/1",
+						RamdiskSize: 16,
+					},
+				},
+				&TierConfig{
+					Tier:  1,
+					Class: ClassDcpm,
+				},
+			},
+		},
+		"empty tier middle": {
+			input: `
+storage:
+-
+  class: ram
+  scm_mount: /mnt/daos/1
+  scm_size: 16
+-
+-
+  class: dcpm
+`,
+			expTiers: TierConfigs{
+				&TierConfig{
+					Class: ClassRam,
+					Scm: ScmConfig{
+						MountPoint:  "/mnt/daos/1",
+						RamdiskSize: 16,
+					},
+				},
+				&TierConfig{
+					Tier:  1,
+					Class: ClassDcpm,
+				},
+			},
+		},
+		"empty tier last": {
+			input: `
+storage:
+-
+  class: ram
+  scm_mount: /mnt/daos/1
+  scm_size: 16
+-
+  class: dcpm
+-
+`,
+			expTiers: TierConfigs{
+				&TierConfig{
+					Class: ClassRam,
+					Scm: ScmConfig{
+						MountPoint:  "/mnt/daos/1",
+						RamdiskSize: 16,
+					},
+				},
+				&TierConfig{
+					Tier:  1,
+					Class: ClassDcpm,
+				},
+			},
+		},
+		"two empty tiers last": {
+			input: `
+storage:
+-
+  class: ram
+  scm_mount: /mnt/daos/1
+  scm_size: 16
+-
+  class: dcpm
+-
+-
+`,
+			expTiers: TierConfigs{
+				&TierConfig{
+					Class: ClassRam,
+					Scm: ScmConfig{
+						MountPoint:  "/mnt/daos/1",
+						RamdiskSize: 16,
+					},
+				},
+				&TierConfig{
+					Tier:  1,
+					Class: ClassDcpm,
+				},
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			cfg := new(Config)
+			err := yaml.Unmarshal([]byte(tc.input), cfg)
+			test.CmpErr(t, tc.expErr, err)
+			if tc.expErr != nil {
+				return
+			}
+
+			if diff := cmp.Diff(tc.expTiers, cfg.Tiers, defConfigCmpOpts()...); diff != "" {
+				t.Fatalf("bad props (-want +got):\n%s", diff)
+			}
 		})
 	}
 }
