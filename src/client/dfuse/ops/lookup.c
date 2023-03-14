be0c6a8a80efb7ee522259682e21e24ec62bab77
Ashley Pittman <ashley.m.pittman@intel.com>
2023-03-14 21:26:27 +0000

    Comment out fix an enable extra test.
    Test-tag: dfuse
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/client/dfuse/ops/lookup.c
+++ src/client/dfuse/ops/lookup.c
@@ -135,7 +135,9 @@ dfuse_reply_entry(struct dfuse_projection_info *fs_handle,
 		entry.attr_timeout = ie->ie_dfs->dfc_attr_timeout;
 	}
 
-	ie->ie_stat.st_mode = entry.attr.st_mode;
+#if 0
+	ie->ie_stat = entry.attr;
+#endif
 
 	if (fi_out)
 		DFUSE_REPLY_CREATE(ie, req, entry, fi_out);
