dcd66017d59f17613525cf6ae4d9dbdc4a1bfe66
wangdi <di.wang@intel.com>
2023-03-21 09:06:19 -0700

    DAOS-12869 object: shrink layout version to 16 bits (#11674)
    shrink object layout version to 16 bits.
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/vos/tests/pool_scrubbing_tests.c
+++ src/vos/tests/pool_scrubbing_tests.c
@@ -335,6 +335,7 @@ set_test_oid(daos_unit_oid_t *oid, uint64_t oid_lo)
 {
 	oid->id_shard	= 1;
 	oid->id_layout_ver = 0;
+	oid->id_padding = 0;
 	oid->id_pub.lo = oid_lo;
 	daos_obj_set_oid(&oid->id_pub, 0, OR_RP_1, MAX_NUM_GROUPS, 0);
 }
