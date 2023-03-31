eca077a8d5cd3e8bf95c6d028524006eb8918d33
Tom Nabarro <tom.nabarro@intel.com>
2023-03-31 19:52:29 +0100

    DAOS-12672 control: Make dRPC calls context aware (#11760)
    - Use context to close connection on cancel
    - Introduce mutex to protect connection reference
    - Remove unused dRPC retry logic
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/cmd/hello_drpc/main.go
+++ src/control/cmd/hello_drpc/main.go
@@ -1,5 +1,5 @@
 //
-// (C) Copyright 2018-2022 Intel Corporation.
+// (C) Copyright 2018-2023 Intel Corporation.
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -98,7 +98,7 @@ func runDrpcClient(log logging.Logger) error {
 		return errors.Wrap(err, "marshalling the Call body")
 	}
 
-	resp, err := client.SendMsg(message)
+	resp, err := client.SendMsg(context.Background(), message)
 	if err != nil {
 		return errors.Wrap(err, "sending message")
 	}
