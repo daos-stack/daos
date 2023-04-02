9ca2f6f234183eb918dc98ea2bbf3d55653479ab
Padmanabhan <ravindran.padmanabhan@intel.com>
2023-04-02 17:50:59 -0400

    DAOS-12943 test: Reduce the sleep time in the loop. Test-tag: test_dmg_storage_query_device_state test_disk_failure_recover test_disk_fault_to_normal
    Required-githooks: true
    
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/deployment/disk_failure.py
+++ src/tests/ftest/deployment/disk_failure.py
@@ -151,7 +151,7 @@ class DiskFailureTest(OSAUtils):
                 for _ in range(10):
                     data = self.dmg_command.storage_replace_nvme(old_uuid=device["uuid"],
                                                                  new_uuid=device["uuid"])
-                    time.sleep(10)
+                    time.sleep(5)
                     if data['error'] or len(data['response']['host_errors']) > 0:
                         error_count = error_count + 1
                     else:
