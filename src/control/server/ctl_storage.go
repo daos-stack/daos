76ca3111e27511115dbe56ed4deaae042bb4dd22
Tom Nabarro <tom.nabarro@intel.com>
2023-04-04 13:09:08 +0100

    move mock functions into server/mocks.go
    Required-githooks: true
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/ctl_storage.go
+++ src/control/server/ctl_storage.go
@@ -75,48 +75,6 @@ func NewStorageControlService(log logging.Logger, ecs []*engine.Config) *Storage
 	}
 }
 
-// MockMemInfo returns a mock MemInfo result.
-func MockMemInfo() *common.MemInfo {
-	return &common.MemInfo{
-		HugepagesTotal:  1024,
-		HugepagesFree:   512,
-		HugepagesRsvd:   64,
-		HugepagesSurp:   32,
-		HugepageSizeKiB: 2048,
-		MemTotalKiB:     (humanize.GiByte * 4) / humanize.KiByte,
-		MemFreeKiB:      (humanize.GiByte * 1) / humanize.KiByte,
-		MemAvailableKiB: (humanize.GiByte * 2) / humanize.KiByte,
-	}
-}
-
-// NewMockStorageControlService returns a StorageControlService with a mocked
-// storage provider consisting of the given sys, scm and bdev providers.
-func NewMockStorageControlService(log logging.Logger, ecs []*engine.Config, sys storage.SystemProvider, scm storage.ScmProvider, bdev storage.BdevProvider, getMemInfo common.GetMemInfoFn) *StorageControlService {
-	topCfg := &storage.Config{
-		Tiers: nil,
-	}
-	if len(ecs) > 0 {
-		topCfg.ControlMetadata = ecs[0].Storage.ControlMetadata
-	}
-	instanceStorage := make(map[uint32]*storage.Config)
-	for i, c := range ecs {
-		instanceStorage[uint32(i)] = &c.Storage
-	}
-
-	if getMemInfo == nil {
-		getMemInfo = func() (*common.MemInfo, error) {
-			return MockMemInfo(), nil
-		}
-	}
-
-	return &StorageControlService{
-		log:             log,
-		instanceStorage: instanceStorage,
-		storage:         storage.MockProvider(log, 0, topCfg, sys, scm, bdev, nil),
-		getMemInfo:      getMemInfo,
-	}
-}
-
 func findPMemInScan(ssr *storage.ScmScanResponse, pmemDevs []string) *storage.ScmNamespace {
 	for _, scanned := range ssr.Namespaces {
 		for _, path := range pmemDevs {
