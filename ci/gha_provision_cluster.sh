cbe42d6c7b3048d18bc493cc62d0121939b5ac7a
Brian J. Murrell <brian.murrell@intel.com>
2023-04-13 08:42:40 -0400

    Move provisioning to a script
    For better linting support.
    
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    
    Required-githooks: true
    
    
    #Pragmas from previous commit message:
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: true
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: true
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: false
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: true
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: false
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: false
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: true
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: false
    Skip-func-test-leap15: false
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Skip-func-test-leap15: false
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: false
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: false
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: true
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: true
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-build-el8-rpm: true
    Skip-func-test-leap15: false
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Provisioning-pool: bmurrell-automate
    Test-tag: always_passes always_fails
    Skip-checkpatch: true
    Skip-python-bandit: true
    Skip-build: true
    Quick-build: true
    Quick-Functional: true
    Allow-unstable-test: true
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

---
+++ ci/gha_provision_cluster.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+uuid=$(uuidgen)
+echo "CLUSTER_REQUEST_UUID=$uuid" >> "$GITHUB_ENV"
+url='https://build.hpdd.intel.com/job/Get%20a%20cluster/buildWithParameters?token=mytoken&LABEL=stage_waittest_vm9&'"UUID=$uuid"
+if ! queue_url=$(curl -D - -f -v -X POST --user "$JENKINS_TOKEN" "$url" |
+                 sed -ne 's/\r//' -e '/Location:/s/.*: //p'); then
+    echo "Failed to request a cluster."
+    exit 1
+fi
+set +x
+curl -sf --user "$JENKINS_TOKEN" "${queue_url}api/json/" | jq -r .why
+while [ ! -f /scratch/Get\ a\ cluster/"$uuid" ]; do
+    echo "Waiting for a cluster"
+    if [ $((SECONDS % 10)) -eq 0 ]; then
+        { read -r cancelled; read -r why; } < \
+            <(curl -sf --user "$JENKINS_TOKEN" "${queue_url}api/json/" |
+              jq -r .cancelled,.why)
+        if [ "$cancelled" == "true" ]; then
+            echo "Cluster request cancelled from Jenkins"
+            exit 1
+        fi
+        echo "$why"
+    fi
+    sleep 1
+done
+NODESTRING=$(cat /scratch/Get\ a\ cluster/"$uuid")
+if [ "$NODESTRING" = "cancelled" ]; then
+    echo "Cluster request cancelled from Jenkins"
+    exit 1
+fi
+{ echo "NODESTRING=$NODESTRING"
+  echo "NODELIST=$NODESTRING"; } >> "$GITHUB_ENV"
+echo "NODE_COUNT=$(echo "$NODESTRING" | tr ',' ' ' | wc -w)" >> "$GITHUB_ENV"
+cat "$GITHUB_ENV"
+ssh -oPasswordAuthentication=false -v root@"${NODESTRING%%vm*}" \
+    "POOL=$CP_PROVISIONING_POOL
+     NODESTRING=$NODESTRING
+     NODELIST=$NODESTRING
+     DISTRO=$DISTRO_WITH_VERSION
+     $(cat ci/provisioning/provision_cluster.sh)"
