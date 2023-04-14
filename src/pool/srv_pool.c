7ba432b2fb86a58c940022ec463c4e8575b40f7c
Jeff Olivier <jeffrey.v.olivier@intel.com>
2023-04-13 21:47:43 -0400

    DAOS-13017 doc: Document checkpointing properties (#11892)
    Add documentation for checkpoint control properties.
    
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- src/pool/srv_pool.c
+++ src/pool/srv_pool.c
@@ -599,6 +599,10 @@ pool_prop_write(struct rdb_tx *tx, const rdb_path_t *kvs, daos_prop_t *prop)
 			break;
 		case DAOS_PROP_PO_CHECKPOINT_FREQ:
 			val32 = entry->dpe_val;
+			if (val32 > DAOS_PROP_PO_CHECKPOINT_FREQ_MAX)
+				val32 = DAOS_PROP_PO_CHECKPOINT_FREQ_MAX;
+			else if (val32 < DAOS_PROP_PO_CHECKPOINT_FREQ_MIN)
+				val32 = DAOS_PROP_PO_CHECKPOINT_FREQ_MIN;
 			d_iov_set(&value, &val32, sizeof(val32));
 			rc = rdb_tx_update(tx, kvs, &ds_pool_prop_checkpoint_freq, &value);
 			if (rc)
