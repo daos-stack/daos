bea296190b1153a6da08886bd251376d9a79efb2
Padmanabhan <ravindran.padmanabhan@intel.com>
2023-04-21 11:04:30 -0400

    DAOS-13042 test: Minor format update. Skip-nlt: true Skip-build-leap15-rpm: true Skip-scan-leap15-rpms: true Test-tag: offline_drain Test-repeat: 3
    Required-githooks: true
    
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/osa/offline_drain.py
+++ src/tests/ftest/osa/offline_drain.py
@@ -124,16 +124,14 @@ class OSAOfflineDrain(OSAUtils, ServerFillUp):
                     self.print_and_assert_on_rebuild_failure(output)
                     total_space_after_reintegration = self.pool.get_total_space(refresh=True)
                     self.assertGreater(total_space_after_reintegration, total_space_after_drain,
-                                       "Expected total space after reintegration \
-                                        is less than drain")
+                            "Expected total space after reintegration is less than drain")
                 if (self.test_during_rebuild is True and val == 0):
                     # Reintegrate rank 3
                     output = self.pool.reintegrate("3")
                     self.print_and_assert_on_rebuild_failure(output)
                     total_space_after_reintegration = self.pool.get_total_space(refresh=True)
                     self.assertGreater(total_space_after_reintegration, total_space_after_drain,
-                                       "Expected total space after reintegration \
-                                        is less than drain")
+                            "Expected total space after reintegration is less than drain")
 
         for val in range(0, num_pool):
             display_string = "Pool{} space at the End".format(val)
