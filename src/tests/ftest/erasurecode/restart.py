32d3c03c6abb8dd81d24d4ea9769ecd9b22f6e23
Ding Ho <ding-hwa.ho@intel.com>
2023-04-28 20:49:30 +0000

    Update erasurecode/restart.py
    Required-githooks: true

--- src/tests/ftest/erasurecode/restart.py
+++ src/tests/ftest/erasurecode/restart.py
@@ -21,12 +21,23 @@ class EcodServerRestart(ErasureCodeIor):
         super().setUp()
         self.percent = self.params.get("size", '/run/ior/data_percentage/*')
 
-    def verify_aggreation(self, first_ior_free_space):
+    def verify_aggreation(self, expected_free_space):
+        """Verify aggrgation by checking pool free space is expected.
+
+        Args:
+            expected_free_space (int): expected free space after aggregation.
+        """
         self.log.info("Waiting for aggregation to complete")
-        if not wait_for_result(self.log, 150, check_free_space_equals, expected=first_ior_free_space):
-            self.fail("aggregation completion not detected")
+        if not wait_for_result(
+            self.log, check_free_space_equals, 150, delay=5, expected=expected_free_space):
+            self.fail("#aggregation completion not detected.")
 
     def check_free_space_equals(self, expected):
+        """Check for pool free space.
+
+        return:
+            bool: if the result was of pool free space match the expected.
+        """
         return expected == self.pool.get_total_free_space(refresh=True)
 
 
@@ -47,14 +58,15 @@ class EcodServerRestart(ErasureCodeIor):
         containers = []
         for oclass in self.obj_class:
             for sizes in self.ior_chu_trs_blk_size:
-                # Skip the object type if server count does not meet the minimum EC object server count
+                # Skip the object type if server count does not meet the minimum EC object
+                # server count
                 if oclass[1] > self.server_count:
                     continue
                 ec_object = get_data_parity_number(self.log, oclass)
                 containers.append(
                     self.get_container(
                         self.pool, daos_command=self.get_daos_command(), oclass=oclass,
-                        properties="rd_fac:{}".format(ec_object['parity']))
+                        properties="rd_fac:{}".format(ec_object['parity'])))
 
         # 2.
         self.log_step("Disable aggregation")
@@ -66,7 +78,6 @@ class EcodServerRestart(ErasureCodeIor):
 
         # 4.
         self.log_step("Run IOR write all EC object data to container")
-#        self.ior_write_dataset(operation="Auto_Write", percent=self.percent)
         for container in containers:
             ior_kwargs['container'] = container
             try:
@@ -90,7 +101,7 @@ class EcodServerRestart(ErasureCodeIor):
             except CommandFailure as error:
                 self.fail('ior failed')
         free_space_after_second_ior = self.pool.get_total_free_space(refresh=True)
-        
+
         # 7.
         self.log_step("Verify the free space after second ior is less at least twice the size of"
                       " space_used_by_ior from initial_free_space")
@@ -98,11 +109,6 @@ class EcodServerRestart(ErasureCodeIor):
             free_space_after_second_ior, (initial_free_space - space_used_by_ior * 2),
             "Pool free space has not been reduced by double after dual ior writes")
 
-#        if agg_check == "Restart_before_agg":
-#            # step-4 for Restart_before_agg test
-#            self.log_step("Shutdown the servers and restart")
-#            self.get_dmg_command().system_stop(True)
-#            self.get_dmg_command().system_start()
         # 8.
         self.log_step("Enable aggregation")
         self.pool.set_property("reclaim", "time")
@@ -110,7 +116,7 @@ class EcodServerRestart(ErasureCodeIor):
         if agg_check == "Restart_after_agg":
             # setp-9 for Restart_after_agg
             self.log_step("Verify aggregation is complete before engine restart")
-            self.verify_aggreation()
+            self.verify_aggreation(free_space_after_ior)
 
         # 9.
         self.log_step("Stop the engines (dmg system stop)")
@@ -123,7 +129,7 @@ class EcodServerRestart(ErasureCodeIor):
         # 11.
         if agg_check == "Restart_before_agg":
             self.log_step("Verify aggregation is complete after engine restart")
-            self.verify_aggreation()
+            self.verify_aggreation(free_space_after_ior)
 
         # 12.
         self.log_step("Verify data after aggregation (ior read)")
@@ -134,65 +140,8 @@ class EcodServerRestart(ErasureCodeIor):
                 result = run_ior(**ior_kwargs)
             except CommandFailure as error:
                 self.fail('ior failed')
-                
+
         self.log_step("Test passed")
-
-
-
-
-        # 4.  step-5 for Restart_before_agg test
-        self.log_step("Enable aggregation")
-        self.pool.set_property("reclaim", "time")
-
-        # 5.  step-6 for Restart_before_agg test
-        self.log_step("Rerun IOR")
-        self.ior_write_dataset(storage='SCM', operation="Auto_Write", percent=self.percent)
-        init_total_free_space = self.pool.get_total_free_space(refresh=True)
-        pool_info = self.pool.get_pool_daos_space()
-        init_scm_free_space = pool_info["s_free"][0]
-        init_nvme_free_space = pool_info["s_free"][1]
-        self.log.info("After aggregation started: total_free: %s, scm_free: %s, nvme_free: %s",
-                      init_total_free_space, init_scm_free_space, init_nvme_free_space)
-
-
-
-        # 6.  step-7 for Restart_before_agg test
-        self.log_step("Verify aggregation triggered, scm free space move to nvme")
-        aggregation_detected = False
-        timed_out = False
-        start_time = time.time()
-        while not aggregation_detected and not timed_out:
-            pool_info = self.pool.get_pool_daos_space()
-            current_scm_free_space = pool_info["s_free"][0]
-            current_nvme_free_space = pool_info["s_free"][1]
-            diff_scm_free_space = current_scm_free_space - init_scm_free_space
-            diff_nvme_free_space = init_nvme_free_space - current_nvme_free_space
-            self.log.info(
-                "pool scm free space during Aggregation = %s, diff_scm_free_space = %s",
-                current_scm_free_space, diff_scm_free_space)
-            self.log.info(
-                "pool nvme free space during Aggregation = %s, diff_nvme_free_space = %s",
-                current_nvme_free_space, diff_nvme_free_space)
-            if diff_scm_free_space > aggr_threshold and diff_nvme_free_space > aggr_threshold:
-                self.log.info("Aggregation Detectted .....")
-                aggregation_detected = True
-            else:
-                time.sleep(5)
-                if time.time() - start_time > 180:
-                    timed_out = True
-        if not aggregation_detected:
-            self.fail("#Aggregation failed to start..")
-
-        if agg_check == "Restart_after_agg":
-            # 7.
-            self.log_step("Shutdown the servers and restart")
-            self.get_dmg_command().system_stop(True)
-            self.get_dmg_command().system_start()
-
-        # 8.
-        self.log_step("run IOR read to verify data")
-        self.ior_read_dataset(operation="Auto_Read", percent=self.percent)
-        self.log.info("Test passed")
 
     def test_ec_restart_before_agg(self):
         """Jira ID: DAOS-7337.
