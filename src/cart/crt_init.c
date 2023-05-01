9c0622b40d4ef39dccf7b7761d5aa05986767ceb
Joseph Moore <joseph.moore@intel.com>
2023-05-01 20:15:31 +0000

    DAOS-13268 cart: Fix UCX.
    Skip-build-leap15-icc: true
    Skip-build-el8-gcc: true
    Skip-build-ubuntu20-rpm: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Test-provider: ucx+dc_x
    
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -307,7 +307,9 @@ static int data_init(int server, crt_init_options_t *opt)
 	if (server)
 		setenv("UCX_IB_FORK_INIT", "n", 1);
 
-	setenv(" UCX_SOCKADDR_TLS_PRIORITY", "rdmacm", 1);
+	setenv("UCX_SOCKADDR_TLS_PRIORITY", "rdmacm", 1);
+	setenv("UCX_LOG_LEVEL", "diag", 1);
+	setenv("D_LOG_STDERR_IN_LOG", "1", 1);
 
 	/* This is a workaround for CART-871 if universe size is not set */
 	d_getenv_int("FI_UNIVERSE_SIZE", &fi_univ_size);
