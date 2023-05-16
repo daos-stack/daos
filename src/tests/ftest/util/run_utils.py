73656f21c5071a1fd43239901b79fbb6351fd99f
Phil Henderson <phillip.henderson@intel.com>
2023-05-16 19:43:03 -0400

    DAOS-13444 test: Include stdout for timed out run_remote commands.
    Updating the RemoteCommandResult.log_output() method to include command
    stdout when logging timedout commands.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Allow-unstable-test: true
    Test-tag: DaosBuild DfuseMUMount DfuseMUPerms RootContainerTest ParallelIo FioSmall
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/run_utils.py
+++ src/tests/ftest/util/run_utils.py
@@ -153,12 +153,14 @@ class RemoteCommandResult():
 
         """
         for data in self.output:
-            if data.timeout:
-                log.debug("  %s (rc=%s): timed out", str(data.hosts), data.returncode)
-            elif len(data.stdout) == 1:
-                log.debug("  %s (rc=%s): %s", str(data.hosts), data.returncode, data.stdout[0])
+            if data.timeout and len(data.stdout) == 1:
+                log.debug(
+                    "  %s (rc=%s) timed out: %s", str(data.hosts), data.returncode, data.stdout[0])
+            elif data.timeout:
+                log.debug("  %s (rc=%s) timed out:", str(data.hosts), data.returncode)
             else:
                 log.debug("  %s (rc=%s):", str(data.hosts), data.returncode)
+            if len(data.stdout) > 1:
                 for line in data.stdout:
                     log.debug("    %s", line)
 
