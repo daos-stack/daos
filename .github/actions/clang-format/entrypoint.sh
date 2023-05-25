d639608316ecf8ed984a2fbddbb65ce383becc3e
Ashley Pittman <ashley.m.pittman@intel.com>
2023-05-25 10:08:41 +0000

    Tweak the artifacts.
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- .github/actions/clang-format/entrypoint.sh
+++ .github/actions/clang-format/entrypoint.sh
@@ -4,7 +4,16 @@ set -e
 
 git config --global --add safe.directory /github/workspace
 
+set +e
+
 # Show what would be changed as a diff.
-git-clang-format "${INPUT_TARGET}" --diffstat || true
-git-clang-format "${INPUT_TARGET}" --diff > chang-format-changes.diff
-cat chang-format-changes.diff
+git-clang-format "${INPUT_TARGET}" --diffstat
+git-clang-format "${INPUT_TARGET}" --diff > auto-format-changes.diff
+cat auto-format-changes.diff
+
+if [ -s auto-format-changes.diff ]
+then
+        echo "Changes advised"
+        exit 1
+fi
+exit 0
