0b5a9a2bdda150e3d0d5e6bd8c446297560f7996
Ashley Pittman <ashley.m.pittman@intel.com>
2023-06-06 09:15:22 +0000

    DAOS-13512 cq: Update clang-format hook.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- .github/actions/clang-format/entrypoint.sh
+++ .github/actions/clang-format/entrypoint.sh
@@ -8,7 +8,17 @@ set +e
 
 # Show what would be changed as a diff.
 git-clang-format "${INPUT_TARGET}" --diffstat
-git-clang-format "${INPUT_TARGET}" --diff --quiet > auto-format-changes.diff
+
+# Fix any formatting.
+git-clang-format "${INPUT_TARGET}"
+
+# Now revert any changes to auto-generated files.
+find . -name *.pb-c.c -exec git checkout '{}' \;
+find . -name *.pb-c.h -exec git checkout '{}' \;
+find . -name *.proto -exec git checkout '{}' \;
+
+git diff > auto-format-changes.diff
+
 cat auto-format-changes.diff
 
 if [ -s auto-format-changes.diff ]
