ecfea6d7bcd6b86eaddc1093b573f9e538357227
Ashley Pittman <ashley.m.pittman@intel.com>
2023-06-06 09:32:56 +0000

    Add comment.
    Required-githooks: true
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- .github/actions/clang-format/entrypoint.sh
+++ .github/actions/clang-format/entrypoint.sh
@@ -3,15 +3,18 @@
 git config --global --add safe.directory /github/workspace
 
 # Fix any formatting.
+echo "Applying code formatting"
 git-clang-format "${INPUT_TARGET}"
 
 git diff
 
+echo "Reverting any suggestions made to generated files"
 # Now revert any changes to auto-generated files.
 find . -name "*.pb-c.c" -exec git checkout --quiet ./'{}' \;
 find . -name "*.pb-c.h" -exec git checkout --quiet ./'{}' \;
 find . -name "*.proto" -exec git checkout --quiet ./'{}' \;
 
+echo "Showing recommended formatting changes"
 git diff > auto-format-changes.diff
 
 cat auto-format-changes.diff
