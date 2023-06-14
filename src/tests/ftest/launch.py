7af02a93df93621d35a659be5c532173367dbbdf
Joseph Moore <joseph.moore@intel.com>
2023-06-14 14:19:41 +0000

    DAOS-13386 cart: Run with UCX debug build.
    Skip-build-leap15-icc: true
    Skip-build-el8-gcc: true
    Skip-build-ubuntu20-rpm: true
    Skip-build-leap15-rpm: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Skip-func-hw-test-medium-ucx-provider: false
    Skip-func-hw-test-medium-verbs-provider: true
    Test-provider: ucx+ud_x
    PR-repos: openucx@PR-9:3
    
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -60,7 +60,7 @@ PROVIDER_KEYS = OrderedDict(
     [
         ("cxi", "ofi+cxi"),
         ("verbs", "ofi+verbs"),
-        ("ucx", "ucx+rc_x"),
+        ("ucx", "ucx+ud_x"),
         ("tcp", "ofi+tcp"),
         ("opx", "ofi+opx"),
     ]
