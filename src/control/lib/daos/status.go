0aba1270af2bd417b7e8ebd3bfc615e174d95f8b
Ashley Pittman <ashley.m.pittman@intel.com>
2023-06-16 08:06:07 +0000

    The "I'm not proud of this" commit.
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/control/lib/daos/status.go
+++ src/control/lib/daos/status.go
@@ -14,6 +14,12 @@ import "fmt"
 #cgo LDFLAGS: -lgurt
 
 #include <daos_errno.h>
+
+static enum daos_errno
+int_to_enum(int rc) {
+	return rc;
+}
+
 */
 import "C"
 
@@ -21,8 +27,8 @@ import "C"
 type Status int32
 
 func (ds Status) Error() string {
-	dErrStr := C.GoString(C.d_errstr(C.int(ds)))
-	dErrDesc := C.GoString(C.d_errdesc(C.int(ds)))
+	dErrStr := C.GoString(C.d_errstr(C.int_to_enum(C.int(ds))))
+	dErrDesc := C.GoString(C.d_errdesc(C.int_to_enum(C.int(ds))))
 	return fmt.Sprintf("%s(%d): %s", dErrStr, ds, dErrDesc)
 }
 
