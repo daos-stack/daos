58d26418fca411d914829c6c51b45a99f164874c
Mohamad Chaarawi <mohamad.chaarawi@intel.com>
2023-06-21 13:40:56 -0500

    DAOS-13650 object: remove multiple RPC version of object (#12306)
    - old one is out of support (2.0)
    
    Signed-off-by: Mohamad Chaarawi <mohamad.chaarawi@intel.com>

--- src/client/array/dc_array.c
+++ src/client/array/dc_array.c
@@ -1822,7 +1822,7 @@ struct key_query_props {
 	char			akey_val;
 	daos_recx_t		recx;
 	daos_size_t		*size;
-	daos_size_t		max_epoch;
+	daos_epoch_t		max_epoch;
 	tse_task_t		*ptask;
 };
 
@@ -1831,8 +1831,7 @@ free_query_cb(tse_task_t *task, void *data)
 {
 	struct key_query_props *props = *((struct key_query_props **)data);
 
-	if (props->array)
-		array_decref(props->array);
+	array_decref(props->array);
 	D_FREE(props);
 	return 0;
 }
@@ -1905,7 +1904,7 @@ dc_array_get_size(tse_task_t *task)
 	query_args->dkey	= &kqp->dkey;
 	query_args->akey	= &kqp->akey;
 	query_args->recx	= &kqp->recx;
-	query_args->max_epoch	= NULL;
+	query_args->max_epoch	= &kqp->max_epoch;
 
 	rc = tse_task_register_comp_cb(task, free_query_cb, &kqp, sizeof(kqp));
 	if (rc != 0)
