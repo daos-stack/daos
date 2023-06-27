f0cdf5aafd6f2ed75e86c25379fc53870d7330b5
Joseph Moore <joseph.moore@intel.com>
2023-06-27 16:38:48 +0000

    DAOS-13386 cart: Upgrade UCX.
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -60,7 +60,7 @@ PROVIDER_KEYS = OrderedDict(
     [
         ("cxi", "ofi+cxi"),
         ("verbs", "ofi+verbs"),
-        ("ucx", "ucx+dc_x,ud_x"),
+        ("ucx", "ucx+dc_x"),
         ("tcp", "ofi+tcp"),
         ("opx", "ofi+opx"),
     ]
