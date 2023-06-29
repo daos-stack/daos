c358bdc61053953007d6abbbba7a0ee7852f8d59
Kris Jacque <kris.jacque@intel.com>
2023-06-29 15:35:39 +0000

    Specify ofi_rxm explicitly in ftests
    We shouldn't use the non-RXM provider by default until it is more
    thoroughly tested with DAOS.
    
    Test-tag: pr control,-test_version
    
    Required-githooks: true
    
    Signed-off-by: Kris Jacque <kris.jacque@intel.com>

--- src/tests/ftest/launch.py
+++ src/tests/ftest/launch.py
@@ -59,9 +59,9 @@ TEST_EXPECT_CORE_FILES = ["./harness/core_files.py"]
 PROVIDER_KEYS = OrderedDict(
     [
         ("cxi", "ofi+cxi"),
-        ("verbs", "ofi+verbs"),
+        ("verbs", "ofi+verbs;ofi_rxm"),
         ("ucx", "ucx+dc_x"),
-        ("tcp", "ofi+tcp"),
+        ("tcp", "ofi+tcp;ofi_rxm"),
         ("opx", "ofi+opx"),
     ]
 )
