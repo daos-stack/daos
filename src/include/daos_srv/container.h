0b2e91fc5f1995cba4b50bdcfe948d2b5f6d6a2a
Di Wang <di.wang@intel.com>
2023-07-14 19:56:13 +0000

    DAOS-13295 rebuild: reverse recx enumeration order for rebuild
    1. reverse recx enumeration order, so data from parity rebuild
    will be rebuilt first, then if any replicate data being left
    after EC aggregation, they will be ignored during rebuild.
    
    2. Exclude EC aggregation with the whole rebuild process to avoid
    enumeration might enumerate any aggregating replicate recx.
    
    Required-githooks: true
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/include/daos_srv/container.h
+++ src/include/daos_srv/container.h
@@ -72,7 +72,7 @@ struct ds_cont_child {
 				 sc_vos_agg_active:1,
 				 sc_ec_agg_active:1,
 				 sc_scrubbing:1,
-				 sc_discarding:1;
+				 sc_migrating:1;
 	uint32_t		 sc_dtx_batched_gen;
 	/* Tracks the schedule request for aggregation ULT */
 	struct sched_request	*sc_agg_req;
