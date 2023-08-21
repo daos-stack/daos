04041c32cab8fa3579acba02582c434b0565a79a
Ashley Pittman <ashley@pittman.co.uk>
2023-08-21 09:28:07 +0100

    Revert "DAOS-13239 cart: Fix send failure error mapping.. (#12199)"
    This reverts commit 07e3cb4366572992c1a01d68b7596315db3e1138.
    
    Test-tag: dfuse
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/include/daos/common.h
+++ src/include/daos/common.h
@@ -633,7 +633,7 @@ static inline bool
 daos_crt_network_error(int err)
 {
 	return err == -DER_HG || err == -DER_UNREACH || err == -DER_CANCELED ||
-	       err == -DER_NOREPLY || err == -DER_OOG || err == -DER_HG_SEND_FAILED;
+	       err == -DER_NOREPLY || err == -DER_OOG;
 }
 
 /** See crt_quiet_error. */
