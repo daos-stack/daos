73f5b57182bbbcd178447ee2e866cea0f8da5df9
Phil Henderson <phillip.henderson@intel.com>
2023-08-21 23:54:10 -0400

    Separate HarnessSetupTest tests into two files.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Allow-unstable-test: true
    Test-tag: pr HarnessSetupTest
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/harness/setup_hw.py
+++ src/tests/ftest/harness/setup_hw.py
@@ -1,5 +1,5 @@
 """
-(C) Copyright 2021-2023 Intel Corporation.
+(C) Copyright 2021-2022 Intel Corporation.
 
 SPDX-License-Identifier: BSD-2-Clause-Patent
 """
@@ -12,15 +12,15 @@ class HarnessSetupTest(TestWithServers):
     :avocado: recursive
     """
 
-    def test_setup(self):
+    def test_setup_hw(self):
         """Verify the TestWithServers.setUp() method.
 
         Useful for setting up the /etc/daos/daos_server.yml files on multiple hosts.
 
         :avocado: tags=all
-        :avocado: tags=vm
+        :avocado: tags=hw,medium,large
         :avocado: tags=harness
-        :avocado: tags=HarnessSetupTest,test_setup
+        :avocado: tags=HarnessSetupTest,test_setup_hw
         """
         prepare_timeout = self.params.get('storage_prepare_timeout')
         format_timeout = self.params.get('storage_format_timeout')
