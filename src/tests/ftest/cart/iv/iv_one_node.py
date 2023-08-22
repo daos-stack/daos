ae438a0627b52650878d78c5f0a6fca5c8e0ab96
Ashley Pittman <ashley.m.pittman@intel.com>
2023-08-22 12:05:08 +0000

    Improve failing test.
    Required-githooks: true
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/tests/ftest/cart/iv/iv_one_node.py
+++ src/tests/ftest/cart/iv/iv_one_node.py
@@ -1,8 +1,8 @@
-'''
+"""
   (C) Copyright 2018-2023 Intel Corporation.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
-'''
+"""
 import time
 import tempfile
 import json
@@ -158,7 +158,12 @@ class CartIvOneNodeTest(CartTest):
 
                 # Read the result into test_result and remove the temp file
                 with open(log_path, 'r') as log_file:
-                    test_result = json.load(log_file)
+                    data = log_file.read()
+                    try:
+                        test_result = json.load(data)
+                    except json.JSONDecodeError:
+                        print(data)
+                        raise
 
                 os.close(log_fd)
                 os.remove(log_path)
@@ -480,7 +485,7 @@ class CartIvOneNodeTest(CartTest):
             self._iv_test_actions(clicmd, actions)
         except ValueError as exception:
             failed = True
-            traceback.print_stack()
+            traceback.print_tb(exception)
             self.print("TEST FAILED: %s" % str(exception))
 
         # Shutdown Servers
