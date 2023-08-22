0f7b4b2603b08ea65e6b5d32f383948f8e4fb658
Brian J. Murrell <brian.murrell@intel.com>
2023-08-22 12:14:29 -0400

    Move to functions file
    Test-tag: always_passes always_fails
    PR-repos: daos@PR-11702:153
    Skip-func-test-leap15: false
    
    Required-githooks: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/gha_functions.sh
+++ ci/gha_functions.sh
@@ -22,3 +22,19 @@ cleanup_provision_request () {
        ls -l /scratch/Get\ a\ cluster/"$reqid";
     fi;
 }
+
+cancel_provision() {
+    trap 'rm -f $cookiejar' EXIT
+    cookiejar="$(mktemp)"
+    # shellcheck disable=SC2153
+    crumb="$(curl --cookie-jar "$cookiejar" \
+             "${JENKINS_URL}crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")"
+    # shellcheck disable=SC2001
+    url="$(echo "$QUEUE_URL" | sed -e 's|item/\(.*\)/|cancelItem?id=\1|')"
+    trap 'set -x; rm -f $cookiejar $headers_file' EXIT
+    headers_file="$(mktemp)"
+    if ! curl -D "$headers_file" -v -f -X POST --cookie "$cookiejar" -H "$crumb" "$url"; then
+        echo "Failed to cancel cluster provision request."
+        cat "$headers_file"
+    fi
+}
