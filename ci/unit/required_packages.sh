f953a0ce835bfb32a23f2318b55e614c130ca240
Ashley Pittman <ashley.m.pittman@intel.com>
2023-09-05 09:20:45 +0000

    Change required packages.
    Quick-build: true
    Required-githooks: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/unit/required_packages.sh
+++ ci/unit/required_packages.sh
@@ -37,23 +37,6 @@ pkgs="argobots                         \
       spdk-devel                       \
       valgrind-devel"
 
-if $quick_build; then
-    if ! read -r mercury_version < "$distro"-required-mercury-rpm-version; then
-        echo "Error reading from $distro-required-mercury-rpm-version"
-        if ! ls -l "$distro"-required-mercury-rpm-version; then
-            ls -l
-        fi
-        cat "$distro"-required-mercury-rpm-version
-        exit 1
-    fi
-    pkgs+=" spdk-tools mercury\ \>=\ $mercury_version"
-    pkgs+=" libisa-l_crypto libfabric-debuginfo"
-    pkgs+=" argobots-debuginfo"
-    if [[ "$distro" == *7 ]];then
-        pkgs+=" protobuf-c-debuginfo"
-    fi
-fi
-
 # output with trailing newline suppressed
 echo  -e "$pkgs\c"
 exit 0
