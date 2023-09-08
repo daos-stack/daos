83e855dc902c781b0b4ac7d62fe2203c1d0bcaa7
Colin Howes <chowes@google.com>
2023-09-08 22:37:25 +0000

    Build DAOS as part of GCP presubmits.
    Change-Id: I38c3ed1e5712e7270b01e4f116b4b22150a0fb5a

--- google_ci/presubmit.sh
+++ google_ci/presubmit.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-echo "TODO: build DAOS"
-echo "TODO: run unit tests"
+set -eExo pipefail
+
+function main() {
+  echo "Installing packages..."
+  dnf install -y sudo git virtualenv epel-release dnf-plugins-core
+  dnf config-manager --enable powertools
+  dnf config-manager --save --setopt=assumeyes=True
+
+  echo "Running install script..."
+  utils/scripts/install-el8.sh
+
+  echo "Installing python dependencies..."
+  virtualenv venv
+  source venv/bin/activate
+  pip install --upgrade pip
+  pip install -r requirements.txt
+
+  echo "Building daos..."
+  scons BUILD_TYPE=dev TARGET_TYPE=release --jobs="$(nproc --all)" --build-deps=yes install PREFIX=/opt/daos
+}
+
+main "$@"
