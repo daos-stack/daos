0b100ea7e244426e070587d1202cd0241c9b51ac
Colin Howes <chowes@google.com>
2023-09-12 17:20:15 +0000

    Publish test artifacts to prow GCS
    Change-Id: Ia58529b77ab1c19db2a728607eb5ced283c2e361

--- google_ci/presubmit.sh
+++ google_ci/presubmit.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
-set -eExo pipefail
+set -Exo pipefail
 
-function main() {
+export CMOCKA_XML_FILE="test_results/%g.xml"
+export CMOCKA_MESSAGE_OUTPUT=xml
+
+UTEST_LOG_DIR="/tmp/daos_utest"
+
+function copy_artifacts() {
+  echo "Copying test artifacts to ${ARTIFACTS}"
+  cp config.log "${ARTIFACTS}"
+  cp -r "${UTEST_LOG_DIR}" "${ARTIFACTS}"
+  ls "./test_results"
+  cp -a "./test_results/." "${ARTIFACTS}"
+}
+
+function build_and_run() {
+  set -e
+
   echo "Installing packages..."
-  dnf install -y sudo git virtualenv epel-release dnf-plugins-core
+  dnf install -y sudo git virtualenv epel-release dnf-plugins-core procps-ng
   dnf config-manager --enable powertools
   dnf config-manager --save --setopt=assumeyes=True
 
   echo "Running install script..."
   utils/scripts/install-el8.sh
+
+  echo "Installing go dependencies..."
+  go install gotest.tools/gotestsum@latest
 
   echo "Installing python dependencies..."
   virtualenv venv
@@ -21,7 +39,15 @@ function main() {
   scons BUILD_TYPE=dev TARGET_TYPE=release --jobs="$(nproc --all)" --build-deps=yes install PREFIX=/opt/daos
 
   echo "Running unit tests..."
-  ./utils/run_utest.py
+  ./utils/run_utest.py --log_dir="${UTEST_LOG_DIR}" --sudo=no --suite_filter="[^control]"
+}
+
+function main() {
+  (build_and_run)
+  success=$?
+  copy_artifacts
+
+  return $success
 }
 
 main "$@"
