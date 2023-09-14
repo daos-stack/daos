9f5c089852593e9901d1915b14d28ff5c0d0d1ee
Michael MacDonald <mjmac@google.com>
2023-09-14 14:08:54 -0400

    DAOS-14332 control: Use gRPC metadata for interop (#13018)
    The interoperability checking code relies on getting the
    peer component from the peer certificate. When running
    in insecure mode, the peer component information is
    unavailable, and the interoperability check defaults
    to the most stringent requirements.
    
    This patch adds new component/version headers to the
    grpc client request to allow the server to perform
    interoperability checks without the peer certificate.
    
    Signed-off-by: Michael MacDonald <mjmac@google.com>

---
+++ src/control/common/proto/consts.go
@@ -0,0 +1,14 @@
+//
+// (C) Copyright 2023 Intel Corporation.
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+//
+
+package proto
+
+const (
+	// DaosComponentHeader defines the header name used to convey the component name.
+	DaosComponentHeader = "x-daos-component"
+	// DaosVersionHeader defines the header name used to convey the component version.
+	DaosVersionHeader = "x-daos-version"
+)
