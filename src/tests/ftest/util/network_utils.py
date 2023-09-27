995fe52a40d86f5764908d13c034b85e2d0eeaa4
Joseph Moore <joseph.moore@intel.com>
2023-09-27 14:51:54 +0000

    DAOS-14418 cart: Retry am_send for UCX out-of-memory error.
    Skip-build-leap15-icc: true
    Skip-build-el8-gcc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Test-tag: pr daily_regression
    PR-repos: mercury@PR-107:1
    Test-provider: ucx+dc_x
    Skip-func-hw-test-medium-ucx-provider: false
    Skip-func-hw-test-medium-verbs-provider: true
    
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/tests/ftest/util/network_utils.py
+++ src/tests/ftest/util/network_utils.py
@@ -13,8 +13,8 @@ from ClusterShell.NodeSet import NodeSet
 from exception_utils import CommandFailure
 from general_utils import run_task, display_task, run_pcmd
 
-SUPPORTED_PROVIDERS = ("ofi+sockets", "ofi+tcp;ofi_rxm", "ofi+verbs;ofi_rxm", "ucx+dc_x", "ucx+ud_x", "ofi+cxi")
-
+SUPPORTED_PROVIDERS = ("ofi+sockets", "ofi+tcp;ofi_rxm", "ofi+verbs;ofi_rxm",
+                       "ucx+dc_x", "ucx+ud_x", "ofi+cxi")
 
 class NetworkDevice():
     """A class to represent the information of a network device."""
