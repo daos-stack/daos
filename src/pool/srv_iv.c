bd378ba0382b45f8e8dbf10d720cfc46e21adc95
Di Wang <di.wang@intel.com>
2023-10-06 21:01:40 +0000

    DAOS-14473 pool: init pool iv key for invalidate
    Initialize pool iv key for ds_iv_invalidate, in case
    the pool srv handle IV invalidate before pool srv handle
    IV update, i.e. iv entry is not created yet.
    
    Required-githooks: true
    
    Signed-off-by: Di Wang <di.wang@intel.com>

--- src/pool/srv_iv.c
+++ src/pool/srv_iv.c
@@ -1419,9 +1419,14 @@ int
 ds_pool_iv_srv_hdl_invalidate(struct ds_pool *pool)
 {
 	struct ds_iv_key	key = { 0 };
+	struct pool_iv_key	*pool_key;
 	int			rc;
 
 	key.class_id = IV_POOL_HDL;
+	pool_key = (struct pool_iv_key *)key.key_buf;
+	pool_key->pik_entry_size = pool_iv_len;
+	pool_key->pik_eph = d_hlc_get();
+	pool_key->pik_term = ns->iv_master_term;
 	rc = ds_iv_invalidate(pool->sp_iv_ns, &key, CRT_IV_SHORTCUT_NONE,
 			      CRT_IV_SYNC_NONE, 0, false /* retry */);
 	if (rc)
