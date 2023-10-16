29b4c7c63215f6cb4f0f0a9e6d4c02e2d3153b42
Makito Kano <makito.kano@intel.com>
2023-10-17 03:19:07 +0900

    DAOS-11742 test: CR Pass 3 - Corrupt label in PS (#13131)
    1. Create a pool.
    2. Mangle the label in the PS metadata.
    3. Call dmg pool get-prop TestLabel_1 label and verify that the label value is
    TestLabel_1-fault.
    4. Stop servers and enable the checker.
    5. Set checker policy to --all-interactive.
    6. Start the checker and query the checker until the fault is detected.
    7. Repair with option 0; Trust MS pool label.
    8. Query the checker until the fault is repaired.
    9. Disable the checker and restart servers.
    10. Wait for all ranks to join.
    11. Call dmg pool get-prop mkp1 label and verify that the original label is
    restored.
    
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/recovery/pool_cleanup.py
+++ src/tests/ftest/recovery/pool_cleanup.py
@@ -155,3 +155,112 @@ class PoolCleanupTest(TestWithServers):
             errors.append(msg)
 
         report_errors(test=self, errors=errors)
+
+    def test_corrupt_label_ps(self):
+        """Test corrupt label in PS.
+
+        1. Create a pool.
+        2. Mangle the label in the PS metadata.
+        3. Call dmg pool get-prop TestLabel_1 label and verify that the label value is
+        TestLabel_1-fault.
+        4. Stop servers and enable the checker.
+        5. Set checker policy to --all-interactive.
+        6. Start the checker and query the checker until the fault is detected.
+        7. Repair with option 0; Trust MS pool label.
+        8. Query the checker until the fault is repaired.
+        9. Disable the checker and restart servers.
+        10. Wait for all ranks to join.
+        11. Call dmg pool get-prop mkp1 label and verify that the original label is
+        restored.
+
+        Jira ID: DAOS-11742
+
+        :avocado: tags=all,pr
+        :avocado: tags=vm
+        :avocado: tags=recovery,pool_cleanup
+        :avocado: tags=PoolCleanupTest,test_corrupt_label_ps
+        """
+        # 1. Create a pool.
+        self.log_step("Create a pool")
+        pool = self.get_pool(connect=False)
+
+        # 2. Mangle the label in the PS.
+        self.log_step("Mangle the label in the PS")
+        dmg_command = self.get_dmg_command()
+        dmg_command.faults_pool_svc(
+            pool=pool.identifier, checker_report_class="CIC_POOL_BAD_LABEL")
+
+        # 3. Call dmg pool get-prop TestLabel_1 label and verify that the label value is
+        # TestLabel_1-fault.
+        self.log_step("Check that the label in PS is corrupted with -fault added")
+        ps_label = pool.get_property(prop_name="label")
+        errors = []
+        expected_label = pool.identifier + "-fault"
+        if ps_label != expected_label:
+            msg = (f"Unexpected label in PS! Expected = {expected_label}; Actual = "
+                   f"{ps_label}")
+            errors.append(msg)
+
+        # 4. Stop the servers and enable the checker.
+        self.log_step("Stop the servers and enable the checker.")
+        dmg_command.check_enable()
+
+        # 5. Set the policy to --all-interactive.
+        self.log_step("Set the policy to --all-interactive.")
+        dmg_command.check_set_policy(all_interactive=True)
+
+        # 6. Start the checker and query the checker until the fault is detected.
+        self.log_step("Start and query the checker until the fault is detected.")
+        seq_num = None
+        # Start checker.
+        dmg_command.check_start()
+        # Query the checker until the label inconsistency is detected.
+        for _ in range(8):
+            check_query_out = dmg_command.check_query()
+            # Status is INIT before starting the checker.
+            if check_query_out["response"]["status"] == "RUNNING":
+                seq_num = check_query_out["response"]["reports"][0]["seq"]
+                break
+            time.sleep(5)
+        if not seq_num:
+            self.fail("Checker didn't detect any fault!")
+
+        # 7. Repair with option 0; Trust MS pool label.
+        self.log_step("Repair with option 0; Trust MS pool label.")
+        dmg_command.check_repair(seq_num=seq_num, action=0)
+
+        # 8. Query the checker until the fault is repaired.
+        self.log_step("Query the checker until the fault is repaired.")
+        repair_report = self.wait_for_check_complete()[0]
+
+        # Verify that the repair report has expected message "Reset the pool property
+        # using the MS label".
+        action_message = repair_report["act_msgs"][0]
+        exp_msg = "Reset the pool property using the MS label"
+        errors = []
+        if exp_msg not in action_message:
+            errors.append(f"{exp_msg} not in {action_message}!")
+
+        # 9. Call dmg check disable and restart the servers.
+        self.log_step("Call dmg check disable and restart the servers.")
+        dmg_command.check_disable()
+
+        # 10. Wait for all ranks to join.
+        self.log_step("Wait for all ranks to join.")
+        rank_list = self.server_managers[0].get_host_ranks(hosts=self.hostlist_servers)
+        failed_ranks = self.server_managers[0].check_rank_state(
+            ranks=rank_list, valid_states="joined", max_checks=10)
+        if failed_ranks:
+            errors.append(
+                f"Following ranks didn't become joined after restart! {failed_ranks}")
+
+        # 11. Call dmg pool get-prop TestPool_1 label and verify that the original label
+        # is restored in PS.
+        self.log_step("Verify that the corrupted label in PS is fixed.")
+        ps_label = pool.get_property(prop_name="label")
+        if ps_label != pool.identifier:
+            msg = (f"Label in PS hasn't been repaired! Expected = "
+                   f"{pool.identifier}; Actual = {ps_label}")
+            errors.append(msg)
+
+        report_errors(test=self, errors=errors)
