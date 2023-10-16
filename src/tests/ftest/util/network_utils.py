cda39b60de8f7c08c8c0694165ca8959aaac5c19
Joseph Moore <joseph.moore@intel.com>
2023-10-16 14:57:38 +0000

    DAOS-14480 test: Extend Python code to support UCX with ud_x.
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/tests/ftest/util/network_utils.py
+++ src/tests/ftest/util/network_utils.py
@@ -14,7 +14,7 @@ from exception_utils import CommandFailure
 from general_utils import run_task, display_task, run_pcmd
 
 SUPPORTED_PROVIDERS = ("ofi+sockets", "ofi+tcp;ofi_rxm", "ofi+verbs;ofi_rxm",
-                       "ucx+dc_x", "ucx+ud_x", ofi+cxi")
+                       "ucx+dc_x", "ucx+ud_x", "ofi+cxi")
 
 
 class NetworkDevice():
