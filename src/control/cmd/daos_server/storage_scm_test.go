94a812e28c7c0393acb0844f7bbd2547840f7cc8
Tom Nabarro <tom.nabarro@intel.com>
2023-10-23 13:57:55 +0100

    fix unit test failure
    Features: control
    Required-githooks: true
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/cmd/daos_server/storage_scm_test.go
+++ src/control/cmd/daos_server/storage_scm_test.go
@@ -346,7 +346,7 @@ func TestDaosServer_resetPMem(t *testing.T) {
 				RebootRequired: true,
 			},
 			expCalls: []storage.ScmPrepareRequest{
-				{Reset: true, SocketID: &one},
+				{Reset: true, Force: true, SocketID: &one},
 			},
 			expLogMsg: "regions will be removed",
 		},
@@ -453,7 +453,7 @@ func TestDaosServer_resetPMem(t *testing.T) {
 
 			if tc.expCalls == nil {
 				tc.expCalls = []storage.ScmPrepareRequest{
-					{Reset: true},
+					{Reset: true, Force: !tc.noForce},
 				}
 			}
 
