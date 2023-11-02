3f57078c1ae70c2182bf359838bb56c83c72180e
Phil Henderson <phillip.henderson@intel.com>
2023-11-02 14:42:53 -0400

    Adding debug for detecting server start.
    Test-tag: test_daos_vol_bigio metadata
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/job_manager_utils.py
+++ src/tests/ftest/util/job_manager_utils.py
@@ -1068,6 +1068,10 @@ class Systemctl(JobManager):
             if timeout is not None:
                 timed_out = duration > timeout
 
+            self.log.info(
+                "Searching for '%s' status: detected=%s/%s, timed_out=%ss/%ss, complete=%s",
+                pattern, detected, quantity, duration, timeout, complete)
+
             if verbose:
                 self.display_log_data(log_data)
 
