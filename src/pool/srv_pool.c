5c858f3823afef748fe51fa12c90ceb97438ef04
Alexander A Oganezov <alexander.a.oganezov@intel.com>
2023-11-17 20:12:03 +0000

    Reverting to avoid conflicts with other prs
    Required-githooks: true
    
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/pool/srv_pool.c
+++ src/pool/srv_pool.c
@@ -2741,7 +2741,7 @@ ds_pool_create_handler(crt_rpc_t *rpc)
 
 	rc = pool_prop_default_copy(prop_dup, prop);
 	if (rc) {
-		DL_ERROR(rc, "daos_prop_default_copy failed.");
+		DL_ERROR(rc, "daos_prop_default_copy failed.\n");
 		D_GOTO(out_tx, rc);
 	}
 
