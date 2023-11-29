bc07aaa955b6a2b1f6bb6e7fbad7ee7dda3aeab8
Phil Henderson <phillip.henderson@intel.com>
2023-11-29 14:41:33 -0500

    Apply feedback and fixes.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Allow-unstable-test: true
    Skip-func-hw-test: true
    
    Test-tag: test_always_passes
    Features: test_setup_vm
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- ci/get_commit_message.py
+++ ci/get_commit_message.py
@@ -91,13 +91,13 @@ def modify_commit_message_pragmas(commit_message):
     feature_values = get_pragma_values(commit_message, 'Features')
     if feature_values:
         # Add daily and weekly feature tests when 'Features' exists
-        for feature in feature_values.split(' '):
-            test_tags.update(f'daily_regression,{feature}')
-            test_tags.update(f'full_regression,{feature}')
+        for feature in filter(None, feature_values.split(' ')):
+            test_tags.add(f'daily_regression,{feature}')
+            test_tags.add(f'full_regression,{feature}')
 
-    # Replace 'Features:' with 'Test-tags:'
+    # Replace 'Features:' with 'Test-tag:'
     commit_message = update_commit_pragma(commit_message, 'Features', None)
-    commit_message = update_commit_pragma(commit_message, 'Test-tag', test_tags)
+    commit_message = update_commit_pragma(commit_message, 'Test-tag', ' '.join(test_tags))
 
     if 'Test-tag-hw-medium-md-on-ssd' not in commit_message:
         # Add tags for MD on SSD testing
