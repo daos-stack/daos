d525cdc4587ba01a02c29ab61039b1f72d093f18
Ashley Pittman <ashley.m.pittman@intel.com>
2024-01-05 15:04:34 +0000

    Move some packages.
    Skip-fault-injection-test: true
    Required-githooks: true
    Skip-unit-tests: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/provisioning/post_provision_config.sh
+++ ci/provisioning/post_provision_config.sh
@@ -27,11 +27,7 @@ DSL_REPO_var="DAOS_STACK_${DISTRO}_LOCAL_REPO"
 DSG_REPO_var="DAOS_STACK_${DISTRO}_GROUP_REPO"
 DSA_REPO_var="DAOS_STACK_${DISTRO}_APPSTREAM_REPO"
 
-retry_cmd 300 clush -B -S -l root -w "$NODESTRING" --copy ci_key* --dest=/tmp/
-
-retry_cmd 300 clush -B -S -l root -w "$NODESTRING" --copy utils/cq/requirements.txt --dest=/tmp/cq-req.txt
-
-retry_cmd 300 clush -B -S -l root -w "$NODESTRING" --copy requirements.txt --dest=/tmp/
+retry_cmd 300 clush -B -S -l root -w "$NODESTRING" --copy requirements-ftest.txt ci_key* --dest=/tmp/
 
 # shellcheck disable=SC2001
 sanitized_commit_message="$(echo "$COMMIT_MESSAGE" | sed -e 's/\(["\$]\)/\\\1/g')"
