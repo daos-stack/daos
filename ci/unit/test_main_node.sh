66104b1f32a408159ae336831c09b71047793a03
Ashley Pittman <ashley.m.pittman@intel.com>
2024-01-09 07:45:30 +0000

    Do not configure pip on 3.6
    Skip-func-test-leap15: False
    Test-tag: test_dfuse_daos_build_wt_il
    Skip-fault-injection-test: false
    Required-githooks: true
    Skip-unit-tests: false
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- ci/unit/test_main_node.sh
+++ ci/unit/test_main_node.sh
@@ -77,9 +77,9 @@ rm -rf "$test_log_dir"
 python3 -m venv venv
 # shellcheck disable=SC1091
 source venv/bin/activate
-touch venv/pip.conf
-pip config set global.progress_bar off
-pip config set global.no_color true
+# touch venv/pip.conf
+# pip config set global.progress_bar off
+# pip config set global.no_color true
 
 pip install --upgrade pip
 pip install --requirement requirements-utest.txt
