0350399bbb9f7379e4ac1795b544112f25af2a7f
Phil Henderson <phillip.henderson@intel.com>
2024-01-12 15:10:10 -0500

    DAOS-14969 test: Increase crt_timeout for test_daos_oid_allocator
    Temporarily increase the crt_timeout for the test_daos_oid_allocator
    test to 60 seconds.
    
    Test-repeat: 10
    Test-tag: test_daos_oid_allocator test_daos_management
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/daos_core_base.py
+++ src/tests/ftest/util/daos_core_base.py
@@ -96,6 +96,14 @@ class DaosCoreBase(TestWithServers):
                         ["=".join(items) for items in list(env_dict.items())]
                     )
 
+        # Update any other server settings unique to this test method
+        for setting in ("crt_timeout"):
+            value = self.get_test_param(setting)
+            if value:
+                for server_mgr in self.server_managers:
+                    for engine_params in server_mgr.manager.job.yaml.engine_params:
+                        engine_params.set_value(setting, value)
+
         # Start the servers
         return super().start_server_managers(force=force)
 
