36e6fa8f597fdb2f695f8988599fd40d16750d04
Ashley Pittman <ashley.m.pittman@intel.com>
2024-01-15 10:04:11 +0000

    DAOS-14824 dfuse: Allow parallel readdir calls.
    Remove the inode lock preventing parallel readdir calls.
    Update readdir caching to be thread safe.
    Required-githooks: true
    
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/client/dfuse/dfuse_core.c
+++ src/client/dfuse/dfuse_core.c
@@ -1110,8 +1110,6 @@ dfuse_ie_init(struct dfuse_info *dfuse_info, struct dfuse_inode_entry *ie)
 	atomic_init(&ie->ie_open_write_count, 0);
 	atomic_init(&ie->ie_il_count, 0);
 	atomic_fetch_add_relaxed(&dfuse_info->di_inode_count, 1);
-
-	D_MUTEX_INIT(&ie->ie_lock, NULL);
 }
 
 void
@@ -1145,8 +1143,6 @@ dfuse_ie_close(struct dfuse_info *dfuse_info, struct dfuse_inode_entry *ie)
 
 		d_hash_rec_decref(&dfp->dfp_cont_table, &dfc->dfs_entry);
 	}
-
-	D_MUTEX_DESTROY(&ie->ie_lock);
 
 	dfuse_ie_free(dfuse_info, ie);
 }
