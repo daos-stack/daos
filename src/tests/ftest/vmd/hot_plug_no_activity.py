bb9192faf36e4451470d407175e0ed46d0c47bd7
Makito Kano <makito.kano@intel.com>
2024-01-22 06:51:23 +0000

    DAOS-15008 test: Support multiple engines per rank when obtaining NVMe info
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: test_no_activity
    Required-githooks: true
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/vmd/hot_plug_no_activity.py
+++ src/tests/ftest/vmd/hot_plug_no_activity.py
@@ -16,7 +16,7 @@ class HotPlugNoActivityTest(IorTestBase):
     :avocado: recursive
     """
     def repeat_query_list_devices(self, exp_disk_state, exp_led_state, error_msg, errors,
-                                  dmg_command):
+                                  dmg_command, rank):
         """Repeatedly call dmg storage query list-devices verify the states.
 
         Args:
@@ -25,16 +25,20 @@ class HotPlugNoActivityTest(IorTestBase):
             error_msg (str): Error message to add to the error list.
             errors (list): Error list for this test.
             dmg_command (DmgCommand): DmgCommand object to call the command.
+            rank (int): Rank where the NVMe disk we want to access is located.
         """
         removed_state = False
         for count in range(2):
             time.sleep(30)
             device_info = get_storage_query_device_info(dmg=dmg_command)
-            self.log.info("device_info = %s", device_info)
-            # We'll use the first device.
-            device_control = device_info[0]["ctrlr"]
-            dev_state = device_control["dev_state"]
-            led_state = device_control["led_state"]
+            self.log.info("device_info (repeat_query_list_devices) = %s", device_info)
+            dev_state = None
+            led_state = None
+            for device in device_info:
+                if device["rank"] == rank:
+                    device_control = device["ctrlr"]
+                    dev_state = device_control["dev_state"]
+                    led_state = device_control["led_state"]
             if dev_state == exp_disk_state and led_state == exp_led_state:
                 removed_state = True
                 break
@@ -42,29 +46,34 @@ class HotPlugNoActivityTest(IorTestBase):
         if not removed_state:
             errors.append(error_msg)
 
-    def get_total_nvme_bytes(self, dmg_command):
+    def get_total_nvme_bytes(self, dmg_command, host_port, rank):
         """Get total NVMe bytes using 'dmg storage query usage'
 
         Args:
             dmg_command (DmgCommand): DmgCommand object used to call the command and to obtain the
                 port number.
+            host_port (str): Host where the NVMe disk we want to access is located. <host>:<port>
+            rank (int): Rank where the NVMe disk we want to access is located.
 
         Returns:
-            int: total_bytes value of the first NVMe device in the server.
+            int: total_bytes value of the NVMe device we're interested, which is specified by
+                host_port and rank parameter. If not found, for example because of invalid host_port
+                rank combination, returns None.
 
         """
+        total_bytes = None
         usage_out = dmg_command.storage_query_usage()
-        hosts_to_smd_devices = {}
         # There's a hash value under HostStorage. Obtain "storage" -> "nvme_devices" and "hosts"
-        # under it. There may be multiple hosts depending on the setup, so use "hosts" as key and
-        # "smd_devices" list as value.
-        for value in usage_out["response"]["HostStorage"].values():
-            smd_devices = value["storage"]["nvme_devices"][0]["smd_devices"]
-            hosts_to_smd_devices[value["hosts"]] = smd_devices
-        self.log.info("hosts_to_smd_devices = %s", hosts_to_smd_devices)
-        port_num = dmg_command.yaml.port.value
-        host_port = f"{str(self.hostlist_servers)}:{port_num}"
-        total_bytes = hosts_to_smd_devices[host_port][0]["total_bytes"]
+        # under it. There may be multiple hosts depending on the setup.
+        for hash_value in usage_out["response"]["HostStorage"].values():
+            if hash_value["hosts"] == host_port:
+                for nvme_device in hash_value["storage"]["nvme_devices"]:
+                    # In HW medium cluster, there's only one dictionary in the smd_devices list, so
+                    # we may be able to just index it, but use for loop just in case.
+                    for smd_device in nvme_device["smd_devices"]:
+                        if smd_device["rank"] == rank:
+                            total_bytes = smd_device["total_bytes"]
+        self.log.info("total_bytes = %s", total_bytes)
         return total_bytes
 
     def test_no_activity(self):
@@ -102,13 +111,23 @@ class HotPlugNoActivityTest(IorTestBase):
         dmg_command = self.get_dmg_command()
         device_info = get_storage_query_device_info(dmg=dmg_command)
         self.log.info("device_info = %s", device_info)
-        # We'll use the first device.
-        device_control = device_info[0]["ctrlr"]
-        pci_addr = device_control["pci_addr"]
-        dev_state = device_control["dev_state"]
-        led_state = device_control["led_state"]
-        self.log.info(
-            "pci_addr = %s; dev_state = %s; led_state = %s", pci_addr, dev_state, led_state)
+        pci_addr = None
+        dev_state = None
+        led_state = None
+        host_port = None
+        rank = 0
+        # We'll use the rank 0 device.
+        for device in device_info:
+            if device["rank"] == rank:
+                device_control = device["ctrlr"]
+                pci_addr = device_control["pci_addr"]
+                dev_state = device_control["dev_state"]
+                led_state = device_control["led_state"]
+                host_port = device["hosts"]
+                break
+        msg = (f"pci_addr = {pci_addr}; dev_state = {dev_state}; led_state = {led_state}; "
+               f"host_port = {host_port}")
+        self.log.info(msg)
         errors = []
         exp_disk_state = "NORMAL"
         if dev_state != exp_disk_state:
@@ -120,7 +139,8 @@ class HotPlugNoActivityTest(IorTestBase):
 
         # 2. Store the total NVMe size.
         self.log_step("Store the total NVMe size.")
-        total_bytes_orig = self.get_total_nvme_bytes(dmg_command=dmg_command)
+        total_bytes_orig = self.get_total_nvme_bytes(
+            dmg_command=dmg_command, host_port=host_port, rank=rank)
 
         # 3. Create a pool and a container.
         self.log_step("Create a pool and a container")
@@ -137,8 +157,6 @@ class HotPlugNoActivityTest(IorTestBase):
         # command = f"sudo python3 /home/mkano/daos/build/external/debug/spdk/scripts/rpc.py -s /var/tmp/spdk_0.sock vmd_remove_device {pci_addr}"
         # rpc_out = run_pcmd(hosts=self.hostlist_servers, command=command)
         # self.log.debug(f"## Hot remove out = {rpc_out}")
-        # self.log.debug("## Sleep for 30 sec...")
-        # time.sleep(30)
         # stdout contains the output as list. Each item in the list represents a line. e.g.,
         # '[',
         # '  {',
@@ -169,21 +187,25 @@ class HotPlugNoActivityTest(IorTestBase):
         # msg = ("Repeatedly call 'dmg storage query list-devices' until the disk state becomes "
         #        "UNPLUGGED and LED becomes NA.")
         # self.log_step(msg)
-        # error_msg = "Disk and LED state didn't turn to removed state. (UNPLUGGED and NA)"
+        # error_msg = "Disk and LED state didn't turn to removed state. (UNPLUGGED and OFF)"
+        # self.repeat_query_list_devices(
+        #     exp_disk_state="UNPLUGGED", exp_led_state="OFF", error_msg=error_msg, errors=errors,
+        #     dmg_command=dmg_command, rank=rank)
 
         # 7. Verify that the disk space is down compared to before the remove.
-        self.log_step("Verify that the disk space is down compared to before the remove.")
-        total_bytes_hot_remove = self.get_total_nvme_bytes(dmg_command=dmg_command)
-        if total_bytes_hot_remove >= total_bytes_orig:
-            msg = (f"Total NVMe bytes haven't been reduced after a hot remove! "
-                   f"Original = {total_bytes_orig}; Hot-removed = {total_bytes_hot_remove}")
-            # errors.append(msg)
+        # self.log_step("Verify that the disk space is down compared to before the remove.")
+        # total_bytes_hot_remove = self.get_total_nvme_bytes(
+        #     dmg_command=dmg_command, host_port=host_port, rank=rank)
+        # if total_bytes_hot_remove >= total_bytes_orig:
+        #     msg = (f"Total NVMe bytes haven't been reduced after a hot remove! "
+        #            f"Original = {total_bytes_orig}; Hot-removed = {total_bytes_hot_remove}")
+        #     errors.append(msg)
 
-        # # 8. Hot-plug.
-        # # self.log_step("Hot-plug.")
-        # # command = f"sudo python3 /home/mkano/daos/build/external/debug/spdk/scripts/rpc.py -s /var/tmp/spdk_0.sock vmd_rescan"
-        # # rpc_out = run_pcmd(hosts=self.hostlist_servers, command=command)
-        # # self.log.debug(f"## Hot plug out = {rpc_out}")
+        # 8. Hot-plug.
+        # self.log_step("Hot-plug.")
+        # command = f"sudo python3 /home/mkano/daos/build/external/debug/spdk/scripts/rpc.py -s /var/tmp/spdk_0.sock vmd_rescan"
+        # rpc_out = run_pcmd(hosts=self.hostlist_servers, command=command)
+        # self.log.debug(f"## Hot plug out = {rpc_out}")
 
         # 9. Repeatedly call "dmg storage query list-devices" until the disk state becomes NORMAL
         # and LED becomes OFF.
@@ -193,11 +215,12 @@ class HotPlugNoActivityTest(IorTestBase):
         error_msg = "Disk and LED state didn't turn to plugged state. (NORMAL and OFF)"
         self.repeat_query_list_devices(
             exp_disk_state="NORMAL", exp_led_state="OFF", error_msg=error_msg, errors=errors,
-            dmg_command=dmg_command)
+            dmg_command=dmg_command, rank = rank)
 
         # 10. Verify that the disk space is back to the original.
         self.log_step("Verify that the disk space is back to the original.")
-        total_bytes_hot_plug = self.get_total_nvme_bytes(dmg_command=dmg_command)
+        total_bytes_hot_plug = self.get_total_nvme_bytes(
+            dmg_command=dmg_command, host_port=host_port, rank=rank)
         if total_bytes_hot_plug != total_bytes_orig:
             msg = (f"Total NVMe bytes haven't been recovered! Original = {total_bytes_orig}; "
                    f"After hot plug = {total_bytes_hot_plug}")
