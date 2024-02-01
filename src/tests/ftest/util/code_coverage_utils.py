6edb64538c3688d13625885a24a3420758e8bd82
Ding Ho <ding-hwa.ho@intel.com>
2024-02-01 21:56:27 +0000

    Update code_coverage_utils.py per Phil's comment.
    Skip-fnbullseye: false
    Skip-bullseye: false
    Skip-python-bandit: true
    Skip-build-EL9-rpm: true
    Allow-unstable-test: true
    Doc-only: false
    Test-tag: test_basic_aggregation
    Required-githooks: true
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/util/code_coverage_utils.py
+++ src/tests/ftest/util/code_coverage_utils.py
@@ -60,16 +60,13 @@ class CodeCoverage():
 
         logger.debug("Removing any existing %s file", self.__test_env.bullseye_file)
         command = ["rm", "-fr", self.__test_env.bullseye_file]
-        command = ["ls", self.__test_env.bullseye_file]
         if not run_remote(logger, self.__hosts, " ".join(command)).passed:
             message = "Error removing bullseye code coverage file on at least one host"
             result.fail_test(logger, "Run", message, None)
             return False
 
-        logger.debug(
-            "Copying %s bullseye code coverage source file", self.__test_env.bullseye_src)
-#        command = ["cp", self.__test_env.bullseye_src, self.__test_env.bullseye_file]
-        command = ["touch", self.__test_env.bullseye_file]
+        logger.debug("Copying %s bullseye code coverage source file", self.__test_env.bullseye_src)
+        command = ["cp", self.__test_env.bullseye_src, self.__test_env.bullseye_file]
         if not run_remote(logger, self.__hosts, " ".join(command)).passed:
             message = "Error copying bullseye code coverage file on at least one host"
             result.fail_test(logger, "Run", message, None)
