7237e1194ad1ec29ffa02f5510a351290850aa01
Jan Michalski <jan.michalski@intel.com>
2024-02-05 11:46:35 -0500

    DAOS-11404 dtx: Introduce local transactions (fix)
    - add RDB update/discard/update test
    
    Features: daosio
    
    Required-githooks: true
    
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

--- src/dtx/tests/dts_local_rdb.c
+++ src/dtx/tests/dts_local_rdb.c
@@ -48,8 +48,52 @@ ut_rdb_mc(void **state)
 	DTS_FETCH_EXISTING(coh, la, DKEY_ID0, test_data_2);
 }
 
+static void
+ut_rdb_lc_discard(void **state)
+{
+	struct io_test_args   *arg = *state;
+	daos_handle_t          coh = arg->ctx.tc_co_hdl;
+	struct dts_local_args *la  = (struct dts_local_args *)arg->custom;
+
+	struct dtx_handle     *dth         = NULL;
+	const char            *test_data_1 = "Hello";
+	const char            *test_data_2 = "Bye";
+	daos_epoch_t           start_epoch = la->epoch;
+	daos_epoch_range_t     epr;
+	int                    rc;
+
+	dts_print_start_message();
+
+	/* populate two epochs */
+	dth = dts_local_begin(arg->ctx.tc_po_hdl, DTX_SUB_MOD_MAX);
+	dts_update(coh, la, DKEY_ID0, test_data_1, dth);
+	++la->epoch;
+	dts_update(coh, la, DKEY_ID0, test_data_2, dth);
+	dts_local_commit(dth);
+	DTS_FETCH_EXISTING(coh, la, DKEY_ID0, test_data_2);
+
+	/* discard these epochs */
+	DTS_PRINT("- discard epoch(s) range <%" PRIu64 ", %" PRIu64 "> (rc=%d)", start_epoch,
+		  la->epoch, 0);
+	epr.epr_lo = start_epoch;
+	epr.epr_hi = la->epoch;
+	rc         = vos_discard(coh, NULL /* oidp */, &epr, NULL, NULL);
+	assert_rc_equal(rc, 0);
+	DTS_FETCH_NON_EXISTING(coh, la, DKEY_ID0);
+
+	/* re-populate discarded epochs with different data */
+	la->epoch = start_epoch;
+	dth       = dts_local_begin(arg->ctx.tc_po_hdl, DTX_SUB_MOD_MAX);
+	dts_update(coh, la, DKEY_ID0, test_data_2, dth);
+	++la->epoch;
+	dts_update(coh, la, DKEY_ID0, test_data_1, dth);
+	dts_local_commit(dth);
+	DTS_FETCH_EXISTING(coh, la, DKEY_ID0, test_data_1);
+}
+
 static const struct CMUnitTest tests_all[] = {
     BASIC_UT(200, "RDB's MC update scheme", ut_rdb_mc),
+    BASIC_UT(201, "RDB's LC update/discard/update", ut_rdb_lc_discard),
 };
 
 int
