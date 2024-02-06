aa0fc3cd28071e376f28bbb19db3a4dc33115cae
Phil Henderson <phillip.henderson@intel.com>
2024-02-06 18:25:56 -0500

    DAOS-14631 test: Adding WAL/Meta SSD fault/reintegration tests
    Also fixing test_dmg_storage_query_device_state to handle setting SysXS
    devices faulty.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-func-hw-test-medium-md-on-ssd: false
    Allow-unstable-test: true
    Test-tag: test_dmg_storage_query_device_state
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/control/dmg_storage_query.py
+++ src/tests/ftest/control/dmg_storage_query.py
@@ -227,17 +227,33 @@ class DmgStorageQuery(ControlTestBase):
         :avocado: tags=control,dmg,storage_query,basic
         :avocado: tags=DmgStorageQuery,test_dmg_storage_query_device_state
         """
+        expect_failed_engine = False
+
         # Get device info and check state is NORMAL
         device_info = get_storage_query_device_info(self.dmg)
         self.check_dev_state(device_info, "NORMAL")
 
         # Set device to faulty state and check that it's in FAULTY state
         for device in device_info:
+            if str(device['has_sys_xs']).lower() == 'true':
+                # Setting a SysXS device faulty will kill the engine
+                self.log.debug("Expecting server to die after setting SysXS device faulty")
+                self.server_managers[0].update_expected_states(0, ["Stopped", "Excluded"])
+                expect_failed_engine = True
             try:
                 self.dmg.storage_set_faulty(uuid=device['uuid'])
             except CommandFailure:
                 self.fail("Error setting the faulty state for {}".format(device['uuid']))
 
         # Check that devices are in FAULTY state
-        device_info = get_storage_query_device_info(self.dmg)
-        self.check_dev_state(device_info, "EVICTED")
+        try:
+            device_info = get_storage_query_device_info(self.dmg)
+            self.check_dev_state(device_info, "EVICTED")
+        except CommandFailure as error:
+            if not expect_failed_engine:
+                raise
+            if "DAOS I/O Engine instance not started or not responding on dRPC" not in str(error):
+                self.log.debug(error)
+                self.fail("dmg storage query list-devices failed for an unexpected reason")
+
+        self.log.info("Test passed")
