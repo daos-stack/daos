8df1dbcc946c48a042edcf4ac50b957ffabe2173
Ding Ho <ding-hwa.ho@intel.com>
2024-02-08 18:41:47 +0000

    DAOS-15150 test: To implement bullseye_generate_report and fix error
    Update bullseye_generate_report.sh
    
    Skip-fnbullseye: false
    Skip-bullseye: false
    Skip-python-bandit: true
    Skip-build-EL9-rpm: true
    Allow-unstable-test: true
    Doc-only: false
    Test-tag: test_aggregation_single_pool test_basic_aggregation test_daos_management test_single_object_with_checksum
    Required-githooks: true
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- ci/bullseye_generate_report.sh
+++ ci/bullseye_generate_report.sh
@@ -3,21 +3,35 @@
 set -eux
 
 if [ ! -d '/opt/BullseyeCoverage/bin' ]; then
-  echo 'Bullseye not found.'
+  echo '#Bullseye not found.'
   exit 1
 fi
 export COVFILE="$WORKSPACE/test.cov"
 export PATH="/opt/BullseyeCoverage/bin:$PATH"
 
-mv "$WORKSPACE/test.cov_1" "$COVFILE"
-if [ -e "$WORKSPACE/test.cov_2" ]; then
-  covmerge --no-banner --file "$COVFILE" "$WORKSPACE"/test.cov_*
-fi
+# Decompress any zipped bullseye files, e.g.
+#   $WORKSPACE/Functional */bullseye_coverage_logs/test.*.cov.bz2
+find "$WORKSPACE" -maxdepth 3 -type f -name 'test.*.cov.bz2' \
+  -print0 | sudo -n xargs -0 -r0 lbunzip2 -v -k
+
+# Merge all of the bullseye files into one, e.g.
+#   $WORKSPACE/covc_test_logs/test.cov
+#   $WORKSPACE/Functional */bullseye_coverage_logs/test.*.cov
+echo "Merging the following bullseye files"
+find "$WORKSPACE" -maxdepth 3 -type f -name 'test*.cov'
+find "$WORKSPACE" -maxdepth 3 -type f -name 'test*.cov' \
+  -print0 | sudo -n xargs -0 -r0 covmerge --no-banner --create --file "$COVFILE"
+
+# Remove decompressed bullseye files after merge
+find "$WORKSPACE" -maxdepth 3 -type f -name 'test.*.cov' -print -delete
 
 if [ ! -e "$COVFILE" ]; then
-  echo "Coverage file $COVFILE is missing"
+  echo "#Coverage file $COVFILE is missing"
 else
   ls -l "$COVFILE"
+  mkdir -p "$WORKSPACE"/test_coverage_report/
 fi
 
-java -jar bullshtml.jar test_coverage
+#To Do: symlink bullseye/src
+#  ie: ln -vs ~/daos/src ~/BullseyeCoverage-9.5.13/src
+java -jar bullshtml.jar "$WORKSPACE"/test_coverage_report
