1a8afda5155baa54526730ce3f66bcd14c23a3db
Padmanabhan <ravindran.padmanabhan@intel.com>
2024-02-14 10:41:37 -0500

    DAOS-7372 test: Review updates. Test-tag: test_scrubber_performance always_passes test_tiers
    Required-githooks: true
    
    Signed-off-by: Padmanabhan <ravindran.padmanabhan@intel.com>

--- src/tests/ftest/scrubber/performance.py
+++ src/tests/ftest/scrubber/performance.py
@@ -10,25 +10,23 @@ from ior_utils import IorMetrics
 
 
 class TestWithScrubberPerf(IorTestBase):
-    # pylint: disable=too-many-nested-blocks
     """Basic Scrubber Test
 
     :avocado: recursive
     """
-
     def test_scrubber_performance(self):
         """JIRA ID: DAOS-7372
         Check IOR performance with checksum enabled.
 
-        :avocado: tags=all,manual
+        :avocado: tags=all,full_regression
         :avocado: tags=hw,medium
-        :avocado: tags=scrubber
-        :avocado: tags=test_scrubber_performance
+        :avocado: tags=checksum,scrubber
+        :avocado: tags=TestWithScrubberPerf,test_scrubber_performance
         """
         write_flags = self.params.get("write_flags", self.ior_cmd.namespace)
         read_flags = self.params.get("read_flags", self.ior_cmd.namespace)
-        self.add_pool()
-        self.add_container(self.pool)
+        self.pool = self.get_pool()
+        self.container = self.get_container(self.pool)
         # First get the write metrics
         self.ior_cmd.flags.update(write_flags)
         cmd_result = self.run_ior_with_pool(create_cont=False,
@@ -46,8 +44,8 @@ class TestWithScrubberPerf(IorTestBase):
         self.log.info("ior_write_size = %d", ior_write_size_without_scrubber)
         self.log.info("ior_read_size = %d", ior_read_size_without_scrubber)
         self.pool.set_prop(properties="scrub:lazy")
-        # self.pool.set_prop(properties="scrub:timed,scrub-freq:1")
         # Wait for the scrubber to scan objects.
+        self.log.info("Waiting for 15 seconds")
         time.sleep(15)
         # Get the write metrics with scrubber enabled.
         self.ior_cmd.flags.update(write_flags)
