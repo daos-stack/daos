2289377d9eff9f704acfe7ce10f85facbf790ebd
Tom Nabarro <tom.nabarro@intel.com>
2024-02-29 21:29:47 +0000

    improve namespace and module string representations
    Features: control
    Required-githooks: true
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/server/storage/scm.go
+++ src/control/server/storage/scm.go
@@ -174,13 +174,14 @@ func (sms ScmModules) String() string {
 	var buf bytes.Buffer
 
 	if len(sms) == 0 {
-		return "\t\tnone\n"
+		return "\tnone\n"
 	}
 
 	sort.Slice(sms, func(i, j int) bool { return sms[i].PhysicalID < sms[j].PhysicalID })
 
+	fmt.Fprintf(&buf, "\n")
 	for _, sm := range sms {
-		fmt.Fprintf(&buf, "\t\t%s\n", sm)
+		fmt.Fprintf(&buf, "\t%s\n", sm)
 	}
 
 	return buf.String()
@@ -229,6 +230,29 @@ func (sn ScmNamespace) Usable() uint64 {
 		return 0
 	}
 	return sn.Mount.UsableBytes
+}
+
+func (sn *ScmNamespace) String() string {
+	// capacity given in IEC standard units.
+	return fmt.Sprintf("UUID:%s BlockDev:%s Name:%s NUMA:%d Size:%s Mount:%+v",
+		sn.UUID, sn.BlockDevice, sn.Name, sn.NumaNode, humanize.IBytes(sn.Size), sn.Mount)
+}
+
+func (sns ScmNamespaces) String() string {
+	var buf bytes.Buffer
+
+	if len(sns) == 0 {
+		return "\tnone\n"
+	}
+
+	sort.Slice(sns, func(i, j int) bool { return sns[i].Name < sns[j].Name })
+
+	fmt.Fprintf(&buf, "\n")
+	for _, sn := range sns {
+		fmt.Fprintf(&buf, "\t%s\n", sn)
+	}
+
+	return buf.String()
 }
 
 // Capacity reports total storage capacity (bytes) across all namespaces.
