53227f0f2bf0b335d8de76e38f2d7c034fe88742
Yokesh Jayakumar <karthj@google.com>
2024-03-06 19:23:57 +0000

    DAOS-15306 gurt: Fix Coverity message for unchecked return value.
    This commit fixes the unchecked return value message from the pthread_mutex_lock call.
    
    Required-githooks: true
    
    Signed-off-by: Yokesh Jayakumar <karthj@google.com>

--- src/gurt/dlog.c
+++ src/gurt/dlog.c
@@ -106,7 +106,7 @@ static const char        *default_fac0name = "CLOG";
 /* whether we should merge log and stderr */
 static bool               merge_stderr;
 
-#define clog_lock()   pthread_mutex_lock(&clogmux)
+#define clog_lock()   (void)pthread_mutex_lock(&clogmux)
 #define clog_unlock() (void)pthread_mutex_unlock(&clogmux)
 
 static int d_log_write(char *buf, int len, bool flush);
