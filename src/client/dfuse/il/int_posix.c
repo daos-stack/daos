193673216f84b0c44bdf58d2f89ddcb85f2e4f9c
Johann Lombardi <johann.lombardi@gmail.com>
2024-03-11 21:28:34 +0100

    DAOS-15412 ioil: add support for SEEK_END
    Seeking to SEEK_END is not impolemented in libioil.
    It causes interception to be disabled with some python framework.
    
    Change-Id: I362d5d1d61449e7b03b2af21460512143547f99d
    Signed-off-by: Johann Lombardi <johann.lombardi@gmail.com>

--- src/client/dfuse/il/int_posix.c
+++ src/client/dfuse/il/int_posix.c
@@ -1419,10 +1419,14 @@ dfuse_lseek(int fd, off_t offset, int whence)
 	} else if (whence == SEEK_CUR) {
 		new_offset = entry->fd_pos + offset;
 	} else if (whence == SEEK_END) {
-		DFUSE_TRA_INFO(entry->fd_dfsoh, "Unsupported function, disabling SEEK_END");
-		entry->fd_status = DFUSE_IO_DIS_STREAM;
-		vector_decref(&fd_table, entry);
-		return __real_lseek(fd, offset, whence);
+		/**
+		 * use dfs_get_size() instead of dfs_ostat() to avoid fetching extra inode
+		 * attributes
+		 */
+		rc = dfs_get_size(entry->fd_cont->ioc_dfs, entry->fd_dfsoh,
+				  (daos_size_t *)&new_offset);
+		if (rc != 0)
+			goto do_real_lseek;
 	} else {
 		DFUSE_TRA_INFO(entry->fd_dfsoh, "Unsupported function, disabling %d", whence);
 		entry->fd_status = DFUSE_IO_DIS_STREAM;
