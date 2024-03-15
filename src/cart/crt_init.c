6fb7dec98f725b5429e30d624dd22598ecf025bb
Joseph Moore <joseph.moore@intel.com>
2024-03-15 02:36:12 +0000

    DAOS-14982 cart: Set fork safe env var for IB.
    Skip-build-leap15-icc: true
    Skip-build-el8-gcc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Test-tag: pr daily_regression
    Test-provider: ucx+ud_x
    Skip-func-hw-test-medium-ucx-provider: false
    Skip-func-hw-test-medium-verbs-provider: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_init.c
+++ src/cart/crt_init.c
@@ -348,6 +348,9 @@ static int data_init(int server, crt_init_options_t *opt)
 	}
 	if (server)
 		d_setenv("UCX_IB_FORK_INIT", "n", 1);
+	else
+		d_setenv("IBV_FORK_SAFE", "y", 1);
+
 
 	/* This is a workaround for CART-871 if universe size is not set */
 	d_getenv_uint("FI_UNIVERSE_SIZE", &fi_univ_size);
