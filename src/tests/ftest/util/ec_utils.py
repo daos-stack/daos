89a02a5fc96677cb2bfe51bd689dc993c2a2919b
Ding Ho <ding-hwa.ho@intel.com>
2024-03-21 16:56:28 +0000

    Update ec_utils.py and rebuild_disabled_single.yaml for write_single_type_dataset oclass.
    Test-tag: test_ec_online_rebuild_single
    Required-githooks: true
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/util/ec_utils.py
+++ src/tests/ftest/util/ec_utils.py
@@ -319,7 +319,7 @@ class ErasureCodeSingle(TestWithServers):
                     self.ec_container_create(oclass[0])
                     self.single_type_param_update(self.container[-1], sizes)
                     # Write the data
-                    self.container[-1].write_objects(obj_class=oclass[0])
+                    self.container[-1].write_objects(obj_class=oclass[2])
                     if results is not None:
                         results.put("PASS")
                 except (CommandFailure, DaosApiError, DaosTestError):
