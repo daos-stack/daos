433e57befd032601f7673731d70d999b643e0338
Tom Nabarro <tom.nabarro@intel.com>
2024-03-27 16:35:57 +0000

    attempt to remove test specific logic from production code
    Required-githooks: true
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/cmd/daos_server/network.go
+++ src/control/cmd/daos_server/network.go
@@ -17,10 +17,29 @@ import (
 	"github.com/daos-stack/daos/src/control/lib/control"
 	"github.com/daos-stack/daos/src/control/lib/hardware"
 	"github.com/daos-stack/daos/src/control/lib/hardware/hwprov"
-	"github.com/daos-stack/daos/src/control/server"
+	"github.com/daos-stack/daos/src/control/logging"
+	"github.com/daos-stack/daos/src/control/server/config"
 )
 
 const allProviders = "all"
+
+type fabricScanFn func(context.Context, ...string) (*hardware.FabricInterfaceSet, error)
+
+// Function pointers that can be mocked when unit testing.
+var (
+	netCmdInit = func() error {
+		if err := common.CheckDupeProcess(); err != nil {
+			return err
+		}
+		return nil
+	}
+	getFabricScanner = func(log logging.Logger) fabricScanFn {
+		return hwprov.DefaultFabricScanner(log).Scan
+	}
+	getConfig = func(cmd cfgCmd) *config.Server {
+		return cmd.config
+	}
+)
 
 type networkCmd struct {
 	Scan networkScanCmd `command:"scan" description:"Scan for network interface devices on local server"`
@@ -70,8 +89,6 @@ func fabricInterfaceSetToHostFabric(fis *hardware.FabricInterfaceSet, filterProv
 	return hf
 }
 
-type fabricScanFn func(context.Context, ...string) (*hardware.FabricInterfaceSet, error)
-
 func GetLocalFabricIfaces(ctx context.Context, scan fabricScanFn, filterProvider string) (*control.HostFabric, error) {
 	results, err := scan(ctx, filterProvider)
 	if err != nil {
@@ -88,32 +105,28 @@ func localHostFabricMap(hf *control.HostFabric) (control.HostFabricMap, error) {
 }
 
 func (cmd *networkScanCmd) Execute(_ []string) error {
-	if cmd.ctlSvc == nil {
-		// Allow parseOpts() unit tests to skip some setup.
-		if err := common.CheckDupeProcess(); err != nil {
-			return err
-		}
-		cmd.ctlSvc = server.NewControlService(cmd.Logger, nil, cmd.config, nil,
-			hwprov.DefaultFabricScanner(cmd.Logger))
+	if err := netCmdInit(); err != nil {
+		return err
 	}
 
 	ctx := cmd.MustLogCtx()
 
 	var prov string
+	cfg := getConfig(cmd.cfgCmd)
 	switch {
 	case cmd.FabricProvider != "":
 		if !strings.EqualFold(cmd.FabricProvider, allProviders) {
 			prov = cmd.FabricProvider
 		}
-	case cmd.config.Fabric.Provider != "":
-		priProv, err := cmd.config.Fabric.GetPrimaryProvider()
+	case cfg != nil && cfg.Fabric.Provider != "":
+		priProv, err := cfg.Fabric.GetPrimaryProvider()
 		if err != nil {
 			return errors.Wrapf(err, "unable to get fabric provider from config")
 		}
 		prov = priProv
 	}
 
-	hf, err := GetLocalFabricIfaces(ctx, cmd.ctlSvc.FabricScan, prov)
+	hf, err := GetLocalFabricIfaces(ctx, getFabricScanner(cmd.Logger), prov)
 	if err != nil {
 		return errors.Wrap(err, "get local fabric interfaces")
 	}
