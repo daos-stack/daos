a54fc8f96d7e58e6a2ecdb4d8ae20e04771190a1
Phil Henderson <phillip.henderson@intel.com>
2024-03-27 17:15:48 -0400

    Trying to get the test to list again.
    Skip-nlt: true
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Allow-unstable-test: true
    Test-tag: test_ioctl_pool_handles
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/dfuse/ioctl_pool_handles.py
+++ src/tests/ftest/dfuse/ioctl_pool_handles.py
@@ -4,13 +4,12 @@
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
 
-from apricot import TestWithServers
 from dfuse_utils import get_dfuse, start_dfuse
 from ior_utils import write_data
-from telemetry_utils import TelemetryUtils
+from telemetry_test_base import TestWithTelemetry
 
 
-class IoctlPoolHandles(TestWithServers):
+class IoctlPoolHandles(TestWithTelemetry):
     """Verifies multi-user dfuse mounting"""
 
     def test_ioctl_pool_handles(self):
@@ -39,19 +38,11 @@ class IoctlPoolHandles(TestWithServers):
             run and pass, it should not be run in normal CI, but in a PR that includes the
             modification.
 
-        Note: This test should be run in non-landing PR that includes the same change implemented in
-            https://github.com/daos-stack/daos/pull/9941 along with the following commit pragmas:
-                Skip-nlt: true
-                Skip-unit-tests: true
-                Skip-fault-injection-test: true
-                Test-tag: test_ioctl_pool_handles test_always_passes
-
-        :avocado: tags=all
+        :avocado: tags=all,manual
         :avocado: tags=vm
         :avocado: tags=dfuse
         :avocado: tags=IoctlPoolHandles,test_ioctl_pool_handles
         """
-        telemetry = TelemetryUtils(self.get_dmg_command(), self.server_managers[0].hosts)
         metrics = ['engine_pool_ops_cont_open', 'engine_pool_ops_pool_connect']
 
         self.log_step('Creating a 10GB pool')
@@ -65,8 +56,8 @@ class IoctlPoolHandles(TestWithServers):
         start_dfuse(self, dfuse, pool, container)
 
         self.log_step('Collecting container metrics before running ior')
-        initial = telemetry.collect_data(metrics)
-        telemetry.display_data()
+        initial = self.telemetry.collect_data(metrics)
+        self.telemetry.display_data()
         for metric in list(initial):
             for label in initial[metric]:
                 value = initial[metric][label]
@@ -77,7 +68,7 @@ class IoctlPoolHandles(TestWithServers):
         write_data(self, container, **ior_params)
 
         self.log_step('Collecting pool/container metrics after running ior')
-        after_ior = telemetry.collect_data(metrics)
+        after_ior = self.telemetry.collect_data(metrics)
         for metric in list(after_ior):
             for label in after_ior[metric]:
                 value = initial[metric][label]
@@ -86,7 +77,7 @@ class IoctlPoolHandles(TestWithServers):
                 after_ior[metric][label] = [value]
 
         self.log_step('Verifying pool/container handle ops have not increased after running ior')
-        if not telemetry.verify_data(initial):
+        if not self.telemetry.verify_data(initial):
             self.fail('Pool/container handle ops increased after running ior')
 
         self.log_step('Unmount dfuse')
@@ -96,10 +87,10 @@ class IoctlPoolHandles(TestWithServers):
         dfuse.run()
 
         self.log_step('Collecting pool/container metrics after remounting dfuse')
-        telemetry.collect_data(metrics)
+        self.telemetry.collect_data(metrics)
 
         self.log_step('Verifying pool/container handle ops have increased after remounting dfuse')
-        if not telemetry.verify_data(after_ior):
+        if not self.telemetry.verify_data(after_ior):
             self.fail('Pool/container handle ops did not increase after remounting dfuse')
 
         self.log.info('Test passed')
