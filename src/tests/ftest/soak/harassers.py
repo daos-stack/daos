637c31d9fdba9588451a82a2fb1227dd482b4fb7
Maureen Jean <maureen.jean@intel.com>
2024-04-05 10:11:52 -0400

    remove 12h soak test. time can be changed with extra yaml
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: soak_smoke
    
    Required-githooks: true
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/soak/harassers.py
+++ src/tests/ftest/soak/harassers.py
@@ -13,7 +13,7 @@ class SoakHarassers(SoakTestBase):
     :avocado: recursive
     """
 
-    def test_soak_offline_harassers_12h(self):
+    def test_soak_offline_harassers(self):
         """Run all soak tests with harassers.
 
         Test ID: DAOS-2511
@@ -25,12 +25,12 @@ class SoakHarassers(SoakTestBase):
         :avocado: tags=manual
         :avocado: tags=hw,24
         :avocado: tags=soak,soak_harassers
-        :avocado: tags=SoakHarassers,test_soak_offline_harassers_12h
+        :avocado: tags=SoakHarassers,test_soak_offline_harassers
         """
         test_param = "/run/soak_harassers/"
         self.run_soak(test_param)
 
-    def test_soak_online_harassers_12h(self):
+    def test_soak_online_harassers(self):
         """Run all soak tests with harassers.
 
         Test ID: DAOS-2511
@@ -42,41 +42,7 @@ class SoakHarassers(SoakTestBase):
         :avocado: tags=manual
         :avocado: tags=hw,24
         :avocado: tags=soak,soak_harassers
-        :avocado: tags=SoakHarassers,test_soak_online_harassers_12h
-        """
-        test_param = "/run/soak_harassers/"
-        self.run_soak(test_param)
-
-    def test_soak_offline_harassers_24h(self):
-        """Run all soak tests with harassers.
-
-        Test ID: DAOS-2511
-        Test Description: This will create a soak job that runs
-        various offline harassers  defined in the soak yaml
-        This test will run for the time specified in
-        /run/test_timeout.
-
-        :avocado: tags=manual
-        :avocado: tags=hw,24
-        :avocado: tags=soak,soak_harassers
-        :avocado: tags=SoakHarassers,test_soak_offline_harassers_24h
-        """
-        test_param = "/run/soak_harassers/"
-        self.run_soak(test_param)
-
-    def test_soak_online_harassers_24h(self):
-        """Run all soak tests with harassers.
-
-        Test ID: DAOS-2511
-        Test Description: This will create a soak job that runs
-        various online harassers  defined in the soak yaml
-        This test will run for the time specified in
-        /run/test_timeout.
-
-        :avocado: tags=manual
-        :avocado: tags=hw,24
-        :avocado: tags=soak,soak_harassers
-        :avocado: tags=SoakHarassers,test_soak_online_harassers_24h
+        :avocado: tags=SoakHarassers,test_soak_online_harassers
         """
         test_param = "/run/soak_harassers/"
         self.run_soak(test_param)
