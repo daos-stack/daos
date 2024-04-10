9a900d1f4e9fa617da68e5bcf34c5f3fa56f5f6b
Lei Huang <lei.huang@intel.com>
2024-04-10 12:41:09 +0000

    Add build directory when checkouting a branch with git
    Features: pil4dfs
    Test-provider-hw-medium: ofi+tcp;ofi_rxm
    Priority: 2
    
    Required-githooks: true
    
    Signed-off-by: Lei Huang <lei.huang@intel.com>

--- src/tests/ftest/dfuse/daos_build.py
+++ src/tests/ftest/dfuse/daos_build.py
@@ -229,7 +229,7 @@ class DaosBuild(DfuseTestBase):
 
         cmds = ['python3 -m venv {}/venv'.format(mount_dir),
                 'git clone https://github.com/daos-stack/daos.git {}'.format(build_dir),
-                'git checkout lei/DAOS-14149',
+                'git -C {} checkout lei/DAOS-14149'.format(build_dir),
                 'git -C {} submodule init'.format(build_dir),
                 'git -C {} submodule update'.format(build_dir),
                 'python3 -m pip install pip --upgrade',
