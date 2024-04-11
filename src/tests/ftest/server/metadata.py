60a1d097f9a2930b1c7c43d1e63d3c7566775614
Ding Ho <ding-hwa.ho@intel.com>
2024-04-11 23:14:00 +0000

    Update metadata.yaml for mux with svc_ops_disable.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: ObjectMetadata
    Doc-only: false
    Required-githooks: true
    Signed-off-by: Ding Ho ding-hwa.ho@intel.com

--- src/tests/ftest/server/metadata.py
+++ src/tests/ftest/server/metadata.py
@@ -250,7 +250,7 @@ class ObjectMetadata(TestWithServers):
         :avocado: tags=server,metadata
         :avocado: tags=ObjectMetadata,test_metadata_fillup
         """
-        self.create_pool()
+        self.create_pool(namespace='/run/test_1_pool/*')
         svc_ops_entry_age = self.pool.get_property("svc_ops_entry_age")
         if not self.run_dummy_metadata_workload(duration=svc_ops_entry_age):
             self.fail("failed to run dummy metadata workload")
@@ -342,8 +342,9 @@ class ObjectMetadata(TestWithServers):
         :avocado: tags=server,metadata,nvme
         :avocado: tags=ObjectMetadata,test_metadata_addremove
         """
-        self.create_pool()
-        if not self.run_dummy_metadata_workload():
+        self.create_pool(namespace='/run/test_2_pool/*')
+        svc_ops_entry_age = self.pool.get_property("svc_ops_entry_age")
+        if not self.run_dummy_metadata_workload(duration=svc_ops_entry_age):
             self.fail("failed to run dummy metadata workload")
 
         self.container = []
@@ -416,7 +417,7 @@ class ObjectMetadata(TestWithServers):
         :avocado: tags=server,metadata,nvme,ior
         :avocado: tags=ObjectMetadata,test_metadata_server_restart
         """
-        self.create_pool()
+        self.create_pool(namespace='/run/test_2_pool/*')
         files_per_thread = 400
         total_ior_threads = 5
 
