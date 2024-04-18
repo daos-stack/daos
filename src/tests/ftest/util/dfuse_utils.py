99cdb9b6d89fb14439f43c7f636e3302f5ad97ad
Ashley Pittman <ashley.m.pittman@intel.com>
2024-04-18 18:02:57 +0000

    More testing feedback.
    Test-tag: test_dfuse_pre_read
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/tests/ftest/util/dfuse_utils.py
+++ src/tests/ftest/util/dfuse_utils.py
@@ -382,17 +382,19 @@ class Dfuse(DfuseCommand):
         self.__need_cleanup = False
 
     def get_stats(self):
-        """Return the I/O stats for the filesystem"""
+        """Return the I/O stats for the filesystem
+
+        Only works if there is one entry in the client list.
+        """
 
         cmd = f"daos filesystem query --json {self.mount_dir.value}"
         result = run_remote(self.log, self.hosts, cmd)
         if not result.passed:
-            raise CommandFailure(f'"{cmd}" failed on {result.failed_hosts}')
+            raise CommandFailure(f'"fs query failed on {result.failed_hosts}')
 
         data = json.loads("\n".join(result.output[0].stdout))
-        print(data)
-        assert data["status"] == 0
-        assert data["error"] is None
+        if data["status"] != 0 or data["error"] is not None:
+            raise CommandFailure("fs query returned bad data.")
         return data["response"]
 
 
