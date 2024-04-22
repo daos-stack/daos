6e78208b9434719723f3a56e29569df5a9b06639
Makito Kano <makito.kano@intel.com>
2024-04-22 06:23:09 +0000

    DAOS-15008 test: Update test steps and made other changes
    Include set nvme-faulty and replace steps.
    Update the logic to check total available NVMe size.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: test_no_activity
    Required-githooks: true
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/vmd/hot_plug_no_activity.py
+++ src/tests/ftest/vmd/hot_plug_no_activity.py
@@ -16,7 +16,7 @@ class HotPlugNoActivityTest(IorTestBase):
     :avocado: recursive
     """
     def repeat_query_list_devices(self, exp_disk_state, exp_led_state, error_msg, errors,
-                                  dmg_command, rank):
+                                  dmg_command, uuid):
         """Repeatedly call dmg storage query list-devices verify the states.
 
         Args:
@@ -25,43 +25,40 @@ class HotPlugNoActivityTest(IorTestBase):
             error_msg (str): Error message to add to the error list.
             errors (list): Error list for this test.
             dmg_command (DmgCommand): DmgCommand object to call the command.
-            rank (int): Rank where the NVMe disk we want to access is located.
+            uuid (str): Device UUID.
         """
         removed_state = False
         for count in range(2):
             time.sleep(30)
             device_info = get_storage_query_device_info(dmg=dmg_command)
-            self.log.info("device_info (repeat_query_list_devices) = %s", device_info)
+            self.log.info("## device_info (repeat_query_list_devices) = %s", device_info)
             dev_state = None
             led_state = None
             for device in device_info:
-                if device["rank"] == rank:
+                if device["uuid"] == uuid:
                     device_control = device["ctrlr"]
                     dev_state = device_control["dev_state"]
                     led_state = device_control["led_state"]
             if dev_state == exp_disk_state and led_state == exp_led_state:
                 removed_state = True
                 break
-            self.log.info("%d: Disk state = %s; LED state = %s", count, dev_state, led_state)
+            self.log.info("## %d: Disk state = %s; LED state = %s", count, dev_state, led_state)
         if not removed_state:
             errors.append(error_msg)
 
-    def get_total_nvme_bytes(self, dmg_command, host_port, rank):
-        """Get total NVMe bytes using 'dmg storage query usage'
+    def get_uuid_to_total_byes(self, dmg_command, host_port):
+        """Call 'dmg storage query usage' and get UUID and total bytes of each NVMe drive.
 
         Args:
             dmg_command (DmgCommand): DmgCommand object used to call the command and to obtain the
                 port number.
             host_port (str): Host where the NVMe disk we want to access is located. <host>:<port>
-            rank (int): Rank where the NVMe disk we want to access is located.
 
         Returns:
-            int: total_bytes value of the NVMe device we're interested, which is specified by
-                host_port and rank parameter. If not found, for example because of invalid host_port
-                rank combination, returns None.
+            dict: UUID to total bytes of each NVMe drive that are in the given host.
 
         """
-        total_bytes = None
+        uuid_to_total_bytes = {}
         usage_out = dmg_command.storage_query_usage()
         # There's a hash value under HostStorage. Obtain "storage" -> "nvme_devices" and "hosts"
         # under it. There may be multiple hosts depending on the setup.
@@ -71,30 +68,34 @@ class HotPlugNoActivityTest(IorTestBase):
                     # In HW medium cluster, there's only one dictionary in the smd_devices list, so
                     # we may be able to just index it, but use for loop just in case.
                     for smd_device in nvme_device["smd_devices"]:
-                        if smd_device["rank"] == rank:
-                            total_bytes = smd_device["total_bytes"]
-        self.log.info("total_bytes = %s", total_bytes)
-        return total_bytes
+                        uuid_to_total_bytes[smd_device["uuid"]] = smd_device["total_bytes"]
+        return uuid_to_total_bytes
 
     def test_no_activity(self):
-        """Conduct VMD hot-remove and hot-plug during no activity.
+        """Test VMD hot-remove and hot-plug during no activity.
 
         1. Determine the PCI address (TrAddr) of the disk we'll hot-remove and verify that its state
         is NORMAL and LED is OFF.
-        2. Store the total NVMe size.
+        2. Store the total NVMe size of each drive.
         3. Create a pool and a container.
         4. Write data with IOR.
-        5. Hot remove the disk we selected at step 1.
-        6. Repeatedly call "dmg storage query list-devices" until the disk state becomes UNPLUGGED
+        5. Call dmg storage set nvme-faulty --uuid=<UUID>
+        6. Repeatedly call "dmg storage query list-devices" until the disk state becomes EVICTED
+        and LED becomes ON.
+        7. Hot remove the disk we selected at step 1.
+        8. Repeatedly call "dmg storage query list-devices" until the disk state becomes UNPLUGGED
         and LED becomes NA.
-        7. Verify that the disk space is down compared to before the remove.
-        8. Hot-plug.
-        9. Repeatedly call "dmg storage query list-devices" until the disk state becomes NORMAL and
+        9. For those untouched devices, verify that the space is unchanged after the hot remove.
+        10. Hot-plug.
+        11. Repeatedly call "dmg storage query list-devices" until the disk state becomes EVICTED
+        and LED becomes ON.
+        12. Call dmg storage replace nvme --old-uuid=<UUID> --new-uuid=<UUID>
+        13. Repeatedly call "dmg storage query list-devices" until the disk state becomes NORMAL and
         LED becomes OFF.
-        10. Verify that the disk space is back to the original.
-        11. Verify that none of the engines have crashed.
-        12. Verify that the disks are healthy by checking the container status.
-        13. Rung IOR and check that it works.
+        14. Verify that the disk spaces are back to the original.
+        15. Verify that none of the engines have crashed.
+        16. Verify that the disks are healthy by checking the container status.
+        17. Rung IOR and check that it works.
 
         Jira ID: DAOS-15008
 
@@ -103,8 +104,6 @@ class HotPlugNoActivityTest(IorTestBase):
         :avocado: tags=vmd,hot_plug
         :avocado: tags=HotPlugNoActivityTest,test_no_activity
         """
-        # 1. Determine the PCI address (TrAddr) of the disk we'll hot-remove and verify that its
-        # state is NORMAL and LED is OFF.
         msg = ("Determine the PCI address (TrAddr) of the disk we'll hot-remove and verify that "
                "its state is NORMAL and LED is OFF.")
         self.log_step(msg)
@@ -116,9 +115,10 @@ class HotPlugNoActivityTest(IorTestBase):
         led_state = None
         host_port = None
         rank = 0
-        # We'll use the rank 0 device.
+        # We'll use the first rank 0 device. There could be multiple devices mapped to rank 0.
         for device in device_info:
             if device["rank"] == rank:
+                remove_uuid = device["uuid"]  # UUID of the drive that we'll remove and plug.
                 device_control = device["ctrlr"]
                 pci_addr = device_control["pci_addr"]
                 dev_state = device_control["dev_state"]
@@ -126,7 +126,7 @@ class HotPlugNoActivityTest(IorTestBase):
                 host_port = device["hosts"]
                 break
         msg = (f"pci_addr = {pci_addr}; dev_state = {dev_state}; led_state = {led_state}; "
-               f"host_port = {host_port}")
+               f"host_port = {host_port}; uuid = {remove_uuid}")
         self.log.info(msg)
         errors = []
         exp_disk_state = "NORMAL"
@@ -137,110 +137,129 @@ class HotPlugNoActivityTest(IorTestBase):
         if led_state != exp_led_state:
             errors.append(f"Unexpected LED state! Expected = {exp_led_state}, Actual = {led_state}")
 
-        # 2. Store the total NVMe size.
-        self.log_step("Store the total NVMe size.")
-        total_bytes_orig = self.get_total_nvme_bytes(
-            dmg_command=dmg_command, host_port=host_port, rank=rank)
+        self.log_step("Store the total NVMe size of each drive.")
+        uuid_to_total_bytes_orig = self.get_uuid_to_total_byes(
+            dmg_command=dmg_command, host_port=host_port)
+        self.log.debug(f"## uuid_to_total_bytes_orig = {uuid_to_total_bytes_orig}")
 
-        # 3. Create a pool and a container.
         self.log_step("Create a pool and a container")
         self.pool = self.get_pool(connect=False)
         self.container = self.get_container(pool=self.pool)
 
-        # 4. Write data with IOR.
         self.log_step("Write data with IOR.")
         self.ior_cmd.set_daos_params(self.server_group, self.pool, self.container.identifier)
         self.run_ior_with_pool(create_pool=False, create_cont=False)
 
-        # 5. Hot remove the disk we selected at step 1.
-        # self.log_step("Hot remove the disk we selected at step 1.")
-        # command = f"sudo python3 /home/mkano/daos/build/external/debug/spdk/scripts/rpc.py -s /var/tmp/spdk_0.sock vmd_remove_device {pci_addr}"
-        # rpc_out = run_pcmd(hosts=self.hostlist_servers, command=command)
-        # self.log.debug(f"## Hot remove out = {rpc_out}")
-        # stdout contains the output as list. Each item in the list represents a line. e.g.,
-        # '[',
-        # '  {',
-        # '    "name": "Nvme_wolf-313.wolf.hpdd.intel.com_0_1_0",',
-        # '    "ctrlrs": [',
-        # '      {',
-        # '        "state": "enabled",',
-        # ...
-        # We'll remove the first and the last bracket, concatenate all items into a single string,
-        # then pass it into yaml.safe_load(). (It works even there are white spaces at the beginning
-        # of each line.)
-        # stdout = rpc_out[0]["stdout"]
-        # First and last item of stdout is "[" and "]", so remove them.
-        # stdout.pop()
-        # stdout.pop(0)
-        # Concatenate each line into a single string.
-        # stdout_str = "".join(stdout)
-        # Convert the string to a yaml object so that we can easily reference the values.
-        # yaml_out = yaml.safe_load(stdout_str)
-        # self.log.debug(f"## yaml_out = {yaml_out}")
-        # name = yaml_out["name"]
-        # self.log.debug(f"## name = {name}")
-        # state = yaml_out["ctrlrs"][0]["state"]
-        # self.log.debug(f"## state = {state}")
+        self.log_step("Call dmg storage set nvme-faulty --uuid=<UUID>.")
+        dmg_command.storage_set_faulty(uuid=remove_uuid)
 
-        # 6. Repeatedly call "dmg storage query list-devices" until the disk state becomes UNPLUGGED
-        # and LED becomes NA.
-        # msg = ("Repeatedly call 'dmg storage query list-devices' until the disk state becomes "
-        #        "UNPLUGGED and LED becomes NA.")
-        # self.log_step(msg)
-        # error_msg = "Disk and LED state didn't turn to removed state. (UNPLUGGED and OFF)"
-        # self.repeat_query_list_devices(
-        #     exp_disk_state="UNPLUGGED", exp_led_state="OFF", error_msg=error_msg, errors=errors,
-        #     dmg_command=dmg_command, rank=rank)
-
-        # 7. Verify that the disk space is down compared to before the remove.
-        # self.log_step("Verify that the disk space is down compared to before the remove.")
-        # total_bytes_hot_remove = self.get_total_nvme_bytes(
-        #     dmg_command=dmg_command, host_port=host_port, rank=rank)
-        # if total_bytes_hot_remove >= total_bytes_orig:
-        #     msg = (f"Total NVMe bytes haven't been reduced after a hot remove! "
-        #            f"Original = {total_bytes_orig}; Hot-removed = {total_bytes_hot_remove}")
-        #     errors.append(msg)
-
-        # 8. Hot-plug.
-        # self.log_step("Hot-plug.")
-        # command = f"sudo python3 /home/mkano/daos/build/external/debug/spdk/scripts/rpc.py -s /var/tmp/spdk_0.sock vmd_rescan"
-        # rpc_out = run_pcmd(hosts=self.hostlist_servers, command=command)
-        # self.log.debug(f"## Hot plug out = {rpc_out}")
-
-        # 9. Repeatedly call "dmg storage query list-devices" until the disk state becomes NORMAL
-        # and LED becomes OFF.
+        exp_disk_state = "EVICTED"
+        exp_led_state = "ON"
         msg = ("Repeatedly call 'dmg storage query list-devices' until the disk state becomes "
-               "NORMAL and LED becomes OFF.")
+               f"{exp_disk_state} and LED becomes {exp_led_state}.")
         self.log_step(msg)
-        error_msg = "Disk and LED state didn't turn to plugged state. (NORMAL and OFF)"
+        error_msg = f"Disk state must be {exp_disk_state} and LED state must be {exp_led_state}."
         self.repeat_query_list_devices(
-            exp_disk_state="NORMAL", exp_led_state="OFF", error_msg=error_msg, errors=errors,
-            dmg_command=dmg_command, rank=rank)
+            exp_disk_state=exp_disk_state, exp_led_state=exp_led_state, error_msg=error_msg,
+            errors=errors, dmg_command=dmg_command, uuid=remove_uuid)
 
-        # 10. Verify that the disk space is back to the original.
-        self.log_step("Verify that the disk space is back to the original.")
-        total_bytes_hot_plug = self.get_total_nvme_bytes(
-            dmg_command=dmg_command, host_port=host_port, rank=rank)
-        if total_bytes_hot_plug != total_bytes_orig:
-            msg = (f"Total NVMe bytes haven't been recovered! Original = {total_bytes_orig}; "
-                   f"After hot plug = {total_bytes_hot_plug}")
+        self.log_step("Hot remove the disk we selected at step 1.")
+        spdk_sock_path = self.params.get(
+            "sock_addr", "/run/server_config/engines/0/spdk_rpc_server/*")
+        command = (f"sudo /usr/share/spdk/scripts/rpc.py -s {spdk_sock_path} vmd_remove_device "
+                   f"{pci_addr}")
+        rpc_out = run_pcmd(hosts=self.hostlist_servers, command=command)
+        self.log.debug(f"## Hot remove out = {rpc_out}")
+        exit_status = rpc_out[0]["exit_status"]
+        if exit_status != 0:
+            self.fail(f"Hot remove failed! {rpc_out}")
+
+        exp_disk_state = "UNPLUGGED"
+        exp_led_state = "NA"
+        msg = ("Repeatedly call 'dmg storage query list-devices' until the disk state becomes "
+               f"{exp_disk_state} and LED becomes {exp_led_state}.")
+        self.log_step(msg)
+        error_msg = (f"Disk and LED state didn't turn to removed state. ({exp_disk_state} and "
+                     f"{exp_led_state})")
+        self.repeat_query_list_devices(
+            exp_disk_state=exp_disk_state, exp_led_state=exp_led_state, error_msg=error_msg,
+            errors=errors, dmg_command=dmg_command, uuid=remove_uuid)
+
+        # Note: For this step, if we don't use JSON as in manual test, we get the sum of total bytes
+        # from all drives in a host, so we just compare the values before and after the hot remove.
+        # However, automated test uses JSON, which returns total bytes for each drive, so we can do
+        # finer grained comparisons.
+        msg = ("For those untouched devices, verify that the space is unchanged after the hot "
+               "remove.")
+        self.log_step(msg)
+        uuid_to_total_bytes_after = self.get_uuid_to_total_byes(
+            dmg_command=dmg_command, host_port=host_port)
+        self.log.debug(f"## uuid_to_total_bytes_after = {uuid_to_total_bytes_after}")
+        # Check that the removed device doesn't appear in 'dmg storage query usage' output.
+        if remove_uuid in uuid_to_total_bytes_after:
+            msg = (f"Removed device ({disk_uuid}) appears after hot remove! "
+                   f"{uuid_to_total_bytes_after}")
+            errors.append(msg)
+        for disk_uuid, total_bytes in uuid_to_total_bytes_orig.items():
+            if disk_uuid != remove_uuid:
+                if disk_uuid not in uuid_to_total_bytes_after:
+                    msg = (f"Untouched disk ({disk_uuid}) disappeared after a hot remove!")
+                    errors.append(msg)
+                elif total_bytes != uuid_to_total_bytes_after[disk_uuid]:
+                    msg = (f"Hot remove resulted in untouched disk ({disk_uuid}) size change!")
+                    errors.append(msg)
+
+        self.log_step("Hot-plug.")
+        command = f"sudo /usr/share/spdk/scripts/rpc.py -s {spdk_sock_path} vmd_rescan"
+        rpc_out = run_pcmd(hosts=self.hostlist_servers, command=command)
+        self.log.debug(f"## Hot plug out = {rpc_out}")
+        exit_status = rpc_out[0]["exit_status"]
+        if exit_status != 0:
+            self.fail(f"Hot plug failed! {rpc_out}")
+
+        exp_disk_state = "EVICTED"
+        exp_led_state = "ON"
+        msg = ("Repeatedly call 'dmg storage query list-devices' until the disk state becomes "
+               f"{exp_disk_state} and LED becomes {exp_led_state}.")
+        self.log_step(msg)
+        error_msg = f"Disk must be {exp_disk_state} and LED must be {exp_led_state}."
+        self.repeat_query_list_devices(
+            exp_disk_state=exp_disk_state, exp_led_state=exp_led_state, error_msg=error_msg,
+            errors=errors, dmg_command=dmg_command, uuid=remove_uuid)
+
+        self.log_step("Call dmg storage replace nvme --old-uuid=<UUID> --new-uuid=<UUID>")
+        dmg_command.storage_replace_nvme(old_uuid=remove_uuid, new_uuid=remove_uuid)
+
+        exp_disk_state = "NORMAL"
+        exp_led_state = "OFF"
+        msg = ("Repeatedly call 'dmg storage query list-devices' until the disk state becomes "
+               f"{exp_disk_state} and LED becomes {exp_led_state}.")
+        self.log_step(msg)
+        error_msg = (f"Disk and LED state didn't go back to normal state! ({exp_disk_state} and "
+                     f"{exp_led_state})")
+        self.repeat_query_list_devices(
+            exp_disk_state=exp_disk_state, exp_led_state=exp_led_state, error_msg=error_msg,
+            errors=errors, dmg_command=dmg_command, uuid=remove_uuid)
+
+        self.log_step("Verify that the disk spaces are back to the original.")
+        uuid_to_total_bytes_after = self.get_uuid_to_total_byes(
+            dmg_command=dmg_command, host_port=host_port)
+        if uuid_to_total_bytes_after != uuid_to_total_bytes_orig:
+            msg = (f"Disk sizes changed after hot remove! Orig = {uuid_to_total_bytes_orig}; "
+                   f"After = {uuid_to_total_bytes_after}")
             errors.append(msg)
 
-        # 11. Verify that none of the engines have crashed.
         self.log_step("Verify that none of the engines have crashed.")
         system_query_out = dmg_command.system_query()
-        system_healthy = check_system_query_status(data=system_query_out)
-        if not system_healthy:
+        if not check_system_query_status(data=system_query_out):
             errors.append("One or more ranks crashed after hot remove and plug!")
 
-        # 12. Verify that the disks are healthy by checking the container status.
         self.log_step("Verify that the disks are healthy by checking the container status.")
         expected_props = {"status": "HEALTHY"}
         container_healthy = self.container.verify_prop(expected_props=expected_props)
         if not container_healthy:
             errors.append("Container status isn't HEALTHY after hot remove and plug!")
 
-        # 13. Rung IOR and check that it works.
         self.log_step("Rung IOR and check that it works.")
         cmd_result = self.run_ior_with_pool(create_pool=False, create_cont=False)
         if cmd_result.exit_status != 0:
