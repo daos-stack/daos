2c41a054a1d1f5b162fdcd95729b1dc9a584a6d4
Dalton Bohning <dalton.bohning@intel.com>
2024-04-22 17:15:32 +0000

    DAOS-623 cq: simplify githook output
    Skip-test: true
    Skip-tests: true
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    Add _print_githook_header to align githook header output.
    Add _git_diff_cached_files so hooks get a diff the same way.
    Remove most verbose output from githooks.
    
    Required-githooks: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- utils/githooks/pre-commit.d/70-isort.sh
+++ utils/githooks/pre-commit.d/70-isort.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-
+#
+# Copyright 2023-2024 Intel Corporation.
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
 # Runs isort for the DAOS project.
 
 set -ue
 
+_print_githook_header "isort"
+
 if ! command -v isort > /dev/null 2>&1; then
-    echo "No isort checking, install isort command to improve pre-commit checks"
+    echo "isort not installed. Install isort command to improve pre-commit checks:"
+    echo "  python3 -m pip install -r ./utils/cq/requirements.txt"
     . /etc/os-release
     if [ "$ID" = "fedora" ]; then
-        echo "dnf install python3-isort"
-        echo "or"
+        echo "  or"
+        echo "  dnf install python3-isort"
     fi
-    echo "python3 -m pip install -r ./utils/cq/requirements.txt"
     exit 0
 fi
 
-echo "isort:"
+echo "Checking if python imports are sorted"
 
-echo "  Running isort for python imports."
 isort_args=(--jobs 8 .)
 if ! isort --check-only "${isort_args[@]}"; then
     echo "  isort check failed, run 'isort ${isort_args[*]}' to fix."
