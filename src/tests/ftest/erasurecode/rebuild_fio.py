ee9656761eec168ef7c5a24cb0a75af7be1aad33
Phil Henderson <phillip.henderson@intel.com>
2024-05-01 14:59:51 -0400

    EcodFioRebuild cleanup.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Allow-unstable-test: true
    Test-tag: DfuseSpaceCheck DaosCoreTestDfuse DmvrNegativeTest DmvrPosixMetaEntry DmvrPosixSubsets DmvrPosixSymlinks DmvrPosixTypesTest DmvrSerialLargePosix DbenchTest DFuseBashdcacheTest DfuseBashCmd DFuseFdTest DfuseCachingCheck DfuseContainerCheck DaosBuild DfuseEnospace FioPil4dfsSmall FioSmall DfuseMUMount DfuseMUPerms Pil4dfsDcache Pil4dfsFio POSIXStatTest DFuseReadTest RootContainerTest PosixSimul SparseFile EcodFioRebuild Ecodtruncate EcodFaultInjection IoConsistency MacsioTest ParallelIo BasicCheckout test_ior_small test_performance_mdtest_easy_dfs_s1 test_performance_mdtest_hard_dfs_s1
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/erasurecode/rebuild_fio.py
+++ src/tests/ftest/erasurecode/rebuild_fio.py
@@ -3,25 +3,20 @@
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 '''
+import queue
+import threading
 import time
 
-from ec_utils import ErasureCodeFio
+from dfuse_utils import get_dfuse, start_dfuse
+from fio_test_base import FioBase
 
 
-class EcodFioRebuild(ErasureCodeFio):
-    # pylint: disable=protected-access
+class EcodFioRebuild(FioBase):
     """Test class Description: Runs Fio with EC object type over POSIX and
         verify on-line, off-line for rebuild and verify the data.
 
     :avocado: recursive
     """
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a EcodFioRebuild object."""
-        super().__init__(*args, **kwargs)
-        self.set_online_rebuild = False
-        self.rank_to_kill = None
-        self.read_option = self.params.get("rw_read", "/run/fio/test/read_write/*")
 
     def execution(self, rebuild_mode):
         """Execute test.
@@ -31,29 +26,42 @@ class EcodFioRebuild(ErasureCodeFio):
         """
         aggregation_threshold = self.params.get("threshold", "/run/pool/aggregation/*")
         aggregation_timeout = self.params.get("aggr_timeout", "/run/pool/aggregation/*")
+        read_option = self.params.get("rw_read", "/run/fio/test/read_write/*")
+
+        engine_count = self.server_managers[0].get_config_value("engines_per_host")
+        server_count = len(self.hostlist_servers) * engine_count
+        rank_to_kill = server_count - 1
+
         # 1. Disable aggregation
         self.log_step("Disable aggregation")
-        self.pool.disable_aggregation()
+        pool = self.get_pool()
+        pool.disable_aggregation()
 
-        # 2.a Kill last server rank first
-        self.log_step("Start fio and kill the last server")
-        self.rank_to_kill = self.server_count - 1
+        # Start dfuse
+        self.log_step('Starting dfuse')
+        container = self.get_container(pool)
+        container.set_attr(attrs={'dfuse-direct-io-disable': 'on'})
+        dfuse = get_dfuse(self, self.hostlist_clients)
+        start_dfuse(self, dfuse, pool, container)
+
+        # Write the Fio data and kill the last server rank if rebuild_mode is on-line
         if 'on-line' in rebuild_mode:
-            # Enabled on-line rebuild for the test
-            self.set_online_rebuild = True
-        # 2.b Write the Fio data and kill server if rebuild_mode is on-line
-        container = self.start_online_fio(self.pool)
+            self.log_step(f"Start fio and stop the last server rank ({rank_to_kill})")
+            self.start_online_fio(dfuse.mount_dir.value, rank_to_kill)
+        else:
+            self.log_step("Start fio and leave all servers running")
+            self.start_online_fio(dfuse.mount_dir.value, None)
 
-        # 3. Get initial total free space (scm+nvme)
+        # Get initial total free space (scm+nvme)
         self.log_step("Get initial total free space (scm+nvme)")
-        init_free_space = self.pool.get_total_free_space(refresh=True)
+        init_free_space = pool.get_total_free_space(refresh=True)
 
-        # 4. Enable aggregation
+        # Enable aggregation
         self.log_step("Enable aggregation")
-        self.pool.enable_aggregation()
+        pool.enable_aggregation()
 
-        # 5. Get total space consumed (scm+nvme) after aggregation enabled, verify and wait until
-        #    aggregation triggered, maximum 3 minutes.
+        # Get total space consumed (scm+nvme) after aggregation enabled, verify and wait until
+        # aggregation triggered, maximum 3 minutes.
         self.log_step("Verify the Fio write finish without any error")
         start_time = time.time()
         timed_out = False
@@ -61,7 +69,7 @@ class EcodFioRebuild(ErasureCodeFio):
         self.log_step("Verify and wait until aggregation triggered")
         while not aggr_triggered and not timed_out:
             # Check if current free space exceeds threshold
-            free_space = self.pool.get_total_free_space(refresh=True)
+            free_space = pool.get_total_free_space(refresh=True)
             difference = free_space - init_free_space
             aggr_triggered = difference >= aggregation_threshold
             self.log.debug("Total Free space: initial=%s, current=%s, difference=%s",
@@ -72,36 +80,86 @@ class EcodFioRebuild(ErasureCodeFio):
             if not aggr_triggered and not timed_out:
                 time.sleep(1)
         if timed_out:
-            self.fail("Aggregation not observed within {} seconds".format(aggregation_timeout))
+            self.fail(f"Aggregation not observed within {aggregation_timeout} seconds")
 
         # ec off-line rebuild fio
         if 'off-line' in rebuild_mode:
-            self.log_step("Stop rank for ec off-line rebuild fio")
-            self.server_managers[0].stop_ranks(
-                [self.server_count - 1], self.d_log, force=True)
+            self.log_step(f"Stop the last server rank ({rank_to_kill}) for ec off-line rebuild fio")
+            self.server_managers[0].stop_ranks([rank_to_kill], self.d_log, force=True)
 
-        # 6. Adding unlink option for final read command
+        # Adding unlink option for final read command
         self.log_step("Adding unlink option for final read command")
         if int(container.properties.value.split(":")[1]) == 1:
-            self.fio_cmd._jobs['test'].unlink.value = 1
+            self.fio_cmd._jobs['test'].unlink.value = 1         # pylint: disable=protected-access
 
-        # 7. Read and verify the original data.
+        # Read and verify the original data.
         self.log_step("Read and verify the original data.")
-        self.fio_cmd._jobs['test'].rw.value = self.read_option
+        self.fio_cmd._jobs['test'].rw.value = read_option       # pylint: disable=protected-access
         self.fio_cmd.run()
 
-        # 8. If RF is 2 kill one more server and validate the data is not corrupted.
-        self.log_step("If RF is 2 kill one more server and validate the data is not corrupted.")
+        # If RF is 2 kill one more server and validate the data is not corrupted.
         if int(container.properties.value.split(":")[1]) == 2:
-            self.fio_cmd._jobs['test'].unlink.value = 1
-            self.log.info("RF is 2,So kill another server and verify data")
             # Kill one more server rank
-            self.server_managers[0].stop_ranks([self.server_count - 2], self.d_log, force=True)
+            rank_to_kill = server_count - 2
+            self.log_step(f"Kill one more server rank {rank_to_kill} when RF=2")
+            self.fio_cmd._jobs['test'].unlink.value = 1         # pylint: disable=protected-access
+            self.server_managers[0].stop_ranks(rank_to_kill, self.d_log, force=True)
+
             # Read and verify the original data.
+            self.log_step(f"Verify the data is not corrupted after stopping rank {rank_to_kill}.")
             self.fio_cmd.run()
 
         # Pre-teardown: make sure rebuild is done before too-quickly trying to destroy container.
-        self.pool.wait_for_rebuild_to_end()
+        pool.wait_for_rebuild_to_end()
+
+        self.log.info("Test passed")
+
+    def start_online_fio(self, directory, rank_to_kill=None):
+        """Run Fio operation with thread in background.
+
+        Trigger the server failure while Fio is running
+
+        Args:
+            directory (str): directory to use with the fio command
+            rank_to_kill (int, optional): the server rank to kill while IO operation is in progress.
+                Set to None to leave all servers running during IO. Defaults to None.
+        """
+        results_queue = queue.Queue()
+
+        # Create the Fio run thread
+        job = threading.Thread(
+            target=self.write_single_fio_dataset,
+            kwargs={"directory": directory, "results": results_queue})
+
+        # Launch the Fio thread
+        job.start()
+
+        # Kill the server rank while IO operation in progress
+        if rank_to_kill is not None:
+            time.sleep(30)
+            self.server_managers[0].stop_ranks([rank_to_kill], self.d_log, force=True)
+
+        # Wait to finish the thread
+        job.join()
+
+        # Verify the queue result and make sure test has no failure
+        while not results_queue.empty():
+            if results_queue.get() == "FAIL":
+                self.fail("Error running fio as a thread")
+
+    def write_single_fio_dataset(self, directory, results):
+        """Run Fio Benchmark.
+
+        Args:
+            directory (str): directory to use with the fio command
+            results (queue): queue for returning thread results
+        """
+        try:
+            self.fio_cmd.update_directory(directory)
+            self.execute_fio()
+            results.put("PASS")
+        except Exception:       # pylint: disable=broad-except
+            results.put("FAIL")
 
     def test_ec_online_rebuild_fio(self):
         """Jira ID: DAOS-7320.
