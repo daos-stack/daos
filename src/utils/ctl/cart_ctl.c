4fc0a866cc1bbf730c48435cc58f1263c8ae893a
Joseph Moore <joseph.moore@intel.com>
2024-05-12 15:33:06 +0000

    DAOS-13900 cart: Add optional y param to fault injection probability.
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/utils/ctl/cart_ctl.c
+++ src/utils/ctl/cart_ctl.c
@@ -148,7 +148,7 @@ parse_rank_string(char *arg_str, d_rank_t *ranks, int *num_ranks)
 		return;
 	}
 
-	D_DEBUG(DB_TRACE, "arg_str %s\n", arg_str);
+	/* D_DEBUG(DB_TRACE, "arg_str %s\n", arg_str); */
 
 	token = strtok_r(arg_str, ",", &saveptr);
 	while (token != NULL) {
