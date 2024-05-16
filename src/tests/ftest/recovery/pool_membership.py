4a25475f5646da5ab9babcd610979ef763fc9ce7
Makito Kano <makito.kano@intel.com>
2024-05-16 07:34:44 +0000

    DAOS-15589 test: Fix pylint
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-func-hw-test-medium-md-on-ssd: false
    Test-tag: PoolListConsolidationTest PoolMembershipTest
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/recovery/pool_membership.py
+++ src/tests/ftest/recovery/pool_membership.py
@@ -3,6 +3,7 @@
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
+import os
 import time
 
 from ClusterShell.NodeSet import NodeSet
@@ -59,6 +60,33 @@ class PoolMembershipTest(IorTestBase):
             self.fail("Checker didn't detect or repair any inconsistency!")
 
         return repair_reports
+
+    def get_vos_file_path(self, pool):
+        """Get the VOS file path.
+
+        If there are multiple VOS files, returns the first file obtained by "ls".
+
+        Args:
+            pool (TestPool): Pool.
+
+        Returns:
+            str: VOS file path such as /mnt/daos0/<pool_uuid>/vos-0
+
+        """
+        hosts = NodeSet(self.hostlist_servers[0])
+        scm_mount = self.server_managers[0].get_config_value("scm_mount")
+        vos_path = os.path.join(scm_mount, pool.uuid.lower())
+        command = " ".join(["sudo", "ls", vos_path])
+        cmd_out = run_remote(log=self.log, hosts=hosts, command=command)
+
+        # return vos_file
+        for file in cmd_out.output[0].stdout:
+            # Assume the VOS file has "vos" in the file name.
+            if "vos" in file:
+                self.log.info("vos_file: %s", file)
+                return file
+
+        return None  # to appease pylint
 
     def test_orphan_pool_shard(self):
         """Test orphan pool shard.
