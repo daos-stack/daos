d0b155444ac1661d0d0b0be2fb360d018d7a244c
Fan Yong <fan.yong@intel.com>
2024-05-30 13:45:47 +0800

    DAOS-15914 dtx: control DTX RPC to reduce network load
    1. Send DTX batched commit RPCs step by step
    
    Currently, for each DTX batched commit operation, it will handle at
    most 512 DTX entries that may generate DTX commit RPCs to thousands
    of DAOS targets. We will not send out the batched RPCs all together,
    instead, we will send them step by step. After each step, the logic
    will yield and wait until replied, and then next batched RPCs. That
    can avoid holding too much system resources for relative long time.
    It is also helpful to reduce the whole system network peak load and
    the pressure on related targets.
    
    2. Cleanup stale DTX based on global RPC timeout
    
    Originally, DTX cleanup logic will be triggered if the life for some
    stale DTX exceeds the fixed threshold DTX_CLEANUP_THD_AGE_UP (90 sec)
    that maybe smaller than global default RPC timeout, as to related DTX
    refresh RPC for cleanup logic maybe send out too early before related
    modification RPC(s) timeout. It increases network load unnecessarily.
    
    Then we adjust the DTX cleanup threshold based on global default RPC
    timeout value, and give related DTX leader sometime after default RPC
    timeout to commit or abort the DTX. If the DTX is still prepared after
    that, then trigger DTX cleanup to handle potential stale DTX entries.
    
    3. Reorg DTX CoS logic
    
    Reduce the RPCs caused by potential repeated DTX commit. More clear
    names for DTX CoS API.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/cart/crt_context.c
+++ src/cart/crt_context.c
@@ -2007,6 +2007,22 @@ exit:
 	return rc;
 }
 
+int
+crt_context_get_timeout(crt_context_t crt_ctx, uint32_t *timeout_sec)
+{
+	struct crt_context	*ctx = crt_ctx;
+	int			 rc = 0;
+
+	if (crt_ctx == CRT_CONTEXT_NULL) {
+		D_ERROR("NULL context passed\n");
+		rc = -DER_INVAL;
+	} else {
+		*timeout_sec = ctx->cc_timeout_sec;
+	}
+
+	return rc;
+}
+
 /* Force complete the rpc. Used for handling of unreachable rpcs */
 void
 crt_req_force_completion(struct crt_rpc_priv *rpc_priv)
