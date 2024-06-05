bd3f19981f2757d985e5a68389c94509ef73ba7a
Ding Ho <ding-hwa.ho@intel.com>
2024-06-05 03:04:23 +0000

    Update test script and yaml per Ken's comments.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-func-hw-test-medium-md-on-ssd: false
    Test-tag: test_metadata_dup_rpc
    Doc-only: false
    Required-githooks: true
    Signed-off-by: Ding Ho <ding-hwa.ho@intel.com>

--- src/tests/ftest/server/metadata_svc_ops.py
+++ src/tests/ftest/server/metadata_svc_ops.py
@@ -14,7 +14,8 @@ from thread_manager import ThreadManager
 
 
 class DuplicateRpcDetection(TestWithServers):
-    """Create system level tests that cover duplicate rpc detection tests and functionality.
+    """Compare metadata handling performance between pools with and without duplicate rpc
+       detection feature enabled.
 
     Test Class Description:
         Create pools run test with and without duplicate rpc detection feature and verify time
@@ -28,12 +29,12 @@ class DuplicateRpcDetection(TestWithServers):
         super().setUp()
         self.dmg = self.get_dmg_command()
 
-    def metadata_workload_test(self, pool=None, cont_num=1, workload_cycles=5000, test_loops=1):
+    def metadata_workload_test(self, pool=None, num_of_cont=1, workload_cycles=5000, test_loops=1):
         """To create single container and perform metadata workload tests.
 
         Args:
             pool (str): pool handle to create container. Defaults to None.
-            container_num (int): container number to create. Defaults to 1.
+            num_of_cont (int): Number of container to be created. Defaults to 1.
             workload_cycles (int): Number of metadata workload test cycles per test loop.
                 Defaults to 5000.
             test_loops (int): Number of metadata workload test loops. Defaults to 1.
@@ -48,10 +49,10 @@ class DuplicateRpcDetection(TestWithServers):
             if container.daos:
                 container.daos.verbose = False
             container.create()
-            self.log.info("Successfully created #%s container", cont_num)
+            self.log.info("Successfully created #%s container", num_of_cont)
         except (DaosTestError, TestFail) as err:
             self.fail(
-                "#({}.{}) container create failed. err={}".format(pool.label, cont_num, err))
+                "#({}.{}) container create failed. err={}".format(pool.label, num_of_cont, err))
         for ind in range(test_loops):
             start = time.time()
             for _ in range(workload_cycles):
@@ -79,16 +80,17 @@ class DuplicateRpcDetection(TestWithServers):
                time has passed.
             5. Create pool2 with property svc_ops_enable:0.
             6. Create containers by ThreadManager on pool2.
-            7. Repeat test step 4 on pool2.
-            8. Compare metadata workload test time with and without duplicate rpc detection.
+            7. To establish a "baseline" time (without duplicate rpc detection), perform test
+               step 4 on pool2, calculating average time per loop executed.
+            8. Compare all metadata workload times (with duplicate rpc detection) to the average
+               baseline time (without duplicate rpc).
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,medium,md_on_ssd
         :avocado: tags=server,metadata
         :avocado: tags=DuplicateRpcDetection,test_metadata_dup_rpc
         """
-        self.dmg.server_set_logmasks("DEBUG", raise_exception=False)
-        cont_num = self.params.get("number_thread", '/run/metadata/*', default=1)
+        num_of_cont = self.params.get("number_thread", '/run/metadata/*', default=1)
         w_cycles = self.params.get("workload_test_cycles", '/run/metadata/*', default=5000)
         t_loops = self.params.get("test_loops", '/run/metadata/*', default=10)
         threshold_factor = self.params.get("threshold_factor", '/run/metadata/*', default=1.75)
@@ -100,7 +102,7 @@ class DuplicateRpcDetection(TestWithServers):
         container_manager = ThreadManager(
             self.metadata_workload_test, self.get_remaining_time() - 30)
         container_manager.add(
-            pool=pool1, cont_num=cont_num, workload_cycles=w_cycles, test_loops=t_loops)
+            pool=pool1, num_of_cont=num_of_cont, workload_cycles=w_cycles, test_loops=t_loops)
 
         self.log_step("Run specified metadata workload cycles in multiple test loops.")
         results = container_manager.run()
@@ -117,11 +119,13 @@ class DuplicateRpcDetection(TestWithServers):
         container_manager = ThreadManager(
             self.metadata_workload_test, self.get_remaining_time() - 30)
         container_manager.add(
-            pool=self.pool, cont_num=cont_num, workload_cycles=w_cycles, test_loops=t_loops)
+            pool=self.pool, num_of_cont=num_of_cont, workload_cycles=w_cycles, test_loops=t_loops)
 
-        self.log_step("Run specified metadata workload cycles in multiple test loops on pool2.")
+        self.log_step(
+            "To establish a baseline time without duplicate rpc detection, ",
+            "calculating average time per loop executed on pool2.")
         base_results = container_manager.run()
-        average_time = statistics.mean(base_results[0].result)
+        base_average_time = statistics.mean(base_results[0].result)
         num_failed = len(list(filter(lambda r: not r.passed, base_results)))
         if num_failed > 0:
             self.fail('#{} container create threads failed'.format(num_failed))
@@ -130,10 +134,10 @@ class DuplicateRpcDetection(TestWithServers):
             "Compare metadata workload test time with and without duplicate rpc detection.")
         self.log.info("pool1 results = %s", results[0].result)
         self.log.info("baseline results = %s", base_results[0].result)
-        self.log.info("average baseline result= %s", average_time)
+        self.log.info("average baseline result= %s", base_average_time)
         for result in results[0].result:
-            if result > average_time * threshold_factor:
+            if result > base_average_time * threshold_factor:
                 self.fail(
-                    "#Dup rpc detection time consuming {} > threshold {} * {}".format(
-                        result, average_time, threshold_factor))
+                    "#Dup rpc detection time {} > baseline_time {} * threshold_factor {}".format(
+                        result, base_average_time, threshold_factor))
         self.log.info("Test passed")
