f2c2fbe84a6efa046dca3e118aa13c679ecef1a3
Michael MacDonald <mjmac@google.com>
2024-06-06 14:15:59 +0000

    WIP: add pciutils binding
    Required-githooks: true
    
    Change-Id: Ib38d7d51ceb6d28f245c8502b7a93490258623e5
    Signed-off-by: Michael MacDonald <mjmac@google.com>

---
+++ src/control/lib/hardware/pciutils/provider.go
@@ -0,0 +1,69 @@
+//
+// (C) Copyright 2024 Intel Corporation.
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+//
+
+package pciutils
+
+import (
+	"context"
+	"errors"
+
+	"github.com/daos-stack/daos/src/control/lib/hardware"
+)
+
+type ctxKey string
+
+const (
+	accessKey ctxKey = "pciutilsAccess"
+)
+
+type (
+	accessProvider interface {
+		PCIDeviceFromConfig(cfgBytes []byte, dev *hardware.PCIDevice) error
+		Cleanup()
+	}
+)
+
+func Init(parent context.Context) (context.Context, error) {
+	api, err := initAPI()
+	if err != nil {
+		return nil, err
+	}
+
+	return context.WithValue(parent, accessKey, api), nil
+}
+
+func accessFromContext(ctx context.Context) (accessProvider, error) {
+	ap, ok := ctx.Value(accessKey).(accessProvider)
+	if !ok {
+		return nil, errors.New("pciutils access not initialized")
+	}
+
+	return ap, nil
+}
+
+func Fini(ctx context.Context) {
+	ap, err := accessFromContext(ctx)
+	if err != nil {
+		return
+	}
+
+	ap.Cleanup()
+}
+
+func PCIDeviceFromConfig(ctx context.Context, cfgBytes []byte) (*hardware.PCIDevice, error) {
+	dev := &hardware.PCIDevice{}
+
+	ap, err := accessFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ap.PCIDeviceFromConfig(cfgBytes, dev); err != nil {
+		return nil, err
+	}
+
+	return dev, nil
+}
---
+++ src/control/lib/hardware/pciutils/provider_test.go
@@ -0,0 +1,41 @@
+package pciutils_test
+
+import (
+	"testing"
+
+	"github.com/daos-stack/daos/src/control/common/test"
+	"github.com/daos-stack/daos/src/control/lib/hardware/pciutils"
+)
+
+func TestProvider(t *testing.T) {
+	ctx, err := pciutils.Init(test.Context(t))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer pciutils.Fini(ctx)
+
+	cfgBytes := []byte(`00: 86 80 53 09 06 04 10 00 01 02 08 01 00 00 00 00
+10: 04 00 00 bc 00 00 00 00 00 00 00 00 00 00 00 00
+20: 00 00 00 00 00 00 00 00 00 00 00 00 90 15 a8 00
+30: 00 00 00 00 40 00 00 00 00 00 00 00 00 01 00 00
+40: 01 50 03 00 08 00 00 00 00 00 00 00 00 00 00 00
+50: 11 60 1f 00 00 20 00 00 00 30 00 00 00 00 00 00
+60: 10 00 02 00 a1 85 00 10 10 29 09 00 43 6c 41 00
+70: 00 00 43 00 00 00 00 00 00 00 00 00 00 00 00 00
+80: 00 00 00 00 1f 00 00 00 00 00 00 00 0e 00 00 00
+90: 03 00 1f 00 00 00 00 00 00 00 00 00 00 00 00 00
+a0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+b0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+c0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+d0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+e0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+f0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+100: 01 00 01 15 00 00 00 00 00 00 00 00 30 20 06 00`)
+
+	dev, err := pciutils.PCIDeviceFromConfig(ctx, cfgBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Fatalf("dev: %+v", dev)
+}
