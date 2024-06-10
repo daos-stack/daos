7504b6ca4b3f885fdcf4e218c8b1aced5658fa1d
Michael MacDonald <mjmac@google.com>
2024-06-10 14:39:45 +0000

    WIP: can read device name now
    Required-githooks: true
    
    Change-Id: Ieb11cecc2ceb02e748617220fab7ce2a291fd994
    Signed-off-by: Michael MacDonald <mjmac@google.com>

--- src/control/lib/hardware/pciutils/bindings.go
+++ src/control/lib/hardware/pciutils/bindings.go
@@ -58,6 +58,14 @@ func (api *api) PCIDeviceFromConfig(cfgBytes []byte, dev *hardware.PCIDevice) er
 		return errors.New("nil dev")
 	}
 
+	// NB: We have to do the pci_init() after we've set the access method,
+	// so the fancy initAPI() flow doesn't work.
+	C.pci_cleanup(api.access)
+	api.access = C.pci_alloc()
+	if api.access == nil {
+		return errors.New("pci_alloc() failed")
+	}
+
 	tmpFile, err := os.CreateTemp("", "pciutils")
 	if err != nil {
 		return err
@@ -75,18 +83,24 @@ func (api *api) PCIDeviceFromConfig(cfgBytes []byte, dev *hardware.PCIDevice) er
 	defer C.free(unsafe.Pointer(cFileName))
 	C.pci_set_param(api.access, cParam, cFileName)
 	api.access.method = C.PCI_ACCESS_DUMP
+	C.pci_init(api.access)
 
 	C.pci_scan_bus(api.access)
 	var pciDev *C.struct_pci_dev
-	var nameBuf [1024]*C.char
+	var nameBuf [1024]C.char
 	var name *C.char
 
 	for pciDev = api.access.devices; pciDev != nil; pciDev = pciDev.next {
-		C.pci_fill_info(pciDev, C.PCI_FILL_IDENT|C.PCI_FILL_BASES|C.PCI_FILL_CLASS)
-		fmt.Fprintf(os.Stderr, "Vendor: %04x, Device: %04x\n", pciDev.vendor_id, pciDev.device_id)
+		C.pci_fill_info(pciDev, C.PCI_FILL_IDENT|C.PCI_FILL_BASES|C.PCI_FILL_CLASS|C.PCI_FILL_CAPS)
+		fmt.Fprintf(os.Stderr, "Vendor: %04x, Device: %04x, Class: %08x, Cap: %+v\n", pciDev.vendor_id, pciDev.device_id, pciDev.device_class, pciDev.first_cap)
 
-		name = C.lookup_pci_name(api.access, nameBuf[0], C.int(len(nameBuf)), C.PCI_LOOKUP_DEVICE, pciDev.vendor_id, pciDev.device_id)
+		dev.PCIAddr.Bus = uint8(pciDev.bus)
+		dev.PCIAddr.Device = uint8(pciDev.dev)
+		dev.PCIAddr.Function = uint8(pciDev._func)
+
+		name = C.lookup_pci_name(api.access, &nameBuf[0], C.int(len(nameBuf)), C.PCI_LOOKUP_DEVICE, pciDev.vendor_id, pciDev.device_id)
 		dev.Name = C.GoString(name)
+
 	}
 
 	return nil
