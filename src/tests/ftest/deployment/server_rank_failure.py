a16dbe0f13bbf1c643e88d710210e33c57e23391
Makito Kano <makito.kano@intel.com>
2024-06-14 05:34:43 +0000

    DAOS-16014 test: Fix pylint
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-func-hw-test-medium-md-on-ssd: false
    Test-tag: test_server_rank_failure_with_rp test_server_rank_failure_with_ec
    Signed-off-by: Makito Kano <makito.kano@intel.com>

--- src/tests/ftest/deployment/server_rank_failure.py
+++ src/tests/ftest/deployment/server_rank_failure.py
@@ -3,20 +3,18 @@
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
-from collections import defaultdict
 import os
 import threading
 import time
+from collections import defaultdict
 
 from ClusterShell.NodeSet import NodeSet
-
 from command_utils_base import CommandFailure
 from general_utils import report_errors
 from ior_test_base import IorTestBase
 from ior_utils import IorCommand
 from job_manager_utils import get_job_manager
 from run_utils import stop_processes
-
 
 HOST_GROUPS = []
 GROUP_1_HOSTS = list(NodeSet("aurora-daos-[0001-0128]"))
