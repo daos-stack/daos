cdfb648aed91b441473eefa03ea3f315da2f7e50
Ken Cain <kenneth.c.cain@intel.com>
2024-06-26 10:56:41 -0400

    DAOS-16111 rebuild: uniform identifier in logs part 1 (#14383)
    To the extent possible in the rebuild code execution flow, when rebuild emits log messages, include a uniform rebuild operation identifier in those messages. This covers activities across all pool storage engines (including the pool service leader), system and per-target threads/xstreams, and dynamically spawned user-level threads.
    
    The motivation is to enable some amount of automated searching through logfiles for all (or specific) rebuilds that occurred during execution, and speed up DAOS engineer analysis/interpretation of the logs.
    
    The baseline format (defined in the DF_RB macro) is:
    "rb=" DF_UUID "/%u/%u/%s"
    and corresponds to:
    <pool_uuid>/<rebuild_ver>/<rebuild_gen>/
    
    A verbose format (defined in the DF_RBF macro) adds the following (for <leader_rank>/)
    " ld=%u/" DF_U64
    
    Various DP_RB_* and DP_RBF_* macros are defined to specify the arguments to go with the DF_RB and DF_RBF formats, given some common rebuild implementation structures such as:
    struct rebuild_global_pool_tracker
    struct rebuild_tgt_pool_tracker
    struct rebuild_scan_in (REBUILD_OBJECTS_SCAN RPC input)
    struct migrate_query_arg
    
    This initial patch covers the pool service leader execution in functions (and those that they invoke) such as:
    rebuild_ults()
    rebuild_task_ult()
    rebuild_leader_start()
    rebuild_leader_status_check()
    rebuild_leader_status_notify().
    
    And this patch covers "scan side" execution in all pool storage engines (including the leader), in functions such as:
    rebuild_tgt_scan_handler()
    rebuild_tgt_status_check_ult()
    ds_migrate_query_status()
    migrate_check_one()
    dss_rebuild_check_one()
    rebuild_scan_leader()
    rebuild_scanner()
    rebuild_objects_send_ult()
    rebuild_scan_done()
    
    Signed-off-by: Kenneth Cain <kenneth.c.cain@intel.com>
    Signed-off-by: Kenneth Cain <kenneth.cain@hpe.com>

--- src/include/daos_srv/rebuild.h
+++ src/include/daos_srv/rebuild.h
@@ -1,5 +1,6 @@
 /**
  * (C) Copyright 2017-2023 Intel Corporation.
+ * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -55,6 +56,17 @@ typedef enum {
 	DP_UUID((rpt)->rt_pool_uuid), (rpt)->rt_rebuild_ver, (rpt)->rt_rebuild_gen,                \
 	    RB_OP_STR((rpt)->rt_rebuild_op)
 #define DP_RBF_RPT(rpt) DP_RB_RPT(rpt), (rpt)->rt_leader_rank, (rpt)->rt_leader_term
+
+/* arguments for log rebuild identifier given a struct rebuild_scan_in *rsin */
+#define DP_RB_RSI(rsi)                                                                             \
+	DP_UUID((rsi)->rsi_pool_uuid), (rsi)->rsi_rebuild_ver, (rsi)->rsi_rebuild_gen,             \
+	    RB_OP_STR((rsi)->rsi_rebuild_op)
+#define DP_RBF_RSI(rsi) DP_RB_RSI(rsi), (rsi)->rsi_master_rank, (rsi)->rsi_leader_term
+
+/* arguments for log rebuild identifier given a struct migrate_query_arg * */
+#define DP_RB_MQA(mqa)                                                                             \
+	DP_UUID((mqa)->pool_uuid), (mqa)->version, (mqa)->generation, RB_OP_STR((mqa)->rebuild_op)
+#define DP_RBF_MQA(mqa) DP_RB_MQA(mqa), (mqa)->leader_rank, (mqa)->leader_term
 
 int ds_rebuild_schedule(struct ds_pool *pool, uint32_t map_ver,
 			daos_epoch_t stable_eph, uint32_t layout_version,
