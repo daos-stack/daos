60c698d9f9b1bdbbae212bde8dec96d6839fe89d
Dalton Bohning <dalton.bohning@intel.com>
2024-07-11 15:18:57 +0000

    DAOS-16215 test: verify dbohning/daos-16215
    Test-tag: DaosBuild
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    Required-githooks: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- src/tests/ftest/dfuse/daos_build.py
+++ src/tests/ftest/dfuse/daos_build.py
@@ -233,7 +233,7 @@ class DaosBuild(TestWithServers):
         preload_cmd = ';'.join(envs)
 
         cmds = ['python3 -m venv {}/venv'.format(mount_dir),
-                'git clone https://github.com/daos-stack/daos.git {}'.format(build_dir),
+                'git clone https://github.com/daos-stack/daos.git {} -b dbohning/daos-16215-verify'.format(build_dir),
                 'git -C {} submodule init'.format(build_dir),
                 'git -C {} submodule update'.format(build_dir),
                 'python3 -m pip install pip --upgrade',
