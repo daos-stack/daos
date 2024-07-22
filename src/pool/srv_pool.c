2ab559a5dd6eab3ce904db60ad63aba4cb1678dc
Wang Shilong <shilong.wang@intel.com>
2024-07-21 23:48:08 -0400

    deny drain and extend operations
    Required-githooks: true
    
    Signed-off-by: Wang Shilong <shilong.wang@intel.com>

--- src/pool/srv_pool.c
+++ src/pool/srv_pool.c
@@ -6863,6 +6863,15 @@ pool_svc_update_map(struct pool_svc *svc, crt_opcode_t opc, bool exclude_rank,
 	daos_epoch_t			rebuild_eph = d_hlc_get();
 	uint64_t			delay = 2;
 
+	/*
+	 * For simplicity, deny drain and extend operations in
+	 * the no_data_sync reinteration mode.
+	 */
+	if (svc->ps_pool->sp_reint_mode == DAOS_REINT_MODE_NO_DATA_SYNC) {
+		if (opc != MAP_REINT && opc != MAP_EXCLUDE)
+			return -DER_NO_PERM;
+	}
+
 	rc = pool_svc_update_map_internal(svc, opc, exclude_rank, extend_rank_list,
 					  extend_domains_nr, extend_domains,
 					  &target_list, list, hint, &updated,
@@ -6895,9 +6904,7 @@ pool_svc_update_map(struct pool_svc *svc, crt_opcode_t opc, bool exclude_rank,
 
 	if (svc->ps_pool->sp_reint_mode == DAOS_REINT_MODE_NO_DATA_SYNC) {
 		D_DEBUG(DB_MD, "self healing is disabled for no_data_sync reintegration mode.\n");
-		if (opc == MAP_EXCLUDE) {
-			rc = ds_pool_tgt_exclude(svc->ps_pool->sp_uuid, &target_list);
-		} else if (opc == MAP_REINT) {
+		if (opc == MAP_REINT) {
 			rc = ds_pool_tgt_finish_rebuild(svc->ps_pool->sp_uuid, &target_list);
 			if (rc)
 				D_INFO("mark target %d of "DF_UUID " as UPIN: "DF_RC"\n",
