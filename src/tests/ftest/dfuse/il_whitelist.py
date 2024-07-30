06b26fb33f4ace4ab11de25c5c3e7d6c1379bb74
Lei Huang <lei.huang@intel.com>
2024-07-30 17:12:44 +0000

    fix spelling and use EnvironmentVariables to set env
    Skip-unit-tests: true
    Test-tag: ILWhiteList
    
    Required-githooks: true
    
    Signed-off-by: Lei Huang <lei.huang@intel.com>

--- src/tests/ftest/dfuse/il_whitelist.py
+++ src/tests/ftest/dfuse/il_whitelist.py
@@ -7,6 +7,7 @@
 import os
 
 from apricot import TestWithServers
+from command_utils_base import EnvironmentVariables
 from dfuse_utils import get_dfuse, start_dfuse
 from host_utils import get_local_host
 from run_utils import run_remote
@@ -26,11 +27,10 @@ class ILWhiteList(TestWithServers):
             Run whitelist_test.
             Check the number of daos_init() with interception library
         """
+        env = EnvironmentVariables()
         if il_lib is not None:
-            lib_path = os.path.join(self.prefix, "lib64", il_lib)
-            env_str = f"export LD_PRELOAD={lib_path}; "
-        else:
-            env_str = ""
+            env["LD_PRELOAD"] = os.path.join(self.prefix, "lib64", il_lib)
+
         exe_path = os.path.join(self.prefix, "lib", 'daos/TESTING/tests/whitelist_test')
 
         pool = self.get_pool(connect=False)
@@ -40,24 +40,25 @@ class ILWhiteList(TestWithServers):
         dfuse_hosts = get_local_host()
         dfuse = get_dfuse(self, dfuse_hosts)
         start_dfuse(self, dfuse, pool, container)
-        fuse_root_dir = dfuse.mount_dir.value
 
-        env_str += f'export D_DFUSE_MNT={fuse_root_dir}; '
-        env_str += 'export D_LOG_MASK=DEBUG; export DD_SUBSYS=il; export DD_MASK=DEBUG; '
+        env["D_DFUSE_MNT"] = dfuse.mount_dir.value
+        env["D_LOG_MASK"] = "DEBUG"
+        env["DD_SUBSYS"] = "il"
+        env["DD_MASK"] = "DEBUG"
 
         if bypass_all:
-            env_str += 'export D_IL_BYPASS_ALL_LIST=whitelist_test; '
+            env["D_IL_BYPASS_ALL_LIST"] = "whitelist_test"
         if bypass:
-            env_str += 'export D_IL_BYPASS_LIST=whitelist_test; '
+            env["D_IL_BYPASS_LIST"] = "whitelist_test"
 
-        result = run_remote(self.log, dfuse_hosts, env_str + exe_path)
+        result = run_remote(self.log, dfuse_hosts, env.to_export_str() + exe_path)
         output = "\n".join(result.all_stdout.values())
         lines = output.split('\n')
         num_daos_init = 0
         for line in lines:
             if "called daos_init()" in line:
                 num_daos_init += 1
-        print(f'num_daos_init = {num_daos_init}')
+        self.log.info('num_daos_init = %d', num_daos_init)
         return num_daos_init
 
     def test_whitelist_pil4dfs(self):
@@ -70,7 +71,7 @@ class ILWhiteList(TestWithServers):
         """
         num_daos_init = self.run_test(il_lib='libpil4dfs.so')
         if num_daos_init != 10:
-            self.fail(f"Test failed: num_daos_init = {num_daos_init}. Expected 10.\n")
+            self.fail(f"Test failed: num_daos_init = {num_daos_init}. Expected 10.")
 
     def test_whitelist_pil4dfs_bypass(self):
         """Jira ID: DAOS-15583.
@@ -82,7 +83,7 @@ class ILWhiteList(TestWithServers):
         """
         num_daos_init = self.run_test(il_lib='libpil4dfs.so', bypass=True)
         if num_daos_init != 5:
-            self.fail(f"Test failed: num_daos_init = {num_daos_init}. Expected 5.\n")
+            self.fail(f"Test failed: num_daos_init = {num_daos_init}. Expected 5.")
 
     def test_whitelist_pil4dfs_bypassall(self):
         """Jira ID: DAOS-15583.
@@ -94,4 +95,4 @@ class ILWhiteList(TestWithServers):
         """
         num_daos_init = self.run_test(il_lib='libpil4dfs.so', bypass_all=True)
         if num_daos_init != 0:
-            self.fail(f"Test failed: num_daos_init = {num_daos_init}. Expected 0.\n")
+            self.fail(f"Test failed: num_daos_init = {num_daos_init}. Expected 0.")
