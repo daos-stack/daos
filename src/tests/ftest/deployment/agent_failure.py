c9beaf9c7c438155995f2f87306627ccf6283de6
Dalton Bohning <dalton.bohning@intel.com>
2024-08-09 12:59:06 -0700

    DAOS-16229 test: support dynamic stonewall file (#14771)
    support dynamic stonewall file for ior and mdtest by using
    DAOS_TEST_LOG_DIR
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- src/tests/ftest/deployment/agent_failure.py
+++ src/tests/ftest/deployment/agent_failure.py
@@ -31,7 +31,7 @@ class AgentFailure(IorTestBase):
             file_name (str): File name used for self.ior_cmd.test_file.
             clients (list): Client hostnames to run IOR from.
         """
-        ior_cmd = IorCommand()
+        ior_cmd = IorCommand(self.test_env.log_dir)
         ior_cmd.get_params(self)
         ior_cmd.set_daos_params(self.pool, self.container.identifier)
         testfile = os.path.join(os.sep, file_name)
