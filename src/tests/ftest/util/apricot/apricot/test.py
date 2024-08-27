1464270b827c899fe59b88c83db8273a23306c1e
Phil Henderson <phillip.henderson@intel.com>
2024-08-27 16:13:04 -0400

    DAOS-16265 test: Fix erasurecode/rebuild_fio.py out of space
    The erasurecode/rebuild_fio.py test runs out of space in self.test_dir
    due to the same path being used for the control metadata path in MD on
    SSD mode.  The test log file is also quite large with 24 test variants.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: EcodFioRebuild
    Skip-func-hw-test-large-md-on-ssd: false
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/util/apricot/apricot/test.py
+++ src/tests/ftest/util/apricot/apricot/test.py
@@ -769,9 +769,13 @@ class TestWithServers(TestWithoutServers):
         self.log.debug("Common test directory (%s) contents:", os.path.dirname(self.test_dir))
         all_hosts = include_local_host(self.host_info.all_hosts)
         test_dir_parent = os.path.dirname(self.test_dir)
-        result = run_remote(self.log, all_hosts, f"df -h {test_dir_parent}")
-        if int(max(re.findall(r" ([\d+])% ", result.joined_stdout) + ["0"])) > 90:
-            run_remote(self.log, all_hosts, f"du -sh {test_dir_parent}/*")
+        _result = run_remote(self.log, all_hosts, f"df -h {test_dir_parent}")
+        _details = NodeSet()
+        for _host, _stdout in _result.all_stdout.items():
+            if int(max(re.findall(r"\s+([\d]+)%\s+", _stdout) + ["0"])) > 90:
+                _details.add(_host)
+        if _details:
+            run_remote(self.log, _details, f"du -sh {test_dir_parent}/*")
         self.log.info("-" * 100)
 
         if not self.start_servers_once or self.name.uid == 1:
