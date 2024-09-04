2fee48a33207f08d8d3e1e491136d8410e668e3f
Phil Henderson <phillip.henderson@intel.com>
2024-09-04 17:46:19 -0400

    Adding restarting systemd-journald
    Test-tag: test_setup_vm
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/scripts/setup_nodes.sh
+++ src/tests/ftest/scripts/setup_nodes.sh
@@ -114,6 +114,7 @@ loginctl enable-linger
 # allow the jenkins user to view journalctl entries for systemctl services it starts
 sudo usermod -aG systemd-journal jenkins
 sudo sed -i 's/.*Storage=.*/Storage=persistent/g' /etc/systemd/journald.conf
+sudo systemctl restart systemd-journald
 
 rm -rf "${TEST_TAG_DIR:?}/"
 mkdir -p "$TEST_TAG_DIR/"
