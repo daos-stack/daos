da701b4ade6487c45a2d6acbbec650dee8ff4143
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-09-06 09:13:43 +0200

    Fix typo in pkg name
    PR-repos: pmdk@PR-38:11
    
    Priority: 2
    Cancel-prev-build: false
    
    Skip-build-leap15-rpm: true
    Skip-build-leap15-icc: true
    Skip-build-el9-rpm: true
    Skip-nlt: true
    Skip-unit-tests: true
    Skip-func-test-vm: true
    Skip-test-rpms: true
    
    Allow-unstable-test: true
    Skip-func-hw-test-large: true
    Skip-func-hw-test-medium: false
    Skip-func-hw-test-medium-verbs-provider: true
    
    Required-githooks: true
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- utils/scripts/install-ubuntu.sh
+++ utils/scripts/install-ubuntu.sh
@@ -29,7 +29,7 @@ apt-get install \
     libcapstone-dev \
     libcmocka-dev \
     libcunit1-dev \
-    libdaxctl-devel \
+    libdaxctl-dev \
     libfuse3-dev \
     libhwloc-dev \
     libibverbs-dev \
