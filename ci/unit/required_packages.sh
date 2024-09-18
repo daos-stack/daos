d9c2ae0fcbf71ee885d5d2ffb505263a884aae72
Brian J. Murrell <brian.murrell@intel.com>
2024-09-18 13:51:34 -0400

    Need a space between names
    Skip-build: false
    Skip-python-bandit: true
    Skip-build-el8-rpm: true
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    Skip-build-ubuntu20-rpm: true
    Skip-test: true
    Skip-PR-comments: true
    PR-repos: pmdk@PR-38:11
    
    Required-githooks: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/unit/required_packages.sh
+++ ci/unit/required_packages.sh
@@ -25,21 +25,21 @@ pkgs="argobots            \
       valgrind-devel"
 
 if [[ $distro = el* ]]; then
-    pkgs+="boost-python3$PY_MINOR_VER-devel \
-           gotestsum                        \
-           libipmctl-devel                  \
-           libisa-l-devel                   \
-           libpmem                          \
-           libpmemobj                       \
-           numactl-devel"
+    pkgs+=" boost-python3$PY_MINOR_VER-devel \
+            gotestsum                        \
+            libipmctl-devel                  \
+            libisa-l-devel                   \
+            libpmem                          \
+            libpmemobj                       \
+            numactl-devel"
 elif [[ $distro = leap* ]]; then
     # need https://artifactory.dc.hpdd.intel.com/ui/repos/tree/General/opensuse-proxy%2Frepositories%2Fdevel:%2Flanguages:%2Fgo to install gotestsum
-    pkgs+="boost-devel  \
-           ipmctl-devel \
-           isa-l-devel  \
-           libpmem1     \
-           libpmemobj1  \
-           libnuma-devel"
+    pkgs+=" boost-devel  \
+            ipmctl-devel \
+            isa-l-devel  \
+            libpmem1     \
+            libpmemobj1  \
+            libnuma-devel"
 fi
 
 # output with trailing newline suppressed
