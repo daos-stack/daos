6490b6399c6f962cff81a096dfecd62e3f96027c
Brian J. Murrell <brian.murrell@intel.com>
2024-09-18 13:07:41 -0400

    Update packages needed for UT on Leap 15
    Skip-build: false
    Skip-python-bandit: true
    Skip-build-el8-rpm: true
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    Skip-build-ubuntu20-rpm: true
    Skip-test: true
    Skip-PR-comments: true
    PR-repos: pmdk@PR-38:11
    
    Required-githooks: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- ci/unit/required_packages.sh
+++ ci/unit/required_packages.sh
@@ -2,35 +2,45 @@
 
 set -eux
 
+distro="$1"
 # No longer used but provided by pipeline-lib
-# distro="$1"
 # quick_build="${2:-false}"
 
 OPENMPI_VER=""
 PY_MINOR_VER=""
 
-pkgs="argobots                         \
-      boost-python3$PY_MINOR_VER-devel \
-      capstone                         \
-      fuse3                            \
-      fuse3-libs                       \
-      gotestsum                        \
-      hwloc-devel                      \
-      libipmctl-devel                  \
-      libisa-l-devel                   \
-      libfabric-devel                  \
-      libpmem                          \
-      libpmemobj                       \
-      libyaml-devel                    \
-      numactl                          \
-      numactl-devel                    \
-      openmpi$OPENMPI_VER              \
-      patchelf                         \
-      pciutils-devel                   \
-      pmix                             \
-      protobuf-c                       \
-      spdk-devel                       \
+pkgs="argobots            \
+      capstone            \
+      fuse3               \
+      hwloc-devel         \
+      libfabric-devel     \
+      libyaml-devel       \
+      numactl             \
+      openmpi$OPENMPI_VER \
+      patchelf            \
+      pciutils-devel      \
+      pmix                \
+      protobuf-c          \
+      spdk-devel          \
       valgrind-devel"
+
+if [[ $distro = el* ]]; then
+    pkgs+="boost-python3$PY_MINOR_VER-devel \
+           gotestsum                        \
+           libipmctl-devel                  \
+           libisa-l-devel                   \
+           libpmem                          \
+           libpmemobj                       \
+           numactl-devel"
+elif [[ $distro = leap* ]]; then
+    # need https://artifactory.dc.hpdd.intel.com/ui/repos/tree/General/opensuse-proxy%2Frepositories%2Fdevel:%2Flanguages:%2Fgo to install gotestsum
+    pkgs+="boost-devel  \
+           ipmctl-devel \
+           isa-l-devel  \
+           libpmem1     \
+           libpmemobj1  \
+           libnuma-devel"
+fi
 
 # output with trailing newline suppressed
 echo  -e "$pkgs\c"
