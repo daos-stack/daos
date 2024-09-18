4455870b1f2939feb1f6e5bf23a0270295b2b818
Alexander A Oganezov <alexander.a.oganezov@intel.com>
2024-09-18 03:17:54 +0000

    - copy deadline
    Required-githooks: true
    
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/cart/crt_hg_proc.c
+++ src/cart/crt_hg_proc.c
@@ -494,6 +494,7 @@ crt_hg_header_copy(struct crt_rpc_priv *in, struct crt_rpc_priv *out)
 
 	out->crp_req_hdr = in->crp_req_hdr;
 	out->crp_reply_hdr.cch_hlc = in->crp_reply_hdr.cch_hlc;
+	out->crp_deadline_sec = in->crp_deadline_sec;
 
 	if (!(out->crp_flags & CRT_RPC_FLAG_COLL))
 		return;
