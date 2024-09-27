88b15b6b7e06151bad8d26d4ac31128714c43fa2
Xuezhao Liu <xuezhao.liu@intel.com>
2024-09-27 10:17:56 +0000

    DAOS-16572 rebuild: some refine and log
    Signed-off-by: Xuezhao Liu <xuezhao.liu@intel.com>

--- src/rebuild/scan.c
+++ src/rebuild/scan.c
@@ -874,7 +874,8 @@ rebuild_container_scan_cb(daos_handle_t ih, vos_iter_entry_t *entry,
 		D_ASSERTF(rpt->rt_pool->sp_rebuilding >= 0, DF_UUID" rebuilding %d\n",
 			  DP_UUID(rpt->rt_pool_uuid), rpt->rt_pool->sp_rebuilding);
 			/* Wait for EC aggregation to abort before discard the object */
-		D_INFO(DF_UUID" wait for ec agg abort.\n", DP_UUID(entry->ie_couuid));
+		D_INFO(DF_UUID" wait for ec agg abort, rebuilding %d.\n",
+		       DP_UUID(entry->ie_couuid), rpt->rt_pool->sp_rebuilding);
 		dss_sleep(1000);
 		if (rpt->rt_abort || rpt->rt_finishing) {
 			D_DEBUG(DB_REBUILD, DF_CONT" rebuild op %s ver %u abort %u/%u.\n",
@@ -919,7 +920,7 @@ close:
 		ds_cont_child_put(cont_child);
 	}
 
-	D_DEBUG(DB_REBUILD, DF_UUID"/"DF_UUID" iterate cont done: "DF_RC"\n",
+	D_INFO(DF_UUID"/"DF_UUID" iterate cont done: "DF_RC"\n",
 		DP_UUID(rpt->rt_pool_uuid), DP_UUID(entry->ie_couuid),
 		DP_RC(rc));
 
@@ -1074,8 +1075,7 @@ rebuild_scan_leader(void *data)
 		}
 	}
 
-	D_DEBUG(DB_REBUILD, "rebuild scan collective "DF_UUID" begin.\n",
-		DP_UUID(rpt->rt_pool_uuid));
+	D_INFO("rebuild scan collective "DF_UUID" begin.\n", DP_UUID(rpt->rt_pool_uuid));
 
 	rc = ds_pool_thread_collective(rpt->rt_pool_uuid, PO_COMP_ST_NEW | PO_COMP_ST_DOWN |
 				       PO_COMP_ST_DOWNOUT, rebuild_scanner, rpt,
@@ -1083,8 +1083,7 @@ rebuild_scan_leader(void *data)
 	if (rc)
 		D_GOTO(out, rc);
 
-	D_DEBUG(DB_REBUILD, "rebuild scan collective "DF_UUID" done.\n",
-		DP_UUID(rpt->rt_pool_uuid));
+	D_INFO("rebuild scan collective "DF_UUID" done.\n", DP_UUID(rpt->rt_pool_uuid));
 
 	ABT_mutex_lock(rpt->rt_lock);
 	rc = ds_pool_task_collective(rpt->rt_pool_uuid, PO_COMP_ST_NEW | PO_COMP_ST_DOWN |
@@ -1096,8 +1095,8 @@ rebuild_scan_leader(void *data)
 		D_GOTO(out, rc);
 	}
 
-	D_DEBUG(DB_REBUILD, DF_UUID" sent objects to initiator: "DF_RC"\n",
-		DP_UUID(rpt->rt_pool_uuid), DP_RC(rc));
+	D_INFO(DF_UUID" sent objects to initiator: "DF_RC"\n",
+	       DP_UUID(rpt->rt_pool_uuid), DP_RC(rc));
 out:
 	tls = rebuild_pool_tls_lookup(rpt->rt_pool_uuid, rpt->rt_rebuild_ver,
 				      rpt->rt_rebuild_gen);
@@ -1227,6 +1226,8 @@ rebuild_tgt_scan_handler(crt_rpc_t *rpc)
 	}
 
 	rpt->rt_pool->sp_rebuilding++; /* reset in rebuild_tgt_fini */
+	D_INFO(DF_UUID": sp_rebuilding inc to %d\n",
+	       DP_UUID(rpt->rt_pool->sp_uuid), rpt->rt_pool->sp_rebuilding);
 
 	rpt_get(rpt);
 	/* step-3: start scan leader */
