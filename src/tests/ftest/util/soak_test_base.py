af7197fdb5dde55235c45f5672ac724547035dfc
Maureen Jean <maureen.jean@intel.com>
2024-10-09 10:52:14 -0400

    fix linting issues
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: soak_smoke
    
    Required-githooks: true
    Skipped-githooks: flake,pylint
    
    Signed-off-by: Maureen Jean <maureen.jean@intel.com>

--- src/tests/ftest/util/soak_test_base.py
+++ src/tests/ftest/util/soak_test_base.py
@@ -25,8 +25,8 @@ from run_utils import run_local, run_remote
 from soak_utils import (SoakTestError, add_pools, build_job_script, cleanup_dfuse,
                         create_app_cmdline, create_dm_cmdline, create_fio_cmdline,
                         create_ior_cmdline, create_macsio_cmdline, create_mdtest_cmdline,
-                        create_racer_cmdline, ddhhmmss_format, get_daos_server_logs, get_harassers,
-                        get_journalctl, launch_exclude_reintegrate, launch_extend, launch_reboot,
+                        create_racer_cmdline, ddhhmmss_format, get_harassers,
+                        launch_exclude_reintegrate, launch_extend, launch_reboot,
                         launch_server_stop_start, launch_snapshot, launch_vmd_identify_check,
                         reserved_file_copy, run_event_check, run_metrics_check, run_monitor_check)
 
