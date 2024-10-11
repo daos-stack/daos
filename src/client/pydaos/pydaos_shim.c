0737dd5da28444be94a0a752efe9175150ef4379
Johann Lombardi <johann.lombardi@gmail.com>
2024-10-11 15:57:10 +0200

    DAOS-16637 pydaos: disable atfork handler (#15292)
    Disable call to pthread_atfork and daos_reinit() in pydaos
    until DAOS-16637 is understood.
    
    Signed-off-by: Johann Lombardi <johann.lombardi@gmail.com>

--- src/client/pydaos/pydaos_shim.c
+++ src/client/pydaos/pydaos_shim.c
@@ -70,6 +70,7 @@ static daos_handle_t eq;
  * Implementations of baseline shim functions
  */
 
+#if 0
 static void
 child_handler(void)
 {
@@ -84,6 +85,7 @@ child_handler(void)
 	if (rc)
 		DL_ERROR(rc, "Failed to re-create global eq");
 }
+#endif
 
 static PyObject *
 __shim_handle__daos_init(PyObject *self, PyObject *args)
@@ -101,11 +103,14 @@ __shim_handle__daos_init(PyObject *self, PyObject *args)
 		return PyLong_FromLong(rc);
 	}
 
+#if 0
+	/** disabled due to DAOS-16637 */
 	rc = pthread_atfork(NULL, NULL, &child_handler);
 	if (rc) {
 		DL_ERROR(rc, "Failed to set atfork handler");
 		return PyLong_FromLong(rc);
 	}
+#endif
 
 	return PyLong_FromLong(rc);
 }
