b7ea05ba7b10b642100d54051a90f85436d30cf2
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-10-16 02:48:45 +0200

    DAOS-14408 common: ensure NDCTL not used for storage class `ram` (#15203)
    * DAOS-14408 common: enable NDCTL for DCPM
    
    This PR prepares DAOS to be used with NDCTL enabled in PMDK, which means:
    - NDCTL must not be used when non-DCPM (simulate PMem) - `storage class: "ram"` is used:
    `PMEMOBJ_CONF=sds.at_create=0` env variable disables NDCTL features in the PMDK
    This change affects all tests run on simulated PMem (e.g. inside VMs).
    Some DOAS utility applications may also require `PMEMOBJ_CONF=sds.at_create=0` to be set.
    
    - The default ULT stack size must be at least 20KiB to avoid stack overuse by PMDK with NDCTL enabled and be aligned with Linux page size.
    `ABT_THREAD_STACKSIZE=20480` env variable is used to increase the default ULT stack size.
    This env variable is set by control/server module just before engine is started.
    Much bigger stack is used for pmempool open/create-related tasks e.g. `tgt_vos_create_one` to avoid stack overusage.
    
    This modification shall not affect md-on-ssd mode as long as `storage class: "ram"` is used for the first tier in the `storage` configuration.
    This change does not require any configuration changes to existing systems.
    
    The new PMDK package with NDCTL enabled (https://github.com/daos-stack/pmdk/pull/38) will land as soon as this PR is merged.
    
    Change-Id: If4c3f7d88a97e4e4f5526da71f4b374a2844057b
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

--- utils/scripts/install-ubuntu.sh
+++ utils/scripts/install-ubuntu.sh
@@ -29,11 +29,13 @@ apt-get install \
     libcapstone-dev \
     libcmocka-dev \
     libcunit1-dev \
+    libdaxctl-dev \
     libfuse3-dev \
     libhwloc-dev \
     libibverbs-dev \
     libjson-c-dev \
     liblz4-dev \
+    libndctl-dev \
     libnuma-dev \
     libopenmpi-dev \
     libpci-dev \
