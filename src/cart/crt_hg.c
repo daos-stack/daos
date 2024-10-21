6f99a4542deed064f9e830f799373601518cc7d1
Joseph Moore <joseph.moore@intel.com>
2024-10-21 22:35:21 +0000

    DAOS-16615 cart: Set multithreaded mode on client.
    PR-repos: mercury@PR-125:3
    Test-provider: ucx+ud_x
    Skip-build-leap15-icc: true
    Skip-build-el8-gcc: true
    Skip-func-test-vm: true
    Skip-unit-tests: true
    Skip-func-hw-test-medium-verbs-provider: true
    Skip-func-hw-test-medium-ucx-provider: false
    
    Required-githooks: true
    
    Signed-off-by: Joseph Moore <joseph.moore@intel.com>

--- src/cart/crt_hg.c
+++ src/cart/crt_hg.c
@@ -864,6 +864,9 @@ crt_hg_class_init(crt_provider_t provider, int ctx_idx, bool primary, int iface_
 	init_info.multi_recv_op_max         = crt_gdata.cg_mrecv_buf;
 	init_info.multi_recv_copy_threshold = crt_gdata.cg_mrecv_buf_copy;
 
+	if (crt_is_service()) 
+		init_info.na_init_info.thread_mode = NA_THREAD_MODE_SINGLE;
+
 	hg_class = HG_Init_opt2(info_string, crt_is_service(), HG_VERSION(2, 4), &init_info);
 	if (hg_class == NULL) {
 		D_ERROR("Could not initialize HG class.\n");
