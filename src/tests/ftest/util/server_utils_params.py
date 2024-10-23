1ecfdb58283021f55903ea536f0dd78abd6cefe3
Dalton Bohning <dalton.bohning@intel.com>
2024-10-23 22:08:00 +0000

    test: hardcode come envs
    SKip-build: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- src/tests/ftest/util/server_utils_params.py
+++ src/tests/ftest/util/server_utils_params.py
@@ -432,18 +432,12 @@ class EngineYamlParameters(YamlParameters):
 
     # Engine environment variables that are required by provider type.
     REQUIRED_ENV_VARS = {
-        "common": [
-            "D_LOG_FILE_APPEND_PID=1",
-            "DAOS_POOL_RF=4",
-            "CRT_EVENT_DELAY=1",
-            "COVFILE=/tmp/test.cov"],
+        "common": [],  # let extra yaml set needed envs
         "ofi+tcp": [],
         "ofi+tcp;ofi_rxm": [],
         "ofi+verbs": [
             "FI_OFI_RXM_USE_SRX=1"],
-        "ofi+cxi": [
-            "FI_OFI_RXM_USE_SRX=1",
-            "CRT_MRC_ENABLE=1"],
+        "ofi+cxi": [],  # let extra yaml set needed envs
     }
 
     def __init__(self, base_namespace, index, provider=None, max_storage_tiers=MAX_STORAGE_TIERS):
