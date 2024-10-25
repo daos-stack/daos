637d24262efebfb4e50a47634fc303250ad01c8a
Ashley Pittman <ashley.m.pittman@intel.com>
2024-10-25 13:35:26 +0000

    Use the correct counter.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- src/client/dfuse/ops/read.c
+++ src/client/dfuse/ops/read.c
@@ -431,7 +431,7 @@ dfuse_cb_read(fuse_req_t req, fuse_ino_t ino, size_t len, off_t position, struct
 
 	DFUSE_IE_STAT_ADD(oh->doh_ie, DS_READ);
 
-	atomic_fetch_add_relaxed(&oh->doh_write_count, 1);
+	atomic_fetch_add_relaxed(&oh->doh_read_count, 1);
 
 	if (oh->doh_linear_read_eof && position == oh->doh_linear_read_pos) {
 		DFUSE_TRA_DEBUG(oh, "Returning EOF early without round trip %#zx", position);
