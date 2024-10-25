9cf40dc779741c24a53a2ac639b46e5870ea6bbc
Ryon Jensen <ryon.jensen@intel.com>
2024-10-25 15:42:44 -0600

    Commit 15
    Signed-off-by: Ryon Jensen <ryon.jensen@intel.com>

--- ci/scons_build.sh
+++ ci/scons_build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # this script is meant to be used from a GitHub Action. The outcome should be a zip file of the
 # built binaries, similar to how the jenkins step (sconsBuild.grovy) does it
+# expect to be in the daos dir
 
-set -e
+set -uex
 echo "hostname: $(hostname)"
 echo "pwd: $(pwd)"
 
-src_dir=${1:-"build"}
-tar_name=${2:-"opt-daos.tar"} # opt-daos.tar is what sconsBuild.grovy names it and what the unit tests look for
+tar_name=${1:-"opt-daos.tar"} # opt-daos.tar is what sconsBuild.grovy names it and what the unit tests look for
 
-cd "$src_dir"
 # install build dependencies
 sudo dnf --assumeyes install dnf-plugins-core
 sudo dnf config-manager --save --setopt=assumeyes=True
@@ -21,8 +20,9 @@ scons-3 --build-deps=yes --config=force -j12 install
 
 # need to simulate: 'tar -cf opt-daos.tar /opt/daos/' (this is how the sconsBuild.grovy does it in Jenkins)
 # will be unpacked with: tar --strip-components=2 --directory /opt/daos -xf opt-daos.tar
-tar -cf "$tar_name" -C ./build/ install
-mv "$tar_name" ./build/
+# so need have two levels of directory (components) that will be striped
+parent=$(basename "$PWD")
+tar -cf "$tar_name" ../"$parent"/install
 
 set +e
 
