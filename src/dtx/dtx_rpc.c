c4c195d36c507045a218a81cb618531348d7f2a7
Fan Yong <fan.yong@intel.com>
2024-10-28 14:23:21 +0800

    DAOS-16721 dtx: handle potential DTX ID reusing trouble - b26
    The patch contains the following improvements:
    
    1. When VOS level logic returns -DER_TX_RESATRT, the object level RPC
       handler should set 'RESEND' flag then restart the transaction with
       newer epoch. Because dtx_abort() logic cannot guarantee all former
       prepared DTX entries (on all related participants) can be aborted,
       especially if the former one failed for some network trouble, that
       may cause restarted transaction hit -DER_TX_ID_REUSED unexpectedly.
    
    2. Compare the epoch for DTX entries with the same transaction ID for
       distinguishing potential reused TX ID more accurately.
    
    3. Add DTX entry into DTX CoS cache if cannot commit it synchronously.
       Then subsequent batched commit logic can handle it.
    
    4. If server complains suspected TX ID reusing, then reports -EIO to
       related application instead of assertion on client.
    
    5. Control DTX related warning message frequency to avoid log flood.
    
    6. Collect more information when generate some error/warning message.
    
    Allow-unstable-test: true
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>

--- src/dtx/dtx_rpc.c
+++ src/dtx/dtx_rpc.c
@@ -1657,8 +1657,9 @@ dtx_coll_commit(struct ds_cont_child *cont, struct dtx_coll_entry *dce, struct d
 	}
 
 	D_CDEBUG(rc != 0 || rc1 != 0 || rc2 != 0, DLOG_ERR, DB_TRACE,
-		 "Collectively commit DTX "DF_DTI": %d/%d/%d\n",
-		 DP_DTI(&dce->dce_xid), rc, rc1, rc2);
+		 "Collectively commit DTX "DF_DTI" in "DF_UUID"/"DF_UUID": %d/%d/%d\n",
+		 DP_DTI(&dce->dce_xid), DP_UUID(cont->sc_pool_uuid), DP_UUID(cont->sc_uuid),
+		 rc, rc1, rc2);
 
 	return rc != 0 ? rc : rc1 != 0 ? rc1 : rc2;
 }
@@ -1717,8 +1718,9 @@ dtx_coll_abort(struct ds_cont_child *cont, struct dtx_coll_entry *dce, daos_epoc
 		rc2 = 0;
 
 	D_CDEBUG(rc != 0 || rc1 != 0 || rc2 != 0, DLOG_ERR, DB_TRACE,
-		 "Collectively abort DTX "DF_DTI": %d/%d/%d\n",
-		 DP_DTI(&dce->dce_xid), rc, rc1, rc2);
+		 "Collectively abort DTX "DF_DTI" with epoch "DF_X64" in "
+		 DF_UUID"/"DF_UUID": %d/%d/%d\n", DP_DTI(&dce->dce_xid), epoch,
+		 DP_UUID(cont->sc_pool_uuid), DP_UUID(cont->sc_uuid), rc, rc1, rc2);
 
 	return rc != 0 ? rc : rc1 != 0 ? rc1 : rc2;
 }
@@ -1766,8 +1768,9 @@ dtx_coll_check(struct ds_cont_child *cont, struct dtx_coll_entry *dce, daos_epoc
 	}
 
 	D_CDEBUG((rc < 0 && rc != -DER_NONEXIST) || (rc1 < 0 && rc1 != -DER_NONEXIST), DLOG_ERR,
-		 DB_TRACE, "Collectively check DTX "DF_DTI": %d/%d/\n",
-		 DP_DTI(&dce->dce_xid), rc, rc1);
+		 DB_TRACE, "Collectively check DTX "DF_DTI" in "DF_UUID"/"DF_UUID": %d/%d/\n",
+		 DP_DTI(&dce->dce_xid), DP_UUID(cont->sc_pool_uuid), DP_UUID(cont->sc_uuid),
+		 rc, rc1);
 
 	return dce->dce_ranks != NULL  ? rc : rc1;
 }
