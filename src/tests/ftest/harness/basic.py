cd45c235abd4da8f42402bf14076cc89a8f4ed51
Phil Henderson <phillip.henderson@intel.com>
2024-11-01 17:19:34 -0400

    Fix basic tests.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: DaosCoreTestDfs DaosCoreTestDfuse harness_cmocka test_daos_management MultiEnginesPerSocketTest FaultDomain
    Allow-unstable-test: true
    
    Required-githooks: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- src/tests/ftest/harness/basic.py
+++ src/tests/ftest/harness/basic.py
@@ -6,11 +6,9 @@
 import os
 
 from apricot import TestWithoutServers
-from cmocka_utils import CmockaUtils, get_cmocka_command
 from command_utils import SubProcessCommand
 from exception_utils import CommandFailure
-from host_utils import get_local_host
-from job_manager_utils import Mpirun, Orterun, get_job_manager
+from job_manager_utils import Mpirun, Orterun
 
 
 class HarnessBasicTest(TestWithoutServers):
@@ -80,12 +78,14 @@ class HarnessBasicTest(TestWithoutServers):
         try:
             Orterun(None)
         except CommandFailure as error:
-            self.fail("Orterun initialization failed: {}".format(error))
+            self.log.error("Orterun initialization failed: %s", error)
+            self.fail("Orterun initialization failed")
 
         try:
             Mpirun(None, mpi_type="mpich")
         except CommandFailure as error:
-            self.fail("Mpirun initialization failed: {}".format(error))
+            self.log.error("Mpirun initialization failed: %s", error)
+            self.fail("Mpirun initialization failed")
 
     def test_load_mpi_hw(self):
         """Simple test of apricot test code to load the openmpi module.
@@ -126,77 +126,3 @@ class HarnessBasicTest(TestWithoutServers):
         if failed:
             self.fail("The '{}' command failed".format(command))
         self.log.info("Test passed")
-
-    def test_no_cmocka_xml(self):
-        """Test to verify CmockaUtils detects lack of cmocka file generation.
-
-        If working correctly this test should fail due to a missing cmocka file.
-
-        :avocado: tags=all
-        :avocado: tags=vm
-        :avocado: tags=harness,harness_cmocka,failure_expected
-        :avocado: tags=HarnessBasicTest,test_no_cmocka_xml
-        """
-        self.log.info("=" * 80)
-        self.log.info("Running the 'hostname' command via CmockaUtils")
-        self.log.info("  This should generate a cmocka xml file with a 'Missing file' error")
-        name = "no_cmocka_xml_file_test"
-        cmocka_utils = CmockaUtils(None, name, self.outputdir, self.test_dir, self.log)
-        command = get_cmocka_command("", "hostname")
-        cmocka_utils.run_cmocka_test(self, command)
-        self._verify_no_cmocka_xml(name, str(command))
-        self.log.info("Test passed")
-
-    def test_no_cmocka_xml_timeout(self):
-        """Test to verify CmockaUtils handles timed out process correctly.
-
-        If working correctly this test should fail due to a test timeout and a missing cmocka file.
-
-        :avocado: tags=all
-        :avocado: tags=vm
-        :avocado: tags=harness,harness_cmocka,failure_expected
-        :avocado: tags=HarnessBasicTest,test_no_cmocka_xml_timeout
-        """
-        self.log.info("=" * 80)
-        self.log.info("Running the 'sleep 30' command via CmockaUtils")
-        self.log.info("  This should generate a test timeout failure")
-        self.log.info("  This should generate a cmocka xml file with a 'Missing file' error")
-        name = "no_cmocka_xml_file_timeout_test"
-        cmocka_utils = CmockaUtils(None, name, self.outputdir, self.test_dir, self.log)
-        command = get_cmocka_command("", "sleep", "60")
-        manager = get_job_manager(self, job=command)
-        manager.assign_hosts(get_local_host())
-        try:
-            cmocka_utils.run_cmocka_test(self, manager)
-        finally:
-            self._verify_no_cmocka_xml(name, str(command))
-        self.fail("Test did not timeout")
-
-    def _verify_no_cmocka_xml(self, name, command):
-        """Verify a cmocka xml file was generated with the expected error.
-
-        Args:
-            name (str): name of the cmocka test
-            command (str): command for the cmocka test
-        """
-        # Verify a generated cmocka xml file exists
-        expected = os.path.join(self.outputdir, f"{name}_cmocka_results.xml")
-        self.log.info("Verifying the existence of the generated cmocka file: %s", expected)
-        if not os.path.isfile(expected):
-            self.fail(f"No {expected} file found")
-
-        # Verify the generated cmocka xml file contains the expected error
-        self.log.info("Verifying contents of the generated cmocka file: %s", expected)
-        with open(expected, "r", encoding="utf-8") as file_handle:
-            actual_contents = file_handle.readlines()
-        error_message = f"Missing cmocka results for {command} in {self.outputdir}"
-        expected_lines = [
-            f"<testsuite errors=\"1\" failures=\"0\" name=\"{name}\" skipped=\"0\" tests=\"1\"",
-            f"<testcase classname=\"{name}\" name=\"{self.name}\"",
-            f"<error message=\"{error_message}\" type=\"Missing file\">"
-        ]
-        for index, actual_line in enumerate(actual_contents[1:4]):
-            self.log.debug("  expecting: %s", expected_lines[index])
-            self.log.debug("  in actual: %s", actual_line[:-1].strip())
-            if expected_lines[index] not in actual_line:
-                self.fail(f"Badly formed {expected} file")
