6abf25a3432876d995951e276a27689e10f5e0a3
Ashley Pittman <ashley.m.pittman@intel.com>
2024-11-15 16:22:52 +0000

    DAOS-16686 dfuse: Move pre-read code to inode from file handle. (#15488)
    Attach pre-read buffers to the inode rather than open file handle. This code
    was written with the idea that a client would open and read the file and that
    would populate the kernel cache however in practice there are often concurrent
    readers for the same file and what was happening was that the first-to-open
    was doing the pre-read but this was often not the first process to perform a read
    and furthermore often there would be multiple reads for the same regions, all
    of which would hit the network.
    
    Use the "active" entry on the inode to launch a pre-read on first open and have
    any request on any file handle use the pre-read buffer for replies if possible.
    
    In addition, when a read is to be serviced from the pre-read buffer but the
    data is not yet in memory rather than spinning on a lock consuming a fuse
    thread add a descriptor to a callback list so that the thread is released and
    the reply is made sooner when the data is available.
    
    This greatly reduces the number of duplicate network round-trip reads for
    workloads where multiple clients are trying to fetch the same data, something
    that we see a lot in some applications.
    
    Signed-off-by: Ashley Pittman ashley.m.pittman@intel.com

--- src/client/dfuse/file.c
+++ src/client/dfuse/file.c
@@ -14,7 +14,7 @@ static pthread_mutex_t alock = PTHREAD_MUTEX_INITIALIZER;
 
 /* Perhaps combine with dfuse_open_handle_init? */
 int
-active_ie_init(struct dfuse_inode_entry *ie)
+active_ie_init(struct dfuse_inode_entry *ie, bool *preread)
 {
 	uint32_t oc;
 	int      rc = -DER_SUCCESS;
@@ -25,8 +25,11 @@ active_ie_init(struct dfuse_inode_entry *ie)
 
 	DFUSE_TRA_DEBUG(ie, "Addref to %d", oc + 1);
 
-	if (oc != 0)
+	if (oc != 0) {
+		if (preread && *preread)
+			*preread = false;
 		goto out;
+	}
 
 	D_ALLOC_PTR(ie->ie_active);
 	if (!ie->ie_active)
@@ -38,20 +41,47 @@ active_ie_init(struct dfuse_inode_entry *ie)
 		goto out;
 	}
 	D_INIT_LIST_HEAD(&ie->ie_active->chunks);
+	if (preread && *preread) {
+		D_ALLOC_PTR(ie->ie_active->readahead);
+		if (ie->ie_active->readahead) {
+			D_INIT_LIST_HEAD(&ie->ie_active->readahead->req_list);
+			atomic_fetch_add_relaxed(&ie->ie_open_count, 1);
+		}
+	}
+	/* Take a reference on the inode to prevent it being released */
+	atomic_fetch_add_relaxed(&ie->ie_ref, 1);
 out:
 	D_MUTEX_UNLOCK(&alock);
 	return rc;
 }
 
 static void
-ah_free(struct dfuse_inode_entry *ie)
+ah_free(struct dfuse_info *dfuse_info, struct dfuse_inode_entry *ie)
 {
-	D_SPIN_DESTROY(&ie->ie_active->lock);
+	struct active_inode *active = ie->ie_active;
+
+	if (active->readahead) {
+		struct dfuse_event *ev;
+
+		D_ASSERT(active->readahead->complete);
+		D_ASSERT(d_list_empty(&active->readahead->req_list));
+
+		ev = active->readahead->dra_ev;
+
+		if (ev) {
+			daos_event_fini(&ev->de_ev);
+			d_slab_release(ev->de_eqt->de_pre_read_slab, ev);
+		}
+		D_FREE(active->readahead);
+	}
+
+	D_SPIN_DESTROY(&active->lock);
 	D_FREE(ie->ie_active);
+	dfuse_inode_decref(dfuse_info, ie);
 }
 
 bool
-active_oh_decref(struct dfuse_obj_hdl *oh)
+active_oh_decref(struct dfuse_info *dfuse_info, struct dfuse_obj_hdl *oh)
 {
 	uint32_t oc;
 	bool     rcb = true;
@@ -68,14 +98,14 @@ active_oh_decref(struct dfuse_obj_hdl *oh)
 
 	rcb = read_chunk_close(oh->doh_ie);
 
-	ah_free(oh->doh_ie);
+	ah_free(dfuse_info, oh->doh_ie);
 out:
 	D_MUTEX_UNLOCK(&alock);
 	return rcb;
 }
 
 void
-active_ie_decref(struct dfuse_inode_entry *ie)
+active_ie_decref(struct dfuse_info *dfuse_info, struct dfuse_inode_entry *ie)
 {
 	uint32_t oc;
 	D_MUTEX_LOCK(&alock);
@@ -88,7 +118,9 @@ active_ie_decref(struct dfuse_inode_entry *ie)
 	if (oc != 1)
 		goto out;
 
-	ah_free(ie);
+	read_chunk_close(ie);
+
+	ah_free(dfuse_info, ie);
 out:
 	D_MUTEX_UNLOCK(&alock);
 }
