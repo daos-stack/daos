eb70b45de0f3ed5cf3dc41ef22d30205b8722c70
Michael MacDonald <mjmac@google.com>
2024-11-20 14:42:23 +0000

    DAOS-16823 container: Add metrics_enabled property
    Add a boolean container property that can be used to
    toggle client-side metrics collection and reporting
    behavior on a per-container basis. By default, containers
    will not participate in metrics collection, and must
    be opted-in via the property. The initial use case for
    this property is to enable DFS-level metrics in
    POSIX containers.
    
    Features: container
    Required-githooks: true
    Signed-off-by: Michael MacDonald <mjmac@google.com>

--- src/container/srv_layout.c
+++ src/container/srv_layout.c
@@ -54,6 +54,7 @@ RDB_STRING_KEY(ds_cont_prop_, scrubber_disabled);
 RDB_STRING_KEY(ds_cont_prop_, co_md_times);
 RDB_STRING_KEY(ds_cont_prop_, cont_obj_version);
 RDB_STRING_KEY(ds_cont_prop_, nhandles);
+RDB_STRING_KEY(ds_cont_prop_, metrics_enabled);
 
 /* dummy value for container roots, avoid malloc on demand */
 static struct daos_prop_co_roots dummy_roots;
@@ -205,6 +206,9 @@ struct daos_prop_entry cont_prop_entries_default[CONT_PROP_NUM] = {
 	}, {
 		.dpe_type	= DAOS_PROP_CO_PERF_DOMAIN,
 		.dpe_val	= 0, /* inherit from pool by default */
+	}, {
+		.dpe_type	= DAOS_PROP_CO_METRICS_ENABLED,
+		.dpe_val	= 0, /* disabled by default */
 	}
 };
 
