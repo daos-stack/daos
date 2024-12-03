21a881a423d8aecc967b11a2932a682a75971229
Tom Nabarro <tom.nabarro@intel.com>
2024-12-03 18:56:54 +0000

    DAOS-7485 control: Implement dmg system drain to act on all hosts (#15506)
    Add dmg system drain command to drain a set of storage nodes or ranks
    from all the pools they belong too. Takes --ranks or --rank-hosts in
    ranged format. Improve unit test coverage for lib/control, cmd/dmg and
    server/mgmt_system system related functions.
    
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- src/control/cmd/dmg/pretty/system_test.go
+++ src/control/cmd/dmg/pretty/system_test.go
@@ -1,5 +1,5 @@
 //
-// (C) Copyright 2021-2022 Intel Corporation.
+// (C) Copyright 2021-2024 Intel Corporation.
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -103,7 +103,7 @@ Ranks Action Result
 			}
 
 			if diff := cmp.Diff(strings.TrimLeft(tc.expPrintStr, "\n"), bld.String()); diff != "" {
-				t.Fatalf("unexpected format string (-want, +got):\n%s\n", diff)
+				t.Fatalf("unexpected string output (-want, +got):\n%s\n", diff)
 			}
 		})
 	}
@@ -357,7 +357,7 @@ Unknown 3 ranks: 7-9
 			}
 
 			if diff := cmp.Diff(strings.TrimLeft(tc.expPrintStr, "\n"), bld.String()); diff != "" {
-				t.Fatalf("unexpected format string (-want, +got):\n%s\n", diff)
+				t.Fatalf("unexpected string output (-want, +got):\n%s\n", diff)
 			}
 		})
 	}
@@ -451,7 +451,7 @@ Unknown 3 hosts: foo[7-9]
 			}
 
 			if diff := cmp.Diff(strings.TrimLeft(tc.expPrintStr, "\n"), bld.String()); diff != "" {
-				t.Fatalf("unexpected format string (-want, +got):\n%s\n", diff)
+				t.Fatalf("unexpected string output (-want, +got):\n%s\n", diff)
 			}
 		})
 	}
@@ -606,7 +606,80 @@ Unknown 3 hosts: foo[7-9]
 			}
 
 			if diff := cmp.Diff(strings.TrimLeft(tc.expPrintStr, "\n"), bld.String()); diff != "" {
-				t.Fatalf("unexpected format string (-want, +got):\n%s\n", diff)
+				t.Fatalf("unexpected string output (-want, +got):\n%s\n", diff)
+			}
+		})
+	}
+}
+
+func TestPretty_PrintSystemDrainResp(t *testing.T) {
+	for name, tc := range map[string]struct {
+		resp   *control.SystemDrainResp
+		expOut string
+	}{
+		"empty response": {
+			resp: &control.SystemDrainResp{},
+			expOut: `
+No pool ranks drained
+`,
+		},
+		"normal response": {
+			resp: &control.SystemDrainResp{
+				Results: []*control.DrainResult{
+					{PoolID: test.MockUUID(1), Ranks: "0-3"},
+					{PoolID: test.MockUUID(2), Ranks: "1-4"},
+				},
+			},
+			expOut: `
+Pool                                 Ranks Result Reason 
+----                                 ----- ------ ------ 
+00000001-0001-0001-0001-000000000001 0-3   OK     N/A    
+00000002-0002-0002-0002-000000000002 1-4   OK     N/A    
+
+`,
+		},
+		"normal response; use labels": {
+			resp: &control.SystemDrainResp{
+				Results: []*control.DrainResult{
+					{PoolID: "label1", Ranks: "0-3"},
+					{PoolID: "label2", Ranks: "1-4"},
+				},
+			},
+			expOut: `
+Pool   Ranks Result Reason 
+----   ----- ------ ------ 
+label1 0-3   OK     N/A    
+label2 1-4   OK     N/A    
+
+`,
+		},
+		"response with failures": {
+			resp: &control.SystemDrainResp{
+				Results: []*control.DrainResult{
+					{PoolID: test.MockUUID(1), Ranks: "1-2"},
+					{PoolID: test.MockUUID(2), Ranks: "0"},
+					{
+						PoolID: test.MockUUID(2), Ranks: "1-2",
+						Status: -1, Msg: "fail1",
+					},
+				},
+			},
+			expOut: `
+Pool                                 Ranks Result Reason 
+----                                 ----- ------ ------ 
+00000001-0001-0001-0001-000000000001 1-2   OK     N/A    
+00000002-0002-0002-0002-000000000002 0     OK     N/A    
+00000002-0002-0002-0002-000000000002 1-2   Failed fail1  
+
+`,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			var out strings.Builder
+			PrintSystemDrainResponse(&out, tc.resp)
+
+			if diff := cmp.Diff(strings.TrimLeft(tc.expOut, "\n"), out.String()); diff != "" {
+				t.Fatalf("unexpected stdout (-want, +got):\n%s\n", diff)
 			}
 		})
 	}
