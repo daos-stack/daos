e8678b5e3c49089cd1c9d80a02e288c82b1b97af
Dalton Bohning <dalton.bohning@intel.com>
2024-12-04 17:14:45 +0000

    fix fix
    Skip-build: true
    
    Required-githooks: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- utils/githooks/pre-commit.d/30-Jenkinsfile.sh
+++ utils/githooks/pre-commit.d/30-Jenkinsfile.sh
@@ -23,7 +23,7 @@ echo "Checking syntax"
 HOST="${HOST:-build.hpdd.intel.com}"
 CURL_VERBOSE=${CURL_VERBOSE:-""}
 CURL_PROXY="${CURL_PROXY:+-x }${CURL_PROXY:-}"
-CURL_OPTS=($CURL_PROXY $CURL_VERBOSE -s)
+CURL_OPTS=("$CURL_PROXY" "$CURL_VERBOSE" -s)
 URL="https://$HOST/pipeline-model-converter/validate"
 if ! output=$(curl "${CURL_OPTS[@]}" -s -X POST -F "jenkinsfile=<${1:-Jenkinsfile}" "$URL"); then
     echo "  Failed to access $URL. Skipping"
