1bbf2b1b6349d2fe327a99e32327269e1221263b
Cedric Koch-Hofer <cedric.koch.hofer@gmail.com>
2024-12-10 14:41:05 +0000

    DAOS-16501 build: Add libsanitize
    Fixing invalid compilation errors reported by gcc on el9.
    
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@hpe.com>

--- src/client/dfuse/ops/lookup.c
+++ src/client/dfuse/ops/lookup.c
@@ -119,11 +119,12 @@ dfuse_reply_entry(struct dfuse_info *dfuse_info, struct dfuse_inode_entry *ie,
 
 			/* Save the old name so that we can invalidate it in later */
 			wipe_parent = inode->ie_parent;
-			strncpy(wipe_name, inode->ie_name, NAME_MAX + 1);
+			strncpy(wipe_name, inode->ie_name, NAME_MAX);
 			wipe_name[NAME_MAX] = '\0';
 
 			inode->ie_parent = ie->ie_parent;
-			strncpy(inode->ie_name, ie->ie_name, NAME_MAX + 1);
+			strncpy(inode->ie_name, ie->ie_name, NAME_MAX);
+			inode->ie_name[NAME_MAX] = '\0';
 		}
 		atomic_fetch_sub_relaxed(&ie->ie_ref, 1);
 		dfuse_ie_close(dfuse_info, ie);
