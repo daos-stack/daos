cbb6611914c90914527393affcfc28121c3c1560
Lei Huang <lei.huang@intel.com>
2025-01-09 14:57:00 +0000

    relax time cutoff considering the overhead from valgrind
    Required-githooks: true
    Skipped-githooks: codespell
    
    Signed-off-by: Lei Huang <lei.huang@intel.com>

--- src/gurt/tests/test_shm.c
+++ src/gurt/tests/test_shm.c
@@ -1,5 +1,6 @@
 /**
- * (C) Copyright 2024 Hewlett Packard Enterprise Development LP.
+ * (C) Copyright 2024-2025 Intel Corporation.
+ * (C) Copyright 2024-2025 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -295,7 +296,7 @@ test_lock(void **state)
 	shm_mutex_lock(mutex, &owner_dead);
 	gettimeofday(&tm2, NULL);
 	dt = (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec) * 0.000001;
-	assert_true(fabs(dt - TIME_SLEEP) < 0.02);
+	assert_true(fabs(dt - TIME_SLEEP) < 0.06);
 	shm_mutex_unlock(mutex);
 	assert_true(owner_dead == false);
 
