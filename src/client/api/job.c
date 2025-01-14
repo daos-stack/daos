0b1fdc815a87ab6b8f89f83dfb63772ba148ca79
Michael MacDonald <mjmac@google.com>
2025-01-14 21:10:31 +0000

    DAOS-16823 client: Allow client metrics dump to container
    Enable DFS-layer metrics dump to a POSIX-layout container.
    Metric collection and dump may be toggled on a per-container
    basis using the `daos container telemetry (enable|disable)`
    commands. When telemetry is enabled for a POSIX container,
    DFS clients will begin recording telemetry on container open,
    and then dump that telemetry on container close.
    
    Change-Id: I29ed830675ebdcf3459cdf64d6c7f8d3bc70ea36
    Signed-off-by: Michael MacDonald <mjmac@google.com>

--- src/client/api/job.c
+++ src/client/api/job.c
@@ -10,8 +10,9 @@
 #include <daos/common.h>
 #include <daos/job.h>
 
-char *dc_jobid_env;
-char *dc_jobid;
+char        *dc_jobid_env;
+char        *dc_jobid;
+static char *default_jobid;
 
 static int
 craft_default_jobid(char **jobid)
@@ -56,11 +57,17 @@ get_jobid_env_var(char **jobid_env)
 }
 
 int
-dc_set_default_jobid(const char *default_jobid)
+dc_set_default_jobid(const char *jobid)
 {
 	char *jobid_env = NULL;
-	char *jobid     = NULL;
+	char *env_jobid = NULL;
 	int   rc        = 0;
+
+	if (jobid == NULL)
+		return -DER_INVAL;
+
+	if (default_jobid != NULL)
+		return -DER_ALREADY;
 
 	/* first, determine which environment variable to check/set */
 	rc = get_jobid_env_var(&jobid_env);
@@ -68,17 +75,31 @@ dc_set_default_jobid(const char *default_jobid)
 		D_GOTO(out, rc);
 
 	/* next, check to see if a jobid has already been set in the environment */
-	d_agetenv_str(&jobid, jobid_env);
-	if (jobid != NULL) {
-		d_freeenv_str(&jobid);
+	d_agetenv_str(&env_jobid, jobid_env);
+	if (env_jobid != NULL) {
+		d_freeenv_str(&env_jobid);
 		goto out;
 	}
 
-	/* set it to the default value since it wasn't already set */
-	d_setenv(jobid_env, default_jobid, 0);
+	D_STRNDUP(default_jobid, jobid, MAX_JOBID_LEN);
+	if (default_jobid == NULL)
+		D_GOTO(out, -DER_NOMEM);
+
 out:
 	D_FREE(jobid_env);
 	return rc;
+}
+
+bool
+dc_jobid_is_default(const char *jobid)
+{
+	if (jobid == NULL)
+		return false;
+
+	if (jobid == default_jobid || strcmp(jobid, default_jobid) == 0)
+		return true;
+
+	return false;
 }
 
 int
@@ -87,22 +108,26 @@ dc_job_init(void)
 	char *jobid;
 	int   err = 0;
 
+	if (default_jobid == NULL) {
+		err = craft_default_jobid(&default_jobid);
+		if (err)
+			D_GOTO(out_env, err);
+	}
+
 	err = get_jobid_env_var(&dc_jobid_env);
 	if (err)
 		D_GOTO(out_err, err);
 
 	d_agetenv_str(&jobid, dc_jobid_env);
 	if (jobid == NULL) {
-		err = craft_default_jobid(&jobid);
-		if (err)
-			D_GOTO(out_env, err);
+		jobid = default_jobid;
 	} else {
 		char *tmp_jobid = jobid;
 
 		D_STRNDUP(jobid, tmp_jobid, MAX_JOBID_LEN);
 		d_freeenv_str(&tmp_jobid);
 		if (jobid == NULL)
-			D_GOTO(out_env, err = -DER_NOMEM);
+			D_GOTO(out_def, err = -DER_NOMEM);
 	}
 
 	dc_jobid = jobid;
@@ -111,6 +136,8 @@ dc_job_init(void)
 	D_INFO("Using JOBID %s\n", dc_jobid);
 	return 0;
 
+out_def:
+	D_FREE(default_jobid);
 out_env:
 	D_FREE(dc_jobid_env);
 out_err:
@@ -122,4 +149,5 @@ dc_job_fini()
 {
 	D_FREE(dc_jobid);
 	D_FREE(dc_jobid_env);
+	D_FREE(default_jobid);
 }
