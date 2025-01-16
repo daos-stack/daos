d581ddfb723c5028cc3cf889902bed27fc4966fe
Jeff Olivier <jeffolivier@google.com>
2025-01-16 15:09:06 -0700

    Update srv_target.c
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- src/container/srv_target.c
+++ src/container/srv_target.c
@@ -1,5 +1,6 @@
 /**
- * (C) Copyright 2016-2025 Intel Corporation.
+ * (C) Copyright 2016-2024 Intel Corporation.
+ * (C) Copyright 2025 Google LLC
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
