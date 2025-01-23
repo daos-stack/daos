0f67d4d16e350616ce1fa24f65dc6dac70902633
Jinshan Xiong <jinshanx@google.com>
2025-01-23 17:09:11 +0000

    ofi and murcury fixes

--- site_scons/prereq_tools/base.py
+++ site_scons/prereq_tools/base.py
@@ -1009,6 +1009,7 @@ class _Component():
             if "^" in raw:
                 (patch_subdir, raw) = raw.split('^')
             if "https://" not in raw:
+                raw = os.path.join(Dir('#').abspath, raw)
                 patches[raw] = patch_subdir
                 continue
             patch_name = f'{self.name}_{self._sanitize_patch_path(raw)}_{patchnum:d}'
