13dd692d8ab18c28c27cacf5820e44bc2b93460d
Jeff Olivier <jeffolivier@google.com>
2025-01-29 09:23:53 -0700

    pmdk_log_function optimization
    pmdk_log_function optimization based on _D_DEBUG_EXPANDED macro.
    
    Skip NLT tests for full HW validation
    Skip-NLT: true
    
    Skip-list: test_ior_intercept_libpil4dfs:DAOS-16260
    Allow-unstable-test: true
    Priority: 2
    
    Co-authored-by: Jeff Olivier <jeffolivier@google.com>
    Co-authored-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- src/vos/pmdk_log.c
+++ src/vos/pmdk_log.c
@@ -1,5 +1,6 @@
 /**
  * (C) Copyright 2024 Intel Corporation.
+ * (C) Copyright 2025 Google LLC
  * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -9,6 +10,7 @@
  *
  * vos/pmdk_log.c
  */
+
 #define D_LOGFAC DD_FAC(pmdk)
 
 #ifdef DAOS_PMEM_BUILD
@@ -24,11 +26,22 @@ static uint64_t pmemobj_log_level_2_dlog_prio[] = {
     [PMEMOBJ_LOG_LEVEL_DEBUG] = DLOG_DBG,
 };
 
+static int * pmemobj_log_level_2_flag_var[] = {
+    [PMEMOBJ_LOG_LEVEL_HARK] = &DD_FLAG(DLOG_INFO, D_LOGFAC),
+    [PMEMOBJ_LOG_LEVEL_FATAL] = &DD_FLAG(DLOG_CRIT, D_LOGFAC),
+    [PMEMOBJ_LOG_LEVEL_ERROR] = &DD_FLAG(DLOG_ERR, D_LOGFAC),
+    [PMEMOBJ_LOG_LEVEL_WARNING] = &DD_FLAG(DLOG_WARN, D_LOGFAC),
+    [PMEMOBJ_LOG_LEVEL_NOTICE] = &DD_FLAG(DLOG_NOTE, D_LOGFAC),
+    [PMEMOBJ_LOG_LEVEL_INFO] = &DD_FLAG(DLOG_INFO, D_LOGFAC),
+    [PMEMOBJ_LOG_LEVEL_DEBUG] = &DD_FLAG(DLOG_DBG, D_LOGFAC),
+};
+
 static void
 pmdk_log_function(enum pmemobj_log_level level, const char *file_name, unsigned line_no,
 		  const char *function_name, const char *message)
 {
 	uint64_t dlog_prio = pmemobj_log_level_2_dlog_prio[level];
+	int *flag_var = pmemobj_log_level_2_flag_var[level];
 
 /*
  * There is a set of handy macros for each of the message priorities
@@ -37,11 +50,12 @@ pmdk_log_function(enum pmemobj_log_level level, const char *file_name, unsigned 
  * are provided via arguments to this callback function instead of
  * via macro definitions (__FILE__, __LINE__, and __func__) as
  * _D_LOG_NOCHECK() would like to consume them. So, the message here is
- * provided a few macro-calls later via _D_DEBUG() macro which allows
+ * provided a few macro-calls later via _D_DEBUG_EXPANDED() macro which allows
  * to swap the _D_LOG_NOCHECK macro for a custom macro.
  *
  * D_ERROR(...) -> D_DEBUG(DLOG_ERR, ...) ->
- *      _D_DEBUG(_D_LOG_NOCHECK, DLOG_ERR, ...)
+ *      _D_DEBUG(_D_LOG_NOCHECK, DLOG_ERR, ...) ->
+ *      _D_DEBUG_EXPANDED(_D_LOG_NOCHECK, DLOG_ERR, flag_var,  ...)
  */
 
 /*
@@ -51,30 +65,8 @@ pmdk_log_function(enum pmemobj_log_level level, const char *file_name, unsigned 
 #define PMDK_LOG_NOCHECK(mask, fmt, ...)                                                           \
 	d_log(mask, "%s:%d %s() " fmt, file_name, line_no, function_name, ##__VA_ARGS__)
 
-/*
- * The calculated message priority can't be passed as an argument to
- * the _D_DEBUG() since the argument's name is used to construct
- * a variable name.
- */
-#define PMDK_DLOG_CASE(DLOG_PRIO)                                                                  \
-	case DLOG_PRIO:                                                                            \
-		_D_DEBUG(PMDK_LOG_NOCHECK, DLOG_PRIO, "%s\n", message);                            \
-		break
+	_D_DEBUG_EXPANDED(PMDK_LOG_NOCHECK, dlog_prio, *flag_var, "%s\n", message);
 
-	switch (dlog_prio) {
-		PMDK_DLOG_CASE(DLOG_EMIT);
-		PMDK_DLOG_CASE(DLOG_CRIT);
-		PMDK_DLOG_CASE(DLOG_ERR);
-		PMDK_DLOG_CASE(DLOG_WARN);
-		PMDK_DLOG_CASE(DLOG_NOTE);
-		PMDK_DLOG_CASE(DLOG_INFO);
-		PMDK_DLOG_CASE(DLOG_DBG);
-	default:
-		D_ERROR("Not implemented dlog priority: %#x\n", (unsigned)dlog_prio);
-		_D_DEBUG(PMDK_LOG_NOCHECK, DLOG_EMIT, "%s\n", message);
-	}
-
-#undef PMDK_DLOG_CASE
 #undef PMDK_LOG_NOCHECK
 }
 
