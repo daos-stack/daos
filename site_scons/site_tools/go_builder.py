00d00a2a5731e91555146c5ca33ad6c9356716ac
Lei Huang <lei.huang@intel.com>
2025-02-05 13:17:33 +0000

    revert unneeded change
    Allow-unstable-test: true
    
    Signed-off-by: Lei Huang <lei.huang@hpe.com>

--- site_scons/site_tools/go_builder.py
+++ site_scons/site_tools/go_builder.py
@@ -118,8 +118,7 @@ def generate(env):
         context.Result(go_version)
         return 1
 
-#    env.d_go_bin = env.get("GO_BIN", env.WhereIs(GO_COMPILER))
-    env.d_go_bin = "/usr/local/go/bin/go"
+    env.d_go_bin = env.get("GO_BIN", env.WhereIs(GO_COMPILER))
 
     if GetOption('help') or GetOption('clean'):
         return
