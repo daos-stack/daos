a5b1da9866f1e70d688177e59e49becfe3106db0
Niu Yawei <yawei.niu@intel.com>
2025-02-17 23:37:34 +0800

    DAOS-16813 test: bump checkpoint duraion (#15875)
    Bump checkpoint valid duration from 2 secs to 4 secs.
    
    Signed-off-by: Niu Yawei <yawei.niu@hpe.com>

--- src/tests/ftest/telemetry/wal_metrics.py
+++ src/tests/ftest/telemetry/wal_metrics.py
@@ -1,5 +1,6 @@
 """
   (C) Copyright 2018-2024 Intel Corporation.
+  (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
@@ -203,8 +204,8 @@ class WalMetrics(TestWithTelemetry):
                             # Check point dirty pages should be 1-30
                             ranges[metric][label] = [1, 30]
                         elif '_duration' in metric:
-                            # Check point duration should be 1-2,000,000
-                            ranges[metric][label] = [1, 2000000]
+                            # Check point duration should be 1-4,000,000 us
+                            ranges[metric][label] = [1, 4000000]
                         elif '_iovs_copied' in metric:
                             # Check point iovs copied should be >= 0
                             ranges[metric][label] = [1]
