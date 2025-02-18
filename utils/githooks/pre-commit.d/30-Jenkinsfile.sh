66b3a594eaa12ab64e296cb9660e148b2d44661a
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-02-18 09:42:13 +0100

    Fix - cleanup code
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- utils/githooks/pre-commit.d/30-Jenkinsfile.sh
+++ utils/githooks/pre-commit.d/30-Jenkinsfile.sh
@@ -19,24 +19,25 @@ if [ -z "$(_git_diff_cached_files "Jenkinsfile")" ] ; then
     exit 0
 fi
 
+echo "Checking syntax"
+
 HOST="${HOST:-build.hpdd.intel.com}"
-if ping -c 1 build.hpdd.intel.com &> /dev/null; then
-    echo "Checking syntax"
-    CURL_VERBOSE=${CURL_VERBOSE:-""}
-    CURL_PROXY="${CURL_PROXY:+-x }${CURL_PROXY:-}"
-    CURL_OPTS=("$CURL_PROXY" "$CURL_VERBOSE" -s)
+if ! ping -c 1 $HOST &> /dev/null; then
+    echo "Failed to access $HOST. Skipping"
+    exit 0
+fi
 
-    URL="https://$HOST/pipeline-model-converter/validate"
-    if ! output=$(curl "${CURL_OPTS[@]}" -s -X POST -F "jenkinsfile=<${1:-Jenkinsfile}" "$URL"); then
-        echo "Failed to access $URL. Skipping"
-        exit 0
-    fi
+CURL_VERBOSE=${CURL_VERBOSE:-""}
+CURL_PROXY="${CURL_PROXY:+-x }${CURL_PROXY:-}"
+CURL_OPTS=("$CURL_PROXY" "$CURL_VERBOSE" -s)
 
-    if echo "$output" | grep -q "Errors encountered validating"; then
-        echo "$output"
-        exit 1
-    fi
-else
-        echo "Failed to access $HOST. Skipping"
-        exit 0
+URL="https://$HOST/pipeline-model-converter/validate"
+if ! output=$(curl "${CURL_OPTS[@]}" -s -X POST -F "jenkinsfile=<${1:-Jenkinsfile}" "$URL"); then
+    echo "Failed to access $URL. Skipping"
+    exit 0
+fi
+
+if echo "$output" | grep -q "Errors encountered validating"; then
+    echo "$output"
+    exit 1
 fi
