54429ac38261d0458be3f8828318502d86fce333
Makito Kano <makito.kano@hpe.com>
2025-02-24 21:49:08 +0000

    DAOS-14426 test: Add (C) in front of HPE copyright line
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-func-hw-test-medium-md-on-ssd: false
    Test-tag: test_dfuse_space_check
    Test-repeat: 5
    Signed-off-by: Makito Kano <makito.kano@hpe.com>

--- src/tests/ftest/aggregation/dfuse_space_check.py
+++ src/tests/ftest/aggregation/dfuse_space_check.py
@@ -1,6 +1,6 @@
 """
   (C) Copyright 2020-2024 Intel Corporation.
-  Copyright 2025 Hewlett Packard Enterprise Development LP
+  (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
