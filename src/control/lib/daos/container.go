aa61fd55fe8f10b65aaf8f9c258b5b79b24b79e4
Michael MacDonald <mjmac@google.com>
2025-02-26 11:45:55 +0000

    DAOS-10028 client: Refactor container properties for Go
    Migrate the daos tool code over to use native Go types
    for container properties and property lists.
    
    Allow-unstable-test: true
    Features: daos_cmd
    Signed-off-by: Michael MacDonald <mjmac@google.com>

--- src/control/lib/daos/container.go
+++ src/control/lib/daos/container.go
@@ -9,6 +9,8 @@ package daos
 
 import (
 	"encoding/json"
+	"sort"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
@@ -67,10 +69,37 @@ const (
 	ContainerLayoutMeteo ContainerLayout = C.DAOS_PROP_CO_LAYOUT_METEO
 )
 
+func (cof ContainerOpenFlag) String() string {
+	flagStrs := []string{}
+	if cof&ContainerOpenFlagReadOnly != 0 {
+		flagStrs = append(flagStrs, "read-only")
+	}
+	if cof&ContainerOpenFlagReadWrite != 0 {
+		flagStrs = append(flagStrs, "read-write")
+	}
+	if cof&ContainerOpenFlagExclusive != 0 {
+		flagStrs = append(flagStrs, "exclusive")
+	}
+	if cof&ContainerOpenFlagForce != 0 {
+		flagStrs = append(flagStrs, "force")
+	}
+	if cof&ContainerOpenFlagReadOnlyMetadata != 0 {
+		flagStrs = append(flagStrs, "read-only-metadata")
+	}
+	if cof&ContainerOpenFlagEvict != 0 {
+		flagStrs = append(flagStrs, "evict")
+	}
+	if cof&ContainerOpenFlagEvictAll != 0 {
+		flagStrs = append(flagStrs, "evict-all")
+	}
+	sort.Strings(flagStrs)
+	return strings.Join(flagStrs, ",")
+}
+
 // FromString converts a string to a ContainerLayout.
 func (l *ContainerLayout) FromString(in string) error {
-	cStr := C.CString(in)
-	defer freeString(cStr)
+	cStr, free := toCString(in)
+	defer free()
 	C.daos_parse_ctype(cStr, (*C.uint16_t)(l))
 
 	if *l == ContainerLayoutUnknown {
