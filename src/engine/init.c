9645e2357f5e2d44000efb825612c39f8e599c3a
Jeff Olivier <jeffolivier@google.com>
2025-03-05 13:00:29 -0700

    Missed one
    Allow-unstable-test: true
    
    Change-Id: Ia3a25bdd4031991fcd004c36296d3b3d1841de18
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- src/engine/init.c
+++ src/engine/init.c
@@ -1243,7 +1243,8 @@ main(int argc, char **argv)
 			dss_dump_ABT_state(fp);
 			/* re-add SIGUSR1 to set */
 			sigaddset(&set, SIGUSR1);
-			fclose(fp);
+			if (pr != stderr)
+				fclose(fp);
 			continue;
 		}
 
