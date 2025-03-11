c124868093395527e2e4963b9fbf26dd945b2713
Lei Huang <lei.huang@intel.com>
2025-03-11 16:16:43 +0000

    1. enable local flock support in dfuse for datamover/serial_large_posix.py 2. fix dfuse/simul.py   a. run with multiple nodes over dfuse with caching disabled   b. disable flock tests   c. add comments why tests failed
    Features: dfuse
    
    Signed-off-by: Lei Huang <lei.huang@hpe.com>

--- src/tests/ftest/dfuse/simul.py
+++ src/tests/ftest/dfuse/simul.py
@@ -8,10 +8,8 @@ import os
 
 from apricot import TestWithServers
 from dfuse_utils import get_dfuse, start_dfuse
-from env_modules import load_mpi
-from exception_utils import MPILoadError
-from general_utils import run_command
-
+from host_utils import get_local_host
+from job_manager_utils import Mpirun
 
 class PosixSimul(TestWithServers):
     """Tests a posix container with simul.
@@ -88,6 +86,7 @@ class PosixSimul(TestWithServers):
         """
         mpi_type = self.params.get("mpi_type", "/run/*", "")
         simul_path = self.params.get("simul_path", "/run/*", "")
+        num_proc = self.params.get("np", "/run/client_processes/*", "")
 
         # Create a pool
         self.log_step("Create a pool")
@@ -103,22 +102,23 @@ class PosixSimul(TestWithServers):
         start_dfuse(self, dfuse, pool, container)
         dfuse.check_running()
 
-        # The use of MPI here is to run in parallel all simul tests on a single host.
-        if not load_mpi(mpi_type):
-            raise MPILoadError(mpi_type)
-
-        # Run simul
         simul_cmd = os.path.join(simul_path, "simul")
         if include and not exclude:
-            cmd = "{0} -vv -d {1} -i {2}".format(simul_cmd, dfuse.mount_dir.value, include)
+            cmd = f"{simul_cmd} -vv -d {dfuse.mount_dir.value} -i {include}"
         elif exclude and not include:
-            cmd = "{0} -vv -d {1} -e {2}".format(simul_cmd, dfuse.mount_dir.value, exclude)
+            cmd = f"{simul_cmd} -vv -d {dfuse.mount_dir.value} -e {exclude}"
         else:
             cmd = None  # appease pylint
             self.fail("##Both include and exclude tests are selected both or empty.")
 
-        self.log_step("Running simul on %s", mpi_type)
-        return run_command(cmd, output_check="combined", raise_exception=raise_exception)
+        self.log_step(f"Running simul on {mpi_type}")
+        mpirun = Mpirun(cmd, mpi_type=mpi_type)
+        mpirun.assign_hosts(get_local_host(), dfuse.mount_dir.value)
+        mpirun.assign_processes(num_proc)
+        out = mpirun.run(raise_exception=raise_exception)
+        if include:
+            # testing cases that are expected to fail
+            return out
 
     def test_posix_simul(self):
         """Test simul.
@@ -128,7 +128,14 @@ class PosixSimul(TestWithServers):
         :avocado: tags=posix,simul,dfuse
         :avocado: tags=PosixSimul,test_posix_simul
         """
-        self.run_simul(exclude="9,18,30,39,40")
+        # test  9, readdir, shared mode, dfuse returns NULL for readdir of an empty dir
+        # test 18, link, shared mode, daos does not support hard link
+        # test 20, fcntl locking, shared mode, daos does not support flock
+        # test 30, readdir, individual mode, dfuse returns NULL for readdir of an empty dir
+        # test 39, link, individual mode, daos does not support hard link
+        # test 40, link, individual mode, daos does not support hard link
+        # test 41, fcntl locking, individual mode, daos does not support flock
+        self.run_simul(exclude="9,18,20,30,39,40,41")
         self.log.info('Test passed')
 
     def test_posix_expected_failures(self):
@@ -139,7 +146,7 @@ class PosixSimul(TestWithServers):
         :avocado: tags=posix,simul,dfuse
         :avocado: tags=PosixSimul,test_posix_expected_failures
         """
-        expected_failures = {"9": None, "18": None, "30": None, "39": None, "40": None}
+        expected_failures = {"9": None, "18": None, "20": None, "30": None, "39": None, "40": None, "41": None}
         for test in sorted(expected_failures):
             expected_failures[test] = self.run_simul(include=test, raise_exception=False)
         failed = []
