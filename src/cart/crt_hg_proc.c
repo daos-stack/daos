aa823a6cea54072ef60e288f25b84dd549822c2e
Li Wei <liwei@hpe.com>
2025-03-19 01:19:44 +0900

    DAOS-17111 cart: Use only swim ctx for outage (#15924)
    The "network outage" detection for swim (see the existing crt_swim_update_last_unpack_hlc) uses all crt contexts. In an engine, if the swim context can't receive or send any message, while at least one other context can and does receive messages constantly, then swim will not detect any "network outage", leading to more false positive DEAD events. The purpose of that detection is to find out swim-specific "network outages", where swim may be unable to receive any swim message.
    
    This patch changes the detection algorithm to use only the swim crt context:
    - Remove crt_context.cc_last_unpack_hlc.
    - Update crt_swim_membs.csm_last_unpack_hlc when receiving swim requests and replies.
    
    Signed-off-by: Li Wei <liwei@hpe.com>

--- src/cart/crt_hg_proc.c
+++ src/cart/crt_hg_proc.c
@@ -450,8 +450,7 @@ crt_hg_unpack_header(hg_handle_t handle, struct crt_rpc_priv *rpc_priv,
 
 	/* Sync the HLC. Clients never decode requests. */
 	D_ASSERT(crt_is_service());
-	rc = d_hlc_get_msg(rpc_priv->crp_req_hdr.cch_hlc,
-			     &ctx->cc_last_unpack_hlc, &clock_offset);
+	rc = d_hlc_get_msg(rpc_priv->crp_req_hdr.cch_hlc, NULL /* hlc_out */, &clock_offset);
 	if (rc != 0) {
 		REPORT_HLC_SYNC_ERR("failed to sync HLC for request: opc=%x ts="
 				    DF_U64" offset="DF_U64" from=%u\n",
