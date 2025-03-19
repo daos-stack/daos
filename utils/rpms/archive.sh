743930e3272a921dab850cdc5d05f4879ebd013d
Jeff Olivier <jeffolivier@google.com>
2025-03-18 20:03:08 -0600

    Get recursion to work
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- utils/rpms/archive.sh
+++ utils/rpms/archive.sh
@@ -35,11 +35,11 @@ sm_file_prefix="${file_extless}-submodule"
 
 # Create an archive, which doesn't include any submodule.
 git archive --prefix "${name}-${version}/" -o "${tmp}/${file}" HEAD
-tar -tvf "${tmp}/${file}"
 
 # Add all submodules to the archive.
-git submodule --quiet foreach \
-    "git archive --prefix ${name}-${version}/\$sm_path/ -o ${tmp}/${sm_file_prefix}-\$name.${ext} \$sha1"
+git submodule --quiet foreach --recursive \
+    "git archive --prefix ${name}-${version}/\$displaypath/ -o ${tmp}/${sm_file_prefix}-\$name.${ext} \$sha1"
+ls "${tmp}/${file}"
 tar -Af "${tmp}/${file}" "${tmp}/${sm_file_prefix}-"*."${ext}"
 
 # Publish the archive.
