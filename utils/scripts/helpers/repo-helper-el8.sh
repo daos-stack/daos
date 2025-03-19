fece9d6cf8d15e7d07506618f2ef45c3b3b117e2
Ryon Jensen <ryon.jensen@intel.com>
2025-03-19 05:58:31 -0600

    SRE-2832 Build: Update for larger Jenkinsfiles (#15935) (#16119)
    Update Jenkinsfile to support testing SCRIPT_SPLITTING_TRANSFORMATION per
    https://issues.jenkins.io/browse/JENKINS-56500.
    
    Commented out for now the Ubuntu 20.04 build as Ubuntu 20.04 is
    shortly going out of support, and the Ubuntu build process needs
    a rewrite for proper support.
    
    ci/provisioning/post_provision_config_common_functions.sh:
      Fix cleanup of old repos not be Jenkins hostname specific.
    
    ci/provisioning/post_provision_config_nodes_EL_8.sh
      Fix to use environment variable for repository URL.
    
    utils/githooks/README.md:
      Add packages that need or are recommended to be installed.
      Fix markuplint flagged issues.
    
    utils/githooks/pre-commit.d:
      Use JENKINS_HOST for specifying the Jenkins host and report it.
      Remove unneeded and confusing CURL_PROXY code.
      cURL uses standard HTTPS_PROXY
    
    utils/scripts/helpers/install_maldet.ch :
      Fix defaults to be more implementation neutral.
    
    utils/scripts/helpers/repo-helper-*.sh :
      Optionally install CA needed to validate local repository mirrors.
    
    Dockerfiles modified to support intalling the CA.
    
    Dockerfile.code_scanning modified to allow specifying the fedora
    version used.
    
    Signed-off-by: John E. Malmberg <john.malmberg@hpe.com>
    Co-authored-by: John E. Malmberg <john.malmberg@hpe.com>
    Co-authored-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
    Co-authored-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/scripts/helpers/repo-helper-el8.sh
+++ utils/scripts/helpers/repo-helper-el8.sh
@@ -10,13 +10,15 @@ set -uex
 : "${BASE_DISTRO:=rockylinux/rockylinux:$MAJOR_VER}"
 : "${JENKINS_URL:=}"
 : "${REPOS:=}"
+: "${DAOS_LAB_CA_FILE_URL:=}"
+: "${REPOSITORY_NAME:=artifactory}"
 
 # shellcheck disable=SC2120
 disable_repos () {
     local repos_dir="$1"
     shift
     local save_repos
-    IFS=" " read -r -a save_repos <<< "${*:-} daos_ci-el$MAJOR_VER-artifactory"
+    IFS=" " read -r -a save_repos <<< "${*:-} daos_ci-el${MAJOR_VER}-${REPOSITORY_NAME}"
     if [ -n "$REPO_FILE_URL" ]; then
         pushd "$repos_dir"
         local repo
@@ -38,6 +40,16 @@ install_curl() {
     :
 }
 
+# Use local repo server if present
+install_optional_ca() {
+    ca_storage="/etc/pki/ca-trust/source/anchors/"
+    if [ -n "$DAOS_LAB_CA_FILE_URL" ]; then
+        curl -k --noproxy '*' -sSf -o "${ca_storage}lab_ca_file.crt" \
+            "$DAOS_LAB_CA_FILE_URL"
+        update-ca-trust
+    fi
+}
+
 # installs/upgrades of epel-release add repos
 # Disable mirrorlist check when using local repos.
 DISTRO="rocky"
@@ -53,12 +65,17 @@ MAJOR_VER="${BASE_DISTRO##*:}"
 MAJOR_VER="${MAJOR_VER%%.*}"
 if [ -n "$REPO_FILE_URL" ]; then
     install_curl
+    install_optional_ca
     mkdir -p /etc/yum.repos.d
     pushd /etc/yum.repos.d/
-    curl -k -f -o daos_ci-el"$MAJOR_VER"-artifactory.repo        \
-         "$REPO_FILE_URL"daos_ci-el"$MAJOR_VER"-artifactory.repo
+    curl -k --noproxy '*' -sSf -o "daos_ci-el${MAJOR_VER}-${REPOSITORY_NAME}.repo"  \
+         "${REPO_FILE_URL}daos_ci-el${MAJOR_VER}-${REPOSITORY_NAME}.repo"
     disable_repos /etc/yum.repos.d/
     popd
+    # These may have been created in the Dockerfile must be removed
+    # when using a local repository.
+    unset HTTPS_PROXY
+    unset https_proxy
 fi
 dnf -y --disablerepo \*epel\* install dnf-plugins-core
 dnf -y config-manager --save --setopt=assumeyes=True
@@ -66,7 +83,7 @@ dnf config-manager --save --setopt=install_weak_deps=False
 if [ ! -f /etc/fedora-release ]; then
     dnf --disablerepo \*epel\* install epel-release
     if [ -n "$REPO_FILE_URL" ]; then
-        PT_REPO="daos_ci-${DISTRO}${MAJOR_VER}-powertools-artifactory"
+        PT_REPO="daos_ci-${DISTRO}${MAJOR_VER}-powertools-${REPOSITORY_NAME}"
         true > /etc/yum.repos.d/epel.repo
         true > /etc/yum.repos.d/epel-modular.repo
         sed "s/^mirrorlist_expire=0*/mirrorlist_expire=99999999/" \
