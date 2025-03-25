dc93ceb1fc86beb04c72afdcae1db41d12227727
Cedric Koch-Hofer <94527853+knard38@users.noreply.github.com>
2025-03-25 23:41:57 +0100

    DAOS-16501 build: Add libsanitize (#15105)
    Add scons build option SANITIZERS allowing to use the libasan.
    This new option takes a list of sanitizer tool such as: AddressSanitizer (i.e. -fsanitize=address), ThreadSanitizer(i.e. -fsanitize=thread), LeakSanitizer (i.e. -fsanitize=leak), etc.
    
    A list of the available santizer tools and their compatibility could be found in the gcc man page.
    
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@hpe.com>

--- site_scons/components/__init__.py
+++ site_scons/components/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2016-2024 Intel Corporation
+# Copyright 2025 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -276,7 +277,15 @@ def define_components(reqs):
     abt_build = ['./configure',
                  '--prefix=$ARGOBOTS_PREFIX',
                  'CC=gcc',
-                 '--enable-stack-unwind']
+                 '--enable-stack-unwind=yes']
+    try:
+        if reqs.get_env('SANITIZERS') != "":
+            # NOTE the address sanitizer library add some extra info on the stack and thus ULTs
+            # need a bigger stack
+            print("Increase argobots default stack size from 16384 to 32768")
+            abt_build += ['--enable-default-stacksize=32768']
+    except KeyError:
+        pass
 
     if reqs.target_type == 'debug':
         abt_build.append('--enable-debug=most')
