80770c7253a8b645a77dbc849844de7098b54136
Jeff Olivier <jeffolivier@google.com>
2025-03-30 20:02:04 +0000

    Fix an issue
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- utils/rpms/fix_files.sh
+++ utils/rpms/fix_files.sh
@@ -6,12 +6,15 @@ fixroot="$1"; shift
 
 run_patchelf()
 {
-  rpath=$(patchelf --print_rpath "$1")
+  set -x
+  echo "Patching $1"
+  rpath=$(patchelf --print-rpath "$1")
   if [ -n "${rpath}" ]; then
     patched_rpath=$(echo "${rpath}" | sed "s!${buildbase}!!g")
     patchelf --set-rpath "${patched_rpath}" "$1"
   fi
 }
 
-grep -IrL '' "${src}" | xargs -I % run_patchelf '%'
-grep -Irl '' "$src" | xargs -I % sed -i "s!${buildbase}!!g" '%'
+export -f run_patchelf
+grep -IrL '' "${fixroot}" | xargs -I % bash -c "run_patchelf '%'"
+grep -Irl '' "${fixroot}" | xargs -I % sed -i "s!${buildbase}!!g" '%'
