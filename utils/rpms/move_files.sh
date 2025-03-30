6eff17b6a2237edaa036da9f098a9b76188badbf
Jeff Olivier <jeffolivier@google.com>
2025-03-30 22:14:49 +0000

    Something not right
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- utils/rpms/move_files.sh
+++ utils/rpms/move_files.sh
@@ -14,6 +14,7 @@ while [ $# -gt 0 ]; do
   if [ -d "${src}" ]; then
     dest="${dest_root}/$1"
     mkdir -p "${dest}"
+    set -x
     utils/rpms/move_files.sh "${buildroot}" "${src}" "${dest}" "${oldprefix}" "${newprefix}" \
                              "${lib}" "${libdir}" $(basename -a "${src}/"*)
     rmdir "${src}"
@@ -26,29 +27,14 @@ while [ $# -gt 0 ]; do
   grep -Il '' "$src" | xargs -I % sed -i "s!${oldprefix}/${lib}!${libdir}!" '%'
   grep -Il '' "$src" | xargs -I % sed -i "s!${oldprefix}/!${newprefix}!" '%'
   grep -Il '' "$src" | xargs -I % sed -i "s!-L${oldprefix}\S*!!" '%'
-  binary=1
-  for ext in ".pc" ".cmake" ".la"; do
-    if [ $(basename "${src}" "${ext}") != $(basename "$src") ]; then
-      binary=0
-      break
-    fi
-  done
-  if [ ${binary} -eq 0 ]; then
-    grep -IL '' "${src}" | xargs -I % patchelf --remove-rpath '%'
-    dbg_src=$(sed "s!${oldprefix}!/usr/lib/debug/${oldprefix}!" <<< "${src}")
-    dbg_dest=$(sed "s!${newprefix}/bin!${newprefix}/lib/debug/${newprefix}/bin!" <<< "${dest_root}")
-    dbg_dest=$(sed "s!${newprefix}/lib!${newprefix}/lib/debug/${newprefix}/lib!" <<< "${dest_root}")
-    fname=$(grep -IL '' "${dbg_src}-"*".debug")
-    if [ -f "${fname}" ]; then
-      mkdir -p "${dbg_dest}"
-      mv "${fname}" "${dbg_dest}"
-    fi
-  else
-    # some files seem to get treated as binary
-    sed -i "s!${buildroot}!!" "${src}"
-    sed -i "s!${oldprefix}/${lib}!${libdir}!" "${src}"
-    sed -i "s!${oldprefix}/!${newprefix}!" "${src}"
-    sed -i "s!-L${oldprefix}\S*!!" "${src}"
+  grep -IL '' "${src}" | xargs -I % patchelf --remove-rpath '%'
+  dbg_src=$(sed "s!${oldprefix}!/usr/lib/debug/${oldprefix}!" <<< "${src}")
+  dbg_dest=$(sed "s!${newprefix}/bin!${newprefix}/lib/debug/${newprefix}/bin!" <<< "${dest_root}")
+  dbg_dest=$(sed "s!${newprefix}/lib!${newprefix}/lib/debug/${newprefix}/lib!" <<< "${dest_root}")
+  fname=$(grep -IL '' "${dbg_src}-"*".debug")
+  if [ -f "${fname}" ]; then
+    mkdir -p "${dbg_dest}"
+    mv "${fname}" "${dbg_dest}"
   fi
   mv "${src}" "${dest_root}"
   set +x
