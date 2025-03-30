44d223c2f89c209c79b2499d1d58dfa0938ec327
Jeff Olivier <jeffolivier@google.com>
2025-03-30 20:14:50 +0000

    Patch the rpath again
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- utils/rpms/fix_files.sh
+++ utils/rpms/fix_files.sh
@@ -9,12 +9,15 @@ run_patchelf()
   set -x
   echo "Patching $1"
   rpath=$(patchelf --print-rpath "$1")
-  if [ -n "${rpath}" ]; then
-    patched_rpath=$(echo "${rpath}" | sed "s!${buildbase}!!g")
-    patchelf --set-rpath "${patched_rpath}" "$1"
+  if [[ ${rpath} =~ ${buildbase} ]]; then
+    if [ -n "${rpath}" ]; then
+      patched_rpath=$(echo "${rpath}" | sed "s!${buildbase}!!g")
+      patchelf --set-rpath "${patched_rpath}" "$1"
+    fi
   fi
 }
 
 export -f run_patchelf
+export buildbase
 grep -IrL '' "${fixroot}" | xargs -I % bash -c "run_patchelf '%'"
 grep -Irl '' "${fixroot}" | xargs -I % sed -i "s!${buildbase}!!g" '%'
