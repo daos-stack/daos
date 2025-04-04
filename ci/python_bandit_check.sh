013101bccc15bc2bb3f0154b8d3e5de04292e6f9
Jeff Olivier <jeffolivier@google.com>
2025-04-04 10:54:08 -0600

    Try disabling bandit folders in pyproject
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- ci/python_bandit_check.sh
+++ ci/python_bandit_check.sh
@@ -5,5 +5,5 @@ set -uex
 git clean -dxf
 
 bandit --format xml -o bandit.xml -r . \
-       --exclude ./utils/rpms/_topdir,./src/deps \
+       --exclude ./utils/rpms/_topdir \
        -c ci/bandit.config || true
