fd59638b878d6af32f8968cecd99e05899194cf7
Jeff Olivier <jeffolivier@google.com>
2025-04-08 19:33:39 -0600

    See if bandit check is messing stuff up
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- ci/python_bandit_check.sh
+++ ci/python_bandit_check.sh
@@ -5,8 +5,12 @@ set -uex
 git clean -dxf
 # Nothing seems to work to filter the submodules but as they are separate
 # projects, things like bandit should be in those projects, not in DAOS.
-rm -rf deps/{spdk,mercury,ofi,argobots,fused,protobufc,isal,isal_crypto,pmdk,ucx}
+rm -rf tmp
+mkdir -p tmp
+cp -R debian src utils ci doc site_scons tmp
 
+pushd tmp || exit 1
 bandit --format xml -o bandit.xml -r . \
        --exclude ./utils/rpms/_topdir \
        -c ci/bandit.config || true
+popd
