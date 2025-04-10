000c1cce586ef5aea08c38a8a0d287b7a7a0043c
Phil Henderson <phillip.henderson@hpe.com>
2025-04-10 17:33:56 -0400

    Always run gcovr with the same python version.
    Skip-build-leap15-icc: true
    Skip-build-leap15-gcc: true
    Skip-build-leap15-rpm: true
    Skip-build-el8-rpm: true
    Skip-build-el9-rpm: true
    Skip-nlt: true
    Skip-unit-test-memcheck: true
    Skip-func-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- ci/unit/test_main_node.sh
+++ ci/unit/test_main_node.sh
@@ -77,14 +77,12 @@ rm -rf "$test_log_dir"
 python3 -m venv venv
 # shellcheck disable=SC1091
 source venv/bin/activate
-# Following commands work with python3.11
 # touch venv/pip.conf
 # pip config set global.progress_bar off
 # pip config set global.no_color true
 
 pip install --upgrade pip
 pip install --requirement requirements-utest.txt
-pip install --requirement requirements-code-coverage.txt
 
 pip install /opt/daos/lib/daos/python/
 
@@ -93,6 +91,17 @@ utils/run_utest.py $RUN_TEST_VALGRIND --no-fail-on-error $VDB_ARG --log_dir="$te
 
 # Generate code coverage report
 if [[ -n $(find build -name "*.gcda") ]]; then
+    # Run gcovr in a python 3.11 environment
+    deactivate
+    python3.11 -m venv venv-code-coverage
+    # shellcheck disable=SC1091
+    source venv/bin/activate
+    touch venv/pip.conf
+    pip config set global.progress_bar off
+    pip config set global.no_color true
+    pip install --upgrade pip
+    pip install --requirement requirements-code-coverage.txt
+
     mkdir -p "${test_log_dir}/code_coverage"
     gcovr -o "${test_log_dir}/code_coverage/code_coverage_report.html" --html-details --gcov-ignore-parse-errors
     gcovr --json "${test_log_dir}/code_coverage/code_coverage.json" --gcov-ignore-parse-errors
