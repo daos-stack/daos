388ebd6c362e7bf4d5700e0187417b6486cfad6d
Niu Yawei <yawei.niu@intel.com>
2025-04-11 10:11:03 +0800

    DAOS-17180 ddb: replace device offline (#16167)
    If a NVMe SSD namespace is failed to be discovered by SPDK, the engine
    will fail to start due to missing device, so there is no way to do the
    online device replacing.
    
    To fix above missing SSD namespace issue, offline device replacing
    could be used in following steps:
    
    - Run 'ddb dev_list' to show all devices;
    - Run 'ddb dev_replace' to replace the broken SSD with an unused new SSD
    - Start engine;
    - Run 'dmg storage replace' with same old and new device UUID to revive
      the replaced SSD (blobs will be auto created and reintegration will be
      triggered);
    
    Limitation:
    Offline device replacing can't handle the missing sys SSD in md-on-ssd
    mode (RDB is located on sys SSD in md-on-ssd mode) so far.
    
    Signed-off-by: Niu Yawei <yawei.niu@hpe.com>

--- src/bio/bio_device.c
+++ src/bio/bio_device.c
@@ -576,15 +576,8 @@ bio_dev_list(struct bio_xs_context *xs_ctxt, d_list_t *dev_list, int *dev_cnt)
 
 	/*
 	 * Scan remaining SMD devices not present bio_bdev list.
-	 *
-	 * As for current implementation, there won't be any device
-	 * present in SMD but not in bio_bdev list, here we just do
-	 * it for sanity check.
 	 */
 	d_list_for_each_entry(s_info, &s_dev_list, sdi_link) {
-		D_ERROR("Found unexpected device "DF_UUID" in SMD\n",
-			DP_UUID(s_info->sdi_id));
-
 		b_info = alloc_dev_info(s_info->sdi_id, NULL, s_info);
 		if (b_info == NULL) {
 			D_ERROR("Failed to allocate device info\n");
