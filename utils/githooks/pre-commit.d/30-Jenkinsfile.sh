7f210887371f72db577ef375b6817ffb1f1b539c
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-04-11 17:28:35 +0200

    SRE-2832 build: unblock Build, Unit tests, Functional test (EL8) stages in FC Lab (#16139)
    Set of changes required for FC Lab to enable RPM build, Unit Tests and Functional tests
    
    All changes shall be harmless for RR Lab
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
    Co-authored-by: John E. Malmberg <john.malmberg@hpe.com>
    Co-authored-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/githooks/pre-commit.d/30-Jenkinsfile.sh
+++ utils/githooks/pre-commit.d/30-Jenkinsfile.sh
@@ -10,6 +10,7 @@
 #
 # Will only check if Jenkinsfile is modified
 #
+# shellcheck disable=SC2317
 
 set -ue
 
@@ -26,6 +27,9 @@ exit 0
 
 echo "Checking syntax"
 : "${JENKINS_HOST:=build.hpdd.intel.com}"
+echo "Temporary no access to $JENKINS_HOST. Skipping"
+exit 0
+# shellcheck disable=SC2317
 if ! ping -c 1 "$JENKINS_HOST" &> /dev/null; then
     echo "Failed to access $JENKINS_HOST. Skipping"
     exit 0
