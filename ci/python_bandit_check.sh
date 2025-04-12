91141642802954c55ff415fa25699ced69804dc9
Jeff Olivier <jeffolivier@google.com>
2025-04-11 21:19:25 -0600

    Fix bandit
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- ci/python_bandit_check.sh
+++ ci/python_bandit_check.sh
@@ -7,7 +7,7 @@ git clean -dxf
 # projects, things like bandit should be in those projects, not in DAOS.
 rm -rf tmp
 mkdir -p tmp
-cp -R debian src utils ci doc site_scons tmp
+cp -R SConstruct debian src utils ci docs site_scons tmp
 
 pushd tmp || exit 1
 bandit --format xml -o bandit.xml -r . \
