705f1516a822c0e37129023dce825f9993b03e55
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-04-14 08:25:06 +0200

    Obvious shellcheck fix
    Priority: 2
    Cancel-prev-build: false
    
    Skip-func-test-el9: false
    Skip-func-test-leap15: false
    Skip-func-hw-test-medium-md-on-ssd: false
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- src/tests/ftest/scripts/main.sh
+++ src/tests/ftest/scripts/main.sh
@@ -90,7 +90,7 @@ export TEST_RPMS
 export DAOS_BASE
 export DAOS_TEST_APP_SRC=${DAOS_TEST_APP_SRC:-"/scratch/daos_test/apps"}
 export DAOS_TEST_APP_DIR=${DAOS_TEST_APP_DIR:-"${DAOS_TEST_SHARED_DIR}/daos_test/apps"}
-if [ -n HTTPS_PROXY ]; then
+if [ -n "$HTTPS_PROXY" ]; then
     # shellcheck disable=SC2154
     export HTTPS_PROXY="${HTTPS_PROXY:-""}"
 fi
