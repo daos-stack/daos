44c0acc6568cd794cd77cfc63b2f6a69bf48e7fd
Jeff Olivier <jeffolivier@google.com>
2025-04-15 14:15:04 -0600

    fix process
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- utils/rpms/process_spec.py
+++ utils/rpms/process_spec.py
@@ -1,17 +1,18 @@
 #!/bin/env python3
 import os
 import re
-import sys
+
 
 def process_input(fname, script_dir, out):
     """Read one spec file and write to out"""
     with open(f"{script_dir}/{fname}", "r") as inspec:
         for line in inspec.readlines():
-            match = re.match("^__include__\s+(\S+\.spec)", line)
+            match = re.match(r"^__include__\s+(\S+\.spec)", line)
             if not match:
                 out.write(line)
                 continue
             process_input(match.group(1), script_dir, out)
+
 
 def process_spec():
     """Convert daos.spec.in to daos.spec"""
@@ -19,6 +20,6 @@ def process_spec():
     with open(f"{script_dir}/daos.spec", "w") as out:
         process_input("daos.spec.in", script_dir, out)
 
+
 if __name__ == '__main__':
     process_spec()
-
