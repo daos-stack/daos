f749dcc0a1d1c3c9306a0b636b3bddc3f1c85829
Dalton Bohning <dalton.bohning@hpe.com>
2025-04-15 11:33:15 -0700

    DAOS-17400 test: add feature tags to io_sys_admin.py (#16231)
    And to critical_integration.py since regressions have been missed multiple times.
    
    Signed-off-by: Dalton Bohning <dalton.bohning@hpe.com>

--- src/tests/ftest/deployment/io_sys_admin.py
+++ src/tests/ftest/deployment/io_sys_admin.py
@@ -1,5 +1,6 @@
 """
   (C) Copyright 2018-2024 Intel Corporation.
+  (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
@@ -33,7 +34,7 @@ class IoSysAdmin(DataMoverTestBase, FileCountTestBase):
 
         :avocado: tags=all,full_regression
         :avocado: tags=hw,medium
-        :avocado: tags=deployment,datamover,ior,mdtest
+        :avocado: tags=deployment,datamover,ior,mdtest,control,dfuse
         :avocado: tags=IoSysAdmin,test_io_sys_admin
         """
         # local param
