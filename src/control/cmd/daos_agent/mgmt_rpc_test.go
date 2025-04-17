9a87dfc208caa115ee77a863ab5cfa9422177a35
Michael MacDonald <mjmac@google.com>
2025-04-17 22:11:20 +0000

    DAOS-17447 control: Selective telemetry registration
    Update daos_agent configuration and logic to allow for
    selective enablement and registration of client telemetry.
    
    Adds two new daos_agent configuration parameters:
      telemetry_enabled_procs: <regex>
      telemetry_disabled_procs: <regex>
    
    Setting one or the other of these will specify a pattern
    against which connecting client process names will be
    matched to provide more control over behavior. If neither
    are set, then all clients will have telemetry enabled,
    assuming that the value of telemetry_enabled is true.
    
    Includes some improvements to the client telemetry init
    logic to more clearly implement different use cases.
    
    Change-Id: Iaa8b98b008de32e9d3b2c2b8d7583a5155fff9de
    Signed-off-by: Michael MacDonald <mjmac@google.com>

--- src/control/cmd/daos_agent/mgmt_rpc_test.go
+++ src/control/cmd/daos_agent/mgmt_rpc_test.go
@@ -9,7 +9,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net"
+	"os"
 	"sync"
 	"testing"
 
@@ -95,6 +97,15 @@ func TestAgent_mgmtModule_getAttachInfo(t *testing.T) {
 		return out
 	}
 
+	cfgRe := func(in string) *ConfigRegexp {
+		t.Helper()
+		cr := new(ConfigRegexp)
+		if err := cr.FromString(in); err != nil {
+			t.Fatal(err)
+		}
+		return cr
+	}
+
 	for name, tc := range map[string]struct {
 		sysName           string
 		mockGetAttachInfo getAttachInfoFn
@@ -102,6 +113,7 @@ func TestAgent_mgmtModule_getAttachInfo(t *testing.T) {
 		mockGetNetIfaces  func() ([]net.Interface, error)
 		numaGetter        *mockNUMAProvider
 		fabricCfg         []*NUMAFabricConfig
+		tmCfg             *TelemetryConfig
 		reqBytes          []byte
 		expResp           *mgmtpb.GetAttachInfoResp
 		expErr            error
@@ -187,6 +199,48 @@ func TestAgent_mgmtModule_getAttachInfo(t *testing.T) {
 			},
 			expResp: &mgmtpb.GetAttachInfoResp{Status: int32(daos.Unreachable)},
 		},
+		"client telemetry enabled": {
+			reqBytes: reqBytes(&mgmtpb.GetAttachInfoReq{}),
+			tmCfg: &TelemetryConfig{
+				Port:    1234,
+				Enabled: true,
+			},
+			expResp: func(resp *mgmtpb.GetAttachInfoResp) *mgmtpb.GetAttachInfoResp {
+				resp.ClientNetHint.EnvVars = append(resp.ClientNetHint.EnvVars,
+					fmt.Sprintf("%s=1", telemetry.ClientMetricsEnabledEnv),
+					fmt.Sprintf("%s=1", telemetry.ClientMetricsRegisterEnv),
+				)
+				return resp
+			}(respWith(testResp, "test1", "dev1")),
+		},
+		"client telemetry enabled; self not in register pattern": {
+			reqBytes: reqBytes(&mgmtpb.GetAttachInfoReq{}),
+			tmCfg: &TelemetryConfig{
+				Port:       1234,
+				Enabled:    true,
+				RegPattern: cfgRe("moo"),
+			},
+			expResp: func(resp *mgmtpb.GetAttachInfoResp) *mgmtpb.GetAttachInfoResp {
+				resp.ClientNetHint.EnvVars = append(resp.ClientNetHint.EnvVars,
+					fmt.Sprintf("%s=1", telemetry.ClientMetricsEnabledEnv),
+				)
+				return resp
+			}(respWith(testResp, "test1", "dev1")),
+		},
+		"client telemetry enabled; self in ignore pattern": {
+			reqBytes: reqBytes(&mgmtpb.GetAttachInfoReq{}),
+			tmCfg: &TelemetryConfig{
+				Port:       1234,
+				Enabled:    true,
+				IgnPattern: cfgRe(`.*\.test`),
+			},
+			expResp: func(resp *mgmtpb.GetAttachInfoResp) *mgmtpb.GetAttachInfoResp {
+				resp.ClientNetHint.EnvVars = append(resp.ClientNetHint.EnvVars,
+					fmt.Sprintf("%s=1", telemetry.ClientMetricsEnabledEnv),
+				)
+				return resp
+			}(respWith(testResp, "test1", "dev1")),
+		},
 	} {
 		t.Run(name, func(t *testing.T) {
 			log, buf := logging.NewTestLogger(t.Name())
@@ -236,14 +290,20 @@ func TestAgent_mgmtModule_getAttachInfo(t *testing.T) {
 				nf := NUMAFabricFromConfig(log, tc.fabricCfg)
 				ic.EnableStaticFabricCache(test.Context(t), nf)
 			}
+			if tc.tmCfg != nil {
+				if err := tc.tmCfg.Validate(); err != nil {
+					t.Fatal(err)
+				}
+			}
 			mod := &mgmtModule{
 				log:        log,
 				sys:        testSys,
 				cache:      ic,
 				numaGetter: tc.numaGetter,
+				tmCfg:      tc.tmCfg,
 			}
 
-			respBytes, err := mod.handleGetAttachInfo(test.Context(t), tc.reqBytes, 123)
+			respBytes, err := mod.handleGetAttachInfo(test.Context(t), tc.reqBytes, int32(os.Getpid()))
 
 			test.CmpErr(t, tc.expErr, err)
 
@@ -321,7 +381,8 @@ func TestAgent_mgmtModule_getAttachInfo_Parallel(t *testing.T) {
 		go func(n int) {
 			defer wg.Done()
 
-			_, err := mod.getAttachInfo(test.Context(t), 0,
+			_, err := mod.getAttachInfo(test.Context(t),
+				&procInfo{pid: 0},
 				&mgmtpb.GetAttachInfoReq{
 					Sys: sysName,
 				})
