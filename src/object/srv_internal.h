42d7c68bb3dc86522b1199faed0c72a4787dd721
Yokesh Jayakumar <karthj@google.com>
2025-04-25 17:30:06 +0000

    Determine whether to fault injection in caller of obj_bulk_transfer
    Signed-off-by: Yokesh Jayakumar <karthj@google.com>

--- src/object/srv_internal.h
+++ src/object/srv_internal.h
@@ -195,12 +195,6 @@ enum latency_type {
 	VOS_LATENCY,
 };
 
-enum dtx_target_identity {
-	DTX_UNSPECIFIED,
-	DTX_LEADER,
-	DTX_FOLLOWER,
-};
-
 static inline void
 obj_update_latency(uint32_t opc, uint32_t type, uint64_t latency, uint64_t io_size)
 {
@@ -291,8 +285,7 @@ typedef int (*ds_iofw_cb_t)(crt_rpc_t *req, void *arg);
 int
 obj_bulk_transfer(crt_rpc_t *rpc, crt_bulk_op_t bulk_op, bool bulk_bind, crt_bulk_t *remote_bulks,
 		  uint64_t *remote_offs, uint8_t *skips, daos_handle_t ioh, d_sg_list_t **sgls,
-		  int sgl_nr, int bulk_nr, struct obj_bulk_args *p_arg, struct dtx_handle *dth,
-		  enum dtx_target_identity);
+		  int sgl_nr, int bulk_nr, struct obj_bulk_args *p_arg, bool check_conn);
 int obj_tgt_punch(struct obj_tgt_punch_args *otpa, uint32_t *shards, uint32_t count);
 int obj_tgt_query(struct obj_tgt_query_args *otqa, uuid_t po_uuid, uuid_t co_hdl, uuid_t co_uuid,
 		  daos_unit_oid_t oid, daos_epoch_t epoch, daos_epoch_t epoch_first,
