1a508e360ef9aea27d5c0300710249bbdd407f2b
Johann Lombardi <johann.lombardi@hpe.com>
2025-04-28 15:35:39 +0200

    Bump defaults further
    Signed-off-by: Johann Lombardi <johann.lombardi@hpe.com>

--- utils/scripts/tune-dfuse.sh
+++ utils/scripts/tune-dfuse.sh
@@ -6,6 +6,6 @@ for bdi in $bdis; do
 	echo "Setting tunables for $bdi"
 	echo "... bumping readahead to 4M"
 	echo 4096 > /sys/class/bdi/"$bdi"/read_ahead_kb
-	echo "... bumping max dirty ratio to 50%"
-	echo 50 > /sys/class/bdi/"$bdi"/max_ratio
+	echo "... bumping max dirty ratio to 100%"
+	echo 100 > /sys/class/bdi/"$bdi"/max_ratio
 done
