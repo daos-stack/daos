8af7c0201e36d8eaa9b5201e40e5abc48858ab8f
Jeff Olivier <jeffolivier@google.com>
2025-05-05 17:55:13 -0600

    Update daos_dist_tx.c
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- src/tests/suite/daos_dist_tx.c
+++ src/tests/suite/daos_dist_tx.c
@@ -3193,7 +3193,6 @@ dtx_44(void **state)
 	test_set_engine_fail_loc(arg, CRT_NO_RANK,
 				 DAOS_CLIENT_UNREACHABLE_CPD_BODY | DAOS_FAIL_ONCE);
 	par_barrier(PAR_COMM_WORLD);
-  
 	MUST(daos_tx_open(arg->coh, &th, 0, NULL));
 
 	arg->async = 0;
