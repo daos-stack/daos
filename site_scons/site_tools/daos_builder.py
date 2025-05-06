6035395330e588926131f801b11335d7b37240a1
Ding-Hwa Ho <ding-hwa.ho@hpe.com>
2025-05-06 18:44:40 -0400

    Restore _static_library
    Signed-off-by: Ding-Hwa Ho <ding-hwa.ho@hpe.com>

--- site_scons/site_tools/daos_builder.py
+++ site_scons/site_tools/daos_builder.py
@@ -129,7 +129,7 @@ def _run_command(env, target, sources, daos_libs, command):
 
 def _static_library(env, *args, **kwargs):
     """Build SharedLibrary with relative RPATH"""
-    kwargs = _add_code_coverage(**kwargs)
+    # kwargs = _add_code_coverage(**kwargs)
     libname = _get_libname(*args, **kwargs)
     if 'hide_syms' in kwargs:
         # Allow for auto-hiding of symbols, used for the Interception library.  There are multiple
@@ -141,6 +141,8 @@ def _static_library(env, *args, **kwargs):
         kwargs['target'] = f"{real_target}_source"
     else:
         real_target = None
+    kwargs = _add_code_coverage(**kwargs)
+
     lib = env.StaticLibrary(*args, **kwargs)
     if real_target:
         lib = env.Command(real_target, lib, 'objcopy --localize-hidden $SOURCE $TARGET')
