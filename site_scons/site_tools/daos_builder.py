bbc60007230a299bf5efd919f87ef06118e3302a
Ding-Hwa Ho <ding-hwa.ho@hpe.com>
2025-05-07 22:01:13 -0400

    Update _add_code_coverage for debug
    Signed-off-by: Ding-Hwa Ho <ding-hwa.ho@hpe.com>

--- site_scons/site_tools/daos_builder.py
+++ site_scons/site_tools/daos_builder.py
@@ -253,27 +253,27 @@ def _configure_mpi(self):
     return None
 
 
-def _add_code_coverage(env, **kwargs):
-    """Add library for code coverage"""
-    if GetOption("test_coverage"):
-        if 'LIBS' not in kwargs:
-            kwargs['LIBS'] = []
-        elif isinstance(kwargs['LIBS'], str):
-            kwargs['LIBS'] = [kwargs['LIBS']]
-        else:
-            env.AppendUnique(LIBS=['gcov'])
-        kwargs['LIBS'].append('gcov')
-    return kwargs
 # def _add_code_coverage(env, **kwargs):
 #     """Add library for code coverage"""
 #     if GetOption("test_coverage"):
-#         if 'LIBS' in kwargs:
-#             if isinstance(kwargs['LIBS'], str):
-#                 kwargs['LIBS'] = [kwargs['LIBS']]
-#             kwargs['LIBS'].append('gcov')
+#         if 'LIBS' not in kwargs:
+#             kwargs['LIBS'] = []
+#         elif isinstance(kwargs['LIBS'], str):
+#             kwargs['LIBS'] = [kwargs['LIBS']]
 #         else:
 #             env.AppendUnique(LIBS=['gcov'])
+#         kwargs['LIBS'].append('gcov')
 #     return kwargs
+def _add_code_coverage(env, **kwargs):
+    """Add library for code coverage"""
+    if GetOption("test_coverage"):
+        if 'LIBS' in kwargs:
+            if isinstance(kwargs['LIBS'], str):
+                kwargs['LIBS'] = [kwargs['LIBS']]
+            kwargs['LIBS'].append('gcov')
+        else:
+            env.AppendUnique(LIBS=['gcov'])
+    return kwargs
 
 
 def generate(env):
