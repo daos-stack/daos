8eebd5a4ac1026880ad015abb0f63fab6d0b8106
Ryon Jensen <ryon.jensen@hpe.com>
2025-05-13 14:57:01 -0600

    Fix line-too-long issue
    Signed-off-by: Ryon Jensen <ryon.jensen@hpe.com>

--- src/tests/ftest/process_core_files.py
+++ src/tests/ftest/process_core_files.py
@@ -320,7 +320,8 @@ class CoreFileProcessing():
         cmd = ["sudo", "dnf", "-y"]
         if self.is_el() or "suse" in self.distro_info.name.lower():
             cmd.append("--enablerepo=*debug*")
-            cmd.append("--disablerepo='epel-modular-debuginfo,epel-testing-debuginfo,epel-testing-modular-debuginfo'")
+            cmd.append("--disablerepo='epel-modular-debuginfo,epel-testing-debuginfo,"
+                       "epel-testing-modular-debuginfo'")
         cmd.append("install")
         for pkg in install_pkgs:
             try:
@@ -342,7 +343,8 @@ class CoreFileProcessing():
             cmd_prefix = ["sudo", "dnf"]
             if self.is_el() or "suse" in self.distro_info.name.lower():
                 cmd_prefix.append("--enablerepo=*debug*")
-                cmd.append("--disablerepo='epel-modular-debuginfo,epel-testing-debuginfo,epel-testing-modular-debuginfo'")
+                cmd.append("--disablerepo='epel-modular-debuginfo,epel-testing-debuginfo,"
+                           "epel-testing-modular-debuginfo'")
             cmds.insert(0, cmd_prefix + ["clean", "all"])
             cmds.insert(1, cmd_prefix + ["makecache"])
             for cmd in cmds:
