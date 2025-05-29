886b08144d2dbf356f55eac96afa05a007f20b4d
Xuezhao Liu <xuezhao.liu@hpe.com>
2025-05-29 08:36:47 +0000

    DAOS-17381 rebuild: refine rebuild IO scheduling and rebuild IV
    1. set iv.riv_master_rank as  ns->iv_master_rank for rebuild_tgt_status_check_ult
    2. for rebuild IO, apply SCHED_REQ_MIGRATE for scheduler throttling
    3. use d_list_del_init for rgt delete
    4. refine yield for rebuild scanner
    5. skip orphan container for rebuild scanner
    
    Several patches backport -
    2a74db871 - rebuild: fix possible hang in rebuild_obj_send_cb (#16146)
    61dcef126 - rebuild: cannot retry infinitely for dsc_obj_fetch timeout (#16083)
    caf1a2524 - rebuild: exit rebuild_tgt_status_check_ult when RPT stale (#15994)
    c86aa7bcc - cart: refine corpc fail handling for CRT_RPC_FLAG_CO_FAILOUT (#15572)
    
    Signed-off-by: Xuezhao Liu <xuezhao.liu@hpe.com>
    ---

--- src/include/daos_errno.h
+++ src/include/daos_errno.h
@@ -1,5 +1,6 @@
 /**
  * (C) Copyright 2016-2024 Intel Corporation.
+ * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -212,7 +213,8 @@ extern "C" {
 	ACTION(DER_DIV_BY_ZERO,	Division by zero)						   \
 	/** Target is overload, retry RPC */							   \
 	ACTION(DER_OVERLOAD_RETRY, "retry later because of overloaded service")			   \
-	ACTION(DER_NOT_RESUME, Cannot resume former DAOS check instance)
+	ACTION(DER_NOT_RESUME, Cannot resume former DAOS check instance)			   \
+	ACTION(DER_CONT_NONEXIST, The specified container does not exist)
 
 /** Defines the gurt error codes */
 #define D_FOREACH_ERR_RANGE(ACTION)	\
