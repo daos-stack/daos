779fa37fc56730d0cee4fdc8612a82e0309cd761
Ding-Hwa Ho <ding-hwa.ho@hpe.com>
2025-06-02 19:28:50 -0400

    Update cp -r daos_srv.
    Signed-off-by: Ding-Hwa Ho <ding-hwa.ho@hpe.com>

--- ci/unit/test_main_node.sh
+++ ci/unit/test_main_node.sh
@@ -86,7 +86,7 @@ python3.11 -m venv venv
 ls -la /usr/lib64/
 cp -r /opt/daos/lib64/python3.6/site-packages/storage_estimator venv/lib64/python3.11/site-packages/
 # cp -r /opt/daos/lib64/daos_srv/libvos_size.so venv/lib64/
-cp -r /opt/daos/lib64/daos_srv/libvos_size.so venv/lib64/daos_srv/
+cp -r /opt/daos/lib64/daos_srv venv/lib64/
 
 # shellcheck disable=SC1091
 source venv/bin/activate
