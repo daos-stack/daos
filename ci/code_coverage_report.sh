2cb807541d27ce14103feb771d5c8eac797f66f0
Ding-Hwa Ho <ding-hwa.ho@hpe.com>
2025-06-13 19:46:00 -0400

    Update with --add-tracefile $jsonfiles
    Skip-build-el8-rpm: true
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    Skip-nlt: true
    Skip-unit-test-memcheck: true
    Skip-func-test: true
    
    Signed-off-by: Ding-Hwa Ho <ding-hwa.ho@hpe.com>

--- ci/code_coverage_report.sh
+++ ci/code_coverage_report.sh
@@ -5,8 +5,9 @@ set -eux
 # Generate code coverage report
 if [[ -n $(find . -name "code_coverage.json") ]]; then
     mkdir -p code_coverage_report
-#  tmp  jsonfiles=$(find * -name "code_coverage.json")
-    gcovr --add-tracefile "*/code_coverage/code_coverage.json" \
+    jsonfiles=$(find * -name "code_coverage.json")
+    # gcovr --add-tracefile "*/code_coverage/code_coverage.json" \
+    gcovr --add-tracefile "$jsonfiles" \
           --output code_coverage_report/code_coverage_report.html \
           --html-details --gcov-ignore-parse-errors
 fi
