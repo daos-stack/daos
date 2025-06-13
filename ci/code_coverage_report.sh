830935c96053cdf77afb91b0960477c299772e85
Ding-Hwa Ho <ding-hwa.ho@hpe.com>
2025-06-13 14:18:35 -0400

    Update with find jsonfiles for gcovr.
    Skip-build-el8-rpm: true
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    Skip-nlt: true
    Skip-unit-test-memcheck: true
    Skip-func-test: true
    
    Signed-off-by: Ding-Hwa Ho <ding-hwa.ho@hpe.com>

--- ci/code_coverage_report.sh
+++ ci/code_coverage_report.sh
@@ -6,7 +6,8 @@ set -eux
 # Generate code coverage report
 if [[ -n $(find . -name "code_coverage.json") ]]; then
     mkdir -p code_coverage_report
-    gcovr --json-add-tracefile "*/code_coverage/code_coverage.json" \
-          -o code_coverage_report/code_coverage_report.html \
+    jsonfiles=$(find * -name "code_coverage.json")
+    gcovr --json-add-tracefile $jsonfiles \
+          --output code_coverage_report/code_coverage_report.html \
           --html-details --gcov-ignore-parse-errors
 fi
