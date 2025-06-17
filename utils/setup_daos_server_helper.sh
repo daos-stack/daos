8db162c49064eeba7e1aa07e7e272d2ec4a98a2b
Tom Nabarro <thomas.nabarro@hpe.com>
2025-06-17 23:45:08 +0100

    DAOS-17708 utils: Fix setup_daos_server_helper.sh
    Signed-off-by: Tom Nabarro <thomas.nabarro@hpe.com>

--- utils/setup_daos_server_helper.sh
+++ utils/setup_daos_server_helper.sh
@@ -68,10 +68,10 @@ USR_CTL=/usr/share/daos/control
 echo -n "Creating SPDK script links under $USR_SPDK ... "
 mkdir -p "$USR_SPDK/scripts" "$USR_CTL"
 if ! [ -e "$USR_SPDK/scripts/setup.sh" ]; then
-        ln -sf "$SL_SPDK_PREFIX/share/spdk/scripts/setup.sh" "$USR_SPDK/scripts"
+        ln -sf "$SL_SPDK_PREFIX/share/daos/spdk/scripts/setup.sh" "$USR_SPDK/scripts"
 fi
 if ! [ -e "$USR_SPDK/scripts/common.sh" ]; then
-        ln -sf "$SL_SPDK_PREFIX/share/spdk/scripts/common.sh" "$USR_SPDK/scripts"
+        ln -sf "$SL_SPDK_PREFIX/share/daos/spdk/scripts/common.sh" "$USR_SPDK/scripts"
 fi
 if ! [ -e "$USR_SPDK/include" ]; then
         ln -s "$SL_SPDK_PREFIX/include" "$USR_SPDK"/include
