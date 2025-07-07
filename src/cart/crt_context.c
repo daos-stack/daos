4e8d81617631df4f2752ad7464a8b9d95576f9b8
Jeff Olivier <jeffolivier@google.com>
2025-07-07 08:44:41 -0600

    Address review comment
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- src/cart/crt_context.c
+++ src/cart/crt_context.c
@@ -349,7 +349,7 @@ crt_context_provider_create(crt_context_t *crt_ctx, crt_provider_t provider, boo
 	}
 
 	*crt_ctx = (crt_context_t)ctx;
-	D_INFO("created context (idx %d, self_uri %s)\n", ctx->cc_idx, ctx->cc_self_uri);
+	D_DEBUG(DB_ALL, "created context (idx %d, self_uri %s)\n", ctx->cc_idx, ctx->cc_self_uri);
 
 out:
 	return rc;
