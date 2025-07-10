3f3a47e117a45b5a48535dccfe47f297f769851a
Kris Jacque <kris.jacque@hpe.com>
2025-07-10 20:40:51 +0000

    Unit tests for MemberAddrMap remove
    Skip-func-test: true
    Skip-nlt: true
    
    Signed-off-by: Kris Jacque <kris.jacque@hpe.com>

--- src/control/system/raft/database_members.go
+++ src/control/system/raft/database_members.go
@@ -1,5 +1,6 @@
 //
 // (C) Copyright 2020-2022 Intel Corporation.
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -68,6 +69,8 @@ func (mam MemberAddrMap) removeMember(m *system.Member) {
 	}
 	if len(members) == 0 {
 		delete(mam, m.Addr.String())
+	} else {
+		mam[m.Addr.String()] = members
 	}
 }
 
