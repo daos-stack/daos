be3db5c20f29c1cd833846e4e23d7a41325ed36f
Alexander A Oganezov <alexander.oganezov@hpe.com>
2025-07-14 19:03:56 +0000

    DAOS-17712 cart: race in test_multisend_server
    When multisend servers start, rank=0 saves group config file,
    which lets clients know they can start sending RPCs.
    
    Since multisend test servers dont communicate with each other there
    is a small race where rank=0 might save group config file before
    all other test servers finished registering for RPCs.
    
    Further, previously even rank=0 was saving group config file before
    it itself was ready to accept RPCs.
    
    The workaround moves RPC registration before group_config_save and
    adds 5 second delay on rank=0 before group info is saved.
    
    Signed-off-by: Alexander A Oganezov <alexander.oganezov@hpe.com>

--- src/tests/ftest/cart/test_multisend_server.c
+++ src/tests/ftest/cart/test_multisend_server.c
@@ -125,14 +125,19 @@ test_run(d_rank_t my_rank)
 	rc = sem_init(&test.tg_token_to_proceed, 0, 0);
 	D_ASSERTF(rc == 0, "sem_init() failed.\n");
 
-	if (my_rank == 0) {
-		DBG_PRINT("Saving group (%s) config file\n", test.tg_local_group_name);
-		rc = crt_group_config_save(grp, true);
-		D_ASSERTF(rc == 0,
-			  "crt_group_config_save() failed. rc: %d\n", rc);
-	}
 	rc = crt_proto_register(&my_proto_fmt);
 	D_ASSERT(rc == 0);
+
+	/* Rank 0 is chosen to save group config file for all servers */
+	if (my_rank == 0) {
+		/* Allow 5 seconds for other servers to register protocol */
+		sleep(5);
+
+		/* Note: saving group config file tells clients they can send rpcs */
+		DBG_PRINT("Saving group (%s) config file\n", test.tg_local_group_name);
+		rc = crt_group_config_save(grp, true);
+		D_ASSERTF(rc == 0, "crt_group_config_save() failed. rc: %d\n", rc);
+	}
 
 	rc = pthread_join(test.tg_tid[0], NULL);
 	D_ASSERTF(rc == 0, "pthread_join failed. rc: %d\n", rc);
