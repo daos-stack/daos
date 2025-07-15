2b45f76c8bd937d44528074ebfc0dd36215c8381
Makito Kano <makito.kano@hpe.com>
2025-07-15 06:13:46 +0000

    DAOS-17768 test: Pool rd_fac modification test
    Pool's rd_fac can now be modified after creating a pool.
    Update pool/rf.py to test this feature.
    
    Test steps:
    1. Create a pool with rd_fac passed in.
    2. Verify pool's rd_fac by calling dmg pool get-prop.
    3. Verify pool's rd_fac can't be changed to invalid value.
    4. Verify that pool and container's rd_fac haven't been
    changed.
    5. Verify container's default and requested rd_fac.
    6. Update pool's rd_fac to a valid value and verify.
    7. Verify container's default and requested rd_fac with
    updated pool rd_fac.
    8. Verify that while pool's rd_fac was changed, container's
    rd_fac remained the
    same.
    
    The original rf.py was just step 5. Added other steps
    that involve changing pool's rd_fac and verifying places
    that are impacted and not impacted by the change.
    
    Also added documentation.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: test_rf_pool_property
    
    Signed-off-by: Makito Kano <makito.kano@hpe.com>

--- src/tests/ftest/pool/rf.py
+++ src/tests/ftest/pool/rf.py
@@ -1,13 +1,17 @@
 '''
-  (C) Copyright 2022 Intel Corporation.
+  (C) Copyright 2022-2024 Intel Corporation.
+  (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 '''
+import re
+
+from exception_utils import CommandFailure
+from general_utils import report_errors
 from ior_test_base import IorTestBase
 
 
 class PoolRedunFacProperty(IorTestBase):
-    # pylint: disable=too-few-public-methods
     """Run tests with different pool redundancy factor.
 
     Test Class Description: To validate pool rf works properly
@@ -27,37 +31,17 @@ class PoolRedunFacProperty(IorTestBase):
         rf_value = int(rf_str.replace("rd_fac", ""))
         self.assertEqual(expected_value, rf_value)
 
-    def test_rf_pool_property(self):
-        """Jira ID: DAOS-9217.
+    def create_verify_destroy_cont(self, pool, cont_rfs, pool_prop_expected):
+        """Create container, verify its rd_fac, and destroy it.
 
-        Test Description:
-            Verify container rf is picked from container property
-            and not the pool default rf.
-
-        Use Case:
-            Create the Pool with different rf value
-            Create the container and verify the rf is same for both
-                pool and container.
-            Create the container with different rf value
-            Verify the rf is updated for the container.
-
-        :avocado: tags=all,full_regression
-        :avocado: tags=hw,large
-        :avocado: tags=pool,redundancy,redundancy_factor,rf
-        :avocado: tags=PoolRedunFacProperty,test_rf_pool_property
+        Args:
+            pool (TestPool): Pool to create container.
+            cont_rfs (str): rd_fac value of container we want to set.
+            pool_prop_expected (str): rd_fac value set in the pool.
         """
-        cont_rfs = self.params.get("cont_rf", '/run/container/*')
-
-        # Create the pool
-        self.add_pool()
-
-        # Verify pool rf.
-        pool_prop_expected = int(self.pool.properties.value.split(":")[1])
-        self.assertEqual(pool_prop_expected, self.pool.get_property("rd_fac"))
-
         for cont_rf in cont_rfs:
-            # Initial container
-            self.add_container(self.pool, create=False)
+            # Create container
+            self.add_container(pool, create=False)
 
             # Use the default pool property for container and do not update
             if cont_rf != pool_prop_expected:
@@ -71,3 +55,115 @@ class PoolRedunFacProperty(IorTestBase):
 
             # Destroy the container.
             self.container.destroy()
+
+    def verify_container_rd_fac(self, container, expected_rd_fac, msg):
+        """Verify container rd_fac.
+
+        Args:
+            container (TestContainer): Container to verify.
+            expected_rd_fac (int): Expected rd_fac.
+            msg (str): Message to print during failure.
+        """
+        cont_prop_json = container.get_prop(properties=["rd_fac"])
+        cont_prop_value = cont_prop_json["response"][0]["value"]
+        # Container's rd_fac value has "rd_fac" in front of it. e.g., rd_fac1. The last
+        # number is the value we want.
+        cont_prop = re.findall(r"\d+", cont_prop_value)
+        cont_prop_int = int(cont_prop[0])
+        self.assertEqual(cont_prop_int, expected_rd_fac, msg)
+
+    def test_rf_pool_property(self):
+        """Test rd_fac configuration for pool and container.
+
+        Pool's rd_fac is used to define the default rd_fac value of its container. If a
+        container is created without rd_fac during container create, pool's rd_fac will be
+        used. If user wants to set different rd_fac from pool's, they can pass it in as
+        --properties=rd_fac:N during container create.
+
+        Test Description:
+        1. Create a pool with rd_fac passed in.
+        2. Verify pool's rd_fac by calling dmg pool get-prop.
+        3. Verify pool's rd_fac can't be changed to invalid value.
+        4. Verify that pool and container's rd_fac haven't been changed.
+        5. Verify container's default and requested rd_fac.
+        6. Update pool's rd_fac to a valid value and verify.
+        7. Verify container's default and requested rd_fac with updated pool rd_fac.
+        8. Verify that while pool's rd_fac was changed, container's rd_fac remained the
+        same.
+
+        Jira ID: DAOS-9217, DAOS-17768
+
+        :avocado: tags=all,full_regression
+        :avocado: tags=hw,large
+        :avocado: tags=pool,redundancy,redundancy_factor,rf
+        :avocado: tags=PoolRedunFacProperty,test_rf_pool_property
+        """
+        cont_rfs = self.params.get("cont_rf", '/run/container/*')
+
+        # 1. Create a pool with rd_fac passed in.
+        self.log_step("Create a pool with rd_fac passed in.")
+        pool = self.get_pool()
+
+        # 2. Verify pool's rd_fac by calling dmg pool get-prop.
+        self.log_step("Verify pool's rd_fac by calling dmg pool get-prop.")
+        pool_prop_expected = int(pool.properties.value.split(":")[1])
+        self.assertEqual(pool_prop_expected, pool.get_property("rd_fac"))
+
+        # Create a container for later steps.
+        container = self.get_container(pool=pool)
+
+        # 3. Verify pool's rd_fac can't be changed to invalid value.
+        self.log_step("Verify pool's rd_fac can't be changed to invalid value.")
+        invalid_rd_fac = 99
+        dmg_command = self.get_dmg_command()
+        errors = []
+        try:
+            properties = f"rd_fac:{invalid_rd_fac}"
+            dmg_command.pool_set_prop(pool=pool.identifier, properties=properties)
+            self.fail(f"Pool rd_fac was changed to invalid value: {invalid_rd_fac}!")
+        except CommandFailure as command_failure:
+            self.log.info(
+                f"Update pool's rd_fac to {invalid_rd_fac} failed as expected")
+            exp_msg = f"invalid redun fac value {invalid_rd_fac}"
+            if exp_msg not in str(command_failure):
+                msg = (f"Updating pool's rd_fac to invalid value didn't return expected "
+                       f"message! {exp_msg}")
+                errors.append(msg)
+
+        # 4. Verify that pool and container's rd_fac haven't been changed.
+        self.log_step("Verify that pool and container's rd_fac haven't been changed.")
+        pool_prop = pool.get_property(prop_name="rd_fac")
+        msg = "Unexpected pool rd_fac after failed to update! "
+        self.assertEqual(pool_prop, pool_prop_expected, msg)
+        msg = "Unexpected container rd_fac after failed to update pool rd_fac! "
+        self.verify_container_rd_fac(
+            container=container, expected_rd_fac=pool_prop_expected, msg=msg)
+
+        # 5. Verify container's default and requested rd_fac.
+        self.log_step("Verify container's default and requested rd_fac.")
+        self.create_verify_destroy_cont(
+            pool=pool, cont_rfs=cont_rfs, pool_prop_expected=pool_prop_expected)
+
+        # 6. Update pool's rd_fac to a valid value and verify.
+        self.log_step("Update pool's rd_fac to a valid value and verify.")
+        new_pool_rd_fac = 0
+        pool.set_property(prop_name="rd_fac", prop_value=str(new_pool_rd_fac))
+        msg = "Pool rd_fac wasn't updated as expected!"
+        self.assertEqual(new_pool_rd_fac, pool.get_property("rd_fac"), msg)
+
+        # 7. Verify container's default and requested rd_fac with updated pool rd_fac.
+        msg = "Verify container's default and requested rd_fac with updated pool rd_fac."
+        self.log_step(msg)
+        self.create_verify_destroy_cont(
+            pool=pool, cont_rfs=cont_rfs, pool_prop_expected=new_pool_rd_fac)
+
+        # 8. Verify that while pool's rd_fac was changed, container's rd_fac remained the
+        # same.
+        msg = ("Verify that while pool's rd_fac was changed, container's rd_fac remained "
+               "the same.")
+        self.log_step(msg)
+        msg = "Unexpected container rd_fac after pool rd_fac was changed!"
+        self.verify_container_rd_fac(
+            container=container, expected_rd_fac=pool_prop_expected, msg=msg)
+
+        report_errors(test=self, errors=errors)
