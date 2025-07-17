11c31ef54e8e293ed83116325df44805011e7be1
rpadma2 <ravindran.padmanabhan@hpe.com>
2025-07-17 15:48:03 -0400

    DAOS-4753 test: Fix intermittant blank test case failure. Test-tag: OSAOnlineReintegration OSAOnlineDrain OSAOfflineReintegration OSAOfflineDrain Skip-unit-tests: true Skip-fault-injection-test: true
    Signed-off-by: rpadma2 <ravindran.padmanabhan@hpe.com>

--- src/tests/ftest/osa/offline_reintegration.py
+++ src/tests/ftest/osa/offline_reintegration.py
@@ -141,7 +141,12 @@ class OSAOfflineReintegration(OSAUtils, ServerFillUp):
                 loop + 1, self.loop_test_cnt, ranks, str(self.pool))
             for index, rank in enumerate(ranks):
                 if self.test_with_blank_node is True:
-                    ip_addr, p_num = self.get_ipaddr_for_rank(rank)
+                    # If dmg query times out, we retry 3 times before failing the test.
+                    ip_addr = None
+                    retry_count = 0
+                    while ip_addr is None and retry_count < 3:
+                        ip_addr, p_num = self.get_ipaddr_for_rank(rank)
+                        retry_count = retry_count + 1
                     self.remove_pool_dir(ip_addr, p_num)
                 if (index == 2 and "RP_2G" in oclass):
                     output = self.pool.reintegrate(rank, "0,2")
