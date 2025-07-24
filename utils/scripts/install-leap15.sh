c3f85cbaf5faae4b41b7daa13ee9a3fc548e1a71
Phil Henderson <phillip.henderson@hpe.com>
2025-07-24 12:46:37 -0400

    Ensure we have lua-lmod > 8.7.34.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-func-hw-test: true
    Skip-func-test-leap15: false
    Skip-func-test-el8: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/scripts/install-leap15.sh
+++ utils/scripts/install-leap15.sh
@@ -75,6 +75,10 @@ dnf --nodocs install ${dnf_install_args} \
     which \
     yasm
 
+# Make sure we have lua-lmod > 8.7.34
+dnf -y remove lua-lmod
+dnf -y --nogpgcheck install lua-lmod '--repo=*lua*' --repo '*network-cluster*'
+
 # shellcheck disable=SC2086
 dnf install ${dnf_install_args} ruby-devel
 gem install json -v 2.7.6
