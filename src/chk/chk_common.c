de99b5aa6afa124168f87ac66523ecbc1a816b90
Fan Yong <fan.yong@hpe.com>
2025-07-25 11:24:41 +0800

    DAOS-17579 chk: misc improvement for check leader and test - b26
    1. If some engine dead during check, then remove it from the ranks list
       for current check instance to avoid membership trouble in subsequent
       CHK RPC.
    
    2. Use upper UUID string as per-pool based bookbmark name for the check
       leader to distinguish from check engine bookmarks.
    
    3. Enhance CHK query logic to merge with check leader status and phase
       even if the check instance has already exited.
    
    4. Fix CR test logic to verify the result properly. Some code cleanup.
    
    Test-tag: cat_recov
    
    Signed-off-by: Fan Yong <fan.yong@hpe.com>

--- src/chk/chk_common.c
+++ src/chk/chk_common.c
@@ -1,5 +1,6 @@
 /**
  * (C) Copyright 2022-2024 Intel Corporation.
+ * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -491,7 +492,10 @@ chk_pool_stop_one(struct chk_instance *ins, uuid_t uuid, uint32_t status, uint32
 			if (status == CHK__CHECK_POOL_STATUS__CPS_STOPPED)
 				ins->ci_pool_stopped = 1;
 			cbk->cb_time.ct_stop_time = time(NULL);
-			uuid_unparse_lower(uuid, uuid_str);
+			if (ins->ci_is_leader)
+				uuid_unparse_upper(uuid, uuid_str);
+			else
+				uuid_unparse_lower(uuid, uuid_str);
 			rc = chk_bk_update_pool(cbk, uuid_str);
 		}
 
@@ -583,7 +587,10 @@ chk_pool_start_one(struct chk_instance *ins, uuid_t uuid, uint64_t gen)
 	char			uuid_str[DAOS_UUID_STR_SIZE];
 	int			rc;
 
-	uuid_unparse_lower(uuid, uuid_str);
+	if (ins->ci_is_leader)
+		uuid_unparse_upper(uuid, uuid_str);
+	else
+		uuid_unparse_lower(uuid, uuid_str);
 	rc = chk_bk_fetch_pool(&cbk, uuid_str);
 	if (rc != 0 && rc != -DER_NONEXIST)
 		goto out;
@@ -623,7 +630,10 @@ chk_pools_load_list(struct chk_instance *ins, uint64_t gen, uint32_t flags,
 				break;
 		}
 
-		uuid_unparse_lower(pools[i], uuid_str);
+		if (ins->ci_is_leader)
+			uuid_unparse_upper(pools[i], uuid_str);
+		else
+			uuid_unparse_lower(pools[i], uuid_str);
 		rc = chk_bk_fetch_pool(&cbk, uuid_str);
 		if (rc != 0 && rc != -DER_NONEXIST)
 			break;
@@ -686,8 +696,13 @@ chk_pools_load_from_db(struct sys_db *db, char *table, d_iov_t *key, void *args)
 	struct chk_bookmark		 cbk;
 	int				 rc = 0;
 
-	if (!daos_is_valid_uuid_string(uuid_str))
-		D_GOTO(out, rc = 0);
+	if (ins->ci_is_leader) {
+		if (!daos_is_valid_uuid_upper_string(uuid_str))
+			D_GOTO(out, rc = 0);
+	} else {
+		if (!daos_is_valid_uuid_lower_string(uuid_str))
+			D_GOTO(out, rc = 0);
+	}
 
 	rc = chk_bk_fetch_pool(&cbk, uuid_str);
 	if (rc != 0)
@@ -753,7 +768,10 @@ chk_pools_update_bk(struct chk_instance *ins, uint32_t phase)
 		if (cbk->cb_phase < phase &&
 		    cbk->cb_pool_status == CHK__CHECK_POOL_STATUS__CPS_CHECKING) {
 			cbk->cb_phase = phase;
-			uuid_unparse_lower(cpr->cpr_uuid, uuid_str);
+			if (ins->ci_is_leader)
+				uuid_unparse_upper(cpr->cpr_uuid, uuid_str);
+			else
+				uuid_unparse_lower(cpr->cpr_uuid, uuid_str);
 			rc1 = chk_bk_update_pool(cbk, uuid_str);
 			if (rc1 != 0)
 				rc = rc1;
@@ -808,7 +826,10 @@ chk_pool_handle_notify(struct chk_instance *ins, struct chk_iv *iv)
 	if (iv->ci_phase != cbk->cb_phase || iv->ci_pool_status != cbk->cb_pool_status) {
 		cbk->cb_phase = iv->ci_phase;
 		cbk->cb_pool_status = iv->ci_pool_status;
-		uuid_unparse_lower(cpr->cpr_uuid, uuid_str);
+		if (ins->ci_is_leader)
+			uuid_unparse_upper(cpr->cpr_uuid, uuid_str);
+		else
+			uuid_unparse_lower(cpr->cpr_uuid, uuid_str);
 		rc = chk_bk_update_pool(cbk, uuid_str);
 	}
 
