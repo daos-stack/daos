dc7d6d2b7eb81f97c2755e0c208dcbaed3641f46
Jinshan Xiong <jinshanx@google.com>
2025-07-29 19:04:37 -0700

    client_behind_firewall is omitted if not set

--- src/control/cmd/dmg/auto_test.go
+++ src/control/cmd/dmg/auto_test.go
@@ -600,7 +600,7 @@ allow_numa_imbalance: false
 control_log_mask: INFO
 control_log_file: /tmp/daos_server.log
 core_dump_filter: 19
-client_behind_firewall: false
+client_behind_firewall: true
 name: daos_server
 socket_dir: /var/run/daos_server
 provider: ofi+verbs
@@ -617,6 +617,7 @@ hyperthreads: false
 		WithMgmtSvcReplicas("hostX:10002").
 		WithDisableVMD(false).
 		WithDisableHotplug(false).
+		WithClientFirewallMode(true).
 		WithEngines(
 			engine.MockConfig().
 				WithTargetCount(defaultTargetCount).
