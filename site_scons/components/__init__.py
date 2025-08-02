16276e6d43aa8656144471df7fca40cb4ed1a528
Jeff Olivier <jeffolivier@google.com>
2025-08-02 15:10:37 -0600

    DAOS-623 build: Switch back to git clone
    Partial revert of previous commit that contained fixes for having
    components as submodules which Google is using.
    There is no good way to avoid the copy that doesn't pollute submodules
    because we apply patches so best to revert back for now.
    
    Change-Id: I27168a621a3dbb091dd7d4548f52812ef03bd7bf
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>

--- site_scons/components/__init__.py
+++ site_scons/components/__init__.py
@@ -25,7 +25,7 @@
 import platform
 
 import distro
-from prereq_tools import CopyRetriever, GitRepoRetriever
+from prereq_tools import GitRepoRetriever
 from SCons.Script import GetOption
 
 # Check if this is an ARM platform
@@ -129,7 +129,7 @@ def define_mercury(reqs):
         ofi_build.append('--disable-debug')
 
     reqs.define('ofi',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(),
                 commands=[['./autogen.sh'],
                           ofi_build,
                           ['make'],
@@ -154,7 +154,7 @@ def define_mercury(reqs):
         ucx_configure.extend(['--disable-debug', '--disable-logging'])
 
     reqs.define('ucx',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(),
                 libs=['ucs', 'ucp', 'uct'],
                 functions={'ucs': ['ucs_debug_disable_signal']},
                 headers=['uct/api/uct.h'],
@@ -191,7 +191,7 @@ def define_mercury(reqs):
         mercury_build.append('-DMERCURY_ENABLE_DEBUG:BOOL=OFF')
 
     reqs.define('mercury',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(True),
                 commands=[mercury_build,
                           ['make'],
                           ['make', 'install']],
@@ -248,14 +248,14 @@ def define_components(reqs):
     define_ompi(reqs)
 
     reqs.define('isal',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(),
                 commands=[['./autogen.sh'],
                           ['./configure', '--prefix=$ISAL_PREFIX', '--libdir=$ISAL_PREFIX/lib'],
                           ['make'],
                           ['make', 'install']],
                 libs=['isal'])
     reqs.define('isal_crypto',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(),
                 commands=[['./autogen.sh'],
                           ['./configure',
                            '--prefix=$ISAL_CRYPTO_PREFIX',
@@ -265,7 +265,7 @@ def define_components(reqs):
                 libs=['isal_crypto'])
 
     reqs.define('pmdk',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(),
                 commands=[['make',
                            'all',
                            'BUILD_EXAMPLES=n',
@@ -289,7 +289,7 @@ def define_components(reqs):
         abt_build.append('--enable-valgrind')
 
     reqs.define('argobots',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(True),
                 commands=[['./autogen.sh'],
                           abt_build,
                           ['make'],
@@ -298,18 +298,8 @@ def define_components(reqs):
                 libs=['abt'],
                 headers=['abt.h'])
 
-    reqs.define('fuse', libs=['fuse3'], defines=['FUSE_USE_VERSION=35'],
-                retriever=GitRepoRetriever(),
-                commands=[['meson', 'setup', '--prefix=$FUSE_PREFIX', '-Ddisable-mtab=True',
-                           '-Dudevrulesdir=$FUSE_PREFIX/udev', '-Dutils=False',
-                           '--default-library', 'both', '../fuse'],
-                          ['ninja', 'install']],
-                headers=['fuse3/fuse.h'],
-                required_progs=['libtoolize', 'ninja', 'meson'],
-                out_of_src_build=True)
-
     reqs.define('fused', libs=['fused'], defines=['FUSE_USE_VERSION=35'],
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(),
                 commands=[['meson', 'setup', '--prefix=$FUSED_PREFIX', '-Ddisable-mtab=True',
                            '-Dudevrulesdir=$FUSED_PREFIX/udev', '-Dutils=False',
                            '--default-library', 'static', '../fused'],
@@ -340,7 +330,7 @@ def define_components(reqs):
         spdk_arch = 'haswell'
 
     reqs.define('spdk',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(True),
                 commands=[['./configure',
                            '--prefix=$SPDK_PREFIX',
                            '--disable-tests',
@@ -369,7 +359,7 @@ def define_components(reqs):
                 patch_rpath=['lib', 'bin'])
 
     reqs.define('protobufc',
-                retriever=CopyRetriever(),
+                retriever=GitRepoRetriever(),
                 commands=[['./autogen.sh'],
                           ['./configure', '--prefix=$PROTOBUFC_PREFIX', '--disable-protoc'],
                           ['make'],
