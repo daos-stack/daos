6ae5e649caae09bbe96ea873737ae550f928d461
Kris Jacque <kris.jacque@hpe.com>
2025-08-06 23:33:55 +0000

    Move protobuf-dependent defs out of drpc_types.h
    Signed-off-by: Kris Jacque <kris.jacque@hpe.com>

--- src/include/daos/drpc.h
+++ src/include/daos/drpc.h
@@ -30,6 +30,37 @@ daos_drpc_free(void *allocater_data, void *pointer);
 				    .alloc.free = daos_drpc_free,	\
 				    .alloc.allocator_data = &self}
 
+struct unixcomm {
+	int fd;    /** File descriptor of the unix domain socket */
+	int flags; /** Flags set on unix domain socket */
+};
+
+typedef void (*drpc_handler_t)(Drpc__Call *, Drpc__Response *);
+typedef int (*drpc_thread_yielder_t)(void);
+
+/**
+ * dRPC connection context. This includes all details needed to communicate
+ * on the dRPC channel.
+ */
+struct drpc {
+	struct unixcomm      *comm;      /** unix domain socket communication context */
+	int                   sequence;  /** sequence number of latest message sent */
+	uint32_t              ref_count; /** open refs to this ctx */
+
+	/**
+	 * Handler for messages received by a listening drpc context.
+	 * For client contexts, this is NULL.
+	 */
+	drpc_handler_t        handler;
+
+	/**
+	 * Function to yield cycles to a non-dRPC thread, if applicable.
+	 */
+	drpc_thread_yielder_t yield;
+};
+
+enum rpcflags { R_SYNC = 1 };
+
 int drpc_call_create(struct drpc *ctx, int32_t module, int32_t method,
 		     Drpc__Call **callp);
 void drpc_call_free(Drpc__Call *call);
--- src/include/daos/drpc_types.h
+++ src/include/daos/drpc_types.h
@@ -8,7 +8,8 @@
 #ifndef __DAOS_DRPC_TYPES_H__
 #define __DAOS_DRPC_TYPES_H__
 
-#include <daos/drpc.pb-c.h>
+#include <stddef.h>
+#include <stdint.h>
 
 /*
  * Using a packetsocket over the unix domain socket means that we receive
@@ -17,37 +18,6 @@
  * buffer to put all of the information in.
  */
 #define UNIXCOMM_MAXMSGSIZE (1 << 17)
-
-struct unixcomm {
-	int fd;    /** File descriptor of the unix domain socket */
-	int flags; /** Flags set on unix domain socket */
-};
-
-typedef void (*drpc_handler_t)(Drpc__Call *, Drpc__Response *);
-typedef int (*drpc_thread_yielder_t)(void);
-
-/**
- * dRPC connection context. This includes all details needed to communicate
- * on the dRPC channel.
- */
-struct drpc {
-	struct unixcomm      *comm;      /** unix domain socket communication context */
-	int                   sequence;  /** sequence number of latest message sent */
-	uint32_t              ref_count; /** open refs to this ctx */
-
-	/**
-	 * Handler for messages received by a listening drpc context.
-	 * For client contexts, this is NULL.
-	 */
-	drpc_handler_t        handler;
-
-	/**
-	 * Function to yield cycles to a non-dRPC thread, if applicable.
-	 */
-	drpc_thread_yielder_t yield;
-};
-
-enum rpcflags { R_SYNC = 1 };
 
 /**
  * drpc_header precedes every dRPC message chunk.
