2fdb50d5006360152f8345ff72b11cc0c4dc9e78
Dalton Bohning <dalton.bohning@hpe.com>
2025-08-13 22:37:03 +0000

    wip: cleanup container_rf_test_base.py
    Test-tag: ContRfEnforce RbldContRfTest
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@hpe.com>

--- src/tests/ftest/util/rebuild_test_base.py
+++ src/tests/ftest/util/rebuild_test_base.py
@@ -29,10 +29,7 @@ class RebuildTestBase(TestWithServers):
         """Initialize a RebuildTestBase object."""
         super().__init__(*args, **kwargs)
         self.inputs = RebuildTestParams()
-        self.targets = None
         self.server_count = 0
-        self.info_checks = None
-        self.rebuild_checks = None
 
     def setUp(self):
         """Set up each test case."""
@@ -42,62 +39,47 @@ class RebuildTestBase(TestWithServers):
         # Get the test parameters
         self.inputs.get_params(self)
 
-        # Get the number of targets per engine for pool info calculations
-        self.targets = self.server_managers[0].get_config_value("targets")
-
         self.server_count = len(self.hostlist_servers)
 
-    def setup_test_pool(self):
-        """Define a TestPool object."""
-        self.add_pool(create=False)
-
-    def setup_test_container(self):
-        """Define a TestContainer object."""
-        self.add_container(self.pool, create=False)
-
-    def setup_pool_verify(self):
-        """Set up pool verification initial expected values."""
-        self.info_checks = {
-            "pi_uuid": self.pool.uuid,
+    def verify_pool_info_before_rebuild(self, pool):
+        # Verify the pool information before rebuild
+        info_checks = {
+            "pi_uuid": pool.uuid,
             "pi_nnodes": self.server_count,
-            "pi_ntargets": (self.server_count * self.targets),
+            "pi_ntargets": (
+                self.server_count * self.server_managers[0].get_config_value("targets")),
             "pi_ndisabled": 0,
         }
-        self.rebuild_checks = {
+        rebuild_checks = {
             "rs_state": 1,
             "rs_obj_nr": 0,
             "rs_rec_nr": 0,
             "rs_errno": 0,
         }
+        status = pool.check_pool_info(**info_checks)
+        status &= pool.check_rebuild_status(**rebuild_checks)
+        if not status:
+            self.fail("Error confirming pool info before rebuild")
 
-    def update_pool_verify(self):
-        """Update the pool verification expected values."""
-        self.info_checks["pi_ndisabled"] = ">0"
-        self.rebuild_checks["rs_state"] = 2
-        self.rebuild_checks["rs_obj_nr"] = ">=0"
-        self.rebuild_checks["rs_rec_nr"] = ">=0"
-
-    def execute_pool_verify(self, msg=None):
-        """Verify the pool info.
-
-        Args:
-            msg (str, optional): additional information to include in the error
-                message. Defaults to None.
-        """
-        status = self.pool.check_pool_info(**self.info_checks)
-        status &= self.pool.check_rebuild_status(**self.rebuild_checks)
-        self.assertTrue(
-            status,
-            "Error confirming pool info{}".format("" if msg is None else msg))
-
-    def create_test_pool(self):
-        """Create the pool and verify its info."""
-        # Create a pool
-        self.pool.create()
-
-        # Verify the pool information before rebuild
-        self.setup_pool_verify()
-        self.execute_pool_verify(" before rebuild")
+    def verify_pool_info_after_rebuild(self, pool):
+        # Verify the pool information after rebuild
+        info_checks = {
+            "pi_uuid": pool.uuid,
+            "pi_nnodes": self.server_count,
+            "pi_ntargets": (
+                self.server_count * self.server_managers[0].get_config_value("targets")),
+            "pi_ndisabled": ">0",
+        }
+        rebuild_checks = {
+            "rs_state": 2,
+            "rs_obj_nr": ">=0",
+            "rs_rec_nr": ">=0",
+            "rs_errno": 0,
+        }
+        status = pool.check_pool_info(**info_checks)
+        status &= pool.check_rebuild_status(**rebuild_checks)
+        if not status:
+            self.fail("Error confirming pool info after rebuild")
 
     def create_test_container(self):
         """Create a container and write objects."""
@@ -138,17 +120,18 @@ class RebuildTestBase(TestWithServers):
     def execute_during_rebuild(self):
         """Execute test steps during rebuild."""
 
-    def verify_container_data(self, txn=None):
+    def verify_container_data(self, container, txn=None):
         """Verify the container data.
 
         Args:
+            container (TestContainer): container to verify
             txn (int, optional): transaction timestamp to read. Defaults to None
                 which uses the last timestamp written.
         """
-        if self.container is not None:
-            self.assertTrue(
-                self.container.read_objects(txn),
-                "Error verifying container data")
+        if container is None:
+            return
+        if not self.container.read_objects(txn):
+            self.fail("Error verifying container data")
 
     def execute_rebuild_test(self, create_container=True):
         """Execute the rebuild test steps.
@@ -158,12 +141,13 @@ class RebuildTestBase(TestWithServers):
                 container. Defaults to True.
         """
         # Get the test params
-        self.setup_test_pool()
+        self.add_pool(create=False)
         if create_container:
-            self.setup_test_container()
+            self.add_container(self.pool, create=False)
 
         # Create a pool and verify the pool information before rebuild
-        self.create_test_pool()
+        self.pool = self.get_pool()
+        self.verify_pool_info_before_rebuild(self.pool)
 
         # Create a container and write objects
         self.create_test_container()
@@ -191,10 +175,9 @@ class RebuildTestBase(TestWithServers):
         self.verify_rank_has_no_objects()
 
         # Verify the pool information after rebuild
-        self.update_pool_verify()
-        self.execute_pool_verify(" after rebuild")
+        self.verify_pool_info_after_rebuild(self.pool)
 
         # Verify the container data can still be accessed
-        self.verify_container_data()
+        self.verify_container_data(self.container)
 
         self.log.info("Test passed")
