7ae21a62c735576ae3d058c32fce56c8c45d02cc
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-08-25 15:51:36 +0200

    100 miliseconds instead of milliseconds
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
    
    Cancel-prev-build: false
    
    Skip-build-el9-gcc: true
    Skip-build-leap15-gcc: true
    Skip-build-leap15-icc: true
    Skip-build-ubuntu20-gcc: true
    
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    
    Allow-unstable-test: true
    
    Skip-func-test-el8: true
    Skip-fault-injection-test: true
    
    Skip-func-hw-test: true
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- src/gurt/dlog.c
+++ src/gurt/dlog.c
@@ -644,9 +644,9 @@ void d_vlog(int flags, const char *fmt, va_list ap)
 	if (mst.oflags & DLOG_FLV_YEAR)
 		hlen = snprintf(b, sizeof(b), "%04d/", tm->tm_year + 1900);
 
-	hlen += snprintf(b + hlen, sizeof(b) - hlen, "%02d/%02d-%02d:%02d:%02d.%6ld %s ",
+	hlen += snprintf(b + hlen, sizeof(b) - hlen, "%02d/%02d-%02d:%02d:%02d.%2ld %s ",
 			 tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec,
-			 (long int)tv.tv_usec, mst.uts.nodename);
+			 (long int)tv.tv_usec / 10000, mst.uts.nodename);
 
 	if (mst.oflags & DLOG_FLV_TAG) {
 		if (mst.oflags & DLOG_FLV_LOGPID) {
