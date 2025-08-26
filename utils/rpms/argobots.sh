6924d32805ef78c8532b0e0d706d0a7f4bc719db
Jeff Olivier <jeffolivier@google.com>
2025-08-26 10:07:03 -0600

    DAOS-17472 build: Enable new RPM builds (#16650)
    Restores PR #16457 and fix the issues with it.
    
    Only build dependency if same version doesn't exist
    isa-l build.config was out of sync with RPM builds
    Build appropriate -devel packages for backward compatibility
    Update isa-l to 2.31.1
    Update isa-l_crypto to v2.24.0
    Reduce what is done in dockerfile
    no need for recursive clone
    
    Skip-func-test-el9: false
    Skip-func-test-leap15: false
    
    Signed-off-by: Jeff Olivier <jeffolivier@google.com>
    Co-authored-by: Kris Jacque <kris.jacque@hpe.com>

--- utils/rpms/argobots.sh
+++ utils/rpms/argobots.sh
@@ -4,13 +4,13 @@ set -eEuo pipefail
 root="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 . "${root}/fpm_common.sh"
 
-if [ -z "${SL_ARGOBOTS_PREFIX}" ]; then
+if [ -z "${SL_ARGOBOTS_PREFIX:-}" ]; then
   echo "Argbots must be installed or never built"
   exit 0
 fi
 
 VERSION="${argobots_version}"
-RELEASE="2"
+RELEASE="${argobots_release}"
 LICENSE="UChicago Argonne, LLC -- Argobots License"
 DESCRIPTION="Argobots is a lightweight, low-level threading and tasking framework.
 This release is an experimental version of Argobots that contains
@@ -26,20 +26,18 @@ append_install_list "${files[@]}"
 ARCH="${isa}"
 build_package "${argobots_lib}"
 
-if [ "${BUILD_EXTRANEOUS:-no}" = "yes" ]; then
-  TARGET_PATH="${libdir}/pkgconfig"
-  list_files files "${SL_ARGOBOTS_PREFIX}/lib64/pkgconfig/*"
-  replace_paths "${SL_ARGOBOTS_PREFIX}" "${files[@]}"
-  append_install_list "${files[@]}"
+TARGET_PATH="${libdir}/pkgconfig"
+list_files files "${SL_ARGOBOTS_PREFIX}/lib64/pkgconfig/*"
+replace_paths "${SL_ARGOBOTS_PREFIX}" "${files[@]}"
+append_install_list "${files[@]}"
 
-  TARGET_PATH="${libdir}"
-  list_files files "${SL_ARGOBOTS_PREFIX}/lib64/libabt.so"
-  append_install_list "${files[@]}"
+TARGET_PATH="${libdir}"
+list_files files "${SL_ARGOBOTS_PREFIX}/lib64/libabt.so"
+append_install_list "${files[@]}"
 
-  TARGET_PATH="${includedir}"
-  list_files files "${SL_ARGOBOTS_PREFIX}/include/abt.h"
-  append_install_list "${files[@]}"
+TARGET_PATH="${includedir}"
+list_files files "${SL_ARGOBOTS_PREFIX}/include/abt.h"
+append_install_list "${files[@]}"
 
-  DEPENDS=("${argobots_lib} = ${argobots_version}-${RELEASE}")
-  build_package "${argobots_dev}"
-fi
+DEPENDS=("${argobots_lib} = ${argobots_full}")
+build_package "${argobots_dev}"
