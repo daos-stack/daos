9ad5edd734ec6fe5900d23dd62f419c6f214e389
Johann Lombardi <johann.lombardi@hpe.com>
2025-08-27 17:55:39 +0200

    Fix nit
    Signed-off-by: Johann Lombardi <johann.lombardi@hpe.com>

--- src/client/pydaos/torch/torch_shim.c
+++ src/client/pydaos/torch/torch_shim.c
@@ -116,10 +116,11 @@ static d_hash_table_ops_t dir_cache_hash_ops = {
 static void
 atfork_handler(void)
 {
-	int rc = daos_reinit();
+	int rc = daos_eq_lib_reset_after_fork()();
 	if (rc) {
-		D_ERROR("daos_reinit() failed in child process %s (rc=%d)", d_errstr(rc), rc);
+		D_ERROR("daos_eq_lib_reset_after_fork() failed in child process %s (rc=%d)", d_errstr(rc), rc);
 	}
+	daos_dti_reset();
 }
 
 static PyObject *
