cf98f3344d3ec5a957dc8be4a3ae1025eef2930d
ravalsam <samirkumar.raval@hpe.com>
2025-08-28 16:57:58 +0000

    Code modified based on Ci result.
    Adding print for debug purpose....
    
    Skip-unit-tests: true
    Quick-Functional: true
    Test-tag: test_config_generate_run
    
    Signed-off-by: ravalsam <samirkumar.raval@hpe.com>

--- src/tests/ftest/control/config_generate_run.py
+++ src/tests/ftest/control/config_generate_run.py
@@ -40,6 +40,7 @@ class ConfigGenerateRun(TestWithServers):
         :avocado: tags=control,dmg_config_generate
         :avocado: tags=ConfigGenerateRun,test_config_generate_run
         """
+        self.log_step(f"self.test_env.log_dir =  {self.test_env.log_dir}")
         num_engines = self.params.get("num_engines", "/run/config_generate_params/*/")
         scm_only = self.params.get("scm_only", "/run/config_generate_params/*/")
         net_class = self.params.get("net_class", "/run/config_generate_params/*/")
@@ -68,7 +69,10 @@ class ConfigGenerateRun(TestWithServers):
         # Iterate & update the log file path for each engine.
         engines = generated_yaml["engines"]
         for engine in engines:
-            engine["log_file"] = os.path.join(self.test_env.log_dir, engine["id"] + ".log")
+            engine["log_file"] = os.path.join(
+                self.test_env.log_dir, os.path.basename(engine["log_file"]))
+
+        self.log_step(f"Modified log_file in generated config {engines}")
 
         # Stop and restart daos_server. self.start_server_managers() has the
         # server start-up check built into it, so if there's something wrong,
