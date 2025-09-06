d4279620142ce07a6645ea17afcdc9c456a2f0b6
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-09-06 08:14:39 +0200

    Exception based approach in __lnext
    Cancel-prev-build: false
    
    Skip-build-el9-gcc: true
    Skip-build-leap15-gcc: true
    Skip-build-leap15-icc: true
    Skip-build-ubuntu20-gcc: true
    
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-NLT: false
    
    Allow-unstable-test: true
    
    Skip-func-test-el8: true
    Skip-fault-injection-test: false
    
    Skip-func-hw-test: true
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- src/tests/ftest/cart/util/cart_logparse.py
+++ src/tests/ftest/cart/util/cart_logparse.py
@@ -677,19 +677,10 @@ class LogIter():
             line = self._fd.readline()
             if not line:
                 raise StopIteration
-            fields = line.split(None, 9)
-            # assuming (mst.oflags & DLOG_FLV_YEAR) always true in src/gurt/dlog.c: 644
-            # pylint: disable=too-many-boolean-expressions
-            if (
-                len(fields) < 7
-                or len(fields[1]) != 10 or fields[1][4] != '/'
-                or len(fields[2]) != 15 or fields[2][2] != ':' or fields[2][8] != '.'
-                or len(fields[0]) > 4 or ".go:" in fields[3]
-            ):
-                # pylint: enable=too-many-boolean-expressions
+            try:
+                return LogLine(line)
+            except InvalidLogLine:
                 return LogRaw(line)
-            return LogLine(line)
-
         try:
             line = self._data[self._offset]
         except IndexError as error:
