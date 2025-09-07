4d547e0999f1640d9bccad4d16061861a58d4928
WangYunPeng <ywang@panasas.com>
2025-09-07 23:11:04 +0800

    DAOS-17308 ddb: move pool management bits into a separate module. (cherry-pick from Janekmi)

---
+++ src/utils/ddb/ddb_mgmt.h
@@ -0,0 +1,29 @@
+/**
+ * (C) Copyright 2025 Vdura Inc.
+ * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+ *
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ */
+
+#ifndef __DDB_MGMT_H__
+#define __DDB_MGMT_H__
+
+int
+ddb_auto_calculate_scm_mount_size(unsigned int *scm_mount_size);
+
+int
+ddb_recreate_pooltgts(const char *scm_mount);
+
+int
+ddb_clear_dir(const char *dir);
+
+int
+ddb_tmpfs_pre_mount_check(const char *path, unsigned int size_limit, bool *need_mount);
+
+int
+ddb_dirs_prepare(const char *scm_mount);
+
+int
+ddb_mount(const char *scm_mount, unsigned int scm_mount_size);
+
+#endif /** __DDB_MGMT_H__ */
