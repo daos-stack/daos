be250c07c43d8d4c0751e355cb90d48b803f262e
Joseph Moore <26410038+jgmoore-or@users.noreply.github.com>
2025-09-12 11:53:54 -0600

    DAOS-16947 test: Update CI to use DOCA OFED. (#16634)
    PR changes the Mellanox version in the provisioning scripts to specify the current DOCA OFED version: 3.0.0. This version of the DOCA OFED installs UCX 1.19. Later versions of UCX require an increase in Argobots ULT stack size, and this PR updates the control plane code to set this for engine executions utilizing the UCX provider.
    
    Signed-off-by: Joseph Moore <joseph.moore@hpe.com>

--- utils/scripts/install-el8.sh
+++ utils/scripts/install-el8.sh
@@ -63,7 +63,6 @@ dnf --nodocs install ${dnf_install_args} \
     ndctl-devel \
     numactl \
     numactl-devel \
-    openmpi-devel \
     openssl-devel \
     pandoc \
     patch \
@@ -81,6 +80,12 @@ dnf --nodocs install ${dnf_install_args} \
     valgrind-devel \
     which \
     yasm
+
+if [[ -z "${NO_OPENMPI_DEVEL+set}" ]]; then
+    # shellcheck disable=SC2086
+    dnf --nodocs install ${dnf_install_args} \
+    	openmpi-devel 
+fi
 
 ruby_version=$(dnf module list ruby | grep -Eow "3\.[0-9]+" | tail -1)
 # shellcheck disable=SC2086
