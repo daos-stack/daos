4adde28cec7b585726b17672d0ce0ac4687eb1cd
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-09-17 09:20:49 +0200

    SRE-2772 ci: remove all references to hpdd.intel.com
    Also enable Jenkins githook as Jenkins is back operational.
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- ci/provisioning/post_provision_config_common.sh
+++ ci/provisioning/post_provision_config_common.sh
@@ -26,7 +26,7 @@ if [ -n "$repo_files_pr" ]; then
         branch="${branch%:*}"
     fi
     # shellcheck disable=SC2034
-    REPO_FILE_URL="${JENKINS_URL:-https://build.hpdd.intel.com/}job/daos-do/job/repo-files/job/$branch/$build_number/artifact/"
+    REPO_FILE_URL="${JENKINS_URL:-https://jenkins.daos.hpc.amslabs.hpecorp.net/}job/daos-do/job/repo-files/job/$branch/$build_number/artifact/"
 fi
 
 # shellcheck disable=SC1091
