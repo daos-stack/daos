590a8535723cc1189999192568e09b841ec8e1f5
Dalton Bohning <dalton.bohning@hpe.com>
2025-09-19 15:47:40 +0000

    DAOS-17996 test: enhance test_dmg_system_reformat
    - Verify dmg system erase actually removes metadata
    - Increase to 3 servers
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@hpe.com>

--- src/tests/ftest/control/dmg_system_reformat.py
+++ src/tests/ftest/control/dmg_system_reformat.py
@@ -1,14 +1,16 @@
 """
   (C) Copyright 2020-2024 Intel Corporation.
+  (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
+import os
 import time
 
 from apricot import TestWithServers
 from avocado.core.exceptions import TestFail
 from exception_utils import CommandFailure
-from general_utils import journalctl_time
+from general_utils import check_file_exists, journalctl_time
 from test_utils_pool import add_pool, get_size_params
 
 
@@ -40,57 +42,68 @@ class DmgSystemReformatTest(TestWithServers):
         """
         dmg = self.get_dmg_command().copy()
 
-        # Create pool using 90% of the available NVMe capacity
-        pools = [add_pool(self, dmg=dmg)]
+        self.log_step("Create pool using 90% of the available NVMe capacity")
+        pool = add_pool(self, dmg=dmg)
 
-        self.log.info("Check that new pool will fail with DER_NOSPACE")
-        dmg.exit_status_exception = False
-        pools.append(add_pool(self, create=False, **get_size_params(pools[0])))
-        try:
-            pools[-1].create()
-        except TestFail as error:
-            self.log.info("Pool create failed: %s", str(error))
-            if "-1007" not in str(error):
-                self.fail("Pool create did not fail due to DER_NOSPACE!")
-        dmg.exit_status_exception = True
+        self.log_step("Check that new pool will fail with DER_NOSPACE")
+        with dmg.no_exception():
+            pool2 = add_pool(self, create=False, **get_size_params(pool))
+            try:
+                pool2.create()
+                self.fail("Pool create was expected to fail with DER_NOSPACE!")
+            except TestFail as error:
+                self.log.info("Pool create failed: %s", str(error))
+                if "-1007" not in str(error):
+                    self.fail("Pool create did not fail due to DER_NOSPACE!")
 
-        self.log.info("Stop running engine instances: 'dmg system stop'")
+        self.log_step("Stop running engine instances: 'dmg system stop'")
         dmg.system_stop(force=True)
-        if dmg.result.exit_status != 0:
-            self.fail("Detected issues performing a system stop: {}".format(
-                dmg.result.stderr_text))
 
-        # Remove pools and disable removing pools that about to be removed by formatting
-        for pool in pools:
-            pool.skip_cleanup()
-        pools = []
+        self.log_step("Verify expected metadata exists before erase")
+        # Check this upfront so later when we check it is gone we know the erase worked
+        scm_mount = self.server_managers[0].get_config_value("scm_mount")
+        superblock_path = os.path.join(scm_mount, "superblock")
+        control_metadata_path = self.server_managers[0].manager.job.control_metadata.path.value
+        _, missing_hosts = check_file_exists(self.hostlist_servers, superblock_path, sudo=True)
+        if missing_hosts:
+            self.fail(f"superblock does not exist before erase: {superblock_path}")
+        if control_metadata_path:
+            self.log.info("control_metadata_path = '%s'", control_metadata_path)
+            _, missing_hosts = check_file_exists(
+                self.hostlist_servers, control_metadata_path, sudo=True)
+            if missing_hosts:
+                self.fail(f"control metadata does not exist before erase: {control_metadata_path}")
 
         # Perform a dmg system erase to allow the dmg storage format to succeed
-        self.log.info("Perform dmg system erase on all system ranks:")
+        self.log_step("Perform dmg system erase on all system ranks:")
         dmg.system_erase()
-        if dmg.result.exit_status != 0:
-            self.fail("Issues performing system erase: {}".format(
-                dmg.result.stderr_text))
 
-        self.log.info("Perform dmg storage format on all system ranks:")
+        # Disable pool cleanup in teardown since this pool was erased
+        pool.skip_cleanup()
+        pool = None
 
+        self.log_step("Verify metadata is erased")
+        _, missing_hosts = check_file_exists(self.hostlist_servers, superblock_path, sudo=True)
+        if missing_hosts != self.hostlist_servers:
+            self.fail(f"superblock still exists after erase: {superblock_path}")
+        if control_metadata_path:
+            _, missing_hosts = check_file_exists(
+                self.hostlist_servers, control_metadata_path, sudo=True)
+            if missing_hosts != self.hostlist_servers:
+                self.fail(f"control metadata still exists after erase: {control_metadata_path}")
+
+        self.log_step("Perform dmg storage format on all system ranks:")
         # Calling storage format after system stop too soon would fail, so
         # wait 10 sec and retry up to 4 times.
-        count = 0
-        while count < 4:
+        for _ in range(4):
             try:
                 dmg.storage_format(force=True)
-                if dmg.result.exit_status != 0:
-                    self.fail(
-                        "Issues performing storage format --force: {}".format(
-                            dmg.result.stderr_text))
                 break
             except CommandFailure as error:
                 self.log.info("Storage format failed. Wait 10 sec and retry. %s", error)
-                count += 1
                 time.sleep(10)
 
-        # Check that engine starts up again
+        self.log_step("Verify engines start again")
         self.log.info("<SERVER> Waiting for the engines to start")
         self.server_managers[-1].manager.timestamps["start"] = journalctl_time()
         self.server_managers[-1].detect_engine_start()
@@ -101,9 +114,8 @@ class DmgSystemReformatTest(TestWithServers):
                 dmg.result.stdout_text))
 
         # Create last pool now that memory has been wiped.
-        pools.append(add_pool(self, connect=False, dmg=dmg))
+        pool = add_pool(self, connect=False, dmg=dmg)
 
         # Lastly, verify that last created pool is in the list
         pool_uuids = dmg.get_pool_list_uuids()
-        self.assertEqual(
-            pool_uuids[0].lower(), pools[-1].uuid.lower(), "{} missing from list".format(pools[-1]))
+        self.assertEqual(pool_uuids[0].lower(), pool.uuid.lower(), f"{pool} missing from list")
