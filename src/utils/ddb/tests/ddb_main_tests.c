40b81bbac3afebbd1542172cf0bbcdc3f5aa9cba
WangYunPeng <ywang@panasas.com>
2025-09-26 23:37:12 +0800

    DAOS-17308: fix copyright

--- src/utils/ddb/tests/ddb_main_tests.c
+++ src/utils/ddb/tests/ddb_main_tests.c
@@ -1,6 +1,6 @@
 /**
  * (C) Copyright 2022-2024 Intel Corporation.
- * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+ * (C) Copyright 2025 Vdura Inc.
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
