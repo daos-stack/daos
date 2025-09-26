8de73c1869e9f5c44084230219f208d3528d8d97
Ding-Hwa Ho <ding-hwa.ho@hpe.com>
2025-09-26 12:46:24 -0400

    Update to RSAKey
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    Signed-off-by: Ding-Hwa Ho <ding-hwa.ho@hpe.com>

--- src/tests/ftest/dfuse/sparse_file.py
+++ src/tests/ftest/dfuse/sparse_file.py
@@ -73,7 +73,7 @@ class SparseFile(IorTestBase):
         ssh.load_system_host_keys()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         pkey_file = os.path.expanduser(os.path.join('~', '.ssh', 'id_rsa'))
-        pkey = paramiko.PKey.from_private_key_file(pkey_file)
+        pkey = paramiko.RSAKey.from_private_key_file(pkey_file)
         ssh.connect(self.hostlist_clients[0], username=getuser(), pkey=pkey)
         sftp = ssh.open_sftp()
 
