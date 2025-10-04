ee8aa4966e10ed41af16c5667fd2433ed79380c6
Mohamad Chaarawi <mohamad.chaarawi@hpe.com>
2025-10-04 09:08:49 -0500

    DAOS-18015 dfuse: do not force foreground if run with MPI (#16941)
    If using MPI to launch dfuse, we should not force running in the
    foreground as that introduces several limitations. MPI like clush should
    be able to launch and daemonize dfuse.
    
    Signed-off-by: Mohamad Chaarawi <mohamad.chaarawi@hpe.com>

--- src/client/dfuse/dfuse_main.c
+++ src/client/dfuse/dfuse_main.c
@@ -571,12 +571,6 @@ main(int argc, char **argv)
 		}
 	}
 
-	if (!dfuse_info->di_foreground && d_isenv_def("PMIX_RANK")) {
-		DFUSE_TRA_WARNING(dfuse_info,
-				  "Not running in background under orterun");
-		dfuse_info->di_foreground = true;
-	}
-
 	if (!dfuse_info->di_mountpoint) {
 		printf("Mountpoint is required\n");
 		show_help(argv[0]);
