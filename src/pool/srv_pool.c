daos-stack/daos

commit 5dbf5a038d01af5200e1cfdc833e6a0107e41591

DAOS-17306 rsvc: Add state DS_RSVC_STEPPING_UP (#16700)
So that a ds_rsvc is stoppable even when the sc_step_up implementation
blocks outside of rsvc or rdb. (See how this patch releases and
re-acquires s_mutex before and after calling sc_step_up.)

For consistency, rename DS_RSVC_DRAINING to DS_RSVC_STEPPING_DOWN.

Also, avoid printing CRT_NO_RANKs in raft log messages, which
occasionally confuse people.

Signed-off-by: Li Wei <liwei@hpe.com>

src/pool/srv_pool.c
@@ -3903,10 +3903,12 @@ out_tx:
 		 */
 		D_DEBUG(DB_MD, DF_UUID": trying to finish stepping up\n",
 			DP_UUID(in->pri_op.pi_uuid));
+		ABT_mutex_unlock(svc->ps_rsvc.s_mutex);
 		if (DAOS_FAIL_CHECK(DAOS_POOL_CREATE_FAIL_STEP_UP))
 			rc = -DER_GRPVER;
 		else
 			rc = pool_svc_step_up_cb(&svc->ps_rsvc);
+		ABT_mutex_lock(svc->ps_rsvc.s_mutex);
 		if (rc != 0) {
 			D_ASSERT(rc != DER_UNINIT);
 			rdb_resign(svc->ps_rsvc.s_db, svc->ps_rsvc.s_term);
@@ -6864,7 +6866,7 @@ pool_svc_schedule(struct pool_svc *svc, struct pool_svc_sched *sched, void (*fun
 	 * and has already called sched_cancel_and_wait.
 	 */
 	state = ds_rsvc_get_state(&svc->ps_rsvc);
-	if (state == DS_RSVC_DRAINING) {
+	if (state == DS_RSVC_STEPPING_DOWN) {
 		D_DEBUG(DB_MD, DF_UUID": end: service %s\n", DP_UUID(svc->ps_uuid),
 			ds_rsvc_state_str(state));
 		return -DER_OP_CANCELED;

src/include/daos_srv/rsvc.h
@@ -1,5 +1,6 @@
 /*
  * (C) Copyright 2019-2024 Intel Corporation.
+ * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -87,10 +88,11 @@ void ds_rsvc_class_unregister(enum ds_rsvc_class_id id);
 
 /** Replicated service state in ds_rsvc.s_term */
 enum ds_rsvc_state {
-	DS_RSVC_UP_EMPTY,	/**< up but DB newly-created and empty */
-	DS_RSVC_UP,		/**< up and ready to serve */
-	DS_RSVC_DRAINING,	/**< stepping down */
-	DS_RSVC_DOWN		/**< down */
+	DS_RSVC_STEPPING_UP,   /**< stepping up */
+	DS_RSVC_UP_EMPTY,      /**< up but DB newly-created and empty */
+	DS_RSVC_UP,            /**< up and ready to serve */
+	DS_RSVC_STEPPING_DOWN, /**< stepping down */
+	DS_RSVC_DOWN           /**< down */
 };
 
 char *ds_rsvc_state_str(enum ds_rsvc_state state);

src/rdb/rdb_raft.c
@@ -1,5 +1,6 @@
 /*
  * (C) Copyright 2017-2024 Intel Corporation.
+ * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -1470,17 +1471,18 @@ static void
 rdb_raft_cb_log(raft_server_t *raft, raft_node_t *node, void *arg, raft_loglevel_e level,
 		const char *buf)
 {
-	struct rdb *db = raft_get_udata(raft);
-	d_rank_t    rank;
+#define RRCL_LOG(flag)                                                                             \
+	if (node == NULL)                                                                          \
+		D_DEBUG(flag, DF_DB ": %s\n", DP_DB(db), buf);                                     \
+	else                                                                                       \
+		D_DEBUG(flag, DF_DB ": %s: rank=%u\n", DP_DB(db), buf,                             \
+			((struct rdb_raft_node *)raft_node_get_udata(node))->dn_rank);
 
-	if (node == NULL)
-		rank = CRT_NO_RANK;
-	else
-		rank = ((struct rdb_raft_node *)raft_node_get_udata(node))->dn_rank;
+	struct rdb *db = raft_get_udata(raft);
 
 	switch (level) {
 	case RAFT_LOG_ERROR:
-		D_ERROR(DF_DB ": %s: rank=%u\n", DP_DB(db), buf, rank);
+		RRCL_LOG(DLOG_ERR);
 		break;
 	case RAFT_LOG_INFO:
 		/*
@@ -1490,11 +1492,13 @@ rdb_raft_cb_log(raft_server_t *raft, raft_node_t *node, void *arg, raft_loglevel
 		 * few election messages, which might attract complaints if done
 		 * with D_INFO.
 		 */
-		D_DEBUG(DB_MD, DF_DB ": %s: rank=%u\n", DP_DB(db), buf, rank);
+		RRCL_LOG(DB_MD);
 		break;
 	default:
-		D_DEBUG(DB_IO, DF_DB ": %s: rank=%u\n", DP_DB(db), buf, rank);
+		RRCL_LOG(DB_IO);
 	}
+
+#undef RRCL_LOG
 }
 
 static raft_time_t

src/rsvc/srv.c
@@ -47,12 +47,14 @@ char *
 ds_rsvc_state_str(enum ds_rsvc_state state)
 {
 	switch (state) {
+	case DS_RSVC_STEPPING_UP:
+		return "STEPPING_UP";
 	case DS_RSVC_UP_EMPTY:
 		return "UP_EMPTY";
 	case DS_RSVC_UP:
 		return "UP";
-	case DS_RSVC_DRAINING:
-		return "DRAINING";
+	case DS_RSVC_STEPPING_DOWN:
+		return "STEPPING_DOWN";
 	case DS_RSVC_DOWN:
 		return "DOWN";
 	default:
@@ -457,6 +459,7 @@ rsvc_step_up_cb(struct rdb *db, uint64_t term, void *arg)
 	}
 	D_ASSERTF(svc->s_state == DS_RSVC_DOWN, "%d\n", svc->s_state);
 	svc->s_term = term;
+	change_state(svc, DS_RSVC_STEPPING_UP);
 	D_DEBUG(DB_MD, "%s: stepping up to "DF_U64"\n", svc->s_name,
 		svc->s_term);
 
@@ -467,7 +470,9 @@ rsvc_step_up_cb(struct rdb *db, uint64_t term, void *arg)
 		map_distd_initialized = true;
 	}
 
+	ABT_mutex_unlock(svc->s_mutex);
 	rc = rsvc_class(svc->s_class)->sc_step_up(svc);
+	ABT_mutex_lock(svc->s_mutex);
 	if (rc == DER_UNINIT) {
 		change_state(svc, DS_RSVC_UP_EMPTY);
 		rc = 0;
@@ -486,6 +491,7 @@ rsvc_step_up_cb(struct rdb *db, uint64_t term, void *arg)
 		 */
 		if (rc == -DER_DF_INCOMPT)
 			rc = -DER_SHUTDOWN;
+		change_state(svc, DS_RSVC_DOWN);
 		goto out_mutex;
 	}
 
@@ -507,10 +513,10 @@ bootstrap_self(struct ds_rsvc *svc, void *arg)
 	ABT_mutex_lock(svc->s_mutex);
 
 	/*
-	 * This single-replica DB shall change from DS_RSVC_DOWN to
-	 * DS_RSVC_UP_EMPTY state promptly.
+	 * This single-replica DB shall change from DS_RSVC_DOWN via
+	 * DS_RSVC_STEPPING_UP to DS_RSVC_UP_EMPTY state promptly.
 	 */
-	while (svc->s_state == DS_RSVC_DOWN)
+	while (svc->s_state == DS_RSVC_DOWN || svc->s_state == DS_RSVC_STEPPING_UP)
 		ABT_cond_wait(svc->s_state_cv, svc->s_mutex);
 	D_ASSERTF(svc->s_state == DS_RSVC_UP_EMPTY, "%d\n", svc->s_state);
 
@@ -521,7 +527,9 @@ bootstrap_self(struct ds_rsvc *svc, void *arg)
 
 	/* Try stepping up again. */
 	D_DEBUG(DB_MD, "%s: calling sc_step_up\n", svc->s_name);
+	ABT_mutex_unlock(svc->s_mutex);
 	rc = rsvc_class(svc->s_class)->sc_step_up(svc);
+	ABT_mutex_lock(svc->s_mutex);
 	if (rc != 0) {
 		D_ASSERT(rc != DER_UNINIT);
 		goto out_mutex;
@@ -547,7 +555,7 @@ rsvc_step_down_cb(struct rdb *db, uint64_t term, void *arg)
 	D_ASSERT(entry_state == DS_RSVC_UP_EMPTY || entry_state == DS_RSVC_UP);
 
 	/* Stop accepting new leader references (ds_rsvc_lookup_leader). */
-	change_state(svc, DS_RSVC_DRAINING);
+	change_state(svc, DS_RSVC_STEPPING_DOWN);
 
 	if (rsvc_class(svc->s_class)->sc_map_dist != NULL)
 		drain_map_distd(svc);
@@ -987,13 +995,15 @@ stop(struct ds_rsvc *svc, bool destroy)
 	svc->s_stop = true;
 	D_DEBUG(DB_MD, "%s: stopping\n", svc->s_name);
 
-	if (svc->s_state == DS_RSVC_UP || svc->s_state == DS_RSVC_UP_EMPTY)
+	if (svc->s_state == DS_RSVC_STEPPING_UP || svc->s_state == DS_RSVC_UP_EMPTY ||
+	    svc->s_state == DS_RSVC_UP)
 		/*
-		 * The service has stepped up. If it is still the leader of
-		 * svc->s_term, the following rdb_resign() call will trigger
-		 * the matching rsvc_step_down_cb() callback in svc->s_term;
-		 * otherwise, the callback must already be pending. Either way,
-		 * the service shall eventually enter the DS_RSVC_DOWN state.
+		 * The service is stepping up or has stepped up. If it is still
+		 * the leader of svc->s_term, the following rdb_resign() call
+		 * will trigger the matching rsvc_step_down_cb() callback in
+		 * svc->s_term; otherwise, the callback must already be pending.
+		 * Either way, the service shall eventually enter the
+		 * DS_RSVC_DOWN state.
 		 */
 		rdb_resign(svc->s_db, svc->s_term);
 	while (svc->s_state != DS_RSVC_DOWN)

commit 8e17e2743c83658e6e0808a40f61fc84e932984d

DAOS-18010 pool: A quick fix for slow pool create
It has been observed that a dmg pool create command took about 5 minutes
to complete (successfully) because internally the first attempt timed
out after the following race.

  rsvc_step_up_cb         ds_pool_create_handler
  -------------------------------------------------------------------
  DOWN -> STEPPING_UP
  Release mutex
  Call pool_svc_step_up   Acquire mutex
                          Initialize pool metadata
                          If UP_EMPTY:             // no, STEPPING_UP
                            Call pool_svc_step_up  // skipped
                            UP_EMPTY -> UP         // skipped
                          Release mutex
  Acquire mutex
  STEPPING_UP -> UP_EMPTY

In other words, this looks like a regression introduced by #16700. The
quick fix is to serialize the two functions by checking the rsvc state
in ds_pool_create_handler, and only start initializing pool metadata
when the rsvc state is UP_EMPTY.

Test-tag: pr test_create_pool_quantity test_container_telemetry
Signed-off-by: Li Wei <liwei@hpe.com>

src/pool/srv_pool.c
@@ -3968,10 +3968,6 @@ ds_pool_create_handler(crt_rpc_t *rpc)
 	if (rc != 0)
 		D_GOTO(out, rc);
 
-	/*
-	 * Simply serialize this whole RPC with rsvc_step_{up,down}_cb() and
-	 * ds_rsvc_stop().
-	 */
 	ABT_mutex_lock(svc->ps_rsvc.s_mutex);
 
 	if (svc->ps_rsvc.s_stop) {
@@ -3988,6 +3984,17 @@ ds_pool_create_handler(crt_rpc_t *rpc)
 		 */
 		rc = rdb_campaign(svc->ps_rsvc.s_db);
 		D_DEBUG(DB_MD, DF_UUID": campaign: "DF_RC"\n", DP_UUID(svc->ps_uuid), DP_RC(rc));
+	}
+
+	if (ds_rsvc_get_state(&svc->ps_rsvc) == DS_RSVC_UP) {
+		D_DEBUG(DB_MD, DF_UUID ": already UP\n", DP_UUID(svc->ps_uuid));
+		rc = 0;
+		goto out_mutex;
+	} else if (ds_rsvc_get_state(&svc->ps_rsvc) != DS_RSVC_UP_EMPTY) {
+		D_DEBUG(DB_MD, DF_UUID ": not yet UP_EMPTY: state=%s\n", DP_UUID(svc->ps_uuid),
+			ds_rsvc_state_str(ds_rsvc_get_state(&svc->ps_rsvc)));
+		rc = -DER_NOTLEADER;
+		goto out_mutex;
 	}
 
 	rc = rdb_tx_begin(svc->ps_rsvc.s_db, RDB_NIL_TERM, &tx);