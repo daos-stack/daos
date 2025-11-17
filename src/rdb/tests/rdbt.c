aa67668ec4af97fd5d260d9ca38cfa7281693277
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-11-17 21:48:48 +0100

    Fix: clang format fix
    Cancel-prev-build: false
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- src/rdb/tests/rdbt.c
+++ src/rdb/tests/rdbt.c
@@ -1,6 +1,6 @@
 /**
  * (C) Copyright 2017-2022 Intel Corporation.
- * Copyright 2025 Hewlett Packard Enterprise Development LP
+ * Copyright 2025-2026 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -403,8 +403,8 @@ rdbt_add_replica_rank(crt_group_t *grp, d_rank_t ldr_rank, d_rank_t new_rank,
 	rc = out->rtmo_rc;
 	*hintp = out->rtmo_hint;
 	if (out->rtmo_failed != NULL)
-		fprintf(stderr, "ERROR: adding replica %u (reply rank %u)\n",
-				new_rank, out->rtmo_failed->rl_ranks[0]);
+		fprintf(stderr, "ERROR: adding replica %u (reply rank %u)\n", new_rank,
+			out->rtmo_failed->rl_ranks[0]);
 	d_rank_list_free(replicas_to_add);
 	destroy_rpc(rpc);
 	return rc;
@@ -432,8 +432,8 @@ rdbt_remove_replica_rank(crt_group_t *group, d_rank_t ldr_rank,
 	rc = out->rtmo_rc;
 	*hintp = out->rtmo_hint;
 	if (out->rtmo_failed != NULL)
-		fprintf(stderr, "ERROR: removing replica %u (reply rank %u)\n",
-				rem_rank, out->rtmo_failed->rl_ranks[0]);
+		fprintf(stderr, "ERROR: removing replica %u (reply rank %u)\n", rem_rank,
+			out->rtmo_failed->rl_ranks[0]);
 	d_rank_list_free(replicas_to_remove);
 	destroy_rpc(rpc);
 	return rc;
