bc4fb8e68cfb5494b79851c2882d710f7cfa6b3c
Makito Kano <makito.kano@hpe.com>
2025-12-03 05:15:35 +0000

    DAOS-18091 test: Initial fix of test_lost_majority_ps_replicas
    The test should remove rdb-pool from two out of three service
    replica ranks. The service replica ranks could change across
    pool creates. However, the test was removing it from first
    mount point of each rank. Fix it so that it selects the right
    mount point and pool path.
    
    The way to obtain the mount point isn't clear yet, so that
    part is hard-coded.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-func-hw-test-medium: false
    Test-tag: PoolMembershipTest PoolListConsolidationTest DMGCheckStartOptionsTest
    Signed-off-by: Makito Kano <makito.kano@hpe.com>

--- src/tests/ftest/recovery/pool_list_consolidation.py
+++ src/tests/ftest/recovery/pool_list_consolidation.py
@@ -265,12 +265,14 @@ class PoolListConsolidationTest(TestWithServers):
     def test_lost_majority_ps_replicas(self):
         """Test lost the majority of PS replicas.
 
-        1. Create a pool with --nsvc=3. Rank 0, 1, and 2 will be pool service replicas.
-        2. Stop servers.
-        3. Remove <scm_mount>/<pool_uuid>/rdb-pool from rank 0 and 2.
-        4. Run DAOS checker under kinds of mode.
-        5. Try creating a container. The pool can be started now, so create should succeed.
-        6. Show that rdb-pool are recovered. i.e., at least three out of four ranks
+        1. Create a pool with --nsvc=3.
+        2. Determine the host with two service ranks.
+        3. Stop servers.
+        4. Remove rdb-pool from the host determined in step 2. Remove from both mount
+        points.
+        5. Run DAOS checker under kinds of mode.
+        6. Try creating a container. The pool can be started now, so create should succeed.
+        7. Show that rdb-pool are recovered. i.e., at least three out of four ranks
         should have rdb-pool.
 
         Jira ID: DAOS-12029
@@ -295,29 +297,70 @@ class PoolListConsolidationTest(TestWithServers):
         self.log_step("Create a pool with --nsvc=3.")
         pool = self.get_pool(connect=False, svcn=3)
 
+        # vos_path = self.server_managers[0].get_vos_path(pool)
+        # self.log.debug(f"## vos_path = {vos_path}")
+
+        self.log.debug("Determine the host with two service ranks.")
+        # Each rank corresponds to a mount point of a host. However, the mapping could
+        # change across server restarts. For example, following configuration could be
+        # possible.
+        # hsw-100:/mnt/daos0/<pool_uuid> -> Rank 0
+        # hsw-100:/mnt/daos1/<pool_uuid> -> Rank 2
+        # hsw-101:/mnt/daos0/<pool_uuid> -> Rank 1
+        # hsw-101:/mnt/daos1/<pool_uuid> -> Rank 3
+        # The goal is to determine the host_name:mount_point from a given service replica
+        # rank. e.g., If service replicas are [0, 1, 2], we need to remove rdb-pool from
+        # two out of the top three rows above. We can easily get the rank to host mapping
+        # (self.server_managers[0].get_host(rank)). However, there's no easy way to
+        # determine which mount point the rank is mapped to (daos0 or daos1). Thus, we'll
+        # use the following algorithm assuming that this test uses two dual-engine hosts.
+        # 1. Determine the non-service replica rank.
+        all_rank_set = set(range(0, 4))
+        svc_rank_set = set(pool.svc_ranks)
+        non_svc_rank_set = all_rank_set - svc_rank_set
+        self.log.debug(f"## non_svc_rank_set = {non_svc_rank_set}")
+        non_svc_rank = non_svc_rank_set.pop()
+        self.log.debug(f"## non_svc_rank = {non_svc_rank}")
+        # 2. Get the host of the non_svc_rank. We will not remove rdb-pool from it, so
+        # call it non_remove_host.
+        rank_to_host = self.server_managers[0].ranks
+        non_remove_host = rank_to_host[non_svc_rank]
+        # 3. Get the other host of non_remove_host.
+        remove_host = None
+        for rank in range(0, 4):
+            host = rank_to_host[rank]
+            if host != non_remove_host:
+                remove_host = host
+                break
+        self.log.debug(f"## remove_host = {remove_host}")
+
         self.log_step("Stop servers")
         dmg_command = self.get_dmg_command()
         dmg_command.system_stop()
 
-        self.log_step("Remove <scm_mount>/<pool_uuid>/rdb-pool from two ranks.")
-        rdb_pool_path = f"{self.server_managers[0].get_vos_path(pool)}/rdb-pool"
-        command = f"sudo rm {rdb_pool_path}"
-        hosts = list(set(self.server_managers[0].ranks.values()))
-        count = 0
-        for host in hosts:
-            node = NodeSet(host)
-            check_out = check_file_exists(hosts=node, filename=rdb_pool_path, sudo=True)
-            if check_out[0]:
-                if not run_remote(log=self.log, hosts=node, command=command).passed:
-                    self.fail(f'Failed to remove {rdb_pool_path} on {host}')
-                self.log.info("rm rdb-pool from %s", str(node))
-                count += 1
-                if count > 1:
-                    break
-        if count < len(hosts):
-            msg = (f"Unexpected number of removals! Expected = {len(hosts)}; "
-                   f"Actual = {count}")
-            self.fail(msg)
+        msg = ("Remove rdb-pool from the host determined in step 2. Remove from both "
+                "mount points.")
+        self.log_step(msg)
+        # vos_path = self.server_managers[0].get_vos_path(pool)
+        # self.log.debug(f"## vos_path = {vos_path}")
+        # self.log_step("Remove <scm_mount>/<pool_uuid>/rdb-pool from two ranks.")
+        self.log_step("Remove /mnt/daos[0,1]/<pool_uuid>/rdb-pool from two ranks.")
+        # rdb_pool_path = f"{self.server_managers[0].get_vos_path(pool)}/rdb-pool"
+        rdb_pool_path_0 = f"/mnt/daos0/{pool.uuid.lower()}/rdb-pool"
+        rdb_pool_path_1 = f"/mnt/daos1/{pool.uuid.lower()}/rdb-pool"
+        command = f"sudo rm {rdb_pool_path_0}; sudo rm {rdb_pool_path_1}"
+        remove_node = NodeSet(remove_host)
+        check_out_0 = check_file_exists(
+            hosts=remove_node, filename=rdb_pool_path_0, sudo=True)
+        check_out_1 = check_file_exists(
+            hosts=remove_node, filename=rdb_pool_path_1, sudo=True)
+        if check_out_0[0] and check_out_1[0]:
+            if not run_remote(log=self.log, hosts=remove_node, command=command).passed:
+                msg = (f"Failed to remove {rdb_pool_path_0} and {rdb_pool_path_1} on "
+                       f"{remove_node}")
+                self.fail(msg)
+            msg = f"{rdb_pool_path_0} and {rdb_pool_path_1} removed from {remove_node}"
+            self.log.info(msg)
 
         self.log_step("Run DAOS checker under kinds of mode.")
         errors = []
@@ -344,14 +387,17 @@ class PoolListConsolidationTest(TestWithServers):
         self.log_step(msg)
         hosts = list(set(self.server_managers[0].ranks.values()))
         count = 0
+        rdb_pool_path_list = [rdb_pool_path_0, rdb_pool_path_1]
         for host in hosts:
             node = NodeSet(host)
-            check_out = check_file_exists(hosts=node, filename=rdb_pool_path, sudo=True)
-            if check_out[0]:
-                count += 1
-                self.log.info("rdb-pool found at %s", str(node))
+            for rdb_pool_path in rdb_pool_path_list:
+                check_out = check_file_exists(
+                    hosts=node, filename=rdb_pool_path, sudo=True)
+                if check_out[0]:
+                    count += 1
+                    self.log.info("## %s found at %s", rdb_pool_path, str(node))
 
-        self.log.info("rdb-pool count = %d", count)
+        self.log.info("## rdb-pool count = %d", count)
         if count < len(hosts) - 1:
             errors.append(f"Not enough rdb-pool has been recovered! - {count} ranks")
 
