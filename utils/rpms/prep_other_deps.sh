884fcd03e8fde301ac8b3cc3298aa1dbe6065f9f
Phil Henderson <phillip.henderson@hpe.com>
2025-12-15 16:30:53 -0500

    Updates
    Skip-fnbullseye: false
    Skip-func-hw-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/rpms/prep_other_deps.sh
+++ utils/rpms/prep_other_deps.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+set -uex
+
 bullseye_key="${1:-}"
 
+# Get the bullseye version
 mydir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 pushd "${mydir}/../.." || exit 1
 source utils/rpms/package_info.sh
 popd
 
-curl --proxy=http://proxy.houston.hpecorp.net:8080/ \
-    https://www.bullseye.com/download/BullseyeCoverage-${bullseye_version}-Linux-x64.tar.xz \
-    --retry 10 --retry-max-time 60 --silent --show-error -o bullseye.tar.xz
+bullseye_url="https://www.bullseye.com/download"
+bullseye_src="${bullseye_url}/BullseyeCoverage-${bullseye_version}-Linux-x64.tar.xz"
+bullseye_out="bullseye.tar.xz"
+curl_proxy="--proxy http://proxy.houston.hpecorp.net:8080/"
+
+curl "${curl_proxy}" "${bullseye_src}" --retry 10 --retry-max-time 60 --silent --show-error -o "${bullseye_out}"
 mkdir -p bullseye
-tar -C bullseye --strip-components=1 -xf bullseye.tar.xz
+tar -C bullseye --strip-components=1 -xf "${bullseye_out}"
 pushd bullseye
 set +x
 echo + sudo ./install --quiet --key "**********" --prefix /opt/BullseyeCoverage
 sudo ./install --quiet --key "${bullseye_key}" --prefix /opt/BullseyeCoverage
 set -x
-ls -aR
-ls -aR /opt/BullseyeCoverage
 popd
+ls -alR bullseye
 rm -rf bullseye.tar.xz bullseye
