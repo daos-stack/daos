c1d0cface3c7fdf2d8d28ca6f5e35992fe1f724a
Phil Henderson <phillip.henderson@hpe.com>
2025-12-15 15:16:30 -0500

    Updates
    Skip-fnbullseye: false
    Skip-func-hw-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- ci/rpm/build_deps.sh
+++ ci/rpm/build_deps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 code_coverage="${1:-false}"
 bullseye_key="${2:-}"
+mydir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd /home/daos/pre || exit 1
 scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos TARGET_TYPE=release -j 32
 
 if [ "${code_coverage}" == "true" ] ; then
-    mydir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
     pushd "${mydir}/../.." || exit 1
     utils/rpms/prep_other_deps.sh "${bullseye_key}"
     ls -aR
