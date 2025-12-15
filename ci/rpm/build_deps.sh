41e4641abbebbc18efb40cc7e4264cb43dc7d184
Phil Henderson <phillip.henderson@hpe.com>
2025-12-15 13:06:14 -0500

    Updates
    Skip-fnbullseye: false
    Skip-func-hw-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- ci/rpm/build_deps.sh
+++ ci/rpm/build_deps.sh
@@ -6,6 +6,7 @@ cd /home/daos/pre || exit 1
 scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos TARGET_TYPE=release -j 32
 
 if [ "${code_coverage}" == "true" ] ; then
+    https_proxy="http://proxy.houston.hpecorp.net:8080/"
     curl https://www.bullseye.com/download/BullseyeCoverage-9.23.7-Linux-x64.tar.xz --retry 10 --retry-max-time 60 --silent --show-error -o bullseye.tar.xz
     mkdir -p bullseye
     tar -C bullseye --strip-components=1 -xf bullseye.tar.xz
