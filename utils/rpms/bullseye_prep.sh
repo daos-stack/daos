f605da3ff651880010cbe809b1aae5e8bbcce7c6
Phil Henderson <phillip.henderson@hpe.com>
2025-12-15 17:00:06 -0500

    Updates
    Skip-fnbullseye: false
    Skip-func-hw-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/rpms/bullseye_prep.sh
+++ utils/rpms/bullseye_prep.sh
@@ -12,6 +12,7 @@ popd
 bullseye_url="https://www.bullseye.com/download"
 bullseye_src="${bullseye_url}/BullseyeCoverage-${bullseye_version}-Linux-x64.tar.xz"
 bullseye_out="bullseye.tar.xz"
+export SL_BULLSEYE_PREFIX="/opt/BullseyeCoverage"
 
 curl -h
 curl --proxy http://proxy.houston.hpecorp.net:8080/ "${bullseye_src}" --retry 10 --retry-max-time 60 --silent --show-error -o "${bullseye_out}"
@@ -20,9 +21,9 @@ mkdir -p bullseye
 tar -C bullseye --strip-components=1 -xf "${bullseye_out}"
 pushd bullseye
 set +x
-echo + sudo ./install --quiet --key "**********" --prefix /opt/BullseyeCoverage
-sudo ./install --quiet --key "${bullseye_key}" --prefix /opt/BullseyeCoverage
+echo + sudo ./install --quiet --key "**********" --prefix "${SL_BULLSEYE_PREFIX}"
+sudo ./install --quiet --key "${bullseye_key}" --prefix "${SL_BULLSEYE_PREFIX}"
 set -x
 popd
-ls -alR bullseye
 rm -rf bullseye.tar.xz bullseye
+ls -alR "${SL_BULLSEYE_PREFIX}"
