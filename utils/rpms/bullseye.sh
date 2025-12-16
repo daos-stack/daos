c86207fe17184455ca1f363dd0be86d4b8e607d6
Phil Henderson <phillip.henderson@hpe.com>
2025-12-16 09:07:44 -0500

    Updates
    Skip-fnbullseye: false
    Skip-func-hw-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/rpms/bullseye.sh
+++ utils/rpms/bullseye.sh
@@ -2,21 +2,25 @@
 root="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 . "${root}/fpm_common.sh"
 
+# Temporary
+SL_BULLSEYE_PREFIX="${SL_BULLSEYE_PREFIX:-/opt/BullseyeCoverage}"
+
 if [ -z "${SL_BULLSEYE_PREFIX:-}" ]; then
   echo "bullseye must be installed or never built in ${SL_BULLSEYE_PREFIX:-}"
-#   exit 0
+  exit 0
 fi
 
 VERSION="${bullseye_version}"
 RELEASE="${bullseye_release}"
 LICENSE="Proprietary"
+ARCH="${isa}"
 DESCRIPTION="The BullseyeCoverage compiler"
 URL="https://www.bullseye.com/index.html"
 RPM_CHANGELOG="bullseye.changelog"
 
 PACKAGE_TYPE="dir"
 files=()
-TARGET_PATH="/opt/BullseyeCoverage"
+TARGET_PATH="${SL_BULLSEYE_PREFIX}"
 list_files files "/opt/BullseyeCoverage/*"
 append_install_list "${files[@]}"
 build_package "bullseye"
