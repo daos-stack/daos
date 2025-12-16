86106d717b4b013d34c82cb0bffed4b05010424d
Phil Henderson <phillip.henderson@hpe.com>
2025-12-16 00:02:55 -0500

    Updates.
    Skip-fnbullseye: false
    Skip-func-hw-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/rpms/bullseye.sh
+++ utils/rpms/bullseye.sh
@@ -4,7 +4,7 @@ root="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
 if [ -z "${SL_BULLSEYE_PREFIX:-}" ]; then
   echo "bullseye must be installed or never built in ${SL_BULLSEYE_PREFIX:-}"
-  exit 0
+#   exit 0
 fi
 
 VERSION="${bullseye_version}"
