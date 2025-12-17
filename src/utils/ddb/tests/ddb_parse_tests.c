ff4eb90c475a44e83c0e3c50e9051d548a37faed
Cedric Koch-Hofer <cedric.koch.hofer@gmail.com>
2025-12-17 20:00:14 +0000

    DAOS-18323 ddb: Incorrect parsing of vos path with DDB
    Remove useless comments.
    
    Features: recovery
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@hpe.com>

--- src/utils/ddb/tests/ddb_parse_tests.c
+++ src/utils/ddb/tests/ddb_parse_tests.c
@@ -121,7 +121,6 @@ parse_vos_file_parts_test_errors(void **state)
 				  &parts);
 	assert_rc_equal(rc, -DER_OVERFLOW);
 
-	/* Test invalid vos paths with too long db path */
 	/* Test invalid vos paths with too long db path - MD-on-SSD */
 	D_ALLOC_ARRAY_CHECK(buf, DB_PATH_SIZE + 1);
 	memset(buf, 'a', DB_PATH_SIZE);
