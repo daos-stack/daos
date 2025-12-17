8dd39d9a01418e485da3f1075ba48312c553435d
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-12-17 13:17:57 +0000

    Fix - -f does not detect symlink to directories
    Cancel-prev-build: false
    
    Quick-build: true
    Skip-build-leap15-gcc: true
    Skip-build-leap15-icc: true
    
    Skip-unit-test: true
    Skip-unit-tests: true
    Skip-func-vm: true
    Skip-fault-injection-test: true
    Skip-test-rpms: true
    
    Test-tag: test_recycle_pools_hw
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- utils/tools/nvme-full-init.sh
+++ utils/tools/nvme-full-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Copyright 2025 Hewlett Packard Enterprise Development LP
+#  Copyright 2025 Hewlett Packard Enterprise Development LP
 #
-# SPDX-License-Identifier: BSD-2-Clause-Patent
+#  SPDX-License-Identifier: BSD-2-Clause-Patent
 #
 # Full reinitialization of all NVMe devices on the DAOS cluster node
 # - unmount all NVMe devices
@@ -25,26 +25,26 @@ function nvme_count_devices {
 
 function nvme_unmount_all {
   local NVME_MAX=${1:-$NVME_MAX_GLOBAL}
-  echo "Unmounting all /dev/nvme[0-${NVME_MAX}]n1 mountpoints (if any)..."
+  echo "Unmounting all /dev/nvme[0-${NVME_MAX}]n1 mountpoints."
   set +e # we can try to unmount unexisted disks
   for i in $(seq 0 $NVME_MAX); do
     nvme="nvme${i}n1"
     dev="/dev/${nvme}"
     mnt="/mnt/${nvme}"
     if [ -b "$dev" ]; then
-      # Get mountpoint (empty if not mounted)
       mp=$(lsblk -nr -o MOUNTPOINT "$dev")
       if [ -n "$mp" ]; then
         echo "Unmounting $dev from $mp"
-        sudo umount -f "$dev" 2>/dev/null || sudo umount -f "$mp" 2>/dev/null
-      else
+        sudo umount -f "$dev"|| sudo umount -f "$mp"
+      elif [ -d "${mnt}" ]; then
         echo "Unmounting ${mnt}"
-        sudo umount -f "${mnt}" 2>/dev/null
+        sudo umount -f "${mnt}"
       fi
     elif [ -d "${mnt}" ]; then
       echo "Force umount of ${mnt}"
-      sudo umount -f "${mnt}" 2>/dev/null
+      sudo umount -f "${mnt}"
     fi
+    rm -rf $mnt
   done
   set -e
 }
@@ -56,7 +56,7 @@ function nvme_bind_all_in_order {
 
   if [ -z "$nvme_pcie_addrs" ]; then
     echo "No NVMe PCI devices found."
-    return 0
+    return 1
   fi
 
   #echo "Found PCI NVMe addresses:"
@@ -64,29 +64,34 @@ function nvme_bind_all_in_order {
   #echo
 
   for dir in /sys/class/nvme/*/; do
-    echo "$dir: $(ls -la ${dir} | grep device | awk -F'-> ' '{print $2}' | sed 's|.*/||')"
+    numa=$(cat ${dir}numa_node)
+    echo "$numa $dir: $(ls -la ${dir} | grep device | awk -F'-> ' '{print $2}' | sed 's|.*/||')"
   done
 
   # Unbind all NVMe devices
   echo "Unbinding NVMe devices from nvme driver (or vfio-pci driver) ..."
   set +e # it's ok if a device isn't bound to one of the drivers
   for addr in $nvme_pcie_addrs; do
-    echo "Unbinding $addr"
-    if [ -f "/sys/bus/pci/drivers/nvme/${addr}" ]; then
-        echo "$addr" | sudo tee /sys/bus/pci/drivers/nvme/unbind > /dev/null 2>&1
+    if [ -e "/sys/bus/pci/drivers/nvme/${addr}" ]; then
+      echo "Unbinding $addr from nvme"
+      echo "$addr" | sudo tee /sys/bus/pci/drivers/nvme/unbind
     fi
-    if [ -f "/sys/bus/pci/drivers/vfio-pci/${addr}" ]; then
-        echo "$addr" | sudo tee /sys/bus/pci/drivers/vfio-pci/unbind > /dev/null 2>&1
+    if [ -e "/sys/bus/pci/drivers/vfio-pci/${addr}" ]; then
+      echo "Unbinding $addr from vfio-pci"
+      echo "$addr" | sudo tee /sys/bus/pci/drivers/vfio-pci/unbind
     fi
   done
   set -e
 
   echo
+  # Bind all NVMe devices in order
   echo "Binding NVMe devices to nvme driver in sorted order..."
+  set +e # for debug purpose
   for addr in $(echo "$nvme_pcie_addrs" | sort); do
     echo "Binding $addr"
-    echo "$addr" | sudo tee /sys/bus/pci/drivers/nvme/bind > /dev/null 2>&1
+    echo "$addr" | sudo tee /sys/bus/pci/drivers/nvme/bind
   done
+  set -e
 }
 
 
@@ -115,10 +120,13 @@ function nvme_recreate_namespace {
     # selects LBA format index 0 (512BK) and no secure erase, just format.
     nvme format $dev_ns --lbaf=0 --ses=0 --force
     nvme reset $dev
-    nvme id-ns $dev_ns |grep -E "lbaf|nvmcap|nsze|ncap|nuse"
+#    nvme id-ns $dev_ns |grep -E "lbaf|nvmcap|nsze|ncap|nuse"
   done
   set -e
 }
+
+# FOR now limit to 2 devices per CPU NUMA node
+: "${DAOS_CI_NVME_NUMA_LIMIT:=2}"
 
 SPDK_PCI_ALLOWED=""
 function nvme_reserve_2_disk_per_numa {
@@ -135,24 +143,24 @@ function nvme_reserve_2_disk_per_numa {
     echo "$nvme: NUMA node $numa_node, PCI addr $pci_addr, numa_node_0 $numa_node_0, numa_node_1 $numa_node_1"
     if [ "$numa_node" -eq 0 ]; then
       ((numa_node_0++)) || true
-      if [ "$numa_node_0" -lt 3 ]; then
+      if [ "$numa_node_0" -le $DAOS_CI_NVME_NUMA_LIMIT ]; then
         SPDK_PCI_ALLOWED="$SPDK_PCI_ALLOWED$pci_addr "
         echo "NUMA0: ${nvme} -> ${pci_addr}"
         continue
       fi
     else
       ((numa_node_1++)) || true
-      if [ "$numa_node_1" -lt 3 ]; then
+      if [ "$numa_node_1" -le $DAOS_CI_NVME_NUMA_LIMIT ]; then
         SPDK_PCI_ALLOWED="$SPDK_PCI_ALLOWED$pci_addr "
         echo "NUMA1: ${nvme} -> ${pci_addr}"
         continue
       fi
     fi
     sudo mkfs.ext4 -F "${dev}"
-    if [ ! -d "${mnt}" ]; then
-      sudo mkdir -p "${mnt}"
-    fi
-    sudo mount "${dev}" "${mnt}"
+#    if [ ! -d "${mnt}" ]; then
+#      sudo mkdir -p "${mnt}"
+#    fi
+#    sudo mount "${dev}" "${mnt}"
   done
   SPDK_PCI_ALLOWED=${SPDK_PCI_ALLOWED% }  # remove trailing space
 }
@@ -161,7 +169,7 @@ nvme_count=$(nvme_count_devices)
 if [ "$nvme_count" -gt 1 ]; then
   ((nvme_count--)) || true
   nvme_unmount_all $nvme_count
-  nvme_bind_all_in_order $nvme_count
+  nvme_bind_all_in_order
   nvme_recreate_namespace $nvme_count
   nvme_reserve_2_disk_per_numa $nvme_count
 
@@ -171,9 +179,16 @@ if [ "$nvme_count" -gt 1 ]; then
   #	cat /sys/class/nvme/nvme$i/address 2>/dev/null || echo "not found"
   #done
 
-  pushd /usr/share/daos/spdk/scripts/
-  export PCI_ALLOWED="$SPDK_PCI_ALLOWED"
-  sudo ./setup.sh
+  if [ -d /usr/share/daos/spdk/scripts/ ] && [ -f /usr/share/daos/spdk/scripts/setup.sh ]; then
+    export PCI_ALLOWED="$SPDK_PCI_ALLOWED"
+    pushd /usr/share/daos/spdk/scripts/
+    set +e
+    sudo ./setup.sh
+    set -e
+    popd
+  else
+    echo "Required spdk/scripts/setup.sh not found!"
+  fi
 
   daos_server nvme scan
 fi
