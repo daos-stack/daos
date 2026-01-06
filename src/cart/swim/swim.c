f7a3ed804f5eca392c2cd235688abf5f629b7d60
Li Wei <liwei@hpe.com>
2026-01-06 16:28:45 +0900

    Fix copyright date
    Signed-off-by: Li Wei <liwei@hpe.com>

--- src/cart/swim/swim.c
+++ src/cart/swim/swim.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2016 UChicago Argonne, LLC
  * (C) Copyright 2018-2024 Intel Corporation.
  * (C) Copyright 2025 Google LLC
- * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+ * (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
