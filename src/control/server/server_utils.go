da4fdfb58b924a8aac8eef9ef51e86a8704c4d14
Tom Nabarro <tom.nabarro@intel.com>
2026-01-26 22:58:47 +0000

    DAOS-17468 control: Prevent start if transparent hugepages are enabled (#16313)
    When THP feature is enabled on linux platforms, SPDK related
    hugepage management in DAOS performs sub-optimally. Resulting problems
    relate to memory accounting and fragmentation. To remedy, refuse to
    start daos_server if THP is enabled on platform and recommend
    disabling THP by applying kernel commandline parameters effective on
    reboot.
    
    Features: control
    Signed-off-by: Tom Nabarro <thomas.nabarro@hpe.com>

--- src/control/server/server_utils.go
+++ src/control/server/server_utils.go
@@ -1,6 +1,6 @@
 //
 // (C) Copyright 2021-2024 Intel Corporation.
-// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+// (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -297,12 +297,28 @@ func getEngineNUMANodes(log logging.Logger, engineCfgs []*engine.Config) ([]stri
 // Prepare bdev storage. Assumes validation has already been performed on server config. Hugepages
 // are required for both emulated (AIO devices) and real NVMe bdevs. VFIO and IOMMU are not
 // mandatory requirements for emulated NVMe.
-func prepBdevStorage(srv *server, iommuEnabled bool) error {
+func prepBdevStorage(srv *server, iommuChecker hardware.IOMMUDetector, thpChecker hardware.THPDetector) error {
 	defer srv.logDuration(track("time to prepare bdev storage"))
 
 	if srv.cfg.DisableHugepages {
 		srv.log.Debugf("skip nvme prepare as disable_hugepages: true in config")
 		return nil
+	}
+
+	// Fail to start if transparent hugepages are enabled. DAOS requires exclusive control over
+	// hugepages and therefore needs feature disabled. AllowTHP override flag provided for
+	// edge cases.
+	if !srv.cfg.AllowTHP {
+		if thpEnabled, err := thpChecker.IsTHPEnabled(); err != nil {
+			return errors.Wrap(err, "transparent hugepage check")
+		} else if thpEnabled {
+			return FaultTransparentHugepageEnabled
+		}
+	}
+
+	iommuEnabled, err := iommuChecker.IsIOMMUEnabled()
+	if err != nil {
+		return errors.Wrap(err, "iommu check")
 	}
 
 	bdevCfgs := srv.cfg.GetBdevConfigs()
