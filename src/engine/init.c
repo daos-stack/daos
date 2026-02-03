60b5bb3e1fc1924a163532a0f4527582adda4726
Mohamad Chaarawi <mohamad.chaarawi@hpe.com>
2026-02-03 04:50:49 +0000

    Revert "DAOS-17607 sched: sched monitor (#16798) (#16825)"
    This reverts commit 1c1f0e9ee510a40f67f7cd8cb8a86a9db0c2fa61.

--- src/engine/init.c
+++ src/engine/init.c
@@ -906,7 +906,7 @@ server_init(int argc, char *argv[])
 	if (rc)
 		D_GOTO(exit_init_state, rc);
 
-	dss_xstreams_open_barrier(false);
+	dss_xstreams_open_barrier();
 	D_INFO("Service fully up\n");
 
 	/** Report timestamp when engine was open for business */
