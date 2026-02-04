c2f1abfcab07f39bf63e9c4d2fa6c4ccfe099bbb
Cedric Koch-Hofer <cedric.koch.hofer@gmail.com>
2026-02-04 09:06:06 +0000

    DAOS-18533 ddb: Fix ddb completion
    Fix completion function of the ddb commands open, rm_pool and feature.
    
    Features: recovery
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@hpe.com>

--- src/control/cmd/ddb/command_completers.go
+++ src/control/cmd/ddb/command_completers.go
@@ -1,5 +1,6 @@
 //
 // (C) Copyright 2022-2024 Intel Corporation.
+// (C) Copyright 2026 Hewlett Packard Enterprise Development LP
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -7,74 +8,139 @@
 package main
 
 import (
-	"io/fs"
+	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
-const (
-	defMntPrefix = "/mnt"
+var (
+	vosRegexp = regexp.MustCompile(`^.+/vos-[1-9]*[[:digit:]]$`)
 )
 
-func listDirVos(match string) (result []string) {
-	if strings.HasSuffix(match, "vos-") {
-		match = filepath.Dir(match)
+func listVosFiles(match string) (result []string) {
+	result = []string{}
+
+	matches, err := filepath.Glob(match + "*")
+	if err != nil {
+		return
 	}
-	filepath.Walk(match, func(path string, info fs.FileInfo, err error) error {
+	for _, match := range matches {
+		path := filepath.Clean(match)
+		fi, err := os.Stat(path)
 		if err != nil {
-			/* ignore error */
-			return nil
+			continue
 		}
-		if strings.Contains(path, "vos-") {
+
+		switch mode := fi.Mode(); {
+		case mode.IsDir():
+			result = append(result, path+string(os.PathSeparator))
+		case mode.IsRegular():
+			if !vosRegexp.MatchString(path) {
+				continue
+			}
 			result = append(result, path)
 		}
-		return nil
-	})
+	}
+
 	return
 }
 
-func filterSuggestions(prefix string, initialSuggestions, additionalSuggestions []string) []string {
-	suggestions := append([]string{}, initialSuggestions...)
-	suggestions = append(suggestions, additionalSuggestions...)
+func listPoolDirs(match string) (result []string) {
+	result = []string{}
 
-	if len(prefix) > 0 {
-		var newSuggestions []string
-		for _, s := range suggestions {
-			if strings.HasPrefix(s, prefix) {
-				newSuggestions = append(newSuggestions, strings.Trim(s, prefix))
-			}
-		}
-		suggestions = newSuggestions
-
+	matches, err := filepath.Glob(match + "*")
+	if err != nil {
+		return
 	}
 
-	return suggestions
+	for _, match := range matches {
+		path := filepath.Clean(match)
+		fi, err := os.Stat(path)
+		if err != nil {
+			continue
+		}
+		if !fi.Mode().IsDir() {
+			continue
+		}
+		entries, err := os.ReadDir(path)
+		if err != nil {
+			continue
+		}
+		if len(entries) == 0 {
+			continue
+		}
+		has_dir := false
+		is_pool := false
+		for _, entry := range entries {
+			if entry.IsDir() {
+				has_dir = true
+				continue
+			}
+			if !entry.Type().Perm().IsRegular() {
+				continue
+			}
+			if !vosRegexp.MatchString(filepath.Join(path, entry.Name())) {
+				continue
+			}
+			result = append(result, path)
+			is_pool = true
+			break
+		}
+		if has_dir && !is_pool {
+			result = append(result, path+string(os.PathSeparator))
+		}
+	}
+
+	return
+}
+
+func appendSuggestion(suggestions []string, suggestion string, prefix string) []string {
+	if len(prefix) == 0 {
+		return append(suggestions, suggestion)
+	}
+
+	if !strings.HasPrefix(suggestion, prefix) {
+		return suggestions
+	}
+
+	if len(suggestion) > 2 && suggestion[1] == prefix[0] {
+		// Workaround to properly handle invalid prefix management
+		return append(suggestions, suggestion)
+	}
+	return append(suggestions, strings.TrimPrefix(suggestion, prefix))
+}
+
+func filterSuggestions(prefix string, initialSuggestions, additionalSuggestions []string) (suggestions []string) {
+	for _, suggestion := range initialSuggestions {
+		suggestions = appendSuggestion(suggestions, suggestion, prefix)
+	}
+
+	for _, suggestion := range additionalSuggestions {
+		suggestions = appendSuggestion(suggestions, suggestion, prefix)
+	}
+
+	return
 }
 
 func openCompleter(prefix string, args []string) []string {
-	return filterSuggestions(prefix, []string{"-h", "-w", "--write_mode"}, listDirVos(defMntPrefix))
+	return filterSuggestions(
+		prefix,
+		[]string{"-w", "--write_mode", "-p", "--db_path=", "-h", "--help"},
+		listVosFiles(prefix),
+	)
 }
 
 func featureCompleter(prefix string, args []string) []string {
-	return filterSuggestions(prefix, []string{"-h", "-e", "--enable", "-d", "--disable", "-s", "--show"},
-		listDirVos(defMntPrefix))
-}
-
-func listDirPool(match string) (result []string) {
-	if strings.HasSuffix(match, "vos-") {
-		match = filepath.Dir(match)
-	}
-	filepath.Walk(match, func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			/* ignore error */
-			return nil
-		}
-		result = append(result, path)
-		return nil
-	})
-	return
+	return filterSuggestions(
+		prefix,
+		[]string{"-e", "--enable", "-d", "--disable", "-s", "--show", "-h", "--help"},
+		listVosFiles(prefix))
 }
 
 func rmPoolCompleter(prefix string, args []string) []string {
-	return filterSuggestions(prefix, []string{"-h"}, listDirPool(defMntPrefix))
+	return filterSuggestions(
+		prefix,
+		[]string{"-h", "--help"},
+		listVosFiles(prefix))
 }
---
+++ src/control/cmd/ddb/command_completers_test.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/daos-stack/daos/src/control/common/test"
+)
+
+var (
+	testPoolDirs = [...]string{"a", "ab", "aac", "aaad"}
+	testVosFiles = [...]string{"vos-0", "vos-1", "vos-2", "vos-10", "vos-a"}
+)
+
+func createFile(t *testing.T, filePath string) {
+	t.Helper()
+
+	fd, err := os.Create(filePath)
+	if err != nil {
+		t.Fatalf("Failed to create test vos file %s: %v", filePath, err)
+	}
+	fd.Close()
+}
+
+func createDirAll(t *testing.T, dirPath string) {
+	t.Helper()
+
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		t.Fatalf("Failed to create test pool directory %s: %v", dirPath, err)
+	}
+}
+
+func testSetup(t *testing.T) (tmpDir string, teardown func()) {
+	t.Helper()
+
+	tmpDir, teardown = test.CreateTestDir(t)
+
+	for _, dir := range testPoolDirs {
+		createDirAll(t, filepath.Join(tmpDir, dir))
+		for _, file := range testVosFiles {
+			createFile(t, filepath.Join(tmpDir, dir, file))
+		}
+	}
+
+	createDirAll(t, filepath.Join(tmpDir, "foo"))
+	createFile(t, filepath.Join(tmpDir, "foo", "bar"))
+
+	createDirAll(t, filepath.Join(tmpDir, "bar"))
+	createDirAll(t, filepath.Join(tmpDir, "bar", "foo"))
+	createDirAll(t, filepath.Join(tmpDir, "bar", "baz"))
+	createFile(t, filepath.Join(tmpDir, "bar", "baz", "no_vos"))
+
+	return
+}
+
+func TestListVosFiles(t *testing.T) {
+	tmpDir, teardown := testSetup(t)
+	t.Cleanup(teardown)
+
+	for name, tc := range map[string]struct {
+		args   string
+		expRes []string
+	}{
+		"unaccessible": {
+			args:   "/root/",
+			expRes: []string{},
+		},
+		"void director prefix": {
+			args: tmpDir + string(os.PathSeparator),
+			expRes: []string{
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "ab") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "aac") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "aaad") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "foo") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "bar") + string(os.PathSeparator),
+			},
+		},
+		"a pool directory prefix": {
+			args: tmpDir + string(os.PathSeparator) + "a",
+			expRes: []string{
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "ab") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "aac") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "aaad") + string(os.PathSeparator),
+			},
+		},
+		"aa pool directory prefix": {
+			args: tmpDir + string(os.PathSeparator) + "aa",
+			expRes: []string{
+				filepath.Join(tmpDir, "aac") + string(os.PathSeparator),
+				filepath.Join(tmpDir, "aaad") + string(os.PathSeparator),
+			},
+		},
+		"all vos files": {
+			args: tmpDir + string(os.PathSeparator) + "a" + string(os.PathSeparator),
+			expRes: []string{
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator) + "vos-0",
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator) + "vos-1",
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator) + "vos-2",
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator) + "vos-10",
+			},
+		},
+		"vos-1 prefix files": {
+			args: tmpDir + string(os.PathSeparator) + "a" + string(os.PathSeparator) + "vos-1",
+			expRes: []string{
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator) + "vos-1",
+				filepath.Join(tmpDir, "a") + string(os.PathSeparator) + "vos-10",
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			results := listVosFiles(tc.args)
+			test.AssertStringsEqual(t, tc.expRes, results, "listDirVos results do not match expected")
+		})
+	}
+}
+
+func TestListPoolDirs(t *testing.T) {
+	tmpDir, teardown := testSetup(t)
+	t.Cleanup(teardown)
+
+	for name, tc := range map[string]struct {
+		args   string
+		expRes []string
+	}{
+		"unaccessible": {
+			args:   "/root/",
+			expRes: []string{},
+		},
+		"void directory prefix": {
+			args: tmpDir + string(os.PathSeparator),
+			expRes: []string{
+				filepath.Join(tmpDir, "a"),
+				filepath.Join(tmpDir, "ab"),
+				filepath.Join(tmpDir, "aac"),
+				filepath.Join(tmpDir, "aaad"),
+				filepath.Join(tmpDir, "bar") + string(os.PathSeparator),
+			},
+		},
+		"a pool directory prefix": {
+			args: tmpDir + string(os.PathSeparator) + "a",
+			expRes: []string{
+				filepath.Join(tmpDir, "a"),
+				filepath.Join(tmpDir, "ab"),
+				filepath.Join(tmpDir, "aac"),
+				filepath.Join(tmpDir, "aaad"),
+			},
+		},
+		"aa pool directory prefix": {
+			args: tmpDir + string(os.PathSeparator) + "aa",
+			expRes: []string{
+				filepath.Join(tmpDir, "aac"),
+				filepath.Join(tmpDir, "aaad"),
+			},
+		},
+		"bar pool directory": {
+			args:   tmpDir + string(os.PathSeparator) + "bar" + string(os.PathSeparator),
+			expRes: []string{},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			results := listPoolDirs(tc.args)
+			test.AssertStringsEqual(t, tc.expRes, results, "listDirPool results do not match expected")
+		})
+	}
+}
+
+func TestFilterSuggestions(t *testing.T) {
+	for name, tc := range map[string]struct {
+		prefix                string
+		initialSuggestions    []string
+		additionalSuggestions []string
+		expRes                []string
+	}{
+		"no prefix": {
+			prefix:                "",
+			initialSuggestions:    []string{"-a", "--all", "-b", "--bar="},
+			additionalSuggestions: []string{"foo", "a", "ab", "aac", "aaad"},
+			expRes:                []string{"-a", "--all", "-b", "--bar=", "foo", "a", "ab", "aac", "aaad"},
+		},
+		"with '-' prefix": {
+			prefix:                "-",
+			initialSuggestions:    []string{"-a", "--all", "-b", "--bar="},
+			additionalSuggestions: []string{"foo", "a", "ab", "aac", "aaad"},
+			expRes:                []string{"a", "--all", "b", "--bar="},
+		},
+		"with '--' prefix": {
+			prefix:                "--",
+			initialSuggestions:    []string{"-a", "--all", "-b", "--bar="},
+			additionalSuggestions: []string{"foo", "a", "ab", "aac", "aaad"},
+			expRes:                []string{"--all", "--bar="},
+		},
+		"with 'a' prefix": {
+			prefix:                "a",
+			initialSuggestions:    []string{"-a", "--all", "-b", "--bar="},
+			additionalSuggestions: []string{"foo", "a", "ab", "aac", "aaad"},
+			expRes:                []string{"", "b", "aac", "aaad"},
+		},
+		"with 'aa' prefix": {
+			prefix:                "aa",
+			initialSuggestions:    []string{"-a", "--all", "-b", "--bar="},
+			additionalSuggestions: []string{"foo", "a", "ab", "aac", "aaad"},
+			expRes:                []string{"aac", "aaad"},
+		},
+		"with 'aaa' prefix": {
+			prefix:                "aaa",
+			initialSuggestions:    []string{"-a", "--all", "-b", "--bar="},
+			additionalSuggestions: []string{"foo", "a", "ab", "aac", "aaad"},
+			expRes:                []string{"aaad"},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			results := filterSuggestions(tc.prefix, tc.initialSuggestions, tc.additionalSuggestions)
+			test.AssertStringsEqual(t, tc.expRes, results, "filterSuggestions results do not match expected")
+		})
+	}
+}
