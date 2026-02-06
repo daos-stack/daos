fc447b2263f80ba1d2073f78288a12995814fb4f
Tom Nabarro <tom.nabarro@intel.com>
2026-02-06 17:13:58 +0000

    DAOS-18566 control: Add spdk iobuf opts to server config (#17512)
    Add SPDK iobuf configurable parameters for small and large pool count
    to the server config file. Tuning these parameters can alleviate
    bottlenecks in high performance environments.
    
    Signed-off-by: Tom Nabarro <thomas.nabarro@hpe.com>

--- src/control/server/storage/config.go
+++ src/control/server/storage/config.go
@@ -1149,6 +1149,18 @@ type BdevAutoFaulty struct {
 	MaxCsumErrs uint32 `yaml:"max_csum_errs,omitempty" json:"max_csum_errs"`
 }
 
+// SpdkIobuf struct describes settings for DAOS I/O buffer pool configuration within the BIO
+// module of the engine process.
+type SpdkIobuf struct {
+	SmallPoolCount uint32 `yaml:"small_pool_count,omitempty" json:"small_pool_count,omitempty"`
+	LargePoolCount uint32 `yaml:"large_pool_count,omitempty" json:"large_pool_count,omitempty"`
+}
+
+// IsEmpty returns true if all struct values are zero.
+func (si *SpdkIobuf) IsEmpty() bool {
+	return si.SmallPoolCount == 0 && si.LargePoolCount == 0
+}
+
 // Config defines engine storage.
 type Config struct {
 	ControlMetadata  ControlMetadata `yaml:"-"` // inherited from server
@@ -1161,6 +1173,7 @@ type Config struct {
 	AccelProps       AccelProps      `yaml:"acceleration,omitempty"`
 	SpdkRpcSrvProps  SpdkRpcServer   `yaml:"spdk_rpc_server,omitempty"`
 	AutoFaultyProps  BdevAutoFaulty  `yaml:"bdev_auto_faulty,omitempty"`
+	SpdkIobufProps   SpdkIobuf       `yaml:"spdk_iobuf,omitempty"`
 }
 
 // SetNUMAAffinity enables the assignment of NUMA affinity to tier configs.
