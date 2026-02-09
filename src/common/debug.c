cf448d3b0ff11d73d71e2ef7d1fb702120e2f5ac
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-02-09 11:01:25 +0100

    Copyright update
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- src/common/debug.c
+++ src/common/debug.c
@@ -1,6 +1,6 @@
 /**
  * (C) Copyright 2016-2023 Intel Corporation.
- * Copyright 2025 Hewlett Packard Enterprise Development LP
+ * Copyright 2025-2026 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
