3d31a0faef191d847c9e47d07d050d532b731150
Michael MacDonald <github@macdonald.cx>
2026-02-11 17:09:29 -0500

    DAOS-16311 control: Sort check reports in the API
    The test helper was modified to sort the reports, but
    the correct place to add sorting is in the API method
    so that all callers may benefit from stable report order.
    
    Features: cr
    Signed-off-by: Michael MacDonald <github@macdonald.cx>

--- src/control/lib/control/check_test.go
+++ src/control/lib/control/check_test.go
@@ -1,6 +1,6 @@
 //
 // (C) Copyright 2023 Intel Corporation.
-// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+// (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 //
@@ -11,8 +11,10 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
 
 	chkpb "github.com/daos-stack/daos/src/control/common/proto/chk"
+	mgmtpb "github.com/daos-stack/daos/src/control/common/proto/mgmt"
 	"github.com/daos-stack/daos/src/control/common/test"
 	"github.com/daos-stack/daos/src/control/lib/daos"
 )
@@ -178,5 +180,40 @@ func TestControl_SystemCheckReport_IsStale(t *testing.T) {
 
 			test.AssertEqual(t, action == expStaleAction, report.IsStale(), "")
 		})
+	}
+}
+
+func TestControl_SystemCheckQuery_ReportsSorted(t *testing.T) {
+	// Reports are returned in scrambled order to verify that
+	// SystemCheckQuery sorts them by class, then by sequence.
+	mockResp := &mgmtpb.CheckQueryResp{
+		Reports: []*chkpb.CheckReport{
+			{Seq: 3, Class: chkpb.CheckInconsistClass_CIC_POOL_BAD_LABEL},
+			{Seq: 1, Class: chkpb.CheckInconsistClass_CIC_POOL_BAD_LABEL},
+			{Seq: 5, Class: chkpb.CheckInconsistClass_CIC_POOL_NONEXIST_ON_MS},
+			{Seq: 4, Class: chkpb.CheckInconsistClass_CIC_CONT_NONEXIST_ON_PS},
+			{Seq: 2, Class: chkpb.CheckInconsistClass_CIC_POOL_MORE_SVC},
+		},
+	}
+
+	mi := NewMockInvoker(nil, &MockInvokerConfig{
+		UnaryResponse: MockMSResponse("", nil, mockResp),
+	})
+
+	resp, err := SystemCheckQuery(test.Context(t), mi, &SystemCheckQueryReq{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expReports := []*SystemCheckReport{
+		{chkpb.CheckReport{Seq: 2, Class: chkpb.CheckInconsistClass_CIC_POOL_MORE_SVC}},
+		{chkpb.CheckReport{Seq: 5, Class: chkpb.CheckInconsistClass_CIC_POOL_NONEXIST_ON_MS}},
+		{chkpb.CheckReport{Seq: 1, Class: chkpb.CheckInconsistClass_CIC_POOL_BAD_LABEL}},
+		{chkpb.CheckReport{Seq: 3, Class: chkpb.CheckInconsistClass_CIC_POOL_BAD_LABEL}},
+		{chkpb.CheckReport{Seq: 4, Class: chkpb.CheckInconsistClass_CIC_CONT_NONEXIST_ON_PS}},
+	}
+
+	if diff := cmp.Diff(expReports, resp.Reports, protocmp.Transform()); diff != "" {
+		t.Fatalf("reports not sorted (-want +got):\n%s", diff)
 	}
 }
