da2c12271861cc4c7afbf624be5f3403ac63b868
Ryon Jensen <ryon.jensen@hpe.com>
2026-03-18 16:01:32 -0600

    SRE-3661: Build on SLES 15sp7
    Signed-off-by: Ryon Jensen <ryon.jensen@hpe.com>

--- ci/rpm/install_deps.sh
+++ ci/rpm/install_deps.sh
@@ -3,6 +3,8 @@
 # Script for installing DAOS dependencies for the expected version
 set -uex
 
+source /etc/os-release
+
 id
 if [ "$(id -u)" = "0" ]; then
     echo "Should not be run as root"
@@ -30,12 +32,24 @@ isal_crypto_pkg="$(utils/rpms/package_version.sh isal_crypto dev)"
 daos_spdk_pkg="$(utils/rpms/package_version.sh daos_spdk dev)"
 pmdk_pkg="$(utils/rpms/package_version.sh pmdk dev pmemobj)"
 
-sudo dnf install --allowerasing -y "${libfabric_pkg}" || echo "${libfabric_pkg} not available"
-sudo dnf install --allowerasing -y "${mercury_pkg}" || echo "${mercury_pkg} not available"
-sudo dnf install --allowerasing -y "${argobots_pkg}" || echo "${argobots_pkg} not available"
-sudo dnf install --allowerasing -y "${daos_spdk_pkg}" || echo "${daos_spdk_pkg} not available"
-sudo dnf install --allowerasing -y "${fused_pkg}" || echo "${fused_pkg} not available"
-sudo dnf install --allowerasing -y "${pmdk_pkg}" || echo "${pmdk_pkg} not available"
-sudo dnf install --allowerasing -y "${isal_pkg}" || echo "${isal_pkg} not available"
-sudo dnf install --allowerasing -y "${isal_crypto_pkg}" || echo "${isal_crypto_pkg} not available"
+if [[ $ID == "sles" ]]; then
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${libfabric_pkg}" || echo "${libfabric_pkg} not available"
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${mercury_pkg}" || echo "${mercury_pkg} not available"
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${argobots_pkg}" || echo "${argobots_pkg} not available"
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${daos_spdk_pkg}" || echo "${daos_spdk_pkg} not available"
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${fused_pkg}" || echo "${fused_pkg} not available"
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${pmdk_pkg}" || echo "${pmdk_pkg} not available"
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${isal_pkg}" || echo "${isal_pkg} not available"
+  sudo zypper --non-interactive install --allow-vendor-change --allow-downgrade "${isal_crypto_pkg}" || echo "${isal_crypto_pkg} not available"
+else
+  sudo dnf install --allowerasing -y "${libfabric_pkg}" || echo "${libfabric_pkg} not available"
+  sudo dnf install --allowerasing -y "${mercury_pkg}" || echo "${mercury_pkg} not available"
+  sudo dnf install --allowerasing -y "${argobots_pkg}" || echo "${argobots_pkg} not available"
+  sudo dnf install --allowerasing -y "${daos_spdk_pkg}" || echo "${daos_spdk_pkg} not available"
+  sudo dnf install --allowerasing -y "${fused_pkg}" || echo "${fused_pkg} not available"
+  sudo dnf install --allowerasing -y "${pmdk_pkg}" || echo "${pmdk_pkg} not available"
+  sudo dnf install --allowerasing -y "${isal_pkg}" || echo "${isal_pkg} not available"
+  sudo dnf install --allowerasing -y "${isal_crypto_pkg}" || echo "${isal_crypto_pkg} not available"
+fi
+
 popd || exit 1
