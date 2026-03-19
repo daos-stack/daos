b8d0700575620307aec78c4f77884e2525c2a456
Kenneth Cain <kenneth.cain@hpe.com>
2026-03-19 09:09:51 -0400

    DAOS-18676 test: DO NOT LAND debug TestPool.disconnect()
    Adjust engine logmasks to DEBUG while disconnecting from the pool
    created in the test_continuous_after_stop test. And, adjust the
    test itself to restart all engines, in case the engine that
    (before this patch) does not get started somehow contributes to
    the pool disconnect delays.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Test-tag: test_continuous_after_stop
    Test-repeat: 10
    
    Signed-off-by: Kenneth Cain <kenneth.cain@hpe.com>

--- src/tests/ftest/rebuild/continues_after_stop.py
+++ src/tests/ftest/rebuild/continues_after_stop.py
@@ -1,6 +1,6 @@
 """
   (C) Copyright 2024 Intel Corporation.
-  (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+  (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 """
@@ -52,7 +52,7 @@ class ContinuesAfterStop(IorTestBase):
         4. Look for the start of the rebuild (Rebuild [scanning]) in journalctl with daos_server
         identifier.
         5. As soon as the message is detected, stop the rest of the ranks (0, 1, 2).
-        6. Restart the three ranks.
+        6. Restart all four ranks.
         7. Wait for rebuild to finish.
 
         Jira ID: DAOS-6287
@@ -99,8 +99,8 @@ class ContinuesAfterStop(IorTestBase):
         self.log_step("As soon as the message is detected, stop the rest of the ranks (0, 1, 2).")
         self.server_managers[0].stop_ranks(ranks=[0, 1, 2])
 
-        self.log_step("Restart the three ranks.")
-        self.server_managers[0].start_ranks(ranks=[0, 1, 2])
+        self.log_step("Restart all ranks.")
+        self.server_managers[0].start_ranks(ranks=[0, 1, 2, 3])
 
         self.log_step("Wait for rebuild to finish.")
         pool.wait_for_rebuild_to_end(interval=5)
