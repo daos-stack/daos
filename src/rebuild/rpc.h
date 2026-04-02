f3b1dd5889d9050f004fefca0df30c6dde6fbeee
Wang Shilong <shilong.wang@hpe.com>
2026-04-02 03:51:15 +0000

    address comments
    Signed-off-by: Wang Shilong <shilong.wang@hpe.com>

--- src/rebuild/rpc.h
+++ src/rebuild/rpc.h
@@ -61,8 +61,8 @@ extern struct crt_proto_format rebuild_proto_fmt;
 
 #define DAOS_OSEQ_REBUILD_SCAN	/* output fields */		 \
 	((uint64_t)		(rso_stable_epoch)	CRT_VAR) \
-	((int32_t)		(rso_status)		CRT_VAR) \
-	((uint64_t)		(rso_padding)		CRT_VAR)
+	((uint64_t)		(rso_padding)		CRT_VAR) \
+	((int32_t)		(rso_status)		CRT_VAR)
 
 CRT_RPC_DECLARE(rebuild_scan, DAOS_ISEQ_REBUILD_SCAN, DAOS_OSEQ_REBUILD_SCAN)
 
@@ -73,12 +73,12 @@ CRT_RPC_DECLARE(rebuild_scan, DAOS_ISEQ_REBUILD_SCAN, DAOS_OSEQ_REBUILD_SCAN)
 	((daos_unit_oid_t)	(roi_oids)		CRT_ARRAY) \
 	((uint64_t)		(roi_ephs)		CRT_ARRAY) \
 	((uuid_t)		(roi_uuids)		CRT_ARRAY) \
-	((uint32_t)		(roi_shards)		CRT_ARRAY) \
-	((uint64_t)		(roi_padding)		CRT_VAR)
+	((uint64_t)		(roi_padding)		CRT_VAR) \
+	((uint32_t)		(roi_shards)		CRT_ARRAY)
 
 #define DAOS_OSEQ_REBUILD	/* output fields */		 \
-	((int32_t)		(roo_status)		CRT_VAR) \
-	((uint64_t)		(roo_padding)		CRT_VAR)
+	((uint64_t)		(roo_padding)		CRT_VAR) \
+	((int32_t)		(roo_status)		CRT_VAR)
 
 CRT_RPC_DECLARE(rebuild, DAOS_ISEQ_REBUILD, DAOS_OSEQ_REBUILD)
 
