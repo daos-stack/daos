d1eb67a7eef71aa54f38f69f2bb1154995cb34c1
Kris Jacque <kris.jacque@hpe.com>
2026-04-11 00:41:32 +0000

    Address review comments
    Features: recovery
    
    Signed-off-by: Kris Jacque <kris.jacque@hpe.com>

--- src/control/server/ctl_check.go
+++ src/control/server/ctl_check.go
@@ -18,29 +18,6 @@ import (
 	"github.com/daos-stack/daos/src/control/lib/ranklist"
 )
 
-func (svc *ControlService) findRankIndex(rank ranklist.Rank) (uint32, error) {
-	foundIdx := -1
-	for _, e := range svc.harness.instances {
-		r, err := e.GetRank()
-		if err != nil {
-			svc.log.Errorf("unable to get rank for engine %d: %s", e.Index(), err)
-			continue
-		}
-
-		if rank == r {
-			svc.log.Tracef("rank %d is local engine %d", rank, e.Index())
-			foundIdx = int(e.Index())
-			break
-		}
-	}
-
-	if foundIdx < 0 {
-		return 0, errors.Errorf("rank %d is not managed by this DAOS server", rank)
-	}
-
-	return uint32(foundIdx), nil
-}
-
 // CheckEngineRepair sends a check repair request to a specific engine.
 func (svc *ControlService) CheckEngineRepair(ctx context.Context, req *ctlpb.CheckEngineActReq) (*ctlpb.CheckEngineActResp, error) {
 	if req == nil {
@@ -56,19 +33,24 @@ func (svc *ControlService) CheckEngineRepair(ctx context.Context, req *ctlpb.Che
 		return nil, errors.New("nil rank in request")
 	}
 
-	idx, err := svc.findRankIndex(r)
+	engSlice, err := svc.harness.FilterInstancesByRankSet(r.String())
 	if err != nil {
 		return nil, err
 	}
 
-	dResp, err := svc.harness.instances[idx].CallDrpc(ctx, daos.MethodCheckerAction, req.Req)
+	if len(engSlice) == 0 {
+		return nil, errors.Errorf("rank %d is not managed by this DAOS server", r)
+	}
+
+	eng := engSlice[0]
+	dResp, err := eng.CallDrpc(ctx, daos.MethodCheckerAction, req.Req)
 	if err != nil {
-		return nil, errors.Wrapf(err, "dRPC to engine %d (rank %d)", idx, r)
+		return nil, errors.Wrapf(err, "dRPC to engine %d (rank %d)", eng.Index(), r)
 	}
 
 	checkActResp := new(mgmtpb.CheckActResp)
 	if err := proto.Unmarshal(dResp.Body, checkActResp); err != nil {
-		return nil, errors.Wrapf(err, "unable to unmarshal engine %d response", idx)
+		return nil, errors.Wrapf(err, "unable to unmarshal engine %d response", eng.Index())
 	}
 
 	return &ctlpb.CheckEngineActResp{
