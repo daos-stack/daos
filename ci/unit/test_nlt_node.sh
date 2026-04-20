a29c5aacf1b5169ef085e8c476dd693474151904
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-04-20 15:32:52 +0200

    SRE-3734 ci: force direct access to CI artifactory
    Configure NO_PROXY along with HTTPS_PROXY to redirect traffic
    directly to DAOS CI artifactory, bypassing the proxy.
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
    
    Priority: 2
    
    Skip-func-test-leap15: false
    
    Skip-func-hw-test-medium: false
    Skip-func-hw-test-medium-vmd: false
    Skip-func-hw-test-medium-verbs-provider: false
    Skip-func-hw-test-medium-ucx-provider: false
    Skip-func-hw-test-large: false

--- ci/unit/test_nlt_node.sh
+++ ci/unit/test_nlt_node.sh
@@ -45,5 +45,8 @@ pip install /opt/daos/lib/daos/python/
 sudo prlimit --nofile=1024:262144 --pid $$
 prlimit -n
 
-HTTPS_PROXY="${DAOS_HTTPS_PROXY:-}" ./utils/node_local_test.py --max-log-size 1950MiB \
+HTTPS_PROXY="${DAOS_HTTPS_PROXY:-}" \
+    NO_PROXY="${DAOS_NO_PROXY:-}" \
+    no_proxy="${DAOS_NO_PROXY:-}" \
+    ./utils/node_local_test.py --max-log-size 1950MiB \
     --dfuse-dir /localhome/jenkins/ --log-usage-save nltir.xml --log-usage-export nltr.json all
