4b7a01acc256a266d543912c6507eb0a6ad7875d
Phil Henderson <phillip.henderson@hpe.com>
2026-05-07 08:56:30 -0400

    Add dc/rc/ud/ protocol synonyms too hg_info output.
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-test-rpms: true
    Test-provider: ucx+dc_x
    Test-tag: DmgNetworkScanTest
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- src/tests/ftest/util/network_utils.py
+++ src/tests/ftest/util/network_utils.py
@@ -290,17 +290,23 @@ def get_hg_info(logger, hosts, filter_provider=None, filter_device=None, verbose
                     device = device.split(":")[0]
                 if filter_device and device not in filter_device:
                     continue
+                providers = []
                 if "_" in protocol:
                     _protocol = protocol.split("_")
-                    if _protocol[0] in ("dc", "rc", "ud"):
-                        # e.g. convert ud_mlx5, ud_verbs => ud_x
-                        protocol = f"{_protocol[0]}_x"
-                provider = f"{_class}+{protocol}"
-                if filter_provider and provider not in filter_provider:
-                    continue
-                if device not in device_providers:
-                    device_providers[device] = set()
-                device_providers[device].add(provider)
+                    if _protocol[0] in ("dc", "rc", "ud") and _protocol[1] in ("mlx5"):
+                        # Add ucx synonyms, e.g. convert ud_mlx5 => ud_mlx5, ud_x, ud
+                        for synonym in (f"_{_protocol[1]}", "_x", ""):
+                            providers.append(f"{_class}+{_protocol[0]}{synonym}")
+                else:
+                    providers.append(f"{_class}+{protocol}")
+
+                for provider in providers:
+                    # provider = f"{_class}+{protocol}"
+                    if filter_provider and provider not in filter_provider:
+                        continue
+                    if device not in device_providers:
+                        device_providers[device] = set()
+                    device_providers[device].add(provider)
 
             for device, provider_set in device_providers.items():
                 if device not in providers:
