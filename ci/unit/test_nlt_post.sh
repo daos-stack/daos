29ef8653e5734de4a58164c661bb144a7807ddc8
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-05-12 13:06:49 +0200

    Fix: copy only what is needed
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- ci/unit/test_nlt_post.sh
+++ ci/unit/test_nlt_post.sh
@@ -13,17 +13,13 @@ mkdir nlt_logs
 # Copy any log files.  Use rsync filters here to allow us to specify
 # all files we want to copy, as it's much more flexible than using
 # standard wildcards.
-rsync -v -dprt -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":/tmp/ \
-      --filter="include dnt*.log" --filter="include dnt*.log.bz2" \
-      --filter="include dnt_fi_*_logs" \
-      --filter="exclude *" nlt_logs/ || true
 
 # When running with --no-root, DAOS logs go to build/nlt_logs/ on the node
 # instead of /tmp/, so fetch them from there as well.
 rsync -v -rlpt -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":build/nlt_logs/ \
       --filter="include dnt*.log" --filter="include dnt*.log.bz2" \
       --filter="include dnt_fi_*_logs" --filter="include */" \
-      --filter="exclude *" nlt_logs/ || true
+      --filter="exclude *" nlt_logs/
 
 rsync -v -dpt -z -e "ssh $SSH_KEY_ARGS" jenkins@"$NODE":build/ \
       --filter="include nlt*.json" --filter="include dnt*.xml" \
