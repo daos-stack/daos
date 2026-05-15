899fab0fe52afd80468f61ebbc3860fa6e7801c9
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-05-15 16:07:01 +0200

    ci: align SCons argument handling and modernize rpm build script
    Unify how build arguments are derived in CI and improve the standalone RPM
    build script defaults/cleanup for reproducible manual and Jenkins runs.
    
    Use sconsArgs() directly when deciding whether to patch rpmlintrc
    Remove redundant SCONS_FAULTS_ARGS environment export
    Update Leap 15 build invocation to use sconsArgs() and match other build
    stages (stash files, stash_opt, and build_deps=no behavior)
    In the RPM build script:
    derive JOBS dynamically via nproc
    add TARGET and PREFIX defaults
    clean stale artifacts before build
    run scons with --config=force
    tee build output to a target/compiler-specific log file
    This reduces duplicated argument plumbing and makes CI/manual builds more
    consistent.
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -118,7 +118,7 @@ Integer getuid() {
 }
 
 void fixup_rpmlintrc() {
-    if (env.SCONS_FAULTS_ARGS != 'BUILD_TYPE=dev') {
+    if (sconsArgs() != 'BUILD_TYPE=dev') {
         return
     }
 
@@ -235,7 +235,6 @@ pipeline {
         TEST_RPMS = cachedCommitPragma(pragma: 'RPM-test', def_val: 'true')
         COVFN_DISABLED = cachedCommitPragma(pragma: 'Skip-fnbullseye', def_val: 'true')
         REPO_FILE_URL = repoFileUrl(env.REPO_FILE_URL)
-        SCONS_FAULTS_ARGS = sconsArgs()
         HTTPS_PROXY = ''
     }
 
@@ -653,9 +652,11 @@ pipeline {
                                 script: './ci/rpm/build_deps.sh'
                             job_step_update(
                                 sconsBuild(parallel_build: true,
-                                scons_args: sconsFaultsArgs() +
-                                ' PREFIX=/opt/daos TARGET_TYPE=release',
-                                build_deps: 'yes'))
+                                           stash_files: 'ci/test_files_to_stash.txt',
+                                           build_deps: 'no',
+                                           stash_opt: true,
+                                           scons_args: sconsArgs() +
+                                                      ' PREFIX=/opt/daos TARGET_TYPE=release'))
                             sh label: 'Generate RPMs',
                                 script: './ci/rpm/gen_rpms.sh suse.lp156 "' + env.DAOS_RELVAL + '"'
                         }
--- ci/rpm/build.sh
+++ ci/rpm/build.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
 # Script for building DAOS
-# If the STAGE_NAME environment variable is present the script will
-# attempt to parse it to determine what distribution to build for.
 #
-# For manual testing, you can set the environment variables CHROOT_NAME
-# and TARGET can be set.
-#
-# Default is to build for CentOS 7.
-# Fault injection will be enabled by default in CI unless a pragma has
-# has disabled fault injection or this is a Release build
+# Default is to build for Rocky 9.
 set -uex
 
 # Default values for variables that may be set in the environment
-: "${JOBS:=144}"
+echo "Get number of processors online"
+: "${JOBS:=$(nproc)}"
 : "${COMPILER:=gcc}"
 : "${DAOS_BUILD_TYPE:=dev}"
 : "${DAOS_TARGET_TYPE:=release}"
+: "${TARGET:=el9}"
+: "${PREFIX:=/opt/daos}"
 
-scons --jobs "$JOBS" --build-deps=no install PREFIX=/opt/daos COMPILER="$COMPILER" \
-      FIRMWARE_MGMT=1 BUILD_TYPE="$DAOS_BUILD_TYPE" TARGET_TYPE="$DAOS_TARGET_TYPE" \
-      USE_INSTALLED=all
+echo "Remove some old build files if present."
+rm -rf src/rdb/raft/CLinkedListQueue bandit.xml test.cov
+
+# Original code from Jenkinsfile, moved here to be used in both Jenkins and manual builds
+# command -v scons
+#
+# /usr/bin/getconf _NPROCESSORS_ONLN
+# echo "Get number of processors online"
+# rm -rf src/rdb/raft/CLinkedListQueue bandit.xml test.cov
+# echo "Remove some old build files if present."
+# String tee_file = '| tee $WORKSPACE/' + stage_info['log_to_file']
+# /home/daos/venv/bin/scons -c
+# rm -rf _build.external install build daos_m.conf daos.conf iof.conf cart-Linux.conf .sconsign.dblite .sconsign-Linux.dblite .sconf-temp .sconf-temp-Linux
+# SCONS_ARGS='-j 144 --build-deps=no install USE_INSTALLED=all COMPILER=gcc BUILD_TYPE=dev PREFIX=/opt/daos TARGET_TYPE=release'
+# /home/daos/venv/bin/scons --config=force -j 144 --build-deps=no install USE_INSTALLED=all COMPILER=gcc BUILD_TYPE=dev PREFIX=/opt/daos TARGET_TYPE=release
+# tee /var/lib/jenkins/jenkins-3/docker_1/workspace/daos-stack_daos_master@2/el8-gcc-build.log
+
+
+scons -c && \
+rm -rf _build.external install build daos_m.conf daos.conf iof.conf\
+      cart-Linux.conf .sconsign.dblite .sconsign-Linux.dblite .sconf-temp .sconf-temp-Linux && \
+scons --config=force --jobs "$JOBS" --build-deps=no install PREFIX="$PREFIX" COMPILER="$COMPILER" \
+      BUILD_TYPE="$DAOS_BUILD_TYPE" TARGET_TYPE="$DAOS_TARGET_TYPE" \
+      USE_INSTALLED=all | tee $WORKSPACE/${TARGET}-${COMPILER}-build.log
