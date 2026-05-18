e4b9a810c34ce490fd6df610fee2830ccce7eac5
Cedric Koch-Hofer <94527853+knard38@users.noreply.github.com>
2026-05-18 16:52:02 +0200

    DAOS-18857 build: Support of Gperftools Heap Profiler (#18068)
    Add a new `HEAP_PROFILER=true` SCons build option that links DAOS executables
    with `-ltcmalloc` to enable heap profiling via the Gperftools Heap Profiler. The
    option is mutually exclusive with Google Sanitizers and is stripped from unit
    test environments to avoid corrupted test output.
    
    Add `gperftools-devel` (EL/SUSE) and `libgoogle-perftools-dev` (Ubuntu) as
    a build dependency of the `daos-devel` package and to all distro install scripts
    so that developers can build with `HEAP_PROFILER=true` without installing extra
    packages.
    
    Document both the compile-time (`HEAP_PROFILER=true`) and runtime
    (`LD_PRELOAD=libtcmalloc.so`) approaches in the development guide.
    
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@hpe.com>

--- utils/scripts/install-ubuntu.sh
+++ utils/scripts/install-ubuntu.sh
@@ -37,6 +37,7 @@ apt-get install ${apt_get_install_args} \
     libcunit1-dev \
     libdaxctl-dev \
     libfuse3-dev \
+    libgoogle-perftools-dev \
     libhwloc-dev \
     libibverbs-dev \
     libjson-c-dev \
