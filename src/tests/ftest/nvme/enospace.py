24e2165e4e31ad11eddd16842319a2517f44536f
Ding-Hwa Ho <ding-hwa.ho@hpe.com>
2026-06-02 22:45:30 -0400

    self.ior_cmd.dfs_cont.update(baseline_cont_uuid)
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Required-githooks: true
    
    Signed-off-by: Ding-Hwa Ho <ding-hwa.ho@hpe.com>

--- src/tests/ftest/nvme/enospace.py
+++ src/tests/ftest/nvme/enospace.py
@@ -695,7 +695,8 @@ class NvmeEnospace(ServerFillUp, TestWithTelemetry):
         self.run_enospace_with_bg_job(self.client_log)
 
         # Read the same container which was written at the beginning.
-        self.nvme_local_cont.uuid = baseline_cont_uuid
+        # self.nvme_local_cont.uuid = baseline_cont_uuid
+        self.ior_cmd.dfs_cont.update(baseline_cont_uuid)
         self.start_ior_load(storage='SCM', operation='Auto_Read', percent=1)
         max_mib_latest = float(self.ior_matrix[0][int(IorMetrics.MAX_MIB)])
         self.log.info("IOR Latest Read MiB %s", max_mib_latest)
