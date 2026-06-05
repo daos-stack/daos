13b2ccfba8085b7cc380ae55f8233ed57ffd7702
Cedric Koch-Hofer <94527853+knard38@users.noreply.github.com>
2026-06-06 01:12:20 +0200

    DAOS-19019 spdk: fix two memory leaks in NVMe control bindings (#18427)
    opts_add_pci_addr() allocates opts.pci_allowed via realloc() but
    daos_spdk_init() never frees it after calling spdk_env_init().
    Add free(opts.pci_allowed) at the out: label so both the success
    and error paths release the buffer.
    
    _collect() allocates ctrlr->pci_addr via calloc() for each
    controller, but free_ctrlr_fields() freed all other heap fields
    (model, serial, fw_rev, vendor_id, pci_type) while omitting
    pci_addr. Add the missing free().
    
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@hpe.com>

--- src/control/lib/spdk/ctests/nvme_control_ut.c
+++ src/control/lib/spdk/ctests/nvme_control_ut.c
@@ -1,6 +1,6 @@
 /**
  * (C) Copyright 2019-2021 Intel Corporation.
- * (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+ * (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
  *
  * SPDX-License-Identifier: BSD-2-Clause-Patent
  */
@@ -22,7 +22,7 @@
 	#pragma GCC diagnostic ignored "-Wframe-larger-than="
 #endif
 
-static struct ret_t	*test_ret;
+static struct ret_t *test_ret;
 
 /**
  * ==============================
@@ -299,15 +299,11 @@ int
 main(void)
 {
 	const struct CMUnitTest tests[] = {
-		cmocka_unit_test_setup_teardown(test_discover_null_controllers,
-						setup, teardown),
-		cmocka_unit_test_setup_teardown(test_discover_set_controllers,
-						setup, teardown),
-		cmocka_unit_test_setup_teardown(test_discover_probe_fail, setup,
-						teardown),
-		cmocka_unit_test_setup_teardown(test_collect, setup, teardown),
-		cmocka_unit_test_setup_teardown(test_get_controller, setup,
-						teardown),
+	    cmocka_unit_test_setup_teardown(test_discover_null_controllers, setup, teardown),
+	    cmocka_unit_test_setup_teardown(test_discover_set_controllers, setup, teardown),
+	    cmocka_unit_test_setup_teardown(test_discover_probe_fail, setup, teardown),
+	    cmocka_unit_test_setup_teardown(test_collect, setup, teardown),
+	    cmocka_unit_test_setup_teardown(test_get_controller, setup, teardown),
 	};
 
 	return cmocka_run_group_tests_name("control_nvme_control_ut", tests,
