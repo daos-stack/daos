d51c4d1027dbb743734f5feb4380d67ca762a8cb
Cedric Koch-Hofer <cedric.koch.hofer@gmail.com>
2026-06-10 08:13:55 +0000

    DAOS-18905 build: fix in_list PATH check due to missing word-split
    $old_path and $added were quoted when passed to in_list, causing
    the entire space-separated string to be treated as a single argument.
    As a result in_list never found a match and $SL_PREFIX/bin was always
    prepended to PATH, even when already present.
    
    Remove the quotes so the shell word-splits the list into individual
    arguments, and suppress the SC2086 shellcheck warning at each call site.
    
    Signed-off-by: Cedric Koch-Hofer <cedric.koch-hofer@hpe.com>

--- utils/sl/setup_local.sh
+++ utils/sl/setup_local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # /*
 #  * (C) Copyright 2016-2023 Intel Corporation.
+#  * (C) Copyright 2026 Hewlett Packard Enterprise Development LP
 #  * (C) Copyright 2025 Google LLC
 #  *
 #  * SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -87,13 +88,15 @@ added="/ /usr /usr/local"
 old_path="${PATH//:/ }"
 echo OLD_PATH is "${old_path}"
 for item in $list; do
-    in_list "${!item}" "${added}"
+    # shellcheck disable=SC2086
+    in_list "${!item}" ${added}
     if [ $? -eq 1 ]; then
         continue
     fi
     export "${item?}"
     added+=" ${!item}"
-    in_list "${!item}/bin" "${old_path}"
+    # shellcheck disable=SC2086
+    in_list "${!item}/bin" ${old_path}
     if [ $? -eq 1 ]; then
         continue
     fi
@@ -102,7 +105,8 @@ for item in $list; do
     fi
 done
 
-in_list "${SL_PREFIX}/bin" "${old_path}"
+# shellcheck disable=SC2086
+in_list "${SL_PREFIX}/bin" ${old_path}
 # shellcheck disable=SC2181
 if [ $? -eq 0 ]; then
     PATH=$SL_PREFIX/bin:$PATH
