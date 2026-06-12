f7c300d153af582c106fdde20083e536b6fdcab7
Kenneth Cain <kenneth.cain@hpe.com>
2026-06-12 17:09:03 -0400

    Expand DAOS-19028 debug logging across MGMT/cart map-update
    Latest changes include:
    - MGMT map distribution logging in map_update_bcast(), with verbose per-rank
      map dumps controlled by DAOS_MAP_UPDATE_VERBOSE (in addition to needing
      log_mask: DEBUG)
    - MGMT target pre-forward logging in ds_mgmt_tgt_map_update_pre_forward(),
      including a "MISMATCH " prefix when self/map state differs.
    - MGMT map update aggregation warning for non-zero member return codes.
    - CaRT group replace-path diagnostics in crt_group_primary_modify() for
      existing-rank SWIM-check flow (incoming rank/incarnation/URI visibility).
    - ftest suite env updates to enable DAOS_MAP_UPDATE_VERBOSE=1 on both engines.
    - launch.py CI repeat cap increased from 10 to 20.
    
    Looking for potential stale membership/address state during reintegrate hangs.
    
    Test-tag: test_rebuild_29
    Test-Repeat: 20
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-test-rpms: true
    Test-provider-hw-medium: ofi+tcp
    
    Signed-off-by: Kenneth Cain <kenneth.cain@hpe.com>

--- src/mgmt/srv_system.c
+++ src/mgmt/srv_system.c
@@ -141,6 +141,20 @@ free_server_list(struct server_entry *list, int len)
 	D_FREE(list);
 }
 
+static bool
+map_update_verbose_enabled(void)
+{
+	static bool initialized;
+	static bool enabled;
+
+	if (!initialized) {
+		d_getenv_bool("DAOS_MAP_UPDATE_VERBOSE", &enabled);
+		initialized = true;
+	}
+
+	return enabled;
+}
+
 static struct server_entry *
 dup_server_list(struct server_entry *in, int in_len)
 {
@@ -224,18 +238,24 @@ map_update_bcast(crt_context_t ctx, struct mgmt_svc *svc, uint32_t map_version,
 	crt_rpc_t		       *rpc;
 	int                             i;
 	int				rc;
+	bool                            verbose;
 
+	verbose = map_update_verbose_enabled();
 	D_DEBUG(DB_MGMT, "enter: version=%u nservers=%d\n", map_version,
 		nservers);
-	for (i = 0; i < nservers; i++) {
-		const char *uri = "<none>";
+	if (verbose) {
+		for (i = 0; i < nservers; i++) {
+			const char *uri = "<none>";
 
-		if (servers[i].se_uri != NULL)
-			uri = servers[i].se_uri;
+			if (servers[i].se_uri != NULL)
+				uri = servers[i].se_uri;
 
-		D_DEBUG(DB_MGMT, "map[%d/%d]: rank=%u inc=%lu uri=%s flags=%u nctxs=%u\n", i + 1,
-			nservers, servers[i].se_rank, (unsigned long)servers[i].se_incarnation, uri,
-			(unsigned int)servers[i].se_flags, (unsigned int)servers[i].se_nctxs);
+			D_DEBUG(DB_MGMT, "map[%d/%d]: rank=%u inc=%lu uri=%s flags=%u nctxs=%u\n",
+				i + 1, nservers, servers[i].se_rank,
+				(unsigned long)servers[i].se_incarnation, uri,
+				(unsigned int)servers[i].se_flags,
+				(unsigned int)servers[i].se_nctxs);
+		}
 	}
 
 	opc = DAOS_RPC_OPCODE(MGMT_TGT_MAP_UPDATE, DAOS_MGMT_MODULE,
@@ -266,13 +286,8 @@ map_update_bcast(crt_context_t ctx, struct mgmt_svc *svc, uint32_t map_version,
 out_rpc:
 	crt_req_decref(rpc);
 out:
-	if (rc != 0) {
-		D_WARN("map update bcast failed for version=%u nservers=%d: " DF_RC "\n",
-		       map_version, nservers, DP_RC(rc));
-	}
-
-	D_DEBUG(DB_MGMT, "leave: version=%u nservers=%d: "DF_RC"\n",
-		map_version, nservers, DP_RC(rc));
+	DL_CDEBUG(rc, DLOG_WARN, DB_MGMT, rc, "map update bcast: version=%u nservers=%d",
+		  map_version, nservers);
 	return rc;
 }
 
