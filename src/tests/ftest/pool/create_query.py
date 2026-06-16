cdf9ce775fa2bedf6c901714a930a3e374d04180
Ryon Jensen <ryon.jensen@hpe.com>
2026-06-15 20:49:33 -0600

    SRE-3775 build: Introduce Cluster Box Stages
    - The new "Cluster in a Box" stage is intended to be able to support testing that currently
      requires hardware resources to run. A Cluster in a Box is a KVM host that hosts VMs that
      have emulated NVMe and passthrough IB devices, so they appear more like HW systems.
    - Changed several "PR" tests to run on Cluster Box stages. Some of the tests' configurations
      were adjusted to be able to run on a VM with less resources. Some tests can take
      longer to run so timeouts were increased.
    - As I was testing the CI changes, I noticed that more than "PR" tests were being run. This
      was because of the gen_commit_pragmas.py functionality, which will include tests if the
      test file was changed. Because test tags were being changed, the whole file was being
      run, including non-PR tests. To prevent that, gen_commit_pragmas.py was modified only
      automatically include tests if actual code changed ... not just comments.
    - In the Jenkinsfile, added pragma to set build description
    
    Signed-off-by: Ryon Jensen <ryon.jensen@hpe.com>
    
    Build-description: Testing from git push
    
    Cancel-prev-build: false
    
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-nlt: true
    Skip-fault-injection-test: true
    Skip-test-el-9-rpms: true
    Skip-test-leap-15-rpms: true
    Skip-func-test-el8: true
    Skip-func-test-el9: true
    Skip-func-test-leap15: true
    Skip-func-test-hw-medium: true
    Skip-func-test-hw-medium-md-on-ssd: true
    Skip-func-test-hw-medium-vmd: true
    Skip-func-test-hw-medium-verbs-provider: true
    Skip-func-test-hw-medium-verbs-provider-md-on-ssd: true
    Skip-func-test-hw-large: true
    Skip-func-test-hw-large-md-on-ssd: true
    Skip-python-bandit-check: true

--- src/tests/ftest/pool/create_query.py
+++ src/tests/ftest/pool/create_query.py
@@ -32,9 +32,9 @@ class PoolCreateQueryTests(TestWithServers):
             pool create.
 
         :avocado: tags=all,pr,daily_regression
-        :avocado: tags=hw,medium
+        :avocado: tags=cb,medium
         :avocado: tags=pool
-        :avocado: tags=PoolCreateQueryTests,test_create_and_query
+        :avocado: tags=PoolCreateQueryTests,test_create_and_query,stage_ftest
         """
         # Create pool
         pool = add_pool(self)
