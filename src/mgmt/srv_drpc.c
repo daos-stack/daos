7a7d67d9f908cd27598fec8643db8af0a4927a40
Tom Nabarro <thomas.nabarro@hpe.com>
2026-06-24 17:35:56 +0100

    add missing plumbing for percentage used stat
    Features: control
    Signed-off-by: Tom Nabarro <thomas.nabarro@hpe.com>

--- src/mgmt/srv_drpc.c
+++ src/mgmt/srv_drpc.c
@@ -2490,6 +2490,7 @@ ds_mgmt_drpc_bio_health_query(Drpc__Call *drpc_req, Drpc__Response *drpc_resp)
 	resp->pll_lock_loss_cnt = stats.pll_lock_loss_cnt;
 	resp->nand_bytes_written = stats.nand_bytes_written;
 	resp->host_bytes_written = stats.host_bytes_written;
+	resp->percentage_used = stats.percentage_used;
 
 out:
 	resp->status = rc;
