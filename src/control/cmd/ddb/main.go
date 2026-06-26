7a517351b2357ec2c96a2c80e1c436d2b51f6221
Tom Nabarro <thomas.nabarro@hpe.com>
2026-06-26 13:48:05 +0100

    DAOS-19243 control: update go.mod and protobuf shims
    Signed-off-by: Tom Nabarro <thomas.nabarro@hpe.com>

--- src/control/cmd/ddb/main.go
+++ src/control/cmd/ddb/main.go
@@ -159,7 +159,7 @@ func printCommands(fd io.Writer, app *grumble.App) {
 		row := c.Name + columnize.DefaultConfig().Delim + c.Help
 		output = append(output, row)
 	}
-	fmt.Fprintf(fd, helpCommandsHeader+columnize.SimpleFormat(output)+"\n\n")
+	fmt.Fprintf(fd, "%s%s\n\n", helpCommandsHeader, columnize.SimpleFormat(output))
 }
 
 func printGeneralHelp(app *grumble.App, generalMsg string) {
