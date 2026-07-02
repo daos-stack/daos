37280c0e72b5c7fee08025d79769b6972254c0a6
Tom Nabarro <thomas.nabarro@hpe.com>
2026-07-02 20:06:19 +0100

    print output changes
    Signed-off-by: Tom Nabarro <thomas.nabarro@hpe.com>

--- src/control/cmd/dmg/pretty/storage_nvme.go
+++ src/control/cmd/dmg/pretty/storage_nvme.go
@@ -80,7 +80,7 @@ func printNvmeHealth(stat *storage.NvmeHealth, out io.Writer, opts ...PrintConfi
 		fmt.Fprintf(iw, "Checksum Errors:%d\n", uint64(stat.ChecksumErrors))
 	}
 	fmt.Fprintf(iw, "Error Log Entries:%d\n", uint64(stat.ErrorLogEntries))
-	fmt.Fprintf(iw, "Percentage Used:%d%%\n", uint32(stat.PercentageUsed))
+	fmt.Fprintf(iw, "Lifetime Percent Used:%d%%\n", uint32(stat.PercentageUsed))
 
 	fmt.Fprintf(out, "Critical Warnings:\n")
 	fmt.Fprintf(iw, "Temperature: ")
