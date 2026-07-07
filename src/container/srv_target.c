22496c447237a91d3d8c25848a186366a6f3f10a
Liu Xuezhao <xuezhao.liu@intel.com>
2026-07-07 22:14:39 +0800

    DAOS-19151 rebuild: skip orphan container in rebuild puller (#18568)
    At rebuild puller side, the migrate_cont_iter_cb() ->
    cont_fetch_start_ult() -> ds_cont_fetch_snaps() possibly
    get -2050/-DER_CONT_NONEXIST for orphan container case.
    In that case skip the container and continue rebuild task.
    
    master commit 26ee6377d88 -
    1. fixed a case of -DER_SHUTDOWN err handling.
    2. lower the log level when container IV hit -DER_CONT_NONEXIST
    3. refine err code returned when abort EC agg
    4. add yield() in cont_agg_eph_sync()/ds_cont_eph_report()
    
    Signed-off-by: Xuezhao Liu <xuezhao.liu@hpe.com>

--- src/container/srv_target.c
+++ src/container/srv_target.c
@@ -2785,6 +2785,7 @@ ds_cont_eph_report(struct ds_pool *pool)
 	struct cont_track_eph	*ec_eph;
 	struct cont_track_eph	*tmp;
 	int                      rc, ret, *failed_tgts = NULL;
+	int                      cont_num = 0;
 	unsigned int             failed_tgts_nr;
 
 	D_ASSERT(pool != NULL && pool->sp_map != NULL);
@@ -2799,6 +2800,7 @@ ds_cont_eph_report(struct ds_pool *pool)
 		daos_epoch_t min_stable_eph;
 		int          i;
 
+		cont_num++;
 		if (dss_ult_exiting(pool->sp_ec_ephs_req))
 			break;
 
@@ -2830,8 +2832,11 @@ ds_cont_eph_report(struct ds_pool *pool)
 
 		if (min_ec_agg_eph <= ec_eph->cte_last_ec_agg_epoch &&
 		    min_stable_eph <= ec_eph->cte_last_stable_epoch &&
-		    pool->sp_reclaim == DAOS_RECLAIM_DISABLED)
+		    pool->sp_reclaim == DAOS_RECLAIM_DISABLED) {
+			if (cont_num % 10 == 0)
+				dss_sleep(0);
 			continue;
+		}
 
 		/* if aggregation enabled, make sure to report ec_agg_eph at the start phase
 		 * when min_ec_agg_eph and cte_last_ec_agg_epoch are both zero.
@@ -2855,6 +2860,8 @@ ds_cont_eph_report(struct ds_pool *pool)
 					min_ec_agg_eph, ec_eph->cte_last_ec_agg_epoch,
 					min_stable_eph, ec_eph->cte_last_stable_epoch,
 					DP_UUID(ec_eph->cte_cont_uuid));
+			if (cont_num % 10 == 0)
+				dss_sleep(0);
 			continue;
 		}
 
