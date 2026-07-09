55b8bfe24aecd8863dd2bdab7e69fbfa55a08ae6
Oksana Salyk <oksana.salyk@hpe.com>
2026-07-09 16:45:53 +0200

    SRE-3829 ci: Remove EL8 support
    Signed-off-by: Oksana Salyk <oksana.salyk@hpe.com>
    
    Priority: 2
    
    Skip-build-leap15-gcc: true

--- utils/rpms/daos.sh
+++ utils/rpms/daos.sh
@@ -173,7 +173,7 @@ systemctl --no-reload disable --now daos_server.service >& /dev/null || :
 EOF
   EXTRA_OPTS+=("--before-remove" "${tmp}/pre_uninstall_server")
 
-  if [[ "${DISTRO:-el8}" =~ suse ]]; then
+  if [[ "${DISTRO:-el9}" =~ suse ]]; then
     cat << EOF  > "${tmp}/post_uninstall_server"
 #!/bin/bash
 ldconfig
@@ -305,7 +305,7 @@ systemctl --no-reload disable --now daos_agent.service >& /dev/null || :
 EOF
 EXTRA_OPTS+=("--before-remove" "${tmp}/pre_uninstall_client")
 
-if [[ "${DISTRO:-el8}" =~ suse ]]; then
+if [[ "${DISTRO:-el9}" =~ suse ]]; then
   cat << EOF  > "${tmp}/post_uninstall_client"
 rm -f "/var/lib/systemd/migrated/daos_agent.service" || :
 /usr/bin/systemctl daemon-reload || :
@@ -376,7 +376,7 @@ fi
 EXTERNAL_DEPENDS+=("${capstone_lib}")
 EXTERNAL_DEPENDS+=("pciutils")
 EXTERNAL_DEPENDS+=("${ndctl_dev}")
-if [[ "${DISTRO:-el8}" =~ el ]]; then
+if [[ "${DISTRO:-el9}" =~ el ]]; then
   EXTERNAL_DEPENDS+=("daxctl-devel")
 fi
 DEPENDS=( "daos-client = ${VERSION}-${RELEASE}" "daos-admin = ${VERSION}-${RELEASE}")
