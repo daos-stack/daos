9abc2a6e058c721541b562d4034928a2ee36ba5b
Fan Yong <fan.yong@hpe.com>
2026-07-23 22:52:01 +0800

    DAOS-18921 tests: add sleep for CR29 - b26
    Wait a while for group membership synchornization.
    
    Temporarily disable some CR tests for CHK leader switch trouble.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    Skip-nlt: true
    Skip-func-test-vm: true
    Test-tag: test_daos_cat_recov_core
    
    Signed-off-by: Fan Yong <fan.yong@hpe.com>

--- src/tests/suite/daos_cr.c
+++ src/tests/suite/daos_cr.c
@@ -2017,6 +2017,9 @@ cr_shutdown(void **state)
 
 	print_message("CR10: checker shutdown\n");
 
+	/* Temporarily disable the test for DAOS-18537. */
+	skip();
+
 	cr_pause(state, false);
 }
 
@@ -2035,6 +2038,9 @@ cr_crash(void **state)
 	FAULT_INJECTION_REQUIRED();
 
 	print_message("CR11: checker crash\n");
+
+	/* Temporarily disable the test for DAOS-18537. */
+	skip();
 
 	cr_pause(state, true);
 }
@@ -2070,6 +2076,9 @@ cr_leader_resume(void **state)
 	FAULT_INJECTION_REQUIRED();
 
 	print_message("CR12: check leader resume from former stop/paused phase\n");
+
+	/* Temporarily disable the test for DAOS-18537. */
+	skip();
 
 	rc = cr_pool_create(state, &pool, false, class);
 	assert_rc_equal(rc, 0);
@@ -2195,6 +2204,9 @@ cr_engine_resume(void **state)
 	FAULT_INJECTION_REQUIRED();
 
 	print_message("CR13: check engine resume from former stop/paused phase\n");
+
+	/* Temporarily disable the test for DAOS-18537. */
+	skip();
 
 	rc = cr_pool_create(state, &pool, false, class);
 	assert_rc_equal(rc, 0);
@@ -3806,6 +3818,9 @@ cr_lost_rank0(void **state)
 	print_message("CR: excluding the rank 0 ...\n");
 	rc = dmg_system_exclude_rank(dmg_config_file, 0);
 	assert_rc_equal(rc, 0);
+
+	/* Wait a while for group membership synchornization. */
+	sleep(3);
 
 	rc = cr_pool_create(state, &pool, false, TCC_NONE);
 	assert_rc_equal(rc, 0);
