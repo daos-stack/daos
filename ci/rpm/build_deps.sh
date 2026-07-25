92a34a46f1e75061f856dfbf35f7bbfd30be25d0
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-07-25 19:02:58 +0000

    Test 2
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- ci/rpm/build_deps.sh
+++ ci/rpm/build_deps.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+cat /etc/pip.conf || true
+cat /etc/gemrc || true
+cat /etc/uv/uv.toml || true
+
+env | grep -i proxy || true
+
 scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos TARGET_TYPE=release -j 32
