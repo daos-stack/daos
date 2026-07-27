78014c2f248083b03d666de38b202c93052a5bd9
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-07-27 14:59:24 +0200

    Always build with UV_NO_BUILD_ISOLATION=1
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
    
    skip-cancel-previous-builds: true
    skip-python-bandit-check: true
    skip-unit-tests: true
    skip-test: true
    skip-test-hardware: true
    
    Priority: 2
    Cancel-prev-build: false
    Skip-python-bandit: true
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-func-hw-test: true
    Skip-build-el8-gcc: true
    Skip-unit-tests:true
    Skip-NLT: true
    Skip-fault-injection-test: true
    Skip-func-vm-all: true
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- ci/rpm/build_deps.sh
+++ ci/rpm/build_deps.sh
@@ -5,5 +5,6 @@ DAOS_TARGET_TYPE="${DAOS_TARGET_TYPE:-release}"
 DAOS_DEPS_JOBS="${DAOS_DEPS_JOBS:-32}"
 echo "Building DAOS dependencies... with $DAOS_DEPS_JOBS jobs and target type \"$DAOS_TARGET_TYPE\""
 
+export UV_NO_BUILD_ISOLATION=1
 scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos \
       TARGET_TYPE="$DAOS_TARGET_TYPE" -j "$DAOS_DEPS_JOBS"
