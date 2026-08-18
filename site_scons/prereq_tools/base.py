3e5761446ece34472195811765aae0190b7d925f
Michael MacDonald <github@macdonald.cx>
2026-08-18 15:19:37 -0400

    DAOS-19333 build: Don't fail release builds on memcheck (#18747)
    The memcheck stage uses a separate instrumented build for Go
    binaries regardless of the BUILD_TYPE value.
    
    Release builds compile out fault injection, so the stage has nothing
    to test and fails on the missing test logs.
    
    Signed-off-by: Michael MacDonald <github@macdonald.cx>

--- site_scons/prereq_tools/base.py
+++ site_scons/prereq_tools/base.py
@@ -517,8 +517,7 @@ class PreReqComponent():
                               ['warning', 'warn', 'error'], ignorecase=2))
         opts.Add(('SANITIZERS', 'Instrument C code with google sanitizers', None))
         opts.Add(BoolVariable('BUILD_GO_VALGRIND',
-                              'Build Go artifacts with the Go "valgrind" tag for Memcheck '
-                              '(also drops -race; ignored for release)',
+                              'Build Go artifacts with the Go "valgrind" tag for Memcheck',
                               False))
         opts.Add(BoolVariable('CMOCKA_FILTER_SUPPORTED', 'Allows to filter cmocka tests', False))
         opts.Add(BoolVariable('CRT_PP', 'Preprocess CaRT sources', False))
