f3e91ddca0a7801ffafce8cb93e472a354e8ad5c
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-08-19 22:41:05 +0200

    ci(repo-helper-el9): clean repository path handling and package flags build(uv): centralize UV isolation behavior across helpers and deps build
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- ci/rpm/build_deps.sh
+++ ci/rpm/build_deps.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# No isolation for building SPDK dependency, since we want to use
+# the system-installed (via python virtual environment) versions of
+# uv and hatchling to avoid installation of them during SPDK build process.
+export UV_NO_BUILD_ISOLATION=1
 scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos TARGET_TYPE=release -j 32
