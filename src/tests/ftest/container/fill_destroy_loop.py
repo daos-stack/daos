6b9d1d528f211324cfdaf93f4c6e7ebe716bb7ac
rpadma2 <ravindran.padmanabhan@hpe.com>
2026-08-19 17:27:29 -0400

    DAOS-17917 test: Update the find prune command option. Skip-func-hw-test-medium: false Skip-func-hw-test-medium-md-on-ssd: false Skip-unit-test: true Skip-fault-injection-test: true Test-tag: test_fill_destroy_cont_loop Test-repeat: 3
    Signed-off-by: rpadma2 <ravindran.padmanabhan@hpe.com>

--- src/tests/ftest/container/fill_destroy_loop.py
+++ src/tests/ftest/container/fill_destroy_loop.py
@@ -47,7 +47,7 @@ class BoundaryPoolContainerSpace(TestWithServers):
 
         cmd = "find {} -type f ".format(log_dir)
         if self.server_managers[0].manager.job.using_control_metadata:
-            cmd = "find {0} -path {0}/control_metadata -prune -type f ".format(log_dir)
+            cmd = "find {0} -path {0}/control_metadata -prune -o -type f ".format(log_dir)
         cmd += r"-regextype egrep "
         cmd += r"-regex '.*/daos_server\.log\.[[:digit:]]+' "
         cmd += r"-exec grep -q -E -e " + err_regex + r" {} ';' -print"
