463bba073ce501d70450843ea512c48a5ecde678
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-08-19 22:41:07 +0200

    build(deps): parameterize target type and parallel jobs
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- ci/rpm/build_deps.sh
+++ ci/rpm/build_deps.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -euo pipefail
+
+DAOS_TARGET_TYPE="${DAOS_TARGET_TYPE:-release}"
+DAOS_DEPS_JOBS="${DAOS_DEPS_JOBS:-32}"
+echo "Building DAOS dependencies... with $DAOS_DEPS_JOBS jobs and target type \"$DAOS_TARGET_TYPE\""
+
 # No isolation for building SPDK dependency, since we want to use
 # the system-installed (via python virtual environment) versions of
 # uv and hatchling to avoid installation of them during SPDK build process.
 export UV_NO_BUILD_ISOLATION=1
-scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos TARGET_TYPE=release -j 32
+
+scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos \
+      TARGET_TYPE="$DAOS_TARGET_TYPE" -j "$DAOS_DEPS_JOBS"
