042b33d51bcf12cbcc3e3a5e9846a30e42fb1bb9
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-08-21 12:35:11 +0200

    UV_NO_BUILD_ISOLATION=1 is used by default for SPDK
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- ci/rpm/build_deps.sh
+++ ci/rpm/build_deps.sh
@@ -5,10 +5,5 @@ DAOS_TARGET_TYPE="${DAOS_TARGET_TYPE:-release}"
 DAOS_DEPS_JOBS="${DAOS_DEPS_JOBS:-32}"
 echo "Building DAOS dependencies... with $DAOS_DEPS_JOBS jobs and target type \"$DAOS_TARGET_TYPE\""
 
-# No isolation for building SPDK dependency, since we want to use
-# the system-installed (via python virtual environment) versions of
-# uv and hatchling to avoid installation of them during SPDK build process.
-export UV_NO_BUILD_ISOLATION=1
-
 scons install --build-deps=only USE_INSTALLED=all PREFIX=/opt/daos \
       TARGET_TYPE="$DAOS_TARGET_TYPE" -j "$DAOS_DEPS_JOBS"
