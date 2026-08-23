035fc9534386ac16f074222498510ca2b7fc3915
rpadma2 <ravindran.padmanabhan@hpe.com>
2026-08-23 10:50:53 -0400

    DAOS-17519 test: Remove the fault injection file after test completion. Skip-func-hw-test-medium: false Skip-func-hw-test-medium-md-on-ssd: false Skip-unit-test: true Skip-fault-injection-test: true Skip-nlt: true Test-tag: DlckBasicFaultTest DlckBasicTest
    Signed-off-by: rpadma2 <ravindran.padmanabhan@hpe.com>

--- src/tests/ftest/recovery/dlck_basic_fault.py
+++ src/tests/ftest/recovery/dlck_basic_fault.py
@@ -9,7 +9,7 @@ from apricot import TestWithServers
 from dlck_utils import DlckCommand
 from fault_config_utils import FaultInjection
 from file_utils import distribute_files
-from run_utils import run_remote
+from run_utils import command_as_user, run_remote
 from test_utils_pool import add_pool
 
 
@@ -89,6 +89,15 @@ class DlckBasicFaultTest(TestWithServers):
                 errors.append(f"dlck failed on {result.failed_hosts}")
         self.log_step("Run the dmg start command")
         dmg.system_start()
+        # Make sure to delete the fi.yaml file after the test
+        if os.path.exists(fault_inject_file):
+            os.remove(fault_inject_file)
+        # Remove the fault injection file from all remote hosts as well
+        result = run_remote(self.log, self.hostlist_servers, command_as_user(f'rm -fr {fault_inject_file}',
+                                                                                self.root))
+        if not result.passed:
+            self.log.error(f'Error removing {fault_inject_file} on {result.failed_hosts}')
+
         if not errors:
             self.fail(f"dlck basic faulttest failed with errors: {errors}")
         self.log.info("dlck basic fault test passed with no errors")
