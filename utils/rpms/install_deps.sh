b34a68d7d7c8d1cf52735975680ef3304eb07398
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-09-03 16:22:36 +0200

    New home for build RPMs scripts
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- utils/rpms/install_deps.sh
+++ utils/rpms/install_deps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#
+#  (C) Copyright 2025 Google LLC
+#  (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
+#
+#  SPDX-License-Identifier: BSD-2-Clause-Patent
+#
 # Script for installing DAOS dependencies for the expected version
 set -uex
 
@@ -13,16 +18,15 @@ mydir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 env
 
-pushd "${mydir}/../.." || exit 1
 export DISTRO="${1}"
-libfabric_pkg="$(utils/rpms/package_version.sh libfabric dev)"
-mercury_pkg="$(utils/rpms/package_version.sh mercury dev)"
-argobots_pkg="$(utils/rpms/package_version.sh argobots dev)"
-fused_pkg="$(utils/rpms/package_version.sh fused dev)"
-isal_pkg="$(utils/rpms/package_version.sh isal dev)"
-isal_crypto_pkg="$(utils/rpms/package_version.sh isal_crypto dev)"
-daos_spdk_pkg="$(utils/rpms/package_version.sh daos_spdk dev)"
-pmdk_pkg="$(utils/rpms/package_version.sh pmdk dev pmemobj)"
+libfabric_pkg="$(${mydir}/package_version.sh libfabric dev)"
+mercury_pkg="$(${mydir}/package_version.sh mercury dev)"
+argobots_pkg="$(${mydir}/package_version.sh argobots dev)"
+fused_pkg="$(${mydir}/package_version.sh fused dev)"
+isal_pkg="$(${mydir}/package_version.sh isal dev)"
+isal_crypto_pkg="$(${mydir}/package_version.sh isal_crypto dev)"
+daos_spdk_pkg="$(${mydir}/package_version.sh daos_spdk dev)"
+pmdk_pkg="$(${mydir}/package_version.sh pmdk dev pmemobj)"
 
 sudo dnf install --allowerasing -y "${libfabric_pkg}" || echo "${libfabric_pkg} not available"
 sudo dnf install --allowerasing -y "${mercury_pkg}" || echo "${mercury_pkg} not available"
@@ -32,4 +36,3 @@ sudo dnf install --allowerasing -y "${fused_pkg}" || echo "${fused_pkg} not avai
 sudo dnf install --allowerasing -y "${pmdk_pkg}" || echo "${pmdk_pkg} not available"
 sudo dnf install --allowerasing -y "${isal_pkg}" || echo "${isal_pkg} not available"
 sudo dnf install --allowerasing -y "${isal_crypto_pkg}" || echo "${isal_crypto_pkg} not available"
-popd || exit 1
