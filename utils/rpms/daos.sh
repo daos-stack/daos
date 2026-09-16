c6ef4f9b7e2169fb61f58692d33ae15b0493952b
Phil Henderson <phillip.henderson@hpe.com>
2026-09-16 09:19:07 -0400

    Add building daos-mofed-shim RPM.
    Test-provider: ucx+dc_x
    Quick-functional: true
    Skip-test: true
    Skip-Functional-Hardware-Medium-Verbs-Provider-MD-on-SSD: true
    Skip-Functional-Hardware-Large-MD-on-SSD: true
    Test-tag: test_setup
    
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- utils/rpms/daos.sh
+++ utils/rpms/daos.sh
@@ -483,3 +483,9 @@ DEPENDS=("daos-tests = ${VERSION}-${RELEASE}")
 DEPENDS+=("daos-client-tests-openmpi = ${VERSION}-${RELEASE}")
 DEPENDS+=("daos-serialize = ${VERSION}-${RELEASE}")
 build_package "daos-tests-internal"
+
+EXTERNAL_DEPENDS=("libmpi.so.40()(64bit)")
+PROVIDES=("libmpi.so.40()(64bit)(openmpi-x86_64)")
+EXTERNAL_DEPENDS+=("libmpi_cxx.so.40()(64bit)")
+PROVIDES+=("libmpi_cxx.so.40()(64bit)(openmpi-x86_64)")
+build_package "daos-mofed-shim"
